61b0af130910ca0eae6826593005ee6a4e1a13e8
thekid <timm.friebe@gmail.com>
2002-04-05 15:57:07 +0000

    -Callbacks direkt in den Caller
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@27 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/xml/XMLParser.class.php
+++ skeleton/xml/XMLParser.class.php
@@ -19,48 +19,12 @@
     
     function &_create() {
       $this->parser = xml_parser_create();
-      xml_set_object($this->parser, $this);
-      xml_set_element_handler($this->parser, '_startElement', '_endElement');
-      xml_set_character_data_handler($this->parser, '_CData');
-      xml_set_default_handler($this->parser, '_defaultHandler');
       xml_parser_set_option($this->parser, XML_OPTION_CASE_FOLDING, FALSE);
       return $this->parser;
     }
     
     function _free() {
-      if (NULL != $this->parser) return xml_parser_free($this->parser);
-    }
-    
-    function _callMethod($methodName, $attributes) {      
-      $methodName= '_pCall'.$methodName;
-      if (method_exists($this->callback, $methodName)) {
-        $this->callback->$methodName($attributes);
-      }
-    }
-    
-    function _startElement($parser, $name, $attrs) {
-      $this->_callMethod('StartElement', array(
-        'name'  => $name,
-        'attrs' => $attrs
-      ));
-    }
-   
-    function _endElement($parser, $name) {
-      $this->_callMethod('EndElement', array(
-        'name'  => $name
-      ));
-    }
-
-    function _CData($parser, $cdata) {
-      $this->_callMethod('CData', array(
-        'cdata' => $cdata
-      ));
-    }
-
-    function _defaultHandler($parser, $data) {
-      $this->_callMethod('Default', array(
-        'data'  => $data
-      ));
+      if (is_resource($this->parser)) return xml_parser_free($this->parser);
     }
     
     function &_error() {
@@ -73,14 +37,20 @@
       return $this->error;
     }
     
-    function parse($data, $isFinal= 0) {
+    function parse($data) {
       unset($this->error);
       if (NULL == $this->parser) $this->_create();
       if (!isset($this->callback) || !is_object($this->callback)) return throw(
         E_PARAM_EXCEPTION,
         $this->callback.' is not an object'
       );
-      if (!xml_parse($this->parser, $data, $isFinal)) return throw(
+      
+      xml_set_object($this->parser, $this->callback);
+      xml_set_element_handler($this->parser, '_pCallStartElement', '_pCallEndElement');
+      xml_set_character_data_handler($this->parser, '_pCallCData');
+      xml_set_default_handler($this->parser, '_pCallDefault');
+
+      if (!xml_parse($this->parser, $data)) return throw(
         E_FORMAT_EXCEPTION,
         $this->_error()
       );
