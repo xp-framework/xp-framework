7e177b932b5cdf0ef9cd863f0ca4613000b2d57d
thekid <timm.friebe@gmail.com>
2002-04-19 11:28:52 +0000

    -auf parse_ini_file umgestellt -Funktionen fuer Sektionen-Iteration hinzugefuegt -Funktionen zum Lesen von Sektionen, Strings, Integers und Booleans hinzugefuegt
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@39 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/util/Properties.class.php
+++ skeleton/util/Properties.class.php
@@ -1,31 +1,70 @@
 <?php
   class Properties extends Object {
-  
-    function Properties() {
-      $this->__construct();
+    var
+      $filename;
+      
+    var
+      $_data= NULL;
+      
+    function __construct($filename) {
+      $this->filename= $filename;
+      Object::__construct();
     }
     
-    function __construct() {
-      parent::__construct();
-    }
-    
-    function load($urn) {
-      $fd= fopen($urn, 'r');
-      while ($str= trim(chop(fgets($fd, 4096)))) {
-        list($key, $val)= explode('=', $str, 2);
-        if (defined($key)) {
-          $result= throw(
-            E_FORMAT_EXCEPTION,
-            $key.' already defined'
-          );
-        }
-        define($key, $val);
+    function _load() {
+      if (NULL != $this->_data) return;
+      
+      $this->_data= parse_ini_file($this->filename, 1);
+      if (!is_array($this->_data)) {
+        return throw(E_FORMAT_EXCEPTION, $this->filename.' format corrupt!');
       }
-      return $result;
     }
     
-    function __destruct() {
-      parent::__destruct();
+    function getFirstSection() {
+      $this->_load();
+      reset($this->_data);
+      return key($this->_data);
     }
+    
+    function getNextSection() {
+      $this->_load();
+      if (!next($this->_data)) return FALSE;
+      return key($this->_data);
+    }
+    
+    function readSection($name, $default= NULL) {
+      $this->_load();
+      return isset($this->_data[$name]) 
+        ? $this->_data[$name] 
+        : $default
+      ;
+    }
+    
+    function readString($section, $key, $default= '') {
+      $this->_load();
+      return isset($this->_data[$section][$key])
+        ? $this->_data[$section][$key]
+        : $default
+      ;
+    }
+    
+    function readInteger($section, $key, $default= 0) {
+      $this->_load();
+      return isset($this->_data[$section][$key])
+        ? intval($this->_data[$section][$key])
+        : $default
+      ;
+    }
+
+    function readBool($section, $key, $default= FALSE) {
+      $this->_load();
+      if (!isset($this->_data[$section][$key])) return $default;
+      return (
+        strcasecmp('on', $this->_data[$section][$key]) ||
+        strcasecmp('yes', $this->_data[$section][$key]) ||
+        strcasecmp('true', $this->_data[$section][$key])
+      );
+    }
+
   }
 ?>
