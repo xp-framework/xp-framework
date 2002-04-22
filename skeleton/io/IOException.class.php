06c45703daca14f59586d2d75859213ec2393c7b
thekid <timm.friebe@gmail.com>
2002-04-22 17:24:31 +0000

    -Initial Release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@43 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/io/IOException.class.php
@@ -0,0 +1,15 @@
+<?php
+/* Diese Klasse ist Teil des XP-Frameworks
+ * 
+ * $Id$
+ */
+ 
+  /**
+   * Kapselt die IOException
+   *
+   * @see Exception
+   */
+  class IOException extends Exception {
+  
+  }
+?>
