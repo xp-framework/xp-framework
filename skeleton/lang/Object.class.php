bac4c9b4cf891d01a142ba772238ee5548c6eb30
thekid <timm.friebe@gmail.com>
2002-04-22 17:33:21 +0000

    -Doku -getName() => Gibt den vollen Namen der Klasse zurck, bspw. lang.Object
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@44 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/lang/Object.class.php
+++ skeleton/lang/Object.class.php
@@ -1,15 +1,41 @@
 <?php
+/* Diese Klasse ist Teil des XP-Frameworks
+ * 
+ * $Id$
+ */
+ 
+  /**
+   * Basis-Objekt für alle anderen
+   */
   class Object {
+    /**
+     * Constructor-Wrapper bis PHP5
+     */
     function Object($params= NULL) {
       $this->__construct($params);
     }
-    
+
+    /**
+     * Constructor
+     */
     function __construct($params= NULL) {
       if (NULL == $params) return;
       foreach ($params as $key=> $val) $this->$key= $val;
     }
     
+    /**
+     * Destructor
+     */
     function __destruct() {
       unset($this);
+    }
+    
+    /** 
+     * Gibt den vollen Namen der Klasse zurück, bspw. lang.Object
+     *
+     * @return  string Voller Name
+     */
+    function getName() {
+      return $GLOBALS['php_class_names'][get_class($this)];
     }
   }
