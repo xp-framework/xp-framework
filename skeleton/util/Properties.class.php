25c369d5f5643eb956c297afd5d9a48cac4021bb
thekid <timm.friebe@gmail.com>
2002-04-22 17:08:15 +0000

    -Viel Doku, neue Exceptions
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@41 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/util/Properties.class.php
+++ skeleton/util/Properties.class.php
@@ -1,37 +1,104 @@
 <?php
+/* Diese Klasse ist Teil des XP-Frameworks
+ * 
+ * $Id$
+ */
+
+  uses('io.IOException');
+  
+  /**
+   * Kapselt das Property-File
+   *
+   * Property-Files sind wie folgt aufgebaut
+   * <pre>
+   * [Sektion]
+   * Key=Wert
+   * Key2="Wert"
+   * ; Kommentar
+   * [Sektion2]
+   * Key=Wert2
+   * </pre>
+   */
   class Properties extends Object {
     var
-      $filename;
-      
-    var
+      $_file,
       $_data= NULL;
       
+    /**
+     * Constructor
+     *
+     * @param   string filename Dateiname (.ini-Datei)
+     */
     function __construct($filename) {
-      $this->filename= $filename;
+      $this->_file= $filename;
       Object::__construct();
     }
     
+    /**
+     * Properties aus Datei laden, falls nötig
+     *
+     * @access  private
+     * @throws  IOException, wenn der Dateiname nicht gefunden werden kann
+     */
     function _load() {
       if (NULL != $this->_data) return;
       
       $this->_data= parse_ini_file($this->filename, 1);
-      if (!is_array($this->_data)) {
-        return throw(E_FORMAT_EXCEPTION, $this->filename.' format corrupt!');
-      }
+      if (FALSE === $this->_prop) return throw(new IOException($this->_file.' not found'));
     }
     
+    /**
+     * Properties speichern
+     *
+     * @access  public
+     */
+    function save() {
+      $fd= fopen($this->_file, 'w');
+      foreach ($this->_data as $section=> $values) {
+        fputs($fd, sprintf("[%s]\n", $section));
+        foreach ($values as $key=> $val) {
+          fputs($fd, sprintf(
+            "%s=%s\n",
+            $key,
+            (is_string($val) ? '"'.$val.'"' : $val)
+          ));
+        }
+      }
+      fclose($fd);
+    }
+
+    /**
+     * Die erste Sektion der Properties holen
+     *
+     * @access  public
+     * @return  string Sektionsname
+     */
     function getFirstSection() {
       $this->_load();
       reset($this->_data);
       return key($this->_data);
     }
     
+    /**
+     * Die nächste Sektion der Properties holen
+     *
+     * @access  public
+     * @return  mixed Sektionsname, bzw. FALSE, wenn keine weiteren existieren
+     */
     function getNextSection() {
       $this->_load();
       if (!next($this->_data)) return FALSE;
       return key($this->_data);
     }
     
+    /**
+     * Eine ganze Sektion zurückgeben
+     *
+     * @access  public
+     * @param   string name Name der Sektion
+     * @param   default default NULL Die Rückgabe, falls die Sektion nicht existiert
+     * @return  mixed Sektion als assoziativer Array, bzw. $default
+     */
     function readSection($name, $default= NULL) {
       $this->_load();
       return isset($this->_data[$name]) 
@@ -40,6 +107,15 @@
       ;
     }
     
+    /**
+     * Einen Wert als String zurückgeben
+     *
+     * @access  public
+     * @param   string section Name der Sektion
+     * @param   string key Name der Keys
+     * @param   default default '' Die Rückgabe, falls der Key bzw. die Sektion nicht existiert
+     * @return  mixed Value-String, bzw. $default
+     */ 
     function readString($section, $key, $default= '') {
       $this->_load();
       return isset($this->_data[$section][$key])
@@ -48,6 +124,15 @@
       ;
     }
     
+    /**
+     * Einen Wert als Integer zurückgeben
+     *
+     * @access  public
+     * @param   string section Name der Sektion
+     * @param   string key Name der Keys
+     * @param   default default 0 Die Rückgabe, falls der Key bzw. die Sektion nicht existiert
+     * @return  mixed Value als Integer, bzw. $default
+     */ 
     function readInteger($section, $key, $default= 0) {
       $this->_load();
       return isset($this->_data[$section][$key])
@@ -56,6 +141,15 @@
       ;
     }
 
+    /**
+     * Einen Wert als Boolean zurückgeben
+     *
+     * @access  public
+     * @param   string section Name der Sektion
+     * @param   string key Name der Keys
+     * @param   default default FALSE Die Rückgabe, falls der Key bzw. die Sektion nicht existiert
+     * @return  mixed TRUE, key 'on', 'yes' oder 'true' ist, FALSE sonst, bzw. $default
+     */ 
     function readBool($section, $key, $default= FALSE) {
       $this->_load();
       if (!isset($this->_data[$section][$key])) return $default;
