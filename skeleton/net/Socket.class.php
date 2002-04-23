d9115fb66097cfeeab3f6c9125cb2aa6673d5849
thekid <timm.friebe@gmail.com>
2002-04-23 17:11:38 +0000

    -Doku
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@65 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/net/Socket.class.php
+++ skeleton/net/Socket.class.php
@@ -1,74 +1,112 @@
 <?php
-  import('net/NetException');
+/* Diese Klasse ist Teil des XP-Frameworks
+ *
+ * $Id$
+ */
+ 
+  uses('io.IOException');
   
   class Socket extends Object {
     var
       $host,
       $port,
-      $timeout;
-  
-    var
+      $timeout= 2,
       $_hSocket;
-      
-    function Socket($params= NULL) {
-      $this->__construct($params);
-    }
     
-    function __construct($params= NULL) {
-      // Default timeout: 2 seconds
-      $this->timeout= 2;
-      
-      parent::__construct($params);
-    }
-    
+    /**
+     * Gibt zurück, ob eine Connection offen ist
+     *
+     * @access  public
+     * @return  bool Connection ist offen
+     */
     function isConnected() {
       return isset($this->_hSocket) ? 1 : 0;
     }
     
+    /**
+     * Verbindung herstellen
+     *
+     * @access  public
+     * @return  bool Status
+     * @throws  IllegalArgumentException wenn host oder port leer
+     * @throws  IOException wenn der Connect nicht (innerhalb des Timeouts) hergestellt werden konnte
+     */
     function connect() {
-      if ($this->isConnected()) return throw(
-        E_ILLEGAL_STATE_EXCEPTION,
-        'sock::socket already open'
+      if ($this->isConnected()) return 1;
+      
+      if (empty($this->host) || empty($this->port)) return (
+        throw(new IllegalArgumentException('Socket::connect ===> no host specified'))
       );
-      if (empty($this->host)) return throw(
-        E_ILLEGAL_ARGUMENT_EXCEPTION,
-        'sock::no host specified'
-      );
-      if (!$this->_hSocket= fsockopen($this->host, $this->port, $errno, $errstr, $this->timeout)) return throw(
-        E_NET_CONNECT_EXCEPTION, sprintf(
+      if (!$this->_hSocket= fsockopen($this->host, $this->port, $errno, $errstr, $this->timeout)) return (
+        throw(new IOException(sprintf(
           'sock::failed connecting to %s:%s within %s seconds [%d: %s]',
           $this->host,
           $this->port,
           $this->timeout,
           $errno,
           $errstr
-        )
+        )))
       );
       return 1;
     }
     
+    /**
+     * Vom Socket lesen
+     *
+     * @access  public
+     * @param   int size default 4096 Anzahl zu lesender Bytes
+     * @return  string Gelesene Daten
+     */
     function read($size= 4096) {
       return fgets($this->_hSocket, $size);
     }
     
+    /**
+     * Testet auf EOF vom Socket
+     *
+     * @access  public
+     * @return  bool EOF erhalten
+     */
     function eof() {
       return feof($this->_hSocket);
     }
     
+    /**
+     * Schreibt auf den Socket
+     *
+     * @access  public
+     * @param   string str Der zu schreibende String
+     * @return  bool Ob's geklappt hat
+     */    
     function write($str) {
       return fputs($this->_hSocket, $str);
     }
-    
+
+    /**
+     * Socket Block-Modus setzen
+     *
+     * @access  public
+     * @param   bool blockMode
+     * @see     php://socket_set_blocking
+     */
     function block($blockMode) {
       socket_set_blocking($this->_hSocket, $blockMode);
     }
     
+    /**
+     * Socket schließen
+     *
+     * @access  public
+     */
     function close() {    
       if (!$this->isConnected()) return;
       @fclose($this->_hSocket);
       unset($this->_hSocket);
     }
     
+    /**
+     * Destructor
+     */
     function __destruct() {
       $this->close();
       parent::__destruct();
