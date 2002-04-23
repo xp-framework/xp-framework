fa84b8ac172409be3c4b308f8ea7eeda94172562
thekid <timm.friebe@gmail.com>
2002-04-23 11:08:35 +0000

    -Initial Release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@59 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/rdbms/SQLException.class.php
@@ -0,0 +1,21 @@
+<?php
+/* Diese Klasse ist Teil des XP-Frameworks
+ * 
+ * $Id$
+ */
+ 
+  /**
+   * Kapselt SQL-Exceptions
+   * 
+   * Besonderes:
+   * - in e->sql findet sich - falls vorhanden - der Query-String, in e->code der SQL-Returncode
+   * - SQL-Returncode ist bspw. bei einer Sybase 1205 für Deadlock
+   *
+   * @see Exception
+   */
+  class SQLException extends Exception {
+    var 
+      $sql,
+      $code;
+  }
+?>
