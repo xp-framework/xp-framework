2ac79340bfa569395917a5c75729f0958d6bd200
thekid <timm.friebe@gmail.com>
2002-05-29 16:17:27 +0000

    -Permanente Variablen und Assoziation
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@96 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/net/session/ToolSession.class.php
+++ skeleton/net/session/ToolSession.class.php
@@ -7,6 +7,10 @@
   define("SESS_TOOL",	0);
   define("SESS_KEY",	1);
 
+  /**
+   * Session-Klasse
+   *
+   */
   class ToolSession extends Object {
     var $ID, $Error, $Debug= 0;
     var $tool_id, $changed;
@@ -46,15 +50,36 @@
       }
     }
     
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
     function logline_text($key, $val) {
       if(!$this->Debug) return 0;
       LOG::info("ToolSession::$key => $val");
     }
     
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
     function urlencode($val) {
       return str_replace("_", "%5F", urlencode($val));
     }
-        
+     
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */   
     function connect() {
       $this->ss_connect($this->remote, $this->port);
       if($this->ss_status!= "+OK") {
@@ -70,17 +95,31 @@
       return 1;
     }
     
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
     function destroy() {
       fclose($this->ss_socket);
     }
-    
-    // Session Keys zurückgeben:
-    // Defaultwert -1 => nur die des aktuellen Tools
-    // 0xFFFF => die aller Tools
+
+    /**
+     * (Insert method's description here)
+     * Session Keys zurückgeben:
+     * Defaultwert -1 => nur die des aktuellen Tools
+     * 0xFFFF => die aller Tools
+     * 
+     * @access  
+     * @param   
+     * @return  
+     */    
     function session_keys($tool_id= -1) {
       if($tool_id< 0) $tool_id= $this->tool_id;
       if($tool_id== 0xFFFF) $tool_id= -1;
-      $keys= explode(" ", urldecode($this->ss_talk("session_keys ".$this->urlencode($this->ID)." tmp")));
+      $keys= explode(" ", urldecode($this->ss_talk("session_keys ".$this->urlencode($this->ID).' tmp')));
       $ret_keys= array();
       foreach($keys as $i=> $key) {
         $cmp_tool_id= substr($key, 0, 4);
@@ -89,19 +128,77 @@
       return $ret_keys;
     }
     
-    function print_debug() {
-      $keys= $this->session_keys();
-      while(list($i, $key)= each($keys)) {
-        logline_text("$i::$key", $this->var_get($key));
-        flush();
-      }
-    }
-    
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
+    function associate($uid) {
+      $command= sprintf("session_associate %s %s",
+        $this->urlencode($this->ID),
+        $this->urlencode($uid)
+      );
+      $this->logline_text("command", $command);
+      $this->ss_talk($command);
+      return ($this->status()== "+OK")? 1: 0;
+    } 
+
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
+    function dissociate() {
+      $command= sprintf("session_dissociate %s",
+        $this->urlencode($this->ID)
+      );
+      $this->logline_text("command", $command);
+      $this->ss_talk($command);
+      return ($this->status()== "+OK")? 1: 0;
+    } 
+
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
     function is_valid () {
       $this->ss_talk("session_isvalid ".$this->urlencode($this->ID));
       return ($this->status()== "+OK")? 1: 0;
     }
     
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
+    function var_regPerm($key, $value) {
+      $command= sprintf("var_write %s perm %04d%s %s",
+        $this->urlencode($this->ID),
+        $this->tool_id,
+        $this->urlencode($key),
+        $this->urlencode(serialize($value))
+      );
+      $this->logline_text("command", $command);
+      $this->ss_talk($command);
+      return ($this->status()== "+OK")? 1: 0;
+    }
+    
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
     function var_reg($key, $value) {
       $command= sprintf("var_write %s tmp %04d%s %s",
         $this->urlencode($this->ID),
@@ -114,6 +211,13 @@
       return ($this->status()== "+OK")? 1: 0;
     }
     
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
     function var_regGlobal($key, $value) {
       $command= sprintf("var_write %s tmp %s %s",
         $this->urlencode($this->ID),
@@ -125,6 +229,13 @@
       return ($this->status()== "+OK")? 1: 0;
     }
     
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
     function var_unreg($key) {
       $this->ss_talk(sprintf("var_delete %s tmp %04d%s",
         $this->urlencode($this->ID),
@@ -133,7 +244,30 @@
       ));
       return ($this->status()== "+OK")? 1: 0;
     }
+
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
+    function var_unregPerm($key) {
+      $this->ss_talk(sprintf("var_delete %s perm %04d%s",
+        $this->urlencode($this->ID),
+        $this->tool_id,
+        $this->urlencode($key)
+      ));
+      return ($this->status()== "+OK")? 1: 0;
+    }
     
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
     function var_unreg_group($range) {
       $keys= explode(" ", urldecode($this->ss_talk("session_keys ".$this->urlencode($this->ID)." tmp")));
       foreach($keys as $key) {        
@@ -143,6 +277,13 @@
       return $this->changed;
     }
     
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
     function var_get($key, $tool_id= -1) {
       if($tool_id< 0) $tool_id= $this->tool_id;
       $command= sprintf("var_read %s tmp %04d%s",
@@ -156,29 +297,90 @@
       if(!empty($return)) return $return;
     }
     
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
+    function var_getPerm($key, $tool_id= -1) {
+      if($tool_id< 0) $tool_id= $this->tool_id;
+      $command= sprintf("var_read %s perm %04d%s",
+        $this->urlencode($this->ID),
+        $tool_id,
+        $this->urlencode($key)
+      );
+      $return= urldecode(trim($this->ss_talk($command)));
+      $this->logline_text("var_getPerm $command", $return);
+      if (!empty($return)) return unserialize($return);
+    }
+
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */    
     function var_ref($key, &$var) {
       $var= $this->var_get($key);
       return 1;
     }
     
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
     function sync($quiet= 0) {
       // Bei Hannah nicht nötig:)
       return 1;
     }
     
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
     function status() {
       return($this->ss_status);
     }
 
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
     function kill() {
       $this->logline_text("session_terminate", "{this->ID} $this->ID");
       $this->ss_talk("session_terminate ".$this->urlencode($this->ID));
     }
     
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
     function __destruct() {
       if (isset($this->ss_socket)) fclose($this->ss_socket);
     }
     
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
     function ss_connect($remote, $port) {
       $this->logline_text('connect ', $remote.':'.$port);
       $socket= fsockopen($remote, $port, $err, $errstr, 2);   // 2 Sekunden Timeout
@@ -191,6 +393,13 @@
       $this->logline_text("ss_connect", $this->ss_status);
     }
     
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
     function ss_create($ttl= 0) {
       if($ttl== 0) $ttl= $this->TTL;
       $this->logline_text("new::ID", "ss_talk session_create $ttl");
@@ -199,6 +408,13 @@
       return ($this->status()== "+OK")? 1: 0;
     }
     
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
     function ss_talk($query) {
       if (!isset($this->ss_socket)) {
         $this->connect();
@@ -223,6 +439,7 @@
       }
       $this->ss_status= $status;
       $this->logline_text("ss_talk.done", $rest);
+      
       return $rest;
     }
 
