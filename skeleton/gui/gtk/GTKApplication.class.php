94a99cd97af0a93c41deacd7ce2031694bb297ef
thekid <timm.friebe@gmail.com>
2002-07-08 10:32:39 +0000

    -Doku
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@141 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/gui/gtk/GTKApplication.class.php
+++ skeleton/gui/gtk/GTKApplication.class.php
@@ -3,20 +3,47 @@
  *
  * $Id$
  */
+ 
+  uses('util.log.Logger', 'util.log.FileAppender');
+ 
   class GtkApplication extends Object {
-    var $name;
-    var $window;
-    var $rcFile;
+    var 
+      $name,
+      $window,
+      $rcFile;
+    
+    var 
+      $_log= NULL,
+      $_done= FALSE;
 
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   string name Name der Anwendung
+     */
     function __construct($name) {
       $this->name= $name;
+      $this->_log= new Logger();
+      $this->_log->identifier= $name;
+      $this->_log->addAppender(new FileAppender('php://stderr'));
       parent::__construct();
     }
 
+    /**
+     * Erzeugt das Hauptfenster
+     *
+     * @access  private
+     */
     function _create() {
       if (!isset($this->window)) $this->window= &new GtkWindow();
     }
 
+    /**
+     * Initialisiert die Anwendung
+     *
+     * @access  public
+     */
     function init() {
       $this->log('init');
       if (isset($this->rcFile)) Gtk::rc_parse($this->rcFile); 
@@ -24,19 +51,38 @@
       $this->window->connect('destroy', array(&$this, 'done'));
       $this->window->show_all();
     }
-    
-    function log($msg, $var= NULL) {
-      echo $msg.' :: ';
-      var_dump($var);
+
+    /**
+     * Logmeldung auf die Konsole
+     *
+     * @access  public
+     * @param   mixed vars Variablen
+     */    
+    function log() {
+      $variables= func_get_args();
+      call_user_func_array(array($this->_log, 'info'), $variables);
     }
-    
+
+    /**
+     * Anwendung ausführen
+     *
+     * @access  public
+     */    
     function run() {
       $this->log('main');
       Gtk::main();
     }
-    
+
+    /**
+     * Anwendung beenden
+     *
+     * @access  public
+     */       
     function done() {
+      if ($this->_done) return;
+      
       $this->log('done');
+      $this->_done= TRUE;
       Gtk::main_quit();
     }
   }
--- skeleton/gui/gtk/GTKGladeApplication.class.php
+++ skeleton/gui/gtk/GTKGladeApplication.class.php
@@ -3,6 +3,7 @@
  *
  * $Id$
  */
+ 
   uses('gui.gtk.GTKApplication');
   
   class GTKGladeApplication extends GTKApplication {
@@ -10,16 +11,34 @@
     var $gladeFile;
     var $windowWidget;
 
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   string name Name der Anwendung
+     * @param   string gladeFile Dateiname des .glade-Files
+     * @param   string windowWidget default 'window1' Name des Hauptfensters
+     */
     function __construct($name, $gladeFile, $windowWidget= 'window1') {
       parent::__construct($name);
       $this->gladeFile= $gladeFile;
       $this->windowWidget= $windowWidget;
     }
 
+    /**
+     * Erzeugt das Hauptfenster
+     *
+     * @access  private
+     */
     function _create() {
       if (!isset($this->window)) $this->window= &$this->glade->get_widget($this->windowWidget);
     }
 
+    /**
+     * Initialisiert die Anwendung
+     *
+     * @access  public
+     */
     function init() {
       $this->log('init', $this->gladeFile);
       if (isset($this->rcFile)) Gtk::rc_parse($this->rcFile); 
@@ -27,6 +46,13 @@
       parent::init();
     }
     
+    /**
+     * Gibt ein Widget aus dem Glade-File zurück
+     *
+     * @access  public
+     * @param   string name Name des Widgets
+     * @return  GtkWidget Das korrespondierende Widget
+     */
     function &widget($name) {
       return $this->glade->get_widget($name);
     }
