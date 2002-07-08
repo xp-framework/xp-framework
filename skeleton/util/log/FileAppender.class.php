de35399b5a690d0efd3c53ad021f885907d3e0cc
thekid <timm.friebe@gmail.com>
2002-07-08 10:53:18 +0000

    -Auf abstrakter Basisklasse LogAppender aufbauen
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@149 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/util/log/FileAppender.class.php
+++ skeleton/util/log/FileAppender.class.php
@@ -1,5 +1,7 @@
 <?php
-  class FileAppender extends Object {
+  uses('util.log.LogAppender');
+  
+  class FileAppender extends LogAppender {
     var 
       $filename;
     
@@ -10,7 +12,7 @@
      */
     function __construct($filename= 'php://stderr') {
       $this->filename= $filename;
-      Object::__construct();
+      parent::__construct();
     }
     
     /**
@@ -26,23 +28,6 @@
       }
       fputs($fd, "\n");
       fclose($fd);
-    }
-    
-    /**
-     * Gibt eine Variable schön lesbar zurück
-     *
-     * @access public
-     * @param  mixed var Variable jeglichen Types
-     * @return string Ein Printout der Variable
-     */
-    function varSource($var) {
-      if (is_array($var) || is_object($var)) {
-        ob_start();
-        var_dump($var);
-        $var= ob_get_contents();
-        ob_end_clean();
-      }
-      return $var;
     }
   }
 ?>
