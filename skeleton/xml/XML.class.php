0383a70ba05ea9595a98c756bc50546445bc999c
thekid <timm.friebe@gmail.com>
2002-07-15 06:01:56 +0000

    -Added documentation -method getDeclaration() instead of defines
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@166 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/xml/XML.class.php
+++ skeleton/xml/XML.class.php
@@ -1,7 +1,28 @@
 <?php
+/* Diese Klasse ist Teil des XP-Frameworks
+ *
+ * $Id$
+ */
+
   if (!defined('XML_ENCODING_DEFAULT')) define('XML_ENCODING_DEFAULT',        'iso-8859-1');
   if (!defined('XML_DECLARATION'))      define('XML_DECLARATION',             '<?xml version="1.0" encoding="'.XML_ENCODING_DEFAULT.'" ?>');
 
+  /**
+   * Base class for
+   *
+   * @access public
+   */
   class XML extends Object {
+    var $encoding= 'iso-8859-1';
+    
+    /**
+     * Returns XML declaration
+     *
+     * @access  public
+     * @return  string declaration
+     */
+    function getDeclaration() {
+      return '<?xml version="1.0" encoding="'.$this->encoding.'" ?>';
+    }
   }
 ?>
