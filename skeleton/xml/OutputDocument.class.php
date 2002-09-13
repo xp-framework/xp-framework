89626f4d6cd6ff28b37aebafcb56a509399d53b9
thekid <timm.friebe@gmail.com>
2002-09-13 05:44:51 +0000

    -Remove obsolete constructor -Move errorhandling to whithin the class, xslt_set_error_handler accepts array($this, "function") syntax
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@220 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/xml/OutputDocument.class.php
+++ skeleton/xml/OutputDocument.class.php
@@ -22,17 +22,13 @@
     // Output-Buffering aktiv?
     var $_ob;
     
-    function OutputDocument($params= NULL) {
-      $this->__construct($params);
-    }
-    
     function __construct($params= NULL) {
       parent::__construct($params);
       $this->language= 'default';
       $this->product= 'default';
       
-      $this->processor= new XSLProcessor();
-      $this->processor->setErrorHandler('OutputDocument__XSL_error');
+      $this->processor= &new XSLProcessor();
+      $this->processor->setErrorHandler(array($this, '_message'));
 
       ob_start();
       $this->_ob= TRUE;
@@ -73,6 +69,33 @@
       return FALSE;
     }
     
+    function _message($parser, $code, $level, $error) {
+      LOG::warn('xsl error code#'.$code.', level '.$level);
+      LOG::warn(array_values($error));
+      list($msgtype, $code, $module, $msg)= array_values($error);
+      
+      // Only erors, no log or warnings
+      if ('error' != $msgtype) return;
+      
+      // Status
+      $codeMap= array(
+        0x0004 => 404,          // [SABLOT] Cannot open File 'bla.xsl'
+        0x0F00 => 501           // [E_USER] Session
+      );
+      $statusCode= isset($codeMap[$code]) ? $codeMap[$code] : 500;
+      header('HTTP/1.1 '.$statusCode.' Error');
+      ob_end_clean();
+
+      // Fehlerausgabe
+      $errorFile= getenv('DOCUMENT_ROOT').'/../static/'.getenv('LANG').'/error'.$statusCode.'.html';
+      if (!file_exists($errorFile)) {
+        $errorFile= getenv('DOCUMENT_ROOT').'/../static/error405.html';
+      }
+      @readfile($errorFile);
+      
+      return TRUE;
+    }
+    
     function __destruct() {
       if ($this->_ob) {
         LOG::info('render ['.$this->_ob.']');
@@ -83,51 +106,5 @@
       }
       parent::__destruct();
     }
-  }
-  
-  function OutputDocument__XSL_error($parser, $code, $level, $error) {
-    if (
-      (3 != $code) &&   // XSL-Error
-      (4 != $code)      // Not found
-    ) return;
-
-    LOG::warn('xsl #'.$code.'@'.$level.' => "'.$error['code'].'"');
-    LOG::warn($error);
-
-    // Status
-    $codeMap= array(
-      0x0004 => 404,          // [SABLOT] Cannot open File 'bla.xsl'
-      0x0F00 => 501           // [E_USER] Session
-    );
-    $statusCode= isset($codeMap[$error['code']]) ? $codeMap[$error['code']] : 500;
-    header('HTTP/1.1 '.$statusCode.' Error');
-    ob_end_clean();
-
-    // Fehlerausgabe
-    $errorFile= getenv('DOCUMENT_ROOT').'/../static/'.getenv('LANG').'/error'.$statusCode.'.html';
-    if (!file_exists($errorFile)) {
-      $errorFile= getenv('DOCUMENT_ROOT').'/../static/error405.html';
-    }
-    @readfile($errorFile);
-
-    /* Sablotron & Debian:: ARGL! Das $error-Array ist total korrumpiert
-    $errorDocument= @implode('', @file(
-      $DOCUMENT_ROOT.'/../static/'.getenv('LANG').'/error'.$statusCode.'.html'
-    ));
-    if (empty($errorDocument)) $errorDocument= '<xp:value-of select="reason"/>';
-    echo str_replace(
-      '<xp:value-of select="reason"/>', 
-      sprintf(
-        "%s #%d at line %d in <b>%s</b>\n%s\n",
-        $error['msgtype'],
-        $code,
-        @$error['line'],
-        preg_replace('|(file:)?'.dirname($DOCUMENT_ROOT).'|', '', @$error['URI']),
-        preg_replace('|(file:)?'.dirname($DOCUMENT_ROOT).'|', '', $error['msg'])
-      ),
-      $errorDocument
-    );
-    */
-    return TRUE;
   }
 ?>
