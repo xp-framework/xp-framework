be2d104770ccfb505ba96722ef3b6f52a514c7ad
thekid <timm.friebe@gmail.com>
2002-09-13 05:55:38 +0000

    -These should be here
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@222 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/text/PregMatcher.class.php
@@ -0,0 +1,54 @@
+<?php
+  /* Diese Klasse ist Teil des XP-Frameworks
+   *
+   * $Id$
+   */
+   
+  uses(
+    'util.text.SyntaxCheck'
+  );
+  
+  /**
+   * Regular expression matcher base class, will need to be extended, e.g.:
+   * <pre>
+   *   class EMailChecker extends PregMatcher {
+   *     var $_expressions= array(
+   *       'localpart' => '#^[^@]$#',
+   *       'domain'    => '#^[a-z.]+\.[a-z]{2,8}$#'
+   *     );
+   *
+   *     var $localpart, $domain;
+   *   }
+   * </pre>
+   * 
+   * @see  php://preg
+   */
+  class PregMatcher extends SyntaxCheck {
+    var 
+      $_expressions= array(),
+      $_matches= array();
+    
+    /**
+     * Validation
+     *
+     * @access  public
+     * @return  bool True when all of the values listed as arraykeys in _expressions match
+     */
+    function validate() {
+      $result= TRUE;
+      foreach ($this->_expressions as $key=> $re) {
+        $this->$key= trim(chop($this->$key)); 
+        $match= preg_match($re, $this->$key);
+        
+        LOG::info($key.'::'.$this->$key.'=~ '.$re.'? '.$match);
+        $this->_matches[$key]= $match;
+        if (!$match) {
+          $this->addError($key, 'match');
+          $result= FALSE;
+        }
+      }
+      return $result;
+    }
+
+  }
+?>
