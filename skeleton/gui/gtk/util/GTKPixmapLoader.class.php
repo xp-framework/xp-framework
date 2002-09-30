8664d678afb57703b72608d23836d6bf3375c539
thekid <timm.friebe@gmail.com>
2002-09-30 10:24:40 +0000

    -Initial Release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@233 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/gui/gtk/util/GTKPixmapLoader.class.php
@@ -0,0 +1,106 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+ 
+  /**
+   * Pixmap loader
+   * 
+   * Pixmaps will be loaded into a struct like this:
+   * <pre>
+   *   ['p:open'] => pixmap_of_open_pixmap,
+   *   ['m:open'] => mask_of_open_pixmap,
+   *   ['p:quit'] => pixmap_of_quit_pixmap,
+   *   ['m:quit'] => mask_of_quit_pixmap
+   * </pre>
+   *
+   * Transparent color will default to black (RGB {0, 0, 0})
+   */
+  class GTKPixmapLoader extends Object {
+    var 
+      $windowRef,
+      $baseDir,
+      $transparentColor;
+
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   GtkWindow window a valid window object
+     * @param   string baseDir default '.' base directory for pixmaps
+     */      
+    function __construct(&$window, $baseDir= '.') {
+      $this->setWindowRef($window);
+      $this->setBase($baseDir);
+      $this->setTransparentColor(new GdkColor(0, 0, 0));
+      parent::__construct();
+    }
+    
+    /**
+     * Sets window reference
+     *
+     * @access  public
+     * @param   GtkWindow window a valid window object
+     */
+    function setWindowRef(&$window) {
+      $this->windowRef= &$window;
+    }
+    
+    /**
+     * Sets basedir for pixmaps
+     *
+     * @access  public
+     * @param   string base base directory
+     */
+    function setBase($base) {
+      $this->baseDir= $base;
+    }
+    
+    /**
+     * Sets transparent color
+     *
+     * @access  public
+     * @param   GdkColor color the color to be transparent
+     */
+    function setTransparentColor(&$color) {
+      $this->transparentColor= &$color;
+    }
+    
+    /**
+     * Loads a pixmap into a container
+     *
+     * @access  private
+     * @param   &array container
+     * @param   string name
+     */
+    function _load(&$container, $name) {
+      list(
+        $container['p:'.$name],
+        $container['m:'.$name]
+      )= Gdk::pixmap_create_from_xpm(
+        &$this->windowRef, 
+        &$this->transparentColor,
+        $this->baseDir.'/'.$name.'.xpm'
+      );
+    }
+    
+    /**
+     * Loads one or more pixmaps
+     *
+     * @access  public
+     * @param   mixed names Either a string or an array of strings containinig the 
+     *          names of the pixmaps to be loaded (w/o trailing .xpm!)
+     * @return  &array pixmaps
+     */
+    function &load($names) {
+      $container= array();
+      if (is_string($names)) $names= array($names);
+      foreach ($names as $name) {
+        $this->_load($container, $name);
+      }
+      
+      return $container;
+    }
+  }
+?>
---
+++ skeleton/gui/gtk/util/GTKWidgetUtil.class.php
@@ -0,0 +1,75 @@
+<?php
+
+  /**
+   * Widget utility class
+   *
+   * @model static
+   */
+  class GTKWidgetUtil extends Object {
+  
+    /**
+     * Connect a list of signals for a widget
+     *
+     * Example:
+     * <pre>
+     *   GTKWidgetUtil::connect($w, array(
+     *     'after:button_press_event' => array(&$this, 'onButtonPressed'),
+     *     ':clicked'                 => array(&$this, 'onButtonClicked', &$data)
+     *   ));
+     * </pre>
+     *
+     * @access  public
+     * @param   &GtkWidget widget
+     * @param   array signals an associative array
+     * @see     php-gtk-doc://GtkObject::connect
+     * @see     php-gtk-doc://GtkObject::connect_after
+     */
+    function connect(&$widget, $signals) {
+      foreach (array_keys($signals) as $key) {
+        list($mode, $signal)= explode(':', $key);
+        
+        switch (sizeof($signals[$key])) {
+          case 1:                           // 'function'
+          case 2:                           // array(&$this, 'function')
+            $handler= &$signals[$key]; 
+            $val= NULL; 
+            break;
+            
+          case 3:                           // array(&$this, 'function', &$custom)
+            $handler= array(&$signals[$key][0], &$signals[$key][1]);
+            $val= &$signals[$key][2];  
+            break;
+        }
+        
+        switch ($mode) {
+          case 'after':
+            $widget->connect_after($signal, $handler, $val);
+            break;
+          default:
+            $widget->connect($signal, $handler, $val);
+            break;
+        }
+      }
+    }
+    
+    /**
+     * Connects a list of signals to a widget's children 
+     *
+     * @access  public
+     * @param   &GtkWidget widget
+     * @see     #connect
+     */
+    function connectChildren(&$widget, $signals) {
+      foreach ($widget->children() as $child) {
+        GTKWidgetUtil::connect($child, $signals);
+      }
+    }
+    
+    function setChildrenSensitive(&$widget, $sensitivity) {
+      foreach ($widget->children() as $child) {
+        if (!isset($sensitivity[$name= $child->get_name()])) continue;
+        $child->set_sensitive($sensitivity[$name]);
+      }    
+    }
+  }
+?>
