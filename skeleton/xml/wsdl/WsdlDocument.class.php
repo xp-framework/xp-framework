a2844b3b32e7fc201d2c616ae7604e3ea949a18c
thekid <timm.friebe@gmail.com>
2002-11-03 18:55:04 +0000

    -Initial Release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@273 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/xml/wsdl/WsdlDocument.class.php
@@ -0,0 +1,191 @@
+<?php
+  uses(
+    'xml.Tree', 
+    'xml.Node',
+    'xml.soap.wsdl.WsdlMessage'
+  );
+  
+  define('XMLNS_WSDL',    'http://schemas.xmlsoap.org/wsdl/');
+  define('XMLNS_XSD',     'http://www.w3.org/2001/XMLSchema');
+  define('XMLNS_SOAP',    'http://schemas.xmlsoap.org/wsdl/soap/');
+  define('XMLNS_SOAPENC', 'http://schemas.xmlsoap.org/soap/encoding/');
+  
+  /**
+   *
+   */
+  class WsdlDocument extends Tree {
+    var 
+      $types    = array(),
+      $messages = array(),
+      $portTypes= array(),
+      $bindings = array(),
+      $service  = array();
+  
+    /**
+     * Constructor
+     *
+     * @access  public
+     */
+    function __construct($name= NULL, $targetNamespace= NULL) {
+      parent::__construct();
+      $this->root= &new Node(array(
+        'name'          => 'definitions',
+        'attribute'     => array(
+          'xmlns:xsd'       => XMLNS_XSD,
+          'xmlns:soap'      => XMLNS_SOAP,
+          'xmlns:soapenc'   => XMLNS_SOAPENC,
+          'xmlns:wsdl'      => XMLNS_WSDL,
+          'xmlns'           => XMLNS_WSDL
+        )
+      ));
+      if (NULL !== $name) $this->setName($name);
+      if (NULL !== $targetNamespace) $this->setTargetNamespace($targetNamespace);
+    }
+    
+    function addNamespace($name, $urn) {
+      $this->root->attribute['xmlns:'.$name]= $urn;
+    }
+    
+    function setTypes(&$schema) {
+      
+    }
+    
+    function addService() {
+    
+    }
+    
+    function addPortType() {
+    
+    }
+    
+    function addBinding() {
+    
+    }
+    
+    /**
+     * Add a message
+     *
+     * @access  public
+     * @param   &xml.soap.wsdl.WsdlMessage message
+     * @throws  IllegalArgumentException when message is not a WsdlMessage object
+     *          or message has already been added
+     * @return  &xml.soap.wsdl.WsdlMessage message
+     */
+    function addMessage(&$message) {
+      if (!is_a($message, 'WsdlMessage')) {
+        return throw(new IllegalArgumentException('message is not a WsdlMessage'));
+      }
+      if (isset($this->messages[$message->name])) {
+        return throw(new IllegalArgumentException('Cannot add message "'.$message->name.'" twice'));
+      }
+      
+      $this->messages[$message->name]= array();
+      $this->messages[$message->name]['obj']= &$message;
+      $this->messages[$message->name]['node']= &$this->root->addChild(new Node(array(
+        'name'        => 'message',
+        'attribute'   => array('name' => $message->name)
+      )));
+      foreach (array_keys($message->parts) as $key) {
+        $n= &$this->messages[$message->name]['node']->addChild(new Node(array(
+          'name'        => 'part',
+          'attribute'   => array('name' => $key)
+        )));
+        if (NULL != $message->parts[$key]->type) {
+          $n->attribute['type']= $message->parts[$key]->namespace.':'.$message->parts[$key]->type;
+        }
+        if (NULL != $message->parts[$key]->element) {
+          $n->attribute['element']= $message->parts[$key]->element;
+        }
+      }
+      
+      return $message;
+    }
+    
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
+    function &getMessageByName($name) {
+      return isset($this->messages[$name]) ? $this->messages[$name]['obj'] : NULL;
+    }
+    
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
+    function getFirstMessage() {
+      reset($this->messages);
+      return key($this->messages);
+    }
+    
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
+    function getNextMessage() {
+      if (FALSE === next($this->messages)) return FALSE;
+      return key($this->messages);
+    }
+    
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
+    function setName($name) {
+      $this->root->attribute['name']= $name;
+    }
+    
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
+    function getName() {
+      return (isset($this->root->attribute['name']) 
+        ? $this->root->attribute['name']
+        : NULL
+      );
+    }
+    
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
+    function setTargetNamespace($ns) {
+      $this->root->attribute['targetNamespace']= $ns;
+    }
+
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
+    function getTargetNamespace() {
+      return (isset($this->root->attribute['targetNamespace']) 
+        ? $this->root->attribute['targetNamespace']
+        : NULL
+      );
+    }
+
+  }
+
+?>
---
+++ skeleton/xml/wsdl/WsdlMessage.class.php
@@ -0,0 +1,92 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  /**
+   * Messages consist of one or more logical parts. Each part is associated 
+   * with a type from some type system using a message-typing attribute.
+   *
+   * And example message's XML representation:
+   * <xmp>
+   *   <message name="doGoogleSearch">
+   *     <part name="key"            type="xsd:string"/>
+   *     <part name="start"          type="xsd:int"/>
+   *     <part name="safeSearch"     type="xsd:boolean"/>
+   *   </message>
+   * </xmp>
+   *
+   * @see http://www.w3.org/TR/wsdl#_messages
+   * @see xml.soap.wsdl.WsdlDocument#addMessage
+   */
+  class WsdlMessage extends Object {
+    var 
+      $name=  '',
+      $parts= array();
+      
+    /**
+     * Constructor
+     *
+     * The argument parts is an associative array, where the keys
+     * define the part names and the values might be one of the following:
+     *
+     * <ul>
+     *   <li>a scalar describing the type
+     *   <li>an array of two values, the first being the type, the second the namespace
+     *   <li>an array of three values, 1st and 2nd as above, the third referencing an element
+     * </ul>
+     *
+     * Example:
+     * <code>
+     *   $d->addMessage(new WsdlMessage('doGoogleSearch', array(
+     *     'key'        => 'string',
+     *     'start'      => 'int',
+     *     'safeSearch' => 'boolean'
+     *   )));
+     * </code>
+     *
+     * @access  public
+     * @param   string name the message's name
+     * @param   array parts default array() 
+     */
+    function __construct($name, $parts= array()) {
+      $this->name= $name;
+      foreach ($parts as $k=> $v) {
+        if (is_scalar($v)) $v= array($v);
+        array_unshift($v, $k);
+        call_user_func_array(array(&$this, 'addPart'), $v);
+      }
+      parent::__construct();
+    }
+    
+    /**
+     * Add a part
+     *
+     * @access  public
+     * @param   string name
+     * @param   string type
+     * @param   string namespace default 'xsd'
+     * @param   string element default NULL
+     */
+    function addPart($name, $type, $namespace= 'xsd', $element= NULL) {
+      $this->parts[$name]= &new stdClass();
+      $this->parts[$name]->type= $type;
+      $this->parts[$name]->namespace= $namespace;
+      $this->parts[$name]->element= $element;
+    }
+    
+    /**
+     * Get a part by its name. The returned object will have three
+     * properties: type, namespace and element
+     *
+     * @access  public
+     * @param   string name
+     * @return  &object part
+     */
+    function &getPartByName($name) {
+      return isset($this->parts[$name]) ? $this->parts[$name] : NULL;
+    }
+  }
+
+?>
