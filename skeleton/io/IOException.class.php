bd977a8de9bf868f541f80c83642d44818ac6501
thekid <timm.friebe@gmail.com>
2002-12-22 01:29:58 +0000

    - Add documentation
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@332 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/io/IOException.class.php
+++ skeleton/io/IOException.class.php
@@ -1,11 +1,13 @@
 <?php
-/* Diese Klasse ist Teil des XP-Frameworks
+/* This class is part of the XP framework
  * 
  * $Id$
  */
  
   /**
-   * Kapselt die IOException
+   * Signals that an I/O exception of some sort has occurred. This 
+   * class is the general class of exceptions produced by failed or 
+   * interrupted I/O operations. 
    *
    * @see Exception
    */
