b47a32a55b9a4fcc405b672a31118af9fb4a5192
thekid <timm.friebe@gmail.com>
2002-12-22 12:10:17 +0000

    - Preparation for new class loader model
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@337 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/lang/ClassLoader.class.php
+++ skeleton/lang/ClassLoader.class.php
@@ -1,5 +1,5 @@
 <?php
-/* Diese Klasse ist Teil des XP-Frameworks
+/* This class is part of the XP framework
  * 
  * $Id$
  */
@@ -7,37 +7,66 @@
   uses('lang.ClassNotFoundException');
   
   /** 
-   * Lädt eine Klasse, instanziiert sie aber nicht
-   * Beispiel: 
-   * <pre>
+   * Loads a class
+   * 
+   * Usage (static calls):
+   * <code>
    *   try(); {
    *     $name= ClassLoader::loadClass($argv[1]);
    *   } if (catch('ClassNotFoundException', $e)) {
    *     die($e->printStackTrace());
    *   }
-   * </pre>
+   *
+   *   $obj= &new $name();
+   * </code>
+   *
+   * Usage (class loader instance):
+   * <code>
+   *   $loader= &new ClassLoader('info.binford6100.webservices');
+   *   try();
+   *     $name= $loader->loadClass($argv[1]);
+   *   } if (catch('ClassNotFoundException', $e)) {
+   *     die($e->printStackTrace());
+   *   }
+   *
+   *   $obj= &new $name();
+   * </code>
    *
    * @access    static
    */
   class ClassLoader extends Object {
-  
+    var $classpath= '';
+    
     /**
-     * Lädt eine Klasse
+     * Constructor. 
+     * The path argument is optional and lets you define where to search for
+     * classes (it will be prefixed to the class name)
      *
-     * @call    static
-     * @param   string className Klassename, bspw. net.http.HTTPConnection
-     * @return  mixed Eine Instanz der Klasse
+     * @access  public
+     * @param   string path default '' classpath
+     */
+    function __construct($path= '') {
+      if (!empty($path)) $this->classpath= $path.'.';
+      parent::__construct();
+    }
+    
+    /**
+     * Load
+     *
+     * @access  public, static
+     * @param   string className fully qualified class name io.File
+     * @return  string class' name for instantiation
+     * @throws  ClassNotFoundException in case the class can not be found
      */
     function loadClass($className) {
       if ('php.' != substr($className, 0, 4)) {
-        uses($className);
-	$phpName= reflect($className);
+        uses($classpath.$className);
+        $phpName= reflect($this->classpath.$className);
       } else {
         $phpName= substr($className, 4);
       }
       if (class_exists($phpName)) return $phpName;
       
-      // Fehler!
       return throw(new ClassNotFoundException(sprintf(
         'class %s [%s] not found',
         $className,
