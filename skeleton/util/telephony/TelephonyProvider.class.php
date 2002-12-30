2e8c4b55630778aeabc3c5772e5caa245c369d11
thekid <timm.friebe@gmail.com>
2002-12-30 18:23:00 +0000

    - Add basic functions - Add createCall() method - Add example
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@493 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/util/telephony/TelephonyProvider.class.php
+++ skeleton/util/telephony/TelephonyProvider.class.php
@@ -4,9 +4,55 @@
  * $Id$
  */
 
+  uses(
+    'util.telephony.TelephonyAddress', 
+    'util.telephony.TelephonyTerminal',
+    'util.telephony.TelephonyCall',
+    'util.telephony.TelephonyException'
+  );
+
   /**
    * Abstract base class for a telephony provider
    * 
+   * Example (using the STLI driver):
+   * <code>
+   *   uses(
+   *     'ch.ecma.StliConnection', 
+   *     'peer.Socket', 
+   *     'util.log.Logger',
+   *     'util.log.FileAppender',
+   *     'util.cmd.ParamString'
+   *   );
+   * 
+   *   $p= &new ParamString();
+   *   if (4 != $p->count) {
+   *     printf("Usage: %s server:port <from> <to>\n", basename($p->value(0)));
+   *     exit();
+   *   }
+   *   list($server, $port)= explode(':', $p->value(1));
+   * 
+   *   $l= &Logger::getInstance();
+   *   $cat= &$l->getCategory();
+   *   $cat->addAppender(new FileAppender('php://stderr'));
+   * 
+   *   $c= &new StliConnection(new Socket($server, $port));
+   *   $c->setTrace($cat);
+   *   try(); {
+   *     $c->connect();
+   *     $term= &$c->getTerminal($c->getAddress('int:'.$p->value(2)));
+   *     $call= &$c->createCall($term, $c->getAddress('ext:'.$p->value(3)));
+   *     $c->releaseTerminal($term);
+   *     $c->close();
+   *   } if (catch('Exception', $e)) {
+   *     $e->printStackTrace();
+   *     exit;
+   *   }
+   * 
+   *   printf("Done\n");
+   * </code>
+   *
+   * @purpose Provides an interface to telephony
+   * @see     http://java.sun.com/products/jtapi/jtapi-1.3/html/overview-summary.html
    */
   class TelephonyProvider extends Object {
     var
@@ -48,12 +94,75 @@
     function connect() { }
 
     /**
-     * Connect and initiate the communication
+     * Close connection and end the communication
      *
      * @access	public
      */
     function close() { }
     
+    /**
+     * Retreive an address
+     *
+     * @access  public  
+     * @param   string number
+     * @return  &util.telephony.TelephonyAddress 
+     */
+    function &getAddress($number) { 
+      return new TelephonyAddress($number);
+    }
     
+    /**
+     * Create a call
+     *
+     * @access  public
+     * @param   &util.telephony.TelephonyTerminal terminal
+     * @param   &util.telephony.TelephonyAddress destination
+     * @throws  IllegalArgumentException in case of parameter type mismatch
+     * @return  &util.telephony.TelephonyCall a call object
+     */
+    function createCall(&$terminal, &$destination) {
+      if (!is_a($terminal, 'TelephonyTerminal')) {
+        trigger_error('type: '.gettype($terminal), E_USER_WARNING);
+        return throw(new IllegalArgumentException('Terminal parameter is not a TelephonyTerminal'));
+      }
+      if (!is_a($destination, 'TelephonyAddress')) {
+        trigger_error('type: '.gettype($destination), E_USER_WARNING);
+        return throw(new IllegalArgumentException('Destination parameter is not a TelephonyAddress'));
+      }
+      return NULL;
+    }
+    
+    /**
+     * Get terminal
+     *
+     * @access  public
+     * @param   &util.telephony.TelephonyAddress address
+     * @throws  IllegalArgumentException in case of parameter type mismatch
+     */
+    function getTerminal(&$address) { 
+      if (!is_a($address, 'TelephonyAddress')) {
+        trigger_error('type: '.gettype($address), E_USER_WARNING);
+        return throw(new IllegalArgumentException('Address parameter is not a TelephonyAddress'));
+      }
+      if (TEL_ADDRESS_INTERNAL !== $address->getType()) {
+        return throw(new IllegalArgumentException('Terminals can only have internal addresses'));
+      }
+      return NULL;
+    }
+    
+    /**
+     * Release terminal
+     *
+     * @access  public
+     * @param   &util.telephony.TelephonyTerminal terminal
+     * @return  bool success
+     */
+    function releaseTerminal(&$terminal) {
+      if (!is_a($terminal, 'TelephonyTerminal')) {
+        trigger_error('type: '.gettype($terminal), E_USER_WARNING);
+        return throw(new IllegalArgumentException('Terminal parameter is not a TelephonyTerminal'));
+      }
+      return TRUE;
+    }
   }
 ?>
