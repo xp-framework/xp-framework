bf772486dc360b7f27a15431498a3c8a9280b222
thekid <timm.friebe@gmail.com>
2002-12-30 14:53:29 +0000

    - Default parameter version to ID3_VERSION_UNKNOWN - Add code to guess version
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@485 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/util/mp3/MP3File.class.php
+++ skeleton/util/mp3/MP3File.class.php
@@ -28,25 +28,50 @@
      * Extract ID3 Tags
      *
      * @access  public
-     * @param   int version default ID3_VERSION_1 ID3 Version
+     * @param   int version default ID3_VERSION_UNKNOWN ID3 Version
      * @see     http://www.id3.org/
      */
-    function getID3Tag($version= ID3_VERSION_1) {
-      switch ($version) {
-        case ID3_VERSION_1:
-          try(); {
-            $this->file->open(FILE_MODE_READ);
-            $this->file->seek(-128, SEEK_END);
-            $buf= $this->file->read(128);
-            $this->file->close();
-          } if (catch('Exception', $e)) {
-            return throw($e);
-          }
-          break;
+    function getID3Tag($version= ID3_VERSION_UNKNOWN) {
+      try(); {
+        $this->file->open(FILE_MODE_READ);
         
-        default:
-          return throw(new IllegalArgumentException('Version '.$version.' not supported'));
+        do {
+          switch ($version) {
+            case ID3_VERSION_UNKNOWN:
+            
+              // Check version 2
+              $this->file->rewind();
+              $buf= $this->file->read(10);
+              if ('ID3' == substr($buf, 0, 3)) {
+                $version= ID3_VERSION_2;
+                break;
+              }
+              
+              $this->file->seek(-128, SEEK_END);
+              $buf= $this->file->read(128);
+              $version= ID3_VERSION_1;
+              break;
+
+            case ID3_VERSION_1:
+            case ID3_VERSION_1_1:
+              $buf= $this->file->read(128);
+              $version= ("\0" == $buf{125} && "\0" != $buf{126}) ? ID3_VERSION_1_1 : ID3_VERSION_1;
+              break;
+
+            case ID3_VERSION_2:
+              $buf= $this->file->read(10);
+              break;
+
+            default:
+              $version= FALSE;
+              throw(new IllegalArgumentException('Version '.$version.' not supported'));
+          }
+        } while (ID3_VERSION_UNKNOWN === $version);
+      } if (catch('Exception', $e)) {
+        $this->file->close();
+        return throw($e);
       }
+      $this->file->close();
       
       return ID3Tag::fromString($buf, $version);
     }
