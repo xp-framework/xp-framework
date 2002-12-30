27ad54d6fc5c59bf930cc1910bdebd3e0babecf0
thekid <timm.friebe@gmail.com>
2002-12-30 15:24:26 +0000

    - Return NULL to indicate no tag was found
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@487 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/util/mp3/MP3File.class.php
+++ skeleton/util/mp3/MP3File.class.php
@@ -29,12 +29,14 @@
      *
      * @access  public
      * @param   int version default ID3_VERSION_UNKNOWN ID3 Version
+     * @return  &util.mp3.ID3Tag tag object or NULL if no tag exists
      * @see     http://www.id3.org/
      */
-    function getID3Tag($version= ID3_VERSION_UNKNOWN) {
+    function &getID3Tag($version= ID3_VERSION_UNKNOWN) {
       try(); {
         $this->file->open(FILE_MODE_READ);
         
+        $done= FALSE;
         do {
           switch ($version) {
             case ID3_VERSION_UNKNOWN:
@@ -54,26 +56,38 @@
 
             case ID3_VERSION_1:
             case ID3_VERSION_1_1:
-              $buf= $this->file->read(128);
-              $version= ("\0" == $buf{125} && "\0" != $buf{126}) ? ID3_VERSION_1_1 : ID3_VERSION_1;
+              if (!isset($buf)) {
+                $this->file->seek(-128, SEEK_END);
+                $buf= $this->file->read(128);
+              }
+              if ('TAG' == substr($buf, 0, 3)) {
+                $version= ("\0" == $buf{125} && "\0" != $buf{126}) ? ID3_VERSION_1_1 : ID3_VERSION_1;
+              } else {
+                $version= FALSE;
+              }
+              $done= TRUE;
               break;
 
             case ID3_VERSION_2:
-              $buf= $this->file->read(10);
+              if (!isset($buf)) {
+                $buf= $this->file->read(10);
+              }
+              
+              // TBD: Implement!
+              $done= TRUE;
               break;
 
             default:
-              $version= FALSE;
+              $done= TRUE;
               throw(new IllegalArgumentException('Version '.$version.' not supported'));
           }
-        } while (ID3_VERSION_UNKNOWN === $version);
+        } while (!$done);
       } if (catch('Exception', $e)) {
         $this->file->close();
         return throw($e);
       }
       $this->file->close();
-      
-      return ID3Tag::fromString($buf, $version);
+      return (FALSE === $version) ? NULL : ID3Tag::fromString($buf, $version);
     }
   }
 ?>
