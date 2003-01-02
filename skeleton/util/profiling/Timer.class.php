28e0f34ae360f19c273c5fe6ec43c3de73454634
thekid <timm.friebe@gmail.com>
2003-01-02 23:25:53 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@549 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/util/profiling/Timer.class.php
@@ -0,0 +1,64 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+ 
+  /**
+   * The Timer class provides a simple timer
+   *
+   * <code>
+   *   $p= &new Profiler();
+   *   $p->start();
+   *   // ... code you want profiled
+   *   $p->stop();
+   *   var_dump($p->elapsedTime());
+   * </code>
+   *
+   * @purpose  Provide a simple profiling timer
+   */
+  class Timer extends Object {
+    var
+      $start    = 0.0,
+      $float    = 0.0;
+      
+    /**
+     * Retreive current microtime
+     *
+     * @access  public
+     * @return  float microtime
+     */
+    function microtime() {
+      list($usec, $sec) = explode(' ', microtime()); 
+      return (float)$usec + (float)$sec;
+    }
+
+    /**
+     * Start the timer
+     *
+     * @access  public
+     */
+    function start() {
+      $this->start= $this->microtime();
+    }
+    
+    /**
+     * Stop the timer
+     *
+     * @access  public
+     */
+    function stop() {
+      $this->stop= $this->microtime();
+    }
+    
+    /**
+     * Retreive elapsed time
+     *
+     * @access  public
+     * @return  float seconds elapsed
+     */
+    function elapsedTime() {
+      return $this->stop - $this->start;
+    }
+  }
+?>
