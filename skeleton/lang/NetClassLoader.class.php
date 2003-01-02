74faad23d7840a4d3c3de84c12c212cb4a0d59cd
thekid <timm.friebe@gmail.com>
2003-01-02 00:43:44 +0000

    - Don`t use .mod file - Add expunge() method - Beautify sourcecode - Disallow static calls `a la NetClassLoader::loadClass() - Provide a working example - Don`t autoexpunge, we might want to keep the classes
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@532 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/lang/NetClassLoader.class.php
+++ skeleton/lang/NetClassLoader.class.php
@@ -7,75 +7,80 @@
   uses('lang.ClassLoader', 'net.http.HTTPConnection', 'io.File');
   
   /** 
-   * Loads an "external" class not belonging to the XP framework
+   * Loads an XP class via HTTP
    * 
    * Usage:
    * <code>
-   *   $e= &new NetClassLoader('http://binford6100.info/classes');
+   *   $e= &new NetClassLoader('http://sitten-polizei.de/php/classes/%s.class.php');
    *   try(); {
-   *     $name= $e->loadClass($argv[1]);
+   *     $name= $e->loadClass($_SERVER['argv'][1]);
    *   } if (catch('ClassNotFoundException', $e)) {
    *     die($e->printStackTrace());
    *   }
-   *
+   * 
    *   $obj= &new $name();
+   *   var_dump($obj, $obj->getClassName(), $obj->toString());
    * </code>
    *
-   * @access    public, static
+   * @purpose  Load classes of the net
+   * @see      xp://lang.ClassLoader
    */
   class NetClassLoader extends ClassLoader {
     var
-      $codebase= '',
-      $cache=    '';
+      $codebase = '',
+      $cache    = '';
     
     /**
      * Constructor
      * 
      * @access  public
      * @param   string codebase
+     * @param   string cache default '/tmp' cache directory
      */
     function __construct($codebase, $cache= '/tmp') {
       $this->codebase= $codebase;
       $this->cache= $cache;
+      $this->prefix= $this->getName().'-';
       parent::__construct();
-    }
-
-    /**
-     * Destructor
-     *
-     * @access  public
-     */    
-    function __destruct() {
-      $d= dir($this->cache);
-      $prefix= $this->getName().'-'.getmypid().'-';
-      while ($entry= $d->read()) {
-        if ($prefix != substr($entry, 0, strlen($prefix))) continue;
-        unlink($d->path.'/'.$entry);
-      }
-      $d->close();
-      parent::__destruct();
     }
     
     /**
-     * Load
+     * Expunge cache directory
      *
-     * @access  static
+     * @access  public
+     * @return  bool success
+     */
+    function expunge() {
+      if (FALSE === ($d= dir($this->cache))) return FALSE;
+      while ($entry= $d->read()) {
+        if ($this->prefix != substr($entry, 0, strlen($this->prefix))) continue;
+        unlink($d->path.'/'.$entry);
+      }
+      $d->close();
+      return TRUE;
+    }
+
+    /**
+     * Load a class via HTTP. The HTTP return status *must* either be 200 OK to
+     * indicate success or 304 Not Modified to indicate the file has'nt changed
+     * since it's last retreival
+     *
+     * @access  public
      * @param   string className fully qualified class name io.File
      * @return  string class' name for instantiation
      * @throws  ClassNotFoundException in case the class can not be found
+     * @throws  FormatException in case there is a unexpected HTTP return code
      */
-    function loadClass($className, $codebase= '', $cache= '/tmp') {
-      if (isset($this)) {
-        $codebase= $this->codebase;
-        $cache= $this->cache;
-      }
+    function loadClass($className) {
+      $uri= sprintf($this->codebase, $className);
+      $cacheName= $this->cache.'/'.$this->prefix.strtr($uri, '/:', '__').'.class.php';
       
-      $uri= sprintf($codebase, $className);
-      $cacheName= $cache.'/'.$this->getName().'-'.getmypid().'-'.strtr($uri, '/:', '__').'.class.php';
       $f= &new File($cacheName);
       $conn= &new HTTPConnection($uri);
       if ($f->exists()) {
-        $conn->headers['If-Modified-Since']= implode('', file($cacheName.'.mod'));
+        $f->open(FILE_MODE_READ);
+        $conn->headers['If-Modified-Since']= substr($f->readLine(), 9, -3);
+        $f->close();
       }
       
       try(); {
@@ -84,16 +89,11 @@
         switch ($conn->response->status) {
           case 200:
             $f->open(FILE_MODE_WRITE);
+            $f->writeLine(sprintf('<?php // %s ?>', $conn->response->header['Last-Modified']));
             while ($buf= $conn->getResponse(FALSE)) {
               $f->write($buf);
             }
             $f->close();
-            
-            $mod= &new File($cacheName.'.mod');
-            $mod->open(FILE_MODE_WRITE);
-            $mod->write($conn->response->header['Last-Modified']);
-            $mod->close();
-            
             break;
             
           case 304:
@@ -110,7 +110,7 @@
         return throw(new ClassNotFoundException(sprintf(
           'class "%s" [codebase %s] not found: %s',
           $className,
-          $codebase,
+          $this->codebase,
           $e->message
         )));
       }
@@ -119,15 +119,15 @@
       if (FALSE === $result) return throw(new ClassNotFoundException(sprintf(
         'class "%s" [codebase %s] not found',
         $className,
-        $codebase
+        $this->codebase
       )));
 
-      $p= parse_url($codebase);
-      $parts= explode('.', $p['host']);
-      for ($i= sizeof($parts), $str= ''; $i > -1; $i--) {
-        $str.= $parts[$i].'.';
-      }
-      $GLOBALS['php_class_names'][strtolower($className)]= substr($str, 1).ucfirst($className);
+      $p= parse_url($this->codebase);
+      $GLOBALS['php_class_names'][strtolower($className)]= (
+        implode('.', array_reverse(explode('.', $p['host']))).
+        '.'.
+        ucfirst($className)
+      );
       return $className;
     }
   }
