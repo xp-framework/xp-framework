6abf6a53a5ad71bb8e8ff72bbb6b7f63fe64dd31
thekid <timm.friebe@gmail.com>
2003-01-02 20:39:37 +0000

    - Fix parse error
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@545 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/io/File.class.php
+++ skeleton/io/File.class.php
@@ -14,7 +14,7 @@
   
   // Define default filehandles
   if (!defined('STDIN'))  define('STDIN',      fopen ('php://stdin',  'r'));          
-  if (!defined('STDOUT')) define('STDOUT',     fopen ('php://stdout', 'w');          
+  if (!defined('STDOUT')) define('STDOUT',     fopen ('php://stdout', 'w'));          
   if (!defined('STDERR')) define('STDERR',     fopen ('php://stderr', 'w'));         
   
   uses(
