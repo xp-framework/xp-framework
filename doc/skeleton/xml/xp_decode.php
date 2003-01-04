154f159d4c00a05cc2278095421e6b396205add2
thekid <timm.friebe@gmail.com>
2003-01-04 15:28:07 +0000

    - Add usage
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@557 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- doc/skeleton/xml/xp_decode.php
+++ doc/skeleton/xml/xp_decode.php
@@ -7,6 +7,12 @@
   require('lang.base.php');
   uses('xml.xp.XMLDecoder', 'util.cmd.ParamString');
   
+  $p= &new ParamString();
+  if ($p->exists('help')) {
+    printf("Usage: cat xmlfile | php -q %s\n", basename($p->value(0)));
+    exit();
+  }
+  
   $d= &new XMLDecoder(new File('php://stdin'));
   $o= &$d->readObject();
   $d->close();  
