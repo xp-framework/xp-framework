ee8adabe288d05ca4134b0466a359c1773590124
thekid <timm.friebe@gmail.com>
2003-01-04 15:25:46 +0000

    - Make class defineable by command line argument
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@555 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- doc/skeleton/xml/xp_encode.php
+++ doc/skeleton/xml/xp_encode.php
@@ -7,7 +7,15 @@
   require('lang.base.php');
   uses('xml.xp.XMLEncoder', 'util.cmd.ParamString');
   
+  $p= &new ParamString();
+  try(); {
+    $name= ClassLoader::loadClass($p->value(1));
+  } if (catch('ClassNotFoundException', $e)) {
+    $e->printStackTrace();
+    exit();
+  }
+  
   $d= &new XMLEncoder(new File('php://stdout'));
-  $d->writeObject(new ParamString());
+  $d->writeObject(new $name());
   $d->close();  
 ?>
