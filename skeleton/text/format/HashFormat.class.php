69bd4ccb30bd8a0d978bda9ecdea4b59c7787263
thekid <timm.friebe@gmail.com>
2003-01-04 20:47:26 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@566 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/text/format/HashFormat.class.php
@@ -0,0 +1,59 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('util.text.format.IFormat');
+  
+  /**
+   * Hash formatter
+   *
+   * @purpose  Provide a Format wrapper for Hashs
+   * @see      xp://util.text.format.IFormat
+   */
+  class HashFormat extends IFormat {
+
+    /**
+     * Get an instance
+     *
+     * @access  public
+     * @return  &util.text.format.HashFormat
+     */
+    function &getInstance() {
+      return parent::getInstance('HashFormat');
+    }  
+  
+    /**
+     * Apply format to argument
+     *
+     * @access  public
+     * @param   mixed fmt
+     * @param   &mixed argument
+     * @return  string
+     */
+    function apply($fmt, &$argument) {
+      if (is_scalar($argument)) {
+        return throw(new FormatException('Argument with type '.gettype($argument).' is not an array or object'));
+      }
+      if (is_a($argument, 'Hashmap')) {
+        $hash= $argument->_hash;
+      } else if (is_object($argument)) {
+        $hash= get_object_vars($argument);
+      } else {
+        $hash= $argument;
+      }
+      
+      $ret= '';
+      $fmt= strtr($fmt, array(
+        '\r'    => "\r",
+        '\n'    => "\n",
+        '\t'    => "\t"
+      ));
+      foreach (array_keys($argument) as $key) {
+        $ret.= sprintf($fmt, $key, $argument[$key]);
+      }
+      return $ret;
+    }
+  }
+?>
