47f4d1404a09d08c7bf62cb29cdd878ecde60646
thekid <timm.friebe@gmail.com>
2003-01-05 21:49:42 +0000

    - Implement get()
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@599 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/org/webdav/impl/DavFileImpl.class.php
+++ skeleton/org/webdav/impl/DavFileImpl.class.php
@@ -7,6 +7,8 @@
   uses(
     'org.webdav.impl.DavImpl',
     'io.Folder',
+    'io.File',
+    'lang.ElementNotFoundException',
     'util.MimeType'
   );
   
@@ -40,22 +42,27 @@
      * @param   string base
      * @param   &org.webdav.xml.WebdavPropFindResponse response
      * @param   int maxdepth
+     * @throws  ElementNotFoundException
      */
     function _recurse($path, $root, &$response, $maxdepth) {
       // DEBUG $l= &Logger::getInstance();
       // DEBUG $c= &$l->getCategory();
       
       $realpath= $this->base.$path;
+      if (!file_exists($realpath)) {
+        return throw(new ElementNotFoundException($path.' not found'));
+      }
+      
       if (is_dir($realpath)) {
         // DEBUG $c->debug('DIRECTORY', $path);
         $f= &new Folder($realpath);
-        $response->addEntry(
+        $response->addEntry(new WebdavObject(
           basename($path),
           $root.$path,
           new Date(filectime($f->uri)),
           new Date(filemtime($f->uri)),
           WEBDAV_COLLECTION
-        );
+        ));
         $maxdepth--;
         while ($maxdepth >= 0 && $entry= $f->getEntry()) {
           // DEBUG $c->debug('RECURSE', $path.$entry);
@@ -66,17 +73,64 @@
       }
       
       // DEBUG $c->debug('FILE', $realpath);
-      $response->addEntry(
+      $response->addEntry(new WebdavObject(
         basename($path),
         $root.$path,
         new Date(filectime($realpath)),
         new Date(filemtime($realpath)),
         NULL,
         filesize($realpath),
-        MimeType::getByFilename($path)
+        MimeType::getByFilename($path, 'text/plain')
+      ));
+    }
+    
+    /**
+     * Get a file
+     *
+     * @access  public
+     * @param   string resourcename
+     * @return  &org.webdav.WebdavObject
+     * @throws  ElementNotFoundException
+     */
+    function &get($filename) {
+      if (is_dir($this->base.$filename)) {
+        return throw(new Exception($filename.' cannot be retreived using GET'));
+      }
+      
+      // Open file and read contents
+      $f= &new File($this->base.$filename);
+      $o= &new WebdavObject(
+        $f->filename,
+        $f->uri,
+        new Date($f->createdAt()),
+        new Date($f->lastModified()),
+        NULL,
+        $f->size(),
+        MimeType::getByFilename($f->uri, 'text/plain')
       );
+      try(); {
+        $f->open(FILE_MODE_READ);
+        $o->setData($f->read($f->size()));
+        $f->close();
+      } if (catch('FileFoundException', $e)) {
+        return throw(new ElementNotFoundException($filename.' not found'));
+      }
+      
+      return $o;
     }
 
+    /**
+     * Patch properties
+     *
+     * @access  public
+     * @param   &org.webdav.xml.WebdavPropFindRequest request
+     * @param   &org.webdav.xml.WebdavPropFindResponse response
+     * @return  &org.webdav.xml.WebdavPropFindResponse response
+     */
+    function &proppatch(&$request, &$response) {
+      
+    }
+    
     /**
      * Find properties
      *
@@ -95,9 +149,9 @@
         return throw(new IllegalArgumentException('Parameters passed of wrong types'));
       }
 
-      // DEBUG $l= &Logger::getInstance();
-      // DEBUG $c= &$l->getCategory();
-      // DEBUG $c->debug('Properties requested', $request->getProperties());
+      $l= &Logger::getInstance();
+      $c= &$l->getCategory();
+      $c->debug('Properties requested', $request->getProperties());
  
       try(); {
         $this->_recurse(
