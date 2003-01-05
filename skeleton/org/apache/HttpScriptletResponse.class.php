0a4d6985c4110bde6e90402ab6e1a1afb0d2ef98
thekid <timm.friebe@gmail.com>
2003-01-05 22:25:21 +0000

    - Define the rest of the status codes
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@605 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/org/apache/HttpScriptletResponse.class.php
+++ skeleton/org/apache/HttpScriptletResponse.class.php
@@ -5,14 +5,54 @@
  */
  
   // HTTP status codes
-  define('HTTP_OK',                     200);
-  define('HTTP_CREATED',                201);
-  define('HTTP_NO_CONTENT',             204);
-  define('HTTP_MOVED_TEMPORARILY',      302);
-  define('HTTP_BAD_REQUEST',            400);
-  define('HTTP_NOT_FOUND',              404);
-  define('HTTP_CONFLICT',               409);
-  define('HTTP_INTERNAL_SERVER_ERROR',  500);
+  define('HTTP_CONTINUE',                          100);
+  define('HTTP_SWITCHING PROTOCOLS',               101);
+  define('HTTP_PROCESSING',                        102);
+  define('HTTP_OK',                                200);
+  define('HTTP_CREATED',                           201);
+  define('HTTP_ACCEPTED',                          202);
+  define('HTTP_NON_AUTHORITATIVE_INFORMATION ',    203);
+  define('HTTP_NO_CONTENT',                        204);
+  define('HTTP_RESET_CONTENT',                     205);
+  define('HTTP_PARTIAL_CONTENT',                   206);
+  define('HTTP_MULTI_STATUS',                      207);
+  define('HTTP_MULTIPLE_CHOICES',                  300);
+  define('HTTP_MOVED_PERMANENTLY',                 301);
+  define('HTTP_FOUND',                             302);
+  define('HTTP_SEE_OTHER',                         303);
+  define('HTTP_NOT_MODIFIED',                      304);
+  define('HTTP_USE_PROXY',                         305);
+  define('HTTP_TEMPORARY_REDIRECT',                307);
+  define('HTTP_BAD_REQUEST',                       400);
+  define('HTTP_AUTHORIZATION_REQUIRED',            401);
+  define('HTTP_PAYMENT_REQUIRED',                  402);
+  define('HTTP_FORBIDDEN',                         403);
+  define('HTTP_NOT_FOUND',                         404);
+  define('HTTP_METHOD_NOT_ALLOWED',                405);
+  define('HTTP_NOT_ACCEPTABLE',                    406);
+  define('HTTP_PROXY_AUTHENTICATION REQUIRED',     407);
+  define('HTTP_REQUEST_TIME_OUT',                  408);
+  define('HTTP_CONFLICT',                          409);
+  define('HTTP_GONE',                              410);
+  define('HTTP_LENGTH_REQUIRED',                   411);
+  define('HTTP_PRECONDITION FAILED',               412);
+  define('HTTP_REQUEST_ENTITY_TOO_LARGE',          413);
+  define('HTTP_REQUEST_URI_TOO_LARGE',             414);
+  define('HTTP_UNSUPPORTED_MEDIA TYPE',            415);
+  define('HTTP_REQUESTED_RANGE_NOT_SATISFIABLE',   416);
+  define('HTTP_EXPECTATION_FAILED',                417);
+  define('HTTP_UNPROCESSABLE_ENTITY',              422);
+  define('HTTP_LOCKED',                            423);
+  define('HTTP_FAILED_DEPENDENCY',                 424);
+  define('HTTP_INTERNAL_SERVER_ERROR',             500);
+  define('HTTP_METHOD_NOT_IMPLEMENTED',            501);
+  define('HTTP_BAD_GATEWAY',                       502);
+  define('HTTP_SERVICE TEMPORARILY UNAVAILABLE',   503);
+  define('HTTP_GATEWAY TIME_OUT',                  504);
+  define('HTTP_HTTP VERSION_NOT SUPPORTED',        505);
+  define('HTTP_VARIANT_ALSO_NEGOTIATES',           506);
+  define('HTTP_INSUFFICIENT_STORAGE',              507);
+  define('HTTP_NOT_EXTENDED',                      510);
 
   /**
    * Defines the response sent from the webserver to the client,
