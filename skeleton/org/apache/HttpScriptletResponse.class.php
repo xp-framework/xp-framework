ab22b148eee57646fbeb7d4a1354249402bbc952
thekid <timm.friebe@gmail.com>
2003-01-05 22:00:53 +0000

    - Add HTTP_CREATED (201) and HTTP_NO_CONTENT (204) - TBD: Add rest of known status codes
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@602 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/org/apache/HttpScriptletResponse.class.php
+++ skeleton/org/apache/HttpScriptletResponse.class.php
@@ -6,6 +6,8 @@
  
   // HTTP status codes
   define('HTTP_OK',                     200);
+  define('HTTP_CREATED',                201);
+  define('HTTP_NO_CONTENT',             204);
   define('HTTP_MOVED_TEMPORARILY',      302);
   define('HTTP_BAD_REQUEST',            400);
   define('HTTP_NOT_FOUND',              404);
