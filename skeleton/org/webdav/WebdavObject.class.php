e53e53397061649dbb3ff202b08d49bd3604b8b4
thekid <timm.friebe@gmail.com>
2003-01-05 21:41:16 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@594 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/org/webdav/WebdavObject.class.php
@@ -0,0 +1,84 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  /**
+   * Webdav Object
+   *
+   * @purpose  Represent a "file" or "directory"
+   */
+  class WebdavObject extends Object {
+    var
+      $displayName      = '',
+      $href             = '',
+      $creationDate     = NULL,
+      $lastModified     = NULL,
+      $resourceType     = '',
+      $contentLength    = 0,
+      $contentType      = NULL,
+      $status           = 0,
+      $properties       = array();
+      
+    var
+      $_data            = '';
+ 
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   string displayName
+     * @param   string href
+     * @param   &util.Date creationDate
+     * @param   &util.Date lastModified
+     * @param   string resourceType
+     * @param   string contentLength default 0
+     * @param   string contentType default NULL
+     * @param   int status default HTTP_STATUS_OK
+     * @param   array properties default array()
+     */
+    function __construct(
+      $displayName,
+      $href,
+      &$creationDate,
+      &$lastModified,
+      $resourceType,
+      $contentLength= 0,
+      $contentType= NULL,
+      $status= HTTP_OK,
+      $properties= array()
+    ) {
+      $this->displayName   = $displayName;       
+      $this->href          = $href;              
+      $this->creationDate  = $creationDate;      
+      $this->lastModified  = $lastModified;      
+      $this->resourceType  = $resourceType;      
+      $this->contentLength = $contentLength;     
+      $this->contentType   = $contentType;       
+      $this->status        = $status;            
+      $this->properties    = $properties;  
+      parent::__construct();      
+    }
+    
+    /**
+     * Set data
+     *
+     * @access  public 
+     * @param   &string data
+     */
+    function setData(&$data) {
+      $this->_data= &$data;
+    }
+    
+    /**
+     * Get data
+     *
+     * @access  public
+     * @return  &string data
+     */
+    function &getData() {
+      return $this->_data;
+    }
+  } 
+?>
