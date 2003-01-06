3b7397d67136b8673eb7fe0bba749bc301606ddc
thekid <timm.friebe@gmail.com>
2003-01-06 01:52:13 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@614 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/security/Permission.class.php
@@ -0,0 +1,17 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+ 
+  /**
+   * Permission base class
+   *
+   * @purpose  A single permission
+   * @see      http://java.sun.com/j2se/1.4.1/docs/guide/security/permissions.html
+   * @see      xp://security.Policy
+   */
+  class Permission extends Object {
+  
+  }
+?>
