533d3f10e93e138d5799a374a88845b4ad301af1
thekid <timm.friebe@gmail.com>
2003-01-07 01:02:03 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@637 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/util/profiling/AssertException.class.php
@@ -0,0 +1,16 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Assertion exception
+   *
+   * @see      xp://util.Assert#exception
+   * @purpose  Indicates an assertion failed
+   */
+  class AssertException extends Exception {
+  
+  }
+?>
