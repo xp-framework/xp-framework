21b1bb386ee1f256e5a277d38c967550198cef9d
thekid <timm.friebe@gmail.com>
2003-01-14 23:52:44 +0000

    - Introduce req target - Update usage
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@672 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/xp.port.mk
+++ ports/xp.port.mk
@@ -8,11 +8,21 @@ PORTSDIR!=  echo ${.CURDIR} | sed -E 's(.*/ports)/(.+)$$\1g'
 all:    usage
 
 usage:
-	@echo "Usage: make dist   - Make distribution"
-	@echo "       make clean  - Clean"
-            
+	@echo "Usage: make dist"
+	@echo "       - Make distribution"
+	@echo ""
+	@echo "       make clean"
+	@echo "       - Clean port directory"
+	@echo ""
+	@echo "       make req DIR=<directory>"
+	@echo "       - Get required files for a collection directory"
+	@echo ""   
+
 dist:
 	@${PHP} ${PORTSDIR}/dist.php ${.CURDIR} 
 
 clean:
 	@-rm -rf ${.CURDIR}/build
+
+req:
+	find ${DIR} -name '*.class.php' | ${PHP} ${PORTSDIR}/req.php
