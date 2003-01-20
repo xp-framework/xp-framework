3b4d30496c77201570fd687fc4215f3cbeabf5ca
thekid <timm.friebe@gmail.com>
2003-01-20 20:14:02 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@791 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ util/xpdoku/util/sabcmd.php
@@ -0,0 +1,52 @@
+<?php
+/* This file is part of the XP framework
+ *
+ * $Id$
+ */
+
+  require('lang.base.php');
+  uses(
+    'util.cmd.ParamString',
+    'util.text.PHPSyntaxHighlighter',
+    'xml.XSLProcessor'
+  );
+  
+  function schemeHandler($proc, $scheme, $rest) {
+    static $s= NULL;
+    if (!isset($s)) $s= &new PHPSyntaxHighlighter();
+
+    switch ($scheme) {
+      case 'php':
+        $s->setSource(substr($rest, 2));
+        return '<php>'.str_replace(
+          '<br />', 
+          "<br />\n", 
+          substr($s->getHighlight(), 6, -7)
+        ).'</php>';
+        break;
+    }
+    return '<xml/>';
+  }
+  
+  // {{{ main
+  $p= &new ParamString();
+  if (3 != $p->count) {
+    printf("Usage: %s xsl xml\n", basename($p->value(0)));
+    exit();
+  }
+  
+  $proc= &new XSLProcessor();
+  $proc->setXSLFile($p->value(1));
+  $proc->setXMLFile($p->value(2));
+  $proc->setSchemeHandler(array('get_all' => 'schemeHandler'));
+  
+  try(); {
+    $proc->run();
+  } if (catch('Exception', $e)) {
+    $e->printStackTrace();
+    exit();
+  }
+  
+  echo $proc->output();
+  // }}}
+?>
