1ecf95da3de8b0d5384ddb49707cd3b20d3af531
thekid <timm.friebe@gmail.com>
2003-02-01 04:01:54 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@877 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/security/PolicyException.class.php
@@ -0,0 +1,16 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * PolicyException
+   *
+   * @see      xp://security.PolicyFile
+   * @purpose  Indicate errors in policy
+   */
+  class PolicyException extends Exception {
+  
+  }
+?>
