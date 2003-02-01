3c55f53404e038100edeb04ba5b90b116751b2a6
thekid <timm.friebe@gmail.com>
2003-02-01 01:35:55 +0000

    - Use peer.http.HttpConnection instead of deprecated API - Also load dependencies if necessary - Make exceptions more verbose - If include_once fails, throw an RuntimeException
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@871 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/lang/NetClassLoader.class.php
+++ skeleton/lang/NetClassLoader.class.php
@@ -4,7 +4,12 @@
  * $Id$
  */
  
-  uses('lang.ClassLoader', 'net.http.HTTPConnection', 'io.File');
+  uses(
+    'lang.ClassLoader', 
+    'peer.http.HttpConnection', 
+    'lang.RuntimeException',
+    'io.File'
+  );
   
   /** 
    * Loads an XP class via HTTP
@@ -59,6 +64,127 @@
       $d->close();
       return TRUE;
     }
+    
+    /**
+     * Uses wrapper for
+     *
+     * @model   static
+     * @access  public
+     * @param   string codebase
+     * @param   string prefix
+     * @param   string cache
+     * @param   string* class names
+     * @return  bool
+     */
+    function uses() {
+      // DEBUG
+      // $args= func_get_args();
+      // echo "NetClassLoader::uses(\n";
+      // var_dump($args);
+      // echo ")\n";
+ 
+      $codebase= func_get_arg(0);
+      $prefix= func_get_arg(1);
+      $cache= func_get_arg(2);
+     
+      for ($i= 3, $c= func_num_args(); $i < $c; $i++) {
+        $name= func_get_arg($i);
+        
+        // If we can't load the class of the net, try locally
+        try(); {
+          NetClassLoader::_load($codebase, $prefix, $cache, $name);
+        } if (catch('FileNotFoundException', $e)) {
+          if (!ClassLoader::loadClass($name)) {
+            return FALSE;
+          }
+        }
+      }
+      
+      return TRUE;
+    }
+    
+    /**
+     * Load a class
+     *
+     * @model   static
+     * @access  protected
+     * @param   string codebase
+     * @param   string prefix
+     * @param   string cache
+     * @param   string className (FQCN)
+     * @return  mixed string classname or FALSE if the class is not found
+     */
+    function _load($codebase, $prefix, $cache, $className) {
+      // DEBUG
+      // echo "NetClassLoader::_load(\n";
+      // var_dump($codebase, $prefix, $cache, $className);
+      // echo ")\n";
+      
+      $uri= $this->codebase.strtr($className, '.', '/').'.class.php';
+      $cacheName= $cache.'/'.$prefix.strtr($uri, '/:', '__').'.class.php';
+      
+      // DEBUG var_dump($uri, $cacheName);
+      
+      $headers= array();
+      
+      $f= &new File($cacheName);
+      $conn= &new HttpConnection($uri);
+      if ($f->exists()) {
+        $f->open(FILE_MODE_READ);
+        $headers['If-Modified-Since']= substr($f->readLine(), 9, -3);
+        $f->close();
+      }
+      
+      $r= &$conn->get(NULL, $headers);
+      $status= $r->getStatusCode();
+
+      switch ($status) {
+        case 200:
+          $f->open(FILE_MODE_WRITE);
+          $f->writeLine(sprintf('<?php // %s ?>', $r->getHeader('Last-Modified')));
+
+          $uses= sprintf(
+            'NetClassLoader::uses(\'%s\', \'%s\', \'%s\', ', 
+            $codebase, 
+            $prefix, 
+            $cache
+          );
+
+          while ($buf= $r->readData()) {
+            $f->write(preg_replace(
+              '#uses[\t\s]*\(#',
+              $uses,
+              $buf
+            ));
+          }
+          $f->close();
+          break;
+
+        case 304:
+          // Not modified, use our local copy
+          break;
+
+        case 404:
+          // Not found
+          return throw(new FileNotFoundException($r->message));
+          break;
+
+        default:
+          trigger_error($conn->request->getRequestString(), E_USER_NOTICE);
+          trigger_error($r->toString(), E_USER_NOTICE);
+          return throw(new FormatException('Unexpected HTTP status code '.$status.' ['.$r->message.']'));
+          break;
+      }
+      
+      // Try to include the file. Other classes are also loaded
+      // off the net as we execute this (using NetClassLoader::uses())
+      if (!include_once($f->uri)) {
+        return throw(new RuntimeException('Internal error'));
+      }
+      
+      $GLOBALS['php_class_names'][reflect($className)]= $className;
+      return reflect($className);
+    }
 
     /**
      * Load a class via HTTP. The HTTP return status *must* either be 200 OK to
@@ -69,66 +195,26 @@
      * @param   string className fully qualified class name io.File
      * @return  string class' name for instantiation
      * @throws  ClassNotFoundException in case the class can not be found
-     * @throws  FormatException in case there is a unexpected HTTP return code
      */
     function loadClass($className) {
-      $uri= sprintf($this->codebase, $className);
-      $cacheName= $this->cache.'/'.$this->prefix.strtr($uri, '/:', '__').'.class.php';
-      
-      $f= &new File($cacheName);
-      $conn= &new HTTPConnection($uri);
-      if ($f->exists()) {
-        $f->open(FILE_MODE_READ);
-        $conn->headers['If-Modified-Since']= substr($f->readLine(), 9, -3);
-        $f->close();
-      }
-      
       try(); {
-        $conn->get();
-
-        switch ($conn->response->status) {
-          case 200:
-            $f->open(FILE_MODE_WRITE);
-            $f->writeLine(sprintf('<?php // %s ?>', $conn->response->header['Last-Modified']));
-            while ($buf= $conn->getResponse(FALSE)) {
-              $f->write($buf);
-            }
-            $f->close();
-            break;
-            
-          case 304:
-            // Not modified, use our local copy
-            break;
-
-          default:
-            throw(new FormatException('HTTP return code ['.$conn->response->status.'] != 200'));
-        }
-        
-        // DEBUG var_dump($conn->response, $conn->request);
-        
+        $name= NetClassLoader::_load(
+          $this->codebase,
+          $this->prefix,
+          $this->cache,
+          $className
+        );
       } if (catch('Exception', $e)) {
         return throw(new ClassNotFoundException(sprintf(
-          'class "%s" [codebase %s] not found: %s',
+          "class '%s' not found\n  Codebase: %s\n  Cause   : %s {\n    %s\n  }\n",
           $className,
           $this->codebase,
+          $e->getClassName(),
           $e->message
         )));
       }
       
-      $result= include_once($f->uri);
-      if (FALSE === $result) return throw(new ClassNotFoundException(sprintf(
-        'class "%s" [codebase %s] not found',
-        $className,
-        $this->codebase
-      )));
-
-      $p= parse_url($this->codebase);
-      $GLOBALS['php_class_names'][strtolower($className)]= (
-        implode('.', array_reverse(explode('.', $p['host']))).
-        '.'.
-        ucfirst($className)
-      );
-      return $className;
+      return $name;
     }
   }
 ?>
