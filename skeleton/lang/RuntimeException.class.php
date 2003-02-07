65f3a95228f75b66db66f026181a4558c65c299e
thekid <timm.friebe@gmail.com>
2003-02-07 21:00:24 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@899 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/lang/RuntimeException.class.php
@@ -0,0 +1,15 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * RuntimeException
+   *
+   * @purpose  Indicates internal errors
+   */
+  class RuntimeException extends Exception {
+  
+  }
+?>
