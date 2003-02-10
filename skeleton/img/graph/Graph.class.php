5a7bfddb256e94ee2739c89ebf67d20e6c9ee613
thekid <timm.friebe@gmail.com>
2003-02-10 23:01:07 +0000

    - Add example
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@929 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/img/graph/Graph.class.php
+++ skeleton/img/graph/Graph.class.php
@@ -9,6 +9,51 @@
   /**
    * Graph class
    *
+   * Example [creating a piechart]:
+   * <code>
+   *   uses(
+   *     'img.graph.Graph',
+   *     'img.graph.PieChart',
+   *     'img.Color',
+   *     'img.PngImage',
+   *     'io.File'
+   *   );
+   *   
+   *   $g= &new Graph(400, 400);
+   *   $g->create();
+   *   $colors= array(
+   *     'white'     => $g->allocate(new Color('#ffffff')),
+   *     'red'       => $g->allocate(new Color('#ff0000')),
+   *     'darkred'   => $g->allocate(new Color('#990000')),
+   *     'blue'      => $g->allocate(new Color('#0000ff')),
+   *     'darkblue'  => $g->allocate(new Color('#000099')),
+   *     'green'     => $g->allocate(new Color('#00ff00')),
+   *     'darkgreen' => $g->allocate(new Color('#009900')),
+   *     'orange'    => $g->allocate(new Color('#ffff00')),
+   *     'brown'     => $g->allocate(new Color('#999900')),
+   *     'violet'    => $g->allocate(new Color('#ff00ff')),
+   *     'purple'    => $g->allocate(new Color('#990099')),
+   *   );
+   *   
+   *   $g->draw(new PieChart(array(
+   *     'first'  => new PieSlice(10.0, array($colors['red'], $colors['darkred'])),
+   *     'second' => new PieSlice(60.0, array($colors['blue'], $colors['darkblue'])),
+   *     'third'  => new PieSlice( 6.0, array($colors['green'], $colors['darkgreen'])),
+   *     'fourth' => new PieSlice( 4.0, array($colors['orange'], $colors['brown'])),
+   *     'fifth'  => new PieSlice(12.0, array($colors['violet'], $colors['purple'])),
+   *   ), 100));
+   *   
+   *   $i= &new PngImage($g->getWidth(), $g->getHeight());
+   *   $i->create();
+   *   $i->copyFrom($g);
+   *   try(); {
+   *     $f= &$i->toFile(new File('out.png'));
+   *   } if (catch('ImagingException', $e)) {
+   *     $e->printStackTrace();
+   *     exit;
+   *   }
+   * </code>
+   *
    * @see img.Image
    */
   class Graph extends Image {
