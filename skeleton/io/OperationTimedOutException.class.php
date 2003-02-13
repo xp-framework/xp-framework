87154866631a18f799634d4af989e46bd1bf111f
thekid <timm.friebe@gmail.com>
2003-02-13 22:01:37 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@950 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/io/OperationTimedOutException.class.php
@@ -0,0 +1,15 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+ 
+  /**
+   * OperationTimedOutException
+   *
+   * @purpose  Indicate an operation timed out
+   */
+  class OperationTimedOutException extends Exception {
+  
+  }
+?>
