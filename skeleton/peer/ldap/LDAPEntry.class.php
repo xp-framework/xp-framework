1ac475e758465f11dea155fbb439918cdb405b6a
thekid <timm.friebe@gmail.com>
2003-02-20 22:28:39 +0000

    - Update documentation - Return NULL if an attribute is not found in getAttribute()
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1030 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/peer/ldap/LDAPEntry.class.php
+++ skeleton/peer/ldap/LDAPEntry.class.php
@@ -7,7 +7,9 @@
   /**
    * Wraps LDAP entry
    *
-   * @see 
+   * @purpose  Represent a single entry
+   * @see      xp://peer.ldap.LDAPSearchResult
+   * @see      xp://peer.ldap.LDAPClient
    */
   class LDAPEntry extends Object {
     var
@@ -42,14 +44,17 @@
     }
         
     /**
-     * (Insert method's description here)
+     * Creates an LDAP from the raw return data of PHP's ldap_* functions
+     * Also performs decoding on the attributes.
      *
-     * @access  static
-     * @param   
-     * @return  
+     * @model   static
+     * @access  public
+     * @param   &mixed data return value from ldap_* functions
+     * @return  &peer.ldap.LDAPEntry object
      */
     function &fromData(&$data) {
       $e= &new LDAPEntry($data['dn']);
+      
       foreach (array_keys($data) as $key) {
         if ('count' == $key || is_int($key)) continue;
         
@@ -63,23 +68,62 @@
       return $e;
     }
     
+    /**
+     * Set this entrie's DN (distinct name)
+     *
+     * @access  public
+     * @param   string dn
+     */
     function setDN($dn) {
       $this->dn= $dn;
     }
     
+    /**
+     * Retreive this entries' DN (distinct name)
+     *
+     * @access  public
+     * @return  string DN
+     */
     function getDN() {
       return $this->dn;
     }
     
+    /**
+     * Set attribute
+     *
+     * @access  public
+     * @param   string key
+     * @param   mixed value
+     */
     function setAttribute($key, $value) {
       $this->attributes[$key]= $value;
     }
     
+    /**
+     * Retreive an attribute - an offset may be supplied to define
+     * the values offset within the attribute. If -1 (the default)
+     * is supplied, an array of attribute values is returned.
+     *
+     * Note: If the value does not exist, NULL is returned
+     *
+     * @access  public
+     * @param   string key
+     * @param   int idx default -1
+     * @return  mixed attribute
+     */
     function getAttribute($key, $idx= -1) {
-      if (-1 != $idx) return $this->attributes[$key][$idx];
-      return $this->attributes[$key];
+      return ((0 < $idx) 
+        ? isset($this->attributes[$key][$idx]) ? $this->attributes[$key][$idx]  : NULL 
+        : isset($this->attributes[$key]) ? $this->attributes[$key] : NULL
+      );
     }
     
+    /**
+     * Retreive all attributes
+     *
+     * @access  public
+     * @return  array
+     */
     function getAttributes() {
       return $this->attributes;
     }
