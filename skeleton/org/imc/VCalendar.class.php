e681d146fb9269a4eaf0be023185ca8c40940647
thekid <timm.friebe@gmail.com>
2003-02-22 21:20:47 +0000

    - First version, debug only
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1059 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/org/imc/VCalendar.class.php
+++ skeleton/org/imc/VCalendar.class.php
@@ -6,6 +6,8 @@
 
   uses('text.parser.VFormatParser', 'org.imc.VEvent');
 
+  define('VCAL_ID',         'VCALENDAR');
+
   /**
    * VCalendar
    * 
@@ -27,6 +29,77 @@
    * @purpose  Handle vCalendar
    */
   class VCalendar extends Object {
+
+    /**
+     * Parser callback
+     *
+     * @access  public
+     * @param   array keys
+     * @param   mixed value
+     * @throws  FormatException
+     */
+    function addProperty($keys, $value) {
+      #ifdef DEBUG
+      echo $this->getClassName().'::addProperty(';
+      var_export($keys);
+      echo ', ';
+      var_export($value);
+      echo ")\n";
+      #endif
+    }
+    
+    /**
+     * Creata a vCalendar from a stream
+     *
+     * <code>
+     *   try(); {
+     *     $cal= &VCalendar::fromStream(new File('/tmp/test.ics'));
+     *   } if (catch('Exception', $e)) {
+     *     $e->printStackTrace();
+     *     exit(-1);
+     *   }
+     *   
+     *   var_dump($cal);
+     * </code>
+     *
+     * @model   static
+     * @access  public
+     * @param   &io.Stream stream
+     * @return  &org.imc.VCard
+     */
+    function &fromStream(&$stream) {
+      $cal= &new VCalendar();
+      
+      $p= &new VFormatParser(VCAL_ID);
+      $p->setDefaultHandler(array(&$cal, 'addProperty'));
+      
+      try(); {
+        $p->parse($stream);
+      } if (catch('Exception', $e)) {
+        return throw($e);
+      }
+      
+      return $cal;
+    }
+  
+    /**
+     * Returns the textual representation of this vCalendar
+     *
+     * <code>
+     *   [...]
+     *   $f= &new File('calendar.ics');
+     *   $f->open(FILE_MODE_WRITE);
+     *   $f->write($cal->export());
+     *   $f->close();
+     * </code>
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
+    function export() {
+      
+    }
   
   }
 ?>
