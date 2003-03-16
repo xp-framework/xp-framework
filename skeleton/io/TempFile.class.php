95d2ffe364dfe7e80d5b0dbc1923481c42b1e7c9
thekid <timm.friebe@gmail.com>
2003-03-16 14:17:34 +0000

    - Update documentation
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1135 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/io/TempFile.class.php
+++ skeleton/io/TempFile.class.php
@@ -1,33 +1,51 @@
 <?php
-/* Diese Klasse ist Bestandteil des XP-Frameworks
+/* This class is part of the XP framework
  *
- * $Id$
+ * $Id$ 
  */
 
-  uses(
-    'io.File'
-  );
+  uses('io.File', 'lang.System');
     
   /**
-   * Temporäre Datei. Das Verzeichnis, in dem die Datei landet, defaultet nach /tmp -
-   * ist eine der beiden Umgebungsvariablen "TMP" oder "TEMP" deklariert
-   * und nicht leer, so wird deren Wert stattdessen genommen 
+   * Represents a temporary file
    *
-   * @see io.File
+   * Example:
+   * <code>
+   *   uses('io.TempFile');
+   *
+   *   $f= &new TempFile();
+   *   $f->open(FILE_MODE_WRITE);
+   *   $f->write('Hello');
+   *   $f->close();
+   *
+   *   printf('Created temporary file "%s"', $f->getURI());
+   * </code>
+   *
+   * Note: The temporary file is not deleted when the file
+   * handle is closed (e.g., a call to close()), this will have
+   * to be done manually.
+   *
+   * Note: A possible race condition exists: From the time the
+   * file name string is created (when the constructor is called)
+   * until the time the file is opened (in the call to open)
+   * another process may also open and/or create a file with the
+   * same name. This would have to happen within the same 
+   * microsecond, though, and is therefore quite unlikely.
+   *
+   * @see      xp://io.File
+   * @see      xp://lang.System#tempDir
+   * @purpose  Temporary file
    */
   class TempFile extends File {
   
     /**
      * Constructor
      *
-     * @param  string prefix default "tmp" Das Präfix
-     * see     php://tempnam
+     * @access  public
+     * @param   string prefix default "tmp"
      */
     function __construct($prefix= 'tmp') {
-      $dir= '/tmp';
-      if ('' != getenv('TEMP')) $dir= getenv('TEMP');
-      if ('' != getenv('TMP')) $dir= getenv('TMP');
-      parent::__construct(tempnam($dir, $prefix));
+      parent::__construct(tempnam(System::tempDir(), $prefix.uniqid((double)microtime())));
     }
   }
 ?>
