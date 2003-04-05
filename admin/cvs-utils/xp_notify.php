206768409687e4fcb6b8887181fea6f34e5c590d
kiesel <alex@kiesel.name>
2003-04-05 19:55:42 +0000

    - moved from xp/util/bin/cvs
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1188 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ admin/cvs-utils/xp_notify.php
@@ -0,0 +1,87 @@
+#!/usr/bin/php -q
+<?php
+/* CVS commit notifier
+ * This shall be called from CVSROOT/loginfo in the following way:
+ * 
+ * ALL     (cat > /tmp/loginfo_$USER ; /home/cvs/bin/notify.php your@email.com %{Vvs} 1>/dev/null 2>/dev/null &)
+ * 
+ * $Id$
+ */
+
+  require (dirname (__FILE__).'/common.inc');
+  
+  // Change directory to imitate PHP4.2.3 behaviour
+  chdir(getenv('CVSROOT'));
+
+  // Read message from stdin, die silently if not possible
+  if (FALSE === ($fd= @fopen('/tmp/loginfo_'.getenv('USER'), 'r'))) {
+    exit();
+  } 
+  $msg= '';
+  while ($buf= fgets($fd, 4096)) {
+    $msg.= $buf;
+  }
+  fclose($fd);
+  unlink('/tmp/loginfo_'.getenv('USER'));
+
+  // Append boundary
+  $msg.= "\n";
+
+  // Argument #1 is the mail address no notify
+  $to= $argv[1];
+  
+  // Argument #2 contains the directory, the committed files and their versions
+  $args= explode(' ', $argv[2]);
+  
+  // First element is directory relative to CVSROOT environment variable
+  // Commits in multiple directories are scheduled as two or more commits, actually
+  $dir= $args[0];
+  
+  // Handle "- New directory". This is probably not safe for files or directories
+  // called "-".
+  if ('-' == $args[1]) {
+    $msg.= sprintf("Index: %s\n--- (add)\n+++ Initial revision\n\n", $dir);
+    $args= array();
+  }
+
+  // Recurse through files. This is probably not safe for files with , in their names
+  foreach (array_slice($args, 1) as $idx) {
+    list($old_ver, $new_ver, $file)= explode(',', $idx);
+  
+    // Diff the file if it's not binary. This is an uncomplete list, of course
+    if (preg_match('/\.(gif|png|jpg|tar\.gz|exe|dll|zip)$/', $file)) continue;
+
+    if ('NONE' == $new_ver) {
+
+      // If new version is "NONE", this means the file has been removed from CVS
+      $msg.= sprintf("Index: %s/%s\n--- %s\n+++ (delete)\n", $dir, $file, $old_ver);
+    } elseif ('NONE' == $old_ver) {
+
+      // If old version is "NONE", this means the file has been added
+      $msg.= sprintf("Index: %s/%s\n--- (add)\n+++ %s\n", $dir, $file, $new_ver);	
+    } else {
+
+      // Execute diff between the two versions
+      $cmd= sprintf('cvs -Ql rdiff -r%s -r%s -u %s/%s 2>&1', $old_ver, $new_ver, $dir, $file); 
+      $msg.= "[{$cmd}]\n".`$cmd`;
+    }
+
+    // Append boundary
+    $msg.= "\n";
+  }
+
+  $realname= getUsername (getenv ('USER'));
+
+  // Append signature
+  $msg.= "-- \n".$realname."\n";
+
+  // Finally, send mail
+  mail(
+    $to, 
+    '[CVS] commit: '.$dir,
+    $msg,
+    'X-CVS: '.getenv('CVSROOT')."\n".
+    'From: '.qp_encode_header($realname).' <'.$uname.'@'.getenv('HOSTNAME').">\n".
+    'Reply-To: '.$to
+  );
+?>  
