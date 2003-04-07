afed8bbf7195209e15f1790d06cfba5fa9416f7c
thekid <timm.friebe@gmail.com>
2003-04-07 15:34:27 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1192 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/com/microsoft/format/excel/Worksheet.class.php
@@ -0,0 +1,17 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  /**
+   * Excel worksheet
+   *
+   * @purpose  Represent Excel object
+   */
+  class Worksheet extends Object {
+    var
+      $sheets   = array();
+      
+  }
+?>
