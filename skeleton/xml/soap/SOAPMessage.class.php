d8fbcbcdc9497a96e8375952aa168ed9e5df045f
thekid <timm.friebe@gmail.com>
2003-04-08 10:50:48 +0000

    - Update documentation - Add example SOAP message - Fix bug with nested maps
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1194 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/xml/soap/SOAPMessage.class.php
+++ skeleton/xml/soap/SOAPMessage.class.php
@@ -12,9 +12,29 @@
   );
   
   /**
-   * SOAP Message
+   * A SOAP Message consists of an envelope containing a body, and optionally,
+   * headers.
    *
-   * @see   xp://xml.Tree
+   * Example message in its XML representation:
+   * <pre>
+   * <?xml version="1.0" encoding="iso-8859-1"?>
+   * <SOAP-ENV:Envelope
+   *  xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
+   *  xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+   *  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+   *  xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
+   *  xmlns:si="http://soapinterop.org/xsd"
+   *  SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"
+   *  xmlns:ctl="urn://binford/Power"
+   * >
+   *   <SOAP-ENV:Body>
+   *     <ctl:getPower/>
+   *   </SOAP-ENV:Body>
+   * </SOAP-ENV:Envelope>
+   * </pre>
+   *
+   * @see      xp://xml.Tree
+   * @purpose  Represent SOAP Message
    */
   class SOAPMessage extends Tree {
     var 
@@ -51,9 +71,10 @@
     }
     
     /**
-     * Setzt die Daten
+     * Set data
      *
-     * @param   array arr Array aller zu übergebender Daten
+     * @access  public
+     * @param   array arr
      */
     function setData($arr) {
       $node= &new SOAPNode();
@@ -68,129 +89,148 @@
     }
 
     /**
-     * (Insert method's description here)
+     * Deserialize a single node
      *
-     * @access  
-     * @param   
-     * @return  
-     */    
-    function _recurseData(&$node, $names= FALSE, $context= NULL) {
-      $results= array();
-      if (!empty($node->children)) foreach ($node->children as $child) {
-        $idx= $names ? $child->name : sizeof($results);
-        
-        if (
-          isset($child->attribute['xsi:null']) or       // Java
-          isset($child->attribute['xsi:nil'])           // SOAP::Lite
-        ) {
-          $results[$idx]= NULL;
-          continue;
-        }
-       
-        // Typenabhängig
-        if (!isset($child->attribute['xsi:type']) || !preg_match(
-          '#^([^:]+):([^\[]+)(\[[0-9+]\])?$#', 
-          $child->attribute['xsi:type'],
-          $regs
-        )) {
-          // Zum Beispiel SOAP-ENV:Fault
-          $regs= array(0, 'xsd', 'string');
-        }
+     * @access  private
+     * @param   &xml.Node child
+     * @return  &mixed result
+     */
+    function &unmarshall(&$child, $context= NULL) {
+      if (
+        isset($child->attribute['xsi:null']) or       // Java
+        isset($child->attribute['xsi:nil'])           // SOAP::Lite
+      ) {
+        return NULL;
+      }
 
-        // SOAP-ENC:arrayType="xsd:anyType[4]"
-        if (isset($child->attribute['SOAP-ENC:arrayType'])) {
-          $regs[2]= 'Array';
-        }
-        
-        // echo "*** {$child->name} [{$names}, {$context}, {$idx}] is {$regs[2]}\n";
-        // echo $child->getSource(0);
-        
-        switch (strtolower($regs[2])) {
-          case 'array':
-          case 'vector':
-            $results[$idx]= $this->_recurseData($child, FALSE, 'ARRAY');
+      // Typenabhängig
+      if (!isset($child->attribute['xsi:type']) || !preg_match(
+        '#^([^:]+):([^\[]+)(\[[0-9+]\])?$#', 
+        $child->attribute['xsi:type'],
+        $regs
+      )) {
+        // Zum Beispiel SOAP-ENV:Fault
+        $regs= array(0, 'xsd', 'string');
+      }
+
+      // SOAP-ENC:arrayType="xsd:anyType[4]"
+      if (isset($child->attribute['SOAP-ENC:arrayType'])) {
+        $regs[2]= 'Array';
+      }
+
+      #ifdef DEBUG
+      # echo "*** {$child->name} [{$names}, {$context}, {$idx}] is {$regs[2]}\n";
+      # echo $child->getSource(0);
+      #endif
+
+      switch (strtolower($regs[2])) {
+        case 'array':
+        case 'vector':
+          $result= $this->_recurseData($child, FALSE, 'ARRAY');
+          break;
+
+        case 'map':
+          // <old_data xmlns:ns4="http://xml.apache.org/xml-soap" xsi:type="ns4:Map">
+          // <item>
+          // <key xsi:type="xsd:string">Nachname</key>
+          // <value xsi:type="xsd:string">Braun</value>
+          // </item>
+          // <item>
+          // <key xsi:type="xsd:string">PLZ</key>
+          // <value xsi:type="xsd:string">76135</value>
+          // </item>
+          // <item>
+          if (empty($child->children)) break;
+          foreach ($child->children as $item) {
+            $key= $item->children[0]->getContent($this->getEncoding());
+            $result[$key]= (empty($item->children[1]->children) 
+              ? $item->children[1]->getContent($this->getEncoding())
+              : $this->unmarshall($item->children[1], 'MAP')
+            );
+          }
+          break;
+
+        case 'soapstruct':
+        case 'struct':      
+        case 'ur-type':
+          if ($regs[1]== 'xsd') {
+            $result= $this->_recurseData($child, TRUE, 'HASHMAP');
             break;
-            
-          case 'map':
-            // <old_data xmlns:ns4="http://xml.apache.org/xml-soap" xsi:type="ns4:Map">
-            // <item>
-            // <key xsi:type="xsd:string">Nachname</key>
-            // <value xsi:type="xsd:string">Braun</value>
-            // </item>
-            // <item>
-            // <key xsi:type="xsd:string">PLZ</key>
-            // <value xsi:type="xsd:string">76135</value>
-            // </item>
-            // <item>
-            if (!empty($node->children)) foreach ($child->children as $item) {
-              $key= $item->children[0]->getContent($this->getEncoding());
-              $results[$idx][$key]= (empty($item->children[1]->children) 
-                ? $item->children[1]->getContent($this->getEncoding())
-                : $this->_recurseData($item->children[1], FALSE, 'MAP')
-              );
+          }
+
+          $n= 'stdClass';
+          if (isset($child->attribute['xmlns:xp'])) {
+            try(); {
+              $n= ClassLoader::loadClass($regs[2]);
+            } if (catch('Exception', $e)) {
+              $n= 'stdClass';
             }
-            break;
-   
-          case 'soapstruct':
-          case 'struct':      
-          case 'ur-type':
+          }
+          $result= &new $n();
+          foreach ($this->_recurseData($child, TRUE, 'OBJECT') as $key=> $val) {
+            $result->$key= $val;
+          }
+          break;
+
+        default:
+          if (!empty($child->children)) {
             if ($regs[1]== 'xsd') {
-              $results[$idx]= $this->_recurseData($child, TRUE, 'HASHMAP');
+              $result= $this->_recurseData($child, TRUE, 'STRUCT');
               break;
             }
-            
-            $n= 'stdClass';
-            if (isset($child->attribute['xmlns:xp'])) {
-              try(); {
-                $n= ClassLoader::loadClass($regs[2]);
-              } if (catch('Exception', $e)) {
-                $n= 'stdClass';
-              }
-            }
-            $results[$idx]= &new $n();
+
+            $result= &new stdClass();
             foreach ($this->_recurseData($child, TRUE, 'OBJECT') as $key=> $val) {
-              $results[$idx]->$key= $val;
+              $result->$key= $val;
             }
             break;
-          
-          default:
-            if (!empty($child->children)) {
-              if ($regs[1]== 'xsd') {
-                $results[$idx]= $this->_recurseData($child, TRUE, 'STRUCT');
-                break;
-              }
-              
-              $results[$idx]= &new stdClass();
-              foreach ($this->_recurseData($child, TRUE, 'OBJECT') as $key=> $val) {
-                $results[$idx]->$key= $val;
-              }
-              break;
-            }
-            
-            $results[$idx]= $child->getContent($this->getEncoding());
-        }
-        
-        /* HACK */
-        if (
-          $context == NULL and 
-          $child->name != 'item' and
-          substr($child->name, 0, 8) != 'c-gensym'
-        ) {
-          $results[$idx]= &new SOAPNamedItem($child->name, $results[$idx]);
-        }
+          }
 
+          $result= $child->getContent($this->getEncoding());
+      }
+
+      /* HACK */
+      if (
+        $context == NULL and 
+        $child->name != 'item' and
+        substr($child->name, 0, 8) != 'c-gensym'
+      ) {
+        $result= &new SOAPNamedItem($child->name, $result);
       }
       
-      // DEBUG var_dump($results);
+      return $result;
+    }
+
+    /**
+     * Recursively unmarshall data
+     *
+     * @access  private
+     * @param   &xml.Node node
+     * @param   bool names default FALSE
+     * @param   string context default NULL
+     * @return  &mixed data
+     */    
+    function &_recurseData(&$node, $names= FALSE, $context= NULL) {
+      if (empty($node->children)) return array();
+      
+      $results= array();
+      for ($i= 0, $s= sizeof($node->children); $i < $s; $i++) {
+        $results[$names ? $node->children[$i]->name : $i]= $this->unmarshall(
+          $node->children[$i], 
+          $context
+        );
+      }
       return $results;
     }
 
     /**
-     * (Insert method's description here)
+     * Set fault
      *
-     * @access  
-     * @param   
-     * @return  
+     * @access  public
+     * @param   int faultcode
+     * @param   string faultstring
+     * @param   string faultactor default NULL
+     * @param   mixed detail default NULL
      */    
     function setFault($faultcode, $faultstring, $faultactor= NULL, $detail= NULL) {
       $this->root->children[0]->children[0]= &new SOAPNode();
@@ -205,13 +245,12 @@
     }
 
     /**
-     * (Insert method's description here)
+     * Get fault
      *
-     * @access  
-     * @param   
-     * @return  
+     * @access  public
+     * @return  &xml.soap.SOAPFault or NULL if none exists
      */
-    function getFault() {
+    function &getFault() {
       //echo "+++ SOAPMessage::getFault\n";
       if ($this->root->children[0]->children[0]->name != 'SOAP-ENV:Fault') return NULL;
       
@@ -225,13 +264,13 @@
     }
     
     /**
-     * (Insert method's description here)
+     * Get data
      *
-     * @access  
-     * @param   
-     * @return  
+     * @access  public
+     * @param   string context default 'ENUM'
+     * @return  &mixed data
      */
-    function getData($context= 'ENUM') {
+    function &getData($context= 'ENUM') {
       foreach ($this->root->attribute as $key=> $val) { // Namespace suchen
         if ($val == $this->action) $this->namespace= substr($key, strlen('xmlns:'));
       }
