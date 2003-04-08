efa9469b5125ef3082f398d25fa5a90dd31c4509
thekid <timm.friebe@gmail.com>
2003-04-08 22:41:30 +0000

    - Untabify - Add getDirectory method
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1204 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/com/microsoft/format/chm/CHMFile.class.php
+++ skeleton/com/microsoft/format/chm/CHMFile.class.php
@@ -6,23 +6,24 @@
 
   uses(
     'util.Date',
-    'com.microsoft.format.chm.CHMHeader'
+    'com.microsoft.format.chm.CHMHeader',
+    'com.microsoft.format.chm.CHMDirectory'
   );
 
   // Languages  
-  define('CHM_LANG_ENGLISH', 	0x0409);
-  define('CHM_LANG_GERMAN', 	0x0407);
+  define('CHM_LANG_ENGLISH',    0x0409);
+  define('CHM_LANG_GERMAN',     0x0407);
   
   // Chunk types
-  define('CHM_CHUNK_LISTING',	0x0000);
-  define('CHM_CHUNK_INDEX',		0x0001);
+  define('CHM_CHUNK_LISTING',   0x0000);
+  define('CHM_CHUNK_INDEX',     0x0001);
   
   // Well known identifier for namelist
-  define('CHM_FILE_NAMELIST',	'::DataSpace/NameList');
+  define('CHM_FILE_NAMELIST',   '::DataSpace/NameList');
   
   // Compressed / uncompressed identifiers
-  define('CHM_COMPRESSED',		'MSCompressed');
-  define('CHM_UNCOMPRESSED',	'Uncompressed');
+  define('CHM_COMPRESSED',      'MSCompressed');
+  define('CHM_UNCOMPRESSED',    'Uncompressed');
 
   /**
    * CHM (Compile HTML) reader
@@ -34,7 +35,8 @@
    *   $f= &new CHMFile(new File('file.chm'));
    *   try(); {
    *     $f->open();
-   *     $header= $f->getHeader();
+   *     $header= &$f->getHeader();
+   *     $dir= &$f->getDirectory();
    *     $f->close();
    *   } if (catch('Exception', $e)) {
    *     $e->printStackTrace();
@@ -42,6 +44,7 @@
    *   }
    *   
    *   var_export($header);
+   *   var_export($dir);
    * </code>
    *
    * @see      http://www.speakeasy.org/~russotto/chm/chmformat.html
@@ -49,8 +52,11 @@
    */
   class CHMFile extends Object {
     var
-      $stream   = NULL,
-      $header   = NULL;
+      $stream           = NULL,
+      $header           = NULL;
+      
+    var
+      $_headeroffset    = 0;
     
     /**
      * Constructor
@@ -87,8 +93,8 @@
      */
     function _substr($str, &$p, $len) {
       $str= substr($str, ++$p, $len);
-	  $p+= $len;
-	  return $str;
+      $p+= $len;
+      return $str;
     }
 
     /**
@@ -102,11 +108,11 @@
      */
     function _int($str, &$p) {
       $r= 0;
-	  while (ord($str{$p}) & 0x80) {
-	    $r= ($r << 7) | (ord($str{$p++}) & 0x7F);
-	  }
-	  $r= ($r << 7) | ord($str{$p++});
-	  return $r;
+      while (ord($str{$p}) & 0x80) {
+        $r= ($r << 7) | (ord($str{$p++}) & 0x7F);
+      }
+      $r= ($r << 7) | ord($str{$p++});
+      return $r;
     }
     
     /**
@@ -117,33 +123,33 @@
      * @return  array entries
      */
     function _dir($length, $qref, $format) {
-	  $str= $this->stream->read($length- $qref);
-	  $pos= 0;
-	  $max= strlen($str);
-	  do {
-	    $len= ord($str{$pos});
-	    $name= $this->_substr($str, $pos, $len);
-	    switch ($format) {
-	      case CHM_CHUNK_LISTING: 
-		    $entries[$name]= array(
-		      'name'	=> $name,
-			  'section' => $this->_int($str, $pos),
-	    	  'offset'	=> $this->_int($str, $pos),
-	    	  'length'	=> $this->_int($str, $pos)
-	        );
-  		    break;
+      $str= $this->stream->read($length- $qref);
+      $pos= 0;
+      $max= strlen($str);
+      do {
+        $len= ord($str{$pos});
+        $name= $this->_substr($str, $pos, $len);
+        switch ($format) {
+          case CHM_CHUNK_LISTING: 
+            $entries[$name]= array(
+              'name'    => $name,
+              'section' => $this->_int($str, $pos),
+              'offset'    => $this->_int($str, $pos),
+              'length'    => $this->_int($str, $pos)
+            );
+              break;
 
-		  case CHM_CHUNK_INDEX:
-		    $entries[$name]= array(
-		      'name'		=> $name,
-			  'index'		=> $this->_int($str, $pos)
-		    );
-  		    break;
+          case CHM_CHUNK_INDEX:
+            $entries[$name]= array(
+              'name'        => $name,
+              'index'        => $this->_int($str, $pos)
+            );
+              break;
         }
       } while ($pos < $max);
-	  $this->stream->seek($qref, SEEK_CUR);
+      $this->stream->seek($qref, SEEK_CUR);
       
-	  return $entries;
+      return $entries;
     }
 
     /**
@@ -174,42 +180,82 @@
      * @throws  lang.FormatException if the identifier is not correct
      */
     function &getHeader() {
-      if (!empty($this->header)) return $this->header;
+      if (!empty($this->header)) {
+        $this->stream->seek($this->_headeroffset, SEEK_SET);
+        return $this->header;
+      }
       
       $this->stream->seek(0x00, SEEK_SET);
-      if ('ITSF' !== ($id= $this->stream->read(4))) {
+      if (CHM_HEADER_IDENTIFIER !== ($id= $this->stream->read(4))) {
         return throw(new FormatException(
           '"'.addcslashes($id, "\0..\17").'" is not a correct identifier, expecting "ITSF"'
         ));
       }
       
-	  $this->header= &new CHMHeader(unpack(
-        'Lversion/Llength/Lunknown/Ltime/Llang', 
-	    $this->stream->read(0x14)
-	  ));
-      $this->header->setIdentifier($id);
- 	  $this->header->setGuid1($this->_guid(unpack(
+      // Create header
+      $this->header= &new CHMHeader(unpack(
+        'a4identifier/Lversion/Llength/Lunknown/Ltime/Llang', 
+        $id.$this->stream->read(0x14)
+      ));
+      
+      // Always {7C01FD10-7BAA-11D0-9E0C-00A0-C922-E6EC}
+       $this->header->setGuid1($this->_guid(unpack(
         'Lguid1/v2guid2/C8guid3',
-	    $this->stream->read(0x10)
-	  )));
-	  $this->header->setGuid2($this->_guid(unpack(
+        $this->stream->read(0x10)
+      )));
+      
+      // Always {7C01FD11-7BAA-11D0-9E0C-00A0-C922-E6EC}
+      $this->header->setGuid2($this->_guid(unpack(
         'Lguid1/v2guid2/C8guid3',
-	    $this->stream->read(0x10)
-	  )));
+        $this->stream->read(0x10)
+      )));
 
-	  // Section table
-	  $this->header->setSection(unpack(
+      // Section table
+      $this->header->setSection(unpack(
         'Loffset/Llength',
-	    $this->stream->read(0x16)
-	  ));
+        $this->stream->read(0x16)
+      ));
 
-	  // Content_offset
-	  if (3 == $this->header->getVersion()) {
+      // Content_offset
+      if (3 == $this->header->getVersion()) {
         $this->stream->seek(0x58, SEEK_SET);
-	    $this->header->setContent_offset(array_shift(array_values(unpack('L', $this->stream->read(0x20)))));
-	  }	
+        $this->header->setContent_offset(array_shift(array_values(unpack('L', $this->stream->read(0x20)))));
+      }    
 
-	  return $this->header;    
+      $this->_headeroffset= $this->stream->tell();
+      return $this->header;    
+    }
+    
+    /**
+     * Retreive CHM directory; extracts it if necessary
+     *
+     * @access  public
+     * @return  &com.microsoft.format.chm.CHMDirectory
+     * @throws  lang.FormatException if the identifier is not correct
+     */ 
+    function &getDirectory() {
+      $this->getHeader();       // We need this so the file pointer position is correct
+
+      if (CHM_DIRECTORY_IDENTIFIER !== ($id= $this->stream->read(4))) {
+        return throw(new FormatException(
+          '"'.addcslashes($id, "\0..\17").'" is not a correct identifier, expecting "ITSP"'
+        ));
+      }
+      
+      $directory= &new CHMDirectory(unpack(
+        'a4identifier/Lversion/Llength/Lunknown/Lchunk_size/Ldensity/Ldepth/Lrootindex_chunk/Lfirst_pmgl/Llast_pmgl/Lunknown/Lnum_chunks/Llang',
+        $id.$this->stream->read(0x30)
+      ));
+      $directory->guid= $this->_guid(unpack(
+        'Lguid1/v2guid2/C8guid3',
+        $this->stream->read(0x10)
+      ));
+      $directory->ext= unpack(
+        'Llength/L3unknown',
+        $this->stream->read(0x10)
+      );
+      
+      return $directory;
     }
   }
 ?>
