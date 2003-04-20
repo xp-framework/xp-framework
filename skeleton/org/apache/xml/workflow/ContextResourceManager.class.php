2431284ba289ef0803d59d6f0718a35476cd0f7e
thekid <timm.friebe@gmail.com>
2003-04-20 18:19:12 +0000

    - Add getContextResource() method
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1254 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/org/apache/xml/workflow/ContextResourceManager.class.php
+++ skeleton/org/apache/xml/workflow/ContextResourceManager.class.php
@@ -11,12 +11,13 @@
   /**
    * (Insert class' description here)
    *
-   * @ext      extensiom
    * @see      reference
    * @purpose  purpose
    */
   class ContextResourceManager extends Object {
-  
+    var
+      $crs  = array();
+      
     /**
      * Called to initialize this resource manager
      *
@@ -25,6 +26,21 @@
      * @return  
      */
     function initialize() {
+    }
+    
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
+    function &getContextResource($name) {
+      if (!isset($this->crs[$name])) {
+        $this->crs[$name]= &new ContextResource($name);
+      }
+      
+      return $this->crs[$name];
     }
   }
 ?>
