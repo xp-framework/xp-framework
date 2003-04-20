5b47bc9215ce330e3dd6fa50ae0285a26658feb4
thekid <timm.friebe@gmail.com>
2003-04-20 18:20:19 +0000

    - Use handlers - Add isSubmitTrigger helper method - Implement isAccessible - Add addHandler helper method - Add accessors for name
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1255 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/org/apache/xml/workflow/State.class.php
+++ skeleton/org/apache/xml/workflow/State.class.php
@@ -4,15 +4,20 @@
  * $Id$ 
  */
 
+  uses('org.apache.xml.workflow.Handler');
+  
   /**
    * (Insert class' description here)
    *
    * @ext      extensiom
    * @see      reference
-   * @purpose  purpose
+   * @purpose  Base class
    */
   class State extends Object {
-  
+    var
+      $name     = '',
+      $handlers = array();
+
     /**
      * (Insert method's description here)
      *
@@ -20,8 +25,70 @@
      * @param   
      * @return  
      */
-    function isAccessible(&$crm) {
-      return TRUE;
+    function &addHandler(&$handler) {
+      $this->handlers[]= &$handler;
+      return $handler;
+    }
+
+    /**
+     * Set Name
+     *
+     * @access  public
+     * @param   string name
+     */
+    function setName($name) {
+      $this->name= $name;
+    }
+
+    /**
+     * Get Name
+     *
+     * @access  public
+     * @return  string
+     */
+    function getName() {
+      return $this->name;
+    }
+
+    /**
+     * Return whether this state should be accessible. This is 
+     * TRUE for a situation in which no handlers exist or for
+     * the situation in which at least _one_ handler's prerequisites
+     * are met.
+     *
+     * @see     xp://org.apache.xml.workflow.Handler#prerequisitesMet
+     * @access  
+     * @param   
+     * @return  
+     */
+    function isAccessible(&$context) {
+      for ($i= 0, $s= sizeof($this->handlers); $i < $s; $i++) {
+        if ($this->handlers[$i]->prerequisitesMet($context)) return TRUE;
+      }
+      return ($s == 0);
+    }
+    
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
+    function isSubmitTrigger(&$request) {
+      return ($request->hasParam('__form') || $request->hasParam('__sendingdata'));
+    }
+    
+    /**
+     * 
+     *
+     * @access  
+     * @param   
+     * @param   
+     * @param   
+     * @return  bool
+     */
+    function getDocument(&$context, &$request, &$response) {
     }
   }
 ?>
