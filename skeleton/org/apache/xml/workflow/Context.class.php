8bfb883427d03878e531ccc6a09ecc3d4e09f5f4
thekid <timm.friebe@gmail.com>
2003-04-20 18:18:20 +0000

    - Add getContextResourceManager() method - Implement first version of state flows - Call state handlers
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1252 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/org/apache/xml/workflow/Context.class.php
+++ skeleton/org/apache/xml/workflow/Context.class.php
@@ -6,6 +6,7 @@
 
   uses(
     'org.apache.xml.workflow.ContextResourceManager',
+    'org.apache.xml.workflow.StateFlowManager',
     'org.apache.xml.workflow.State'
   );
 
@@ -18,8 +19,8 @@
    */
   class Context extends Object {
     var
-      $classloader  = NULL,
-      $crm          = NULL;
+      $crm          = NULL,
+      $sfm          = NULL;
       
     /**
      * Called to initialize this application context
@@ -28,9 +29,21 @@
      * @param   &lang.ClassLoader classloader
      */
     function initialize(&$classloader) {
-      $this->classloader= &$classloader;
       $this->crm= &new ContextResourceManager();
       $this->crm->initialize();
+      $this->sfm= &new StateFlowManager();
+      $this->sfm->initialize($classloader);
+    }
+    
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
+    function &getContextResourceManager() {
+      return $this->crm;
     }
     
     /**
@@ -48,34 +61,71 @@
       $cat= &$l->getCategory($this->getClassName());
       
       // Load corresponding state
-      // TBD: StateFlows
-      do {
-        if (!($state= &$this->_getCorrespondingState($request->getState()))) return FALSE;
+      $state= &$this->sfm->getStateByName($request->getState());
+      while ($state && !$state->isAccessible($this)) {
+        $state= &$this->sfm->getNextState();
+      }
+      
+      // No state found
+      if (!$state) {
+        return throw(new HttpScriptletException('No state found'));
+      }
+      
+      $this->sfm->setCurrentState($state);
+      $cat->info($this->sfm, $state);
+      
+      // Now that we have the correct state:
+      // - call all of its handlers and if this succeeds,
+      // - call its getDocument method
+      try(); {
+        $has_error= FALSE;
+        if ($state->isSubmitTrigger($request)) {
+          for ($i= 0, $s= sizeof($state->handlers); $i < $s; $i++) {
+            $cat->info('Calling handler #'.$i, $state->handlers[$i]);
+            
+            // If this handler is not active, ask the next
+            if (!$state->handlers[$i]->isActive($this)) {
+              $cat->warn('Handler is not active, proceeding...');
+              continue;
+            }
+
+            // If this handler is satisfied, ask the next
+            if (!$state->handlers[$i]->needsData($this)) {
+              $cat->warn('Handler does not need data, proceeding...');
+              continue;
+            }  
+
+            // Handle submitted data
+            if ($state->handlers[$i]->handleSubmittedData($this, $request)) {
+              $cat->info('handleSubmittedData returns TRUE, proceeding...');
+              continue;
+            }
+
+            $cat->error('Errors occured', $state->handlers[$i]->errors);
+            // In case of an error, add all errors to formerrors ...
+            foreach ($state->handlers[$i]->errors as $statuscode) {
+              $response->addFormError(
+                $state->handlers[$i]->getClassName(),
+                $statuscode
+              );
+            }
+
+            // ...and break out of the loop immediately
+            $has_error= TRUE;
+            break;
+          }
+        }
         
-        $accessible= $state->isAccessible();
-        $cat->info($state, $accessible);
-      } while (!$accessible);
+        if (!$has_error) {
+          $cat->info('Calling getDocument()');
+          $state->getDocument($this, $request, $response);
+        }
+      } if (catch('Exception', $e)) {
+        return throw(new HttpScriptletException($e->message));
+      }
       
       return TRUE;
     }
     
-    /**
-     * Returns corresponding state
-     *
-     * @access  private
-     * @param   string name
-     * @return  &org.apache.xml.workflow.State
-     */
-    function &_getCorrespondingState($name) {
-      try(); {
-        $class= &$this->classloader->loadClass(ucfirst($name).'State');
-      } if (catch('ClassNotFoundException', $e)) {
-        return throw(new HttpScriptletException($e->message));
-      } if (catch('RunTimeException', $e)) {
-        return throw(new HttpScriptletException($e->message));
-      }
-      
-      return new $class();
-    }
   }
 ?>
