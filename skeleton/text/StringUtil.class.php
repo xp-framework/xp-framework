542b1e060334bcde45d6eb593516205c57110d02
thekid <timm.friebe@gmail.com>
2003-04-24 15:01:08 +0000

    - Also return results
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1259 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/text/StringUtil.class.php
+++ skeleton/text/StringUtil.class.php
@@ -14,28 +14,35 @@
   
     /**
      * Delete a specified amount of characters from a string as
-     * of a specified position.
+     * of a specified position. The resulting string is copied to the 
+     * parameter "string" and also returned as result.
      *
      * @model   static
      * @access  public
      * @param   &string string
      * @param   int pos
      * @param   int len default 1
+     * @return  string
      */
     function delete(&$string, $pos, $len= 1) {
       $string= substr($string, 0, $pos).substr($string, $pos+ 1);
+      return $string;
     }
     
     /**
-     * Insert a character into a string at a specified position
+     * Insert a character into a string at a specified position. The 
+     * resulting string is copied to the parameter "string" and also 
+     * returned as result.
      *
      * @access  public
      * @param   &string string
      * @param   înt pos
      * @param   char char
+     * @return  string
      */
     function insert(&$string, $pos, $char) {
       $string= substr($string, 0, $pos).$char.substr($string, $pos);
+      return $string;
     } 
   }
 ?>
