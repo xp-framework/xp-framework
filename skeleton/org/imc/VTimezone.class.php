ce01a76aa70f2009f3e184dbc6ddebf0bc366890
kiesel <alex@kiesel.name>
2003-04-26 23:27:22 +0000

    - export functions for VTimezone
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1281 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/org/imc/VTimezone.class.php
+++ skeleton/org/imc/VTimezone.class.php
@@ -9,13 +9,16 @@
     'peer.URL'
   );
   
+  // Identifier
+  define ('VTZ_ID',       'VTIMEZONE');
+  
   class VTimezone extends Object {
     var
       $tzid=      '',
       $daylight=  NULL,
       $standard=  NULL,
       $tzurl=     NULL,
-      $lastmod=   '';
+      $lastmod=   NULL;
 
     /**
      * Construcotr
@@ -29,11 +32,11 @@
         'dtstart'       => NULL,    // Mandatory
         'tzoffsetto'    => NULL,    // Mandatory
         'tzoffsetfrom'  => NULL,    // Mandatory
-        'comment'       => '',
-        'rdate'         => '',
-        'rrule'         => '',
-        'tzname'        => '',
-        'x-prop'        => ''
+        'comment'       => NULL,
+        'rdate'         => NULL,
+        'rrule'         => NULL,
+        'tzname'        => NULL,
+        'x-prop'        => NULL,
       );
     }
 
@@ -94,6 +97,66 @@
      */    
     function &getLastMod() {
       return $this->lastmod;
+    }
+
+    /**
+     * Export function helper
+     *
+     * @access  private
+     * @param   string key
+     * @param   mixed value
+     * @return  string exported
+     */    
+    function _export($key, $value) {
+      // Never add empty fields
+      if (NULL === $value)
+        return '';
+        
+      if (is_a ($value, 'Date')) {
+        // Convert date into string
+        $value= $value->toString ('Ymd').'T'.$value->toString ('His').'Z';
+      } else if (is_a ($value, 'URL')) {
+        // Convert URL to string
+        $value= $value->toString();
+      }
+
+      // Escape string, encode it to UTF8    
+      return ($key.':'.strtr(utf8_encode ($value), array (
+        ','   => '\,',
+        "\n"  => '\n'
+      ))."\n");
+    }
+    
+    /**
+     * Return a string representation of this object suitable
+     * for inclusion in an VCalendar
+     *
+     * @access  public
+     * @return  string representation
+     */    
+    function export() {
+      $ret = $this->_export ('BEGIN',     VTZ_ID);
+      $ret.= $this->_export ('TZID',      $this->getTzid());
+
+      $ret.= $this->_export ('TZURL',     $this->getTZurl());
+      $ret.= $this->_export ('LAST-MOD',  $this->getLastMod());
+      
+      foreach (array ('daylight', 'standard') as $type) {
+        // Begin daylight/standard part
+        $ret.= $this->_export ('BEGIN', strtoupper ($type));
+        
+        // Export all attributes
+        foreach ($this->{$type} as $key=> $value) {
+          if ($value) { $ret.= $this->_export (strtoupper ($key), $value); }
+        }
+        
+        // End daylight/standard part
+        $ret.= $this->_export ('END', strtoupper ($type));
+      }
+      
+      $ret.= $this->_export ('END', VTZ_ID);
+      
+      return $ret;
     }
   }
 ?>
