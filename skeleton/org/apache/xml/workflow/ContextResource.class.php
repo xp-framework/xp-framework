484540daf86c93d938a957ae8fbef4af326d95b7
thekid <timm.friebe@gmail.com>
2003-04-26 20:45:59 +0000

    - Add accessors for values - Add abstract method insertStatus - Add documentation
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1269 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/org/apache/xml/workflow/ContextResource.class.php
+++ skeleton/org/apache/xml/workflow/ContextResource.class.php
@@ -5,14 +5,42 @@
  */
 
   /**
-   * (Insert class' description here)
+   * Generic context resource
    *
-   * @ext      extensiom
-   * @see      reference
-   * @purpose  purpose
+   * @purpose  Base class
    */
   class ContextResource extends Object {
     var
       $values   = array();
+
+    /**
+     * Set a value
+     *
+     * @access  public
+     * @param   string name
+     * @param   &mixed data
+     */
+    function setValue($name, &$data) {
+      $this->values[$name]= &$data;
+    }
+
+    /**
+     * Get a value
+     *
+     * @access  public
+     * @param   string name
+     * @return  &mixed
+     */
+    function &getValue($name) {
+      return $this->values[$name];
+    }
+    
+    /**
+     * Insert status
+     *
+     * @access  public
+     * @param   &xml.Node elem
+     */
+    function insertStatus(&$elem) { }
   }
 ?>
