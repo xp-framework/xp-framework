f162fe39205797437c3b6045d85a6e2016793c9d
thekid <timm.friebe@gmail.com>
2003-05-15 21:29:41 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1344 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/peer/ConnectException.class.php
@@ -0,0 +1,17 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses('peer.SocketException');
+
+  /**
+   * Indicate an error occured during connect
+   *
+   * @purpose  Exception
+   */
+  class ConnectException extends SocketException {
+
+  }
+?>
---
+++ skeleton/peer/SocketException.class.php
@@ -0,0 +1,17 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses('io.IOException');
+
+  /**
+   * Indicate a generic I/O error on a socket
+   *
+   * @purpose  Exception
+   */
+  class SocketException extends IOException {
+
+  }
+?>
