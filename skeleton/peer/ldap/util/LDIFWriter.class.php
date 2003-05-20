0a98f42ccb9eabb0f5187baaa11e37542a4a38ec
thekid <timm.friebe@gmail.com>
2003-05-20 10:08:27 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1379 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/peer/ldap/util/LDIFWriter.class.php
@@ -0,0 +1,113 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Write LDAP entries to LDIF format
+   *
+   * Example:
+   * <code>
+   *   uses(
+   *     'io.File',
+   *     'peer.ldap.LDAPClient',
+   *     'peer.ldap.util.LDIFWriter',
+   *     'peer.URL'
+   *   );
+   *   
+   *   // {{{ main
+   *   $dsn= &new URL('ldap://my.ldap.host:389/uid=friebe,ou=People,o=XP,c=DE');
+   *   $dn= substr($dsn->getPath(), 1);
+   *   
+   *   $l= &new LDAPClient($dsn->getHost(), $dsn->getPort(389));
+   *   $writer= &new LDIFWriter(new File('php://stdout'));
+   *   
+   *   try(); {
+   *     $l->connect();
+   *     $l->bind($dsn->getUser(NULL), $dsn->getPassword(NULL));
+   *     
+   *     $writer->initialize();
+   *     $writer->write($l->read(new LDAPEntry($dn)));
+   *   } if (catch('Exception', $e)) {
+   *     fputs(STDERR, $e->getStackTrace());
+   *   }
+   *   
+   *   $l->close();
+   *   $writer->finish();
+   *   // }}}
+   * </code>
+   *
+   * @purpose  LDIF Writer
+   */
+  class LDIFWriter extends Object {
+    var
+      $stream       = NULL;
+      
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   &io.Stream stream
+     */
+    function __construct(&$stream) {
+      $this->stream= &$stream;
+      parent::__construct();
+    }
+    
+    /**
+     * Initialize this writer
+     *
+     * @access  public
+     * @param   string mode default STREAM_MODE_WRITE
+     * @return  bool success
+     */
+    function initialize($mode= STREAM_MODE_WRITE) {
+      return $this->stream->open($mode);
+    }
+    
+    /**
+     * Destructor
+     *
+     * @access  public
+     */
+    function __destruct() {
+      $this->finish();
+      parent::__destruct();
+    }
+    
+    /**
+     * Write an entry
+     *
+     * @access  public
+     * @param   &peer.ldap.LDAPEntry entry
+     * @throws  lang.IllegalArgumentException in case the parameter is not an LDAPEntry object
+     */
+    function write(&$entry) {
+      if (!is_a($entry, 'LDAPEntry')) {
+        trigger_error('Type: '.gettype($entry).'@'.get_class($entry), E_USER_NOTICE);
+        return throw(new IllegalArgumentException('Parameter entry is expected to be a peer.ldap.LDAPEntry object'));
+      }
+      $this->stream->write(sprintf("dn: %s\n", $entry->getDN()));
+      foreach (array_keys($entry->attributes) as $key) {
+        if ('dn' == $key) continue;
+        for ($i= 0, $s= sizeof($entry->attributes[$key]); $i < $s; $i++) {
+          $this->stream->write(sprintf("%s: %s\n", $key, $entry->attributes[$key][$i]));
+        }
+      }
+      $this->stream->write("\n");
+    }
+    
+    /**
+     * Finish off writing, close and destroy stream
+     *
+     * @access  public
+     */
+    function finish() {
+      if (!$this->stream) return;
+      $this->stream->close();
+      $this->stream->__destruct();
+      $this->stream= NULL;
+    }
+  }
+?>
