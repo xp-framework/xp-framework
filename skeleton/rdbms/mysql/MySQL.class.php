609dc81d3458c512aaaae840db9a6d65e377f4d5
thekid <timm.friebe@gmail.com>
2003-05-25 18:33:45 +0000

    - Migrate to new API
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1416 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/rdbms/mysql/MySQL.class.php
+++ skeleton/rdbms/mysql/MySQL.class.php
@@ -1,399 +1,246 @@
 <?php
 /* This class is part of the XP framework
  *
- * $Id$
+ * $Id$ 
  */
-  
-  uses(
-    'rdbms.SQLException',
-    'util.Date',
-    'util.log.Logger'
-  );
-  
+
+  uses('rdbms.DBConnection', 'rdbms.mysql.MySQLResultSet');
+
   /**
-   * A class that wraps PHP's MySQL functions
+   * Connection to MySQL Databases
    *
-   * @purpose Database class for MySQL
-   * @see     http://mysql.org/
+   * @see      http://mysql.org/
+   * @ext      mysql
+   * @purpose  Database connection
    */
-  class MySQL extends Object {
-    var 
-      $handle= NULL,
-      $host, 
-      $user, 
-      $pass, 
-      $db, 
-      $field_map= array(),
-      $last_affected_rows= -1, 
-      $last_num_rows= -1,
-      $transaction= 0;
-   
-    // Logger
-    var
-      $log;
-	  
-	var
-	  $fields	= NULL,
-	  $lengths	= NULL;
- 
-    /**
-     * Constructor
-     */
-    function __construct($params) {
-      $l= &Logger::getInstance();
-      $this->cat= $l->getCategory($this->getClassName());
-      parent::__construct($params);
-    }
-    
-    /**
-     * Konnektieren
-     *
-     * @access  public
-     * @return  resource Datenbank-Handle
-     * @throws  SQLException, wenn kein Connect zustande kommt
-     */
-    function connect() {
-      $this->handle= mysql_connect($this->host, $this->user, $this->pass);
-      if (FALSE === $this->handle) return throw(new SQLException(sprintf(
-        'Unable to connect to %s@%s',
-        $this->user,
-        $this->host
-      )));
-      return $this->handle;
-    }
+  class MySQL extends DBConnection {
 
     /**
-     * Datenbank auswählen
+     * Connect
      *
-     * @access  public
-     * @param   string db default NULL Auszuwählende Datenbank (wenn NULL, $this->db)
-     * @return  bool result Datenbank ausgewählt?
-     */    
-    function select_db($db= NULL) {
-      if(!is_null($db)) $this->db= $db;
-      return @mysql_select_db($this->db, $this->handle);
+     * @access  public  
+     * @return  bool success
+     * @throws  rdbms.SQLException
+     */
+    function connect() {
+      if (is_resource($this->handle)) return TRUE;  // Already connected
+      if (FALSE === $this->handle) return FALSE;    // Previously failed connecting
+
+      if ($this->flags & DB_PERSISTENT) {
+        $this->handle= mysql_pconnect(
+          $this->dsn->getHost(), 
+          $this->dsn->getUser(), 
+          $this->dsn->getPassword()
+        );
+      } else {
+        $this->handle= mysql_connect(
+          $this->dsn->getHost(), 
+          $this->dsn->getUser(), 
+          $this->dsn->getPassword()
+        );
+      }
+
+      if (!is_resource($this->handle)) {
+        return throw(new SQLException(sprintf(
+          'Unable to connect to %s@%s - using password: %s',
+          $this->dsn->getUser(),
+          $this->dsn->getHost(),
+          $this->dsn->getPassword() ? 'yes' : 'no'
+        )));
+      }
+      
+      return parent::connect();
     }
     
     /**
-     * Private Helper-Funktion, ermöglicht solcherlei Aufrufe
+     * Disconnect
      *
-     * <pre>
-     * $qrh= $dbo->query('
-     *   select 
-     *     domain_id 
-     *   from 
-     *     domain 
-     *   where 
-     *     domainname = %s 
-     *     and adminc= %d
-     *     and ext_vertragpos= %s
-     *   ', 
-     *   'thekid.de', 
-     *   2828822,
-     *   NULL
-     * );
-     * </pre>
+     * @access  public
+     * @return  bool success
+     */
+    function close() { 
+      if ($r= mysql_close($this->handle)) {
+        $this->handle= NULL;
+      }
+      return $r;
+    }
+    
+    /**
+     * Select database
      *
-     * => Um das Quoten von Strings muss sich nicht mehr gekümmert werden!
-     * => NULL wird automatisch zu NULL (bei String-Feldern)
+     * @access  public
+     * @param   string db name of database to select
+     * @return  bool success
+     * @throws  rdbms.SQLException
+     */
+    function selectdb($db) {
+      if (!mysql_select_db($db, $this->handle)) {
+        return throw(new SQLException('Cannot select database', 'use '.$db));
+      }
+      return TRUE;
+    }
+    
+    /**
+     * Protected helper methid
      *
-     * @access  private
-     * @param   array args Die Argumente
+     * @access  protected
+     * @param   array args
+     * @return  string
      */
     function _prepare($args) {
       $sql= $args[0];
-      if (sizeof($args)<= 1) return $sql;
-      $j= 0;    
+      if (sizeof($args) <= 1) return $sql;
+
+      $i= 0;    
       $sql= $tok= strtok($sql, '%');
-      while (++$j && $tok= strtok('%')) {
-        $arg= (is_object($args[$j]) && method_exists($args[$j], 'toString') 
-          ? is_a($args[$j], 'Date') ? $args[$j]->toString('Y-m-d H:i:s') : $args[$j]->toString()
-          : $args[$j]
-        );
-        switch ($tok{0}) {
-          case 'd': $sql.= ($arg === NULL ? 'NULL' : intval($arg)).substr($tok, 1); break;
-          case 'f': $sql.= ($arg === NULL ? 'NULL' : floatval($arg)).substr($tok, 1); break;
-          case 'c': $sql.= ($arg === NULL ? 'NULL' : $arg).substr($tok, 1); break;
-          case 's': $sql.= ($arg === NULL ? 'NULL' : '"'.str_replace('"', '\"', $arg).'"').substr($tok, 1); break;
-          case 'l': $sql.= ($arg === NULL ? 'NULL' : '"'.str_replace('"', '\"', $arg).'%"').substr($tok, 1); break;
-          default: $sql.= '%'.$tok; $j--;
+      while (++$i && $tok= strtok('%')) {
+        if (is_a($args[$i], 'Date')) {
+          $arg= date('Y-m-d H:i:s', $arg->getTime());
+        } elseif (is_a($args[$i], 'Object')) {
+          $arg= $args[$i]->toString();
+        } else {
+          $arg= $args[$i];
         }
+        switch ($tok{0}) {
+          case 'd': $r= intval($arg); break;
+          case 'f': $r= floatval($arg); break;
+          case 'c': $r= $arg; break;
+          case 's': $r= '"'.str_replace('"', '\"', $arg).'"'; break;
+          case 'u': $r= '"'.date ('Y-m-d h:i:s', $arg).'"'; break;
+          default: $sql.= '%'.$tok; $i--; continue;
+        }
+        $sql.= $r.substr($tok, 1);
       }
       return $sql;
     }
-    
-    /**
-     * Query-Funktion. Connected, falls nötig
-     *
-     * @access  public
-     * @param   string sql Der abzusetzende SQL-Query-String
-     * @return  bool result Query-Ergebnis
-     * @throws  SQLException, wenn der Query schiefgeht
-     */
-    function query() {
-      $args= func_get_args();
-	  $this->lengths= $this->fields= NULL;
-	  
-      $sql= $this->_prepare($args);
-
-      // Wenn es keinen Connect gibt, einen herstellen
-      if(!$this->handle) {
-        $connect= $this->connect();
-        if (isset($this->db)) $this->select_db();
-      }
-      
-      $this->cat->info('MySQL::'.$sql);
-      $result= mysql_query($sql, $this->handle);
-      if (FALSE === $result) {
-        return throw(new SQLException(sprintf(
-          'statement failed [%d: %s]', 
-          mysql_errno(),
-          mysql_error()
-        ), $sql));
-      }
-      
-      // Feldtypen herausfinden
-      $i= -1;
-	  $this->lengths= $this->fields= array();
-      while (++$i < @mysql_num_fields($result)) {
-        $field= mysql_fetch_field($result, $i);
-        $this->fields[$field->name]= $field->type;
-		$this->lengths[$field->name]= $field->max_length;
-      }
-      // $this->cat->debug('MySQL::fields', $this->fields);
-      
-      return $result;
-    }
-    
-    /**
-     * Data Seek: Offset innerhalb eines Querys definieren
-     *
-     * @access  public
-     * @param	resource query Queryhandle, z.B. aus query()
-     * @param   int offset Der Offset, zu dem gesprungen wird
-     * @return  bool Konnte geseekt werden?
-     */
-    function data_seek($query, $offset) {
-      return mysql_data_seek($query, $offset);
-    }
-    
-    /**
-     * Einen Datensatz holen
-     *
-     * @access  public
-     * @param	resource query Queryhandle, z.B. aus query()
-     * @return  array Der selektierte Datensatz
-     */
-    function &fetch($query) {
-      $row= mysql_fetch_assoc($query);
-      if (FALSE === $row) return FALSE;
-      
-      foreach($row as $key=> $val) {
-        
-        // Field-Mapping
-        if (isset($this->field_map[$key])) $row['map_'.$key]= $this->field_map[$key][$val];
-                
-        // Convert datetime, int and real
-        switch ($this->fields[$key]) {
-          case 'datetime': 
-            $row[$key]= &new Date($val); 
-            break;
-            
-          case 'bit':
-          case 'int': 
-            settype($row[$key], 'integer'); 
-            break;
-            
-          case 'real':
-            settype($row[$key], 'double'); 
-            break;
-        }
-      }
-      
-      // $this->cat->debug($row);
-      return $row;
-    }
-    
-    /**
-     * Datensätze als assoziativen Array holen
-     *
-     * @access  public
-     * @param	string sql Das SQL
-     * @return  array rows Folgende Form (bei Anzahl zurückgegebener Felder):
-     *          1) field[0].content => field[0].content
-     *          2) field[0].content => field[1].content
-     *          3) field[0].content => array(field[1].content, field[2].content, ...)
-     */   
-    function &select_ref() {
-      $args= func_get_args();
-      $query= $this->query('select '.preg_replace('/^[\s\t\r\n]*select/i', '', $this->_prepare($args)));
-      if($query) {
-        $result_set= array();
-        while($data= mysql_fetch_row($query)) {
-          switch (sizeof($data)) {
-            case 1: $result_set[$data[0]]= $data[0]; break;
-            case 2: $result_set[$data[0]]= $data[1]; break;
-            default: $result_set[$data[0]]= array_slice($data, 1);
-          }
-        }
-        $this->last_num_rows= mysql_num_rows($query);
-        return $result_set;
-      }
-      return $query;
-    }
-	 
 
     /**
-     * Select-Wrapper
+     * Prepare an SQL statement
      *
      * @access  public
-     * @param	string sql Das SQL (ohne select)
-     * @return  array Alle Rows
-     */   
-    function &select() {
+     * @param   mixed* args
+     * @return  string
+     */
+    function prepare() {
       $args= func_get_args();
-      $query= $this->query('select '.preg_replace('/^[\s\t\r\n]*select/i', '', $this->_prepare($args)));
-      if($query) {
-        $result_set= array();
-        while($result_set[]= $this->fetch($query)) {};
-        unset($result_set[sizeof($result_set)- 1]);
-        $this->last_num_rows= mysql_num_rows($query);
-        return $result_set;
-      }
-      return $query;
+      return $this->_prepare($args);    
     }
     
     /**
-     * Update-Wrapper
+     * Retreive identity
      *
      * @access  public
-     * @param	string sql Das SQL (ohne update)
-     * @return  bool Query-Ergebnis
-     */   
+     * @return  mixed identity value
+     */
+    function identity() { 
+      if (FALSE === ($r= &$this->query('@@identity as identity'))) {
+        return FALSE;
+      }
+      return $r->next('identity');
+    }
+
+    /**
+     * Execute an insert statement
+     *
+     * @access  public
+     * @param   mixed *args
+     * @return  int number of affected rows
+     * @throws  rdbms.SQLException
+     */
+    function insert() { 
+      $args= func_get_args();
+      $args[0]= 'insert '.$args[0];
+      if (FALSE === ($r= &call_user_func_array(array(&$this, 'query'), $args))) {
+        return FALSE;
+      }
+      
+      return mysql_affected_rows($this->handle);
+    }
+    
+    
+    /**
+     * Execute an update statement
+     *
+     * @access  public
+     * @param   mixed* args
+     * @return  int number of affected rows
+     * @throws  rdbms.SQLException
+     */
     function update() {
       $args= func_get_args();
-      $this->last_affected_rows= -1;
-      $result= $this->query('update '.preg_replace('/^[\s\t\r\n]*update/i', '', $this->_prepare($args)));
-      if($result) {
-        $this->last_affected_rows= mysql_affected_rows();
+      $args[0]= 'update '.$args[0];
+      if (FALSE === ($r= &call_user_func_array(array(&$this, 'query'), $args))) {
+        return FALSE;
       }
-      return $result;
+      
+      return mysql_affected_rows($this->handle);
     }
-
-    /**
-     * Insert-Wrapper
-     *
-     * @access  public
-     * @param	string sql Das SQL (ohne insert)
-     * @return  bool result Query-Ergebnis
-     */   
-    function insert() {
-      $args= func_get_args();
-      $this->last_insert_id= $this->last_affected_rows= -1;
-      $result= $this->query('insert '.preg_replace('/^[\s\t\r\n]*insert/i', '', $this->_prepare($args)));
-      if($result) {
-        $this->last_affected_rows= mysql_affected_rows();
-      }
-      return $result;
-    }
-	
-    /**
-     * Delete-Wrapper
-     *
-     * @access  public
-     * @param	string sql Das SQL (ohne delete)
-     * @return  bool result Query-Ergebnis
-     */   
-    function delete() {
-      $args= func_get_args();
-      $this->last_affected_rows= -1;
-      $result= $this->query('delete '.preg_replace('/^[\s\t\r\n]*delete/i', '', $this->_prepare($args)));
-      if($result) {
-        $this->last_affected_rows= mysql_affected_rows();
-      }
-      return $result;
-    }
-
-    /**
-     * Get the ID generated from the previous INSERT operation
-     *
-     * @access  public
-     * @return  int ID generated for an AUTO_INCREMENT column by the previous INSERT query 
-     */   
-    function insert_id() {
-      return mysql_insert_id($this->handle);
-    }
-
     
     /**
-     * Letzen Fehler zurückgeben
+     * Execute an update statement
      *
      * @access  public
-     * @return  int Error number
-     */      
-    function get_error() {
-      return mysql_errno();
-    }
-    
-        
-    /**
-     * Transaktion beginnen
-     *
-     * @access  public
-     * @param   string name default php_transaction Transaktions-Name
-     * @return  int Success
-     */   
-    function start_tran($name= "php_transaction") {
-      return throw(new Exception('Transactions not implemented in MySQL'));
-    }
- 
-    /**
-     * Transaktion committen
-     *
-     * @access  public
-     * @return  int Success
-     */      
-    function commit_tran() {
-      return throw(new Exception('Transactions not implemented in MySQL'));
-    }
-
-    /**
-     * Transaktion rollback'en
-     *
-     * @access  public
-     * @return  int Success
-     */          
-    function rollback_tran() {
-      return throw(new Exception('Transactions not implemented in MySQL'));
-    }
-
-    /**
-     * Destructor
+     * @param   mixed* args
+     * @return  int number of affected rows
+     * @throws  rdbms.SQLException
      */
-    function __destruct() {
-      if ($this->handle) mysql_close($this->handle);
-      parent::__destruct();
-    } 
+    function delete() { 
+      $args= func_get_args();
+      $args[0]= 'delete '.$args[0];
+      if (FALSE === ($r= &call_user_func_array(array(&$this, 'query'), $args))) {
+        return FALSE;
+      }
+      
+      return mysql_affected_rows($this->handle);
+    }
     
     /**
-     * Anzahl der vom letzten Select-Query betroffene Rows zurückgeben
+     * Execute a select statement and return all rows as an array
      *
      * @access  public
-     * @params  resource query Query-Handle
-     * @return  int Anzahl Rows
-     */      
-    function num_rows($query) {
-      return mysql_num_rows($query);
+     * @param   mixed* args
+     * @return  array rowsets
+     * @throws  rdbms.SQLException
+     */
+    function select() { 
+      $args= func_get_args();
+      if (FALSE === ($r= &call_user_func_array(array(&$this, 'query'), $args))) {
+        return FALSE;
+      }
+      
+      $rows= array();
+      while ($row= $r->next()) $rows[]= $row;
+      return $rows;
     }
     
     /**
-     * Gibt den Handle (resource id #x) der Funktion mysql_connect() zurück
+     * Execute any statement
      *
-     * @return  resource Datenbankhandle
+     * @access  public
+     * @param   mixed* args
+     * @return  &rdbms.sybase.MySQLResultSet or FALSE to indicate failure
+     * @throws  rdbms.SQLException
      */
-    function getHandle() {
-      return $this->handle;
+    function &query() { 
+      $args= func_get_args();
+      $sql= $this->_prepare($args);
+
+      if (!is_resource($this->handle)) {
+        if (!($this->flags & DB_AUTOCONNECT)) return throw(new SQLException('Not connected'));
+        if (!$this->connect()) return FALSE;
+      }
+      
+      if ($this->flags & DB_BUFFER_RESULTS) {
+        $result= mysql_unbuffered_query($sql, $this->handle, $this->flags & DB_STORE_RESULT);
+      } else {
+        $result= mysql_query($sql, $this->handle);
+      }
+
+      if (FALSE === $result) {
+        return throw(new SQLException('Statement failed', $sql));
+      } else {
+        return new MySQLResultSet($result);
+      }
     }
-    
-  } //end::class(MySQL)
+  }
 ?>
