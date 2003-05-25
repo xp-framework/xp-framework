747f379a256e10237f8349ff9a8c93b2407e9f32
thekid <timm.friebe@gmail.com>
2003-05-25 02:36:09 +0000

    - Add getters
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1388 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/rdbms/DSN.class.php
+++ skeleton/rdbms/DSN.class.php
@@ -27,5 +27,39 @@
       $this->parts['dsn']= $str;
       parent::__construct();
     }
+    
+    /**
+     * Retreive host
+     *
+     * @access  public
+     * @param   mixed default default NULL  
+     * @return  string host or default if none is set
+     */
+    function getHost($default= NULL) {
+      return isset($this->parts['host']) ? $this->parts['host'] : $default;
+    }
+
+    /**
+     * Retreive user
+     *
+     * @access  public
+     * @param   mixed default default NULL  
+     * @return  string user or default if none is set
+     */
+    function getUser($default= NULL) {
+      return isset($this->pass['user']) ? $this->pass['user'] : $default;
+    }
+
+    /**
+     * Retreive password
+     *
+     * @access  public
+     * @param   mixed default default NULL  
+     * @return  string password or default if none is set
+     */
+    function getPassword($default= NULL) {
+      return isset($this->pass['password']) ? $this->pass['password'] : $default;
+    }
+
   }
 ?>
