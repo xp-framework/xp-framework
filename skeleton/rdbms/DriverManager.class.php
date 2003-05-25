560be6bd726a0f5ee7163a149eafd42b986ff5dc
thekid <timm.friebe@gmail.com>
2003-05-25 18:06:14 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1411 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/rdbms/DriverManager.class.php
@@ -0,0 +1,77 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses('rdbms.DBConnection', 'rdbms.DriverNotSupportedException');
+
+  /**
+   * Manages database drivers
+   *
+   * @purpose  purpose
+   */
+  class DriverManager extends Object {
+    var
+      $drivers  = array();
+      
+    /**
+     * Gets an instance
+     *
+     * @model   static
+     * @access  public
+     * @return  &rdbms.DriverManager
+     */
+    function &getInstance() {
+      static $instance= NULL;
+      
+      if (!$instance) {
+        $instance= new DriverManager();
+
+        if (extension_loaded('sybase_ct')) {
+          $instance->drivers['sybase']= &XPClass::forName('rdbms.sybase.Sybase');
+        }
+        if (extension_loaded('mysql')) {
+          $instance->drivers['mysql']= &XPClass::forName('rdbms.mysql.MySQL');
+        }
+      }
+      return $instance;
+    }
+  
+    /**
+     * Register a driver
+     *
+     * @model   static
+     * @access  public
+     * @param   string name identifier
+     * @param   &lang.XPClass class
+     */
+    function register($name, &$class) {
+      $i= &DriverManager::getInstance();
+      $i->drivers[$name]= &$class;
+    }
+    
+    /**
+     * Get a connection by a DSN string
+     *
+     * @model   static
+     * @access  public
+     * @param   string str
+     * @return  &rdbms.DBConnection
+     * @throws  rdbms.DriverNotSupportedException
+     */
+    function &getConnection($str) {
+      $dsn= &new DSN($str);
+      $id= $dsn->getDriver();
+      $i= &DriverManager::getInstance();
+      
+      // Lookup driver by identifier
+      if (!isset($i->drivers[$id])) {
+        return throw(new DriverNotSupportedException('No driver registered for '.$id));
+      }
+      
+      return $i->drivers[$id]->newInstance($dsn);
+    }
+    
+  }
+?>
