9cdf7c130d8c25d54baeffc1ffdcca45982d7255
thekid <timm.friebe@gmail.com>
2003-05-30 00:27:59 +0000

    - Add method assertIn() - Add optional parameter "error" to all assert* metzhods
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1445 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/util/profiling/unittest/TestCase.class.php
+++ skeleton/util/profiling/unittest/TestCase.class.php
@@ -97,10 +97,11 @@
      *
      * @access  public
      * @param   mixed var
+     * @param   string error default 'notbool'
      * @return  bool
      */
-    function assertBoolean($var) {
-      return assert('$this->test(is_bool($var), "notbool", gettype($var))');
+    function assertBoolean($var, $error= 'notbool') {
+      return assert('$this->test(is_bool($var), $error, gettype($var))');
     }
     
     /**
@@ -108,10 +109,11 @@
      *
      * @access  public
      * @param   mixed var
+     * @param   string error default 'notfloat'
      * @return  bool
      */
-    function assertFloat($var) {
-      return assert('$this->test(is_float($var), "notfloat", gettype($var))');
+    function assertFloat($var, $error= 'notfloat') {
+      return assert('$this->test(is_float($var), $error, gettype($var))');
     }
     
     /**
@@ -119,10 +121,11 @@
      *
      * @access  public
      * @param   mixed var
+     * @param   string error default 'notinteger'
      * @return  bool
      */
-    function assertInteger($var) {
-      return assert('$this->test(is_int($var), "notint", gettype($var))');
+    function assertInteger($var, $error= 'notinteger') {
+      return assert('$this->test(is_int($var), $error, gettype($var))');
     }
 
     /**
@@ -130,10 +133,11 @@
      *
      * @access  public
      * @param   mixed var
+     * @param   string error default 'notstring'
      * @return  bool
      */
-    function assertString($var) {
-      return assert('$this->test(is_string($var, "notstring", gettype($var)))');
+    function assertString($var, $error= 'notstring') {
+      return assert('$this->test(is_string($var, $error, gettype($var)))');
     }
 
     /**
@@ -141,10 +145,11 @@
      *
      * @access  public
      * @param   mixed var
+     * @param   string error default 'notnull'
      * @return  bool
      */
-    function assertNull($var) {
-      return assert('$this->test(is_null($var), "notnull", gettype($var))');
+    function assertNull($var, $error= 'notnull') {
+      return assert('$this->test(is_null($var), $error, gettype($var))');
     }
     
     /**
@@ -152,10 +157,11 @@
      *
      * @access  public
      * @param   mixed var
+     * @param   string error default 'notarray'
      * @return  bool
      */
-    function assertArray($var) {
-      return assert('$this->test(is_array($var), "notarray", gettype($var))');
+    function assertArray($var, $error= 'notarray') {
+      return assert('$this->test(is_array($var), $error, gettype($var))');
     }
     
     /**
@@ -163,10 +169,11 @@
      *
      * @access  public
      * @param   mixed var
+     * @param   string error default 'notobject'
      * @return  bool
      */
-    function assertObject(&$var) {
-      return assert('$this->test(is_object($var), "notobject", gettype($var))');
+    function assertObject(&$var, $error= 'notobject') {
+      return assert('$this->test(is_object($var), $error, gettype($var))');
     }
     
     /**
@@ -175,10 +182,11 @@
      * @access  public
      * @param   mixed var
      * @return  bool
+     * @param   string error default 'notempty'
      * @see     php://empty
      */
-    function assertEmpty($var) {
-      assert('$this->test(empty($var), "notempty", $var)');
+    function assertEmpty($var, $error= 'notempty') {
+      assert('$this->test(empty($var), $error, $var)');
     }
 
     /**
@@ -187,10 +195,11 @@
      * @access  public
      * @param   mixed var
      * @return  bool
+     * @param   string error default 'empty'
      * @see     php://empty
      */
-    function assertNotEmpty($var) {
-      assert('$this->test(!empty($var), "empty", $var)');
+    function assertNotEmpty($var, $error= 'empty') {
+      assert('$this->test(!empty($var), $error, $var)');
     }
 
     /**
@@ -199,10 +208,11 @@
      * @access  public
      * @param   mixed a
      * @param   mixed b
+     * @param   string error default 'notequal'
      * @return  bool
      */
-    function assertEquals($a, $b) {
-      return assert('$this->test($a === $b, "notequal", array($a, $b))');
+    function assertEquals($a, $b, $error= 'notequal') {
+      return assert('$this->test($a === $b, $error, array($a, $b))');
     }
     
     /**
@@ -211,10 +221,11 @@
      * @access  public
      * @param   mixed a
      * @param   mixed b
+     * @param   string error default 'equal'
      * @return  bool
      */
-    function assertNotEquals($a, $b) {
-      return assert('$this->test($a !== $b, "equal", array($a, $b))');
+    function assertNotEquals($a, $b, $error= 'equal') {
+      return assert('$this->test($a !== $b, $error, array($a, $b))');
     }
 
     /**
@@ -222,11 +233,12 @@
      *
      * @access  public
      * @param   mixed var
+     * @param   string error default 'nottrue'
      * @return  bool
      */
-    function assertTrue($var) {
-      if ($r= $this->assertBoolean($var)) {
-        $r= assert('$this->test($var === TRUE, "nottrue", $var)');
+    function assertTrue($var, $error= 'nottrue') {
+      if ($r= $this->assertBoolean($var, $error)) {
+        $r= assert('$this->test($var === TRUE, $error, $var)');
       }
       return $r;
     }
@@ -236,11 +248,12 @@
      *
      * @access  public
      * @param   mixed var
+     * @param   string error default 'notfalse'
      * @return  bool
      */
-    function assertFalse($var) {
-      if ($r= $this->assertBoolean($var)) {
-        $r= assert('$this->test($var === FALSE, "notfalse", $var)');
+    function assertFalse($var, $error= 'notfalse') {
+      if ($r= $this->assertBoolean($var, $error)) {
+        $r= assert('$this->test($var === FALSE, $error, $var)');
       }
       return $r;
     }
@@ -251,11 +264,12 @@
      * @access  public
      * @param   mixed var
      * @param   string pattern
+     * @param   string error default 'nomatches'
      * @return  bool
      * @see     php://preg_match
      */
-    function assertMatches($var, $pattern) {
-      return assert('$this->test(preg_match($var, $pattern), "nomatches", array($var, $pattern))');
+    function assertMatches($var, $pattern, $error= 'nomatches') {
+      return assert('$this->test(preg_match($var, $pattern), $error, array($var, $pattern))');
     }
     
     /**
@@ -264,11 +278,12 @@
      * @access  public
      * @param   &lang.Object var
      * @param   string name
+     * @param   string error default 'notequal'
      * @return  bool
      */
-    function assertClass(&$var, $name) {
-      if ($r= $this->assertObject($var)) {
-        $r= assert('$this->test($var->getClassName() === $name, "notequal", $var->getClassName())');
+    function assertClass(&$var, $name, $error= 'notequal') {
+      if ($r= $this->assertObject($var, $error)) {
+        $r= assert('$this->test($var->getClassName() === $name, $error, $var->getClassName())');
       }
       return $r;
     }
@@ -279,14 +294,28 @@
      * @access  public
      * @param   &lang.Object var
      * @param   string name
+     * @param   string error default 'notsubclass'
      * @return  bool
      */
-    function assertSubclass(&$var, $name) {
-      if ($r= $this->assertObject($var)) {
+    function assertSubclass(&$var, $name, $error= 'notsubclass') {
+      if ($r= $this->assertObject($var, $error)) {
         $c= array_search($name, $GLOBALS['php_class_names']);
-        $r= assert('$this->test(is_a($var, $c), "notsubclass", $name)');
+        $r= assert('$this->test(is_a($var, $c), $error, $name)');
       }
       return $r;
+    }
+    
+    /**
+     * Assert that a value is contained in a list
+     *
+     * @access  public
+     * @param   mixed var
+     * @param   array list
+     * @param   string error default 'notinlist'
+     * @return  bool
+     */
+    function assertIn($var, $list, $error= 'notinlist') {
+      return assert('$this->test(in_array($var, $list, TRUE), $error, $list)');
     }
 
     /**
