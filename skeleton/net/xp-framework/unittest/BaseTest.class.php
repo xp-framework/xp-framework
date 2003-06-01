29d1867ebf3bc325cc5c67506e1320e61aab298a
thekid <timm.friebe@gmail.com>
2003-06-01 15:19:34 +0000

    - Add new test:   Test reflection API via lang.XPClass
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1458 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/net/xp-framework/unittest/BaseTest.class.php
+++ skeleton/net/xp-framework/unittest/BaseTest.class.php
@@ -26,5 +26,22 @@
       $this->assertEquals(get_magic_quotes_runtime(), 0, 'magicquotesruntime');
       $this->assertIn(version_compare(phpversion(), '4.2.0'), array(0, 1), 'phpversion');
     }
+    
+    /**
+     * Test reflection API via lang.XPClass
+     *
+     * @access  public
+     */
+    function testReflection() {
+      $class= &XPClass::forName('lang.Exception');
+      if (!$this->assertClass($class, 'lang.XPClass')) return;
+      $parent= &$class->getParentClass();
+      if (!$this->assertClass($parent, 'lang.XPClass')) return;
+      $e= &$class->newInstance(__CLASS__);
+      if (!$this->assertClass($e, 'lang.Exception')) return;
+      $this->assertEquals($e->message, __CLASS__);
+      $o= &$parent->newInstance();
+      if (!$this->assertClass($o, 'lang.Object')) return;
+    }
   }
 ?>
