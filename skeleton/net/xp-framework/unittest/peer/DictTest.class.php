9a0e72bc2a2a672ad177bb0c7a89e22c93a7f233
thekid <timm.friebe@gmail.com>
2003-06-03 22:29:00 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1533 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/net/xp-framework/unittest/peer/DictTest.class.php
@@ -0,0 +1,70 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+ 
+  uses(
+    'org.dict.DictClient', 
+    'util.profiling.unittest.TestCase'
+  );
+
+  /**
+   * Test DICT client
+   *
+   * @see      xp://org.dict.DictClient
+   * @purpose  Unit Test
+   */
+  class DictTest extends TestCase {
+    var
+      $dc      = NULL,
+      $server  = '',
+      $port    = 0;
+      
+    /**
+     * Constructor
+     *
+     * @access  publuc
+     * @param   string name
+     * @param   string uri
+     */
+    function __construct($name, $server, $port) {
+      $this->server= $server;
+      $this->port= $port;
+      parent::__construct($name);
+    }
+      
+    /**
+     * Setup function
+     *
+     * @access  public
+     */
+    function setUp() {
+      $this->dc= &new DictClient();
+      try(); {
+        $this->dc->connect($this->server, (int)$this->port);
+      } if (catch('Exception', $e)) {
+        return $e;
+      }
+    }
+    
+    /**
+     * Tear down this test case.
+     *
+     * @access  public
+     */
+    function tearDown() {
+      $this->dc->close();
+    }
+    
+    /**
+     * Test getting a definition
+     *
+     * @access  public
+     */
+    function testDefinition() {
+      $definition= $this->dc->getDefinition('XP', '*');
+      return $definition;
+    }
+  }
+?>
