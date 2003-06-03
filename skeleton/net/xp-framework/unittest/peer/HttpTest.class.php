929c46af0e2bc1e839b3b5181fb5f44fbcd4b092
thekid <timm.friebe@gmail.com>
2003-06-03 18:58:17 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1511 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/net/xp-framework/unittest/peer/HttpTest.class.php
@@ -0,0 +1,127 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+ 
+  uses(
+    'peer.http.HttpConnection', 
+    'util.profiling.unittest.TestCase'
+  );
+
+  /**
+   * Test HTTP API
+   *
+   * Needs a counter part, a PHP script running under a webserver.
+   * This script shall contain the following source code:
+   * <code>
+   *   $str= serialize(array(
+   *     'headers'   => getallheaders(),
+   *     'method'    => getenv('REQUEST_METHOD'),
+   *     'uri'       => getenv('REQUEST_URI'),
+   *     'request'   => $_REQUEST
+   *   ));
+   *   header('Content-type: application/x-php-serialized');
+   *   header('Content-length: '.strlen($str));
+   * 
+   *   echo $str;
+   * </code>
+   *
+   * @purpose  Unit Test
+   */
+  class HttpTest extends TestCase {
+    var
+      $conn = NULL,
+      $uri  = '';
+      
+    /**
+     * Constructor
+     *
+     * @access  publuc
+     * @param   string name
+     * @param   string uri
+     */
+    function __construct($name, $uri) {
+      $this->uri= $uri;
+      parent::__construct($name);
+    }
+      
+    /**
+     * Setup function
+     *
+     * @access  public
+     * @throws  rdbms.DriverNotSupportedException
+     */
+    function setUp() {
+      $this->conn= &new HttpConnection($this->uri);
+    }
+    
+    /**
+     * Private helper method
+     *
+     * @access  private
+     * @param   string method
+     * @param   bool expectingData
+     * @return  array data
+     */
+    function _test($method, $expectingData) {
+      try(); {
+        $response= &$this->conn->request($method, array(
+          'a'   => 'b',
+          'b'   => 'c'
+        ));
+      } if (catch('Exception', $e)) {
+        return $this->fail($e->getClassName(), $e->getStackTrace(), 'HttpConnection::get');
+      }
+      if (!$this->assertSubclass($response, 'peer.http.HttpResponse')) return;
+
+      // Check headers
+      $length= $response->getHeader('Content-length');
+      $this->assertNotEmpty($length, 'contentlength.missing');
+      $ctype= $response->getHeader('Content-type');
+      $this->assertNotEmpty($length, 'contenttype.missing');
+      
+      $data= NULL;
+      if ($expectingData) {
+        $buf= $response->readData($length, TRUE);
+        $this->assertEquals(strlen($buf), (int)$length, 'readdata');
+        $data= unserialize($buf);
+        $this->assertArray($data, 'data.corrupt');
+
+        // Check return
+        $this->assertArray($data['headers'], 'requestheaders.missing');
+        $this->assertEquals($data['method'], $method, 'requestmethod');
+        $this->assertArray($data['request'], 'querymissing');
+      }
+      
+      return array($response->getHeaders(), $data);
+    }
+    
+    /**
+     * Test get method
+     *
+     * @access  public
+     */
+    function testGet() {
+      return $this->_test(HTTP_GET, TRUE);
+    }
+
+    /**
+     * Test post method
+     *
+     * @access  public
+     */
+    function testPost() {
+      return $this->_test(HTTP_POST, TRUE);
+    }
+
+    /**
+     * Test head method
+     *
+     * @access  public
+     */
+    function testHead() {
+      return $this->_test(HTTP_HEAD, FALSE);
+    }
+  }
+?>
