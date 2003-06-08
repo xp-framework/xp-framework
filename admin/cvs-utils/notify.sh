e99712dede2ad3644e8ae06679910ec1e5db70e6
thekid <timm.friebe@gmail.com>
2003-06-08 16:35:02 +0000

    - Make log message reappear, hopefully
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1575 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- admin/cvs-utils/notify.sh
+++ admin/cvs-utils/notify.sh
@@ -6,6 +6,5 @@
 # $Id$
 
 FILE="/tmp/loginfo_`md5 -q -s "$1"`"
-read MESSAGE
-echo ${MESSAGE} > ${FILE}
+cat > ${FILE}
 php -q `dirname $0`/xp_notify.php ${FILE} $2 "$1" 1>/tmp/cvs_notify.log 2>/tmp/cvs_notify.err &
