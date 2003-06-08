f29cbe449a3f514d0cfddd6818293982e5c17d37
thekid <timm.friebe@gmail.com>
2003-06-08 16:20:31 +0000

    - Test
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1568 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- admin/cvs-utils/notify.sh
+++ admin/cvs-utils/notify.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-FILE="/tmp/loginfo_`md5 -q -s $1`"
-echo $FILE
-exit 1
+FILE="/tmp/loginfo_`md5 -q -s "$1"`"
 read MESSAGE
 echo ${MESSAGE} > ${FILE}
 php -q `dirname $0`/xp_notify.php ${FILE} $2
