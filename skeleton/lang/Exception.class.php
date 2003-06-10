61782285a4ef9a6ccc139961e9a01cc999d9fbc6
thekid <timm.friebe@gmail.com>
2003-06-10 18:42:32 +0000

    - Add some links on exceptions
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1616 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/lang/Exception.class.php
+++ skeleton/lang/Exception.class.php
@@ -9,6 +9,9 @@
    *
    * @purpose  Base class for all other exceptions
    * @see      http://java.sun.com/docs/books/tutorial/essential/exceptions/definition.html
+   * @see      http://jinx.swiki.net/352
+   * @see      http://www.artima.com/designtechniques/exceptions.html
+   * @see      http://www.artima.com/designtechniques/desexcept.html
    */
   class Exception extends Object {
     var 
