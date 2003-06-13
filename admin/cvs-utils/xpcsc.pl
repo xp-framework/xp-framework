97afc10ea93d81b7d6349152637b5464280a937a
thekid <timm.friebe@gmail.com>
2003-06-13 11:16:36 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1624 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ admin/cvs-utils/xpcsc.pl
@@ -0,0 +1,96 @@
+#!/usr/bin/perl
+
+##
+# Checks XP classes for conformance to coding standards
+#
+# $Id$
+
+use constant ETAB       => "ETAB";
+use constant EINDENT    => "EINDENT";
+use constant ECOMMENT   => "ECOMMENT";
+use constant ENOHEADER  => "ENOHEADER";
+use constant ESHORTOPEN => "ESHORTOPEN";
+
+use constant WTBD       => "WTBD";
+use constant WOUTPUT    => "WOUTPUT";
+
+%LINK = (
+  # Errors
+  ETAB        => "http://xp-framework.net/content/about.coding.html#5",
+  EINDENT     => "http://xp-framework.net/content/about.coding.html#5",
+  ECOMMENT    => "http://xp-framework.net/content/about.coding.html#9",
+  ENOHEADER   => "http://xp-framework.net/content/about.coding.html#2",
+  ESHORTOPEN  => "http://xp-framework.net/content/about.coding.html#3",
+
+  # Warnings
+  WTBD        => "http://xp-framework.net/content/about.coding.html#13",
+  WOUTPUT     => "n/a"
+);
+
+# {{{ void error (string message, string code)
+sub error() {
+  my $message= shift;
+  my $code= shift;
+  
+  $_ =~ s/\t/\\t/g;
+  chomp $_;
+  print "*** Error: ".$message." at line ".$l." of ".$ARGV[0]."\n    ".$_."\n---> [".$code."] ".$LINK{$code}."\n";
+  exit 32;
+}
+# }}}
+
+# {{{ void warning (string message, string code)
+sub warning() {
+  my $message= shift;
+  my $code= shift;
+  
+  $_ =~ s/\t/\\t/g;
+  chomp $_;
+  print "--- Warning: ".$message." at line ".$l." of ".$ARGV[0]."\n    ".$_."\n---> [".$code."] ".$LINK{$code}."\n";
+  $warnings++;
+}
+# }}}
+
+# {{{ main
+$warnings= 0;
+
+open(FILE, $ARGV[0]) || die "Cannot open $ARGV[0]";
+$l= 0;
+while (<FILE>) {
+  $l++;
+  
+  SWITCH: {
+    if (1 == $l && $_ !~ /^\<\?php/) { &error("First line does not contain <?php", ESHORTOPEN); }
+    if (2 == $l && $_ !~ /^\/\*/) { &error("Second line does not contain XP header", ENOHEADER); }
+    if (4 == $l && $_ !~ /\$Id/) { &error("Second line does not contain CVS Id-Tag", ENOHEADER); }
+    if ($l < 5) { next; }
+  }
+  
+  if ($_ =~ /\t/) {
+    &error("Tab character found", ETAB);
+  }
+  
+  if ($_ =~ /^(\s+)class/ && 2 != length($1)) {
+    &error("Class declarations must be indented with 2 spaces", EINDENT);
+  }
+  
+  if ($_ =~ /^(\s+)function/ && 4 != length($1)) {
+    &error("Methods must be indented with 4 spaces", EINDENT);
+  }
+  
+  if ($_ =~ /\/\*[^\*]/ && $l > 2) {
+    &error("Inline comments may not be contained within source, use // instead", ECOMMENT);
+  }
+  
+  if ($_ =~ /(echo|var_dump|print_r)/) {
+    &warning("You should not be using direct output statements ($1)", WOUTPUT);
+  }
+  
+  if ($_ =~ /(TODO|TBI|TBD|FIXME)/) {
+    &warning("You have a $1 comment in your sourcecode...", WTBD);
+  }
+}
+close FILE;
+
+print $ARGV[0]." coding standards conformance ok [".$warnings." warning(s)]\n";
+# }}}
