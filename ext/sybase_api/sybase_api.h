f851d298177720c660da6a397f2992b14c9b4ef3
thekid <timm.friebe@gmail.com>
2003-07-26 18:13:17 +0000

    - Add sybase_query - Add sybase_cancel - Add sybase_results - Add sybase_nameoftype - Add sybase_nameofcode - Add struct for sybase results
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1956 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ext/sybase_api/sybase_api.h
+++ ext/sybase_api/sybase_api.h
@@ -24,6 +24,12 @@ typedef struct {
     CS_CONNECTION   *connection;
 } sybase_link;
 
+typedef struct {
+    CS_COMMAND *cmd;
+    CS_INT type;
+    CS_INT code;
+} sybase_result;
+
 SYBASE_API int sybase_init(sybase_environment **env);
 SYBASE_API int sybase_set_messagehandler(sybase_environment *env, int type, CS_VOID *handler);
 SYBASE_API int sybase_shutdown(sybase_environment *env);
@@ -31,5 +37,10 @@ SYBASE_API int sybase_alloc(sybase_link **link);
 SYBASE_API int sybase_connect(sybase_environment *env, sybase_link *link, char *host, char *user, char *pass);
 SYBASE_API CS_INT sybase_connection_status(sybase_link *link);
 SYBASE_API int sybase_is_connected(sybase_link *link);
+SYBASE_API int sybase_query(sybase_link *link, sybase_result **result, char *query);
+SYBASE_API int sybase_cancel(sybase_result *result, int mode);
+SYBASE_API int sybase_results(sybase_result **result);
 SYBASE_API int sybase_close(sybase_link *link);
 SYBASE_API int sybase_free(sybase_link *link);
+SYBASE_API char *sybase_nameoftype(CS_INT type);
+SYBASE_API char *sybase_nameofcode(CS_INT code);
