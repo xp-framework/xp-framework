a759528e5224becd54f5538dffbd0a838508d7e4
thekid <timm.friebe@gmail.com>
2003-07-27 12:23:18 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1987 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/people/friebe/c/hash.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+typedef struct {
+    int key;
+    char *value;
+} hash;
+
+static void printhash(char *name, hash *h)
+{
+    printf("%s = {\n", name);
+    while (h->key != NULL) {
+        printf("  %d: %s\n", h->key, h->value);
+        h++;
+    }
+    printf("}\n");
+}
+
+int main(int argc, char **argv)
+{
+    hash a[] = {
+        { 1, "Hello" },
+        { 2, "World" },
+        { NULL, NULL }
+    };
+    hash b[2];
+
+    printhash("a", a);
+    
+    b[0].key= 1;
+    b[0].value= "Dynamic";
+    b[1].key= NULL;
+    b[1].value= NULL;
+    printhash("b", b);
+    
+    return 0;
+}
