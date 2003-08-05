07893e8caa1b5d2faf3bfe14060c2fd43c8a3695
kiesel <alex@kiesel.name>
2003-08-05 05:44:08 +0000

    - fix indenting
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2036 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/people/kiesel/cstaproxy/csta_error.h
+++ experiments/people/kiesel/cstaproxy/csta_error.h
@@ -1,5 +1,5 @@
 extern void print_error (char*, int, char*, ...);
 extern void log (char*, int, char*, ...);
 
-#define ERR(s)		print_error (__FILE__, __LINE__, s)
-#define LOG(s)          log (__FILE__, __LINE__, s)
+#define ERR(s)			print_error (__FILE__, __LINE__, s)
+#define LOG(s)			log (__FILE__, __LINE__, s)
