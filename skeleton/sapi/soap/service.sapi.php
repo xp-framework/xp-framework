644cf6ea99cb54cc3056a7395ef84e3c4ab030d8
thekid <timm.friebe@gmail.com>
2003-08-26 10:24:54 +0000

    - Configuration directive "display_errors" must be on for fatal error to be able to work - Add code parameter to fault()   Example XML representation: <faultcode xsi:type="xsd:string">xp.internalerror</faultcode>
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2124 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/sapi/soap/service.sapi.php
+++ skeleton/sapi/soap/service.sapi.php
@@ -11,9 +11,9 @@
   // {{{ final class sapi新oap新ervice
   class sapi新oap新ervice {
   
-    // {{{ internal string fault(&lang.Throwable exception)
+    // {{{ internal string fault(&lang.Throwable exception, string code)
     //     Convert an exception to XML
-    function fault(&$exception) {
+    function fault(&$exception, $code) {
       $answer= &new SOAPMessage();
       $answer->create('Server', 'Error');
 
@@ -22,7 +22,7 @@
       }
 
       $answer->setFault(
-        HTTP_INTERNAL_SERVER_ERROR,
+        $code,
         $exception->getMessage(),
         getenv('SERVER_NAME').':'.getenv('SERVER_PORT'),
         $stacktrace
@@ -38,15 +38,21 @@
     // {{{ internal string output(string buf)
     //     Output handler
     function output($buf) {
-      
+
       // Check for fatal errors
       if (FALSE !== ($p= strpos($buf, EPREPEND_IDENTIFIER))) {
-        return sapi新oap新ervice::fault(new Error(str_replace(EPREPEND_IDENTIFIER, '', substr($buf, $p))));
+        return sapi新oap新ervice::fault(
+          new Error(str_replace(EPREPEND_IDENTIFIER, '', substr($buf, $p))),
+          'xp.internalerror'
+        );
       }
 
       // Check for uncaught exceptions
       if ($exceptions= &xp::registry('exceptions')) {
-        return sapi新oap新ervice::fault($exceptions[key($exceptions)]);
+        return sapi新oap新ervice::fault(
+          $exceptions[key($exceptions)],
+          'xp.uncaughtexception'
+        );
       }
 
       return $buf;
@@ -57,7 +63,8 @@
   // }}}
   
   ini_set('html_errors', 0);
-  ini_set('display_errors', 0);
-    
+  ini_set('display_errors', 1);
+  ini_set('error_prepend_string', EPREPEND_IDENTIFIER);
+
   ob_start(array('sapi新oap新ervice', 'output'));
 ?>
