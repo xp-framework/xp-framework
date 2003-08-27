3ba69c348ab9755de31113a8631bf3b4982f8b10
thekid <timm.friebe@gmail.com>
2003-08-27 09:30:26 +0000

    - Add accessors
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2134 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/xml/soap/SOAPFault.class.php
+++ skeleton/xml/soap/SOAPFault.class.php
@@ -15,7 +15,7 @@
       $faultstring  = '', 
       $faultactor   = NULL,
       $detail       = NULL;
-    
+
     /**
      * Fill in this fault with data
      *
@@ -35,6 +35,86 @@
       $this->faultstring= $faultstring;
       $this->faultactor= $faultactor;
       $this->detail= $detail;
+    }
+    
+    /**
+     * Set Faultcode
+     *
+     * @access  public
+     * @param   string faultcode
+     */
+    function setFaultcode($faultcode) {
+      $this->faultcode= $faultcode;
+    }
+
+    /**
+     * Get Faultcode
+     *
+     * @access  public
+     * @return  string
+     */
+    function getFaultcode() {
+      return $this->faultcode;
+    }
+
+    /**
+     * Set Faultstring
+     *
+     * @access  public
+     * @param   string faultstring
+     */
+    function setFaultstring($faultstring) {
+      $this->faultstring= $faultstring;
+    }
+
+    /**
+     * Get Faultstring
+     *
+     * @access  public
+     * @return  string
+     */
+    function getFaultstring() {
+      return $this->faultstring;
+    }
+
+    /**
+     * Set Faultactor
+     *
+     * @access  public
+     * @param   string faultactor
+     */
+    function setFaultactor($faultactor) {
+      $this->faultactor= $faultactor;
+    }
+
+    /**
+     * Get Faultactor
+     *
+     * @access  public
+     * @return  string
+     */
+    function getFaultactor() {
+      return $this->faultactor;
+    }
+
+    /**
+     * Set Detail
+     *
+     * @access  public
+     * @param   mixed detail
+     */
+    function setDetail($detail) {
+      $this->detail= $detail;
+    }
+
+    /**
+     * Get Detail
+     *
+     * @access  public
+     * @return  mixed
+     */
+    function getDetail() {
+      return $this->detail;
     }
   }
 ?>
