3aa289f5c88b413997ea1fe570624a7221ef43db
thekid <timm.friebe@gmail.com>
2003-08-28 14:04:09 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2143 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/sapi/compat/version4_1.sapi.php
@@ -0,0 +1,47 @@
+<?php
+/* This file provides the CLI sapi for the XP framework
+ * 
+ * $Id$
+ */
+
+  // {{{ proto bool is_a (&object object, string class_name) 
+  //     See php://is_a
+  if (!function_exists('is_a')) { function is_a(&$object, $name) {
+    return (
+      (get_class($object) == strtolower($name)) ||
+      (is_subclass_of($object, $name))
+    );
+  }}
+  // }}}
+  
+  // {{{ proto mixed var_export (&mixed data [, bool return= 0])
+  //     See php://var_export
+  if (!function_exists('var_export')) { function var_export(&$data, $return= 0) {
+    ob_start();
+    var_dump($data);
+    $dump= ob_get_contents();
+    ob_end_clean();
+    if ($return) return $dump;
+    
+    echo $dump;
+  }}
+  // }}}
+  
+  // {{{ proto array array_change_key_case(array a, int case) 
+  //     See php://array_change_key_case
+  if (!function_exists('array_change_key_case')) { 
+    define('CASE_LOWER', 0);
+    define('CASE_UPPER', 1);
+
+    function array_change_key_case($a, $case) {
+      $r= array();
+      switch ($case) {
+        case CASE_LOWER: foreach (array_keys($a) as $k) $r[strtolower($k)]= &$a[$k]; break;
+        case CASE_UPPER: foreach (array_keys($a) as $k) $r[strtoupper($k)]= &$a[$k]; break;
+        default: $r= FALSE;
+      }
+      return $r;
+    }
+  }
+  // }}}
+?>
