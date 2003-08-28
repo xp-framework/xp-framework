84186106c0ab0e9fee060ff8bb373c946a151eb9
thekid <timm.friebe@gmail.com>
2003-08-28 14:07:07 +0000

    - Add sybase_fetch_assoc
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2144 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/sapi/compat/version4_1.sapi.php
+++ skeleton/sapi/compat/version4_1.sapi.php
@@ -44,4 +44,14 @@
     }
   }
   // }}}
+  
+  // {{{ proto array sybase_fetch_assoc(resource result)
+  //     See php://sybase_fetch_assoc
+  if (!function_exists('sybase_fetch_assoc')) { function sybase_fetch_assoc($res) {
+    if (is_array($r= sybase_fetch_array($res))) foreach (array_keys($r) as $k) {
+      if (is_int($r[$k])) unset($r[$k]);
+    }
+    return $r;
+  }}
+  // }}}
 ?>
