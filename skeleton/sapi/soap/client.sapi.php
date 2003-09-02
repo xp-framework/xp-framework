4f0ef6d888b998e9127e6fb79cc96538daaf97ed
thekid <timm.friebe@gmail.com>
2003-09-02 16:00:04 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2168 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/sapi/soap/client.sapi.php
@@ -0,0 +1,11 @@
+<?php
+/* This file provides the SOAP client sapi for the XP framework
+ * 
+ * $Id$
+ */
+
+  uses(
+    'xml.soap.SOAPClient',
+    'xml.soap.transport.SOAPHTTPTransport'
+  );
+?>
