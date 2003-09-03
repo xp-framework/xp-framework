5fb7b636531022e3a7c19ac2f647d589b82d11ab
thekid <timm.friebe@gmail.com>
2003-09-03 12:18:23 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2172 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/util/log/BufferedAppender.class.php
@@ -0,0 +1,41 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('util.log.LogAppender');
+
+  /**
+   * Appender which appends all data to an appender
+   *
+   * @see   xp://util.log.LogAppender
+   */  
+  class BufferedAppender extends LogAppender {
+    var 
+      $buffer = '';
+
+    /**
+     * Appends log data to the buffer
+     *
+     * @access public
+     * @param  mixed args variables
+     */
+    function append() {
+      foreach (func_get_args() as $arg) {
+        $buffer.= $this->varSource($arg).' ';
+      }
+      $buffer.= "\n";
+    }
+    
+    /**
+     * Get Buffer
+     *
+     * @access  public
+     * @return  string
+     */
+    function getBuffer() {
+      return $this->buffer;
+    }
+  }
+?>
