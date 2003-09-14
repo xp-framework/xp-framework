c9c05197ab851b5ee89646b81f218e31cbbda121
thekid <timm.friebe@gmail.com>
2003-09-14 16:18:06 +0000

    - Add API documentation - Fix list() for when filename should contain blanks - WS fixes - Remove commented out and therefore never executed folder->open() - Actually change directory back to old directory - Add reference operator (&) to avoid unintentional copies - Use DIRECTORY_SEPARATOR instead of hardcoded "/"
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2245 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/org/cvshome/CVSDirectory.class.php
+++ skeleton/org/cvshome/CVSDirectory.class.php
@@ -4,28 +4,54 @@
  * $Id$
  */
 
+  /**
+   * CVS directory
+   *
+   * @purpose  Interface to CVS binary
+   */
   class CVSDirectory extends CVSInterface {
     var
-      $path= NULL,
-      $_folder= NULL;
+      $path     = NULL,
+      $_folder  = NULL;
     
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   string path
+     */
     function __construct($path) {
       $this->path= $path;
       $this->_folder= &new Folder ($path);
-      
-      try(); {
-        // $this->_folder->open();
-      } if (catch ('IOException', $e)) {
-        return throw ($e);
-      }
     }
     
+    /**
+     * Protected helper method that executes a CVS command, changing
+     * to the desired directory before doing so and changing it back
+     * after finishing.
+     *
+     * @access  protected
+     * @param   string cvsCmd
+     * @return  string[]
+     */
     function _execute($cvsCmd) {
-      $olddir= getcwd(); chdir ($this->path);
-      return parent::_execute ($cvsCmd);
-      chdor ($olddir);
+      $olddir= getcwd(); 
+      chdir($this->path);
+      $r= parent::_execute ($cvsCmd);
+      chdir($olddir);
+      
+      return $r;
     }
     
+    /**
+     * Update a directory
+     *
+     * @access  public
+     * @param   bool recursive default FALSE
+     * @param   bool sim default FALSE whether to simulate
+     * @return  stdclass[] objects
+     * @throws  org.cvshome.CVSInterfaceException
+     */
     function update($recursive= FALSE, $sim= TRUE) {
       try(); {
         $results= $this->_execute (sprintf ('%s update %s',
@@ -46,12 +72,12 @@
           continue;
         }
     
-        list ($state, $filename)= explode (' ', $r);
+        list ($state, $filename)= explode (' ', $r, 2);
         if (FALSE !== ($s= $this->getCVSStatus ($state))) {
-          $f= new StdClass();
+          $f= &new stdClass();
           $f->status= $s;
           $f->filename= $filename;
-          $f->uri= $this->path.'/'.$filename;
+          $f->uri= $this->path.DIRECTORY_SEPARATOR.$filename;
           $stats[]= $f;
         } else {
           // We could not identify this status, so ignore this file
@@ -60,8 +86,6 @@
       }
       
       return $stats;
-    }
-    
-  }
-  
+    } 
+  }  
 ?>
