7df2e351f611494a6864d0e24ddcb9797f85f614
thekid <timm.friebe@gmail.com>
2003-09-14 17:34:50 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2275 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/xml/XMLFormatException.class.php
@@ -0,0 +1,16 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Indicates the XML cannot be parsed (i.e., it is not well-
+   * formed).
+   *
+   * @purpose  Exception
+   */
+  class XMLFormatException extends FormatException {
+  
+  }
+?>
