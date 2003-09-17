06b0e87a30bc9c965358e7fe5b7136d194fbc339
thekid <timm.friebe@gmail.com>
2003-09-17 11:10:55 +0000

    - Add member variable errors - Add accessor for new member variable - Override Exception::toString
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2302 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/security/cert/CertificateException.class.php
+++ skeleton/security/cert/CertificateException.class.php
@@ -11,6 +11,39 @@
    * @purpose  This exception indicates one of a variety of certificate problems.
    */
   class CertificateException extends Exception {
+    var
+      $errors = array();
+      
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   string message
+     * @param   string[] errors default array()
+     */
+    function __construct($message, $errors= array()) {
+      parent::__construct($message);
+      $this->errors= $errors;
+    }
   
+    /**
+     * Returns errors
+     *
+     * @access  public
+     * @return  string[] errors
+     */
+    function getErrors() {
+      return $this->errors;
+    }
+    
+    /**
+     * Return formatted output of stacktrace
+     *
+     * @access  public
+     * @return  string
+     */
+    function toString() {
+      return parent::toString()."\n".implode("\n  @", $this->errors)."\n";
+    }
   }
 ?>
