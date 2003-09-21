943def40bc9ef0ec2795fc8e7a38742e63aefe5f
thekid <timm.friebe@gmail.com>
2003-09-21 11:58:37 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2322 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/com/microsoft/RegistryException.class.php
@@ -0,0 +1,15 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Indicates an error occured when talking to the Windows registry
+   *
+   * @purpose  Exception
+   */
+  class RegistryException extends Exception {
+  
+  }
+?>
