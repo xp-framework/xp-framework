e47a8dc81190773d8cfc52dd56ba123ac38d4d46
thekid <timm.friebe@gmail.com>
2003-09-26 13:45:00 +0000

    - Add class api doc with examples
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2375 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/xml/CData.class.php
+++ skeleton/xml/CData.class.php
@@ -4,6 +4,24 @@
  * $Id$
  */
 
+  /**
+   * CData allows to insert a CDATA section:
+   *
+   * Example:
+   * <code>
+   *   $tree= &new Tree();
+   *   $tree->addChild(new Node('data', new CData('<Hello World>')));
+   * </code>
+   *
+   * The output will then be:
+   * <pre>
+   *   <document>
+   *     <data><![CDATA[<Hello World>]]></data>
+   *   </document>
+   * </pre>
+   *
+   * @purpose  Wrapper
+   */
   class CData extends Object {
     var
       $cdata= '';
--- skeleton/xml/PCData.class.php
+++ skeleton/xml/PCData.class.php
@@ -4,6 +4,28 @@
  * $Id$
  */
 
+  /**
+   * PCData allows to insert literal XML into a nodes contents.
+   *
+   * Example:
+   * <code>
+   *   $tree= &new Tree();
+   *   $tree->addChild(new Node('data', new PCData('Hello<br/>World')));
+   * </code>
+   *
+   * The output will then be:
+   * <pre>
+   *   <document>
+   *     <data>Hello<br/>World</data>
+   *   </document>
+   * </pre>
+   *
+   * Note: The XML passed to PCDatas constructor is not validated!
+   * Passing incorrect XML to this class will result in a not-
+   * wellformed output document.
+   *
+   * @purpose  Wrapper
+   */
   class PCData extends Object {
     var
       $pcdata= '';
