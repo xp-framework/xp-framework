4446f521e437835e6e51cf7884e6afaff03f95c5
thekid <timm.friebe@gmail.com>
2003-10-05 16:18:52 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2448 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/people/friebe/req/getrequirements.php
@@ -0,0 +1,129 @@
+<?php
+  require('lang.base.php');
+  xp::sapi('cli');
+  uses('text.PHPParser', 'lang.ElementNotFoundException');
+  
+  // {{{ public class FullName
+  class FullName extends Object {
+
+    // {{{ public static string searchFor(string filename) throws ElementNotFoundException
+    function searchFor($filename) {
+      static $includepaths= NULL;
+      
+      if (NULL === $includepaths) {
+        $includepaths= explode(PATH_SEPARATOR, ini_get('include_path'));
+      }
+      foreach ($includepaths as $path) {
+        $fn= $path.DIRECTORY_SEPARATOR.$filename;
+        if (!file_exists($fn)) continue;
+        return realpath($fn);
+      }
+      return throw(new ElementNotFoundException('Could not find file "'.$filename.'"'));
+    }
+    // }}}
+    
+    // {{{ public static string forFile(string filename)
+    function forFile($filename) {
+      if (strstr($filename, DIRECTORY_SEPARATOR)) {     // Already fully qualified
+        return $filename;
+      }
+      return FullName::searchFor($filename);
+    }
+    // }}}
+    
+    // {{{ public static string forUses(string uses)
+    function forUses($uses) {
+      return FullName::searchFor(
+        strtr($uses, '.', DIRECTORY_SEPARATOR).'.class.php'
+      );
+    }
+    // }}}
+    
+    // {{{ public static string forSapi(string sapi)
+    function forSapi($sapi) {
+      return FullName::searchFor(
+        'sapi'.DIRECTORY_SEPARATOR.strtr($sapi, '.', DIRECTORY_SEPARATOR).'.sapi.php'
+      );
+    }
+    // }}}
+
+  }
+  // }}}
+  
+  // {{{ public class Requirements
+  class Requirements extends Object {
+    var
+      $files= array();
+      
+    // {{{ public string add(string filename)
+    function add($fullname) {
+      try(); {
+        $r= &Requirements::forFile($fullname);
+      } if (catch('Exception', $e)) {
+        return throw($e);
+      }
+
+      $this->files[]= $fullname;
+      $this->files= array_unique(array_merge($this->files, $r->files));
+      return $fullname;
+    }
+    // }}}
+
+    // {{{ public static Requirements forFile(string filename)    
+    function forFile($filename) {
+      if (is('null', $filename)) return;    // NullPointer
+
+      $requirements= &new Requirements();
+      $idx= basename($filename);
+      try(); {
+        $parser= &new PHPParser($filename);
+        $parser->parse();
+
+        defined('VERBOSE') && Console::writeLine($parser->toString());
+
+        do {
+          // First, go through the requirements
+          foreach ($parser->requires as $required) {
+            if (!($fn= $requirements->add(FullName::forFile($required)))) break 2;
+            Console::writeLine('+ ', $idx, ' requires file ', $required, ' (', $fn, ')');
+          }
+
+          // Second, the SAPIs
+          foreach ($parser->sapis as $sapi) {
+            if (!($fn= $requirements->add(FullName::forSapi($sapi)))) break 2;
+            Console::writeLine('+ ', $idx, ' has SAPI ', $sapi, ' (', $fn, ')');
+          }
+
+          // Then check all of the classes in uses()
+          foreach ($parser->uses as $uses) {
+            if (!($fn= $requirements->add(FullName::forUses($uses)))) break 2;
+            Console::writeLine('+ ', $idx, ' uses class ', $uses, ' (', $fn, ')');
+          }
+
+          break;
+        } while (1);
+      } if (catch('Exception', $e)) {
+        return throw($e);
+      }
+
+      return $requirements;
+    }
+    // }}}
+
+  }
+  // }}}
+  
+  // {{{ main
+  $param= &new ParamString();
+  if ($param->exists('verbose')) define('VERBOSE', 1);
+  
+  try(); {
+    $requirements= &Requirements::forFile($param->value(1));
+  } if (catch('Exception', $e)) {
+    $e->printStackTrace();
+    exit(-1);
+  }
+  
+  Console::writeLine($requirements->toString());
+  // }}}
+?>
