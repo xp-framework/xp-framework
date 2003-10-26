f7b5b72d5cc77e98b83e31ee0d038e98f011abdc
thekid <timm.friebe@gmail.com>
2003-10-26 13:13:09 +0000

    - STDIN, STDOUT and STDERR are predefined in 4.3.x   For 4.2.x, the compat SAPI takes care of this
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2602 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/io/sys/StdStream.class.php
+++ skeleton/io/sys/StdStream.class.php
@@ -6,11 +6,6 @@
 
   uses('io.File');
 
-  // Define default filehandles
-  if (!defined('STDIN'))  define('STDIN',  fopen('php://stdin',  'r'));          
-  if (!defined('STDOUT')) define('STDOUT', fopen('php://stdout', 'w'));          
-  if (!defined('STDERR')) define('STDERR', fopen('php://stderr', 'w'));         
-  
   /**
    * Standard I/O streams
    *
