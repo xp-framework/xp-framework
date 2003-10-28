19f7f6df7abfacf142e111db932f8b12cd0bbbdc
kiesel <alex@kiesel.name>
2003-10-28 12:20:48 +0000

    - refactor class; fix it in respect to current overloading usage   . added __get()   . added __set()   . added getCalledFunction()   . added getTiming()   . added toString()   . removed getProfile() # Warning: because of the removal of getProfile() this chance is # a BC-breaker. I think this class is not widely used, so this # should not be a big problem.
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2619 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/util/profiling/ClassProfiler.class.php
+++ skeleton/util/profiling/ClassProfiler.class.php
@@ -27,7 +27,6 @@
      */
     function __construct() {
       $this->_profilee= NULL;
-      overload ('ClassProfiler');
     }
 
     /**
@@ -37,7 +36,7 @@
      * @param   &Object obj
      */    
     function attachProfilee(&$obj) {
-      $this->_profilee= &$obj;
+      $this->_profilee= $obj;
     }
 
     /**
@@ -67,27 +66,104 @@
 
       return TRUE;
     }
-
+    
     /**
-     * Retrieve the profiling results for a given function, or for all
-     * if no function is given.
+     * Get a property
      *
      * @access  public
-     * @param   string method default NULL
-     * @return  mixed results
-     */    
-    function getProfile($method= NULL) {
-      if (NULL === $method)
-        return array (
-          'timer' => $this->timer,
-          'calls' => $this->calls
-        );
-      
-      return array (
-        'timer' => $this->timer[$method],
-        'calls' => $this->calls[$method]
-      );
+     * @param   string propname
+     * @param   &mixed propvalue
+     * @return  boolean success
+     */
+    function __get($propname, &$propvalue) {
+      if (!isset ($this->_profilee->{$propname}))
+        return FALSE;
+        
+      $propvalue= &$this->_profilee->{$propname};
+      return TRUE;        
     }
-  }
+    
+    /**
+     * Set a property
+     *
+     * @access  public
+     * @param   string propname
+     * @param   &mixed propvalue
+     * @return  bool success
+     */
+    function __set($propname, $propvalue) {
+      $this->_profilee->{$propname}= $propvalue;
+      return TRUE;
+    }
+    
+    /**
+     * Gets all method names for called methods
+     *
+     * @access  public
+     * @return  array names
+     */
+    function getCalledFunction() {
+      return array_keys ($this->timer);
+    }    
+
+    /**
+     * Retrieve the profiling results for a given function
+     *
+     * @access  public
+     * @param   string method
+     * @return  float time
+     */
+    function getTiming($method) {
+      return $this->timer[$method];
+    }
+    
+    /**
+     * Retrieve the call counter for a given function
+     *
+     * @access  public
+     * @param   string method
+     * @return  int calls
+     */
+    function getCalledCount($method) {
+      return $this->calls[$method];
+    }
+    
+    /**
+     * Creates a string representation for this class and
+     * its profiling information.
+     *
+     * @access  public
+     * @return  string representation
+     */
+    function toString() {
+      $vals= array_unique(array_merge(array_keys ($this->timer), array_keys ($this->calls)));
+      $t= sprintf ("Profiling information for class %s\n", 
+        (is_a($this->_profilee, 'Object') 
+          ? $this->_profilee->getClassName() 
+          : get_class ($this->_profilee)
+      ));
+      
+      $sumCalls= $sumTimer= 0;
+      foreach ($vals as $v) {
+        $calls= max ($this->calls[$v], 1); 
+        $sumCalls+= $this->calls[$v]; $sumTimer+= $this->timer[$v];
+        $t.= sprintf ("Method %15s: %2d calls, %1.3fs, avg. %1.3fs\n",
+          $v,
+          $this->calls[$v],
+          $this->timer[$v],
+          $this->timer[$v] / $calls
+        );
+      }
+      
+      $t.= sprintf ("Total                *: %2d calls, %1.3fs, avg. %1.3fs\n",
+        $sumCalls,
+        $sumTimer,
+        $sumTimer / $sumCalls
+      );
+      
+      return $t;
+    }
+    
+  } overload ('ClassProfiler');
 
 ?>
