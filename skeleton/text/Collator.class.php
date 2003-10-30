c7c2177d65d8ac61750af4b9c1bc25a4d69714ef
thekid <timm.friebe@gmail.com>
2003-10-30 13:33:58 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2631 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/text/Collator.class.php
@@ -0,0 +1,61 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * The Collator class performs locale-sensitive String comparison. 
+   *
+   * @see      reference
+   * @purpose  purpose
+   */
+  class Collator extends Object {
+    var
+      $locale = '';
+     
+    /**
+     * Constructor
+     *
+     * @access  protected
+     * @param   string locale
+     */ 
+    function __construct($locale) {
+      parent::__construct();
+      $this->locale= $locale;
+    }
+    
+    /**
+     * Gets the Collator for the desired locale.
+     *
+     * @access  public
+     * @param   &util.Locale locale
+     * @return  &text.Collator
+     */
+    function getInstance(&$locale) {
+      static $instance= array();
+      
+      $id= $locale->hashCode();
+      if (!isset($instance[$id])) {
+        $instance[$id]= new Collator($locale->toString());
+      }
+      return $instance[$id];
+    } 
+
+    /**
+     * Compares its two arguments for order. Returns a negative integer, 
+     * zero, or a positive integer as the first argument is less than, 
+     * equal to, or greater than the second.
+     *
+     * @access  public
+     * @param   &string a
+     * @param   &string b
+     * @return  int
+     */
+    function compare(&$a, &$b) {
+      setlocale(LC_COLLATE, $this->locale);
+      return strcoll($a, $b);
+    }
+  
+  } implements(__FILE__, 'util.Comparator');
+?>
---
+++ skeleton/util/Comparator.class.php
@@ -0,0 +1,28 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Comparator interface 
+   *
+   * @see      xp://util.Hashmap#usort
+   * @see      php://usort
+   * @purpose  A comparison function
+   */
+  class Comparator extends Interface {
+  
+    /**
+     * Compares its two arguments for order. Returns a negative integer, 
+     * zero, or a positive integer as the first argument is less than, 
+     * equal to, or greater than the second.
+     *
+     * @access  public
+     * @param   &mixed a
+     * @param   &mixed b
+     * @return  int
+     */
+    function compare(&$a, &$b) { }
+  }
+?>
