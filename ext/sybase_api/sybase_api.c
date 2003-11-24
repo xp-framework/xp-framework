1c9d4eabd0ce6c99498c92aff1353a03900b9dc1
kiesel <alex@kiesel.name>
2003-11-24 23:13:18 +0000

    - correct macro
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2729 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ext/sybase_api/sybase_api.c
+++ ext/sybase_api/sybase_api.c
@@ -642,7 +642,7 @@ SYBASE_API char *sybase_nameofdatatype(CS_INT datatype)
             return "CS_USHORT_TYPE";
         case CS_UNIQUE_TYPE:
             return "CS_UNIQUE_TYPE";
-#if defined CS_UNICHAR_TYPE
+#ifdef CS_UNICHAR_TYPE
         case CS_UNICHAR_TYPE:
             return "CS_UNICHAR_TYPE";
 #endif
