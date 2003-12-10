bf9440a420016ddf47ba1a7e573f38a544871d4a
thekid <timm.friebe@gmail.com>
2003-12-10 16:02:43 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2763 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/xml/QName.class.php
@@ -0,0 +1,40 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Qualified name
+   *
+   * @purpose  Wrapper
+   */
+  class QName extends Object {
+    var
+      $namespace    = '',
+      $localpart    = '';
+    
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   string namespace
+     * @param   string localpart
+     */
+    function __construct($namespace, $localpart) {
+      parent::__construct();
+      $this->namespace= $namespace;
+      $this->localpart= $localpart;
+    }
+    
+    /**
+     * Returns a string representation
+     *
+     * @access  public
+     * @return  string
+     */
+    function toString() {
+      return ltrim($this->namespace.'/'.$this->localpart, '/');
+    }
+  }
+?>
