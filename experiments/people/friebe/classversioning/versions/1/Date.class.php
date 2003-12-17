46f4b5100a8cce093d3146b046bc0e6d4bd2ad46
thekid <timm.friebe@gmail.com>
2003-12-17 12:30:07 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2784 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/people/friebe/classversioning/versions/1/Date.class.php
@@ -0,0 +1,17 @@
+<?php
+/* This class is part of the XP framework's experiments
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Date, version 1
+   *
+   * @purpose  Dummy class for versioning experiment
+   */
+  class Date extends Object {
+    var
+      $version= 1;
+
+  }
+?>
---
+++ experiments/people/friebe/classversioning/versions/3/Date.class.php
@@ -0,0 +1,17 @@
+<?php
+/* This class is part of the XP framework's experiments
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Date, version 3
+   *
+   * @purpose  Dummy class for versioning experiment
+   */
+  class Date extends Object {
+    var
+      $version= 3;
+
+  }
+?>
