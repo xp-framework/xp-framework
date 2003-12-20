927e6b21f56a851f02060fadf3d6bff324a660b8
thekid <timm.friebe@gmail.com>
2003-12-20 18:03:33 +0000

    - Start with property file instead of with URL
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2819 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/dist/desktop/util/klip/bin/klip.php
+++ ports/dist/desktop/util/klip/bin/klip.php
@@ -6,15 +6,26 @@
 
   require('lang.base.php');
   xp::sapi('gui.gtk');
-  uses('de.thekid.gui.gtk.Klip');
+  uses(
+    'util.Properties',
+    'de.thekid.gui.gtk.Klip'
+  );
   
   // {{{ main
   $p= &new ParamString();
   if (!$p->exists(1)) {
-    Console::writeLinef('Usage: %s <url_to_klip_file>', $p->value(0));
+    Console::writeLinef('Usage: %s <ini_file>', $p->value(0));
     exit(-1);
   }
   
-  run(new Klip($p));
+  $prop= &new Properties($p->value(1));
+  try(); {
+    $prop->reset();
+  } if (catch('Exception', $e)) {
+    $e->printStackTrace();
+    exit(-1);
+  }
+ 
+  run(new Klip($p, $prop));
   // }}}
 ?>
