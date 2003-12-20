991233e8708ceed5513574a8c0eed4100c8623da
thekid <timm.friebe@gmail.com>
2003-12-20 12:57:42 +0000

    - Correctly align output of toString()
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2810 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/peer/URL.class.php
+++ skeleton/peer/URL.class.php
@@ -44,14 +44,14 @@
     function toString() {
       return sprintf(
         "%s@ {\n".
-        "\t[scheme]        %s\n".
-        "\t[host]        %s\n".
-        "\t[port]        %d\n".
-        "\t[user]        %s\n".
-        "\t[password]    %s\n".
-        "\t[path]        %s\n".
-        "\t[query]        %s\n".
-        "\t[fragment]    %s\n".
+        "  [scheme]      %s\n".
+        "  [host]        %s\n".
+        "  [port]        %d\n".
+        "  [user]        %s\n".
+        "  [password]    %s\n".
+        "  [path]        %s\n".
+        "  [query]       %s\n".
+        "  [fragment]    %s\n".
         "}",
         $this->getClassName(),
         $this->getScheme(),
