1c8ab2b8b4ca3f43b6e4ac1db4579318ff7717a5
thekid <timm.friebe@gmail.com>
2003-12-29 23:04:35 +0000

    - Adapt to changes in XPClass::getMethods() - Add optional parameter --invoke which lets the user specify   a method of this class to invoke
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2841 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- doc/examples/reflection.php
+++ doc/examples/reflection.php
@@ -18,17 +18,34 @@
     $e->printStackTrace();
     exit(-2);
   }
+  
+  $methods= '';
+  for ($i= 0, $m= $class->getMethods(), $s= sizeof($m); $i < $s; $i++) {
+    $methods.= '  - '.$m[$i]->getName()."()\n";
+  }
 
   Console::writef(
     "Class '%s'\n".
-    "* Methods:\n  - %s()\n\n".
+    "* Methods:\n%s\n".
     "* Fields:\n  - \$%s\n\n".
     "* Has method 'toString': %s\n\n".
-    "* Instance toString() output: %s\n",
+    "* Instance toString() output: %s\n\n",
     $class->getName(),
-    implode("()\n  - ", $class->getMethods()),
+    $methods,
     implode("\n  - \$", array_keys($class->getFields())),
     var_export($class->hasMethod('toString'), 1),
     $instance->toString()
   );
+  
+  if ($p->exists('invoke')) {
+    with ($m= &$class->getMethod($p->value('invoke'))); {
+      $result= &$m->invoke($class);
+      Console::writeLinef(
+        '* Method %s() invokation (without parameters) results in [%s]%s', 
+        $m->getName(),
+        xp::typeOf($result),
+        var_export($result, 1)
+      );
+    }
+  }
 ?>
