3c0567d64d556f40a9415d9acd7d3344e645b72a
thekid <timm.friebe@gmail.com>
2004-01-11 16:09:33 +0000

    - Include parent class in output - Do not try to instanciate interfaces or invoke methods on them - List implemented interfaces for classes
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2901 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- doc/examples/reflection.php
+++ doc/examples/reflection.php
@@ -6,11 +6,11 @@
   require('lang.base.php');
   xp::sapi('cli');
 
+  // {{{ main
   $p= &new ParamString();
-
   try(); {
     $class= &XPClass::forName($p->value(1));
-    $instance= &$class->newInstance();
+    $parent= &$class->getParentClass();
   } if (catch ('ClassNotFoundException', $e)) {
     $e->printStackTrace();
     exit(-1);
@@ -19,34 +19,68 @@
     exit(-2);
   }
   
+  // Retrieve class methods
   $methods= '';
   for ($i= 0, $m= $class->getMethods(), $s= sizeof($m); $i < $s; $i++) {
-    $class= &$m[$i]->getDeclaringClass();
-    $methods.= '  - '.$m[$i]->getName().'() [from '.$class->getName()."]\n";
+    $decl= &$m[$i]->getDeclaringClass();
+    $methods.= '  - '.$m[$i]->getName().'() [declared in '.$decl->getName()."]\n";
   }
-
-  Console::writef(
-    "Class '%s'\n".
-    "* Methods:\n%s\n".
-    "* Fields:\n  - \$%s\n\n".
-    "* Has method 'toString': %s\n\n".
-    "* Instance toString() output: %s\n\n",
-    $class->getName(),
-    $methods,
-    implode("\n  - \$", array_keys($class->getFields())),
-    var_export($class->hasMethod('toString'), 1),
-    $instance->toString()
-  );
   
-  if ($p->exists('invoke')) {
-    with ($m= &$class->getMethod($p->value('invoke'))); {
-      $result= &$m->invoke($class);
-      Console::writeLinef(
-        '* Method %s() invokation (without parameters) results in [%s]%s', 
-        $m->getName(),
-        xp::typeOf($result),
-        var_export($result, 1)
-      );
+  // Check whether this class is an interface
+  if ($class->isInterface()) {
+    Console::writef(
+      "Interface '%s' (extends %s)\n".
+      "* Methods:\n%s\n".
+      "* Has method 'toString': %s\n\n",
+      $class->getName(),
+      $parent ? $parent->getName() : '(n/a)',
+      $methods,
+      var_export($class->hasMethod('toString'), 1)
+    );
+  } else {
+
+    // Retrieve constructor
+    $constructor= &$class->getConstructor();
+    $decl= &$constructor->getDeclaringClass();
+    
+    // Retrieve implemented interfaces
+    $implements= '';
+    for ($c= 0, $i= $class->getInterfaces(), $s= sizeof($i); $c < $s; $c++) {
+      $implements.= ', '.$i[$c]->getName();
+    }
+
+    // Create a new instance
+    $instance= &$class->newInstance();
+  
+    Console::writef(
+      "Class '%s' (extends %s) %s\n".
+      "* Constructor: %s() [declared in %s]\n".
+      "* Methods:\n%s\n".
+      "* Fields:\n  - \$%s\n\n".
+      "* Has method 'toString': %s\n\n".
+      "* Instance toString() output: %s\n\n",
+      $class->getName(),
+      $parent ? $parent->getName() : '(n/a)',
+      $implements ? 'implements '.substr($implements, 2) : '',
+      $constructor->getName(),
+      $decl->getName(),
+      $methods,
+      implode("\n  - \$", array_keys($class->getFields())),
+      var_export($class->hasMethod('toString'), 1),
+      $instance->toString()
+    );
+  
+    if ($p->exists('invoke')) {
+      with ($m= &$class->getMethod($p->value('invoke'))); {
+        $result= &$m->invoke($instance);
+        Console::writeLinef(
+          '* Method %s() invokation (without parameters) results in [%s]%s', 
+          $m->getName(),
+          xp::typeOf($result),
+          var_export($result, 1)
+        );
+      }
     }
   }
+  // }}}
 ?>
