c547ccfaa443ecaca3f9c63bbf3d0d1ac5cc771f
thekid <timm.friebe@gmail.com>
2004-01-18 00:38:16 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2932 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/sapi/class.sapi.php
@@ -0,0 +1,24 @@
+<?php
+/* This file provides the class sapi for the XP framework
+ * 
+ * $Id$
+ */
+  uses('util.cmd.ParamString');
+  
+  // {{{ void runnable(void)
+  //     Syntax: class Test extends Object { ... } runnable();
+  function runnable() {
+    $p= &new ParamString();
+    $class= xp::reflect(basename($p->value(0), '.class.php'));
+
+    $target= array($class, 'main');
+    if (!is_callable($target)) {
+      xp::error('Target '.$class.'::main() is not runnable');
+      // Bails out
+    }
+
+    xp::registry('class.'.$class, 'Runnable$'.$class);
+    exit(call_user_func($target, $p));
+  }
+  // }}}
+?>
