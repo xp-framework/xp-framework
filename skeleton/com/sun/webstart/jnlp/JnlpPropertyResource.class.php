516d561418d8e0b64a42a901e3c90a051035c2d4
thekid <timm.friebe@gmail.com>
2004-02-06 15:57:00 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@2980 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/com/sun/webstart/jnlp/JnlpPropertyResource.class.php
@@ -0,0 +1,100 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses('com.sun.webstart.jnlp.JnlpResource');
+
+  /**
+   * JNLP resource that points to a system propetry
+   *
+   * XML representation
+   * <pre>
+   *   <property name="org.omg.CORBA.ORBClass" value="com.inprise.vbroker.orb.ORB"/>
+   * </pre>
+   *
+   * @see      xp://com.sun.webstart.JnlpResource
+   * @purpose  JNLP resource
+   */
+  class JnlpPropertyResource extends JnlpResource {
+    var 
+      $name   = '',
+      $value  = '';
+
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   string name
+     * @param   string value
+     */
+    function __construct($name, $value) {
+      parent::__construct();
+      $this->name= $name;
+    }
+
+    /**
+     * Set Name
+     *
+     * @access  public
+     * @param   string name
+     */
+    function setName($name) {
+      $this->name= $name;
+    }
+
+    /**
+     * Get Name
+     *
+     * @access  public
+     * @return  string
+     */
+    function getName() {
+      return $this->name;
+    }
+
+    /**
+     * Set Value
+     *
+     * @access  public
+     * @param   string value
+     */
+    function setValue($value) {
+      $this->value= $value;
+    }
+
+    /**
+     * Get Value
+     *
+     * @access  public
+     * @return  string
+     */
+    function getValue() {
+      return $this->value;
+    }
+
+    /**
+     * Get name
+     *
+     * @access  public
+     * @return  string
+     */
+    function getTagName() { 
+      return 'property';
+    }
+
+    /**
+     * Get attributes
+     *
+     * @access  public
+     * @return  array
+     */
+    function getTagAttributes() { 
+      return array(
+        'name'  => $this->name,
+        'value' => $this->value
+      );
+    }
+  }
+?>
