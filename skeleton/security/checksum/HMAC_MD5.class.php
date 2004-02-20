cd2ae2107c7d5e7840c063c1fdcdf2639332fdf8
thekid <timm.friebe@gmail.com>
2004-02-20 23:39:14 +0000

    - Add optional parameter key to fromFile() and fromString()
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3003 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/security/checksum/HMAC_MD5.class.php
+++ skeleton/security/checksum/HMAC_MD5.class.php
@@ -15,6 +15,29 @@
   class HMAC_MD5 extends Checksum {
   
     /**
+     * Calculate HMAC_MD5 for given 
+     *
+     * @model   static
+     * @access  protected
+     * @param   string str
+     * @param   string key default NULL
+     * @return  string
+     */
+    function hash($str, $key= NULL) {
+      if (NULL === $key) return pack('H*', md5($str));
+      
+      $key= str_pad($key, 0x40, "\x00");
+      if (strlen($key) > 0x40) {
+        $key= pack('H*', md5($key));
+      }
+
+      $ip= $key ^ str_repeat("\x36", 0x40) ;
+      $op= $key ^ str_repeat("\x5c", 0x40) ;
+      
+      return HMAC_MD5::hash($op.pack('H*', md5($ip.$str)));
+    }
+      
+    /**
      * Create a new checksum from a string
      *
      * @access  public
@@ -22,8 +45,8 @@
      * @param   string key default NULL
      * @return  &security.checksum.HMAC_MD5
      */
-    function &fromString($str) {
-      return new HMAC_MD5(pack('H*', md5($str)));
+    function &fromString($str, $key= NULL) {
+      return new HMAC_MD5(HMAC_MD5::hash($str, $key));
     }
 
     /**
@@ -31,17 +54,18 @@
      *
      * @access  public
      * @param   &io.File file
+     * @param   string key default NULL
      * @return  &security.checksum.HMAC_MD5
      */
-    function &fromFile(&$file) {
+    function &fromFile(&$file, $key= NULL) {
       try(); {
         $file->open(FILE_MODE_READ);
-        $data= $file->read($file->size());
+        $str= $file->read($file->size());
         $file->close();
       } if (catch('Exception', $e)) {
         return throw($e);
       }
-      return HMAC_MD5::fromString($data);
+      return new HMAC_MD5(HMAC_MD5::hash($str, $key));
     }
   }
 ?>
