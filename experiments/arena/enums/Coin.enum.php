f4602ca32a25c7e7b23577e854258484f5cb5b06
thekid <timm.friebe@gmail.com>
2004-02-23 02:35:09 +0000

    - Add a static function
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3018 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/enums/Coin.enum.php
+++ experiments/arena/enums/Coin.enum.php
@@ -10,6 +10,25 @@
    * @purpose  Enumeration
    */
   enum Coin {
-    PENNY(1), NICKEL(5), DIME(10), QUARTER(25); 
+    PENNY(1), NICKEL(5), DIME(10), QUARTER(25);
+    
+    /**
+     * Returns color of a specified coin
+     *
+     * @model   static
+     * @access  public
+     * @param   int c
+     * @return  string color
+     * @throws  lang.IllegalArgumentException
+     */
+    function colorOf($c) {
+      switch ($c) {
+        case Coin_PENNY:    return 'COPPER';
+        case Coin_NICKEL:   return 'NICKEL';
+        case Coin_DIME:
+        case Coind_QUARTER: return 'SILVER';
+        default: throw(new IllegalArgumentException('Unknown coin '.$c));
+      }
+    }
   }
 ?>
