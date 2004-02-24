24b69a99f07cba151fbcf6b367f16a3a4b429b34
thekid <timm.friebe@gmail.com>
2004-02-24 18:24:25 +0000

    - Merge changes from PHP4 "branch"
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3034 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/skeleton2/lang/ClassLoader.class.php
+++ experiments/skeleton2/lang/ClassLoader.class.php
@@ -3,87 +3,62 @@
  * 
  * $Id$
  */
- 
+
   uses('lang.ClassNotFoundException');
-  
+
   /** 
    * Loads a class
    * 
-   * Usage (static calls):
-   * <code>
-   *   try(); {
-   *     $name= ClassLoader::loadClass($argv[1]);
-   *   } if (catch('ClassNotFoundException', $e)) {
-   *     die($e->printStackTrace());
-   *   }
-   *
-   *   $obj= &new $name();
-   * </code>
-   *
-   * Usage (class loader instance):
-   * <code>
-   *   $loader= &new ClassLoader('info.binford6100.webservices');
-   *   try();
-   *     $name= $loader->loadClass($argv[1]);
-   *   } if (catch('ClassNotFoundException', $e)) {
-   *     die($e->printStackTrace());
-   *   }
-   *
-   *   $obj= &new $name();
-   * </code>
-   *
-   * @access    public, static
+   * @purpose  Load classes
+   * @see      xp://lang.XPClass#forName
    */
   class ClassLoader extends Object {
-    var $classpath= '';
+    public static $instance= NULL;
+    public
+      $classpath= '';
     
     /**
      * Constructor. 
+     *
      * The path argument is optional and lets you define where to search for
-     * classes (it will be prefixed to the class name)
+     * classes (it will be prefixed to the class name).
      *
      * @access  public
      * @param   string path default '' classpath
      */
-    function __construct($path= '') {
+    public function __construct($path= '') {
       if (!empty($path)) $this->classpath= $path.'.';
-      parent::__construct();
     }
     
     /**
-     * Returns whether this class is a built-in class (e.g., "Directory")
+     * Retrieve the default class loader
      *
-     * @access  static
-     * @param   string name
-     * @return  bool true if name represents a built-in class
+     * @model   static
+     * @access  public
+     * @return  &lang.ClassLoader
      */
-    function isBuiltin($name) {
-      return ('php.' == substr($name, 0, 4));
+    public static function getDefault() {
+      if (!self::$instance) self::$instance= new ClassLoader();
+      return self::$instance;
     }
     
     /**
-     * Load
+     * Load the class by the specified name
      *
-     * @access  public, static
-     * @param   string className fully qualified class name io.File
-     * @return  string class' name for instantiation
-     * @throws  ClassNotFoundException in case the class can not be found
+     * @access  public
+     * @param   string class fully qualified class name io.File
+     * @return  &lang.XPClass
+     * @throws  lang.ClassNotFoundException in case the class can not be found
      */
-    function loadClass($className) {
-      if (!ClassLoader::isBuiltin($className)) {
-        $str= (isset($this) ? @$this->classpath : '').$className;
-        uses($str);
-        $phpName= xp::reflect($str);
-      } else {
-        $phpName= substr($className, 4);
+    public function loadClass($class) {
+      $qname= $this->classpath.$class;
+      $name= xp::reflect($qname);
+
+      if (class_exists($name) || uses($qname)) {
+        return new XPClass($name);
       }
-      if (class_exists($phpName)) return $phpName;
       
-      throw(new ClassNotFoundException(sprintf(
-        'class "%s" [%s] not found',
-        $className,
-        $phpName
-      )));
+      throw (new ClassNotFoundException('Class "'.$qname.'" not found'));
     }
   }
 ?>
