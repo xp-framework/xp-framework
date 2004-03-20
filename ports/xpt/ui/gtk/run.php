d0de534d969ba704c424dba1c9e60191bafe7d55
thekid <timm.friebe@gmail.com>
2004-03-20 19:13:54 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3177 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/xpt/ui/gtk/run.php
@@ -0,0 +1,15 @@
+<?php
+  require('lang.base.php');
+  xp::sapi('gui.gtk');
+  uses('net.xp-framework.unittest.runner.gtk.UnitTestUI');
+  
+  try(); {
+    $app= &new UnitTestUI(new ParamString());
+    $app->init();
+  } if (catch('GuiException', $e)) {
+    $e->printStackTrace();
+    exit(-1);
+  }
+  $app->run();
+  $app->done();
+?>
