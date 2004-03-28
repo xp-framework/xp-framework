f8dde78324db2802083eab0bc189094081332f18
thekid <timm.friebe@gmail.com>
2004-03-28 15:17:03 +0000

    - Initial release # PHP5 only
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3197 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/arena/serialize/SerializationException.class.php
@@ -0,0 +1,14 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Indicates a value cannot be serialized
+   *
+   * @purpose  Exception
+   */
+  class SerializationException extends Throwable {
+  }
+?>
