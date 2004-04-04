ad325269fe104050bfaeecb523a17522170ba362
thekid <timm.friebe@gmail.com>
2004-04-04 17:47:39 +0000

    - Throw a lang.FormatException in case the date could not be parsed
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3211 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/text/parser/DateParser.class.php
+++ skeleton/text/parser/DateParser.class.php
@@ -28,9 +28,16 @@
      * @access  public
      * @param   string s
      * @return  &util.Date
+     * @throws  lang.FormatException in case the date could not be parsed
      */
     function &parse($s) {
-      if (preg_match('/^([0-9]+)\.([0-9]+)(\.([0-9]+))? ?([0-9]+)?:?([0-9]+)?:?([0-9]+)?/', $s, $matches)) {
+      if (empty($s)) {
+      
+        // Border case
+        return throw(new FormatException('Cannot parse empty string'));
+      } elseif (preg_match('/^([0-9]+)\.([0-9]+)(\.([0-9]+))? ?([0-9]+)?:?([0-9]+)?:?([0-9]+)?/', $s, $matches)) {
+      
+        // German date format
         $stamp= mktime(
           isset($matches[5]) ? $matches[5] : 0, 
           isset($matches[6]) ? $matches[6] : 0, 
@@ -40,7 +47,19 @@
           isset($matches[4]) ? $matches[4] : date('Y')
         );
       } else {
-        $stamp= strtotime(strtolower($s));
+      
+        // FIXME: strtotime() returns -1 on failure, but also for the date
+        // Jan 01 1970 00:59:59 (in case the underlying OS supports negative
+        // timestamps). Unfortunately, no warnings are issued whatsoever, so
+        // there is no way to find out if this is the case or if the function 
+        // actually failed.
+        //
+        // I would consider this a bug in PHP (the function should return FALSE
+        // or at least raise a warning), but for now, we will have to live 
+        // with it.
+        if (-1 === ($stamp= strtotime(strtolower($s)))) {
+          return throw(new FormatException('Could not parse "'.$s.'"'));
+        }
       }
       
       return new Date($stamp);
