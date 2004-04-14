4fa52e132336fe002bec3bf63061f2254ec24706
kiesel <alex@kiesel.name>
2004-04-14 17:14:22 +0000

    - add regular expressions to parse date string given in a   Generalized Time format (which is used by e.g. OpenLDAP).
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3251 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/text/parser/DateParser.class.php
+++ skeleton/text/parser/DateParser.class.php
@@ -4,7 +4,7 @@
  * $Id$ 
  */
 
-  uses('util.Date');
+  uses('util.Date', 'util.TimeZone');
 
   /**
    * Parses dates
@@ -14,9 +14,17 @@
    *
    *   $d= &DateParser::parse('13.02');
    *   echo $d->toString();
+   *    
+   *   $d= &DateParser::parse('200401190957+0100');
+   *   echo $d->toString();
+   *
+   *   $d= &DateParser::parse('20040414101045Z');
+   *   echo $d->toString();
+   *
    * </code>
    *
    * @see      http://www.gnu.org/software/tar/manual/html_chapter/tar_7.html
+   * @see      http://ldap.akbkhome.com/syntax/1%2E3%2E6%2E1%2E4%2E1%2E1466%2E115%2E121%2E1%2E24.html
    * @purpose  Parser
    */
   class DateParser extends Object {
@@ -46,6 +54,34 @@
           $matches[1], 
           isset($matches[4]) ? $matches[4] : date('Y')
         );
+      } elseif (preg_match('/^(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})?(Z|([+-]\d{4}))?$/', $s, $matches)) {
+      
+        // Generalized date format
+        $stamp= mktime(
+          $matches[4],
+          $matches[5],
+          $matches[6],
+          $matches[2],
+          $matches[3],
+          $matches[1]
+        );
+        
+        // If a timezone information has been given, try to convert timestamp into local time
+        if (!empty($matches[7])) {
+          try(); {
+            if (
+              ($tz= &new TimeZone($matches[7])) &&
+              ($lc= &TimeZone::getLocal())
+            ) {
+            
+              $date= &$lc->convertDate(new Date($stamp), $tz);
+              $stamp= $date->getTime();
+            }
+          } if (catch ('IllegalArgumentException', $e)) {
+          
+            // Ignore, do not modify timestamp...
+          }
+        }
       } else {
       
         // FIXME: strtotime() returns -1 on failure, but also for the date
