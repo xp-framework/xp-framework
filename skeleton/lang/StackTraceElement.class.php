7b39e7659d76e47241ab9cabf464d36449c8d50b
thekid <timm.friebe@gmail.com>
2004-04-15 19:37:03 +0000

    - Prevent sprintf() from substituting "%s" in arguments by   one of the parameters passed to it
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3259 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/lang/StackTraceElement.class.php
+++ skeleton/lang/StackTraceElement.class.php
@@ -55,10 +55,10 @@
           } elseif (is_object($this->args[$j])) {
             $args[]= get_class($this->args[$j]).'{}';
           } elseif (is_string($this->args[$j])) {
-            $display= addcslashes(substr($this->args[$j], 0, min(
+            $display= str_replace('%', '%%', addcslashes(substr($this->args[$j], 0, min(
               (FALSE === $p= strpos($this->args[$j], "\n")) ? 0x40 : $p, 
               0x40
-            )), "\0..\17");
+            )), "\0..\17"));
             $args[]= (
               '(0x'.dechex(strlen($this->args[$j])).")'".
               $display.
