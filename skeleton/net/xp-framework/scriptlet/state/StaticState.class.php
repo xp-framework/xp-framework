87fd54b57d15ff862adb0329dc33465003e4e16d
thekid <timm.friebe@gmail.com>
2004-04-24 15:51:47 +0000

    - Retrieve top 5 news from database # TBD: caching
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3313 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/net/xp-framework/scriptlet/state/StaticState.class.php
+++ skeleton/net/xp-framework/scriptlet/state/StaticState.class.php
@@ -4,7 +4,10 @@
  * $Id$ 
  */
 
-  uses('scriptlet.xml.workflow.AbstractState');
+  uses(
+    'scriptlet.xml.workflow.AbstractState',
+    'net.xp-framework.db.caffeine.XPNews'
+  );
 
   /**
    * Handles /xml/static
@@ -12,6 +15,26 @@
    * @purpose  State
    */
   class StaticState extends AbstractState {
-  
+
+    /**
+     * Process this state.
+     *
+     * @access  public
+     * @param   &scriptlet.xml.workflow.WorkflowScriptletRequest request
+     * @param   &scriptlet.xml.XMLScriptletResponse response
+     */
+    function process(&$request, &$response) {
+    
+      // Retrieve top 5 news from database - TBD: caching
+      try(); {
+        $news= XPNews::getByDateOrdered(5);
+      } if (catch('SQLException', $e)) {
+        $news= array();
+        // Fall through
+      }
+      
+      $n= &$response->addFormResult(new Node('news'));
+      $n->addChild(Node::fromArray($news, 'items'));
+    }
   }
 ?>
