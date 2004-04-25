dc9bac93179195140221f9b0be42d987270ad077
thekid <timm.friebe@gmail.com>
2004-04-25 16:46:59 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3351 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/net/xp-framework/scriptlet/state/LookupState.class.php
@@ -0,0 +1,48 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses(
+    'scriptlet.xml.workflow.AbstractState'
+  );
+
+  /**
+   * Handles /xml/lookup
+   *
+   * @purpose  State
+   */
+  class LookupState extends AbstractState {
+
+    /**
+     * Process this state. Redirects to known targets or invokes the 
+     * search if the lookup fails.
+     *
+     * @access  public
+     * @param   &scriptlet.xml.workflow.WorkflowScriptletRequest request
+     * @param   &scriptlet.xml.XMLScriptletResponse response
+     */
+    function process(&$request, &$response) {
+      with ($query= $request->getData(), $uri= $request->getURI()); {
+        $target= 'search?'.$query;
+        
+        // HACK until build system infrastructure provides a better method
+        if (file_exists('../build/cache/class.php/'.basename($query))) {
+          $target= 'documentation/class?core/'.$query;
+        }
+
+        $response->sendRedirect(sprintf(
+          '%s://%s/xml/%s.%s%s/%s', 
+          $uri['scheme'],
+          $uri['host'],          
+          $request->getProduct(),
+          $request->getLanguage(),
+          $request->session ? '.psessionid='.$request->session->getId() : '',
+          $target
+        ));
+        return FALSE; // Indicate no further processing is to be done
+      }
+    }
+  }
+?>
