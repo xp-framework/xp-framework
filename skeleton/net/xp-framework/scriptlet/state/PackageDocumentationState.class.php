1140e50fac30fcaa7468e5d3c644d4e2fc1f1c87
thekid <timm.friebe@gmail.com>
2004-04-25 13:20:45 +0000

    - Initial rrelease
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3335 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/net/xp-framework/scriptlet/state/PackageDocumentationState.class.php
@@ -0,0 +1,28 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses(
+    'scriptlet.xml.workflow.AbstractState'
+  );
+
+  /**
+   * Handles /xml/documentation/package
+   *
+   * @purpose  State
+   */
+  class PackageDocumentationState extends AbstractState {
+
+    /**
+     * Process this state.
+     *
+     * @access  public
+     * @param   &scriptlet.xml.workflow.WorkflowScriptletRequest request
+     * @param   &scriptlet.xml.XMLScriptletResponse response
+     */
+    function process(&$request, &$response) {
+    }
+  }
+?>
