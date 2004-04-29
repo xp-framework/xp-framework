0aa03940dc77b5e8def97c7367f7d624af610ea3
Christian Lang <clang@developer.xp-framework.net>
2004-04-29 15:07:57 +0000

    - add possibility to add path on request - add getConnection Method - fixed proppatch
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3386 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/peer/webdav/WebdavClient.class.php
+++ skeleton/peer/webdav/WebdavClient.class.php
@@ -6,8 +6,7 @@
   
   uses(
     'peer.webdav.WebdavConnection',
-    'peer.Header',
-    'peer.http.BasicAuthorization'
+    'peer.Header'
   );
 
   /**
@@ -16,63 +15,118 @@
    * @purpose  Provide a client to access an webdav server
    */
   class WebdavClient extends Object {
+    var 
+      $url= NULL,
+      $path= '';
   
     /**
      * Constructor
      *
      * @access  public
-     * @param   string host default 'localhost' Webdav server
-     * @param   string directory default webdav
-     * @param   int port default port 81
+     * @param   string url or peer.Url
      */
     function __construct($url) {
-      $this->c= new WebdavConnection($url);
+      if (!is_a($url, 'URL')) $this->url= &new URL($url);
       parent::__construct();
+    }
+    
+    /**
+     * Get e Connection
+     *
+     * @access  public
+     * @param   string name
+     * @return  &peer.webdav.WebdavConnection
+     */
+    function &getConnection($name= NULL) {
+      $url= &new URL($this->url->getURL().$this->path.($name === NULL ? '' : '/'.$name));
+      return new WebdavConnection($url);
+    }
+    
+    /**
+     * Helper Method to set the path
+     *
+     * @access  public
+     * @param   string path
+     */
+    function setPath($path) {
+      $this->path= $path;
     }
     
     /**
      * Do a Propfind on Webdav server
      *
      * @access  public
+     * @param   string filename or directory
      * @return  &peer.http.HttpResponse response object
      */
-    function read() {    
+    function &read($name= NULL) {    
       try(); {
-        $response= &$this->c->propfind();
+        $c= &$this->getConnection($name === NULL ? NULL : $name);
+        $response= &$c->propfind();
       } if (catch('Exception', $e)) {
         return throw($e);
       }
-      return $response;
+    return $response;
     }
     
     /**
      * Do a Put on Webdav server
      *
      * @access  public
+     * @param   io.File
+     * @param   string filename or directory
      * @return  &peer.http.HttpResponse response object
      */
-    function put() {
+    function &put($file, $name= NULL) {    
+      
+      if (!$file->isOpen()) $file->open(FILE_MODE_READ);
+      
       try(); {
-        $response= &$this->c->put();
+        $c= &$this->getConnection($name === NULL ? NULL : $name);
+        $response= &$c->put(
+          $file->read($file->size()),
+          new header('Content-Type', $file->getExtension())
+        );
       } if (catch('Exception', $e)) {
         return throw($e);
       }
-      return $response;
+    return $response;
     }
     
     /**
      * Do a Get on Webdav server
      *
      * @access  public
+     * @param   string filename or directory
      * @return  &peer.http.HttpResponse response object
      */
-    function get() {
+    function &get($name= NULL) {
       try(); {
-        $response= &$this->c->put();
+        $c= &$this->getConnection($name === NULL ? NULL : $name);
+        $response= &$c->get();
       } if (catch('Exception', $e)) {
         return throw($e);
       }
-      return $response;
+    return $response;
+    }
+    
+    /**
+     * Do a Proppatch request
+     *
+     * @access  public
+     * @param   string properties
+     * @param   string filename or directory
+     * @return  &peer.http.HttpResponse response object
+     */
+    function &proppatch($properties, $name= NULL) {
+      
+      try(); {
+        $c= &$this->getConnection($name === NULL ? NULL : $name);
+        $response= &$c->proppatch($properties);
+      } if (catch('Exception', $e)) {
+        return throw($e);
+      }
+    return $response;    
     }
 
   
