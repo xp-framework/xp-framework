707119f3053f2073fbd7b197cc083a411ad7053e
thekid <timm.friebe@gmail.com>
2004-05-09 13:54:51 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3418 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/doc2/examples/autoload.php
@@ -0,0 +1,12 @@
+<?php
+/* This file is part of the XP framework's examples
+ *
+ * $Id$ 
+ */
+  require('lang.base.php');
+  uses('text.String');
+
+  // {{{ main
+  echo Collection::forClass('text.String'), "\n";
+  // }}}
+?>
