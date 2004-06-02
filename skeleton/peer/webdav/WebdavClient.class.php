e2091850e51070f487bc9b772bc85c91072bb708
Christian Lang <clang@developer.xp-framework.net>
2004-06-02 11:36:59 +0000

    - Add Move, Copy, Lock, Unlock and Delete Methods - Documentation completed
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3486 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/peer/webdav/WebdavClient.class.php
+++ skeleton/peer/webdav/WebdavClient.class.php
@@ -44,8 +44,14 @@
    */
   class WebdavClient extends Object {
     var 
-      $url= NULL,
-      $path= '';
+      $url=     NULL,
+      $uri=     '',
+      $path=    '',
+      $depth=   '1',
+      $xml=     NULL,
+      $source=        '',
+      $destination=   '',
+      $overwrite=     '1';   
   
     /**
      * Constructor.
@@ -61,11 +67,11 @@
      * Get a Connection
      *
      * @access  public
-     * @param   string name
+     * @param   string uri
      * @return  &peer.webdav.WebdavConnection
      */
-    function &getConnection($name= NULL) {
-      $url= &new URL($this->url->getURL().$this->path.($name === NULL ? '' : '/'.$name));
+    function &getConnection($uri= NULL) {
+      $url= &new URL($this->url->getURL().$this->path.($uri === NULL ? '' : '/'.$uri));
       return new WebdavConnection($url);
     }
     
@@ -83,14 +89,17 @@
      * Do a Propfind on Webdav server
      *
      * @access  public
-     * @param   string filename or directory
+     * @param   string uri, filename or directory
+     * @param   string xml, The XML of the Propfind Request (e.g. to select properties)  
      * @param   int depth, default 1
      * @return  &peer.http.HttpResponse response object
+     * @see     rfc://2518
      */
-    function &read($name= NULL, $depth= '1') {    
+    function &read($uri= NULL, $xml= NULL, $depth= '1') {     
       try(); {
-        $c= &$this->getConnection($name);
-        $response= &$c->propfind('',
+        $c= &$this->getConnection($uri);
+        $response= &$c->propfind(
+          $xml,
           array(
             new Header('Depth', $depth)
           )
@@ -106,17 +115,18 @@
      *
      * @access  public
      * @param   &io.File file
-     * @param   string filename or directory
+     * @param   string uri, filename or directory
      * @return  &peer.http.HttpResponse response object
+     * @see     rfc://2518
      */
-    function &put(&$file, $name= NULL) {    
+    function &put(&$file, $uri= NULL) {    
       if (!$file->isOpen()) $file->open(FILE_MODE_READ);
       try(); {
-        $c= &$this->getConnection($name);
+        $c= &$this->getConnection($uri);
         $response= &$c->put(
           $file->read($file->size()),
           array(
-            new Header('Content-Type', MimeType::getByFilename($name))
+            new Header('Content-Type', MimeType::getByFilename($uri))
           )
         );
       } if (catch('Exception', $e)) {
@@ -129,12 +139,13 @@
      * Do a Get on Webdav server
      *
      * @access  public
-     * @param   string filename or directory
+     * @param   string uri, filename or directory
      * @return  &peer.http.HttpResponse response object
+     * @see     rfc://2518
      */
-    function &get($name= NULL) {
+    function &get($uri= NULL) {    
       try(); {
-        $c= &$this->getConnection($name);
+        $c= &$this->getConnection($uri);
         $response= &$c->get();
       } if (catch('Exception', $e)) {
         return throw($e);
@@ -146,18 +157,142 @@
      * Do a Proppatch request
      *
      * @access  public
-     * @param   string properties
-     * @param   string filename or directory
+     * @param   string xml, The XML Representation of the Properties
+     * @param   string uri, filename or directory
      * @return  &peer.http.HttpResponse response object
+     * @see     rfc://2518
      */
-    function &proppatch($properties, $name= NULL) {
+    function &proppatch($xml, $uri= NULL) {          
       try(); {
-        $c= &$this->getConnection($name);
-        $response= &$c->proppatch($properties);
+        $c= &$this->getConnection($uri);
+        $response= &$c->proppatch($xml);
       } if (catch('Exception', $e)) {
         return throw($e);
       }
       return $response;    
     }
+        
+    /**
+     * Do a Copy Request
+     *
+     * @access  public
+     * @param   string source
+     * @param   string destination
+     * @param   bool overwrite, default FALSE
+     * @param   mixed depth, default Infinity
+     * @return  &peer.http.HttpResponse response object
+     * @see     rfc://2518
+     */
+    function copy($source, $destination, $overwrite= FALSE, $depth= 'Infinity') {        
+      try(); {
+        $c= &$this->getConnection($source);
+        $response= &$c->copy(
+          NULL,
+          array(
+            new Header('Overwrite', $overwrite ? 'T' : 'F'),
+            new Header('Destination', $destination),
+            new Header('Depth', $depth)
+          )
+        );
+      } if (catch('Exception', $e)) {
+        return throw($e);
+      }
+      return $response;      
+    }
+    
+    /**
+     * Do a Move Request
+     *
+     * @access  public 
+     * @param   string source
+     * @param   string destination
+     * @param   bool overwrite, default FALSE
+     * @return  &peer.http.HttpResponse response object
+     * @see     rfc://2518
+     */
+    function move($source, $destination, $overwrite= FALSE) {   
+      try(); {
+        $c= &$this->getConnection($source);
+        $response= &$c->move(
+          NULL,
+          array(
+            new Header('Overwrite', $overwrite ? 'T' : 'F'),
+            new Header('Destination', $destination)
+          )
+        );
+      } if (catch('Exception', $e)) {
+        return throw($e);
+      }
+      return $response;     
+    }
+    
+    /**
+     * Do a Lock Request
+     *
+     * @access  public  
+     * @param   string uri The uri of the collection or file
+     * @param   string xml, The XML of the lockrequest
+     * @return  &peer.http.HttpResponse response object
+     * @see     rfc://2518
+     */
+    function lock($uri, $xml) {    
+      try(); {
+        $c= &$this->getConnection($uri);
+        $response= &$c->lock(
+          $xml,
+          array(
+            new Header('Timeout', 'Infinity'),
+            new Header('Content-Type', 'text/xml'),
+            new Header('Content-Length', strlen($xml))
+          )
+        );
+      } if (catch('Exception', $e)) {
+        return throw($e);
+      }
+      return $response;   
+    }  
+    
+    /**
+     * Do a Unlock Request
+     *
+     * @access  public
+     * @param   string uri, filename or directory
+     * @param   string locktoken
+     * @return  &peer.http.HttpResponse response object
+     * @see     rfc://2518
+     */
+    function unlock($uri, $locktoken) {    
+      try(); {
+        $c= &$this->getConnection($uri);
+        $response= &$c->unlock(
+          NULL,
+          array(
+            new Header('Lock-Token', $locktoken)
+          )
+        );
+      } if (catch('Exception', $e)) {
+        return throw($e);
+      }
+      return $response;  
+    }
+    
+    /**
+     * Do a Delete Request
+     *
+     * @access  public  
+     * @param   string uri, filename or directory
+     * @return  &peer.http.HttpResponse response object
+     * @see     rfc://2518
+     */
+    function delete($uri) {    
+      try(); {
+        $c= &$this->getConnection($uri);
+        $response= &$c->delete();
+      } if (catch('Exception', $e)) {
+        return throw($e);
+      }
+      return $response;  
+    }
+    
   }
 ?>
