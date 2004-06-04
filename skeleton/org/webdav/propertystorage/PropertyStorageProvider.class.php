7ca65f24095d90591c0262ad55b50055929ddbb4
thekid <timm.friebe@gmail.com>
2004-06-04 00:08:35 +0000

    - Add setProperties() to interface - QA: Complete api documentation
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3517 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/org/webdav/propertystorage/PropertyStorageProvider.class.php
+++ skeleton/org/webdav/propertystorage/PropertyStorageProvider.class.php
@@ -10,10 +10,42 @@
    * @purpose  Interface
    */
   class PropertyStorageProvider extends Interface {
+
+    /**
+     * Set all properties for specific uri
+     *
+     * @access public
+     * @param  string uri The URI
+     * @param  org.webdav.WebdavProperty[] properties
+     */
+    function setProperties($uri, $properties) { }
   
-    function getProperties($uri) {}
-    function setProperty($uri, $name) {}
-    function getProperty($uri, $name) {}
-  
+    /**
+     * Read all properties for specific uri
+     *
+     * @access public
+     * @param  string uri The URI
+     * @return org.webdav.WebdavProperty[]
+     */
+    function getProperties($uri) { }
+
+    /**
+     * Sets a property for a specific URI
+     *
+     * @access public
+     * @param  string uri The URI
+     * @param  org.webdav.WebdavProperty property The WebDav property (use NULL to remove property)
+     */
+    function setProperty($uri, &$property) { }
+
+    /**
+     * Retrieve property for specific URI
+     *
+     * @access public
+     * @param  string uri  The URI
+     * @param  string name The property's name
+     * @return &org.webdav.WebdavProperty
+     */
+    function &getProperty($uri, $name) { }
   }
 ?>
