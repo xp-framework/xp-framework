6a5737872be88fb6470b17e00b7add44f05457a1
thekid <timm.friebe@gmail.com>
2004-06-04 00:13:01 +0000

    - QA: Fix WS in api docs
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3518 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/org/webdav/propertystorage/PropertyStorageProvider.class.php
+++ skeleton/org/webdav/propertystorage/PropertyStorageProvider.class.php
@@ -14,37 +14,37 @@
     /**
      * Set all properties for specific uri
      *
-     * @access public
-     * @param  string uri The URI
-     * @param  org.webdav.WebdavProperty[] properties
+     * @access  public
+     * @param   string uri The URI
+     * @param   org.webdav.WebdavProperty[] properties
      */
     function setProperties($uri, $properties) { }
   
     /**
      * Read all properties for specific uri
      *
-     * @access public
-     * @param  string uri The URI
-     * @return org.webdav.WebdavProperty[]
+     * @access  public
+     * @param   string uri The URI
+     * @return  org.webdav.WebdavProperty[]
      */
     function getProperties($uri) { }
 
     /**
      * Sets a property for a specific URI
      *
-     * @access public
-     * @param  string uri The URI
-     * @param  org.webdav.WebdavProperty property The WebDav property (use NULL to remove property)
+     * @access  public
+     * @param   string uri The URI
+     * @param   &org.webdav.WebdavProperty property The WebDav property (use NULL to remove property)
      */
     function setProperty($uri, &$property) { }
 
     /**
      * Retrieve property for specific URI
      *
-     * @access public
-     * @param  string uri  The URI
-     * @param  string name The property's name
-     * @return &org.webdav.WebdavProperty
+     * @access  public
+     * @param   string uri  The URI
+     * @param   string name The property's name
+     * @return  &org.webdav.WebdavProperty
      */
     function &getProperty($uri, $name) { }
   }
