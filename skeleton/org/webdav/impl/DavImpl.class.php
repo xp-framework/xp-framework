d9eb79f043fa3a9e4b76ee1fc15fbf5db27c9386
thekid <timm.friebe@gmail.com>
2004-06-04 00:20:16 +0000

    - QA: WS/CS fixes - QA: Supply fully qualified name for @throws clause - Remove unused code (which would have generated fatal errors if executed) # PLEASE DO NOT COMMIT COMMENTED CODE! THAT IS WHAT CVS IS FOR!
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3520 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/org/webdav/impl/DavImpl.class.php
+++ skeleton/org/webdav/impl/DavImpl.class.php
@@ -10,7 +10,7 @@
     'org.webdav.OperationNotAllowedException',
     'org.webdav.WebdavObject',
     'org.webdav.util.WebdavBool'
-    );
+  );
   
   define('WEBDAV_IMPL_PROPFIND',    0x0001);
   define('WEBDAV_IMPL_PROPPATCH',   0x0002);
@@ -49,46 +49,32 @@
       $p= preg_replace('#//#','/', $p);
       $p= preg_replace('#/$#','', $p);
       return $p;
-      
     }
 
     /**
-     * Encode the parts of a path (e.g. "/Test Folder/file.txt" -> "/Text%20File/file.txt")
+     * Encode the parts of a path 
+     *
+     * Example:
+     * <pre>
+     *   "/Test Folder/file.txt" -> "/Text%20File/file.txt"
+     * </pre>
+     *
+     * The optional parameter davDisplaynameStyle indicates that the path 
+     * should be transformed for display.
      *
      * @access  private
-     * @param   string path                The path
-     * @param   bool   davDisplaynameStyle Flag which indicates that the path should be transformed for display
+     * @param   string path The path
+     * @param   bool davDisplaynameStyle default FALSE
      * @return  string
      */
-    function _urlencodePath($path, $davDisplaynameStyle= 0){
+    function _urlencodePath($path, $davDisplaynameStyle= FALSE){
       $p= explode('\/', $this->_normalizePath($path));
       $ret= '';
-      for ($t= 0; $t<sizeof($p); $t++){
-        if (!empty($p[$t]))
-          $ret.= '/'.rawurlencode($p[$t]);
+      for ($t= 0; $t < sizeof($p); $t++){
+        if (!empty($p[$t])) $ret.= '/'.rawurlencode($p[$t]);
       }
 
-      if ($davDisplaynameStyle)
-        return substr($ret,1);  // ohne fuehrendes /
-      return $ret;
-
-      // windows Korrektur: weils alte ME kein echtes urldecode kann
-      $winpath= '';
-      $tok= strtok($ret, '%');
-      $this->c->debug('TOKENIZE ', $tok);
-      while (!empty($tok)) {
-        $asc= hexdec(substr($tok,0,2));
-        if ($asc<60 and $asc>35 or 
-          $asc>65 and $asc< 127 and $asc != 92 )
-          $winpath.=chr($asc).substr($tok,2);
-        else 
-          $winpath.='%'.$tok;
-        $this->c->debug('TOKENIZE ', $tok,"Winpath", $winpath);
-        $tok= strtok('%');
-      }
-      $winpath= substr($winpath,1);
-      $this->c->debug('TOKENIZE Done', $winpath);
-      return $winpath;
+      return substr($ret, (int)$davDisplaynameStyle);
     }
 
     /**
@@ -99,7 +85,7 @@
      * @param   string destination
      * @param   bool overwrite
      * @return  bool created
-     * @throws  MethodNotImplementedException
+     * @throws  lang.MethodNotImplementedException
      */
     function &move($filename, $destination, $overwrite) {
       return throw(new MethodNotImplementedException($this->getName().'::move not implemented'));
@@ -113,7 +99,7 @@
      * @param   string destination
      * @param   bool overwrite
      * @return  bool created
-     * @throws  MethodNotImplementedException
+     * @throws  lang.MethodNotImplementedException
      */
     function &copy($filename, $destination, $overwrite) {
       return throw(new MethodNotImplementedException($this->getName().'::copy not implemented'));
@@ -125,7 +111,7 @@
      * @access  abstract
      * @param   string colname
      * @return  bool success
-     * @throws  MethodNotImplementedException
+     * @throws  lang.MethodNotImplementedException
      */
     function &mkcol($colname) {
       return throw(new MethodNotImplementedException($this->getName().'::mkcol not implemented'));
@@ -137,7 +123,7 @@
      * @access  abstract
      * @param   string filename
      * @return  bool success
-     * @throws  MethodNotImplementedException
+     * @throws  lang.MethodNotImplementedException
      */
     function &delete($filename) {
       return throw(new MethodNotImplementedException($this->getName().'::delete not implemented'));
@@ -150,7 +136,7 @@
      * @param   string filename
      * @param   &string data
      * @return  bool new
-     * @throws  MethodNotImplementedException
+     * @throws  lang.MethodNotImplementedException
      */
     function &put($filename, &$data) {
       return throw(new MethodNotImplementedException($this->getName().'::put not implemented'));
@@ -162,7 +148,7 @@
      * @access  abstract
      * @param   string filename
      * @return  string &org.webdav.WebdavObject
-     * @throws  MethodNotImplementedException
+     * @throws  lang.MethodNotImplementedException
      */
     function &get($filename) {
       return throw(new MethodNotImplementedException($this->getName().'::get not implemented'));
@@ -175,7 +161,7 @@
      * @param   &org.webdav.xml.WebdavPropFindRequest request
      * @param   &org.webdav.xml.WebdavMultistatus response
      * @return  &org.webdav.xml.WebdavMultistatus response
-     * @throws  MethodNotImplementedException
+     * @throws  lang.MethodNotImplementedException
      */
     function &propfind(&$request, &$response) { 
       return throw(new MethodNotImplementedException($this->getName().'::propfind not implemented'));
@@ -186,11 +172,10 @@
      *
      * @access  abstract
      * @param   &org.webdav.xml.WebdavPropPatcRequest request
-     * @throws  MethodNotImplementedException
+     * @throws  lang.MethodNotImplementedException
      */
     function &proppatch(&$request) { 
       return throw(new MethodNotImplementedException($this->getName().'::proppatch not implemented'));
     }
-  
   }
 ?>
