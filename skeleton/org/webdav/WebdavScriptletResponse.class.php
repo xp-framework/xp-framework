c98ab14f2f55b20ec3b9a6d59f4e431464fc6fb9
ohinckel <juniorchicken@gmx.de>
2004-06-09 06:07:55 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3591 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/org/webdav/WebdavScriptletResponse.class.php
@@ -0,0 +1,83 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+ 
+  uses(
+    'xml.Tree',
+    'scriptlet.HttpScriptletResponse'
+  );
+
+  /**
+   * Webdav scriptlet request
+   *
+   * @purpose  Request object
+   */
+  class WebdavScriptletResponse extends HttpScriptletResponse {
+  
+    var
+      $status= HTTP_OK,
+      $tree=   NULL;
+    
+    /**
+     * Sets the root node for the response
+     *
+     * @access public
+     * @param  xml.Node node The node
+     */
+    function setRootNode(&$node) {
+      if ($this->tree === NULL) {
+        $this->tree= new Tree();
+        $this->tree->setEncoding('UTF-8');
+      }
+      $this->tree->root= &$node;
+    }
+    
+    /**
+     * Adds a child to the root node of the tree
+     *
+     * @access protected
+     * @param  xml.Node  node The node
+     * @return xml.Node
+     */
+    function &addChild(&$node) {
+      return $this->tree->addChild($node);
+    }
+    
+    /**
+     * Finalize the response (setting status code, adding XML data to
+     * response body, ...)
+     *
+     * @access public
+     */
+    function finalize() {
+      if ($this->tree !== NULL) {
+        $this->setHeader('Content-Type', 'text/xml, charset="'.$this->tree->getEncoding().'"');
+        $this->setContent($body= $this->tree->getDeclaration()."\n".$this->tree->getSource(0));
+        $this->setHeader('Content-length', strlen($body));
+      }
+      $this->setStatus($this->status);
+    }
+  
+    /**
+     * Encode the parts of a path 
+     *
+     * Example:
+     * <pre>
+     *   "/Test Folder/file.txt" -> "/Test%20Folder/file.txt"
+     * </pre>
+     *
+     * @access  private
+     * @param   string path The path
+     * @return  string
+     * @see org.webdav.WebdavScriptletRequest#decodePath
+     */
+    function encodePath($path) {
+      $parts = explode('/', $path);
+      for ($i = 0; $i < sizeof($parts); $i++) $parts[$i]= rawurlencode($parts[$i]);
+      return implode('/', $parts);
+    }
+    
+  }
+?>
