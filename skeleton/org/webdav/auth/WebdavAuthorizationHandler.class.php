29613bd8ef49593554b110ebdd0b839851b2be2b
Christian Lang <clang@developer.xp-framework.net>
2004-06-18 10:42:36 +0000

    - add possibility to check permissions by specified handler
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3664 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/org/webdav/auth/WebdavAuthorizationHandler.class.php
+++ skeleton/org/webdav/auth/WebdavAuthorizationHandler.class.php
@@ -12,16 +12,18 @@
   class WebdavAuthorizationHandler extends Object {
   
     /**
-     * Checks if the user is authorized to do something. Always returns
-     * TRUE in this default implementation.
+     * Checks if the user is authorized to do something. 
      *
      * @access  public
-     * @param   org.webdav.WebdavScriptletRequest request The request
-     * @param   org.webdav.auth.WebdavUser user The Webdav user
+     * @param   class handler
+     * @param   string uri of actual directory
+     * @param   string username
      * @return  bool
      */
-    function isAuthorized(&$request, $user) {
-      return TRUE;
+    function isAuthorized($handler= NULL, $uri, $username) {
+      // Always return TRUE if no handler is specified
+      if ($handler === NULL) return TRUE;
+      return $handler->hasPermission($uri, $username);
     }
   }
 ?>
