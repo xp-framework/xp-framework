c611216c9843bb0236623560e761c686b634c3f3
kiesel <alex@kiesel.name>
2004-06-23 00:17:36 +0000

    - initial revision
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3705 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/net/xp-framework/scriptlet/interop/state/DetailsState.class.php
@@ -0,0 +1,56 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses(
+    'scriptlet.xml.workflow.AbstractState',
+    'io.File',
+    'io.FileUtil'
+  );
+
+  /**
+   * Handles /xml/overview
+   *
+   * @purpose  Overview state
+   */
+  class OverviewState extends AbstractState {
+
+    /**
+     * Process this state
+     *
+     * @access  public
+     * @param   &scriptlet.xml.workflow.WorkflowScriptletRequest request
+     * @param   &scriptlet.xml.XMLScriptletResponse response
+     */
+    function process(&$request, &$response) {
+
+      $service= NULL;
+      $method=  NULL;
+      $type=    NULL;
+      
+      
+
+      try(); {
+
+        // Open log xml
+        $f= &new File($request->getEnvValue('DOCUMENT_ROOT').'/../log/servicetests.xml');
+        if (!(
+          $f->exists() &&
+          $tree= &Tree::fromString(FileUtil::getContents($f))
+        )) {
+          
+          $response->addFormError($this->getClassName(), 'not-available', 'servicetests');
+          return;
+        }
+      } if (catch ('XMLFormatException', $e)) {
+        $response->addFormError($this->getClassName(),'not-well-formed', 'servicetests');
+        return;
+      }
+      
+      // Append result overview to the result tree
+      $response->addFormResult($tree->root);
+    }
+  }
+?>
