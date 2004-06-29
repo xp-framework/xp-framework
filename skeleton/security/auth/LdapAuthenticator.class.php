1ae32a4540ebe91fd773ece63ec4fd10e55285cd
thekid <timm.friebe@gmail.com>
2004-06-29 10:46:16 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3722 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/security/auth/LdapAuthenticator.class.php
@@ -0,0 +1,56 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses(
+    'peer.ldap.LDAPClient',
+    'security.crypto.UnixCrypt'
+  );
+
+  /**
+   * Autenticates users against LDAP
+   *
+   * @purpose  Authenticator
+   */
+  class LdapAuthenticator extends Object {
+    var
+      $lc     = NULL,
+      $basedn = '';
+
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   &peer.ldap.LDAPClient
+     */
+    function __construct(&$lc, $basedn) {
+      $this->lc= &$lc;
+      $this->basedn= $basedn;
+    }
+  
+    /**
+     * Authenticate a user
+     *
+     * @access  public
+     * @param   string user
+     * @param   string pass
+     * @return  bool
+     */
+    function authenticate($user, $pass) {
+      try(); {
+        $r= $this->lc->search($this->basedn, '(uid='.$user.')');
+      } if (catch('LDAPException', $e)) {
+        return throw(new AuthenticatorException($e->getMessage(), $e));
+      }
+      
+      // Check return, we must find a distinct user
+      if (1 != $r->numEntries()) return FALSE;
+      
+      $entry= $r->getNextEntry();
+      return UnixCrypt::matches($entry->getAttribute('userpassword', 0), $pass);
+    }
+    
+  } implements(__FILE__, 'security.auth.Authenticator');
+?>
