d92390c4dbe50768f23ceceea4b91454c0f247d3
kiesel <alex@kiesel.name>
2004-07-08 13:53:53 +0000

    - fix illegal char detection for valid strings # *sigh*
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3807 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/xml/Node.class.php
+++ skeleton/xml/Node.class.php
@@ -144,8 +144,10 @@
       // found, strtok returns the unchanged string. Otherwise the string up to the 
       // illegal character will be returned (or - if the char is the first in the 
       // string, the string excluding the first char will be returned).
-      if (strlen($content) > strlen($p= strtok(' '.$content, XML_ILLEGAL_CHARS) - 1)) {
-        return throw (new XMLFormatException('Content contains illegal character at position '.strlen($p)));
+      if (strlen($content) > ($p= (strlen(strtok(' '.$content, XML_ILLEGAL_CHARS)) - 1))) {
+        return throw (new XMLFormatException(
+          'Content contains illegal character at position '.$p. ' / chr('.ord($content{$p}).')'
+        ));
       }
       
       $this->content= $content;
