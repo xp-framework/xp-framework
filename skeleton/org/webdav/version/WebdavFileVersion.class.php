1aaaeb7ed40ef6effa6246dd61ca69b113d70477
Christian Lang <clang@developer.xp-framework.net>
2004-07-08 16:04:52 +0000

    - Initial Release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3812 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/org/webdav/version/WebdavFileVersion.class.php
@@ -0,0 +1,193 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+ 
+  /**
+   * Webdav lock object
+   *
+   * @see      org.webdav.impl.DavFileImpl
+   * @purpose  Represent a Version of a File
+   */
+  class WebdavFileVersion extends Object {
+    var
+      $versnr=        NULL,
+      $href=          NULL,
+      $versionname=   NULL,
+      $creatorname=   NULL,
+      $contentlength= NULL,
+      $lastmodified=  NULL;
+   
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   string filename
+     */   
+    function __construct($filename) {
+      $this->filename= $filename;
+    }
+    
+    /**
+     * Set the Filename
+     *
+     * @access  public
+     * @param   string filename
+     */
+    function setFilename($filename) {
+      $this->filename= $filename;
+    }
+    
+    /**
+     * Get the Filename
+     *
+     * @access  public
+     * @return  string filename
+     */
+    function getFilename() {
+      return $this->filename;
+    }
+    
+    /**
+     * Set the versionnumber
+     *
+     * @access  public
+     * @param   mixed versnr
+     */
+    function setVersionNumber($versnr) {
+      $this->versnr= $versnr;
+    }
+    
+    /**
+     * Get the versionnumber
+     *
+     * @access  public
+     * @return  mixed versnr
+     */
+    function getVersionNumber() {
+      return $this->versnr;
+    }
+    
+    /**
+     * Set the Href
+     *
+     * @access  public
+     * @param   string href
+     */
+    function setHref($href) {
+      $this->href= $href;
+    }
+    
+    /**
+     * Get the Href
+     *
+     * @access  public
+     * @return  string href
+     */
+    function getHref() {
+      return $this->href;
+    }
+    
+    /**
+     * Set the versionname (e.g. test_1.0.txt)
+     *
+     * @access  public
+     * @param   string versionname
+     */
+    function setVersionName($versname) {
+      $this->versionname= $versname;
+    }
+    
+    /**
+     * Get the versionname
+     *
+     * @access  public
+     * @return  string versionname
+     */
+    function getVersionName() {
+      return $this->versionname;
+    }
+    
+    /**
+     * Set the name of the creator
+     *
+     * @access  public
+     * @param   string creator, default NULL
+     */
+    function setCreatorName($creator= NULL) {
+      $this->creatorname= $creator;
+    }
+    
+    /**
+     * Get the name of the creator
+     *
+     * @access  public
+     * @return  string creatorname
+     */
+    function getCreatorName() {
+      return $this->creatorname;
+    }
+    
+    /**
+     * Set the conentlength
+     *
+     * @access  public
+     * @param   int length
+     */
+    function setContentLength($length) {
+      $this->contentlength= $length;
+    }
+    
+    /**
+     * Get the contentlength
+     *
+     * @access  public
+     * @return  int contentlength
+     */
+    function getContentLength() {
+      return $this->contentlength;
+    }
+    
+    /**
+     * Set date of last modification
+     *
+     * @access  public
+     * @param   &util.Date
+     */
+    function setLastModified(&$date) {
+      $this->lastmodified= &$date;
+    }
+    
+    /**
+     * Get the date of last modification
+     *
+     * @access  public
+     * @return  &util.Date
+     */
+    function &getLastModified() {
+      return $this->lastmodified;
+    }
+    
+    /**
+     * Set the location (e.g. /dav/versions/test_1.0.txt )
+     *
+     * @access  public
+     * @param   string location
+     */
+    function setLocation($loc) {
+      $this->location= $loc;
+    }
+    
+    /**
+     * Get the location
+     *
+     * @access  public
+     * @return  string location
+     */
+    function getLocation() {
+      return $this->location;
+    }
+     
+  }
+?>
---
+++ skeleton/org/webdav/version/WebdavVersionsContainer.class.php
@@ -0,0 +1,48 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Container which collects all versions of file
+   *
+   * @see      org.webdav.DavImpl#VersionControl
+   * @purpose  Container of versions
+   */
+  class WebdavVersionsContainer extends Object {
+    var
+      $versions= array();
+  
+    /**
+     * Construct
+     *
+     * @access  public
+     * @param   org.webdav.version.Webdav*Version
+     */
+    function __construct($version= NULL) {
+      if ($version !== NULL) $this->addVersion($version);
+    }
+    
+    /**
+     * Add a version to the container
+     *
+     * @access  public
+     * @param   org.webdav.version.Webdav*Version
+     */
+    function addVersion(&$version) {
+      $this->versions[]= $version;
+    }
+    
+    /**
+     * Get all versions
+     *
+     * @access  public
+     * @return  array versions
+     */
+    function getVersions() {
+      return $this->versions;
+    }
+  
+  }
+?>
