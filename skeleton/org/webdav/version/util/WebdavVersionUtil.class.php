57512ab60be3f97f63592efb62fe4d84926dcfd3
Christian Lang <clang@developer.xp-framework.net>
2004-07-19 15:52:32 +0000

    - Add Directory to version object
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3879 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/org/webdav/version/util/WebdavVersionUtil.class.php
+++ skeleton/org/webdav/version/util/WebdavVersionUtil.class.php
@@ -22,17 +22,19 @@
      * @return  &org.webdav.version.Webdav*Version
      */
     function &getNextVersion(&$actVersion, &$file) {
-    
       // Load same type of version as before
       $obj= &XPClass::forName(XP::typeOf($actVersion));
       
       // Get name of file, without extension    
       $fname= basename($actVersion->getFilename(), '.'.$file->getExtension());
-    
+      
+      // Get name of directory
+      $dir= substr(dirname($actVersion->getHref()), 12);
+      
       // Create new version object 
       with ($version= &$obj->newInstance($actVersion->getFilename())); {
         $version->setVersionNumber($actVersion->getVersionNumber()+0.1);
-        $version->setHref('../versions/'.$fname.'['.$version->getVersionNumber().'].'.$file->getExtension());
+        $version->setHref('../versions/'.$dir.'/'.$fname.'['.$version->getVersionNumber().'].'.$file->getExtension());
         $version->setVersionName($fname.'['.$version->getVersionNumber().'].'.$file->getExtension());
         $version->setContentLength($file->size());
         $version->setLastModified(Date::now());
