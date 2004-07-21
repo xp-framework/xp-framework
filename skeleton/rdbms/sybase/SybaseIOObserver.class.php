2f0adb662625512935c62f13e2c3a13bff477b45
kiesel <alex@kiesel.name>
2004-07-21 11:21:21 +0000

    - parse the sybase server messages based on its message number, store the   results if needed. At the end of a query print out diagnostic messages   about the query IO performance.   These diagnostics include:   + scan count for each table in the query   + logical and physical reads for each table (summarized)     (especially apf stats are ignored due to their little information)   + overall scan count, reads for the whole query   + average scan count, reads for the whole query if more than one row     has been retrieved
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3888 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/rdbms/sybase/SybaseIOObserver.class.php
+++ skeleton/rdbms/sybase/SybaseIOObserver.class.php
@@ -39,7 +39,10 @@
 
       // Filter 'IO statistics'-messages by their msgnumber
       if (in_array($msgnumber, array(3614, 3615))) {
-        $this->messages[]= rtrim($text);
+        $this->messages[]= array(
+          'msgnumber' => $msgnumber,
+          'text'      => rtrim($text)
+        );
       }
       
       // Indicate we did not process the message
@@ -85,8 +88,6 @@
      * @param   &mixed dbevent
      */
     function onConnect(&$obs, &$arg) {
-      $this->cat->debug(__FUNCTION__);
-      
       if (0 <= version_compare(phpversion(), '4.3.5')) {
         ini_set('sybct.min_server_severity', 0);
         sybase_set_message_handler(array(&$this, '_msghandler'), $obs->handle);
@@ -95,6 +96,8 @@
       }
       
       sybase_query('set statistics io on', $obs->handle);
+      
+      // Reset query- and message-cache
       $this->queries= $this->messages= array();
     }
     
@@ -106,8 +109,8 @@
      * @param   &mixed dbevent
      */
     function onQuery(&$obs, &$arg) {
-      $this->cat->debug(__FUNCTION__);
       
+      // Add query to cache
       $this->queries[]= $arg->getArgument();
     }
     
@@ -119,14 +122,49 @@
      * @param   &mixed dbevent
      */
     function onQueryEnd(&$obs, &$arg) {
-      $this->cat->info($this->getClassName(), '::', __FUNCTION__, 'Query was:', $this->queries);
+      $this->cat->info($this->getClassName().'::onQueryEnd() Query was:', (sizeof($this->queries) == 1 ? $this->queries[0] : $this->queries));
+      $result= &$arg->getArgument();
       
-      foreach ($this->messages as $msg) {
-        $this->cat->info('Measured IO: '.$msg);
+      $sc= 0; $reads= 0;
+      foreach (array_keys($this->messages) as $idx) {
+        $msg= &$this->messages[$idx];
+        switch($msg['msgnumber']) {
+          case 3615: {
+            $split= sscanf($msg['text'], 'Table: %s scan count %d, logical reads: (regular=%d apf=%d total=%d), physical reads: (regular=%d apf=%d total=%d), apf IOs used=%d');
+            $this->cat->infof('IO(%s): scan count= %d, logical= %d, physical= %d',
+              $split[0],
+              $split[1],
+              $split[4],
+              $split[7]
+            );
+            
+            // Add overall statistics
+            $sc+= $split[1];
+            $reads+= $split[4] + $split[7];
+            
+            break;
+          }
+          
+          case 3614: {
+            $split= sscanf($msg['text'], 'Total writes for this command: %d');
+            if ($split[0] > 0) $this->cat->infof('Write-IO: %d', $split[0]);
+            break;
+          }
+        }
       }
       
+      $this->cat->infof('Overall stats for query: scan count= %d, reads= %d', $sc, $reads);
+      
+      // Retrieve number of rows returned, then calculate average cost of row
+      if (1 < ($nrows= sybase_num_rows($result->handle))) {
+        $this->cat->infof('Average stats for query: scan count= %0.02f, reads= %0.02f (%d lines)',
+          $sc / $nrows,
+          $reads / $nrows,
+          $nrows
+        );
+      }
+
       $this->queries= $this->messages= array();
     }
-
   } implements (__FILE__, 'rdbms.DBObserver');
 ?>
