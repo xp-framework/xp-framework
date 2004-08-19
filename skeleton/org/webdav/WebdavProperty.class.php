3ae33dedffe5da982e5486857089b40c3898069f
Christian Lang <clang@developer.xp-framework.net>
2004-08-19 10:24:57 +0000

    - Switch date format if property is the getlastmodified property
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3906 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/org/webdav/WebdavProperty.class.php
+++ skeleton/org/webdav/WebdavProperty.class.php
@@ -154,7 +154,7 @@
         case 'util.Date':
           $this->attributes['xmlns:b']= 'urn:uuid:c2f41010-65b3-11d1-a29f-00aa00c14882/';
           $this->attributes['b:dt']= 'dateTime.rfc1123';
-          return $this->value->toString('D, j M Y H:m:s \G\M\T');
+          return $this->value->toString($this->name == 'getlastmodified' ? 'D, j M Y H:m:s \G\M\T' : 'Y-m-d\TH:m:s\Z');
 
         default:
           return (string)$this->value;
