4d422a44d9844b9c8f1d046ad1e8a834774acd2d
thekid <timm.friebe@gmail.com>
2004-08-31 22:06:27 +0000

    - Add debugging if --debug is passed
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@3973 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- doc/examples/createrdf.php
+++ doc/examples/createrdf.php
@@ -7,19 +7,33 @@
   xp::sapi('cli');
   uses(
     'xml.rdf.RDFNewsFeed',
+    'util.log.Logger',
+    'util.log.LogObserver',
+    'util.log.ConsoleAppender',
     'util.Properties',
     'rdbms.DriverManager',
     'rdbms.ConnectionManager',
     'net.xp-framework.db.caffeine.XPNews'
   );
   
+  // {{{ main
   $p= &new ParamString();
   
+  // Set up connection manager
   $cm= &ConnectionManager::getInstance();
-  $cm->register(DriverManager::getConnection(
+  $conn= &$cm->register(DriverManager::getConnection(
     'sybase://'.$p->value(1).'/CAFFEINE?autoconnect=1'
   ), 'caffeine');
   
+  // Enable debugging if requested
+  if ($p->exists('debug')) {
+    $l= &Logger::getInstance();
+    $cat= &$l->getCategory('sql');
+    $cat->addAppender(new ConsoleAppender());
+    $conn->addObserver(LogObserver::instanceFor('sql'));
+  }
+
+  // Create RDF object
   $rdf= &new RDFNewsFeed();
   $rdf->setChannel(
     'XP News', 
@@ -32,13 +46,15 @@
     'http://xp-framework.net/copyright.html'
   );
   
+  // Get news entries ordered by date, maximum 20
   try(); {
-    $news= &XPNews::getByDateOrdered();
+    $news= &XPNews::getByDateOrdered(20);
   } if (catch('Exception', $e)) {
     $e->printStackTrace();
     exit(-1);
   }
   
+  // Add news entries to RDF
   for ($i= 0, $s= sizeof($news); $i < $s; $i++) {
     $rdf->addItem(
       $news[$i]->getCaption(),
@@ -48,5 +64,7 @@
     );
   }
   
+  // Output XML
   Console::write($rdf->getSource($indent= INDENT_DEFAULT));
+  // }}}
 ?>
