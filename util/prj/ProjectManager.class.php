40778ba48263c4d392934fea41f962c03284c901
kiesel <alex@kiesel.name>
2004-10-03 14:06:05 +0000

    - use ParserManager in favour of PHPParser - introduce a reference model to associate one parser element   with a node and other way round. # again, GTK1 seems to be annoyingly broken for CTree's.
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@4081 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- util/prj/ProjectManager.class.php
+++ util/prj/ProjectManager.class.php
@@ -11,7 +11,7 @@
     'util.log.FileAppender',
     'util.Properties',
     'util.cmd.ParamString',
-    'text.PHPParser',
+    'ParserManager',
     'lang.System'
   );
   
@@ -24,6 +24,10 @@
       $menu=        NULL,
       $pixmap=      array(),
       $files=       array();
+    
+    var
+      $hash=        array(),
+      $refdata=     array();
       
     /**
      * Constructor
@@ -38,7 +42,106 @@
       $this->prop= &new Properties (getenv ('HOME').'/.prj.ini');
       parent::__construct($p, dirname(__FILE__).'/prj.glade', 'mainwindow');
     }
+    
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
+    function _getRef($idx, $cidx= NULL, $fidx= NULL) {
+      $arr= array();
+      $arr[]= 'f='.$this->files[$idx]->filename;
+      if (NULL !== $cidx) {
+        $arr[]= 'c='.$this->files[$idx]->classes[$cidx]->name;
+        if (NULL !== $fidx) $arr[]= 'fn='.$this->files[$idx]->classes[$cidx]->functions[$fidx]->name;
+      } elseif (NULL !== $fidx) {
+        $arr[]= 'fn='.$this->files[$idx]->functions[$fidx]->name;
+      }
+      
+      return implode(PATH_SEPARATOR, $arr);
+    }
+    
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
+    function _nodeExists($ref) {
+      return isset ($this->hash[$ref]);
+    }
+    
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
+    function _getNode($ref) {
+      return $this->hash[$ref];
+    }
+    
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
+    function _setRefInfo($ref, $info) {
+      $this->refdata[$ref]= $info;
+    }
+    
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
+    function _getRefInfo($ref) {
+      return $this->refdata[$ref];
+    }    
+    
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
+    function _getObjectByRef($ref) {
+      if (!isset($this->revhash[$ref]))
+        return throw(new IllegalArgumentException('"'.$ref.'" is not a valid reference.'));
+        
+      list($idx, $cidx, $fidx)= $this->revhash[$ref];
 
+      if (isset($fidx) && isset($cidx)) {
+        return $this->files[$idx]->classes[$cidx]->functions[$fidx];
+      } elseif (isset($cidx)) {
+        return $this->files[$idx]->classes[$cidx];
+      } elseif (isset($fidx)) {
+        return $this->files[$idx]->functions[$fidx];
+      } else {
+        return $this->files[$idx];
+      }
+    }
+    
+    /**
+     * (Insert method's description here)
+     *
+     * @access  
+     * @param   
+     * @return  
+     */
+    function _getParentRef($ref) {
+      return substr($ref, 0, strrpos($ref, ':'));
+    }    
+    
     /**
      * Initializes all windows
      *
@@ -79,7 +182,7 @@
         } if (catch ('Exception', $e)) { 
           break; 
         }
-        if ('-' !== $a{0}) $this->_recursiveAddFile (new PHPParser ($a));
+        if ('-' !== $a{0}) $this->_recursiveAddFile (new ParserManager($a));
         $this->updateList();
       }
       
@@ -89,6 +192,7 @@
     }
     
     function onAutoUpdate() {
+    
       // Automatically reload all data
       $this->reparse();
       return TRUE;
@@ -97,6 +201,8 @@
     function onTreeSelectRow(&$widget, $row, &$data, &$event) {
       $node= &$this->tree->node_nth ($row);
       $this->_selectedNode= &$node;
+      
+      $this->log->debug('Selected ref:', $this->tree->node_get_row_data($this->_selectedNode));
     }
     
     function onTreeUnselectRow(&$widget, $row, &$data, &$event) {
@@ -105,6 +211,7 @@
     }
     
     function onTreeClick(&$clist, &$event) {
+    
       // Check for right-click
       if (3 == $event->button)
         return $this->onTreeRightClick($click, $event);
@@ -125,7 +232,7 @@
         $this->menu->addSeparator();
         $this->menu->addMenuItem ('Open', array (&$this, 'onFileOpenCtx'));
       }
-      
+
       $this->menu->show(MENU_WANT_LEFTCLICK);
     }
     
@@ -136,7 +243,7 @@
           $this->statusbar->push (1, 'Reparsing '.$this->files[$idx]->filename);
           $this->files[$idx]->parse();
           $this->statusbar->push (1, 'Reparsed '.$this->files[$idx]->filename.' at '.date ('H:i:s'));
-          
+      
           $reparse= TRUE;
         }
       }
@@ -146,6 +253,7 @@
     }
     
     function _recursiveAddFile(&$parser, $recurse= TRUE) {
+    
       // Try to prevent adding of the same files twice
       foreach (array_keys ($this->files) as $idx) {
         if ($this->files[$idx]->filename === $parser->filename)
@@ -163,7 +271,7 @@
         foreach (explode (':', ini_get ('include_path')) as $p) {
           $fn= $p.'/'.str_replace ('.', DIRECTORY_SEPARATOR, $u).'.class.php';
           if (is_file ($fn)) {
-            $this->_recursiveAddFile (new PHPParser (realpath($fn)));
+            $this->_recursiveAddFile (new ParserManager(realpath($fn)));
             break;
           }
         }
@@ -173,7 +281,7 @@
         foreach (explode (':', ini_get ('include_path')) as $p) {
           $fn= $p.'/'.$r;
           if (file_exists ($fn)) {
-            $this->_recursiveAddFile (new PHPParser (realpath($fn)));
+            $this->_recursiveAddFile (new ParserManager(realpath($fn)));
             break;
           }
         }
@@ -181,7 +289,7 @@
     }
 
     function addFile(&$parser) {
-      if (!is_a ($parser, 'PHPParser'))
+      if (!is_a ($parser, 'ParserManager'))
         return FALSE;
         
       $this->_recursiveAddFile ($parser);
@@ -190,11 +298,11 @@
       return TRUE;
     }
     
-    function &_addNode(&$parent, $data, &$nodeData, $pixmap= 'sv_session') {
+    function &_addNode(&$parent, $label, $ref, $pixmap= 'sv_session') {
       $node= &$this->tree->insert_node (
         $parent,
         NULL,
-        $data,
+        $label,
         0,
         $this->pixmap['p:'.$pixmap],
         $this->pixmap['m:'.$pixmap],
@@ -204,7 +312,8 @@
         FALSE
       );
       
-      $this->tree->node_set_row_data($node, $nodeData);
+      $this->tree->node_set_row_data($node, $ref);
+      $this->hash[$ref]= &$node;
 
       // $style= gtk::widget_get_default_style();
       $style= &new GtkStyle();
@@ -217,20 +326,22 @@
     
     function updateList() {
       $this->tree->freeze();
-      $this->tree->clear();
+      // $this->tree->clear();
       
-      $rootNode= &$this->tree->insert_node (
-        NULL,
-        NULL,
-        array ('global program space', '', ''),
-        0,
-        $this->pixmap['p:sv_session'],
-        $this->pixmap['m:sv_session'],
-        $this->pixmap['p:sv_session'],
-        $this->pixmap['m:sv_session'],
-        FALSE,
-        TRUE
-      );
+      if (empty($this->rootNode)) {
+        $this->rootNode= &$this->tree->insert_node (
+          NULL,
+          NULL,
+          array ('global program space'),
+          0,
+          $this->pixmap['p:sv_session'],
+          $this->pixmap['m:sv_session'],
+          $this->pixmap['p:sv_session'],
+          $this->pixmap['m:sv_session'],
+          FALSE,
+          TRUE
+        );
+      }
       
       foreach (array_keys ($this->files) as $idx) {
         $file= &$this->files[$idx];
@@ -238,28 +349,36 @@
         foreach (array_keys ($file->classes) as $cIdx) {
           $c= &$file->classes[$cIdx];
 
-          $node= &new StdClass();
-          $node->file= &$file;
-          $node->object= &$c;
-
-          $classNode= &$this->_addNode (
-            $rootNode,
-            array ($c->name, basename ($file->filename), $c->line),
-            $node,
-            'sv_class'
-          );
-
+          if (!$this->_nodeExists($this->_getRef($idx, $cIdx))) {
+            $classNode= &$this->_addNode (
+              $this->rootNode,
+              array ($c->name),
+              $this->_getRef($idx, $cIdx),
+              'sv_class'
+            );
+          }
+          
+          $this->_setRefInfo($this->_getRef($idx, $cIdx), array(
+            'file' => $file->filename,
+            'line' => $c->line
+          ));
+          
+          $classNode= &$this->_getNode($this->_getRef($idx, $cIdx));
           foreach (array_keys ($c->functions) as $fIdx) {
             $f= &$c->functions[$fIdx];
 
-            $node= &new StdClass();
-            $node->file= &$file;
-            $node->object= &$f;
-            
-            $funcNode= &$this->_addNode(
+            $this->_setRefInfo($this->_getRef($idx, $cIdx, $fIdx), array(
+              'file' => $file->filename,
+              'line' => $f->line
+            ));
+
+            if ($this->_nodeExists($this->_getRef($idx, $cIdx, $fIdx)))
+              continue;
+              
+            $this->_addNode(
               $classNode,
-              array ($f->name, '', $f->line),
-              $node,
+              array ($f->name),
+              $this->_getRef($idx, $cIdx, $fIdx),
               'sv_scalar'
             );
           }
@@ -272,21 +391,26 @@
         
         foreach (array_keys ($file->functions) as $fIdx) {
           $f= &$file->functions[$fIdx];
-
-          $node= &new StdClass();
-          $node->file= &$file;
-          $node->object= &$f;
           
-          $funcNode= &$this->_addNode (
-            $rootNode,
-            array ($f->name, basename ($file->filename), $f->line),
-            $node,
+          $this->_setRefInfo($this->_getRef($idx, NULL, $fIdx), array(
+            'file' => $file->filename,
+            'line' => $f->line
+          ));
+          
+          if ($this->_nodeExists($this->_getRef($idx, NULL, $fIdx)))
+            continue;
+
+          $this->_addNode (
+            $this->rootNode,
+            array ($f->name),
+            $this->_getRef($idx, NULL, $fIdx),
             'sv_private_scalar'
           );
         }
       }
+      
 
-      $this->tree->columns_autosize();
+      // $this->tree->columns_autosize();
       $this->tree->thaw();
     }
     
@@ -304,15 +428,12 @@
         'cmdline',
         'nedit -line %d %s'
       );
-        
-
-      $line= 1;
-      if (isset ($n->object))
-        $line= $n->object->line;
+      
+      $d= $this->_getRefInfo($n);
 
       $cmdLine= sprintf ($cmd,
-        $line,
-        $n->file->filename        
+        $d['line'],
+        $d['file']
       );
       
       System::exec ($cmdLine, '1>/dev/null 2>/dev/null', TRUE);
