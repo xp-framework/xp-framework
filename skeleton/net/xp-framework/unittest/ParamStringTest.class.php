dff165d8a53cf710e1702252f7d1b04077fb9235
thekid <timm.friebe@gmail.com>
2004-10-08 08:53:48 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@4102 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/net/xp-framework/unittest/ParamStringTest.class.php
@@ -0,0 +1,76 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+ 
+  uses(
+    'util.profiling.unittest.TestCase',
+    'util.cmd.ParamString'
+  );
+
+  /**
+   * Test framework code
+   *
+   * @see      xp://util.cmd.ParamString
+   * @purpose  Unit Test
+   */
+  class ParamStringTest extends TestCase {
+    
+    /**
+     * Test short option without value ("flag")
+     *
+     * @access  public
+     */
+    function testShortFlag() {
+      $p= &new ParamString(array('-k'));
+
+      $this->assertTrue($p->exists('k'));
+      $this->assertNull($p->value('k'));
+
+      delete($p);
+    }
+
+    /**
+     * Test short option with value
+     *
+     * @access  public
+     */
+    function testShortValue() {
+      $p= &new ParamString(array('-d', 'sql'));
+
+      $this->assertTrue($p->exists('d'));
+      $this->assertEquals($p->value('d'), 'sql');
+      
+      delete($p);
+    }
+
+    /**
+     * Test long option without value ("flag")
+     *
+     * @access  public
+     */
+    function testLongFlag() {
+      $p= &new ParamString(array('--verbose'));
+
+      $this->assertTrue($p->exists('verbose'));
+      $this->assertNull($p->value('verbose'));
+
+      delete($p);
+    }
+
+    /**
+     * Test Long option with value
+     *
+     * @access  public
+     */
+    function testLongValue() {
+      $p= &new ParamString(array('--level=3'));
+
+      $this->assertTrue($p->exists('level'));
+      $this->assertEquals($p->value('level'), '3');
+
+      delete($p);
+    }
+  }
+?>
