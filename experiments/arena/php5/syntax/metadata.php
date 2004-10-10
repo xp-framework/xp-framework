938a574badfe2083d0e2d8fdeadc1649ffa02a2d
thekid <timm.friebe@gmail.com>
2004-10-10 18:00:53 +0000

    - Add support for method meta data ("annotations")
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@4123 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/arena/php5/syntax/metadata.php
@@ -0,0 +1,48 @@
+<?php
+/* This file is part of the XP framework's experiments
+ *
+ * $Id$
+ */
+  
+  // {{{ class CalculatorService
+  class CalculatorService {
+
+    protected function log() {
+      for ($i= 0, $n= func_num_args(); $i < $n; $i++) {
+        var_export(func_get_arg($i));
+        echo ' ';
+      }
+    }
+
+    @webmethod 
+    public function add($a, $b) {
+      $this->log('Adding', $a, 'and', $b);
+      return $a + $b;
+    }
+
+    @webmethod 
+    public function subtract($a, $b) {
+      $this->log('Subtracting', $b, 'from', $a);
+      return $a - $b;
+    }
+    
+    @webmethod,@deprecated('Use subtract() instead') 
+    public function sub($a, $b) {
+      return $this->subtract($a, $b);
+    }
+  }
+  // }}}
+  
+  // {{{
+  $c= new ReflectionClass('CalculatorService');
+  printf("The class %s provides the following web methods:\n", $c->getName());
+  foreach ($c->getMethods() as $method) {
+    if (!$method->hasAnnotation('webmethod')) continue;
+    
+    printf('- %s(): ', $method->getName());
+    if ($method->hasAnnotation('deprecated')) {
+      echo '[DEPRECATED: ', var_export($method->getAnnotation('deprecated'), 1), ']: ';
+    }
+    var_dump($method->getAnnotations());
+  }
+?>
