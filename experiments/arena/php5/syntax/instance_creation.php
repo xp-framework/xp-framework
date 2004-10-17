0c659b3a38958469fc9a4dfa965a6ebf90c71bd2
thekid <timm.friebe@gmail.com>
2004-10-17 16:51:11 +0000

    - Implement instance creation expressions - Disallow omitting brackets in class construction statements:   new Test; is now illegal
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@4142 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/arena/php5/syntax/instance_creation.php
@@ -0,0 +1,37 @@
+<?php
+/* This file is part of the XP framework's experiments
+ *
+ * $Id$
+ */
+
+  interface Comparator {
+    public function compare($a, $b);
+  }
+  
+  class ArrayList {
+    public
+      $elements = array();
+
+    public function sort(Comparator $c) {
+      usort($this->elements, array($c, 'compare'));
+    }
+  }
+  
+  // {{{ main
+  $list= new ArrayList();
+  $list->elements[]= 1;
+  $list->elements[]= 10;
+  $list->elements[]= 8;
+  $list->elements[]= 9;
+
+  echo 'Before: ', implode(', ', $list->elements), "\n";
+  
+  $list->sort(new Comparator() {
+    public function compare($a, $b) {
+      return strnatcmp($a, $b);
+    }
+  });
+  
+  echo 'After : ', implode(', ', $list->elements), "\n";
+  // }}}
+?>
