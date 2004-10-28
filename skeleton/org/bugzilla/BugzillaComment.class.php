acbb5e08568cc71982091fbece26ab57907f42a2
Christian Lang <clang@developer.xp-framework.net>
2004-10-28 15:35:17 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@4166 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/org/bugzilla/BugzillaComment.class.php
@@ -0,0 +1,130 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+ 
+  uses(
+    'org.bugzilla.db.BugzillaLongDescs',
+    'util.Date'
+  );
+
+  /**
+   * Bugzilla comment accessor class
+   *
+   * @purpose  Simplify handling with bugzilla comments
+   */
+  class BugzillaComment extends Object {
+    var
+      $bug_id=      NULL,
+      $user_id=     NULL,
+      $comment=     NULL;
+   
+
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   int bug_id,     The ID of the bug related to this comment
+     * @param   int user_id,    The bugzilla user id
+     * @param   string comment, The comment
+     */
+    function __construct($bug_id= NULL, $user_id= NULL, $comment= NULL) {
+      $this->bug_id= $bug_id;
+      $this->user_id= $user_id;
+      $this->comment= $comment;
+    }    
+    
+    /**
+     * Set Bug_id
+     *
+     * @access  public
+     * @param   int bug_id
+     */
+    function setBug_id($bug_id) {
+      $this->bug_id= $bug_id;
+    }
+
+    /**
+     * Get Bug_id
+     *
+     * @access  public
+     * @return  int bug_id
+     */
+    function getBug_id() {
+      return $this->bug_id;
+    }
+
+    /**
+     * Set User_id
+     *
+     * @access  public
+     * @param   int user_id
+     */
+    function setUser_id($user_id) {
+      $this->user_id= $user_id;
+    }
+
+    /**
+     * Get User_id
+     *
+     * @access  public
+     * @return  int user_id
+     */
+    function getUser_id() {
+      return $this->user_id;
+    }
+
+    /**
+     * Set Comment
+     *
+     * @access  public
+     * @param   string comment
+     */
+    function setComment($comment) {
+      $this->comment= $comment;
+    }
+
+    /**
+     * Get Comment
+     *
+     * @access  public
+     * @return  string
+     */
+    function getComment() {
+      return $this->comment;
+    }
+    
+    /**
+     * Add a comment
+     *
+     * @access  public
+     * @throws  IllegalArgumentException
+     * @throws  SQLException
+     * @return  bool
+     */
+    function add() {
+
+      // Check if all needed params are given
+      if (
+        empty($this->bug_id) or
+        empty($this->user_id) or
+        empty($this->comment)
+      ) return throw(new IllegalArgumentException('Too few arguments given'));
+
+      try(); {
+        with ($desc= &new BugzillaLongDescs()); {
+          $desc->setBug_id($this->bug_id);
+          $desc->setBug_when(Date::now());
+          $desc->setThetext($this->comment);
+          $desc->setWho($this->user_id);
+        }
+        $desc->insert();
+        
+      } if (catch('SQLException', $e)) {
+        return throw($e);
+      }
+      return TRUE;
+    }
+  }
+?>
---
+++ skeleton/org/bugzilla/db/BugzillaMySQLConnection.class.php
@@ -0,0 +1,60 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses ('rdbms.mysql.MySQLConnection');
+
+  /**
+   * Bugzilla MySQL connection. Automatically handles shadow db
+   * writing.
+   *
+   * @see      xp://rdbms.mysql.MySQLConnection
+   * @purpose  Specialized connection class
+   */
+  class BugzillaMySQLConnection extends MySQLConnection {
+    var
+      $shadow   = TRUE;
+
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   &rdbms.DSN dsn
+     */
+    function __construct(&$dsn) { 
+      parent::__construct($dsn); 
+            
+      $this->shadow= $this->dsn->getValue('shadow', FALSE);
+    }
+
+    /**
+     * Execute any statement
+     *
+     * @access  public
+     * @param   mixed* args
+     * @return  &rdbms.mysql.MySQLResultSet or FALSE to indicate failure
+     * @throws  rdbms.SQLException
+     */
+    function &query() {
+      $args= func_get_args();
+      $sql= $this->_prepare($args);
+
+      try(); {
+        $res= &parent::query($sql);
+      } if (catch('SQLException', $e)) {
+        return throw($e);
+      }
+      
+      if (TRUE !== $res || !$this->shadow) return $res;
+      
+      // This was an SQL update, insert or delete, or: something that does
+      // not return a resultset. Write it into the shadow log
+      mysql_query(
+        $this->_prepare(array('insert into shadowlog (command) values (%s)', $sql)),
+        $this->handle
+      );
+    }
+  }
+?>
