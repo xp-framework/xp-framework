b36fd84bfda9ff29874abc522499b34203b115ba
thekid <timm.friebe@gmail.com>
2004-11-07 18:45:46 +0000

    - Test comparisons - Beautify output
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@4208 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/php5/operator_overloading/bigdecimal.php
+++ experiments/arena/php5/operator_overloading/bigdecimal.php
@@ -40,6 +40,10 @@
     public static operator % (BigDecimal $d1, BigDecimal $d2) {
       return new BigDecimal(bcmod($d1->value, $d2->value));
     }
+
+    public static operator __compare(BigDecimal $d1, BigDecimal $d2) {
+      return bccomp($d1->value, $d2->value);
+    }
     
     public function toString() {
       return $this->value;
@@ -53,6 +57,16 @@
   // Create some complex numbers
   $d1= new BigDecimal('92233720368547758075');
   $d2= new BigDecimal('192233726199758075');
+  printf("First big decimal:                %s\n", $d1->toString());
+  printf("Second big decimal:               %s\n", $d2->toString());
+  
+  // Test comparions
+  echo "------------------------------------------------------------\n";
+  printf("Equal (first == second):          %s\n", var_export($d1 == $d2, 1));
+  printf("Smaller (first < second):         %s\n", var_export($d1 < $d2, 1));
+  printf("Larger (second > first):          %s\n", var_export($d1 > $d2, 1));
+  printf("Comparison (second <=> first):    %s\n", var_export($d1 <=> $d2, 1));
+  
   $add= $d1 + $d2;
   $sub= $d1 - $d2;
   $mul= $d1 * $d2;
@@ -60,8 +74,7 @@
   $mod= $d1 % $d2;
 
   // Test addition, subtraction, multiplication, division and modulus
-  printf("First big decimal:                %s\n", $d1->toString());
-  printf("Second big decimal:               %s\n", $d2->toString());
+  echo "------------------------------------------------------------\n";
   printf("Addition (first + second):        %s\n", $add->toString());
   printf("Subtraction (first - second):     %s\n", $sub->toString());
   printf("Multiplication (first * second):  %s\n", $mul->toString());
@@ -69,12 +82,14 @@
   printf("Modulus (first %% second):         %s\n", $mod->toString());
 
   // Test increment / decrement (postfix)
+  echo "------------------------------------------------------------\n";
   $d1--;
   $d2++;
   printf("First big decimal (after \$d1--)   %s\n", $d1->toString());
   printf("Second big decimal (after \$d2++)  %s\n", $d2->toString());
 
   // Test increment / decrement (prefix)
+  echo "------------------------------------------------------------\n";
   ++$d1;
   --$d2;
   printf("First big decimal (after ++\$d1)   %s\n", $d1->toString());
