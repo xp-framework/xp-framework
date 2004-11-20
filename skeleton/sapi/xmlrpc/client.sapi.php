9611a318f6062562a434a9428506e7dc6d734c1d
kiesel <alex@kiesel.name>
2004-11-20 21:40:12 +0000

    - initial revision
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@4273 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/sapi/xmlrpc/client.sapi.php
@@ -0,0 +1,11 @@
+<?php
+/* This file provides the XMLRPC client sapi for the XP framework
+ * 
+ * $Id$
+ */
+
+  uses(
+    'xml.xmlrpc.XmlRpcClient',
+    'xml.xmlrpc.transport.XmlRpcHttpTransport'
+  );
+?>
