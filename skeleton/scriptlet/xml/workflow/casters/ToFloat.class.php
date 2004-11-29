d3c6af8611570f54d77cfafe5debaab13d9ac40d
thekid <timm.friebe@gmail.com>
2004-11-29 13:33:51 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@4298 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/scriptlet/xml/workflow/casters/ToFloat.class.php
@@ -0,0 +1,38 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses('scriptlet.xml.workflow.casters.ParamCaster');
+  
+  /**
+   * Casts given values to floating point numbers
+   *
+   * @purpose  Caster
+   */
+  class ToFloat extends ParamCaster {
+  
+    /**
+     * Cast a given value.
+     *
+     * @see     php://intval
+     * @see     xp://scriptlet.xml.workflow.casters.ParamCaster
+     * @access  public
+     * @param   array value
+     * @return  array value
+     */
+    function castValue($value) {
+      $return= array();
+      foreach ($value as $k => $v) {
+        if ('' == ltrim($v, ' +-0')) {
+          $return[$k]= 0.0;
+        } else {
+          $return[$k]= floatval($v);
+        }
+      }
+
+      return $return;
+    }
+  }
+?>
