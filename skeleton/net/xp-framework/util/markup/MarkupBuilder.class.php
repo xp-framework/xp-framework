b2406355334a96436b9f8fe2ada7ed860391b916
thekid <timm.friebe@gmail.com>
2004-12-15 14:29:34 +0000

    - Fix popProcessor() - Simplify sourcecode within while loop - Add inline documentation
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@4340 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/net/xp-framework/util/markup/MarkupBuilder.class.php
+++ skeleton/net/xp-framework/util/markup/MarkupBuilder.class.php
@@ -39,7 +39,8 @@
      * @return  &net.xp-framework.util.markup.MarkupProcessor
      */
     function &popProcessor() {
-      return array_pop($this->stack);
+      array_shift($this->stack);
+      return $this->stack[0];
     }
 
     /**
@@ -68,26 +69,41 @@
       $out= '';      
       while ($st->hasMoreTokens()) {
         if ('<' == ($token= $st->nextToken())) {
-          
-          // Found beginning of tag
           $tag= $st->nextToken('>');
-          $st->nextToken('>');
+          
+          // If this is an opening tag and a behaviour is defined for it, switch
+          // states and pass control to the processor.
+          if (ctype_alnum($tag[0])) {
+            $st->nextToken('>');
+            $lookup= strtolower($tag);
 
-          // See if we have a processor state push/pop operation
-          $lookup= strtolower($tag);
-          if (isset($state[$lookup])) {
-            $processor= &$this->pushProcessor($processors[$state[$lookup]]);
-            $out.= $processor->initialize();
-          } elseif (isset($state[ltrim($lookup, '/')])) {
-            $out.= $processor->finalize();
-            $processor= &$this->popProcessor();
-          } else {
-            $out.= '<'.$tag.'>';
+            if (isset($state[$lookup])) {
+              $processor= &$this->pushProcessor($processors[$state[$lookup]]);
+              $out.= $processor->initialize();
+            } else {
+              $out.= '<'.$tag.'>';
+            }
+            continue;
+          }           
+          
+          // If this is a closing tag and behaviour is defined for it, switch back
+          // state and return control to the previous processor.
+          if ('/' == $tag[0]) {
+            $st->nextToken('>');
+            $lookup= ltrim(strtolower($tag), '/');
+
+            if (isset($state[$lookup])) {
+              $out.= $processor->finalize();
+              $processor= &$this->popProcessor();
+            } else {
+              $out.= '<'.$tag.'>';
+            }
+            continue;
           }
-
-          continue;
+          
+          $token= '<'.$tag;
         }
-        
+
         $out.= $processor->process($token);
       }
       
