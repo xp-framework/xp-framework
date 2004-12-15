ba1759edaa6ecad76ab6aa5347863a254c5ffffa
thekid <timm.friebe@gmail.com>
2004-12-15 13:40:04 +0000

    - Migrate to serendipity database
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@4339 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/net/xp-framework/scriptlet/state/ViewNewsState.class.php
+++ skeleton/net/xp-framework/scriptlet/state/ViewNewsState.class.php
@@ -5,8 +5,10 @@
  */
 
   uses(
+    'net.xp-framework.util.markup.FormresultHelper',
     'scriptlet.xml.workflow.AbstractState',
-    'net.xp-framework.db.caffeine.XPNews'
+    'rdbms.ConnectionManager',
+    'util.Date'
   );
 
   /**
@@ -24,16 +26,49 @@
      * @param   &scriptlet.xml.XMLScriptletResponse response
      */
     function process(&$request, &$response) {
+      $cm= &ConnectionManager::getInstance();
       
-      // Retrieve news from database - TBD: caching
+      // Fetch entry
       try(); {
-        $item= XPNews::getByNews_id($request->getData());
+        $db= &$cm->getByHost('news', 0);
+        $q= &$db->query('
+          select 
+            entry.id as id,
+            entry.title as title,
+            entry.body as body,
+            entry.extended as extended,
+            entry.author as author,
+            entry.timestamp as timestamp
+          from
+            serendipity_entries entry
+          where
+            entry.id= %d
+            and isdraft = "false"
+          ',
+          $request->getQueryString()
+        );
       } if (catch('SQLException', $e)) {
-        $item= NULL;
-        // Fall through
+        return throw($e);
       }
       
-      $response->addFormResult(Node::fromObject($item, 'item'));
+      // Check if we found an entry
+      if (!($record= $q->next())) {
+        $response->addFormError('entry', 'notfound', '*', $request->getQueryString());
+        return;
+      }
+      
+      // Add entry to the formresult
+      with ($entry= &$response->addFormResult(new Node('entry'))); {
+        $entry->setAttribute('id', $record['id']);
+        $entry->addChild(new Node('title', $record['title']));
+        $entry->addChild(new Node('author', $record['author']));
+        $entry->addChild(new Node('category', $record['category'], array(
+          'id' => $record['category_id']
+        )));
+        $entry->addChild(Node::fromObject(new Date($record['timestamp']), 'date'));
+        $entry->addChild(FormresultHelper::markupNodeFor('body', $record['body']));
+        $entry->addChild(FormresultHelper::markupNodeFor('extended', $record['extended']));
+      }
     }
   }
 ?>
