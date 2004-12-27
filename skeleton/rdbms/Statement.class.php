85cc7edf7ced4d1c23f49e9524da4ed611c08d92
thekid <timm.friebe@gmail.com>
2004-12-27 10:58:41 +0000

    - Initial release # See http://xp-framework.net/rfc/0011.rfc.html
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@4453 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/rdbms/Statement.class.php
@@ -0,0 +1,69 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Represents an SQL statement
+   *
+   * <code>
+   *  with ($peer= &News::getPeer()); {
+   *    $statement= &new Statement('select * from news where news_id < 10');
+   * 
+   *    // Use doSelect()
+   *    $objects= $peer->doSelect($statement);
+   * 
+   *    // Use iteratorFor()
+   *    for ($iterator= &$peer->iteratorFor($statement); $iterator->hasNext(); ) {
+   *      $object= &$iterator->next();
+   *    }
+   *  }
+   * </code>
+   *
+   * @purpose  Expression
+   */
+  class Statement extends Object {
+    var
+      $arguments = array();
+
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   string format
+     * @param   mixed* args
+     */
+    function __construct() {
+      $this->arguments= func_get_args();
+    }
+
+    /**
+     * Creates a string representation
+     *
+     * @access  public
+     * @return  string
+     */
+    function toString() {
+      return $this->getClassName()."@{\n  ".$this->arguments[0]."\n}";
+    }
+        
+    /**
+     * Executes an SQL SELECT statement
+     *
+     * @access  package
+     * @param   &rdbms.DBConnection conn
+     * @param   &rdbms.Peer peer
+     * @return  &rdbms.ResultSet
+     */
+    function executeSelect(&$conn, &$peer) {
+      $this->arguments[0]= preg_replace(
+        '/object\([^\)]*\)/i', 
+        implode(', ', array_keys($peer->types)),
+        $this->arguments[0]
+      );
+      return $conn->query($conn->_prepare($this->arguments));
+    }
+
+  } implements(__FILE__, 'rdbms.SQLExpression');
+?>
