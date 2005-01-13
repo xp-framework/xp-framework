85177f246bb138623bf2442aa9a36797fe27ac9e
thekid <timm.friebe@gmail.com>
2005-01-13 23:29:50 +0000

    - Implement a "socket-wheel" - all the client sockets are polled   using select() and whenever data comes available on one of them, it   is processed (the listeners are notified). This way, the default   server implementation can now handle multiple clients simultaneously # Before, this class was quite pointless IMHO
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@4535 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/peer/server/Server.class.php
+++ skeleton/peer/server/Server.class.php
@@ -101,32 +101,75 @@
      */
     function service() {
       if (!$this->socket->isConnected()) return FALSE;
+
+      $null= NULL;
+      $handles= array();
+      $accepting= $this->socket->getHandle();
       
+      // Loop
       while (!$this->terminate) {
-        if (!($m= &$this->socket->accept())) continue;
-        
-        // Have connection
-        $m->setBlocking(TRUE);
-        $this->notify(new ConnectionEvent(EVENT_CONNECTED, $m));
-        
-        // Loop
-        do {
-          try(); {
-            if (NULL === ($data= $m->readBinary())) break;
-          } if (catch('IOException', $e)) {
-            $this->notify(new ConnectionEvent(EVENT_ERROR, $m, $e));
-            break;
+        xp::gc();
+
+        // Build array of sockets that we want to check for data. If one of them
+        // has disconnected in the meantime, notify the listeners (socket will be
+        // already invalid at that time) and remove it from the clients list.
+        $read= array($this->socket->_sock);
+        foreach (array_keys($handles) as $h) {
+          if (!$handles[$h]->isConnected()) {
+            $this->notify(new ConnectionEvent(EVENT_DISCONNECTED, $handles[$h]));
+            unset($handles[$h]);
+          } else {
+            $read[]= $handles[$h]->getHandle();
           }
+        }
 
-          // Notify listeners
-          $this->notify(new ConnectionEvent(EVENT_DATA, $m, $data));
+        // Check to see if there are sockets with data on it. In case we can
+        // find some, loop over the returned sockets. In case the select() call
+        // fails, break out of the loop and terminate the server - this really 
+        // should not happen!
+        if (FALSE === socket_select($read, $null, $null, NULL)) {
+          return throw(new SocketException('Call to select() failed'));
+        }
 
-        } while (!$m->eof());
+        foreach ($read as $i => $handle) {
 
-        $this->notify(new ConnectionEvent(EVENT_DISCONNECTED, $m));
-
-        // Close communications
-        $m->close();
+          // If there is data on the server socket, this means we have a new client.
+          // In case the accept() call fails, break out of the loop and terminate
+          // the server - this really should not happen!
+          if ($handle === $accepting) {
+            if (!($m= &$this->socket->accept())) {
+              return throw(new SocketException('Call to accept() failed'));
+            }
+            
+            $this->notify(new ConnectionEvent(EVENT_CONNECTED, $m));
+            $handles[(int)$m->getHandle()]= &$m;
+            continue;
+          }
+          
+          // Otherwise, a client is sending data: read it and notify the listeners.
+          // In case of an I/O error, close the client socket and remove the client
+          // from the list.
+          $index= (int)$handle;
+          try(); {
+            $data= $handles[$index]->readBinary();
+          } if (catch('IOException', $e)) {
+            $this->notify(new ConnectionEvent(EVENT_ERROR, $handles[$index], $e));
+            $handles[$index]->close();
+            unset($handles[$index]);
+            continue;
+          }
+          
+          // Check if we got an EOF from the client - in this file the connection
+          // was gracefully closed.
+          if ($handles[$index]->eof()) {
+            $this->notify(new ConnectionEvent(EVENT_DISCONNECTED, $handles[$h]));
+            $handles[$index]->close();
+            unset($handles[$index]);
+            continue;
+          }
+          
+          $this->notify(new ConnectionEvent(EVENT_DATA, $handles[$index], $data));
+        }
       }
     }
   }
