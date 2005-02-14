65cb2e8af59bc116ed9d4d3eed1cb2fc5738f55d
kiesel <alex@kiesel.name>
2005-02-14 23:24:40 +0000

    - implement install, firstinstall-targets
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@4677 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/xp.port.mk
+++ ports/xp.port.mk
@@ -2,17 +2,29 @@
 #
 # $Id$
 
-PHP=php -q -C
-XPI=xpi/install.php
-XPT=xpt/run.php
+BASE?=			../..
+PHP?=			php
+XPINSTALL=		${BASE}/xpi/install.php
+XPFIRSTINSTALL=	${BASE}/xpi/firstinstall.php
+XPT=			${BASE}/xpt/run.php
 
 all:    usage
 
 usage:
-	@echo "Usage: make install"
+	@echo "Usage:"
+	@echo "======"
+	@echo "  make firstinstall.<installtarget>    - Prepare installation"
+	@echo "  make install.<installtarget>         - Install <installtarget>"
 
-install:
-	${PHP} ${XPI}
+install.%:
+	@echo "-------------------------------------------------------"
+	${PHP} ${XPINSTALL} xpinstall/$*.xpi.ini -f $(file) $(flags)
+	@echo "-------------------------------------------------------"
+
+firstinstall.%:
+	@echo "-------------------------------------------------------"
+	$(PHP) $(XPFIRSTINSTALL) xpinstall/$*.xpi.ini
+	@echo "-------------------------------------------------------"
 
 test:
 	${PHP} ${XPT}
