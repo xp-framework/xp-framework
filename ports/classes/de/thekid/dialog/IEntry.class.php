2f78d9309edd914ad825d0bcac09a8a9ec48177d
thekid <timm.friebe@gmail.com>
2005-03-05 15:15:13 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@4812 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/classes/de/thekid/dialog/IEntry.class.php
@@ -0,0 +1,15 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Interface for album entries
+   *
+   * @purpose  Interface
+   */
+  class IEntry extends Interface {
+  
+  }
+?>
