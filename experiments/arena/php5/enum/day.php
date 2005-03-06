4a78d7701c7130bea66295cb18780a25b39cfbca
thekid <timm.friebe@gmail.com>
2005-03-06 14:55:30 +0000

    - Add workDays() method which returns an array of Day objects   for every day that is a work day (Monday through Friday) - Add weekend() method which returns an array of Day objects   for every day that is in the weekend (Saturday and Sunday) - Use implode() and array_map() instead of foreach() for display
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@4858 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/php5/enum/day.php
+++ experiments/arena/php5/enum/day.php
@@ -5,13 +5,31 @@
  */
 
   enum Day {
-    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday;
+
+    private static function isWorkDay(Day $day) {
+      return $day->ordinal < self::Saturday;
+    }
+    
+    public static function workDays() {
+      return array_filter(self::values(), array('self', 'isWorkDay'));
+    }
+
+    private static function isWeekend(Day $day) {
+      return $day->ordinal > self::Friday;
+    }
+
+    public static function weekend() {
+      return array_filter(self::values(), array('self', 'isWeekend'));
+    }
   }
   
-  // {{{ main
-  echo "Workdays:\n";
-  foreach (range(Day::Monday, Day::Friday) as $number) {
-    echo $number, ': ', Day::valueOf($number)->name, "\n";
+  function nameOf(Enumeration $e) {
+    return $e->name;
   }
+
+  // {{{ main
+  echo 'Workdays: ', implode(', ', array_map('nameOf', Day::workDays())), "\n";
+  echo 'Weekend:  ', implode(', ', array_map('nameOf', Day::weekend())), "\n";
   // }}}
 ?>
