bf7a38c20feba47a1cbeb3aec59cbb7435367483
thekid <timm.friebe@gmail.com>
2005-03-06 16:47:29 +0000

    - Fix an off-by-one in name length calculation. Lead to queer problems when   using an enumeration's name for anything else than printing it out. Short   reproduce case:   <?php     enum Result {       RECORDS, STATUS, FAILURE     }
        var_dump(Result::valueOf(Result::RECORDS)->name);
      ?>
      Printed: string(8) "RECORDS"
      Should print: string(7) "RECORDS"
    # PHP's strlen() does *not* include \0
    
    
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@4862 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/php5/Zend/zend_enumerations.c
+++ experiments/arena/php5/Zend/zend_enumerations.c
@@ -67,7 +67,7 @@ ZEND_METHOD(enumeration, values)
 		if (object_init_ex(container, EG(scope)) != SUCCESS) {
 			zend_error(E_CORE_ERROR, "Cannot create object in Enumeration::values()");
 		}
-		add_property_stringl(container, "name", name, length, 1);
+		add_property_stringl(container, "name", name, length - 1, 1);
 		add_property_long(container, "ordinal", (*ordinal)->value.lval);
 
 		/* Add to result array and proceed to next element */
@@ -101,7 +101,7 @@ ZEND_METHOD(enumeration, valueOf) {
 			zend_hash_get_current_key_ex(&EG(scope)->constants_table, &name, &length, &dummy, 0, &pos);
 			
 			object_init_ex(return_value, EG(scope));
-			add_property_stringl(return_value, "name", name, length, 1);
+			add_property_stringl(return_value, "name", name, length - 1, 1);
 			add_property_long(return_value, "ordinal", (*ordinal)->value.lval);
 			return;
 		}
