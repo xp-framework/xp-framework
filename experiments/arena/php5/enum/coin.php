b253aa1d1a7a860b8128b05ed4fad7df89a2e7c5
thekid <timm.friebe@gmail.com>
2005-03-06 14:32:56 +0000

    - Move colorOf() function into Coin enumeration, thus making   it a method
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@4856 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/php5/enum/coin.php
+++ experiments/arena/php5/enum/coin.php
@@ -5,21 +5,22 @@
  */
 
   enum Coin {
-    penny(1), nickel(5), dime(10), quarter(25)
-  }
+    penny(1), nickel(5), dime(10), quarter(25);
 
-  function colorOf(Coin $coin) {
-    switch ($coin->ordinal) {
-      case Coin::penny: return 'copper';
-      case Coin::nickel: return 'nickel';
-      case Coin::dime: case Coin::quarter: return 'silver';
+    public function color() {
+      switch ($this->ordinal) {
+        case self::penny: return 'copper';
+        case self::nickel: return 'nickel';
+        case self::dime: case self::quarter: return 'silver';
+      }
     }
   }
+
 
   // {{{ main
   echo 'Coin (', Coin::size(), " values):\n";
   foreach (Coin::values() as $coin) {
-    echo '* ', $coin->name, ': ', $coin->ordinal, '¢ (', colorOf($coin), ")\n";
+    echo '* ', $coin->name, ': ', $coin->ordinal, '¢ (', $coin->color(), ")\n";
   }
   // }}}
 ?>
