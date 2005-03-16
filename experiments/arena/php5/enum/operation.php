24d15d9d952c24b410f2f9627f8e104d391e9f65
thekid <timm.friebe@gmail.com>
2005-03-16 12:11:54 +0000

    - Fix indentation
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@4877 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/php5/enum/operation.php
+++ experiments/arena/php5/enum/operation.php
@@ -1,27 +1,27 @@
-  <?php
-  /* This class is part of the XP framework's experiments
-   *
-   * $Id$ 
-   */
+<?php
+/* This class is part of the XP framework's experiments
+ *
+ * $Id$ 
+ */
 
-    enum Operation {
-      plus { 
-        function evaluate($x, $y) { return $x + $y; } 
-      },
-      minus { 
-        function evaluate($x, $y) { return $x - $y; } 
-      },
-      times { 
-        function evaluate($x, $y) { return $x * $y; } 
-      },
-      divided_by { 
-        function evaluate($x, $y) { return $x / $y; } 
-      };
-    }
+  enum Operation {
+    plus { 
+      function evaluate($x, $y) { return $x + $y; } 
+    },
+    minus { 
+      function evaluate($x, $y) { return $x - $y; } 
+    },
+    times { 
+      function evaluate($x, $y) { return $x * $y; } 
+    },
+    divided_by { 
+      function evaluate($x, $y) { return $x / $y; } 
+    };
+  }
 
-    $x= 2;
-    $y= 4;
-    foreach (Operation::values() as $op) {
-      printf("%d %s %s = %.1f\n", $x, $op->name, $y, $op->evaluate($x, $y));
-    }
-  ?>
+  $x= 2;
+  $y= 4;
+  foreach (Operation::values() as $op) {
+    printf("%d %s %s = %.1f\n", $x, $op->name, $y, $op->evaluate($x, $y));
+  }
+?>
