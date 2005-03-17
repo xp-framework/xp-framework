96112f7cbfa036b514a4273c55ba0fd384f22814
kiesel <alex@kiesel.name>
2005-03-17 11:17:09 +0000

    - initial revision
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@4880 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/xml/wddx/transport/WddxHttpTransport.class.php
@@ -0,0 +1,136 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses(
+    'xml.wddx.transport.WddxTransport',
+    'peer.http.HttpConnection'
+  );
+
+  /**
+   * Transport for Wddx requests over HTTP.
+   *
+   * @see      xp://xml.wddx.WddxClient
+   * @purpose  HTTP Transport
+   */
+  class WddxHttpTransport extends WddxTransport {
+    var
+      $_conn    = NULL,
+      $_headers = array();
+    
+    /**
+     * Constructor.
+     *
+     * @access  public
+     * @param   string url
+     * @param   array headers
+     */
+    function __construct($url, $headers= array()) {
+      $this->_conn= &new HttpConnection($url);
+      $this->_headers= $headers;
+    }
+    
+    /**
+     * Create a string representation
+     *
+     * @access  public
+     * @return  string
+     */
+    function toString() {
+      return sprintf('%s { %s }', $this->getClassName(), $this->_conn->request->url->_info['url']);
+    }
+
+    /**
+     * Send XML-RPC message
+     *
+     * @access  public
+     * @param   &xml.wddx.WddxMessage message
+     * @return  &scriptlet.HttpScriptletResponse
+     */
+    function &send(&$message) {
+      
+      if (!is('xml.wddx.WddxMessage', $message)) return throw(new IllegalArgumentException(
+        'parameter "message" must be a xml.wddx.WddxMessage'
+      ));
+      
+      // Send request
+      $this->_conn->request->setMethod(HTTP_POST);
+      $this->_conn->request->setParameters(new RequestData(
+        $message->getDeclaration()."\n".
+        $message->getSource(0)
+      ));
+      
+      $this->_conn->request->setHeader('Content-Type', 'text/xml; charset='.$message->getEncoding());
+      $this->_conn->request->setHeader('User-Agent', 'XP Framework WDDX Client (http://xp-framework.net)');
+
+      // Add custom headers
+      $this->_conn->request->addHeaders($this->_headers);
+      
+      try(); {
+        $this->cat && $this->cat->debug('>>>', $this->_conn->request->getRequestString());
+        $res= &$this->_conn->request->send($this->_conn->getTimeout());
+      } if (catch('IOException', $e)) {
+        return throw ($e);
+      }
+      
+      return $res;
+    }
+    
+    /**
+     * Retrieve a WDDX message.
+     *
+     * @access  public
+     * @param   &scriptlet.HttpScriptletResponse response
+     * @return  &xml.wddx.WddxMessage
+     */
+    function &retrieve(&$response) {
+      $this->cat && $this->cat->debug('<<<', $response->toString());
+      
+      try(); {
+        $code= $response->getStatusCode();
+      } if (catch('SocketException', $e)) {
+        return throw($e);
+      }
+      
+      switch ($code) {
+        case HTTP_OK:
+        case HTTP_INTERNAL_SERVER_ERROR:
+          try(); {
+            $xml= '';
+            while ($buf= $response->readData()) $xml.= $buf;
+
+            $this->cat && $this->cat->debug('<<<', $xml);
+            if ($answer= &WddxMessage::fromString($xml)) {
+
+              // Check encoding
+              if (NULL !== ($content_type= $response->getHeader('Content-Type'))) {
+                @list($type, $charset)= explode('; charset=', $content_type);
+                if (!empty($charset)) $answer->setEncoding($charset);
+              }
+            }
+          } if (catch('Exception', $e)) {
+            return throw($e);
+          }
+
+          // Fault?
+          if (NULL !== ($fault= $answer->getFault())) {
+            return throw(new WddxFaultException($fault));
+          }
+          
+          return $answer;
+        
+        case HTTP_AUTHORIZATION_REQUIRED:
+          return throw(new IllegalAccessException(
+            'Authorization required: '.$response->getHeader('WWW-Authenticate')
+          ));
+        
+        default:
+          return throw(new IllegalStateException(
+            'Unexpected return code: '.$response->getStatusCode()
+          ));
+      }
+    }    
+  }
+?>
---
+++ skeleton/xml/wddx/transport/WddxTransport.class.php
@@ -0,0 +1,46 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses('util.log.Traceable');
+
+  /**
+   * Base class for WDDX transports.
+   *
+   * @purpose  Base class.
+   */
+  class WddxTransport extends Object {
+    var
+      $cat  = NULL;
+      
+    /**
+     * Set trace for debugging
+     *
+     * @access  public
+     * @param   &util.log.LogCategory cat
+     */
+    function setTrace(&$cat) {
+      $this->cat= &$cat;
+    }
+ 
+    /**
+     * Send XML-RPC message
+     *
+     * @access  public
+     * @param   &xml.wddx.WddxMessage message
+     * @return  &scriptlet.HttpScriptletResponse
+     */
+    function &send(&$message) { }
+    
+    /**
+     * Retrieve a XML-RPC message.
+     *
+     * @access  public
+     * @param   &scriptlet.HttpScriptletResponse response
+     * @return  &xml.wddx.WddxMessage
+     */
+    function &retrieve(&$response) { }    
+  } implements(__FILE__, 'util.log.Traceable');
+?>
