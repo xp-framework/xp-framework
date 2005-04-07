416d36d5baa4dc7e6aa2634fffa0f20ac44a78ed
kiesel <alex@kiesel.name>
2005-04-07 22:20:52 +0000

    - initial revision
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@4947 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/dist/www/uska/doc/database.sql
@@ -0,0 +1,101 @@
+/*
+ * Database structure for uska.de
+ *
+ * $Id$
+ */
+
+create database uska;
+use uska;
+
+create table player (
+  player_id                   int auto_increment primary key,
+  
+  firstname                   varchar(50) not null,
+  lastname                    varchar(50) not null,
+  username                    varchar(20) not null,
+  
+  password                    varchar(60) not null,
+  email                       varchar(255) not null,
+  
+  position                    int null,
+  sex                         int not null
+) Type=InnoDB
+alter table player add index (username)
+
+create table team (
+  team_id                     int auto_increment primary_key,
+  
+  name                        varchar(255) not null
+) Type=InnoDB
+
+create table player_team_matrix (
+  team_id                     int not null,
+  player_id                   int not null
+) Type=InnoDB
+alter table player_team_matrix add unique index (team_id, player_id)
+alter table player_team_matrix add foreign key (team_id) references team(team_id)
+alter table player_team_matrix add foreign key (player_id) references player(player_id)
+
+create table event_type (
+  event_type_id               int primary key,
+  description                 varchar(50) not null
+) Type=InnoDB
+insert into event_type values (1, "Training")
+insert into event_type values (2, "Turnier")
+insert into event_type values (3, "Sonstiges")
+
+create table event (
+  event_id                    int auto_increment primary key,
+  team_id                     int not null,
+  
+  name                        varchar(255) not null,
+  description                 varchar(1023) null,
+  
+  target_date                 datetime not null,
+  deadline                    datetime null,
+  
+  max_attendees               int null,
+  req_attendees               int null,
+  
+  event_type_id               int not null
+) Type=InnoDB
+alter table event add index (target_date)
+alter table event add foreign key team_id references team(team_id)
+alter table event add foreign key event_type_id references event_type(event_type_id)
+
+create table event_attendee (
+  event_id                    int not null,
+  player_id                   int not null,
+  
+  offers_seats                int null,
+  needs_driver                int null
+) Type=InnoDB
+alter table event_attendee add foreign key event_id references event(event_id)
+alter table event_attendee add foreign key player_id references player(player_id)
+alter table event_attendee add index (event_id)
+alter table event_attendee add index (player_id)
+alter table event_attendee add unique index (event_id, player_id)
+
+create table event_points (
+  event_id                    int not null,
+  player_id                   int not null,
+  
+  points                      int null
+) Type=InnoDB
+alter table event_points add foreign key event_id references event(event_id)
+alter table event_points add foreign key player_id references player(player_id)
+alter table event_points add index (player_id)
+
+create table right (
+  right_id                    int auto_increment primary_key,
+  name                        varchar(50) not null
+) Type=InnoDB
+
+create table plane_right_matrix (
+  right_id                    int not null,
+  player_id                   int not null
+) Type=InnoDB
+alter table plane_right_matrix add index (player_id)
+alter table plane_right_matrix add unique index (right_id, player_id)
+alter table plane_right_matrix add foreign key right_id references right(right_id)
+alter table plane_right_matrix add foreign key player_id references player(player_id)
