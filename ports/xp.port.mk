2d54ca7b24f684a10754d3768a74aea75a175ef3
kiesel <alex@kiesel.name>
2005-04-09 13:36:39 +0000

    - add wrapper target
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@4954 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/xp.port.mk
+++ ports/xp.port.mk
@@ -7,6 +7,11 @@ PHP?=			php
 XPINSTALL=		${BASE}/xpi/install.php
 XPFIRSTINSTALL=	${BASE}/xpi/firstinstall.php
 XPT=			${BASE}/xpt/run.php
+XSLPROC?=		sabcmd
+
+# Default values for wrapper target
+WRAPPER_SRC?=	wrapper
+WRAPPER_XSL?=	$(BASE)/../skeleton/scriptlet/xml/workflow/generator/wrapper.xsl
 
 all:    usage
 
@@ -15,6 +20,7 @@ usage:
 	@echo "======"
 	@echo "  make firstinstall.<installtarget>    - Prepare installation"
 	@echo "  make install.<installtarget>         - Install <installtarget>"
+	@echo "  make wrapper                         - Regenerate wrappers"
 
 install.%:
 	@echo "-------------------------------------------------------"
@@ -28,3 +34,19 @@ firstinstall.%:
 
 test:
 	${PHP} ${XPT}
+
+wrapper:
+	@if [ -z "$(WRAPPER_PHP)" ]; then \
+        echo "You must set the target for the generated wrapper files via the WRAPPER_PHP variable."; \
+        exit 1; \
+	else \
+		if [ -z "$(file)" ] ; then \
+			TARGETS="wrapper/*.iwrp"; \
+        else \
+			TARGETS="$(file)"; \
+        fi; \
+		for i in `ls -1 $$TARGETS | cut -d '.' -f 1` ; do \
+			echo "--->" $$i; \
+    		sabcmd $(WRAPPER_XSL) $$i.iwrp > $(WRAPPER_PHP)/`basename $$i`Wrapper.class.php; \
+		done \
+	fi
