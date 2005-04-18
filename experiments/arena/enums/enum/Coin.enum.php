06147ab6e82059ccc9fa521d7434d6d71c9c7804
thekid <timm.friebe@gmail.com>
2005-04-18 18:45:33 +0000

    - Moved from . to ./enum
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5005 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/arena/enums/enum/Coin.enum.php
@@ -0,0 +1,29 @@
+<?php
+/* This file is part of the XP framework's experiments
+ *
+ * $Id$
+ */
+
+  /**
+   * Enumeration of US coins
+   *
+   * @purpose  Enumeration
+   */
+  enum Coin {
+    penny(1), nickel(5), dime(10), quarter(25);
+    
+    /**
+     * Returns color of this coin
+     *
+     * @access  public
+     * @return  string color
+     */
+    function color() {
+      switch ($this->ordinal) {
+        case penny: return 'copper';
+        case nickel: return 'nickel';
+        case dime: case quarter: return 'silver';
+      }
+    }
+  }
+?>
