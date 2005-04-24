c6ac4871fe2363e5f2c8fbcbe91f817d15bc2e35
thekid <timm.friebe@gmail.com>
2005-04-24 11:20:28 +0000

    - Migrate to new Doclet API
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5030 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/apidoc/doc.php
+++ experiments/arena/apidoc/doc.php
@@ -5,39 +5,48 @@
  */
   require('lang.base.php');
   xp::sapi('cli');
-  uses('util.profiling.Timer', 'util.cmd.ParamString', 'RootDoc');
-  
-  // {{{ void exportClass(&ClassDoc class [, string indent = ''])
-  //     Export a single class
-  function exportClass(&$class, $indent= '') {
-    echo '[', $class->qualifiedName(), "] {\n";
+  uses('util.profiling.Timer', 'Doclet');
 
-    if ($class->superclass) {
-      echo $indent.'  + extends ', exportClass($class->superclass, $indent.'  ');
+  // {{{ TreeDoclet
+  //     Prints a class tree  
+  class TreeDoclet extends Doclet {
+  
+    // {{{ void exportClass(&ClassDoc class [, string indent = ''])
+    //     Export a single class
+    function exportClass(&$class, $indent= '') {
+      echo '[', $class->qualifiedName(), "] {\n";
+
+      if ($class->superclass) {
+        echo $indent.'  + extends ', $this->exportClass($class->superclass, $indent.'  ');
+      }
+      while ($class->interfaces->hasNext()) {
+        $iface= &$class->interfaces->next();
+        echo $indent.'  + implements ', $this->exportClass($iface, $indent.'  ');
+      }
+      while ($class->usedClasses->hasNext()) {
+        $used= &$class->usedClasses->next();
+        echo $indent.'  + uses ', $this->exportClass($used, $indent.'  ');
+      }
+      echo $indent, "}\n";
     }
-    while ($class->interfaces->hasNext()) {
-      $iface= &$class->interfaces->next();
-      echo $indent.'  + implements ', exportClass($iface, $indent.'  ');
+    // }}}
+
+    // {{{ bool start(&RootDoc root)
+    //     Entry point method
+    function start(&$root) {
+      with ($timer= &new Timer(), $timer->start()); {
+        while ($root->classes->hasNext()) {
+          $this->exportClass($root->classes->next());
+        }
+
+        $timer->stop();
+        printf("\n%.3f seconds\n", $timer->elapsedTime());
+      }
     }
-    while ($class->usedClasses->hasNext()) {
-      $used= &$class->usedClasses->next();
-      echo $indent.'  + uses ', exportClass($used, $indent.'  ');
-    }
-    echo $indent, "}\n";
   }
   // }}}
-  
-  // {{{ main
-  $timer= &new Timer();
-  $timer->start(); {
-    $doc= &new RootDoc(new ParamString());
-    
-    while ($doc->classes->hasNext()) {
-      exportClass($doc->classes->next());
-    }
-  }
-  $timer->stop();
-  
-  printf("\n%.3f seconds\n", $timer->elapsedTime());
+
+  // {{{ main  
+  RootDoc::start(new TreeDoclet(), new ParamString());
   // }}}
 ?>
