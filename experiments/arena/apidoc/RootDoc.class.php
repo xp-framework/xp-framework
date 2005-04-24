55e5e55032925919a0306d607f9b9d757b7f84a9
thekid <timm.friebe@gmail.com>
2005-04-24 10:30:51 +0000

    - Parse command line into options and classes
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5024 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/apidoc/RootDoc.class.php
+++ experiments/arena/apidoc/RootDoc.class.php
@@ -5,6 +5,9 @@
  */
  
   uses('ClassIterator');
+  
+  define('OPTION_ONLY', 0x0000);
+  define('HAS_VALUE',   0x0001);
 
   /**
    *
@@ -12,7 +15,8 @@
    */
   class RootDoc extends Object {
     var
-      $classes = NULL;
+      $classes = NULL,
+      $options = array();
     
     /**
      * Constructor
@@ -21,8 +25,61 @@
      * @param   &util.cmd.ParamString options
      */
     function __construct(&$options) {
-      $classes= array($options->value(1));
+      $classes= array();
+      
+      // Separate options from classes
+      $valid= $this->validOptions();
+      for ($i= 1; $i < $options->count; $i++) {
+        $option= &$options->list[$i];
+        
+        if (0 == strncmp($option, '--', 2)) {        // Long: --foo / --foo=bar
+          $p= strpos($option, '=');
+          $name= substr($option, 2, FALSE === $p ? strlen($option) : $p- 2);
+          if (isset($valid[$name])) {
+            if ($valid[$name] == HAS_VALUE) {
+              $this->options[$name]= FALSE === $p ? NULL : substr($option, $p+ 1);
+            } else {
+              $this->options[$name]= TRUE;
+            }
+          }
+        } elseif (0 == strncmp($option, '-', 1)) {   // Short: -f / -f bar
+          $name= substr($option, 1);
+          if (isset($valid[$name])) {
+            if ($valid[$name] == HAS_VALUE) {
+              $this->options[$name]= $options->list[++$i];
+            } else {
+              $this->options[$name]= TRUE;
+            }
+          }          
+        } else {
+          $classes[]= $option;
+        }
+      }
+      
+      // Set up class iterator
       $this->classes= &new ClassIterator($classes);
+    }
+    
+    /**
+     * Return a list of valid options as an associative array, keys
+     * forming parameter names and values defining whether this option
+     * expects a value.
+     *
+     * Example:
+     * <code>
+     *   return array(
+     *     'classpath' => HAS_VALUE,
+     *     'verbose'   => OPTION_ONLY
+     *   );
+     * </code>
+     *
+     * Returns an empty array in this default implementation.
+     *
+     * @access  public
+     * @return  array
+     */
+    function validOptions() {
+      return array();
     }
   }
 ?>
