ca08796fcf16e665ce0a0ab72b60a64892105cc5
thekid <timm.friebe@gmail.com>
2005-04-24 11:33:42 +0000

    - Show off new option() method by making indentation configurable
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5033 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/apidoc/doc.php
+++ experiments/arena/apidoc/doc.php
@@ -10,38 +10,52 @@
   // {{{ TreeDoclet
   //     Prints a class tree  
   class TreeDoclet extends Doclet {
-  
-    // {{{ void exportClass(&ClassDoc class [, string indent = ''])
+    var 
+      $indent = '';
+
+    // {{{ void exportClass(&ClassDoc class [, string offset = ''])
     //     Export a single class
-    function exportClass(&$class, $indent= '') {
+    function exportClass(&$class, $offset= '') {
       echo '[', $class->qualifiedName(), "] {\n";
 
+      $indent= $offset.$this->indent;
       if ($class->superclass) {
-        echo $indent.'  + extends ', $this->exportClass($class->superclass, $indent.'  ');
+        echo $indent.'+ extends ', $this->exportClass($class->superclass, $indent);
       }
       while ($class->interfaces->hasNext()) {
         $iface= &$class->interfaces->next();
-        echo $indent.'  + implements ', $this->exportClass($iface, $indent.'  ');
+        echo $indent.'+ implements ', $this->exportClass($iface, $indent);
       }
       while ($class->usedClasses->hasNext()) {
         $used= &$class->usedClasses->next();
-        echo $indent.'  + uses ', $this->exportClass($used, $indent.'  ');
+        echo $indent.'+ uses ', $this->exportClass($used, $indent);
       }
-      echo $indent, "}\n";
+      echo $offset, "}\n";
     }
     // }}}
 
     // {{{ bool start(&RootDoc root)
     //     Entry point method
     function start(&$root) {
+      $this->indent= str_repeat(' ', $root->option('indent', 2));
+    
       with ($timer= &new Timer(), $timer->start()); {
         while ($root->classes->hasNext()) {
-          $this->exportClass($root->classes->next());
+          $this->exportClass($root->classes->next(), $indent);
         }
 
         $timer->stop();
         printf("\n%.3f seconds\n", $timer->elapsedTime());
       }
+    }
+    // }}}
+    
+    // {{{ array validOptions()
+    //     Returns an array of valid options
+    function validOptions() {
+      return array(
+        'indent' => HAS_VALUE
+      );
     }
   }
   // }}}
