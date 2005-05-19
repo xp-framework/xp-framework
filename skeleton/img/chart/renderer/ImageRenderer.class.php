cd058609b692565ea4cda71f4baea582467c4072
thekid <timm.friebe@gmail.com>
2005-05-19 21:53:21 +0000

    - Add ranges and stepping - Draw the Y-axis and labels - Flood fill chart are with chartback color - Add support for negative values
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5158 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/img/chart/renderer/ImageRenderer.class.php
+++ skeleton/img/chart/renderer/ImageRenderer.class.php
@@ -38,12 +38,32 @@
      * @return  &img.Image
      */
     function &renderBarChart(&$bc) {
+ 
+      // Create local variables for faster access
+      $max= $bc->max();
+      $min= $bc->min();
+      $width= $bc->getBarWidth();
+      $count= $bc->count();
+      
+      // Calculate ranges and stepping
+      list($lower, $upper, $step)= $bc->getRange();
+      RANGE_AUTO == $lower && $lower= $min;
+      RANGE_AUTO == $upper && $upper= $max;
+      RANGE_AUTO == $step && $step= round(($max- $min) / 10);
+
+      // Sanity checks
+      if ($lower > $upper) {
+        return throw(new IllegalArgumentException('Lower range greater than upper range'));
+      }
+
+      // Create image
       with ($img= &Image::create($this->width, $this->height)); {
       
         // Flood fill with background color
         $img->fill($img->allocate($bc->getColor('background')));
       
-        $leftBorder= $rightBorder= $topBorder= $bottomBorder= 10;
+        $leftBorder= 30;
+        $rightBorder= $topBorder= $bottomBorder= 10;
         
         // Calculate inner borders
         $innerWidth= $this->width- $leftBorder- $rightBorder;
@@ -57,26 +77,60 @@
           $this->height - $bottomBorder,
           $color->handle
         );
+
+        // Flood fill with background color
+        $img->fill($img->allocate($bc->getColor('chartback')), $leftBorder+ 1, $topBorder+ 1);
         
-        // Create local variables for faster access
-        $max= $bc->max();
-        $width= $bc->getBarWidth();
-        $count= $bc->count();
+        // Calculate zero line
+        $zero= $this->height - $bottomBorder + ($lower / ($upper - $lower) * $innerHeight);
+        imageline(
+          $img->handle,
+          $leftBorder,
+          $zero,
+          $this->width - $rightBorder,
+          $zero,
+          $color->handle
+        );
+
+        // Draw Y axis scale
+        $font= 2;
+        $fontw= imagefontwidth($font);
+        $fonth= imagefontheight($font);
+        for ($i= $lower; $i <= $upper; $i+= $step) {
+          $y= $zero - ($i / ($upper - $lower) * $innerHeight);
+          imageline(
+            $img->handle,
+            $leftBorder - 5,
+            $y,
+            $leftBorder,
+            $y,
+            $color->handle
+          );
+          imagestring(
+            $img->handle,
+            $font,
+            $leftBorder - 10 - $fontw * strlen($i),
+            $y - $fonth / 2,
+            $i,
+            $color->handle
+          );
+        }
         
         // Figure out the distance between the bars
         if (DISTANCE_AUTO == ($distance= $bc->getDistance())) {
           $distance= $innerWidth / $count;
         }
-
+        
         // Draw bars
         $color= &$img->allocate($bc->getColor('sample'));
         for ($i= 0; $i < $count; $i++) {
+          $h= ($bc->series[0]->values[$i] / ($upper - $lower) * $innerHeight);
           imagefilledrectangle(
             $img->handle,
             $leftBorder + $i * $distance,
-            $this->height - $bottomBorder - ($bc->series[0]->values[$i] / $max * $innerHeight),
+            $h < 0 ? $zero : $zero - $h,
             $leftBorder + $i * $distance + $width,
-            $this->height - $bottomBorder,
+            $h < 0 ? min($zero - $h, $this->height- $bottomBorder) : $zero,
             $color->handle
           );
         }
