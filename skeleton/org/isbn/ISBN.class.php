7732e801bdf8ce05c253b15af4031c39e5aa7f29
thekid <timm.friebe@gmail.com>
2005-06-10 14:03:52 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5203 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/org/isbn/ISBN.class.php
@@ -0,0 +1,74 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * An ISBN is a 10-digit number that identifies a book for purposes of 
+   * commerce and supply chains. The last digit of the ISBN is a check 
+   * digit used to detect transcription errors.
+   *
+   * @see      http://www.isbn.org/
+   * @see      http://www.isbn.org/standards/home/isbn/us/isbnqa.asp - ISBN FAQ 
+   * @see      http://isbntools.com/
+   * @see      http://isbntools.com/details.html - Format details
+   * @purpose  Represent an International Standard Book Number (ISBN)
+   */
+  class ISBN extends Object {
+    var
+      $number = '';
+      
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   string isbn
+     * @throws  lang.IllegalArgumentException in case the ISBN is invalid
+     */
+    function __construct($isbn) {
+      if (!ISBN::isValid($isbn)) {
+        return throw(new IllegalArgumentException('ISBN "'.$isbn.'" is invalid'));
+      }
+      $this->number= $isbn;
+    }
+    
+    /**
+     * Verify an ISBN number
+     *
+     * @model   static
+     * @access  public
+     * @param   string isbn
+     * @return  bool TRUE if the number is valid
+     */
+    function isValid($isbn) {
+      static $values= '0123456789X';
+
+      for ($checksum= 0, $weight= 10, $i= 0, $s= strlen($isbn); $i < $s; $i++) {
+        if ('-' == $isbn{$i}) continue;
+
+        // If we encounter an invalid character, break immediately
+        if (FALSE === ($value= strpos($values, $isbn{$i}))) return FALSE;
+        
+        // If we encounter an X in an invalid place (may only be at the 
+        // end), break immediately
+        if ($value == 10 && $weight != 1) return FALSE;
+
+        $checksum+= $weight * $value;
+        $weight--;
+      }
+
+      return 0 === ($checksum % 11);
+    }
+    
+    /**
+     * Returns a string representation of this object
+     *
+     * @access  public
+     * @return  string
+     */
+    function toString() {
+      return $this->getClassName().'('.$this->number.')';
+    }
+  }
+?>
---
+++ skeleton/org/isbn/ISBNTest.class.php
@@ -0,0 +1,49 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses('util.profiling.unittest.TestCase', 'org.isbn.ISBN');
+
+  /**
+   * ISBN class unit test
+   *
+   * @see      xp://org.isbn.ISBN
+   * @purpose  Unit test case
+   */
+  class ISBNTest extends TestCase {
+  
+    /**
+     * Test correct ISBN
+     *
+     * @access  public
+     * @see     http://isbntools.com/default.html#ij
+     */
+    #[@test]
+    function verifyISBNToolsExampleISBN() {
+      $this->assertTrue(ISBN::isValid('0-8436-1072-7'));
+    }
+
+    /**
+     * Test ISBN of "Softwaretest mit JUnit" book
+     *
+     * @access  public
+     * @see     http://www.amazon.de/exec/obidos/ISBN=3898643255
+     */
+    #[@test]
+    function verifyJUnitTestBookISBN() {
+      $this->assertTrue(ISBN::isValid('3-89864-325-5'));
+    }
+
+    /**
+     * Test ISBN with invalid checksum (is: 3, should be: 7)
+     *
+     * @access  public
+     */
+    #[@test]
+    function verifyInvalidChecksum() {
+      $this->assertFalse(ISBN::isValid('0-8436-1072-3'));
+    }
+  }
+?>
