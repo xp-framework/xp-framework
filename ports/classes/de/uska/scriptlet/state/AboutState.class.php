47da515eb3efed71ec2c90bd0763f58641dbc43f
kiesel <alex@kiesel.name>
2005-06-30 20:36:52 +0000

    - initial revision
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5272 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/classes/de/uska/scriptlet/state/AboutState.class.php
@@ -0,0 +1,17 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses('scriptlet.xml.workflow.AbstractState');
+
+  /**
+   * About state. Does nothing.
+   *
+   * @purpose  Display about information.
+   */
+  class AboutState extends AbstractState {
+  
+  }
+?>
