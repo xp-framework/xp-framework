5352eb984a66dac05b2c13441c82af278fd98b69
thekid <timm.friebe@gmail.com>
2005-07-22 12:52:02 +0000

    - Add tests for new serialize methods:   . String   . int   . long   . boolean
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5360 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/easc/java/src/net/xp_framework/easc/unittest/SerializerTest.java
+++ experiments/arena/easc/java/src/net/xp_framework/easc/unittest/SerializerTest.java
@@ -12,7 +12,21 @@ import static net.xp_framework.easc.protocol.standard.Serializer.*;
 public class SerializerTest {
 
     @Test public void serializeString() {
-        // assertEquals("s:11:\"Hello World\";", serialize("Hello World"));
+        assertEquals("s:11:\"Hello World\";", serialize("Hello World"));
     }
 
+    @Test public void serializeIntPrimitive() {
+        assertEquals("i:6100;", serialize(6100));
+        assertEquals("i:-6100;", serialize(-6100));
+    }
+
+    @Test public void serializeLongPrimitive() {
+        assertEquals("i:6100;", serialize(6100L));
+        assertEquals("i:-6100;", serialize(-6100L));
+    }
+    
+    @Test public void serializeBoolean() {
+        assertEquals("b:1;", serialize(true));
+        assertEquals("b:0;", serialize(false));
+    }
 }
