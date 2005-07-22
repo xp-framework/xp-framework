2b02d0ccb5f8f9cc6e5d679227981d443eb352e4
thekid <timm.friebe@gmail.com>
2005-07-22 13:24:16 +0000

    - Add test for serializer methods for:   . char   . byte   . short   . double   . float
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5362 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/easc/java/src/net/xp_framework/easc/unittest/SerializerTest.java
+++ experiments/arena/easc/java/src/net/xp_framework/easc/unittest/SerializerTest.java
@@ -15,6 +15,24 @@ public class SerializerTest {
         assertEquals("s:11:\"Hello World\";", serialize("Hello World"));
     }
 
+    @Test public void serializeCharPrimitive() {
+        assertEquals("s:1:\"X\";", serialize('X'));
+    }
+
+    @Test public void serializeUmlautCharPrimitive() {
+        assertEquals("s:1:\"Ü\";", serialize('Ü'));
+    }
+
+    @Test public void serializeBytePrimitive() {
+        assertEquals("i:16;", serialize((byte)16));
+        assertEquals("i:-16;", serialize((byte)-16));
+    }
+
+    @Test public void serializeShortPrimitive() {
+        assertEquals("i:1214;", serialize((short)1214));
+        assertEquals("i:-1214;", serialize((short)-1214));
+    }
+
     @Test public void serializeIntPrimitive() {
         assertEquals("i:6100;", serialize(6100));
         assertEquals("i:-6100;", serialize(-6100));
@@ -23,6 +41,16 @@ public class SerializerTest {
     @Test public void serializeLongPrimitive() {
         assertEquals("i:6100;", serialize(6100L));
         assertEquals("i:-6100;", serialize(-6100L));
+    }
+
+    @Test public void serializeDoublePrimitive() {
+        assertEquals("d:0.1;", serialize(0.1));
+        assertEquals("d:-0.1;", serialize(-0.1));
+    }
+
+    @Test public void serializeFloatPrimitive() {
+        assertEquals("d:0.1;", serialize(0.1f));
+        assertEquals("d:-0.1;", serialize(-0.1f));
     }
     
     @Test public void serializeBoolean() {
