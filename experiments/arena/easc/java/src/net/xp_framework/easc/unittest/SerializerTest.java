acc03acdf979dc42d511f9f055963f40c507b03f
thekid <timm.friebe@gmail.com>
2005-07-22 15:31:13 +0000

    - Add test for overloaded serialize() methods for every primitive wrapper type - Add test for a java.util.HashMap with mixed values
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5373 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/easc/java/src/net/xp_framework/easc/unittest/SerializerTest.java
+++ experiments/arena/easc/java/src/net/xp_framework/easc/unittest/SerializerTest.java
@@ -22,8 +22,16 @@ public class SerializerTest {
         assertEquals("s:1:\"X\";", serialize('X'));
     }
 
+    @Test public void serializeCharacter() throws Exception {
+        assertEquals("s:1:\"X\";", serialize(new Character('X')));
+    }
+
     @Test public void serializeUmlautCharPrimitive() throws Exception {
         assertEquals("s:1:\"Ü\";", serialize('Ü'));
+    }
+
+    @Test public void serializeUmlautCharacter() throws Exception {
+        assertEquals("s:1:\"Ü\";", serialize(new Character('Ü')));
     }
 
     @Test public void serializeBytePrimitive() throws Exception {
@@ -31,9 +39,19 @@ public class SerializerTest {
         assertEquals("i:-16;", serialize((byte)-16));
     }
 
+    @Test public void serializeBytes() throws Exception {
+        assertEquals("i:16;", serialize(new Byte((byte)16)));
+        assertEquals("i:-16;", serialize(new Byte((byte)-16)));
+    }
+
     @Test public void serializeShortPrimitive() throws Exception {
         assertEquals("i:1214;", serialize((short)1214));
         assertEquals("i:-1214;", serialize((short)-1214));
+    }
+
+    @Test public void serializeShorts() throws Exception {
+        assertEquals("i:1214;", serialize(new Short((short)1214)));
+        assertEquals("i:-1214;", serialize(new Short((short)-1214)));
     }
 
     @Test public void serializeIntPrimitive() throws Exception {
@@ -41,9 +59,19 @@ public class SerializerTest {
         assertEquals("i:-6100;", serialize(-6100));
     }
 
+    @Test public void serializeIntegers() throws Exception {
+        assertEquals("i:6100;", serialize(new Integer(6100)));
+        assertEquals("i:-6100;", serialize(new Integer(-6100)));
+    }
+
     @Test public void serializeLongPrimitive() throws Exception {
         assertEquals("i:6100;", serialize(6100L));
         assertEquals("i:-6100;", serialize(-6100L));
+    }
+
+    @Test public void serializeLongs() throws Exception {
+        assertEquals("i:6100;", serialize(new Long(6100L)));
+        assertEquals("i:-6100;", serialize(new Long(-6100L)));
     }
 
     @Test public void serializeDoublePrimitive() throws Exception {
@@ -51,14 +79,29 @@ public class SerializerTest {
         assertEquals("d:-0.1;", serialize(-0.1));
     }
 
+    @Test public void serializeDoubles() throws Exception {
+        assertEquals("d:0.1;", serialize(new Double(0.1)));
+        assertEquals("d:-0.1;", serialize(new Double(-0.1)));
+    }
+
     @Test public void serializeFloatPrimitive() throws Exception {
         assertEquals("d:0.1;", serialize(0.1f));
         assertEquals("d:-0.1;", serialize(-0.1f));
+    }
+
+    @Test public void serializeFloats() throws Exception {
+        assertEquals("d:0.1;", serialize(new Float(0.1f)));
+        assertEquals("d:-0.1;", serialize(new Float(-0.1f)));
     }
     
     @Test public void serializeBooleanPrimitive() throws Exception {
         assertEquals("b:1;", serialize(true));
         assertEquals("b:0;", serialize(false));
+    }
+
+    @Test public void serializeBooleans() throws Exception {
+        assertEquals("b:1;", serialize(new Boolean(true)));
+        assertEquals("b:0;", serialize(new Boolean(false)));
     }
 
     @Test public void serializeValueObject() throws Exception {
@@ -75,6 +118,17 @@ public class SerializerTest {
         
         assertEquals(
             "a:2:{s:3:\"key\";s:5:\"value\";s:6:\"number\";s:4:\"6100\";}", 
+            serialize(h)
+        );
+    }
+
+    @Test public void serializeMixedHashMap() throws Exception {
+        HashMap h= new HashMap();
+        h.put("key", "value");
+        h.put("number", 6100);
+        
+        assertEquals(
+            "a:2:{s:3:\"key\";s:5:\"value\";s:6:\"number\";i:6100;}", 
             serialize(h)
         );
     }
