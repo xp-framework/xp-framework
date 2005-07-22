55ab7c989acdc650d831211456dc671a0dc74626
thekid <timm.friebe@gmail.com>
2005-07-22 14:30:18 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5364 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/arena/easc/java/src/net/xp_framework/easc/protocol/standard/Handler.java
@@ -0,0 +1,13 @@
+/* This class is part of the XP framework's EAS connectivity
+ *
+ * $Id$
+ */
+
+package net.xp_framework.easc.protocol.standard;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Handler {
+}
---
+++ experiments/arena/easc/java/src/net/xp_framework/easc/unittest/Person.java
@@ -0,0 +1,16 @@
+/* This class is part of the XP framework's EAS connectivity
+ *
+ * $Id$
+ */
+
+package net.xp_framework.easc.unittest;
+
+/**
+ * Person class
+ *
+ * @purpose Value object for SerializerTest
+ */
+public class Person {
+    public int id = 1549;
+    public String name = "Timm Friebe";
+}
