be8b2de54721931f8d038a8a3d37301daf3c3999
thekid <timm.friebe@gmail.com>
2005-07-25 20:00:12 +0000

    - QA: Add apidoc and example
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5384 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/easc/java/src/net/xp_framework/easc/protocol/standard/ArraySerializer.java
+++ experiments/arena/easc/java/src/net/xp_framework/easc/protocol/standard/ArraySerializer.java
@@ -6,13 +6,30 @@
 package net.xp_framework.easc.protocol.standard;
 
 /**
- * Helper class for Serializer
+ * Helper class for Serializer. Is expected to be used in an anonymous
+ * class instance creation expression.
+ *
+ * Usage example:
+ * <code>
+ *   return new ArraySerializer() {
+ *       public void yield(int i) {
+ *           this.buffer.append(serialize(array[i]));
+ *       }
+ *   }.run(array.length);
+ * </code>
  *
  * @see net.xp_framework.easc.protocol.standard.Serializer
  */
 public abstract class ArraySerializer {
     protected StringBuffer buffer = null;
     
+    /**
+     * Run this serializer.
+     *
+     * @access  public
+     * @param   int length
+     * @return  java.lang.String
+     */
     public String run(int length) {
         this.buffer= new StringBuffer("a:" + length + ":{");
         for (int i= 0; i < length; i++) {
@@ -23,5 +40,14 @@ public abstract class ArraySerializer {
         return this.buffer.toString();
     }
     
+    /**
+     * This method is executed for each element in the array and should
+     * append to the buffer the serialized version of the element at the
+     * passed offset i of the array.
+     *
+     * @model   abstract
+     * @access  public
+     * @param   int i
+     */
     abstract public void yield(int i);
 }
