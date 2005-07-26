48253dab97c1cf7f02d63385f5e2c68ed29c774d
thekid <timm.friebe@gmail.com>
2005-07-26 16:02:50 +0000

    - QA: Add api documentation to class and all methods
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5429 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/easc/java/src/net/xp_framework/easc/unittest/SerializerTest.java
+++ experiments/arena/easc/java/src/net/xp_framework/easc/unittest/SerializerTest.java
@@ -19,98 +19,226 @@ import static net.xp_framework.easc.protocol.standard.Serializer.representationO
 import static net.xp_framework.easc.protocol.standard.Serializer.valueOf;
 import static net.xp_framework.easc.protocol.standard.Serializer.registerMapping;
 
+/**
+ * Test the serialization / deserialization functionality
+ *
+ *
+ * @see   net.xp_framework.easc.protocol.standard.Serializer
+ */
 public class SerializerTest {
 
+    /**
+     * Tests serialization of strings
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfString() throws Exception {
         assertEquals("s:11:\"Hello World\";", representationOf("Hello World"));
     }
 
+    /**
+     * Tests serialization of the char primitive
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfCharPrimitive() throws Exception {
         assertEquals("s:1:\"X\";", representationOf('X'));
     }
 
+    /**
+     * Tests serialization of Character primitive wrapper
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfCharacter() throws Exception {
         assertEquals("s:1:\"X\";", representationOf(new Character('X')));
     }
 
+    /**
+     * Tests serialization of char primitive containing a German umlaut
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfUmlautCharPrimitive() throws Exception {
         assertEquals("s:1:\"Ü\";", representationOf('Ü'));
     }
 
+    /**
+     * Tests serialization of Character primitive wrapper containing a German umlaut
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfUmlautCharacter() throws Exception {
         assertEquals("s:1:\"Ü\";", representationOf(new Character('Ü')));
     }
 
+    /**
+     * Tests serialization of the byte primitive
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfBytePrimitive() throws Exception {
         assertEquals("i:16;", representationOf((byte)16));
         assertEquals("i:-16;", representationOf((byte)-16));
     }
 
+
+    /**
+     * Tests serialization of the Byte primitive wrapper
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfBytes() throws Exception {
         assertEquals("i:16;", representationOf(new Byte((byte)16)));
         assertEquals("i:-16;", representationOf(new Byte((byte)-16)));
     }
 
+    /**
+     * Tests serialization of the short primtive
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfShortPrimitive() throws Exception {
         assertEquals("i:1214;", representationOf((short)1214));
         assertEquals("i:-1214;", representationOf((short)-1214));
     }
 
+    /**
+     * Tests serialization of the Short primitive wrapper
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfShorts() throws Exception {
         assertEquals("i:1214;", representationOf(new Short((short)1214)));
         assertEquals("i:-1214;", representationOf(new Short((short)-1214)));
     }
 
+    /**
+     * Tests serialization of the int primitive
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfIntPrimitive() throws Exception {
         assertEquals("i:6100;", representationOf(6100));
         assertEquals("i:-6100;", representationOf(-6100));
     }
 
+    /**
+     * Tests serialization of the Integer primitive wrapper
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfIntegers() throws Exception {
         assertEquals("i:6100;", representationOf(new Integer(6100)));
         assertEquals("i:-6100;", representationOf(new Integer(-6100)));
     }
 
+    /**
+     * Tests serialization of the long primitive
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfLongPrimitive() throws Exception {
         assertEquals("i:6100;", representationOf(6100L));
         assertEquals("i:-6100;", representationOf(-6100L));
     }
 
+    /**
+     * Tests serialization of the Long primitive wrapper
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfLongs() throws Exception {
         assertEquals("i:6100;", representationOf(new Long(6100L)));
         assertEquals("i:-6100;", representationOf(new Long(-6100L)));
     }
 
+    /**
+     * Tests serialization of the double primitive
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfDoublePrimitive() throws Exception {
         assertEquals("d:0.1;", representationOf(0.1));
         assertEquals("d:-0.1;", representationOf(-0.1));
     }
 
+    /**
+     * Tests serialization of the Double primitive wrapper
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfDoubles() throws Exception {
         assertEquals("d:0.1;", representationOf(new Double(0.1)));
         assertEquals("d:-0.1;", representationOf(new Double(-0.1)));
     }
 
+    /**
+     * Tests serialization of the float primitive
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfFloatPrimitive() throws Exception {
         assertEquals("d:0.1;", representationOf(0.1f));
         assertEquals("d:-0.1;", representationOf(-0.1f));
     }
 
+    /**
+     * Tests serialization of the Float primitive wrapper
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfFloats() throws Exception {
         assertEquals("d:0.1;", representationOf(new Float(0.1f)));
         assertEquals("d:-0.1;", representationOf(new Float(-0.1f)));
     }
     
+    /**
+     * Tests serialization of the boolean primitive
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfBooleanPrimitive() throws Exception {
         assertEquals("b:1;", representationOf(true));
         assertEquals("b:0;", representationOf(false));
     }
 
+    /**
+     * Tests serialization of the Boolean primitive wrapper
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfBooleans() throws Exception {
         assertEquals("b:1;", representationOf(new Boolean(true)));
         assertEquals("b:0;", representationOf(new Boolean(false)));
     }
 
+    /**
+     * Tests serialization of a value object
+     *
+     * @see     net.xp_framework.easc.unittest.Person
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfValueObject() throws Exception {
         assertEquals(
             "O:37:\"net.xp_framework.easc.unittest.Person\":2:{s:2:\"id\";i:1549;s:4:\"name\";s:11:\"Timm Friebe\";}", 
@@ -118,6 +246,12 @@ public class SerializerTest {
         );
     }
 
+    /**
+     * Tests serialization of a java.util.HashMap<String, String>
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfStringHashMap() throws Exception {
         HashMap<String, String> h= new HashMap<String, String>();
         h.put("key", "value");
@@ -129,6 +263,13 @@ public class SerializerTest {
         );
     }
 
+    /**
+     * Tests serialization of a java.util.HashMap<String, Object> which contains
+     * a string for the first key and an integer for the second
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfMixedHashMap() throws Exception {
         HashMap<String, Object> h= new HashMap<String, Object>();
         h.put("key", "value");
@@ -140,6 +281,12 @@ public class SerializerTest {
         );
     }
     
+    /**
+     * Tests serialization of an array of strings
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfStringArray() throws Exception {
         assertEquals(
             "a:2:{i:0;s:5:\"First\";i:1;s:6:\"Second\";}", 
@@ -147,6 +294,12 @@ public class SerializerTest {
         );
     }
 
+    /**
+     * Tests serialization of an array of integer primitives
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfIntegerPrimitiveArray() throws Exception {
         assertEquals(
             "a:2:{i:0;i:3;i:1;i:4;}", 
@@ -154,6 +307,12 @@ public class SerializerTest {
         );
     }
     
+    /**
+     * Tests serialization of a java.util.Date
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfDate() throws Exception {
         assertEquals(
             "D:1122369782;",
@@ -161,6 +320,13 @@ public class SerializerTest {
         );
     }
     
+    /**
+     * Tests serialization of a "user type"
+     *
+     * @see     net.xp_framework.easc.protocol.standard.Serializer#registerMapping
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void representationOfUserType() throws Exception {
         registerMapping(UUID.class, new Invokeable<String, UUID>() {
             public String invoke(UUID u) throws Exception {
@@ -174,56 +340,114 @@ public class SerializerTest {
         ));
     }
     
+    /**
+     * Tests deserialization of null (identified by "N" token)
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void valueOfNull() throws Exception {
         assertEquals(null, valueOf("N;"));
     }
 
+    /**
+     * Tests deserialization of booleans (identified by "b" token)
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void valueOfBoolean() throws Exception {
         assertEquals(true, valueOf("b:1;"));
         assertEquals(false, valueOf("b:0;"));
     }
 
+    /**
+     * Tests deserialization of longs (identified by "i" token)
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void valueOfLong() throws Exception {
         assertEquals(6100L, (Long)valueOf("i:6100;"));
         assertEquals(-6100L, (Long)valueOf("i:-6100;"));
     }
 
+    /**
+     * Tests deserialization of doubles (identified by "f" token)
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void valueOfDouble() throws Exception {
         assertEquals(0.1, (Double)valueOf("f:0.1;"), 0.0001);
         assertEquals(-0.1, (Double)valueOf("f:-0.1;"), 0.0001);
     }
 
+    /**
+     * Tests deserialization of strings (identified by "s" token)
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void valueOfString() throws Exception {
         assertEquals("Hello", valueOf("s:5:\"Hello\";"));
     }
 
+    /**
+     * Tests deserialization of strings with quotation marks
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
     @Test public void valueOfQuotedString() throws Exception {
         assertEquals("\"Hello\", he said.", valueOf("s:17:\"\"Hello\", he said.\";"));
     }
 
-    @Test
-    public void valueOfIntegerArray() throws Exception {
+    /**
+     * Tests deserialization of an array (identified by "a" token) 
+     * consisting of integers
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
+    @Test public void valueOfIntegerArray() throws Exception {
         assertEquals("{1=4, 0=3}", ((HashMap)valueOf("a:2:{i:0;i:3;i:1;i:4;}")).toString());
     }
 
-    @Test
-    public void valueOfStringArray() throws Exception {
+    /**
+     * Tests deserialization of an array of consisting of strings
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
+    @Test public void valueOfStringArray() throws Exception {
         assertEquals(
             "{1=More, 0=Binford}", 
             ((HashMap)valueOf("a:2:{i:0;s:7:\"Binford\";i:1;s:4:\"More\";}")).toString()
         );
     }
     
-    @Test
-    public void valueOfPersonObject() throws Exception {
+    /**
+     * Tests deserialization an object (identified by "O" token)
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     * @see     net.xp_framework.easc.unittest.Person
+     */
+    @Test public void valueOfPersonObject() throws Exception {
         assertEquals(
             new Person(), 
             valueOf("O:37:\"net.xp_framework.easc.unittest.Person\":2:{s:2:\"id\";i:1549;s:4:\"name\";s:11:\"Timm Friebe\";}")
         );
     }
 
-    @Test
-    public void valueOfDate() throws Exception {
+    /**
+     * Tests deserialization of a date (identified by "D" token)
+     *
+     * @access  public
+     * @throws  java.lang.Exception
+     */
+    @Test public void valueOfDate() throws Exception {
         assertEquals(
             new Date(1122369782000L), 
             valueOf("D:1122369782;")
