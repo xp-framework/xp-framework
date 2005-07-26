d23fc6fa93f764cf4ee11c993eea79c6c58e6753
thekid <timm.friebe@gmail.com>
2005-07-26 12:03:49 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5405 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/arena/easc/java/src/net/xp_framework/easc/protocol/standard/Invokeable.java
@@ -0,0 +1,28 @@
+package net.xp_framework.easc.protocol.standard;
+
+/**
+ * Generic invokeable interface
+ * 
+ * Example:
+ * <code>
+ *   registerMapping(UUID.class, new Invokeable<String, UUID>() {
+ *       public String invoke(UUID u) throws Exception {
+ *           return "U:" + u + ";";
+ *       }
+ *   });
+ * </code>
+ *
+ * @see   net.xp_framework.easc.protocol.standard.Serializer#registerMapping
+ */
+public interface Invokeable<Return, Parameter> {
+
+    /**
+     * Invoke this instance
+     *
+     * @access  public
+     * @param   <Parameter> p
+     * @return  <Return> 
+     * @throws  lang.Exception
+     */
+    public Return invoke(Parameter p) throws Exception;
+}
