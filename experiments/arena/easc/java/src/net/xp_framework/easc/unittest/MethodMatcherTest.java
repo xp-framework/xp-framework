63ed118880dbc9bbeaf889dbf8f5c00d23da0314
thekid <timm.friebe@gmail.com>
2005-07-29 11:06:29 +0000

    - Add test for MethodMatcher.methodFor() method
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5442 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/easc/java/src/net/xp_framework/easc/unittest/MethodMatcherTest.java
+++ experiments/arena/easc/java/src/net/xp_framework/easc/unittest/MethodMatcherTest.java
@@ -6,6 +6,10 @@
 package net.xp_framework.easc.unittest;
 
 import org.junit.Test;
+import net.xp_framework.easc.unittest.Person;
+import java.lang.reflect.Method;
+
+import static net.xp_framework.easc.util.MethodMatcher.methodFor;
 import static org.junit.Assert.*;
 
 /**
@@ -17,5 +21,33 @@ import static org.junit.Assert.*;
  */
 public class MethodMatcherTest {
 
+    protected String methodString(Method m) {
+        if (m == null) return "(null)";   // Catch border-case, prevent NPE
 
+        Class[] parameters= m.getParameterTypes();
+        StringBuffer buf= new StringBuffer()
+            .append(m.getReturnType().getName())
+            .append(' ')
+            .append(m.getName())
+            .append(':')
+            .append(parameters.length)
+            .append("(");
+        
+        // Append parameter types
+        for (Class c: parameters) {
+            buf.append(c.getName()).append(", ");
+        }
+        if (parameters.length > 0) {  
+            buf.delete(buf.length() - 2, 2);
+        }
+        
+        return buf.append(')').toString();
+    }
+
+    @Test public void noArgMethod() throws Exception {
+        assertEquals(
+            "int getId:0()", 
+            methodString(methodFor(Person.class, "getId", new Object[] { }))
+        );
+    }
 }
