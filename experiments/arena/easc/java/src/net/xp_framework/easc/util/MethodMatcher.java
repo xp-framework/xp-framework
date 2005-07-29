fef07b97da7d5db263b77f3bce89d73dd7e23630
thekid <timm.friebe@gmail.com>
2005-07-29 11:06:00 +0000

    - Add methodFor() method # TBD: Check signatures
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5441 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/easc/java/src/net/xp_framework/easc/util/MethodMatcher.java
+++ experiments/arena/easc/java/src/net/xp_framework/easc/util/MethodMatcher.java
@@ -5,10 +5,29 @@
 
 package net.xp_framework.easc.util;
 
+import java.lang.reflect.Method;
+
 /**
  * Matches methods by given argument list
  *
  */
 public class MethodMatcher {
 
+    public static Method methodFor(Class c, String name, Object[] arguments) {
+        for (Method m : c.getMethods()) {
+
+            // First, check name equalitiy
+            if (!name.equals(m.getName())) continue;
+
+            // Check argument length against signature length            
+            Class[] parameters= m.getParameterTypes();
+            if (parameters.length != arguments.length) continue;
+            
+            // Now we have a candidate!
+            return m;
+        }
+        
+        // We can't find anything...
+        return null;
+    }
 }
