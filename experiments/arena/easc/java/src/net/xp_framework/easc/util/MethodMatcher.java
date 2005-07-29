7ab7ed0f7bb5eacb6af52b10b708c8cb2ecb08b2
thekid <timm.friebe@gmail.com>
2005-07-29 10:28:39 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5437 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/arena/easc/java/src/net/xp_framework/easc/util/MethodMatcher.java
@@ -0,0 +1,14 @@
+/* This class is part of the XP framework's EAS connectivity
+ *
+ * $Id$
+ */
+
+package net.xp_framework.easc.util;
+
+/**
+ * Matches methods by given argument list
+ *
+ */
+public class MethodMatcher {
+
+}
