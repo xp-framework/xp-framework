cbc883380be5b7eb764769d477eda80b9f5f7920
thekid <timm.friebe@gmail.com>
2005-07-29 11:58:53 +0000

    - Check for primitive wrappers
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5449 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/easc/java/src/net/xp_framework/easc/util/MethodMatcher.java
+++ experiments/arena/easc/java/src/net/xp_framework/easc/util/MethodMatcher.java
@@ -6,12 +6,26 @@
 package net.xp_framework.easc.util;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
 
 /**
  * Matches methods by given argument list
  *
  */
 public class MethodMatcher {
+
+    private static HashMap<Class, Class> wrapperTypes= new HashMap<Class, Class>();
+    
+    static {
+        wrapperTypes.put(char.class, Character.class);
+        wrapperTypes.put(byte.class, Byte.class);
+        wrapperTypes.put(short.class, Short.class);
+        wrapperTypes.put(int.class, Integer.class);
+        wrapperTypes.put(long.class, Long.class);
+        wrapperTypes.put(double.class, Double.class);
+        wrapperTypes.put(float.class, Float.class);
+        wrapperTypes.put(boolean.class, Boolean.class);
+    }
 
     private static boolean signatureMatchesArguments(Class[] signature, Object[] arguments) {
 
@@ -21,7 +35,7 @@ public class MethodMatcher {
         // Check argument types vs. signature types
         int offset= 0;
         for (Class c : signature) {
-            if (!c.equals(arguments[offset++].getClass())) return false;
+            if (!((c.isPrimitive() ? wrapperTypes.get(c) : c).isAssignableFrom(arguments[offset++].getClass()))) return false;
         }
         
         return true;
