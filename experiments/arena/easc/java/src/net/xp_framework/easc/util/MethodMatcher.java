35c8fd77871256470192840b9f4b012c93f78d9a
thekid <timm.friebe@gmail.com>
2005-07-29 11:27:53 +0000

    - Check argument types vs. signature types (in private helper method)
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5446 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/easc/java/src/net/xp_framework/easc/util/MethodMatcher.java
+++ experiments/arena/easc/java/src/net/xp_framework/easc/util/MethodMatcher.java
@@ -13,15 +13,28 @@ import java.lang.reflect.Method;
  */
 public class MethodMatcher {
 
+    private static boolean signatureMatchesArguments(Class[] signature, Object[] arguments) {
+
+        // Check argument length against signature length            
+        if (signature.length != arguments.length) return false;
+
+        // Check argument types vs. signature types
+        int offset= 0;
+        for (Class c : signature) {
+            if (!c.equals(arguments[offset++].getClass())) return false;
+        }
+        
+        return true;
+    }
+
     public static Method methodFor(Class c, String name, Object[] arguments) {
         for (Method m : c.getMethods()) {
 
             // First, check name equalitiy
             if (!name.equals(m.getName())) continue;
 
-            // Check argument length against signature length            
-            Class[] parameters= m.getParameterTypes();
-            if (parameters.length != arguments.length) continue;
+            // Check signatures vs. arguments
+            if (!signatureMatchesArguments(m.getParameterTypes(), arguments)) continue;
             
             // Now we have a candidate!
             return m;
