b39efc8481e46ff6692d0fffabb41bc28b013983
thekid <timm.friebe@gmail.com>
2005-07-29 14:25:02 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5473 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/arena/easc/php/wrapper/Number.class.php
@@ -0,0 +1,18 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * The abstract class Number is the superclass of classes representing
+   * numbers
+   *
+   * @model    abstract
+   * @purpose  Base class
+   */
+  class Number extends Object {
+    var
+      $value = '';
+  }
+?>
