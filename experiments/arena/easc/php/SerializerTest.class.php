1276c651da17a6fa1335b59aa138872a2376d88c
thekid <timm.friebe@gmail.com>
2005-07-29 13:40:23 +0000

    - Add two array tests:   . representationOfIntegerArray()   . representationOfStringArray()
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5461 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/easc/php/SerializerTest.class.php
+++ experiments/arena/easc/php/SerializerTest.class.php
@@ -68,5 +68,33 @@
     function representationOfString() {
       $this->assertEquals('s:11:"Hello World";', Serializer::representationOf($var= 'Hello World'));
     }
+    
+    /**
+     * Test serialization of an array containing three integers 
+     * (1, 2 and 5)
+     *
+     * @access  public
+     */
+    #[@test]
+    function representationOfIntegerArray() {
+      $this->assertEquals(
+        'a:3:{i:0;i:1;i:1;i:2;i:2;i:5;}', 
+        Serializer::representationOf($var= array(1, 2, 5))
+      );
+    }
+    
+    /**
+     * Test serialization of an array containing two strings 
+     * ("More" and "Power")
+     *
+     * @access  public
+     */
+    #[@test]
+    function representationOfStringArray() {
+      $this->assertEquals(
+        'a:2:{i:0;s:4:"More";i:1;s:5:"Power";}', 
+        Serializer::representationOf($var= array('More', 'Power'))
+      );
+    }
   }
 ?>
