f62265860e119b8ece34b19f01d42ca37c00e371
thekid <timm.friebe@gmail.com>
2005-07-29 10:28:45 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5438 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/arena/easc/java/src/net/xp_framework/easc/unittest/MethodMatcherTest.java
@@ -0,0 +1,21 @@
+/* This class is part of the XP framework's EAS connectivity
+ *
+ * $Id$
+ */
+
+package net.xp_framework.easc.unittest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Test method matching functionality. 
+ *
+ * Note: This is a JUnit 4 testcase!
+ *
+ * @see   net.xp_framework.easc.util.MethodMatcher
+ */
+public class MethodMatcherTest {
+
+
+}
