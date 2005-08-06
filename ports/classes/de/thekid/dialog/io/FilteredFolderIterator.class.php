1375e90f9613f0c55df3c70a7352d277f901e86a
thekid <timm.friebe@gmail.com>
2005-08-06 12:28:43 +0000

    - Implement recursing into subdirectories - Return fully qualified file name from next() # The latter is a BC break
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5487 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/classes/de/thekid/dialog/io/FilteredFolderIterator.class.php
+++ ports/classes/de/thekid/dialog/io/FilteredFolderIterator.class.php
@@ -4,6 +4,8 @@
  * $Id$ 
  */
 
+  uses('io.Folder');
+
   /**
    * Iterates over elements a directory, only returning those elements that
    * match the given pattern.
@@ -20,8 +22,9 @@
    */
   class FilteredFolderIterator extends Object {
     var
-      $pattern = '',
-      $folder  = NULL;
+      $pattern   = '',
+      $folders   = array(),
+      $recursive = FALSE;
     
     var
       $_entry  = FALSE;
@@ -32,10 +35,12 @@
      * @access  public
      * @param   &io.Folder folder
      * @param   string pattern regular expression
+     * @param   bool recursive default FALSE whether to recurse into subdirectories
      */
-    function __construct(&$folder, $pattern) {
-      $this->folder= &$folder;
+    function __construct(&$folder, $pattern, $recursive= FALSE) {
+      $this->folders= array(&$folder);
       $this->pattern= $pattern;
+      $this->recursive= $recursive;
     }
     
     /**
@@ -48,7 +53,14 @@
      */
     function hasNext() {
       do {
-        if (FALSE === ($this->_entry= $this->folder->getEntry())) return FALSE;
+        if (FALSE === ($this->_entry= $this->folders[0]->getEntry())) {
+          array_shift($this->folders);
+          if (empty($this->folders)) return FALSE;
+          continue;
+        }
+        if ($this->recursive && is_dir($this->folders[0]->getURI().$this->_entry)) {
+          array_unshift($this->folders, new Folder($this->folders[0]->getURI().$this->_entry));
+        }
       } while (!preg_match($this->pattern, $this->_entry));
 
       return TRUE;
@@ -58,14 +70,14 @@
      * Returns the next element in the iteration.
      *
      * @access  public
-     * @return  string the non-fully qualified name of the element
+     * @return  string the fully qualified name of the element
      * @throws  util.NoSuchElementException when there are no more elements
      */
     function next() {
       if (FALSE === $this->_entry) {
         return throw(new NoSuchElementException('No more '.$this->pattern.' entries'));
       }
-      return $this->_entry;
+      return $this->folders[0]->getURI().$this->_entry;
     }  
 
   } implements(__FILE__, 'util.Iterator');
