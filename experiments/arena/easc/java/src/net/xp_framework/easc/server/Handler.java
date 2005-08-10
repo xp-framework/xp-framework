8dffff898077c7ff0861f2d57ef214a1e6d7cedb
thekid <timm.friebe@gmail.com>
2005-08-10 14:39:22 +0000

    - Add apidoc
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5543 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/easc/java/src/net/xp_framework/easc/server/Handler.java
+++ experiments/arena/easc/java/src/net/xp_framework/easc/server/Handler.java
@@ -9,7 +9,19 @@ import java.io.IOException;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 
+/**
+ * Handler interface
+ *
+ */
 public interface Handler {
 
+    /**
+     * Handle client
+     *
+     * @access  public
+     * @param   java.io.DataInputStream in
+     * @param   java.io.DataOutputStream out
+     * @throws  java.io.IOException
+     */
     public void handle(DataInputStream in, DataOutputStream out) throws IOException;
 }
