880981c69f0b4aa5adafe71a521106ae2e0a8ff0
thekid <timm.friebe@gmail.com>
2005-08-23 09:41:04 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5577 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/arena/easc/java/src/net/xp_framework/easc/unittest/ITest.java
@@ -0,0 +1,15 @@
+/* This class is part of the XP framework's EAS connectivity
+ *
+ * $Id$
+ */
+
+package net.xp_framework.easc.unittest;
+
+/**
+ * Test interface
+ *
+ * @purpose Base for Proxy generator
+ */
+public interface ITest {
+    
+}
