77ae69144d6a5699e2cb86b6221cf3db667b6be3
thekid <timm.friebe@gmail.com>
2005-08-26 12:27:18 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5650 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/net/xp-framework/unittest/reflection/TestClass.class.php
@@ -0,0 +1,74 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses('util.Date');
+
+  /**
+   * Test class
+   *
+   * @see      xp://net.xp-framework.unittest.reflection.XPClassTest
+   * @purpose  Test class
+   */
+  #[@test('Annotation')]
+  class TestClass extends Object {
+    var
+      #[@type('util.Date')]
+      $date= NULL;
+
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   mixed in
+     */
+    function __construct($in) {
+      $this->date= &new Date($in);
+    }
+    
+    /**
+     * Retrieve date
+     *
+     * @access  public
+     * @return  &util.Date
+     */    
+    function &getDate() {
+      return $this->date;
+    }
+
+    /**
+     * Set date
+     *
+     * @access  public
+     * @param   &util.Date date
+     */    
+    function setDate(&$date) {
+      $this->date= &$date;
+    }
+    
+    /**
+     * Retrieve current date as UN*X timestamp
+     *
+     * @access  public
+     * @return  int
+     */
+    #[@webmethod, @security(roles= array('admin', 'god'))]
+    function currentTimestamp() {
+      return time();
+    }
+    
+    /**
+     * Set a trace for debugging
+     *
+     * @access  public
+     * @param   &util.log.LogCategory cat
+     * @throws  lang.IllegalStateException
+     */
+    function setTrace(&$cat) {
+      return throw(new IllegalStateException('Not debuggable yet'));
+    }
+
+  } implements(__FILE__, 'util.log.Traceable');
+?>
