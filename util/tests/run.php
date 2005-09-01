ac3143f79de958aec5e43a7c08aa76ade9f87122
thekid <timm.friebe@gmail.com>
2005-09-01 08:43:24 +0000

    - Add usage - Make it possible to run tests without a configuration, by supplying   the "-c <ClassName>" command line switch
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5691 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- util/tests/run.php
+++ util/tests/run.php
@@ -5,52 +5,136 @@
  */
   require('lang.base.php');
   xp::sapi('cli');
-  uses('util.profiling.unittest.TestSuite', 'util.Properties');
+  uses(
+    'util.profiling.unittest.TestSuite',
+    'util.Properties'
+  );
   
-  // {{{ main
-  $p= &new ParamString();
-  if (!$p->exists(1)) {
-    printf("Usage: %s configuration\n", $p->value(0));
-    exit(-1);
-  }
+  // {{{ proto void addTest(&util.profiling.unittest.TestSuite suite, &lang.XPClass class [, array arguments])
+  //     Adds a test
+  function addTest(&$suite, &$class, $arguments= array()) {
   
-  $config= &new Properties($p->value(1));
-  $suite= &new TestSuite();
-  
-  $section= $config->getFirstSection();
-  do {
-    try(); {
-      $class= &XPClass::forName($config->readString($section, 'class'));
-    } if (catch('ClassNotFoundException', $e)) {
-      Console::write('*** Error: Test group "'.$section.'" ~ ', $e->toString());
-      exit(-2);
+    // Sanity check
+    if (!$class->isSubclassOf('util.profiling.unittest.TestCase')) {
+      Console::writeLine('*** Error: ', $class->getName(), ' is not a TestCase');
+      exit(-3);
     }
-    
+
+    // Iterate over methods, adding all method annotated with @test (but not @ignore)
     for ($methods= $class->getMethods(), $i= 0, $s= sizeof($methods); $i < $s; $i++) {
-      $name= $methods[$i]->getName();
-      
-      // Ignore non-test and non-public methods
       if (!$methods[$i]->hasAnnotation('test')) continue;
+      
       if ($methods[$i]->hasAnnotation('ignore')) {
         Console::writeLinef(
-          '*** Ignoring %s (%s)', 
-          $name,
+          '     >> Ignoring %s::%s (%s)', 
+          $class->getName(TRUE), 
+          $methods[$i]->getName(),
           $methods[$i]->getAnnotation('ignore')
         );
         continue;
       }
       
       // Add test method
-      $arguments= array_merge($name, $config->readArray($section, 'args'));
-      $suite->addTest(call_user_func_array(
-        array(&$class, 'newInstance'), 
+      Console::writeLinef('     >> Adding test %s::%s', $class->getName(), $methods[$i]->getName(TRUE));
+      $suite->addTest(call_user_func_array(array(&$class, 'newInstance'), array_merge(
+        (array)$methods[$i]->getName(TRUE),
         $arguments
-      ));
+      )));
     }
-  } while ($section= $config->getNextSection());
+
+    // Print warning and exit if no test are found
+    if (0 == $suite->numTests()) {
+      Console::writeLine('*** Warning: No tests found in ', $class->getName());
+      exit(-4);
+    }
+  }
+  // }}}
   
-  // Run test suite
+  // {{{ main
+  $p= &new ParamString();
+  if (!$p->exists(1) || $p->exists('help', '?')) {
+    Console::writeLinef(<<<__
+Console-based test runner for XP unit tests
+
+Usage: 
+  1) Load test from property-file
+     \$ %1\$s %2\$s configuration.ini [<section>]
+
+     The configuration file is expected to have the following format:
+     
+     -- Sample ------------------------------------------------------
+     [dict]
+     class="net.xp-framework.unittest.peer.DictTest"
+     args="test.dict.org|2628"
+
+     [ldap]
+     class="net.xp-framework.unittest.peer.LDAPTest"
+     ----------------------------------------------------------------
+     
+     The args line is optional.
+
+
+  2) Specify a class to run:
+     \$ %1\$s %2\$s --class|-c classname [--arguments|-a arguments]
+     
+     The class name is expected to extend the base class for all test
+     cases, util.profiling.unittest.TestCase.
+     
+     Arguments is comma-separated, e.g. "test.dict.org,2628"
+
+__
+,
+      $p->value(-1),
+      $p->value(0)
+    );
+    exit(-1);
+  }
+
+  $tests= array();
+  if ($p->exists('class')) {        // Class based  
+    $name= $p->value('class');
+    Console::writeLinef('===> Using class %s', $name);
+
+    $tests[]= array($name, explode(',', $p->value('arguments', 'a', '')));
+  } else {                          // Property-file based
+    $config= &new Properties($p->value(1));
+
+    Console::writeLinef('===> Using configuration from %s', $p->value(1));
+    if ($p->exists(2)) {
+      $section= $p->value(2);
+      Console::writeLinef('---> Adding tests from section %s', $section);
+      
+      $tests[]= array(
+        $config->readString($section, 'class'),
+        $config->readArray($section, 'args')
+      );
+    } else {
+      Console::writeLinef('---> Adding all tests');
+      $section= $config->getFirstSection();
+      do {
+        $tests[0]= array(
+          $config->readString($section, 'class'),
+          $config->readArray($section, 'args')
+        );
+      } while ($section= $config->getNextSection());
+    }
+  }
+  
+  Console::writeLine('===> Setting up suite');
+  $suite= &new TestSuite();
+  foreach ($tests as $test) {
+    try(); {
+      $class= &XPClass::forName($test[0]);
+    } if (catch('ClassNotFoundException', $e)) {
+      Console::write('*** Error: Class "'.$test[0].'" ~ ', $e->toString());
+      exit(-2);
+    }
+    
+    addTest($suite, $class, $test[1]);
+  }
+
+  Console::writeLine('===> Running test suite');
   $result= &$suite->run();
-  Console::writeLine($result->toString());
+  Console::write($result->toString());
   // }}}
 ?>
