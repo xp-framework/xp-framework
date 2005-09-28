85ad46ed5d509c65a48b6b258bba928cf5d3f338
thekid <timm.friebe@gmail.com>
2005-09-28 16:15:58 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5857 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/arena/easc/clients/net/xp_framework/beans/stateless/CalculatorHome.class.php
@@ -0,0 +1,23 @@
+<?php
+/* This file is part of the XP framework's EASC API
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Calculator home interface
+   *
+   * @purpose  Demo class  
+   */
+  class CalculatorHome extends Interface {
+  
+    /**
+     * Create method
+     *
+     * @access  public
+     * @return  &net.xp_framework.beans.stateless.Calculator
+     */
+    function &create() { }
+  
+  }
+?>
