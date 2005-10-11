23c8698eb2f303b40ca88c49c348693ed4bcdad1
thekid <timm.friebe@gmail.com>
2005-10-11 13:46:11 +0000

    - Add toString() method
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5983 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/easc/clients/net/xp_framework/beans/entities/FeedValue.class.php
+++ experiments/arena/easc/clients/net/xp_framework/beans/entities/FeedValue.class.php
@@ -10,5 +10,28 @@
    * @purpose  Demo class  
    */
   class FeedValue extends Object {
+
+    /**
+     * Returns a string representation of this object
+     *
+     * @access  public
+     * @return  string
+     */
+    function toString() {
+      return sprintf(
+        "%s(pk= %s){\n".
+        "  [title]      %s\n".
+        "  [url]        %s\n".
+        "  [bz_id]      %s\n".
+        "  [lastchange] %s\n".
+        "}",
+        $this->getClassName(),
+        xp::stringOf($this->pk),
+        xp::stringOf($this->title),
+        xp::stringOf($this->url),
+        xp::stringOf($this->bz_id),
+        xp::stringOf($this->lastchange)
+      );
+    }
   }  
 ?>
