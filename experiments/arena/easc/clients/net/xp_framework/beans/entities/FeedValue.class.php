90381310236e15c899668543b144d53157ffebf2
thekid <timm.friebe@gmail.com>
2005-10-11 13:26:01 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5982 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/arena/easc/clients/net/xp_framework/beans/entities/FeedValue.class.php
@@ -0,0 +1,14 @@
+<?php
+/* This file is part of the XP framework's EASC API
+ *
+ * $Id$
+ */
+
+  /**
+   * Feed value value object
+   *
+   * @purpose  Demo class  
+   */
+  class FeedValue extends Object {
+  }  
+?>
