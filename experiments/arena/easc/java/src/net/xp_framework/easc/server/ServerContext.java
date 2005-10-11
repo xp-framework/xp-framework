5b3fbee9b0ba93e57d88dceaff10d68a2cb92971
thekid <timm.friebe@gmail.com>
2005-10-11 10:27:31 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5969 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/arena/easc/java/src/net/xp_framework/easc/server/ServerContext.java
@@ -0,0 +1,11 @@
+/* This class is part of the XP framework's EAS connectivity
+ *
+ * $Id$
+ */
+
+package net.xp_framework.easc.server;
+
+import java.util.HashMap;
+
+public class ServerContext extends HashMap<Integer, Object> {
+}
