c13b8ad095b21d54b6794dc7ad4344de15b97704
thekid <timm.friebe@gmail.com>
2005-10-12 16:36:59 +0000

    - Add activate and deactivate operations
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@5994 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/easc/clients/feed.php
+++ experiments/arena/easc/clients/feed.php
@@ -7,6 +7,16 @@
   xp::sapi('cli');
   ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.'../php/'.PATH_SEPARATOR.'.');
   uses('Remote', 'util.cmd.ParamString');
+  
+  // {{{ bool equals(lang.Object object, lang.Object compare)
+  //     Compares two objects.
+  function equals(&$object, &$compare) {
+    return (
+      (NULL === $object && NULL === $compare) ||
+      $object->equals($compare)
+    );
+  }
+  // }}}
 
   // {{{ main
   $p= &new ParamString();
@@ -28,6 +38,12 @@ $ php feed.php <hostname> [<command> [<arguments>]] [-p <port> ] [-j <jndi_name>
 
     - get <id>
       Gets a feed by a supplied feed id
+
+    - activate <id>
+      Activates a feed by a supplied feed id (bz_id 500)
+
+    - deactivate <id>
+      Deactivates a feed by a supplied feed id (bz_id 30000)
 
     - create <title> <url> [<bz_id>]
       Creates a new feed
@@ -70,6 +86,28 @@ __
         $instance= &$home->create($value);
         Console::writeLine(xp::stringOf($instance->getFeedValue()));
       }
+      break;
+    
+    case 'deactivate':
+      $feed= &$home->findByPrimaryKey(new Long($p->value(3)));
+      if (equals(new Long(30000), $feed->getBz_id())) {
+        Console::write('(Already deactivated): ');
+      } else {
+        Console::write('Deactivating: ');
+        $feed->setBz_id(new Long(30000));
+      }
+      Console::writeLine(xp::stringOf($feed->getFeedValue()));
+      break;
+
+    case 'activate':
+      $feed= &$home->findByPrimaryKey(new Long($p->value(3)));
+      if (equals(new Long(500), $feed->getBz_id())) {
+        Console::write('(Already activated): ');
+      } else {
+        Console::write('Activating: ');
+        $feed->setBz_id(new Long(500));
+      }
+      Console::writeLine(xp::stringOf($feed->getFeedValue()));
       break;
     
     case 'list':
