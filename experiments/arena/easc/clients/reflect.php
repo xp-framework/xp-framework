99588f2c713939f3f2b48e486920b662daef0cf9
thekid <timm.friebe@gmail.com>
2005-12-21 12:44:04 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6319 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/arena/easc/clients/reflect.php
@@ -0,0 +1,43 @@
+<?php
+/* This file is part of the XP framework's EASC API
+ *
+ * $Id$ 
+ */
+  require('lang.base.php');
+  xp::sapi('cli');
+  uses('remote.Remote', 'util.cmd.ParamString');
+  
+  // {{{ main
+  $p= &new ParamString();
+  if (!$p->exists(1)) {
+    Console::writeLine(<<<__
+EASC entity bean demo application
+
+Usage
+-----
+$ php feed.php <hostname> [-p <port> ]
+  
+  * hostname is the host name (or IP) that your JBoss + XP-MBean server 
+    is running on. The feed entity bean (from the easc/beans directory) 
+    is expected to be deployed.
+  
+  * port is the port the ESDL-MBean is listening on. It defaults to 6449.
+  
+__
+    );
+    exit(1);
+  }
+  
+  try(); {
+    $remote= &Remote::forName('xp://'.$p->value(1).':'.$p->value('port', 'p', 6449).'/');
+    $remote && $services= &$remote->lookup('Services');
+  } if (catch('Exception', $e)) {
+    $e->printStackTrace();
+    exit(-1);
+  }
+
+  foreach ($services as $name => $description) {
+    Console::writeLine(xp::stringOf($description));
+  }
+  // }}}
+?>
