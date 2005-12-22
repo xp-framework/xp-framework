be7137b7dbf6ead21aa4be1fb930737857cbf860
thekid <timm.friebe@gmail.com>
2005-12-22 11:37:02 +0000

    - Add optional command line argument "-b" which will introspect   a single EJB specified by the arguments` value
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6335 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/easc/clients/reflect.php
+++ experiments/arena/easc/clients/reflect.php
@@ -15,7 +15,7 @@ ESDL demo application
 
 Usage
 -----
-$ php reflect.php <hostname> [-p <port> ]
+$ php reflect.php <hostname> [-p <port>] [-b <jndiname>]
   
   * hostname is the host name (or IP) that your JBoss + XP-MBean server 
     is running on. The feed entity bean (from the easc/beans directory) 
@@ -23,6 +23,8 @@ $ php reflect.php <hostname> [-p <port> ]
   
   * port is the port the ESDL-MBean is listening on. It defaults to 6449.
   
+  * jndiname is the name of the bean. If this parameter is omitted,
+    all deployed beans are listed.
 __
     );
     exit(1);
@@ -36,8 +38,12 @@ __
     exit(-1);
   }
 
-  foreach ($services->beans() as $description) {
-    Console::writeLine(xp::stringOf($description));
+  if ($p->exists('bean')) {
+    Console::writeLine(xp::stringOf($services->bean($p->value('bean'))));
+  } else {
+    foreach ($services->beans() as $description) {
+      Console::writeLine(xp::stringOf($description));
+    }
   }
   // }}}
 ?>
