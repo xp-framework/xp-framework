e132749da21466e2cbd0d39bc122c8428b6cbd37
thekid <timm.friebe@gmail.com>
2006-01-16 16:12:31 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6430 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/net/xp_framework/unittest/scriptlet/workflow/AbstractCasterTest.class.php
@@ -0,0 +1,54 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('util.profiling.unittest.TestCase');
+  
+  /**
+   * Scriptlet/Caster test case
+   *
+   * @model     abstract
+   * @see       xp://scriptlet.xml.workflow.casters.ParamCaster
+   * @purpose   Base class for Caster test
+   */
+  class AbstractCasterTest extends TestCase {
+    var
+      $caster = NULL;
+
+    /**
+     * Return the caster
+     *
+     * @access  protected
+     * @return  &scriptlet.xml.workflow.casters.ParamCaster
+     */
+    function &caster() { }
+
+    /**
+     * Setup method.
+     *
+     * @model   final
+     * @access  public
+     */
+    function setUp() {
+      $this->caster= &$this->caster();
+    }
+
+    /**
+     * Helper method that uses the caster to cast a value. Returns 
+     * the casted value.
+     *
+     * @access  protected
+     * @param   mixed value
+     * @return  mixed
+     * @throws  util.profiling.unittest.AssertionFailedError in case the caster fails
+     */
+    function castValue($value) {
+      if (!is_array($casted= call_user_func(array(&$this->caster, 'castValue'), array((string)$value)))) {
+        return $this->fail('Cast failed', array(), $value);
+      }
+      return array_pop($casted);
+    }
+  }
+?>
