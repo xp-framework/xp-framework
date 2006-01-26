db6053a1e3571a9352f8c3a8aaacf579e4e91b0f
thekid <timm.friebe@gmail.com>
2006-01-26 14:50:03 +0000

    - Remove assertEquivalent() method, it compared objects using their   equals() methods. assertEquals() already takes care of this now
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6483 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/net/xp_framework/unittest/DateUtilTest.class.php
+++ skeleton/net/xp_framework/unittest/DateUtilTest.class.php
@@ -14,19 +14,6 @@
   class DateUtilTest extends TestCase {
   
     /**
-     * Helper method to compare two objects with
-     * their equals method.
-     *
-     * @access  public
-     * @param   &lang.Object expect
-     * @param   &lang.Object var
-     * @param   string error
-     */
-    function assertEquivalent(&$expect, &$var, $error= 'not_equivalent') {
-      assert('$this->_test($expect->equals($var), $error, $expect, $var)');
-    }
-
-    /**
      * Test simple add operations on a usual date.
      *
      * @access  public
@@ -35,27 +22,27 @@
     function testSimpleAddition() {
       $date= &new Date(Date::mktime(12, 15, 11, 1, 1, 2000));
       
-      $this->assertEquivalent(
+      $this->assertEquals(
         new Date(Date::mktime(12, 15, 30, 1, 1, 2000)),
         DateUtil::addSeconds($date, 19)
       );
       
-      $this->assertEquivalent(
+      $this->assertEquals(
         new Date(Date::mktime(12, 44, 11, 1, 1, 2000)),
         DateUtil::addMinutes($date, 29)
       );
       
-      $this->assertEquivalent(
+      $this->assertEquals(
         new Date(Date::mktime(13, 15, 11, 1, 1, 2000)),
         DateUtil::addHours($date, 1)
       );
       
-      $this->assertEquivalent(
+      $this->assertEquals(
         new Date(Date::mktime(12, 15, 11, 1, 2, 2000)),
         DateUtil::addDays($date, 1)
       );
       
-      $this->assertEquivalent(
+      $this->assertEquals(
         new Date(Date::mktime(12, 15, 11, 2, 1, 2000)),
         DateUtil::addMonths($date, 1)
       );
@@ -70,27 +57,27 @@
     function testSimpleSubstraction() {
       $date= &new Date(Date::mktime(12, 15, 11, 1, 1, 2000));
       
-      $this->assertEquivalent(
+      $this->assertEquals(
         new Date(Date::mktime(12, 14, 52, 1, 1, 2000)),
         DateUtil::addSeconds($date, -19)
       );
       
-      $this->assertEquivalent(
+      $this->assertEquals(
         new Date(Date::mktime(11, 46, 11, 1, 1, 2000)),
         DateUtil::addMinutes($date, -29)
       );
       
-      $this->assertEquivalent(
+      $this->assertEquals(
         new Date(Date::mktime(11, 15, 11, 1, 1, 2000)),
         DateUtil::addHours($date, -1)
       );
       
-      $this->assertEquivalent(
+      $this->assertEquals(
         new Date(Date::mktime(12, 15, 11, 12, 31, 1999)),
         DateUtil::addDays($date, -1)
       );
       
-      $this->assertEquivalent(
+      $this->assertEquals(
         new Date(Date::mktime(12, 15, 11, 12, 1, 1999)),
         DateUtil::addMonths($date, -1)
       );
@@ -105,12 +92,12 @@
     function testLeapYear() {
       $date= &new Date(Date::mktime(0, 0, 0, 2, 1, 2000));
       
-      $this->assertEquivalent(
+      $this->assertEquals(
         new Date(Date::mktime(0, 0, 0, 3, 1, 2000)),
         DateUtil::addMonths($date, 1)
       );
       
-      $this->assertEquivalent(
+      $this->assertEquals(
         new Date(Date::mktime(0, 0, 0, 3, 2, 2000)),
         DateUtil::addDays($date, 30)
       );
@@ -125,12 +112,12 @@
     function testNonLeapYear() {
       $date= &new Date(Date::mktime(0, 0, 0, 2, 1, 1999));
       
-      $this->assertEquivalent(
+      $this->assertEquals(
         new Date(Date::mktime(0, 0, 0, 3, 1, 1999)),
         DateUtil::addMonths($date, 1)
       );
       
-      $this->assertEquivalent(
+      $this->assertEquals(
         new Date(Date::mktime(0, 0, 0, 3, 3, 1999)),
         DateUtil::addDays($date, 30)
       );
