9824bc82593faeef300d44489f5b10edb554d75b
thekid <timm.friebe@gmail.com>
2006-01-28 23:01:41 +0000

    - Show usage when given no arguments or "-?" as first argument
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6495 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/people/friebe/rfc0052/Hello.class.php
+++ experiments/people/friebe/rfc0052/Hello.class.php
@@ -6,7 +6,17 @@
 import util·cmd·Console;
 
 public class Hello {
+
+  public static function usage() {
+    Console::writeLine(
+      "Usage: php -dauto_prepend_file=xp.php Hello.class.php <Name>\n".
+      "* Name is whom to say hello to"
+    );
+  }
+
   public static function main($args) {
+    if (!isset($args[1]) || '-?' == $args[1]) return self::usage();
+
     Console::writeLine('Hello ', $args[1]);
   }
 }
