085dace44c927a4dfb4202408bf815991f8ed304
thekid <timm.friebe@gmail.com>
2006-01-29 01:44:11 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6510 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/people/friebe/rfc0052/Instanciate.class.php
@@ -0,0 +1,19 @@
+/* This class is part of the XP framework's experiments
+ *
+ * $Id$ <?php
+ */
+
+import util·cmd·Console;
+
+public class Instanciate {
+
+  public static function main($args) {
+    try {
+      $class= XPClass::forName($args[1]);
+    } catch (ClassNotFoundException $e) {
+      Console::writeLine($e->toString());
+      return 1;
+    }
+    Console::writeLine($class->getName(), '::newInstance()= ', xp::stringOf($class->newInstance()));
+  }
+}
