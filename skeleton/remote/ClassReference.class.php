36027e5fbbd488ebcc4cf09008439e88af214390
thekid <timm.friebe@gmail.com>
2006-02-03 11:33:01 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6525 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/remote/ClassReference.class.php
@@ -0,0 +1,49 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Holds a reference to a class
+   *
+   * @see      xp://remote.Serializer
+   * @purpose  Class reference
+   */
+  class ClassReference extends Object {
+    var 
+      $classname = '',
+      $fields    = array();
+
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   string classname
+     */
+    function __construct($classname) {
+      $this->classname= $classname;
+    }
+
+    /**
+     * Retrieved referenced class name
+     *
+     * @access  public
+     * @return  string
+     */
+    function referencedName() {
+      return $this->classname;
+    }
+
+    /**
+     * Retrieved referenced class name
+     *
+     * @access  public
+     * @param   lang.ClassLoader cl default NULL
+     * @return  &lang.XPClass
+     */
+    function &referencedClass($cl= NULL) {
+      return XPClass::forName($this->classname, $cl);
+    }
+  }
+?>
