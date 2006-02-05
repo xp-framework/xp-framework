361c3261c5039a5f36064d03284e76e9672fa171
thekid <timm.friebe@gmail.com>
2006-02-05 22:40:23 +0000

    - Refactor opcode handler creation into its own function using newinstance()
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6555 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/people/friebe/executor/test.php
+++ experiments/people/friebe/executor/test.php
@@ -35,84 +35,77 @@
     return new $name();
   }
   // }}}
+  
+  // {{{ &OpcodeHandler opcode(string bytes)
+  //     Creates an opcode handler
+  function &opcode($bytes) {
+    return newinstance('OpcodeHandler', '{
+      function handle(&$context, $args) {
+        '.$bytes.'
+      }
+    }');
+  }
+  // }}}
 
   // {{{ handlers
   $handlers= array();
-  $handlers['OP_NEW']= &newinstance('OpcodeHandler', '{
-    function handle(&$context, $args) {
-      $classname= xp::reflect($args[0]);
-      $context["T"]= &new $classname();
-    }
-  }');
-  $handlers['OP_ASSIGN']= &newinstance('OpcodeHandler', '{
-    function handle(&$context, $args) {
-      $context["symbols"][$args[0]]= &$context["T"];
-    }
-  }');
-  $handlers['OP_PRINTLN']= &newinstance('OpcodeHandler', '{
-    function handle(&$context, $args) {
-      Console::writeLine(xp::stringOf($context["symbols"][$args[0]]));
-    }
-  }');
-  $handlers['OP_INVOKE']= &newinstance('OpcodeHandler', '{
-    function handle(&$context, $args) {
-      $context["T"]= call_user_func_array(
-        array($context["symbols"][$args[0]], $args[1]),
-        $args[2]
-      );
-    }
-  }');
-  $handlers['OP_TRY']= &newinstance('OpcodeHandler', '{
-    function handle(&$context, $args) {
-      $context["E"]= NULL;
-    }
-  }');
-  $handlers['OP_CATCH']= &newinstance('OpcodeHandler', '{
-    function handle(&$context, $args) {
-      if (!$context["E"]) {
+  $handlers['OP_NEW']= &opcode('
+    $classname= xp::reflect($args[0]);
+    $context["T"]= &new $classname();
+  ');
+  $handlers['OP_ASSIGN']= &opcode('
+    $context["symbols"][$args[0]]= &$context["T"];
+  ');
+  $handlers['OP_PRINTLN']= &opcode('
+    Console::writeLine(xp::stringOf($context["symbols"][$args[0]]));
+  ');
+  $handlers['OP_INVOKE']= &opcode('
+    $context["T"]= call_user_func_array(
+      array($context["symbols"][$args[0]], $args[1]),
+      $args[2]
+    );
+  ');
+  $handlers['OP_TRY']= &opcode('
+    $context["E"]= NULL;
+  ');
+  $handlers['OP_CATCH']= &opcode('
+    if (!$context["E"]) {
 
-        // Search for end catch opcode
-        for ($i= $context["O"]->offset; $i < $context["O"]->size; $i++) {
-          if ("OP_END_CATCH" != $context["O"]->opcodes[$i][0]) continue;
-          $context["O"]->offset= $i- 1;
-          return;
-        }
-      }
-      
-      $context["symbols"][$args[1]]= &$context["E"];
-    }
-  }');
-  $handlers['OP_END_CATCH']= &newinstance('OpcodeHandler', '{
-    function handle(&$context, $args) {
-      $context["E"]= NULL;
-    }
-  }');
-  $handlers['OP_THROW']= &newinstance('OpcodeHandler', '{
-    function handle(&$context, $args) {
-      $classname= xp::reflect($args[0]);
-      $exception= &new $classname();
-      
-      // Search for catch opcode
+      // Search for end catch opcode
       for ($i= $context["O"]->offset; $i < $context["O"]->size; $i++) {
-        if ("OP_CATCH" != $context["O"]->opcodes[$i][0]) continue;
-        
-        // Check whether exception was caught by the found opcode
-        if (!is($context["O"]->opcodes[$i][1][0], $exception)) continue;
-        
-        // We have found the correct opcode
-        $context["E"]= &$exception;
+        if ("OP_END_CATCH" != $context["O"]->opcodes[$i][0]) continue;
         $context["O"]->offset= $i- 1;
         return;
       }
-      
-      xp::error("Uncaught exception ".$exception->toString());
     }
-  }');
-  $handlers['OP_EXIT']= &newinstance('OpcodeHandler', '{
-    function handle(&$context, $args) {
-      exit(isset($args[0]) ? $args[0] : 0);
+
+    $context["symbols"][$args[1]]= &$context["E"];
+  ');
+  $handlers['OP_END_CATCH']= &opcode('
+    $context["E"]= NULL;
+  ');
+  $handlers['OP_THROW']= &opcode('
+    $classname= xp::reflect($args[0]);
+    $exception= &new $classname();
+
+    // Search for catch opcode
+    for ($i= $context["O"]->offset; $i < $context["O"]->size; $i++) {
+      if ("OP_CATCH" != $context["O"]->opcodes[$i][0]) continue;
+
+      // Check whether exception was caught by the found opcode
+      if (!is($context["O"]->opcodes[$i][1][0], $exception)) continue;
+
+      // We have found the correct opcode
+      $context["E"]= &$exception;
+      $context["O"]->offset= $i- 1;
+      return;
     }
-  }');
+
+    xp::error("Uncaught exception ".$exception->toString());
+  ');
+  $handlers['OP_EXIT']= &opcode('
+    exit(isset($args[0]) ? $args[0] : 0);
+  ');
   // }}}
   
   // {{{ opcodes
