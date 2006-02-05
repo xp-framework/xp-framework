c20e1bc2b41d7930d7aeac6b1b5f65562bf8395e
thekid <timm.friebe@gmail.com>
2006-02-05 13:23:37 +0000

    - Add new tests:   . exceptionsBreakChain()   . nullThrowsNPE()
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6535 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/people/friebe/chain/ChainTest.class.php
+++ experiments/people/friebe/chain/ChainTest.class.php
@@ -27,5 +27,27 @@
       $cat= &chain(Logger::getInstance(), 'getCategory()');
       $this->assertClass($cat, 'util.log.LogCategory');
     }
+
+    /**
+     * Tests XPClass::forName($class)->newInstance() will throw an 
+     * exception if $class is not existant
+     *
+     * @access  public
+     */
+    #[@test, @expect('lang.ClassNotFoundException')]
+    function exceptionsBreakChain() {
+      chain(XPClass::forName('@@NOTEXISTANTCLASS@@'), 'newInstance()');
+    }
+
+    /**
+     * Tests NULL doesn't cause fatal errors (e.g. $instance->toString()
+     * where $instance is NULL)
+     *
+     * @access  public
+     */
+    #[@test, @expect('lang.NullPointerException')]
+    function nullThrowsNPE() {
+      chain($instance= NULL, 'toString()');
+    }
   }
 ?>
