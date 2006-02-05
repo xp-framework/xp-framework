55bbce221b8bd6276411901d975980b4c306d454
thekid <timm.friebe@gmail.com>
2006-02-05 13:39:40 +0000

    - Add support for method calls with arguments
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6537 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/people/friebe/chain/chain-function.php
+++ experiments/people/friebe/chain/chain-function.php
@@ -16,9 +16,25 @@
       if (!is_object($begin)) {
         return throw(new NullPointerException('Call to method '.func_get_arg($i).' on '.xp::typeOf($begin)));
       }
+      
+      // Check what we're getting:
+      $arg= func_get_arg($i);
+      if ('(' == $arg[strlen($arg) - 1]) {
+        // * Method call with argument: "getCategory(", $arg, ")"
+        $args= array();
+        $j= 0;
+        while (')' != func_get_arg(++$i)) {
+          $args[]= func_get_arg($i);
+          $arg.= '$args['.$j++.'], ';
+        }
+        $arg= substr($arg, 0, -2).')';
+      } else {
+        // * Method call without arguments: "toString()"
+      }
 
       try(); {
-        eval('$begin= &$begin->'.func_get_arg($i).';');
+        // DEBUG print("Evaluating \$begin->".$arg.";\n");
+        eval('$begin= &$begin->'.$arg.';');
       } if (catch('Throwable', $e)) {
         return throw($e);
       }
