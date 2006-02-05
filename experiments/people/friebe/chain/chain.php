10a5addf50cacabac6bee0ad0af67d34cf0e8e26
thekid <timm.friebe@gmail.com>
2006-02-05 13:19:42 +0000

    - Move chain function to chain-function.php
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6534 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/people/friebe/chain/chain.php
+++ experiments/people/friebe/chain/chain.php
@@ -4,29 +4,7 @@
  * $Id$ 
  */
   require('lang.base.php');
-  
-  function &chain(&$begin) {
-  
-    // Border case #1: Exception already thrown at entry
-    if ($e= xp::registry('exceptions')) return throw($e[key($e)]);
-    
-    // Iterate over chain
-    for ($i= 1; $i < func_num_args(); $i++) {
-      
-      // Make sure $begin is an object
-      if (!is_object($begin)) {
-        return throw(new NullPointerException('Call to method '.func_get_arg($i).' on '.xp::typeOf($begin)));
-      }
-
-      try(); {
-        eval('$begin= &$begin->'.func_get_arg($i).';');
-      } if (catch('Throwable', $e)) {
-        return throw($e);
-      }
-    }
-
-    return $begin;
-  }
+  include('chain-function.php');
   
   // {{{ main
   // What we would like to be able to do:
