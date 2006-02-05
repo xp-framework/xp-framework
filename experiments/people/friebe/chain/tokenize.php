e427e7cbfb0ab5c861281796bafbb2813549e64e
thekid <timm.friebe@gmail.com>
2006-02-05 12:54:00 +0000

    - Use Console::writeLinef() instead of printf() - Use constant instead of hardcoding the format string in two different places - Enable $Id$ expansion
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6532 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/people/friebe/chain/tokenize.php
+++ experiments/people/friebe/chain/tokenize.php
@@ -6,6 +6,8 @@
   require('lang.base.php');
   xp::sapi('cli');
 
+  define('OUTPUT_FORMAT', '%3d [%-26s] "%s"');
+
   // {{{ main
   $p= &new ParamString();
   if (!$p->exists(1) || $p->exists('help', '?')) {
@@ -25,9 +27,9 @@ __
   $tokens= token_get_all('<?php '.trim($p->value(1)).' ?>');
   for ($i= 1, $s= sizeof($tokens); $i < $s - 2; $i++) {
     if (is_array($tokens[$i])) {
-      printf("%3d [%-26s] '%s'\n", $i, token_name($tokens[$i][0]), $tokens[$i][1]);
+      Console::writeLinef(OUTPUT_FORMAT, $i, token_name($tokens[$i][0]), $tokens[$i][1]);
     } else {
-      printf("%3d [%-26s] '%s'\n", $i, '', $tokens[$i]);
+      Console::writeLinef(OUTPUT_FORMAT, $i, '', $tokens[$i]);
     }
   }
   // }}}
