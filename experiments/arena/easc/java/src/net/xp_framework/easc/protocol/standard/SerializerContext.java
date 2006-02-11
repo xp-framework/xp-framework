f5db8d20dd766c79deb1995c816491f62fa615ba
thekid <timm.friebe@gmail.com>
2006-02-11 20:15:27 +0000

    - Remove class list from SerializerContext
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6588 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/easc/java/src/net/xp_framework/easc/protocol/standard/SerializerContext.java
+++ experiments/arena/easc/java/src/net/xp_framework/easc/protocol/standard/SerializerContext.java
@@ -5,15 +5,12 @@
 
 package net.xp_framework.easc.protocol.standard;
 
-import java.util.HashSet;
-
 /**
  * Indicates an error during serialization
  *
  * @see   net.xp_framework.easc.protocol.standard.Serializer
  */
 public class SerializerContext extends Object {
-    public HashSet<Class> classList= new HashSet<Class>();
     public ClassLoader classLoader= null;
     
     /**
@@ -24,15 +21,5 @@ public class SerializerContext extends Object {
      */
     public SerializerContext(ClassLoader classLoader) {
         this.classLoader= classLoader;
-    }
-    
-    /**
-     * Adds a class
-     *
-     * @access  public
-     * @param   java.lang.Class c
-     */
-    public void addClass(Class c) {
-        this.classList.add(c);
     }
 }
