46cf9041b2e61cdc8e49f0c61a453052584bc543
thekid <timm.friebe@gmail.com>
2006-02-11 20:16:13 +0000

    - Add serializer invokeable for ClassWrapper class
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6590 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/easc/java/src/net/xp_framework/easc/protocol/standard/ReflectionServerHandler.java
+++ experiments/arena/easc/java/src/net/xp_framework/easc/protocol/standard/ReflectionServerHandler.java
@@ -8,7 +8,10 @@ package net.xp_framework.easc.protocol.standard;
 import net.xp_framework.easc.protocol.standard.ServerHandler;
 import net.xp_framework.easc.server.ServerContext;
 import net.xp_framework.easc.protocol.standard.Serializer;
-import java.lang.reflect.Proxy;
+import net.xp_framework.easc.reflect.ClassWrapper;
+
+import java.util.ArrayList;
+import java.lang.reflect.Field;
 
 public class ReflectionServerHandler extends ServerHandler {
     
@@ -19,5 +22,33 @@ public class ReflectionServerHandler extends ServerHandler {
      * @param   net.xp_framework.easc.server.ServerContext ctx
      */
     public void setup(final ServerContext ctx) {
+        Serializer.registerMapping(ClassWrapper.class, new Invokeable<String, ClassWrapper>() {
+            public String invoke(ClassWrapper w, Object arg) throws Exception {
+                StringBuffer buffer= new StringBuffer();
+                String name= ClassWrapper.class.getName();
+
+                // Object declaration
+                buffer.append("O:").append(name.length()).append(":\"").append(name).append("\":2:{");
+
+                // Referenced class' name
+                buffer.append("s:9:\"className\";").append(Serializer.representationOf(w.referencedClass.getName()));
+
+                // Fields
+                ArrayList<Field> fields= Serializer.classFields(w.referencedClass);
+                buffer.append("s:6:\"fields\";a:").append(fields.size()).append(":{");
+                for (Field f : fields) { 
+                    buffer.append("s:");
+                    buffer.append(f.getName().length());
+                    buffer.append(":\"");
+                    buffer.append(f.getName());
+                    buffer.append("\";");
+
+                    buffer.append(Serializer.representationOf(f.getType()));
+                }
+                
+                buffer.append("}}");        
+                return buffer.toString();
+            }
+        });
     }
 }
