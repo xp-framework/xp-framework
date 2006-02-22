fc41a056a2ad49dce433730b33ea35c1c8b64d6e
thekid <timm.friebe@gmail.com>
2006-02-22 16:12:04 +0000

    - Add optional command line argument "-c"
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6624 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ext/java/easc/clients/reflect.php
+++ ext/java/easc/clients/reflect.php
@@ -15,7 +15,7 @@ ESDL demo application
 
 Usage
 -----
-$ php reflect.php <hostname> [-p <port>] [-b <jndiname>]
+$ php reflect.php <hostname> [-p <port>] [-b <jndiname> [-c]]
   
   * hostname is the host name (or IP) that your JBoss + XP-MBean server 
     is running on. The feed entity bean (from the easc/beans directory) 
@@ -25,6 +25,8 @@ $ php reflect.php <hostname> [-p <port>] [-b <jndiname>]
   
   * jndiname is the name of the bean. If this parameter is omitted,
     all deployed beans are listed.
+  
+  * The -c flag lists all classes used by the bean's interfaces
 __
     );
     exit(1);
@@ -40,6 +42,25 @@ __
   if ($p->exists('bean')) {
     $bean= &$remote->lookup('Services:'.$p->value('bean'));
     Console::writeLine(xp::stringOf($bean));
+    
+    if ($p->exists('classlist')) {
+      $cl= &ClassLoader::getDefault();
+      $jndi= $bean->getJndiName();
+      foreach ($bean->classSet() as $classref) {
+        if (!$cl->findClass($classref->referencedName())) {
+          try(); {
+            $class= &$remote->lookup('Class:'.$jndi.':'.$classref->referencedName());
+          } if (catch('Exception', $e)) {
+            $e->printStackTrace();
+            continue;
+          }
+        } else {
+          $class= &$cl->loadClass($classref->referencedName());
+        }
+        Console::writeLine(xp::stringOf($class));
+        xp::gc();
+      }
+    }
   } else {
     $services= &$remote->lookup('Services');
     Console::writeLinef('# Beans found= %d', $services->size());
@@ -47,6 +68,5 @@ __
       Console::writeLine(xp::stringOf($description));
     }
   }
-  
   // }}}
 ?>
