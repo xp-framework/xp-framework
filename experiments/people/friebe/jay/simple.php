3410e31409e9815eaa5e1f64c4d81ef78bb65167
thekid <timm.friebe@gmail.com>
2006-03-17 15:26:16 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6680 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/people/friebe/jay/simple.php
@@ -0,0 +1,65 @@
+<?php
+  import xp~lang~IllegalArgumentException, xp~util~DateFormatException;
+
+  package de~thekid { 
+    class Date extends xp~lang~Object {
+      private $stamp= 0;
+      protected static $tz= array(
+        'GMT' => 0
+      ); 
+
+      public __construct($stamp) throws IllegalArgumentException, DateFormatException {
+        $this->setStamp($stamp);
+      }
+      
+      [@override] public string toString() {
+        return date('r', $this->stamp);
+      }
+      
+      public static operator + (Date $a, Date $b) {
+        return new Date($a->stamp + $b->stamp);
+      }
+      
+      public Comparator comparator() {
+        return new Comparator() {
+          public bool compare(Date $a, Date $b) {
+            return strcmp($a->stamp, $b->stamp);
+          }
+        };
+      }
+      
+      private void setStamp($stamp) {
+        $this->stamp= $stamp;
+      }
+    }
+    
+    enum Coin {
+      penny(1), nickel(5), dime(10), quarter(25);
+    }
+    
+    interface Comparator {
+      bool compare($a, $b);
+    }
+  }
+
+  try {
+    $one= new Date(1);
+  } catch (IllegalArgumentException $e) {
+    echo $e;
+    exit;
+  } catch (DateFormatException $e) {
+    echo $e;
+    exit;
+  } catch (xp~lang~Exception $e) {
+    echo $e;
+    exit;
+  }
+
+  echo $one, "\n";
+  
+  $coin= Coin::penny();
+  switch ($coin) {
+    case Coin::penny: if ($debug) echo "It's a penny!\n"; break;
+    default: echo "Unknown coin...\n";
+  }
+?>
