f3ae0bf5ca8703fdea269a3be8ba5ced8db2de50
thekid <timm.friebe@gmail.com>
2006-03-24 12:49:01 +0000

    - Remove utf8_decode() at the end, seems this is not necessary # Verified but this seems to be broken in some way I can`t figure # out at the moment. Integration tests will follow
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6692 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/remote/protocol/ByteCountedString.class.php
+++ skeleton/remote/protocol/ByteCountedString.class.php
@@ -106,7 +106,7 @@
         $s.= ByteCountedString::readFully($stream, $ctl['length']);
       } while ($ctl['next']);
       
-      return utf8_decode($s);
+      return $s; // utf8_decode($s);
     }
   }
 ?>
