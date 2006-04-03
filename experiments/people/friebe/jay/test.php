e420220a5b920d53563e175435e0bffe58806cc9
thekid <timm.friebe@gmail.com>
2006-04-03 15:25:48 +0000

    - Make "<?php" and "?>" obsolete
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6723 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/people/friebe/jay/test.php
+++ experiments/people/friebe/jay/test.php
@@ -1,55 +1,53 @@
-<?php
-  package io {
-    interface Serializable { 
-      const SERIALVERSIONUID= -32423020;
-      
-      function getId();
-    }
+package io {
+  interface Serializable { 
+    const SERIALVERSIONUID= -32423020;
+
+    function getId();
+  }
+}
+
+package bar {
+  import io~Serializable;
+
+  enum Coin {
+    penny(1), nickel(5), dime(10), quarter(25);
   }
 
-  package bar {
-    import io~Serializable;
-    
-    enum Coin {
-      penny(1), nickel(5), dime(10), quarter(25);
+  class Legacy {
+    var $old;
+  }
+
+  class Foo extends xp~lang~Object implements Serializable {
+    public $id= 0;
+    protected $foo= -1;
+    public static $instance= NULL;
+
+    protected __construct() { }
+
+    public static function getInstance() {
+      if (!isset(self::$instance)) {
+        self::$instance= new self();
+      }
+
+      return self::$instance;
     }
-    
-    class Legacy {
-      var $old;
+
+    [@webmethod, @deprecated('use id() instead'), @security(role= 'user')]
+    public function getId() {
+      return $this->id;
     }
 
-    class Foo extends xp~lang~Object implements Serializable {
-      public $id= 0;
-      protected $foo= -1;
-      public static $instance= NULL;
-
-      protected __construct() { }
-
-      public static function getInstance() {
-        if (!isset(self::$instance)) {
-          self::$instance= new self();
-        }
-
-        return self::$instance;
-      }
-
-      [@webmethod, @deprecated('use id() instead'), @security(role= 'user')]
-      public function getId() {
-        return $this->id;
-      }
-      
-      public static operator + () throws xp~lang~Exception {
-        throw new Exception('Not yet implemented');
-      }
+    public static operator + () throws xp~lang~Exception {
+      throw new Exception('Not yet implemented');
     }
   }
-  
-  try {
-    $id= bar~Foo::getInstance()->getId();
-    throw new Exception();
-  } catch (Exception $e) {
-    $e->printStackTrace();
-  }
-  
-  echo $id;
-?>
+}
+
+try {
+  $id= bar~Foo::getInstance()->getId();
+  throw new Exception();
+} catch (Exception $e) {
+  $e->printStackTrace();
+}
+
+echo $id;
