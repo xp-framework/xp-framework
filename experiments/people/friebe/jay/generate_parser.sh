6a208522e9e048496383316daa5ebc5718bb3760
kiesel <alex@kiesel.name>
2006-04-03 19:32:46 +0000

    - make generate_parser.sh run on Windows and Unix # though Win currently untested
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6731 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/people/friebe/jay/generate_parser.sh
+++ experiments/people/friebe/jay/generate_parser.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 # $Id$
-./phpJay.exe -cv -g _PHP_PARSER < xp-skeleton.in XP2.jay > Parser.php
+
+SYSTEM=`env | grep OS | grep -i WINDOWS | wc -l`
+
+JAY_BIN=./phpJay
+
+[ 0 -lt $SYSTEM ] && {
+  JAY_BIN=./phpJay.exe
+}
+$JAY_BIN -cv -g _PHP_PARSER < xp-skeleton.in XP2.jay > Parser.php
 
