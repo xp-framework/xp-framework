d76337caafc6ec945141ccb9afa50c4cbcfde782
thekid <timm.friebe@gmail.com>
2006-04-14 10:28:18 +0000

    - Remove unused compiler class
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6783 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/people/friebe/jay/compile.php
+++ experiments/people/friebe/jay/compile.php
@@ -3,43 +3,20 @@
   uses(
     'net.xp_framework.tools.vm.Parser',
     'net.xp_framework.tools.vm.Lexer',
-    'net.xp_framework.tools.vm.PNode', 
     'util.cmd.Console', 
     'io.File', 
     'io.FileUtil'
   );
   
-  class compiler {
-    function error($level, $message) {
-      switch ($level) {
-        case E_ERROR:
-        case E_CORE_ERROR:
-        case E_COMPILE_ERROR:
-          xp::error($message);
-          // Bails out
-      }
-      echo '*** ', $message, "\n";
-    }
-
-    function call($function, $args= array()) {
-      // Can be removed as soon as XP2.jay doesn't use this anymore
-    }
-
-  }
-  
-  function get_next_op_number($a) {
-    // Can be removed as soon as XP2.jay doesn't use this anymore
-    return 1;
-  }
-  
   // {{{ compile
   $parser= &new Parser();
   $parser->debug= FALSE;
   $nodes= $parser->yyparse(new Lexer(file_get_contents($argv[1]), $argv[1]));
-  xp::gc();
   
+  // Dump AST if specified
   isset($argv[2]) && Console::writeLine(PNode::stringOf($nodes));
   
+  // Write to file
   $out= &new File($argv[1].'c');
   try(); {
     FileUtil::setContents($out, serialize($nodes));
