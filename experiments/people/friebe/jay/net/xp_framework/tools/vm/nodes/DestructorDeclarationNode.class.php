34b76e02e7a80b60743dd83804e48a4ecb26941f
thekid <timm.friebe@gmail.com>
2006-04-15 17:46:41 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6821 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/people/friebe/jay/net/xp_framework/tools/vm/nodes/DestructorDeclarationNode.class.php
@@ -0,0 +1,37 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('net.xp_framework.tools.vm.InvokeableDeclarationNode');
+
+  /**
+   * DestructorDeclaration
+   *
+   * @see   xp://net.xp_framework.tools.vm.nodes.VNode
+   */ 
+  class DestructorDeclarationNode extends InvokeableDeclarationNode {
+      
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   mixed statements
+     * @param   mixed modifiers
+     * @param   mixed annotations
+     * @param   mixed throws
+     */
+    function __construct($statements, $modifiers, $annotations, $throws) {
+      parent::__construct(
+        '<destruct>', 
+        array(), 
+        NULL, 
+        $statements, 
+        $modifiers, 
+        $annotations, 
+        $throws
+      );
+    }  
+  }
+?>
