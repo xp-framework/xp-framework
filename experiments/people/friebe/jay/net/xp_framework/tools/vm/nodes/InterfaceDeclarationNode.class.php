d54b7389321cb8adb9fc874ed7e3c463ad7dcecf
thekid <timm.friebe@gmail.com>
2006-04-15 16:22:04 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6806 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/people/friebe/jay/net/xp_framework/tools/vm/nodes/InterfaceDeclarationNode.class.php
@@ -0,0 +1,40 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('net.xp_framework.tools.vm.VNode');
+
+  /**
+   * ClassDeclaration
+   *
+   * @see   xp://net.xp_framework.tools.vm.nodes.VNode
+   */ 
+  class InterfaceDeclarationNode extends VNode {
+    var
+      $name, 
+      $extends, 
+      $statements, 
+      $modifiers, 
+      $annotations;
+      
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   mixed name
+     * @param   mixed[] extends
+     * @param   mixed statements
+     * @param   mixed modifiers
+     * @param   mixed annotations
+     */
+    function __construct($name, $extends, $statements, $modifiers, $annotations) {
+      $this->name= $name;
+      $this->extends= $extends;
+      $this->statements= $statements;
+      $this->modifiers= $modifiers;
+      $this->annotations= $annotations;
+    }  
+  }
+?>
