3448d64a6f84bca06c9bf96e1231ee1354f562eb
kiesel <alex@kiesel.name>
2006-04-20 11:22:45 +0000

    - remove incomplete line # found by Aurelian Oancea
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6860 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/people/kiesel/rfc0056/skeleton/org/json/rpc/JsonClient.class.php
+++ experiments/people/kiesel/rfc0056/skeleton/org/json/rpc/JsonClient.class.php
@@ -56,7 +56,6 @@
     
       $this->transport->setMessageClass(XPClass::forName('org.json.rpc.JsonMessage'));
       $args= func_get_args();
-      $method= 
       
       $this->message= &new JsonMessage();
       $this->message->createCall(array_shift($args), time().(++$serial));
