7b29e964f0a3a1dd0fc3075a74094fb83764885f
thekid <timm.friebe@gmail.com>
2006-04-30 14:39:16 +0000

    - QA: Add apidoc
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6941 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/people/friebe/generics/GenericMap.class.php
+++ experiments/people/friebe/generics/GenericMap.class.php
@@ -4,18 +4,43 @@
  * $Id$
  */
 
+  /**
+   * Generic map class
+   *
+   * @purpose  Generics demonstration
+   */
   class GenericMap<K, V> extends Object {
     var
       $elements= array();
       
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   array<K, V> initial default array()
+     */
     function __construct($initial= array()) {
       $this->elements= $initial;
     }
   
+    /**
+     * Put an element into this map
+     *
+     * @access  public
+     * @param   K key
+     * @param   &V value
+     */
     function &put($key, &$value) {
       $this->elements[$key]= &$value;
     }
 
+    /**
+     * Retrieve an element from this map
+     *
+     * @access  public
+     * @param   K key
+     * @return  &V value associated with the key
+     */
     function &get($key) {
       return $this->elements[$key];
     }
