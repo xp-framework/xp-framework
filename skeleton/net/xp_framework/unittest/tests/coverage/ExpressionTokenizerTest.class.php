a14d870d434cd734167b96bde58d9534ca8bafb1
thekid <timm.friebe@gmail.com>
2006-05-05 15:02:09 +0000

    - Refactoring: Move expressionsOf() to its own class, PHPCodeFragmentizer
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6984 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/net/xp_framework/unittest/tests/coverage/ExpressionTokenizerTest.class.php
+++ skeleton/net/xp_framework/unittest/tests/coverage/ExpressionTokenizerTest.class.php
@@ -6,10 +6,7 @@
  
   uses(
     'util.profiling.unittest.TestCase',
-    'lang.Collection',
-    'net.xp_framework.unittest.tests.coverage.Expression',
-    'net.xp_framework.unittest.tests.coverage.Comment',
-    'net.xp_framework.unittest.tests.coverage.Block'
+    'net.xp_framework.unittest.tests.coverage.PHPCodeFragmentizer'
   );
 
   /**
@@ -21,72 +18,15 @@
   class ExpressionTokenizerTest extends TestCase {
     
     /**
-     * Retrieve expressions from a given piece of code
+     * Retrieve fragments for a given piece of code
      *
+     * @see     xp://net.xp_framework.unittest.tests.coverage.PHPCodeFragmentizer
      * @access  protected
      * @param   string code
      * @return  net.xp_framework.unittest.tests.coverage.Fragment[] expressions
      */
-    function expressionsOf($code) {
-      $tokens= token_get_all('<?php '.trim($code).' ?>');
-      $expressions= &Collection::forClass('Fragment');
-      $expression= '';
-      $line= $last= 1;
-      $level= 0;
-      $collections= array(&$expressions);
-      
-      // Iterate over tokens, starting from the T_OPEN_TAG and ending 
-      // before the traling T_WHITESPACE and T_CLOSE_TAG tokens.
-      for ($i= 1, $s= sizeof($tokens)- 2; $i < $s; $i++) {
-        switch ($tokens[$i][0]) {
-          case ';':           // EOE
-            $collections[$level]->add(new Expression(trim($expression).';', $last, $line));
-            $expression= '';
-            $last= -1;
-            break;
-          
-          case '{':           // SOB
-            $block= &$expressions->add(new Block(trim($expression), array(), $line, -1));
-            $collections[++$level]= &$block->expressions;
-            $expression= '';
-            $last= -1;
-            break;
-          
-          case '}':           // EOB
-            unset($collections[$level]);
-            $block->end= $line;
-            $level--;
-            $expression= '';
-            $last= -1;
-            break;
-          
-          case T_COMMENT:
-            $last= substr_count($tokens[$i][1], "\n");
-            $collections[$level]->add(new Comment($tokens[$i][1], $line, $last+ $line));
-            $expression= '';
-            $line+= $last;
-            $last= -1;
-            break;
-            
-          case T_CONSTANT_ENCAPSED_STRING:
-          case T_WHITESPACE:
-            $line+= substr_count($tokens[$i][1], "\n");
-            $last == -1 && $last= $line;
-            $expression.= $tokens[$i][1];
-            break;
-            
-          default:
-            $last == -1 && $last= $line;
-            $expression.= is_array($tokens[$i]) ? $tokens[$i][1] : $tokens[$i];
-        }
-      }
-      
-      // Check for leftover tokens
-      if ($expression) {
-        $collections[$level]->add(new Expression(trim($expression).';', $last, $line));
-      }
-      
-      return $expressions->values();
+    function fragmentsOf($code) {
+      return PHPCodeFragmentizer::fragmentsOf('<?php '.trim($code).' ?>');
     }
     
     /**
@@ -98,7 +38,7 @@
      * @throws  util.profiling.unittest.AssertionFailedError
      */
     function assertExpressions($expected, $code) {
-      $fragments= $this->expressionsOf($code);
+      $fragments= $this->fragmentsOf($code);
 
       // Compare sizes
       $s= sizeof($fragments);
