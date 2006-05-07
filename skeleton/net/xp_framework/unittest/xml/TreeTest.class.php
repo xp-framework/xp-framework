737b9258a41d04f2147a7cac95f0338f93180b0f
thekid <timm.friebe@gmail.com>
2006-05-07 14:31:46 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@6995 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/net/xp_framework/unittest/xml/TreeTest.class.php
@@ -0,0 +1,111 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+ 
+  uses(
+    'util.profiling.unittest.TestCase',
+    'xml.Tree'
+  );
+
+  /**
+   * Test XML Tree class
+   *
+   * @see      xp://net.xp_framework.unittest.xml.NodeTest 
+   * @purpose  Unit Test
+   */
+  class TreeTest extends TestCase {
+    
+    /**
+     * Helper method which returns the XML representation of a Tree object,
+     * trimmed of trailing \ns.
+     *
+     * @access  protected
+     * @param   &xml.Tree tree
+     * @return  string
+     */
+    function sourceOf(&$tree, $mode= INDENT_DEFAULT) {
+      return rtrim($tree->getSource($mode), "\n");
+    }
+
+    /**
+     * Tests an empty tree (one with only a root node)
+     *
+     * @access  public
+     */
+    #[@test]
+    function emptyTree() {
+      $this->assertEquals(
+        '<root/>', 
+        $this->sourceOf(new Tree('root'))
+      );
+    }
+
+    /**
+     * Tests root member
+     *
+     * @access  public
+     */
+    #[@test]
+    function rootMember() {
+      with ($t= &new Tree('formresult'), $r= &$t->root); {
+        $this->assertClass($r, 'xml.Node');
+        $this->assertEmpty($r->children);
+        $this->assertEmpty($r->attribute);
+        $this->assertEquals('formresult', $r->getName());
+      }
+    }
+
+    /**
+     * Tests adding a child
+     *
+     * @access  public
+     */
+    #[@test]
+    function addChild() {
+      $t= &new Tree('tests');
+      $child= &new Node('test', 'success', array('name' => 'TreeTest'));
+      $this->assertEquals($child, $t->addChild($child));
+    }
+
+    /**
+     * Tests fromString
+     *
+     * @access  public
+     */
+    #[@test]
+    function fromString() {
+      $t= &Tree::fromString('
+        <c:config xmlns:c="http://example.com/cfg/1.0">
+          <attribute name="key">value</attribute>
+        </c:config>
+      ');
+      
+      with ($r= &$t->root); {
+        $this->assertEquals('c:config', $r->getName());
+        $this->assertTrue($r->hasAttribute('xmlns:c'));
+        $this->assertEquals('http://example.com/cfg/1.0', $r->getAttribute('xmlns:c'));
+        $this->assertEquals(1, sizeof($r->children));
+      }      
+      
+      with ($c= &$t->root->children[0]); {
+        $this->assertEquals('attribute', $c->name);
+        $this->assertTrue($c->hasAttribute('name'));
+        $this->assertEquals('key', $c->getAttribute('name'));
+        $this->assertEquals(0, sizeof($c->children));
+        $this->assertEquals('value', $c->getContent());
+      }
+    }
+
+    /**
+     * Tests fromString when given incorrect XML
+     *
+     * @access  public
+     */
+    #[@test, @expect('xml.XMLFormatException')]
+    function fromNonXmlString() {
+      Tree::fromString('@@NO-XML-HERE@@');
+    }
+  }
+?>
