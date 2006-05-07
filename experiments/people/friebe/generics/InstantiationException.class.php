bc66149602e651c6e59feae22b41407fb054d066
thekid <timm.friebe@gmail.com>
2006-05-07 18:01:57 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@7006 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/people/friebe/generics/InstantiationException.class.php
@@ -0,0 +1,14 @@
+<?php
+/* This class is part of the XP framework's experiments
+ *
+ * $Id$
+ */
+
+  /**
+   * Thrown when a class cannot be instantiated.
+   *
+   * @purpose  Exception
+   */
+  class InstantiationException extends Exception {
+  }
+?>
