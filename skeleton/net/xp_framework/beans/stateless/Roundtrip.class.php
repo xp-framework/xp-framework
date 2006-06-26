e7dd4f664c5490673c3525a2667d2d6871bddc59
thekid <timm.friebe@gmail.com>
2006-06-26 10:51:14 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@7183 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/net/xp_framework/beans/stateless/Roundtrip.class.php
@@ -0,0 +1,87 @@
+<?php
+/* This file is part of the XP framework
+ *
+ * $Id$
+ */
+
+  /**
+   * Remote interface for xp/demo/Roundtrip
+   *
+   * @purpose  EASC Client stub
+   */
+  class Roundtrip extends Interface {
+
+    /**
+     * EchoString method
+     *
+     * @access  public
+     * @param   string arg1
+     * @return  string
+     */
+    function echoString($arg1) { }
+
+    /**
+     * EchoInt method
+     *
+     * @access  public
+     * @param   integer arg1
+     * @return  integer
+     */
+    function echoInt($arg1) { }
+
+    /**
+     * EchoDouble method
+     *
+     * @access  public
+     * @param   double arg1
+     * @return  double
+     */
+    function echoDouble($arg1) { }
+
+    /**
+     * EchoBool method
+     *
+     * @access  public
+     * @param   boolean arg1
+     * @return  boolean
+     */
+    function echoBool($arg1) { }
+
+    /**
+     * EchoNull method
+     *
+     * @access  public
+     * @param   java.lang.Object arg1
+     * @return  java.lang.Object
+     */
+    function echoNull($arg1) { }
+
+    /**
+     * EchoDate method
+     *
+     * @access  public
+     * @param   util.Date arg1
+     * @return  util.Date
+     */
+    function echoDate($arg1) { }
+
+    /**
+     * EchoHash method
+     *
+     * @access  public
+     * @param   array<mixed, mixed> arg1
+     * @return  array<mixed, mixed>
+     */
+    function echoHash($arg1) { }
+
+    /**
+     * EchoArrayList method
+     *
+     * @access  public
+     * @param   lang.types.ArrayList arg1
+     * @return  lang.types.ArrayList
+     */
+    function echoArray($arg1) { }
+
+  }
+?>
