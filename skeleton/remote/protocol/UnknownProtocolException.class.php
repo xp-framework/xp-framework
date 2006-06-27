42b71bc0599058e17fdafba866b23b49b6f64fc1
thekid <timm.friebe@gmail.com>
2006-06-27 09:28:09 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@7193 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/remote/protocol/UnknownProtocolException.class.php
@@ -0,0 +1,16 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  /**
+   * Indicates a protocol passed to the handler factory is unknown.
+   *
+   * @see      xp://remote.HandlerFactory
+   * @purpose  Exception
+   */
+  class UnknownProtocolException extends IllegalArgumentException {
+
+  }
+?>
