9aead2739234f814d8c323a8f96a621ccf53de40
thekid <timm.friebe@gmail.com>
2006-06-30 14:46:29 +0000

    - Add two tests for the LimitTest
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@7259 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/net/xp_framework/unittest/tests/SimpleTestCase.class.php
+++ skeleton/net/xp_framework/unittest/tests/SimpleTestCase.class.php
@@ -7,9 +7,11 @@
   uses('util.profiling.unittest.TestCase');
 
   /**
-   * This class is used in the SuiteTest class' running* methods.
+   * This class is used in the SuiteTest class' running* methods and
+   * by the LimitTest methods
    *
    * @see      xp://net.xp_framework.unittest.tests.SuiteTest
+   * @see      xp://net.xp_framework.unittest.tests.LimitTest
    * @purpose  Unit Test
    */
   class SimpleTestCase extends TestCase {
@@ -32,6 +34,29 @@
     #[@test]
     function fails() {
       $this->assertTrue(FALSE);
+    }
+
+    /**
+     * A test that timeouts
+     *
+     * @access  public
+     */
+    #[@test, @limit(time= 0.010)]
+    function timeouts() {
+      $start= gettimeofday();
+      $end= (1000000 * $start['sec']) + $start['usec'] + 1000 * 50;    // 0.05 seconds
+      do {
+        $now= gettimeofday();
+      } while ((1000000 * $now['sec']) + $now['usec'] < $end);
+    }
+
+    /**
+     * A test that does not timeout
+     *
+     * @access  public
+     */
+    #[@test, @limit(time= 1.0)]
+    function noTimeout() {
     }
   }
 ?>
