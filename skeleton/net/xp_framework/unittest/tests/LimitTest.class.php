73b73bdeebb9ac904f4827374e066d5d85d86ac0
thekid <timm.friebe@gmail.com>
2006-06-30 14:45:35 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@7258 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/net/xp_framework/unittest/tests/LimitTest.class.php
@@ -0,0 +1,50 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+ 
+  uses('util.profiling.unittest.TestCase', 'net.xp_framework.unittest.tests.SimpleTestCase');
+
+  /**
+   * Test TestSuite class methods
+   *
+   * @see      xp://util.profiling.unittest.TestSuite
+   * @purpose  Unit Test
+   */
+  class LimitTest extends TestCase {
+    var
+      $suite= NULL;
+      
+    /**
+     * Setup method. Creates a new test suite.
+     *
+     * @access  public
+     */
+    function setUp() {
+      $this->suite= &new TestSuite();
+    }
+
+    /**
+     * Tests running the test that times out
+     *
+     * @access  public
+     */    
+    #[@test]
+    function timeouts() {
+      $r= &$this->suite->runTest(new SimpleTestCase('timeouts'));
+      $this->assertEquals(1, $r->failureCount());
+    }    
+
+    /**
+     * Tests running the test that doesn't timeout
+     *
+     * @access  public
+     */    
+    #[@test]
+    function noTimeout() {
+      $r= &$this->suite->runTest(new SimpleTestCase('noTimeout'));
+      $this->assertEquals(1, $r->successCount());
+    }    
+  }
+?>
