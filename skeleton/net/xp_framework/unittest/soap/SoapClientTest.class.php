9d4c5e89c616f42750eed8a9054d76b873a18053
kiesel <alex@kiesel.name>
2006-07-03 15:46:42 +0000

    - add testcases for return values with   . no return value   . one return value
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@7275 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/net/xp_framework/unittest/soap/SoapClientTest.class.php
+++ skeleton/net/xp_framework/unittest/soap/SoapClientTest.class.php
@@ -49,5 +49,66 @@
       $client= &new SOAPClient($transport, 'urn://test');
       $client->invoke('irrelevant');
     }
+
+  
+    /**
+     * Ensures no exception is thrown in case we have 
+     * no output arguments
+     *
+     * @access  public
+     */
+    #[@test]
+    function testNoOutputArguments() {
+      $transport= &new SOAPDummyTransport();
+      $transport->setAnswer('<?xml version="1.0" encoding="iso-8859-1"?>
+<SOAP-ENV:Envelope
+ xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
+ xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+ xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+ xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
+ xmlns:si="http://soapinterop.org/xsd"
+ SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"
+ xmlns:ctl="ctl"
+>
+  <SOAP-ENV:Body>  
+    <ctl:irrelevant/>    
+  </SOAP-ENV:Body>
+</SOAP-ENV:Envelope> 
+');
+      
+      $client= &new SOAPClient($transport, 'urn://test');
+      $client->invoke('irrelevant');
+    }
+
+    /**
+     * Ensures no exception is thrown in case we have 
+     * one output arguments
+     *
+     * @access  public
+     */
+    #[@test]
+    function testOneOutputArguments() {
+      $transport= &new SOAPDummyTransport();
+      $transport->setAnswer('<?xml version="1.0" encoding="iso-8859-1"?>
+<SOAP-ENV:Envelope
+ xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
+ xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+ xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+ xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"
+ xmlns:si="http://soapinterop.org/xsd"
+ SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"
+ xmlns:ctl="ctl"
+>
+  <SOAP-ENV:Body>  
+    <ctl:irrelevant>
+      <item xsi:type="xsd:int">5</item>
+    </ctl:irrelevant>
+  </SOAP-ENV:Body>
+</SOAP-ENV:Envelope> 
+');
+      
+      $client= &new SOAPClient($transport, 'urn://test');
+      $this->assertEquals(5, $client->invoke('irrelevant'));
+    }
   }
 ?>
