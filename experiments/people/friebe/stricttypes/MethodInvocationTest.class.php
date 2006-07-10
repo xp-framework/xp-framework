390be05b6182342555b73356ff955de21c5b94e8
thekid <timm.friebe@gmail.com>
2006-07-10 15:17:56 +0000

    - Add tests:   . setDateWithNull()   . addWithInts() - Make these new tests pass
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@7307 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/people/friebe/stricttypes/MethodInvocationTest.class.php
+++ experiments/people/friebe/stricttypes/MethodInvocationTest.class.php
@@ -34,7 +34,15 @@
     function invoke($name, $args= array()) {
       $method= &$this->fixture->getMethod($name);
       foreach ($method->getArguments() as $pos => $arg) {
-        if (is($arg->getType(), $args[$pos])) continue;
+        if (
+          ('int' == $arg->getType() && is_int($args[$pos])) ||
+          ('bool' == $arg->getType() && is_bool($args[$pos])) ||
+          ('float' == $arg->getType() && is_float($args[$pos])) ||
+          ('string' == $arg->getType() && is_string($args[$pos])) ||
+          ('array' == $arg->getType() && is_array($args[$pos])) ||
+          (is($arg->getType(), $args[$pos])) ||
+          (NULL === $args[$pos])
+        ) continue;
         
         return throw(new IllegalArgumentException(
           'Argument #'.$pos.': '.xp::typeOf($args[$pos]).' does not match '.$arg->getType()
@@ -65,6 +73,16 @@
     }
 
     /**
+     * Tests invoking TestClass::setDate() with NULL
+     *
+     * @access  public
+     */
+    #[@test]
+    function setDateWithNull() {
+      $this->invoke('setDate', array(NULL));
+    }
+
+    /**
      * Tests invoking TestClass::setDate() with an Object instance
      *
      * @access  public
@@ -82,6 +100,16 @@
     #[@test, @expect('lang.IllegalArgumentException')]
     function setDateWithPrimitive() {
       $this->invoke('setDate', array('2007-07-07'));
+    }
+
+    /**
+     * Tests invoking TestClass::add() with two ints
+     *
+     * @access  public
+     */
+    #[@test]
+    function addWithInts() {
+      $this->invoke('add', array(1, 2));
     }
   }
 ?>
