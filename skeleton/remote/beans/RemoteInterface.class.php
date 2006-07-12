cefeba5924c4cee392b2b17fb3ed69eeda83010f
kiesel <alex@kiesel.name>
2006-07-12 13:06:56 +0000

    - initial revision
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@7430 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/remote/beans/RemoteInterface.class.php
@@ -0,0 +1,17 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses('remote.beans.BeanInterface');
+
+  /**
+   * RemoteInterface 
+   *
+   * @purpose  Remote Interface
+   */
+  class RemoteInterface extends BeanInterface {
+  
+  }
+?>
