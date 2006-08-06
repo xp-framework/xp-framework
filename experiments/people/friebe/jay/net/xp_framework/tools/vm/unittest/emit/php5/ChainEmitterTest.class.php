1ddd135b9bf9b895a3142ecd01e22b9f729b5047
thekid <timm.friebe@gmail.com>
2006-08-06 15:58:18 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@7560 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/people/friebe/jay/net/xp_framework/tools/vm/unittest/emit/php5/ChainEmitterTest.class.php
@@ -0,0 +1,115 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+ 
+  uses('net.xp_framework.tools.vm.unittest.emit.php5.AbstractEmitterTest');
+
+  /**
+   * Tests PHP5 emitter
+   *
+   * @purpose  Unit Test
+   */
+  class ChainEmitterTest extends AbstractEmitterTest {
+
+    /**
+     * Tests chaining method calls. Note this works without translation
+     * in PHP5!
+     *
+     * @access  public
+     */
+    #[@test]
+    function chainedMethodCalls() {
+      $this->assertSourcecodeEquals(
+        '$o= new xp·lang·Object(); echo $o->getClass()->getName();',
+        $this->emit('$o= new xp~lang~Object(); echo $o->getClass()->getName();')
+      );
+    }
+
+    /**
+     * Tests chaining method calls. Note this needs a wrapper function
+     * to work in PHP5!
+     *
+     * @access  public
+     */
+    #[@test]
+    function chainedMethodCallsAfterConstructor() {
+      $this->assertSourcecodeEquals(
+        'echo xp::create(new xp·lang·Object())->getClass()->getName();',
+        $this->emit('echo new xp~lang~Object()->getClass()->getName();')
+      );
+    }
+
+    /**
+     * Tests chaining members and methods, and after the constructor
+     *
+     * @access  public
+     */
+    #[@test]
+    function chainedMethodsAndMembers() {
+      $this->assertSourcecodeEquals(
+        preg_replace('/\n\s*/', '', 'class Long extends xp·lang·Object{
+          public $number;
+
+          public function __construct($initial){
+            $this->number= $initial; 
+          }
+
+          public function intValue(){
+            return $this->number; 
+          }
+        }; 
+
+        class Date extends xp·lang·Object{
+          public $stamp;
+
+          public function __construct(){
+            $this->stamp= new Long(time()); 
+          }
+        }; 
+
+        class News extends xp·lang·Object{
+          public $date;
+
+          public function __construct(){
+            $this->date= new Date(); 
+          }
+        }; 
+
+        echo date(\'r\', xp::create(new News())->date->stamp->intValue());
+        '),
+        $this->emit('class Long {
+          public int $number;
+
+          public __construct(int $initial= 0) {
+            $this->number= $initial;
+          }
+
+          public int intValue() {
+            return $this->number;
+          }
+        }
+
+        class Date {
+          public Long $stamp;
+
+          public __construct() {
+            $this->stamp= new Long(time());
+          }
+        }
+
+        class News {
+          public Date $date;
+
+          public __construct() {
+            $this->date= new Date();
+          }
+        }
+
+        echo date("r", new News()->date->stamp->intValue());
+        ')
+      );
+    }
+  }
+?>
