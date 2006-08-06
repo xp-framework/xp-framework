724b000a109fe39a092bf9c0498739487822ba84
thekid <timm.friebe@gmail.com>
2006-08-06 16:43:55 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@7570 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/people/friebe/jay/net/xp_framework/tools/vm/unittest/emit/php5/LoopEmitterTest.class.php
@@ -0,0 +1,68 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+ 
+  uses('net.xp_framework.tools.vm.unittest.emit.php5.AbstractEmitterTest');
+
+  /**
+   * Tests PHP5 emitter
+   *
+   * @purpose  Unit Test
+   */
+  class LoopEmitterTest extends AbstractEmitterTest {
+
+    /**
+     * Tests for-loop
+     *
+     * @access  public
+     */
+    #[@test]
+    function forLoop() {
+      $this->assertSourcecodeEquals(
+        'for ($i= 0;$i<10;$i++) {echo $i; };',
+        $this->emit('for ($i= 0; $i < 10; $i++) { echo $i; }')
+      );
+    }
+
+    /**
+     * Tests foreach-loop
+     *
+     * @access  public
+     */
+    #[@test]
+    function foreachLoop() {
+      $this->assertSourcecodeEquals(
+        'foreach (range(1, 2, 3) as $i) {echo $i; };',
+        $this->emit('foreach (range(1, 2, 3) as $i) { echo $i; }')
+      );
+    }
+
+    /**
+     * Tests while-loop
+     *
+     * @access  public
+     */
+    #[@test]
+    function whileLoop() {
+      $this->assertSourcecodeEquals(
+        'while ($i<10) {echo $i++; };',
+        $this->emit('while ($i < 10) { echo $i++; }')
+      );
+    }
+
+    /**
+     * Tests do-loop
+     *
+     * @access  public
+     */
+    #[@test]
+    function doLoop() {
+      $this->assertSourcecodeEquals(
+        'do {echo $i++; } while ($i<10);',
+        $this->emit('do { echo $i++; } while ($i < 10);')
+      );
+    }
+  }
+?>
