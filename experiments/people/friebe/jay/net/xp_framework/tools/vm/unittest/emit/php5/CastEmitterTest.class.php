8799dd2650b65e5063e2973ed0bafe3d2cce22d5
thekid <timm.friebe@gmail.com>
2006-08-11 15:50:31 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@7653 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/people/friebe/jay/net/xp_framework/tools/vm/unittest/emit/php5/CastEmitterTest.class.php
@@ -0,0 +1,55 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+ 
+  uses('net.xp_framework.tools.vm.unittest.emit.php5.AbstractEmitterTest');
+
+  /**
+   * Tests PHP5 emitter
+   *
+   * @purpose  Unit Test
+   */
+  class CastEmitterTest extends AbstractEmitterTest {
+
+    /**
+     * Tests a cast to int
+     *
+     * @access  public
+     */
+    #[@test]
+    function integerCast() {
+      $this->assertSourcecodeEquals(
+        '$i= (int)$str;',
+        $this->emit('$i= (int)$str;')
+      );
+    }
+
+    /**
+     * Tests a cast to string
+     *
+     * @access  public
+     */
+    #[@test]
+    function stringCast() {
+      $this->assertSourcecodeEquals(
+        '$s= (string)$num;',
+        $this->emit('$s= (string)$num;')
+      );
+    }
+
+    /**
+     * Tests a cast to string
+     *
+     * @access  public
+     */
+    #[@test]
+    function fqcnCast() {
+      $this->assertSourcecodeEquals(
+        '$s= xp::cast(\'xp~lang~Object\', $o->getClass());',
+        $this->emit('$s= (xp~lang~Object)$o->getClass();')
+      );
+    }
+  }
+?>
