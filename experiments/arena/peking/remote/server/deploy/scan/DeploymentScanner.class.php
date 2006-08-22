56b4e4e69f1cbe0e96b931177e1193dfe19a474a
thekid <timm.friebe@gmail.com>
2006-08-22 16:18:49 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@7811 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/arena/peking/remote/server/deploy/scan/DeploymentScanner.class.php
@@ -0,0 +1,24 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses('remote.server.deploy.Deployment');
+
+  /**
+   * Deployment scanner
+   *
+   * @purpose  Interface
+   */
+  class DeploymentScanner extends Interface {
+  
+    /**
+     * Get a list of deployments
+     *
+     * @access  public
+     * @return  remote.server.deploy.Deployable[]
+     */
+    function getDeployments() { }
+  }
+?>
---
+++ experiments/arena/peking/remote/server/deploy/scan/FileSystemScanner.class.php
@@ -0,0 +1,88 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses(
+    'io.cca.ArchiveClassLoader',
+    'io.File',
+    'io.Folder',
+    'util.Properties',
+    'remote.server.deploy.IncompleteDeployment'
+  );
+  
+  /**
+   * Deployment scanner that as
+   *
+   * @purpose  Interface
+   */
+  class FileSystemScanner extends Object {
+    var
+      $folder   = NULL,
+      $pattern  = '';
+      
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   string dir
+     */
+    function __construct($dir, $pattern= '.ear$') {
+      $this->folder= &new Folder($dir);
+      $this->pattern= '/'.$pattern.'/';
+    }
+  
+    /**
+     * Get a list of deployments
+     *
+     * @access  public
+     * @return  remote.server.deploy.Deployable[]
+     */
+    function getDeployments() {
+      $deployments= array();
+
+      while ($entry= $this->folder->getEntry()) {
+        if (!preg_match($this->pattern, $entry)) continue;
+
+        $ear= &new Archive(new File($this->folder->getURI().$entry));
+        try(); {
+          $ear->open(ARCHIVE_READ) &&
+          $meta= $ear->extract('META-INF/bean.properties');
+        } if (catch('Exception', $e)) {  
+          $deployments[]= &new IncompleteDeployment($entry, $e);
+          continue;
+        }
+        
+        $prop= &Properties::fromString($meta);
+        $beanclass= $prop->readString('bean', 'class');
+        
+        if (!$beanclass) {
+          $deployments[]= &new IncompleteDeployment($entry, new FormatException('bean.class property missing!'));
+          continue;
+        }
+
+        $cl= &new ArchiveClassLoader($ear);
+        try(); {
+          $class= &$cl->loadClass($beanclass);
+          $class && $deployments[]= &new Deployment($entry, $class);
+        } if (catch('Exception', $e)) {
+          $deployments[]= &new IncompleteDeployment($entry, $e);
+        }
+      }
+
+      return $deployments;
+    }
+    
+    /**
+     * Creates a string representation of this object
+     *
+     * @access  public
+     * @return  string
+     */
+    function toString() {
+      return $this->getClassName().'(pattern= '.$this->pattern.') {'.$this->folder->toString().'}';
+    }
+
+  } implements(__FILE__, 'remote.server.deploy.scan.DeploymentScanner');
+?>
