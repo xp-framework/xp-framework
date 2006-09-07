11a500006d4acde8f4d91f75a38e1d91788319fb
kiesel <alex@kiesel.name>
2006-09-07 11:16:23 +0000

    - initial revision
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@7878 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ util/etc/nedit/xp_completion.sh
@@ -0,0 +1,44 @@
+#!/usr/local/bin/bash
+#
+# $Id$
+#
+
+#
+# This is a bash-completion script for the XP Framework classes
+#
+# Place it somewhere in your homedir and source it from your .bashrc like this
+#
+# . ~/.nedit/xp_completion.sh
+#
+# Use nx to encapsulate "nedit for XP"
+
+export PHP_INCLUDE_PATH=`php -r 'echo ini_get("include_path");'`
+
+_xpclasses() {
+  local CUR CP_PREFIX RESULTS DIR
+  COMPREPLY=()
+
+  CUR=${COMP_WORDS[$COMP_CWORD]}
+  
+  local IFS=':'
+  CP_PREFIX=`echo $CUR | tr . /`
+  for DIR in $PHP_INCLUDE_PATH; do
+    IFS=" "
+    RESULTS=$( \
+      find $DIR/$CP_PREFIX* -type d ! -path '*CVS*' ! -path '*.svn*' ! -name '.*' -depth 0 -maxdepth 1 -exec echo {}. \; 2>/dev/null; \
+      find $DIR/$CP_PREFIX* -type f -name '*.class.php' ! -path '*CVS*' ! -path '*.svn*' ! -name '.*' -depth 0 -maxdepth 1 2>/dev/null \
+    )
+    
+    #echo "===> RESULTS= $RESULTS"
+    FILTERED=$( echo $RESULTS | gsed -r "s#$DIR/?##g" | gsed -r "s#\.class\.php\$##g" | tr / . )
+    
+    unset IFS
+    for ROW in ${FILTERED}; do
+      if [ -n "$ROW" ]; then
+        COMPREPLY[${#COMPREPLY[@]}]=$ROW
+      fi
+    done
+  done
+}
+
+complete -o nospace -F _xpclasses nx
