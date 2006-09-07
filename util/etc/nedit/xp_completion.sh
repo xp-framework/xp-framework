d96f4267462a033bfe323af1aaef9dc46ec1cd7e
Christian Gellweiler <gelli@developer.xp-framework.net>
2006-09-07 11:33:10 +0000

    - Use -mindepth instead of -depth - Check which sed is installed on the system
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@7879 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- util/etc/nedit/xp_completion.sh
+++ util/etc/nedit/xp_completion.sh
@@ -12,25 +12,27 @@
 #
 # Use nx to encapsulate "nedit for XP"
 
+export SED_CMD=`which gsed sed 2>/dev/null | head -n1`
+
 export PHP_INCLUDE_PATH=`php -r 'echo ini_get("include_path");'`
+
 
 _xpclasses() {
   local CUR CP_PREFIX RESULTS DIR
   COMPREPLY=()
 
   CUR=${COMP_WORDS[$COMP_CWORD]}
-  
   local IFS=':'
   CP_PREFIX=`echo $CUR | tr . /`
   for DIR in $PHP_INCLUDE_PATH; do
     IFS=" "
     RESULTS=$( \
-      find $DIR/$CP_PREFIX* -type d ! -path '*CVS*' ! -path '*.svn*' ! -name '.*' -depth 0 -maxdepth 1 -exec echo {}. \; 2>/dev/null; \
-      find $DIR/$CP_PREFIX* -type f -name '*.class.php' ! -path '*CVS*' ! -path '*.svn*' ! -name '.*' -depth 0 -maxdepth 1 2>/dev/null \
+      find $DIR/$CP_PREFIX* -type d ! -path '*CVS*' ! -path '*.svn*' ! -name '.*' -mindepth 0 -maxdepth 1 -exec echo {}. \; 2>/dev/null; \
+      find $DIR/$CP_PREFIX* -type f -name '*.class.php' ! -path '*CVS*' ! -path '*.svn*' ! -name '.*' -mindepth 0 -maxdepth 1 2>/dev/null \
     )
     
-    #echo "===> RESULTS= $RESULTS"
-    FILTERED=$( echo $RESULTS | gsed -r "s#$DIR/?##g" | gsed -r "s#\.class\.php\$##g" | tr / . )
+    # echo "===> RESULTS= $RESULTS"
+    FILTERED=$( echo $RESULTS | $SED_CMD -r "s#$DIR/?##g" | $SED_CMD -r "s#\.class\.php\$##g" | tr / . )
     
     unset IFS
     for ROW in ${FILTERED}; do
