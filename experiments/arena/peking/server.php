bbd83ee848ab49e35442ea70d64ff619de164f65
Christian Gellweiler <gelli@developer.xp-framework.net>
2006-09-11 11:11:30 +0000

    - Move deploymentscanner into protocol
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@7896 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/peking/server.php
+++ experiments/arena/peking/server.php
@@ -4,7 +4,7 @@
   uses(
     'util.log.Logger',
     'util.log.ColoredConsoleAppender',
-    'peer.server.Server',
+    'peer.server.PreforkingServer',
     'remote.server.deploy.Deployer',
     'remote.server.deploy.scan.FileSystemScanner',
     'remote.server.ContainerManager',
@@ -17,23 +17,13 @@
   $cat->addAppender(new ColoredConsoleAppender());
   
   // {{{ main
-  with ($server= &new Server($argv[1], 6448)); {
-    $server->setProtocol(new EascProtocol());
-    $server->setTcpNodelay(TRUE);
+  with ($server= &new PreforkingServer($argv[1], 6448)); {
+    $server->setProtocol(new EascProtocol(
+      new FileSystemScanner(dirname(__FILE__).'/deploy')
+    ));
     
-    // Perform deployment
-    $scanner= &new FileSystemScanner(dirname(__FILE__).'/deploy');
-    $cat->info('Deploying all beans from', $scanner);
-    $cm= &new ContainerManager();
-    $deployer= &new Deployer();
-    foreach ($scanner->getDeployments() as $deployment) {
-      try(); {
-        $deployer->deployBean($deployment, $cm);
-      } if (catch('DeployException', $e)) {
-        $cat->warn($e);
-        // Fall through
-      }
-    }
+    $server->setTcpNodelay(TRUE);
+    $server->setTrace($cat);
 
     $cat->info('Starting server');
     $server->init();
