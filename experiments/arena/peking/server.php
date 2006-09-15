1bb69ce1c0ec0bfa648afc96a593bfb1b12dd794
Christian Gellweiler <gelli@developer.xp-framework.net>
2006-09-15 15:27:29 +0000

    - Add ScannerThread
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@7922 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/peking/server.php
+++ experiments/arena/peking/server.php
@@ -8,7 +8,8 @@
     'remote.server.deploy.Deployer',
     'remote.server.deploy.scan.FileSystemScanner',
     'remote.server.ContainerManager',
-    'remote.server.EascProtocol'
+    'remote.server.EascProtocol',
+    'remote.server.ScannerThread'
   );
   
   // Set up loggin
@@ -17,6 +18,12 @@
   $cat->addAppender(new ColoredConsoleAppender());
   
   // {{{ main
+  declare(ticks= 1);
+
+  $thread= &new ScannerThread(new FileSystemScanner(dirname(__FILE__).'/deploy'));
+  $thread->setTrace($cat);
+  $thread->setScanPeriod(5);
+  
   with ($server= &new PreforkingServer($argv[1], 6448)); {
     $server->setProtocol(new EascProtocol(
       new FileSystemScanner(dirname(__FILE__).'/deploy')
@@ -25,10 +32,12 @@
     $server->setTcpNodelay(TRUE);
     $server->setTrace($cat);
 
-    declare(ticks= 1);
+
     $cat->info('Starting server');
     $server->init();
+    $thread->start();
     $server->service();
+    $thread->stop();
     $server->shutdown();
   }
   // }}}
