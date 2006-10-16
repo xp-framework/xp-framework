7a92723a3c23e2fcb0657c80af33932c87c237a7
thekid <timm.friebe@gmail.com>
2006-10-16 10:39:37 +0000

    - Remove parseEntryFrom() method and make use of FtpEntry::parseFrom() instead
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@8190 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/net/xp_framework/unittest/peer/FtpRawListTest.class.php
+++ skeleton/net/xp_framework/unittest/peer/FtpRawListTest.class.php
@@ -20,61 +20,13 @@
   class FtpRawListTest extends TestCase {
       
     /**
-     * Parse entry from string. FIXME: This method should really be in 
-     * FtpDir or a utility class, but not here!
-     *
-     * @access  protected
-     * @param   string entry
-     * @return  &peer.ftp.FtpEntry
-     */
-    function &parseEntryFrom($entry) {
-      sscanf(
-        $entry, 
-        '%s %d %s %s %d %s %d %[^ ] %[^$]',
-        $permissions,
-        $numlinks,
-        $user,
-        $group,
-        $size,
-        $month,
-        $day,
-        $date,
-        $filename
-      );
-      
-      if ('d' == $permissions{0}) {
-        $e= &new FtpDir($filename, $this->_hdl);
-      } else {
-        $e= &new FtpEntry($filename, $this->_hdl);
-      }
-      with ($e); {
-        $d= &new Date($month.' '.$day.' '.(strstr($date, ':') ? date('Y').' '.$date : $date));
-        
-        // Check for "recent" file which are specified "HH:MM" instead
-        // of year for the last 6 month (as specified in coreutils/src/ls.c)
-        if (strstr($date, ':')) {
-          $now= &Date::now();
-          if ($d->getMonth() > $now->getMonth()) $d->year--;
-        }
-        
-        $e->setPermissions(substr($permissions, 1));
-        $e->setNumlinks($numlinks);
-        $e->setUser($user);
-        $e->setGroup($group);
-        $e->setSize($size);
-        $e->setDate($d);
-      }
-      return $e;
-    }
-
-    /**
      * Test directory
      *
      * @access  public
      */
     #[@test]
     function dotDirectory() {
-      $e= &$this->parseEntryFrom('drwx---r-t 37 p159995 ftpusers 4096 Apr 4 20:16 .');
+      $e= &FtpEntry::parseFrom('drwx---r-t 37 p159995 ftpusers 4096 Apr 4 20:16 .');
 
       $this->assertSubclass($e, 'peer.ftp.FtpDir') &&
       $this->assertEquals('.', $e->getName()) &&
@@ -93,7 +45,7 @@
      */
     #[@test]
     function regularFile() {
-      $e= &$this->parseEntryFrom('-rw----r-- 1 p159995 ftpusers 415 May 23 2000 write.html');
+      $e= &FtpEntry::parseFrom('-rw----r-- 1 p159995 ftpusers 415 May 23 2000 write.html');
 
       $this->assertSubclass($e, 'peer.ftp.FtpEntry') &&
       $this->assertEquals('write.html', $e->getName()) &&
@@ -112,7 +64,7 @@
      */
     #[@test]
     function whitespaceInFileName() {
-      $e= &$this->parseEntryFrom('-rw----r-- 1 p159995 ftpusers 415 May 23 2000 answer me.html');
+      $e= &FtpEntry::parseFrom('-rw----r-- 1 p159995 ftpusers 415 May 23 2000 answer me.html');
 
       $this->assertSubclass($e, 'peer.ftp.FtpEntry') &&
       $this->assertEquals('answer me.html', $e->getName()) &&
