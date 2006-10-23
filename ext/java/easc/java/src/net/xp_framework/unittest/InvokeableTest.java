ba3d040d0429418ad0fd7bb8a38b53be732718fb
thekid <timm.friebe@gmail.com>
2006-10-23 15:18:32 +0000

    - Uncomment "BROKEN" source - works now (after removing caching from serializer) - Add another test, ejbHomeAndEjbHomeExtClass()
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@8231 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ext/java/easc/java/src/net/xp_framework/unittest/InvokeableTest.java
+++ ext/java/easc/java/src/net/xp_framework/unittest/InvokeableTest.java
@@ -23,6 +23,8 @@ import static net.xp_framework.easc.protocol.standard.Serializer.unregisterMappi
  */
 public class InvokeableTest {
 
+    static interface EJBHomeExt extends EJBHome { }
+
     /**
      * Helper method
      *
@@ -30,6 +32,20 @@ public class InvokeableTest {
      */
     protected EJBHome anonymousEjbHomeInstance() {
         return new EJBHome() { 
+            public javax.ejb.HomeHandle getHomeHandle() { return null; }
+            public javax.ejb.EJBMetaData getEJBMetaData() { return null; }
+            public void remove(Object o) { }
+            public void remove(javax.ejb.Handle h) { }
+        };
+    }
+
+    /**
+     * Helper method
+     *
+     * @return  An anonymous EJBHome instance
+     */
+    protected EJBHomeExt anonymousEjbHomeExtInstance() {
+        return new EJBHomeExt() { 
             public javax.ejb.HomeHandle getHomeHandle() { return null; }
             public javax.ejb.EJBMetaData getEJBMetaData() { return null; }
             public void remove(Object o) { }
@@ -50,6 +66,9 @@ public class InvokeableTest {
         
         // Map EJBHome
         registerMapping(EJBHome.class, new Invokeable<String, EJBHome>() {
+            public String toString() {
+              return "Invokeable<" + EJBHome.class.getName() + ">";
+            }
             public String invoke(EJBHome p, Object arg) throws Exception {
                 return arg.toString();
             }
@@ -59,6 +78,49 @@ public class InvokeableTest {
         
         // Unregister, should be unknown again (= use default invokeable)
         unregisterMapping(EJBHome.class);
-        // assertEquals(null, invokeableFor(c));    BROKEN!!!
+        assertEquals(null, invokeableFor(c));
+    }
+
+    /**
+     * Tests 
+     *
+     */
+    @Test public void ejbHomeAndEjbHomeExtClass() throws Exception {
+        EJBHomeExt ix= this.anonymousEjbHomeExtInstance();
+        Class cx= ix.getClass();
+        EJBHome i= this.anonymousEjbHomeInstance();
+        Class c= i.getClass();
+
+        // Before: Unknown (= use default invokeable)
+        assertEquals(null, invokeableFor(cx));
+        assertEquals(null, invokeableFor(c));
+        
+        // Map EJBHomeExt and EJBHome
+        registerMapping(EJBHomeExt.class, new Invokeable<String, EJBHomeExt>() {
+            public String toString() {
+              return "Invokeable<" + EJBHomeExt.class.getName() + ">";
+            }
+            public String invoke(EJBHomeExt p, Object arg) throws Exception {
+                return arg.toString();
+            }
+        });
+        registerMapping(EJBHome.class, new Invokeable<String, EJBHome>() {
+            public String toString() {
+              return "Invokeable<" + EJBHome.class.getName() + ">";
+            }
+            public String invoke(EJBHome p, Object arg) throws Exception {
+                return arg.toString();
+            }
+        });
+        assertTrue(invokeableFor(cx) instanceof Invokeable);
+        assertEquals("EXT", ((Invokeable)invokeableFor(cx)).invoke(ix, new String("EXT")));
+        assertTrue(invokeableFor(c) instanceof Invokeable);
+        assertEquals("OK", ((Invokeable)invokeableFor(c)).invoke(i, new String("OK")));
+        
+        // Unregister, should be unknown again (= use default invokeable)
+        unregisterMapping(EJBHomeExt.class);
+        unregisterMapping(EJBHome.class);
+        assertEquals(null, invokeableFor(cx));
+        assertEquals(null, invokeableFor(c));
     }
 }
