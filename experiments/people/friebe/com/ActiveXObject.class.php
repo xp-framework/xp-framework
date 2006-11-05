31005411708d03c8bf297512d422b435828d0b59
thekid <timm.friebe@gmail.com>
2006-11-05 15:22:25 +0000

    - QA: Add apidoc
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@8320 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/people/friebe/com/ActiveXObject.class.php
+++ experiments/people/friebe/com/ActiveXObject.class.php
@@ -1,16 +1,42 @@
 <?php
+/* This class is part of the XP framework's experiments
+ *
+ * $Id$
+ */
+ 
+  /**
+   * ActiveXObject class
+   *
+   * @ext      com
+   * @see      http://www.webreference.com/js/column55/activex.html
+   * @purpose  COM Wrapper
+   */
   class ActiveXObject extends Object {
     var
-      $object  = '',
+      $_qname  = '',
       $_handle = NULL;
 
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   string class
+     * @param   string server default NULL
+     * @throws  lang.IllegalArgumentException if the automation object cannot be loaded
+     */
     function __construct($class, $server= NULL) {
-      $this->object= $class.($server ? '@'.$server : '');
+      $this->_qname= $class.($server ? '@'.$server : '');
       if (!($this->_handle= com_load($class, $server))) {
-        throw(new IllegalArgumentException('Cannot load '.$this->object));
+        throw(new IllegalArgumentException('Cannot load '.$this->_qname));
       }
     }
     
+    /**
+     * Creates a string representation of this object
+     *
+     * @access  public
+     * @return  string
+     */
     function toString() {
       ob_start();
       com_print_typeinfo($this->_handle, NULL, FALSE);
@@ -18,9 +44,14 @@
       ob_end_clean();
       
       sscanf($buffer, 'class %[^ ] { /* GUID={%[^}]}', $class, $guid);
-      return $this->getClassName().'<'.$this->object.'>('.$class.'$'.$guid.')';
+      return $this->getClassName().'<'.$this->_qname.'>('.$class.'$'.$guid.')';
     }
 
+    /**
+     * Destructor. Releases the com automation handle.
+     *
+     * @access  public
+     */
     function __destruct() {
       if ($this->_handle) {
         com_release($this->_handle);
