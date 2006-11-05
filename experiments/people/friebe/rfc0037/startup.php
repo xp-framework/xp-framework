3919a0d757e13f7c238173a43d5c0efeb14c3f12
thekid <timm.friebe@gmail.com>
2006-11-05 14:58:24 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@8318 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/people/friebe/rfc0037/startup.php
@@ -0,0 +1,31 @@
+<?php
+/* This file is part of the XP framework's peoples' experiments
+ *
+ * $Id: startup.php 5797 2005-09-21 11:41:41Z friebe $
+ */
+
+  function _microtime() {
+    list($usec, $sec) = explode(' ', microtime()); 
+    return (float)$usec + (float)$sec;
+  }
+
+  $startup_start= _microtime();
+  require($argv[1].'/lang.base.php');
+  $startup_stop= _microtime();
+
+  $core_classes= XPClass::getClasses();
+  
+  $load_start= _microtime();
+  uses('util.Binford', 'util.Date', 'util.Hashmap');
+  $load_stop= _microtime();
+  
+  printf("Startup time: %.3f seconds\n", $startup_stop - $startup_start);
+  foreach ($core_classes as $i => $class) {
+    printf("- Core #%2d: %s\n", $i, $class->getName());
+  }
+
+  printf("Load classes: %.3f seconds\n", $load_stop - $load_start);
+  foreach (array_slice(XPClass::getClasses(), sizeof($core_classes)) as $i => $class) {
+    printf("- User #%2d: %s\n", $i, $class->getName());
+  }
+?>
