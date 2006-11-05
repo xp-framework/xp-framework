038d0edbb7e326f58c83e0523b1ed2acde4bb906
thekid <timm.friebe@gmail.com>
2006-11-05 15:26:43 +0000

    - Show class information
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@8323 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/people/friebe/com/reflection.php
+++ experiments/people/friebe/com/reflection.php
@@ -16,5 +16,11 @@
   }
 
   Console::writeLine($object->toString());
+  
+  with ($class= &$object->getClass()); {
+    Console::writeLine('Class name:  ', $class->getName());
+    Console::writeLine('Constructor: ', xp::stringOf($class->getConstructor()));
+    Console::writeLine('Methods:     ', xp::stringOf($class->getMethods()));
+  }
   // }}}
 ?>
