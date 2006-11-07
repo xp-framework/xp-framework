0949193bfe62bc450395a0851280877881427b46
thekid <timm.friebe@gmail.com>
2006-11-07 09:22:02 +0000

    - Refactor to make use of RFC #0087
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@8338 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/people/friebe/com/ActiveXObject.class.php
+++ experiments/people/friebe/com/ActiveXObject.class.php
@@ -4,6 +4,8 @@
  * $Id$
  */
  
+  uses('ComClassLoader');
+  
   /**
    * ActiveXObject class
    *
@@ -13,49 +15,17 @@
    */
   class ActiveXObject extends Object {
     var
-      $_qname  = '',
-      $_handle = NULL;
-
-    /**
-     * Constructor
-     *
-     * @access  public
-     * @param   string class
-     * @param   string server default NULL
-     * @throws  lang.IllegalArgumentException if the automation object cannot be loaded
-     */
-    function __construct($class, $server= NULL) {
-      $this->_qname= $class.($server ? '@'.$server : '');
-      if (!($this->_handle= com_load($class, $server))) {
-        throw(new IllegalArgumentException('Cannot load '.$this->_qname));
-      }
-    }
+      $var = NULL;
     
-    /**
-     * Creates a string representation of this object
-     *
-     * @access  public
-     * @return  string
-     */
-    function toString() {
-      ob_start();
-      com_print_typeinfo($this->_handle, NULL, FALSE);
-      $buffer= ob_get_contents();
-      ob_end_clean();
-      
-      sscanf($buffer, 'class %[^ ] { /* GUID={%[^}]}', $class, $guid);
-      return $this->getClassName().'<'.$this->_qname.'>('.$class.'$'.$guid.')';
-    }
-
     /**
      * Destructor. Releases the com automation handle.
      *
      * @access  public
      */
     function __destruct() {
-      if ($this->_handle) {
-        com_release($this->_handle);
-        $this->_handle= NULL;
+      if ($this->var) {
+        com_release($this->var);
+        $this->var= NULL;
       }
     }
   }
--- experiments/people/friebe/com/reflection.php
+++ experiments/people/friebe/com/reflection.php
@@ -5,11 +5,11 @@
  */
   require('lang.base.php');
   xp::sapi('cli');
-  uses('ActiveXObject');
+  uses('ActiveX');
   
   // {{{ main
   try(); {
-    $object= &new ActiveXObject($argv[1]);
+    $object= &ActiveX::forName($argv[1]);
   } if (catch('Exception', $e)) {
     $e->printStackTrace();
     exit(-1);
@@ -18,9 +18,12 @@
   Console::writeLine($object->toString());
   
   with ($class= &$object->getClass()); {
-    Console::writeLine('Class name:  ', $class->getName());
-    Console::writeLine('Constructor: ', xp::stringOf($class->getConstructor()));
-    Console::writeLine('Methods:     ', xp::stringOf($class->getMethods()));
+    Console::writeLine('Class name:   ', $class->getName());
+    Console::writeLine('Annotations:  ', xp::stringOf($class->getAnnotations()));
+    Console::writeLine('Classloader:  ', xp::stringOf($class->getClassLoader()));
+    Console::writeLine('Constructor:  ', xp::stringOf($class->getConstructor()));
+    Console::writeLine('Methods:      ', xp::stringOf($class->getMethods()));
+    Console::writeLine('Fields:       ', xp::stringOf($class->getFields()));
   }
   // }}}
 ?>
