38174915cf1448c32d3f213dc3168576a4befc8f
thekid <timm.friebe@gmail.com>
2006-11-15 16:04:11 +0000

    - Add -d / --showdeclaring arg
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@8467 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/people/friebe/cli/Api.class.php
+++ experiments/people/friebe/cli/Api.class.php
@@ -13,7 +13,8 @@
    */
   class Api extends Object {
     var
-      $class= '';
+      $class          = '',
+      $showDeclaring  = FALSE;
 
     /**
      * Set classname
@@ -24,6 +25,17 @@
     #[@arg(position= 0)]
     function setClassname($classname) {
       $this->class= &XPClass::forName($classname);
+    }
+
+    /**
+     * Set whether declaring classes should be shown
+     *
+     * @access  public
+     * @param   bool show default FALSE
+     */
+    #[@arg(short= 'd')]
+    function setShowDeclaring($show= FALSE) {
+      $this->showDeclaring= (bool)$show;
     }
 
     /**
@@ -40,8 +52,10 @@
       if ($this->class->hasConstructor()) {
         Console::writeLine('  ', xp::stringOf($this->class->getConstructor()));
       }
-      foreach($this->class->getMethods() as $field) {
-        Console::writeLine('  ', xp::stringOf($field));
+      foreach($this->class->getMethods() as $method) {
+        Console::write('  ', xp::stringOf($method));
+        $this->showDeclaring && Console::write(' declared in '.xp::stringOf($method->getDeclaringClass()));
+        Console::writeLine();
       }
       Console::writeLine('}');
     }
