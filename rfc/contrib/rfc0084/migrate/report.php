8f54f7eb65432002563c2790d959ed6c8602fbb4
thekid <timm.friebe@gmail.com>
2006-11-16 17:12:38 +0000

    - Make list of acceptible extensions passable via command line - Do not create a report when nothing can be found - QA: Update usage
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@8489 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- rfc/contrib/rfc0084/migrate/report.php
+++ rfc/contrib/rfc0084/migrate/report.php
@@ -11,6 +11,7 @@
     'io.File',
     'io.collections.FileCollection',
     'io.collections.iterate.FilteredIOCollectionIterator',
+    'io.collections.iterate.AnyOfFilter',
     'io.collections.iterate.ExtensionEqualsFilter'
   );
   
@@ -49,31 +50,56 @@
     Console::writeLine(<<<__
 Creates a report for all files in a given directpry
 
-Usage: php report.php <base_directory> [-O output]
+Usage: 
+$ php report.php <basedir> [-O output] [-r report] [-e extensions]
+
+Arguments:
+* output defines the file to write the report to
+  Default: "./rfc-0084_" + basename(basedir) + ".report"
+
+* report controls the report format. Available: text or html
+  Default: text
+
+* extensions is a comma-separated list of extension to search for
+  Default: php
+  
 __
     );
     exit(1);
   }
 
+  // Initialize
   try(); {
+    $scan= &new FileCollection($p->value(1));
     $rules= &Rules::allRules();
     $report= &ReportFactory::factory($p->value('report', 'r', 'text'));
-    $scan= &new FileCollection($p->value(1));
     $out= &new File($p->value('output', 'O', 'rfc-0084_'.basename($scan->getURI()).'.report'));
+
+    $filters= array();
+    foreach (explode(',', $p->value('extensions', 'e', 'php')) as $ext) {
+      $filters[]= &new ExtensionEqualsFilter($ext);
+    }
   } if (catch('Exception', $e)) {
     $e->printStackTrace();
     exit(-1);
   }
 
+  // Iterate
   Console::writeLine('===> Generating ', $report->getType(), ' report for ', $scan->getURI(), ' to ', $out->getURI());
   for (
-    $it= &new FilteredIOCollectionIterator($scan, new ExtensionEqualsFilter('.php'), TRUE);
+    $it= &new FilteredIOCollectionIterator($scan, new AnyOfFilter($filters), TRUE);
     $it->hasNext();
   ) {
     performWork($it->next(), $report, $rules);
   }
   
   Console::writeLine();
+  
+  if ($report->isEmpty()) {
+    Console::writeLine('===> Nothing to be migrated here');
+    exit(0);
+  }
+  
   Console::writeLine('---> Creating summary');
   $report->summarize($scan, $out, $rules);
   Console::writeLine('===> Done');
