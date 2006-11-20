fec236accb28d3ecb48b1ffb0898aa07b450f42c
thekid <timm.friebe@gmail.com>
2006-11-20 20:52:34 +0000

    - Add an example of raw data from an Windows FTPd
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@8524 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/peer/ftp/FtpEntry.class.php
+++ skeleton/peer/ftp/FtpEntry.class.php
@@ -41,10 +41,17 @@
     /**
      * Parse raw listing entry:
      *
-     * Examples:
+     * Example (Un*x):
      * <pre>
      *   drwx---r-t 37 p159995 ftpusers 4096 Apr 4 20:16 .
      *   -rw----r-- 1 p159995 ftpusers 415 May 23 2000 write.html
+     * </pre>
+     *
+     * Example (Windows):
+     * <pre>
+     *   01-04-06  04:51PM       <DIR>          _db_import
+     *   12-23-05  04:49PM                  807 1and1logo.gif
+     *   11-08-06  10:04AM                   27 info.txt 
      * </pre>
      *
      * @model   static
