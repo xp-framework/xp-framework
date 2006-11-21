406687e586574a2d109650adda8431cc797127ec
thekid <timm.friebe@gmail.com>
2006-11-21 13:34:50 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@8599 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/people/friebe/jay/net/xp_framework/tools/vm/unittest/migrate/MethodTest.class.php
@@ -0,0 +1,68 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+ 
+  uses('net.xp_framework.tools.vm.unittest.migrate.AbstractRewriterTest');
+
+  /**
+   * Tests method rewriting
+   *
+   * @purpose  Unit Test
+   */
+  class MethodTest extends AbstractRewriterTest {
+
+    /**
+     * Tests default modifier is "public" and default return type is "void".
+     *
+     * @access  public
+     */
+    #[@test]
+    function defaultModifierAndReturnValue() {
+      $this->assertMethodRewritten(
+        'public void noop() { }', 
+        'noop', array(), '() { }'
+      );
+    }
+
+    /**
+     * Tests protected modifier
+     *
+     * @access  public
+     */
+    #[@test]
+    function protectedMethod() {
+      $this->assertMethodRewritten(
+        'protected void noop() { }', 
+        'noop', array('@access' => array('protected')), '() { }'
+      );
+    }
+
+    /**
+     * Tests protected modifier
+     *
+     * @access  public
+     */
+    #[@test]
+    function privateMethod() {
+      $this->assertMethodRewritten(
+        'private void noop() { }', 
+        'noop', array('@access' => array('private')), '() { }'
+      );
+    }
+
+    /**
+     * Tests static modifier
+     *
+     * @access  public
+     */
+    #[@test]
+    function staticMethod() {
+      $this->assertMethodRewritten(
+        'public static void noop() { }', 
+        'noop', array('@model' => array('static')), '() { }'
+      );
+    }
+  }
+?>
