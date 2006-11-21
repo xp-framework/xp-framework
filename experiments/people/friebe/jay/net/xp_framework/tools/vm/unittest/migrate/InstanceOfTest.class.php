ceb1734694c56c0d9e366d8aa89e14815592a0a1
thekid <timm.friebe@gmail.com>
2006-11-21 12:53:40 +0000

    - Add test expressionAndString()
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@8588 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/people/friebe/jay/net/xp_framework/tools/vm/unittest/migrate/InstanceOfTest.class.php
+++ experiments/people/friebe/jay/net/xp_framework/tools/vm/unittest/migrate/InstanceOfTest.class.php
@@ -22,5 +22,15 @@
     function variableAndString() {
       $this->assertRewritten('($a instanceof util.Date);', 'is_a($a, "Date");');
     }
+
+    /**
+     * Tests is_a() when used with a expression and a string
+     *
+     * @access  public
+     */
+    #[@test]
+    function expressionAndString() {
+      $this->assertRewritten('(get_class($this) instanceof util.Date);', 'is_a(get_class($this), "Date");');
+    }
   }
 ?>
