ca34d633327a223f636cc81ee83afe7bd4cad204
thekid <timm.friebe@gmail.com>
2006-11-27 22:08:55 +0000

    - Add core functionality newinstance() # Implements RFC #0080
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@8686 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/lang.base.php
+++ skeleton/lang.base.php
@@ -551,6 +551,42 @@
   }
   // }}}
 
+  // {{{ proto &lang.Object newinstance(string classname, mixed[] args, string bytes)
+  //     Anonymous instance creation
+  function &newinstance($classname, $args, $bytes) {
+    static $u= 0;
+
+    $class= xp::reflect($classname);
+    if (!class_exists($class)) {
+      xp::error(xp::stringOf(new Error('Class "'.$classname.'" does not exist')));
+      // Bails
+    }
+
+    $name= $class.'·'.(++$u);
+    xp::registry('class.'.$name, $name);
+    
+    // Build paramstr for evaluation
+    for ($paramstr= '', $i= 0, $m= sizeof($args); $i < $m; $i++) {
+      $paramstr.= ', $args['.$i.']';
+    }
+
+    // Checks whether an interface or a class was given
+    $c= $class;
+    while ($c= get_parent_class($c)) {
+      if ('interface' != $c) continue;
+      
+      // It's an interface
+      eval('class '.$name.' extends Object '.$bytes.' $instance= &new '.$name.'('.substr($paramstr, 2).');');
+      implements($name.'.class.php', $class);
+      return $instance;
+    }
+    
+    // It's a class
+    eval('class '.$name.' extends '.$class.' '.$bytes.' $instance= &new '.$name.'('.substr($paramstr, 2).');');
+    return $instance;
+  }
+  // }}}
+
   // {{{ initialization
   error_reporting(E_ALL);
   
