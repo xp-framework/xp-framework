61e3cc82cd6c921b0518d423d1d55f066465c0cc
thekid <timm.friebe@gmail.com>
2006-12-03 22:19:32 +0000

    - Initial reease
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@8731 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/people/friebe/jay/net/xp_framework/tools/vm/unittest/emit/php5/TypeErrorsTest.class.php
@@ -0,0 +1,102 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses(
+    'unittest.TestCase',
+    'net.xp_framework.tools.vm.Parser',
+    'net.xp_framework.tools.vm.Lexer',
+    'net.xp_framework.tools.vm.emit.php5.Php5Emitter'
+  );
+
+  /**
+   * Test types
+   *
+   * @purpose  Test Case
+   */
+  class TypeErrorsTest extends TestCase {
+  
+    /**
+     * Asserts the given source raises a type error
+     *
+     * @access  protected
+     * @param   string source
+     * @param   string message
+     * @throws  unittest.AssertionFailedError
+     */
+    function assertTypeError($source, $message) {
+      $emitter= &new Php5Emitter();
+      
+      $parser= &new Parser();
+      $emitter->emitAll($parser->parse(new Lexer($source), '<'.$this->getName().'>'));
+      
+      $errors= $emitter->getErrors();
+      foreach ($errors as $e) { if (3001 == $e->code) return; }
+
+      $this->fail('No type errors occured ('.$message.')', $errors, '(type error)');
+    }
+    
+    /**
+     * Tests return value incorrectly casted raises an error
+     *
+     * @access  public
+     */
+    #[@test]
+    function returnIncorrectlyCastedValue() {
+      $this->assertTypeError('
+        class String {
+          protected string $buffer= "";
+
+          public bool matches($pattern) {
+            return (int)preg_match($pattern, $this->buffer);
+          }
+        }
+      ');
+    }
+
+    /**
+     * Tests return value type mismatch raises an error
+     *
+     * @access  public
+     */
+    #[@test]
+    function returnValueTypeMismatch() {
+      $this->assertTypeError('
+        class Test {
+          public string toString() {
+            return 1;
+          }
+        }
+      ');
+    }
+
+    /**
+     * Tests assigning a typed member to an incorrect type raises an error
+     *
+     * @access  public
+     */
+    #[@test]
+    function memberReassign() {
+      $this->assertTypeError('
+        class Test {
+          protected int $id= 0;
+          
+          public void setId() {
+            $this->id= "string";
+          }
+        }
+      ', 'hardcoded') &&
+      $this->assertTypeError('
+        class Test {
+          protected int $id= 0;
+          
+          public void setId(string $name) {
+            $this->id= $name;
+          }
+        }
+      ', 'passed');
+    }
+  }
+?>
