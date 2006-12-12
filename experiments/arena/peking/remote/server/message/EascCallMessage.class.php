5925a5fa35341effa62ab25d4d4a10b5dc003b74
Christian Gellweiler <gelli@developer.xp-framework.net>
2006-12-12 18:00:58 +0000

    - Create an remote.protocol.SerializedData object
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@8836 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/peking/remote/server/message/EascCallMessage.class.php
+++ experiments/arena/peking/remote/server/message/EascCallMessage.class.php
@@ -4,7 +4,10 @@
  * $Id$ 
  */
 
-  uses('remote.server.message.EascMessage');
+  uses(
+    'remote.server.message.EascMessage',
+    'remote.protocol.SerializedData'
+  );
 
   /**
    * (Insert class' description here)
@@ -25,12 +28,12 @@
     function handle(&$listener, $data) {
       $oid= unpack('Nzero/Noid', substr($data, 0, 8));
       $p= &$listener->context[RIH_OBJECTS_KEY]->get($oid['oid']);
-      
+
       $offset= 8;
       $method= $this->readString($data, $offset);
       
       $offset+= 2;  // ?
-      $args= $listener->serializer->valueOf($this->readString($data, $offset), $l, $listener->context);
+      $args= $listener->serializer->valueOf(new SerializedData($this->readString($data, $offset)), $l, $listener->context);
       try(); {
         $result= call_user_func_array(array(&$p, $method), $args->values);
         $this->setValue($result);
