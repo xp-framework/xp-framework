36394c78870c4225cb4f8410707e065ef3e0a809
No author <xp-framework-null@xp-framework.de>
2006-12-21 19:28:29 +0000

    -- new exceptions
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@8951 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/people/bayer/turpitude/src/java/net/xp_framework/turpitude/PHPEvalException.java
@@ -0,0 +1,11 @@
+package net.xp_framework.turpitude;
+
+import javax.script.*;
+
+public class PHPEvalException extends PHPScriptException {
+
+    public PHPEvalException(String msg) {
+        super(msg);
+    }
+
+}
---
+++ experiments/people/bayer/turpitude/src/java/net/xp_framework/turpitude/PHPScriptException.java
@@ -0,0 +1,11 @@
+package net.xp_framework.turpitude;
+
+import javax.script.*;
+
+public class PHPScriptException extends ScriptException {
+
+    public PHPScriptException(String msg) {
+        super(msg);
+    }
+
+}
