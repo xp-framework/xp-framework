a6a3bcca40c61f7c8543a867670894bad7772126
thekid <timm.friebe@gmail.com>
2006-12-27 15:18:19 +0000

    - Initial release # See RFC #0102
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@8968 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ util/bin/xpcli.php
@@ -0,0 +1,13 @@
+<?php
+/* This file is part of the XP framework's experiments
+ *
+ * $Id$ 
+ */
+  require('lang.base.php');
+  xp::sapi('cli');
+  uses('util.cmd.Runner');
+
+  // {{{ main
+  exit(Runner::main($argv));
+  // }}}
+?>
