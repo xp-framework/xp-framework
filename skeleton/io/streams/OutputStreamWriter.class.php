a057ea7b9af5829a266a5b6870b704639ad71c8f
thekid <timm.friebe@gmail.com>
2006-12-27 15:19:08 +0000

    - Initial release # See RFC #0102
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@8969 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/io/streams/OutputStreamWriter.class.php
@@ -0,0 +1,59 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Writes data to an OutputStream
+   *
+   * @purpose  Interface
+   */
+  interface OutputStreamWriter {
+
+    /**
+     * Constructor
+     *
+     * @param   io.streams.OutputStream out
+     */
+    public function __construct($out);
+  
+    /**
+     * Flush output buffer
+     *
+     */
+    public function flush();
+
+    /**
+     * Print arguments
+     *
+     * @param   mixed* args
+     */
+    public function write();
+    
+    /**
+     * Print arguments and append a newline
+     *
+     * @param   mixed* args
+     */
+    public function writeLine();
+    
+    /**
+     * Print a formatted string
+     *
+     * @param   string format
+     * @param   mixed* args
+     * @see     php://writef
+     */
+    public function writef();
+
+    /**
+     * Print a formatted string and append a newline
+     *
+     * @param   string format
+     * @param   mixed* args
+     */
+    public function writeLinef();
+  
+  }
+?>
