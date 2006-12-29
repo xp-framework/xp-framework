7b321dc83a9dda4f29749603ba0b6aa8b4f9af95
No author <xp-framework-null@xp-framework.de>
2006-12-29 23:56:42 +0000

    -- context sample added
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@9059 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/people/bayer/turpitude/src/java/net/xp_framework/turpitude/samples/ContextSample.java
@@ -0,0 +1,70 @@
+package net.xp_framework.turpitude.samples;
+
+import javax.script.*;
+import net.xp_framework.turpitude.PHPEvalException;
+import net.xp_framework.turpitude.PHPCompileException;
+
+public class ContextSample {
+
+   /**
+    * default constructor
+    */
+    public ContextSample() {
+    }
+
+    /**
+     * executes a script from a file
+     */
+    public void exec() {
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine eng = mgr.getEngineByName("turpitude");
+        if (null == eng) {
+            System.out.println("unable to find engine, please check classpath");
+            return;
+        }
+        System.out.println("found Engine: " + eng.getFactory().getEngineName());
+        ScriptContext ctx = eng.getContext();
+        ctx.setAttribute("string", "stringval", ScriptContext.ENGINE_SCOPE);
+
+        Object retval;
+        try {
+            retval = eng.eval(getSource());
+        } catch(PHPCompileException e) {
+            System.out.println("Compile Error:");
+            e.printStackTrace();
+            return;
+        } catch(PHPEvalException e) {
+            System.out.println("Eval Error:");
+            e.printStackTrace();
+            return;
+        } catch(ScriptException e) {
+            System.out.println("ScriptException caught:");
+            e.printStackTrace();
+            return;
+        }
+        if (null == retval)
+            System.out.println("done evaluating, return value " + retval);
+        else 
+            System.out.println("done evaluating, return value " + retval.getClass() + " : " + retval);
+    }
+
+    private static String getSource() {
+        StringBuffer src = new StringBuffer();
+        src.append("<?php");
+        src.append("$a = func_get_args();");
+        src.append("var_dump($a);");
+        src.append("?>"); 
+        return src.toString();
+    }
+
+    /**
+     * entry point
+     */
+    public static void main(String[] argv) {
+        ContextSample cs = new ContextSample();
+        cs.exec();
+    }
+ 
+
+}
+
