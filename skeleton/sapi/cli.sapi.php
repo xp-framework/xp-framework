b4828b925d07e9f303a80a2369b2389d07dedb66
thekid <timm.friebe@gmail.com>
2006-12-29 16:03:37 +0000

    - Add exception and fatal error handlers
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@9036 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/sapi/cli.sapi.php
+++ skeleton/sapi/cli.sapi.php
@@ -7,9 +7,38 @@
   uses('util.cmd.ParamString', 'util.cmd.Console');
   
   define('EPREPEND_IDENTIFIER', "\6100");
+
+  // {{{ final class sapi·cli
+  class sapi·cli {
+
+    // {{{ internal string output(string buf)
+    //     Output handler
+    function output($buf) {
+
+      // Check for fatal errors
+      if (FALSE !== ($p= strpos($buf, EPREPEND_IDENTIFIER))) {
+        $e= &new Error(str_replace(EPREPEND_IDENTIFIER, '', substr($buf, $p)));
+        fputs(STDERR, $e->toString());
+      }
+
+      return $buf;
+    }
+    // }}}
+    
+    // {{{ internal void except(Exception e)
+    //     Exception handler
+    function except($e) {
+      echo '[sapi::cli] Uncaught exception: '.xp::stringOf($e);
+    }    
+    // }}}
+  }
   
   if (PHP_SAPI != 'cli') {
     xp::error('[sapi::cli] Cannot be run under '.PHP_SAPI.' SAPI');
     // Bails out
   }
+
+  ini_set('error_prepend_string', EPREPEND_IDENTIFIER);
+  set_exception_handler(array('sapi·cli', 'except'));
+  ob_start(array('sapi·cli', 'output'));
 ?>
