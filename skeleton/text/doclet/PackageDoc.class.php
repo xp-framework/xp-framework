cc019cdcf5992f1f9e3f9b55dd0f9039b4f5536f
thekid <timm.friebe@gmail.com>
2007-01-03 14:23:53 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@9092 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/text/doclet/PackageDoc.class.php
@@ -0,0 +1,33 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+ 
+  uses('text.doclet.Doc');
+
+  /**
+   * Represents an XP package.
+   *
+   * @purpose  Documents a package
+   */
+  class PackageDoc extends Doc {
+
+    /**
+     * Constructor
+     *
+     */
+    public function __construct($name= NULL) {
+      $this->name= $name;
+    }
+
+    /**
+     * Returns a hashcode for this object
+     *
+     * @return  string
+     */
+    public function hashCode() {
+      return $this->getClassName().$this->name;
+    }
+  }
+?>
