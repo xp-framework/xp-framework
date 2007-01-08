9403a63b233a575866e731a0a624c37d78fcf07a
thekid <timm.friebe@gmail.com>
2007-01-08 11:39:17 +0000

    - QA: Remove reference operator from apidoc - QA: Simplify hasAttribute() - Make getBy*() methods static
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@9170 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/rdbms/DBTable.class.php
+++ skeleton/rdbms/DBTable.class.php
@@ -24,35 +24,34 @@
      */
     public function __construct($name) {
       $this->name= $name;
-      
     }
 
     /**
      * Get a table by it's name
      *
-     * @param   &rdbms.DBAdapter and adapter
+     * @param   rdbms.DBAdapter an adapter
      * @param   string name
-     * @return  &rdbms.DBTable a table object
+     * @return  rdbms.DBTable a table object
      */
-    public function getByName($adapter, $name) {
+    public static function getByName($adapter, $name) {
       return $adapter->getTable($name);
     }
 
     /**
      * Get tables by database
      *
-     * @param   &rdbms.DBAdapter and adapter
+     * @param   rdbms.DBAdapter and adapter
      * @param   string database
-     * @return  &rdbms.DBTable[] an array of table objects
+     * @return  rdbms.DBTable[] an array of table objects
      */
-    public function getByDatabase($adapter, $database) {
+    public static function getByDatabase($adapter, $database) {
       return $adapter->getTables($database);
     }
 
     /**
      * Get first attribute - Iterator function
      *
-     * @return  &rdbms.DBAttribute an attribute
+     * @return  rdbms.DBAttribute an attribute
      * @see     getNextAttribute
      */
     public function getFirstAttribute() {
@@ -65,14 +64,13 @@
      *
      * Example:
      * <code>
-     *   $table= &DBTable::getByName($adapter, 'person');
-     *   $attr= $table->getFirstAttribute();
+     *   $attr= DBTable::getByName($adapter, 'person')->getFirstAttribute();
      *   do {
      *     var_dump($attr);
      *   } while ($attr= $table->getNextAttribute());
      * </code>
      *
-     * @return  &rdbms.DBAttribute an attribute or FALSE if none more exist
+     * @return  rdbms.DBAttribute an attribute or FALSE if none more exist
      */
     public function getNextAttribute() {
       return next($this->attributes);
@@ -81,8 +79,8 @@
     /**
      * Add an attribute
      *
-     * @param   &rdbms.DBAttribute attr the attribute to add
-     * @return  &rdbms.DBAttribute the added attribute
+     * @param   rdbms.DBAttribute attr the attribute to add
+     * @return  rdbms.DBAttribute the added attribute
      */
     public function addAttribute($attr) {
       $this->attributes[]= $attr;
@@ -92,8 +90,8 @@
     /**
      * Add an index
      *
-     * @param   &rdbms.DBIndex index the index to add
-     * @return  &rdbms.DBIndex the added index
+     * @param   rdbms.DBIndex index the index to add
+     * @return  rdbms.DBIndex the added index
      */
     public function addIndex($index) {
       $this->indexes[]= $index;
@@ -103,8 +101,7 @@
     /**
      * Get first index - Iterator function
      *
-     * @return  &rdbms.DBIndex an index
-     * @see     getNextIndex
+     * @return  rdbms.DBIndex an index
      */
     public function getFirstIndex() {
       reset($this->indexes);
@@ -114,8 +111,7 @@
     /**
      * Get next index - Iterator function
      *
-     * @return  &rdbms.DBIndex an index or FALSE to indicate there are none left
-     * @see     getNextIndex
+     * @return  rdbms.DBIndex an index or FALSE to indicate there are none left
      */
     public function getNextIndex() {
       return next($this->indexes);
@@ -129,9 +125,7 @@
      */
     public function hasAttribute($name) {
       for ($i= 0, $m= sizeof($this->attributes); $i < $m; $i++) {
-        if ($name == $this->attributes[$i]->name) {
-          return TRUE;
-        }
+        if ($name == $this->attributes[$i]->name) return TRUE;
       }
       return FALSE;
     }
