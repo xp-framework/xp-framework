279650a839d6f7a452f471e0bd8b2041b5c421cc
thekid <timm.friebe@gmail.com>
2007-01-10 17:19:07 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@9228 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/text/doclet/markup/CopyProcessor.class.php
@@ -0,0 +1,55 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('text.doclet.markup.MarkupProcessor');
+
+  /**
+   * Processes <pre> ... </pre>
+   *
+   * @purpose  Processor
+   */
+  class CopyProcessor extends MarkupProcessor {
+    public
+      $patterns= array(
+        '#\r#',
+        '#\n#',
+        '#&(?![a-z0-9\#]+;)#',
+      ),
+      $replacements= array(
+        '',
+        '<br/>',
+        '&amp;', 
+      );
+
+    /**
+     * Initializes the processor.
+     *
+     * @return  string
+     */
+    public function initialize() {
+      return '<pre>';
+    }
+
+    /**
+     * Finalizes the processor.
+     *
+     * @return  string
+     */    
+    public function finalize() {
+      return '</pre>';
+    }
+
+    /**
+     * Process
+     *
+     * @param   string token
+     * @return  string
+     */
+    public function process($token) {
+      return preg_replace($this->patterns, $this->replacements, $token);
+    }
+  }
+?>
---
+++ skeleton/text/doclet/markup/MarkupProcessor.class.php
@@ -0,0 +1,46 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  /**
+   * Markup processor
+   *
+   * @see      xp://text.doclet.markup.MarkupBuilder
+   * @purpose  Base class
+   */
+  class MarkupProcessor extends Object {
+    
+    /**
+     * Initializes the processor. Returns an empty string in this default
+     * implementation.
+     *
+     * @return  string
+     */
+    public function initialize() {
+      return '';
+    }
+    
+    /**
+     * Process a token. Returns an empty string in this default
+     * implementation.
+     *
+     * @param   string token
+     * @return  string
+     */
+    public function process($token) {
+      return '';
+    }
+    
+    /**
+     * Finalizes the processor. Returns an empty string in this default
+     * implementation.
+     *
+     * @return  string
+     */
+    public function finalize() {
+      return '';
+    }
+  }
+?>
