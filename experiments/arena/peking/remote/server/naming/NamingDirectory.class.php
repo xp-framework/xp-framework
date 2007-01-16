37aff068ef5eb699fcd5c9a3ae920a1e8295cc9b
kiesel <alex@kiesel.name>
2007-01-16 12:11:21 +0000

    - make members protected and rename them
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@9298 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/peking/remote/server/naming/NamingDirectory.class.php
+++ experiments/arena/peking/remote/server/naming/NamingDirectory.class.php
@@ -18,9 +18,9 @@
    * @purpose  purpose
    */
   class NamingDirectory extends Object {
-    public
-      $_map   = NULL,
-      $_cat   = NULL;
+    protected
+      $map   = NULL,
+      $cat   = NULL;
     
     protected static
       $instance = NULL;
@@ -31,11 +31,10 @@
      * @param   
      * @return  
      */
-    public function __construct() {
-      $this->_map= new Hashmap();
+    protected function __construct() {
+      $this->map= new Hashmap();
       
-      $log= Logger::getInstance();
-      $this->_cat= $log->getCategory($this->getClassName());
+      $this->cat= Logger::getInstance()->getCategory($this->getClassName());
     }
     
     static function __static() {
@@ -59,8 +58,8 @@
      * @return  
      */
     public function bind($name, $object) {
-      $this->_cat->info($this->getClassName(), 'binding new naming entry', $name);
-      $this->_map->putref($name, $object);
+      $this->cat->info($this->getClassName(), 'binding new naming entry', $name);
+      $this->map->putref($name, $object);
     }
     
     /**
@@ -70,12 +69,12 @@
      * @return  
      */
     public function lookup($name) {
-      if (!$this->_map->containsKey($name)) throw(new NameNotFoundException(
+      if (!$this->map->containsKey($name)) throw new NameNotFoundException(
         $name.' not bound.'
-      ));
+      );
       
-      $this->_cat->debug($this->getClassName(), 'looked up naming entry', $name);
-      return $this->_map->get($name);
+      $this->cat->debug($this->getClassName(), 'looked up naming entry', $name);
+      return $this->map->get($name);
     }
   }
 ?>
