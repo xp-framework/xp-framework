d562a5d43ac653b79d4e765cc055c4c10bc8cb56
thekid <timm.friebe@gmail.com>
2007-01-25 13:49:53 +0000

    - Transfer exception cause # Implements RFC #0076 # It was forgotten to add that here because the representationOf() part # is only used in EASC server implementations - see RFC #0069 for details
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@9366 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/remote/protocol/ExceptionMapping.class.php
+++ skeleton/remote/protocol/ExceptionMapping.class.php
@@ -4,7 +4,10 @@
  * $Id$ 
  */
 
-  uses('remote.protocol.SerializerMapping');
+  uses(
+    'remote.protocol.SerializerMapping',
+    'util.ChainedException'
+  );
 
   /**
    * Mapping for lang.Throwable
@@ -61,23 +64,28 @@
       if (FALSE !== ($token= array_search($value->getClassName(), $serializer->exceptions, TRUE))) {
       
         // It's a known exception
-        $s= 'e:'.strlen($token).':"'.$token.'":2:{';
+        $s= 'e:'.strlen($token).':"'.$token.'":3:{';
       } else {
       
         // Generic exceptions
-        $s= 'E:'.strlen($value->getClassName()).':"'.$value->getClassName().'":2:{';
+        $s= 'E:'.strlen($value->getClassName()).':"'.$value->getClassName().'":3:{';
       }
       $s.= 's:7:"message";';
       $s.= $serializer->representationOf($value->getMessage());
       
       $s.= 's:5:"trace";a:'.sizeof($trace).':{';
-      
       $i= 0;
       foreach ($trace as $element) {
         $s.= 'i:'.$i++.';'.$serializer->representationOf($element, $context);
       }
       
-      return $s.'}}';
+      // Transfer cause
+      $s.= '}s:5:"cause";'.(($value instanceof ChainedException) 
+        ? $serializer->representationOf($value->getCause(), $context)
+        : 'N;'
+      );
+      
+      return $s.'}';
     }
     
     /**
