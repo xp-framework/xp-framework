7b32a7d58eb4636bf20fb6519a4bb22cab713814
thekid <timm.friebe@gmail.com>
2007-02-25 14:58:54 +0000

    - Simplify example # Makes use of argument passing
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@9483 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/people/friebe/c/php-threads/threads.php
+++ experiments/people/friebe/c/php-threads/threads.php
@@ -1,45 +1,23 @@
 <?php
-  function run0() {
-    echo "### THREAD-1: Hello###\n";
+  function run($name) {
+    echo "### THREAD-", $name, ": Hello###\n";
     for ($i= 0; $i < 5; $i++) {        
-      printf("    1: %d\n", $i);        
+      printf("    %s: %d\n", $name, $i);        
       usleep(rand(1, 4) * 100000);      
     }
-    printf("### THREAD-1: DONE ###\n");    
+    printf("### THREAD-%d: DONE ###\n", $name);    
   }
-
-  function run1() {
-    echo "### THREAD-2: Hello###\n";
-    for ($i= 0; $i < 5; $i++) {        
-      printf("    2: %d\n", $i);        
-      usleep(rand(1, 4) * 100000);      
-    }
-    printf("### THREAD-2: DONE ###\n");    
-  }
-
-  function run2() {
-    echo "### THREAD-3: Hello###\n";
-    for ($i= 0; $i < 5; $i++) {        
-      printf("    3: %d\n", $i);        
-      usleep(rand(1, 4) * 100000);      
-    }
-    printf("### THREAD-3: DONE ###\n");    
-  }
-  
+ 
   $t= thread_new('#1');
   var_dump($t);
-  $s= thread_start($t, 'run0');
-  var_dump($s);
-
   $r= thread_new('#2');
   var_dump($r);
-  $q= thread_start($r, 'run1');
-  var_dump($q);
-
   $x= thread_new('#3');
   var_dump($x);
-  $y= thread_start($x, 'run2');
-  var_dump($y);
+
+  thread_start($t, 'run', array('NumberOne'));
+  thread_start($r, 'run', array('NumberTwo'));
+  thread_start($x, 'run', array('NumberThree'));
   
   var_dump(thread_join($t));
   var_dump(thread_join($r));
