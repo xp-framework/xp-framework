8782940beb0296bf9f94685ee17e7d31ab54ff37
Christian Gellweiler <gelli@developer.xp-framework.net>
2007-02-28 16:01:40 +0000

    - Initial revision
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@9526 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/classes/net/xp_framework/unittest/text/parser/CSVParserTest.class.php
@@ -0,0 +1,62 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses(
+    'unittest.TestCase',
+    'text.parser.CSVParser'
+  );
+
+  /**
+   * Test string tokenizing.
+   *
+   * @see      xp://text.parser.CSVParser
+   * @purpose  TestCase
+   */
+  class CSVParserTest extends TestCase {
+  
+    /**
+     * Setup
+     *
+     */
+    public function setup() {
+      $this->stream= new Stream();
+      $this->parser= new CSVParser();
+      $this->parser->setInputStream($this->stream);
+    }
+
+    /**
+     * Test guess of delimiters
+     *
+     */
+    #[@test]
+    function testGuessDelimiter() {
+      $this->stream->write('a,b,c,d,e,f');
+      $this->stream->rewind();
+      
+      $this->assertEquals(',', $this->parser->guessDelimiter());
+    }
+
+    /**
+     * Test record with escape character
+     *
+     */
+    #[@test]
+    public function testRecordWithEscapes() {
+      $this->stream->write('"UTDI.DE",13.56,-0.63,"9:16am","2/28/2007",2274004');
+      $this->stream->rewind();
+      $this->parser->setColDelimiter(',');
+      
+      with ($record= $this->parser->getNextRecord()); {
+        $this->assertEquals('UTDI.DE', array_shift($record));
+        $this->assertEquals('13.56', array_shift($record));
+        $this->assertEquals('-0.63', array_shift($record));
+        $this->assertEquals('9:16am', array_shift($record));
+        $this->assertEquals('2/28/2007', array_shift($record));
+        $this->assertEquals('2274004', array_shift($record));
+      }
+    }
+  }
+?>
