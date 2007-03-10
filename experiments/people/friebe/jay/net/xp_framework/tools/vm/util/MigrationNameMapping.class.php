d0f98ea387fa3f4e8bdd947ca6cfa8a55cbc2f28
thekid <timm.friebe@gmail.com>
2007-03-10 19:32:38 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@9611 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/people/friebe/jay/net/xp_framework/tools/vm/util/MigrationNameMapping.class.php
@@ -0,0 +1,28 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('net.xp_framework.tools.vm.util.NameMapping');
+
+  /**
+   * Maps names and types plus supports PHP builtin classes
+   *
+   * @see      xp://net.xp_framework.tools.vm.util.NameMapping
+   * @purpose  Utility
+   */
+  class MigrationNameMapping extends NameMapping {
+
+    /**
+     * Constructor. Registers all builtin classes in mapping.
+     *
+     */
+    function __construct() {
+      foreach (get_declared_classes() as $name) {
+        $r= new ReflectionClass($name);
+        if ($r->isInternal()) $this->mapping[$name]= 'php.'.$name;
+      }
+    }
+  }
+?>
