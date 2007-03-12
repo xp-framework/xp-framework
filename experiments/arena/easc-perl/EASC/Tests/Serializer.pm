bc3bdc983281fe1c701ab3fa4dcab418ebad04b1
thekid <timm.friebe@gmail.com>
2007-03-12 21:29:10 +0000

    - Add valueOf() tests
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@9636 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/easc-perl/EASC/Tests/Serializer.pm
+++ experiments/arena/easc-perl/EASC/Tests/Serializer.pm
@@ -12,28 +12,48 @@ use EASC::Protocol::Datatypes;
 
 use base qw(Test::TestCase);
 
-sub testString {
+sub testRepresentationOfString {
     my $self= shift;
     $self->assertEquals('s:5:"Hello";', EASC::Protocol::Serializer::representationOf('Hello'));
 }
 
-sub testUmlauts {
+sub testValueOfString {
+    my $self= shift;
+    $self->assertEquals('Hello', EASC::Protocol::Serializer::valueOf('s:5:"Hello";'));
+}
+
+sub testRepresentationOfUmlauts {
     my $self= shift;
     $self->assertEquals('s:5:"ÄÖÜßé";', EASC::Protocol::Serializer::representationOf('ÄÖÜßé'));
 }
 
-sub testTrue {
+sub testValueOfUmlauts {
+    my $self= shift;
+    $self->assertEquals('ÄÖÜßé', EASC::Protocol::Serializer::valueOf('s:5:"ÄÖÜßé";'));
+}
+
+sub testRepresentationOfTrue {
     my $self= shift;
     $self->assertEquals('b:1;', EASC::Protocol::Serializer::representationOf(Bool::->new(1)));
     $self->assertEquals('b:1;', EASC::Protocol::Serializer::representationOf(Bool::->new(2)));
 }
 
-sub testFalse {
+sub testValueOfTrue {
+    my $self= shift;
+    $self->assertEquals(1, EASC::Protocol::Serializer::valueOf('b:1;')->value());
+}
+
+sub testRepresentationOfFalse {
     my $self= shift;
     $self->assertEquals('b:0;', EASC::Protocol::Serializer::representationOf(Bool::->new(0)));
 }
 
-sub testIntegers {
+sub testValueOfFalse {
+    my $self= shift;
+    $self->assertEquals(0, EASC::Protocol::Serializer::valueOf('b:0;')->value());
+}
+
+sub testRepresentationOfIntegers {
     my $self= shift;
 
     foreach my $int (0, -1, 1, 65536) {
@@ -41,11 +61,27 @@ sub testIntegers {
     }
 }
 
-sub testLongs {
+sub testValueOfIntegers {
+    my $self= shift;
+
+    foreach my $int (0, -1, 1, 65536) {
+        $self->assertEquals($int, EASC::Protocol::Serializer::valueOf('i:'.$int.';')->value());
+    }
+}
+
+sub testRepresentationOfLongs {
     my $self= shift;
 
     foreach my $long (-1, 0, 1, 65536) {
         $self->assertEquals('l:'.$long.';', EASC::Protocol::Serializer::representationOf(Long::->new($long)));
+    }
+}
+
+sub testValueOfLongs {
+    my $self= shift;
+
+    foreach my $long (0, -1, 1, 65536) {
+        $self->assertEquals($long, EASC::Protocol::Serializer::valueOf('l:'.$long.';')->value());
     }
 }
 
