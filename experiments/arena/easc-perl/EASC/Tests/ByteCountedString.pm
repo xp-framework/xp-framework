d6b59a55fb5ee5305054963f7e978619c6a92655
thekid <timm.friebe@gmail.com>
2007-03-12 21:10:21 +0000

    - Add tests for sendTo() and readFrom() methods
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@9634 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/easc-perl/EASC/Tests/ByteCountedString.pm
+++ experiments/arena/easc-perl/EASC/Tests/ByteCountedString.pm
@@ -8,6 +8,7 @@ package EASC::Tests::ByteCountedString;
 
 use Test::TestCase;
 use EASC::Protocol::ByteCountedString;
+use EASC::Tests::MockSocket;
 
 use base qw(Test::TestCase);
 
@@ -25,6 +26,37 @@ sub testUmlautString {
     $bcs= EASC::Protocol::ByteCountedString::->new('Hallöle');
     $self->assertEquals('HallÃ¶le', $bcs->{string});
     $self->assertEquals(11, $bcs->slength);
+}
+
+sub testSending {
+    my $self= shift;
+
+    $bcs= EASC::Protocol::ByteCountedString::->new('Hallöle');
+    $sock= EASC::Tests::MockSocket::->new();
+
+    $bcs->writeTo($sock);
+    $self->assertEquals("\x00\x08\x00HallÃ¶le", $sock->{bytes});
+}
+
+sub testReading {
+    my $self= shift;
+
+    $sock= EASC::Tests::MockSocket::->new("\x00\x08\x00HallÃ¶le");
+
+    $bcs= EASC::Protocol::ByteCountedString::readFrom($sock);
+    $self->assertEquals('Hallöle', $bcs);
+}
+
+sub testReadingTwoChunks {
+    my $self= shift;
+
+    $sock= EASC::Tests::MockSocket::->new(
+      "\x00\x08\x01HallÃ¶le".
+      "\x00\x05\x00 Moto"
+    );
+
+    $bcs= EASC::Protocol::ByteCountedString::readFrom($sock);
+    $self->assertEquals('Hallöle Moto', $bcs);
 }
 
 1;
