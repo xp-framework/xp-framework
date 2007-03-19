4ee2cb5680d8136c8ec378fb7ae8aa6d711cf36f
Ruben Wagner <ruben@developer.xp-framework.net>
2007-03-19 11:58:51 +0000

    - add: configurable naming for generated methods to access referenced entity database objects # a naming strategy has to implement the abstract class DBXMLNamingStrategy or an illegal argument exeption will be thrown
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@9719 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/people/wagner/dbConstraints/rdbms/util/DBXMLNamingStrategyDefault.class.php
@@ -0,0 +1,42 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+uses('rdbms.util.DBXMLNamingStrategy');
+
+  /**
+   * Generate Names for database generated classes
+   *
+   */
+  class DBXMLNamingStrategyDefault extends DBXMLNamingStrategy {
+    
+    /**
+     * assemble th name of a foreign key constraint
+     *
+     * @param   rdbms.DBTable t referencing table
+     * @param   rdbms.DBConstraint c
+     * @return  string
+     */
+    function foreignKeyConstraintName($t, $c) {
+      $role= '';
+      foreach (array_keys($c->getKeys()) as $attribute) $role.= ucfirst('_id' == substr($attribute, -3) ? substr($attribute, 0, -3) : $attribute);
+      return $role;
+    }
+
+    /**
+     * assemble the name of a referencing foreign Key constraint
+     * (current entity at the tip)
+     *
+     * @param   rdbms.DBTable t referencing table
+     * @param   rdbms.DBConstraint c
+     * @return  string
+     */
+    function referencingForeignKeyConstraintName($t, $c) {
+      $role= self::foreignKeyConstraintName($t, $c);
+      return trim(((ucfirst($t->name) == $role) ? $role : ucfirst($t->name).$role));
+    }
+
+  }
+?>
