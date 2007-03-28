147c9cbe79c418fa9abfba9f6ceab59422e8952d
Ruben Wagner <ruben@developer.xp-framework.net>
2007-03-28 08:54:05 +0000

    - add: doku and some tests
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@9775 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/people/wagner/dbProjection/dbProjectiontest.php
+++ experiments/people/wagner/dbProjection/dbProjectiontest.php
@@ -27,13 +27,42 @@
   Logger::getInstance()->getCategory()->addAppender(new ColoredConsoleAppender());
   ConnectionManager::getInstance()->register(DriverManager::getConnection('mysql://test:test@localhost/?autoconnect=1&log=default'));
   
-  $crit= new Criteria();
-  $crit->setProjection(
-    Projections::projectionList()
-      ->add(Projections::average("person_id"))
+  $crits['average']= new Criteria();
+  $crits['average']->setProjection(
+    Projections::average("person_id")
   );
   
-  $l= RubentestPerson::getPeer()->doSelect($crit);
-  var_dump($l);
+  $crits['max']= new Criteria();
+  $crits['max']->setProjection(
+    Projections::max("person_id")
+  );
+  
+  $crits['min']= new Criteria();
+  $crits['min']->setProjection(
+    Projections::min("person_id")
+  );
+  
+  $crits['sum']= new Criteria();
+  $crits['sum']->setProjection(
+    Projections::sum("person_id")
+  );
+  
+  $crits['property']= new Criteria();
+  $crits['property']->setProjection(
+    Projections::property("name")
+  );
+  
+  $crits['projectionList']= new Criteria();
+  $crits['projectionList']->setProjection(
+    Projections::projectionList()
+    ->add(Projections::property("person_id"))
+    ->add(Projections::property("name"))
+  );
+
+  foreach ($crits as $name => $crit) {
+    echo "\n$name:\n";
+    $l= RubentestPerson::getPeer()->doSelect($crit);
+    var_dump($l);
+  }
 
 ?>
