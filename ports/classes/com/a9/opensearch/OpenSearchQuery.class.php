f0627512f46efa5958542147c0f690ceb6ba488c
thekid <timm.friebe@gmail.com>
2007-03-29 10:06:32 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@9797 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/classes/com/a9/opensearch/OpenSearchQuery.class.php
@@ -0,0 +1,70 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Represents an open search Query
+   *
+   * @see      xp://com.a9.opensearch.OpenSearchDescription
+   * @purpose  purpose
+   */
+  #[@xmlns(s= 'http://a9.com/-/spec/opensearch/1.1/')]
+  class OpenSearchQuery extends Object {
+    protected
+      $role        = NULL,
+      $searchTerms = NULL;
+
+    /**
+     * Constructor
+     *
+     * @param   string role default ''
+     * @param   string searchTerms default ''
+     */
+    public function __construct($role= '', $searchTerms= '') {
+      $this->role= $role;
+      $this->searchTerms= $searchTerms;
+    }
+
+    /**
+     * Set role
+     *
+     * @param   string role
+     */
+    #[@xmlmapping(element= '@role')]
+    public function setRole($role) {
+      $this->role= $role;
+    }
+
+    /**
+     * Get role
+     *
+     * @return  string
+     */
+    #[@xmlfactory(element= '@role')]
+    public function getRole() {
+      return $this->role;
+    }
+
+    /**
+     * Set searchTerms
+     *
+     * @param   string searchTerms
+     */
+    #[@xmlmapping(element= '@searchTerms')]
+    public function setSearchTerms($searchTerms) {
+      $this->searchTerms= $searchTerms;
+    }
+
+    /**
+     * Get searchTerms
+     *
+     * @return  string
+     */
+    #[@xmlfactory(element= '@searchTerms')]
+    public function getSearchTerms() {
+      return $this->searchTerms;
+    }
+  }
+?>
