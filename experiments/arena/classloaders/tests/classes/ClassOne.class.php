88ac6c4a30b4bb038192271612bbcfcb6f7dd8d3
thekid <timm.friebe@gmail.com>
2007-04-04 15:09:34 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@9853 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/arena/classloaders/tests/classes/ClassOne.class.php
@@ -0,0 +1,17 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * (Insert class' description here)
+   *
+   * @ext      extension
+   * @see      reference
+   * @purpose  purpose
+   */
+  class ClassOne extends Object {
+  
+  }
+?>
