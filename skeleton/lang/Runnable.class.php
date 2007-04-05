d77e60a93d9492c970124cd7a9d6ab6756dba346
thekid <timm.friebe@gmail.com>
2007-04-05 14:53:30 +0000

    - Initial release # Implements RFC #0081
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@9892 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/lang/Runnable.class.php
@@ -0,0 +1,22 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Denotes instances of implementing classes are runnable by 
+   * invoking the run() method.
+   *
+   * @purpose  Interface
+   */
+  interface Runnable {
+  
+    /**
+     * Runs this object
+     *
+     */
+    public function run();
+  
+  }
+?>
