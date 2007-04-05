66d8c92ef32d4065068d7b362e5beb85cd364985
thekid <timm.friebe@gmail.com>
2007-04-05 11:49:31 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@9880 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/arena/classloaders/lang/IClassLoader.class.php
@@ -0,0 +1,31 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * (Insert class' description here)
+   *
+   * @purpose  purpose
+   */
+  interface IClassLoader {
+
+    /**
+     * Loads a class
+     *
+     * @param   string class fully qualified class name
+     * @return  string class name of class loaded
+     */
+    public function load($class);
+    
+    /**
+     * Load the class by the specified name
+     *
+     * @param   string class fully qualified class name io.File
+     * @return  lang.XPClass
+     * @throws  lang.ClassNotFoundException in case the class can not be found
+     */
+    public function loadClass($class);
+  }
+?>
