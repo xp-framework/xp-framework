39d71606f6b5be6388c93a1fdce4a83b672182ea
Ruben Wagner <ruben@developer.xp-framework.net>
2007-04-10 15:36:42 +0000

    - add: free restrictions
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@9951 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/people/wagner/merge/dbFreeExpTest.php
@@ -0,0 +1,38 @@
+<?php
+/* This file is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  require('lang.base.php');
+  xp::sapi('cli');
+
+  uses(    
+    'util.log.Logger',
+    'util.log.ColoredConsoleAppender',
+    'util.PropertyManager',
+    'xml.Node',
+    'rdbms.ConnectionManager',
+    'rdbms.criterion.Restrictions',
+    'de.schlund.db.rubentest.RubentestColor'
+  );
+
+  // Params
+  $p= new ParamString();
+  
+  // Display help
+  if ($p->exists('help', 'h')) {
+    Console::writeLinef('...');
+    exit();
+  }
+  
+  Logger::getInstance()->getCategory()->addAppender(new ColoredConsoleAppender());
+  ConnectionManager::getInstance()->register(DriverManager::getConnection('mysql://test:test@localhost/?autoconnect=1&log=default'));
+  
+  $c1= RubentestColor::getPeer()->iteratorFor(new Criteria(
+    Restrictions::free('name = "lightgreen"')
+  ));
+  
+  var_dump($c1->next());
+
+?>
