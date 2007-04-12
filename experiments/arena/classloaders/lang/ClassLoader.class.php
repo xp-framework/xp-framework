4ad46af774d5f41d690dbbe01c0bdceab9a81c09
thekid <timm.friebe@gmail.com>
2007-04-12 17:06:30 +0000

    - Move defineClass() to lang.ClassLoader - Provide findClass in lang.ClassLoader # BC break in findClass - returns the loader delegate and no longer # the URL
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@9970 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/classloaders/lang/ClassLoader.class.php
+++ experiments/arena/classloaders/lang/ClassLoader.class.php
@@ -74,6 +74,19 @@
         xp::stringOf(self::$delegates)
       ));
     }
+
+    /**
+     * Load the class by the specified name
+     *
+     * @param   string class fully qualified class name io.File
+     * @return  lang.IClassLoader the classloader that provides this class
+     */
+    public function findClass($class) {
+      foreach (self::$delegates as $delegate) {
+        if ($delegate->providesClass($class)) return $delegate;
+      }
+      return NULL;
+    }    
     
     /**
      * Load the class by the specified name
@@ -85,5 +98,82 @@
     public function loadClass($class) {
       return new XPClass($this->load($class));
     }    
+
+    /**
+     * Define a class with a given name
+     *
+     * @param   string class fully qualified class name
+     * @param   string bytes sourcecode of the class
+     * @return  lang.XPClass
+     * @throws  lang.FormatException in case the class cannot be defined
+     */
+    protected function _defineClassFromBytes($class, $bytes) {
+      $name= xp::reflect($class);
+
+      if (!class_exists($name) && !interface_exists($name)) {
+        
+        // Load InlineLoader
+        XPClass::forName('lang.InlineLoader');
+        InlineLoader::setClassBytes($class, $bytes);
+        if (FALSE === include('inline://'.$class)) {
+          throw new FormatException('Cannot define class "'.$class.'"');
+        }
+        InlineLoader::removeClassBytes($class);
+        
+        if (!class_exists($name) && !interface_exists($name)) {
+          throw new FormatException('Class "'.$class.'" not defined');
+        }
+        xp::$registry['class.'.$name]= $class;
+        xp::$registry['classloader.'.$class]= __CLASS__.'://'.$this->path;
+        is_callable(array($name, '__static')) && call_user_func(array($name, '__static'));
+      }      
+
+      return new XPClass($name);
+    }
+    
+    /**
+     * Define a class with a given name
+     *
+     * @param   string class fully qualified class name
+     * @param   string parent either sourcecode of the class or FQCN of parent
+     * @param   string[] interfaces default NULL FQCNs of implemented interfaces
+     * @param   string bytes default NULL inner sourcecode of class (containing {}) 
+     * @return  lang.XPClass
+     * @throws  lang.FormatException in case the class cannot be defined
+     * @throws  lang.ClassNotFoundException if given parent class does not exist
+     */
+    public function defineClass($class, $parent, $interfaces= NULL, $bytes= NULL) {
+      
+      // If invoked with less than four arguments, old behaviour will be executed
+      if (NULL === $bytes) {
+        return $this->_defineClassFromBytes($this->path.$class, $parent);
+      }
+      
+      $name= xp::reflect($class);
+      if (!class_exists($name)) {
+        $qname= $this->path.$class;
+        $parentName= xp::reflect($parent);
+        
+        if (!class_exists($parentName)) {
+          throw(new ClassNotFoundException('Parent class '.$parent.' does not exist.'));
+        }
+        
+        $newBytes= 'class '.$name.' extends '.$parentName;
+        if (sizeof($interfaces)) {
+          $newBytes.= ' implements ';
+
+          $ifaces= array();
+          foreach ($interfaces as $i) { $ifaces[]= xp::reflect($i); }
+          
+          $newBytes.= implode(', ', $ifaces);
+        }
+        
+        $newBytes.= ' '.$bytes;
+        
+        return $this->_defineClassFromBytes($qname, $newBytes);
+      }
+      
+      return new XPClass($name);
+    }
   }
 ?>
