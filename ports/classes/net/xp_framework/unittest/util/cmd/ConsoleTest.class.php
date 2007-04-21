1243e3db2ee52254d4c080459d6ff9ed409ca0ff
thekid <timm.friebe@gmail.com>
2007-04-21 18:58:42 +0000

    - Initial release # Part of RFC #0120
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@10072 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/classes/net/xp_framework/unittest/util/cmd/ConsoleTest.class.php
@@ -0,0 +1,165 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses(
+    'util.cmd.Console',
+    'io.streams.StringWriter', 
+    'io.streams.MemoryOutputStream',
+    'unittest.TestCase'
+  );
+
+  /**
+   * TestCase
+   *
+   * @see      xp://util.cmd.Console
+   * @purpose  purpose
+   */
+  class ConsoleTest extends TestCase {
+    protected
+      $original = array(),
+      $streams  = array();
+
+    /**
+     * Sets up test case. Redirects console standard output/error streams
+     * to memory streams
+     *
+     */
+    public function setUp() {
+      $this->original= array(clone Console::$out, clone Console::$err);
+      $this->streams= array(new MemoryOutputStream(), new MemoryOutputStream());
+      Console::$out= new StringWriter($this->streams[0]);
+      Console::$err= new StringWriter($this->streams[1]);
+    }
+    
+    /**
+     * Tear down testcase. Restores original standard output/error streams
+     *
+     */
+    public function tearDown() {
+      Console::$out= $this->original[0];
+      Console::$err= $this->original[1];
+    }
+    
+    /**
+     * Test write() method
+     *
+     */
+    #[@test]
+    public function write() {
+      Console::write('.');
+      $this->assertEquals('.', $this->streams[0]->getBytes());
+    }
+
+    /**
+     * Test write() method
+     *
+     */
+    #[@test]
+    public function writeToOut() {
+      Console::$out->write('.');
+      $this->assertEquals('.', $this->streams[0]->getBytes());
+    }
+
+    /**
+     * Test write() method
+     *
+     */
+    #[@test]
+    public function writeToErr() {
+      Console::$err->write('.');
+      $this->assertEquals('.', $this->streams[1]->getBytes());
+    }
+
+    /**
+     * Test writef() method
+     *
+     */
+    #[@test]
+    public function writef() {
+      Console::writef('Hello "%s"', 'Timm');
+      $this->assertEquals('Hello "Timm"', $this->streams[0]->getBytes());
+    }
+
+    /**
+     * Test writef() method
+     *
+     */
+    #[@test]
+    public function writefToOut() {
+      Console::$out->writef('Hello "%s"', 'Timm');
+      $this->assertEquals('Hello "Timm"', $this->streams[0]->getBytes());
+    }
+
+    /**
+     * Test writef() method
+     *
+     */
+    #[@test]
+    public function writefToErr() {
+      Console::$err->writef('Hello "%s"', 'Timm');
+      $this->assertEquals('Hello "Timm"', $this->streams[1]->getBytes());
+    }
+
+    /**
+     * Test writeLine() method
+     *
+     */
+    #[@test]
+    public function writeLine() {
+      Console::writeLine('.');
+      $this->assertEquals(".\n", $this->streams[0]->getBytes());
+    }
+
+    /**
+     * Test writeLine() method
+     *
+     */
+    #[@test]
+    public function writeLineToOut() {
+      Console::$out->writeLine('.');
+      $this->assertEquals(".\n", $this->streams[0]->getBytes());
+    }
+
+    /**
+     * Test writeLine() method
+     *
+     */
+    #[@test]
+    public function writeLineToErr() {
+      Console::$err->writeLine('.');
+      $this->assertEquals(".\n", $this->streams[1]->getBytes());
+    }
+
+    /**
+     * Test writeLine() method
+     *
+     */
+    #[@test]
+    public function writeLinef() {
+      Console::writeLinef('Hello %s', 'World');
+      $this->assertEquals("Hello World\n", $this->streams[0]->getBytes());
+    }
+
+    /**
+     * Test writeLine() method
+     *
+     */
+    #[@test]
+    public function writeLinefToOut() {
+      Console::$out->writeLinef('Hello %s', 'World');
+      $this->assertEquals("Hello World\n", $this->streams[0]->getBytes());
+    }
+    /**
+     * Test writeLine() method
+     *
+     */
+    #[@test]
+    public function writeLinefToErr() {
+      Console::$err->writeLinef('Hello %s', 'World');
+      $this->assertEquals("Hello World\n", $this->streams[1]->getBytes());
+    }
+  }
+?>
