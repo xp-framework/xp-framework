02c842e778e6a1f6972fc0f621a9870fad835f3a
thekid <timm.friebe@gmail.com>
2007-04-27 12:59:53 +0000

    - Move runtime class definition tests to tests.RuntimeClassDefinitionTest
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@10143 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/classloaders/tests/ClassLoaderTest.class.php
+++ experiments/arena/classloaders/tests/ClassLoaderTest.class.php
@@ -4,7 +4,7 @@
  * $Id$ 
  */
 
-  uses('unittest.TestCase', 'util.log.Traceable');
+  uses('unittest.TestCase');
 
   /**
    * TestCase
@@ -24,20 +24,6 @@
     protected function assertXPClass($name, $class) {
       $this->assertClass($class, 'lang.XPClass');
       $this->assertEquals($name, $class->getName());
-    }
-
-    /**
-     * Helper method
-     *
-     * @param   string name
-     * @param   lang.XPClass class
-     * @throws  unittest.AssertionFailedError
-     */
-    protected function defineClass($name, $parent, $interfaces, $bytes) {
-      if (class_exists(xp::reflect($name))) {
-        $this->fail('Class "'.$name.'" may not exist!');
-      }
-      return ClassLoader::getDefault()->defineClass($name, $parent, $interfaces, $bytes);
     }
 
     /**
@@ -141,50 +127,6 @@
     #[@test, @expect('lang.ClassNotFoundException')]
     public function loadNonExistantClass() {
       ClassLoader::getDefault()->loadClass('@@NON-EXISTANT@@');
-    }
-
-    /**
-     * Test defineClass() method with the new signature
-     *
-     */
-    #[@test]
-    public function defineRuntimeClass() {
-      $name= 'net.xp_framework.unittest.reflection.RuntimeDefinedClass';
-      $class= $this->defineClass($name, 'lang.Object', NULL, '{
-        public static $initializerCalled= FALSE;
-        
-        static function __static() { 
-          self::$initializerCalled= TRUE; 
-        }
-      }');
-      $this->assertXPClass($name, $class);
-      $this->assertTrue(RuntimeDefinedClass::$initializerCalled);
-      $this->assertClass($class->getClassLoader(), 'lang.DynamicClassLoader');
-    }
-    
-    /**
-     * Tests defineClass() with a given interface
-     *
-     */
-    #[@test]
-    public function defineRuntimeClassImplements() {
-      $name= 'net.xp_framework.unittest.reflection.RuntimeDefinedClassWithInterface';
-      $class= $this->defineClass($name, 'lang.Object', array('util.log.Traceable'), '{
-        public function setTrace($cat) { } 
-      }');
-
-      $this->assertTrue($class->isSubclassOf('util.log.Traceable'));
-      $this->assertClass($class->getClassLoader(), 'lang.DynamicClassLoader');
-    }
-
-    /**
-     * Tests newinstance()
-     *
-     */
-    #[@test]
-    public function newInstance() {
-      $i= newinstance('lang.Object', array(), '{ public function bar() { return TRUE; }}');
-      $this->assertClass($i->getClass()->getClassLoader(), 'lang.DynamicClassLoader');
     }
   }
 ?>
