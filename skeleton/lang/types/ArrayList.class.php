68576803dad3fee91792123120223f3e5ac16695
thekid <timm.friebe@gmail.com>
2007-04-29 17:04:14 +0000

    - Overload array operators - Overload foreach() iteration - Simplify sourcecode in equas() # Part of RFC #0106
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@10161 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/lang/types/ArrayList.class.php
+++ skeleton/lang/types/ArrayList.class.php
@@ -9,17 +9,120 @@
    *
    * @purpose  Wrapper
    */
-  class ArrayList extends Object {
+  class ArrayList extends Object implements ArrayAccess, IteratorAggregate {
+    protected
+      $iterator = NULL;
+
     public
-      $values=  NULL;
+      $values   = NULL,
+      $length   = 0;
+
+    /**
+     * Create a new instance of an ArrayList
+     *
+     * @param   mixed arg either mixed[] => values or an int => size
+     * @return  lang.types.ArrayList
+     */
+    public static function newInstance($arg) {
+      if (is_array($arg)) {
+        $self= new self();
+        $self->values= array_values($arg);
+        $self->length= sizeof($self->values);
+      } else {
+        $self= new self();
+        $self->length= (int)$arg;
+      }
+      return $self;
+    }
+
+    /**
+     * Returns a hashcode for this number
+     *
+     * @return  string
+     */
+    public function hashCode() {
+      return $this->length.'['.serialize($this->values);
+    }
     
     /**
      * Constructor
      *
-     * @param   mixed[] values default array()
+     * @param   mixed* values
      */
-    public function __construct($values= array()) {
-      $this->values= $values;
+    public function __construct() {
+      if (0 != ($this->length= func_num_args())) {
+        $this->values= func_get_args();
+      }
+    }
+    
+    /**
+     * Returns an iterator for use in foreach()
+     *
+     * @see     php://language.oop5.iterations
+     * @return  php.Iterator
+     */
+    public function getIterator() {
+      if (!$this->iterator) $this->iterator= newinstance('Iterator', array($this), '{
+        private $i= 0, $v;
+        public function __construct($v) { $this->v= $v; }
+        public function current() { return $this->v->values[$this->i]; }
+        public function key() { return $this->i; }
+        public function next() { $this->i++; }
+        public function rewind() { $this->i= 0; }
+        public function valid() { return $this->i < $this->v->length; }
+      }');
+      return $this->iterator;
+    }
+
+    /**
+     * = list[] overloading
+     *
+     * @param   int offset
+     * @return  mixed
+     * @throws  lang.IndexOutOfBoundsException if key does not exist
+     */
+    public function offsetGet($offset) {
+      if ($offset >= $this->length || $offset < 0) {
+        raise('lang.IndexOutOfBoundsException', 'Offset '.$offset.' out of bounds');
+      }
+      return $this->values[$offset];
+    }
+
+    /**
+     * list[]= overloading
+     *
+     * @param   int offset
+     * @param   mixed value
+     * @throws  lang.IllegalArgumentException if key is neither numeric (set) nor NULL (add)
+     */
+    public function offsetSet($offset, $value) {
+      if (!is_int($offset)) {
+        throw new IllegalArgumentException('Incorrect type '.$t.' for index');
+      }
+      
+      if ($offset >= $this->length || $offset < 0) {
+        raise('lang.IndexOutOfBoundsException', 'Offset '.$offset.' out of bounds');
+      }
+      $this->values[$offset]= $value;
+    }
+
+    /**
+     * isset() overloading
+     *
+     * @param   int offset
+     * @return  bool
+     */
+    public function offsetExists($offset) {
+      return ($offset >= 0 && $offset < $this->length);
+    }
+
+    /**
+     * unset() overloading
+     *
+     * @param   int offset
+     */
+    public function offsetUnset($offset) {
+      throw new IllegalArgumentException('Cannot remove from immutable list');
     }
     
     /**
@@ -59,7 +162,7 @@
      * @return  bool
      */
     public function equals($cmp) {
-      return is('ArrayList', $cmp) && $this->arrayequals($this->values, $cmp->values);
+      return $cmp instanceof self && $this->arrayequals($this->values, $cmp->values);
     }
     
     /**
