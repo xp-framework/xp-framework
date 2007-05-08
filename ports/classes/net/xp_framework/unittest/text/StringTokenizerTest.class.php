e74d1d4368fd535621ad330c0ade988bbb454452
thekid <timm.friebe@gmail.com>
2007-05-08 21:15:47 +0000

    - Extend new TokenizerTest base class
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@10308 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/classes/net/xp_framework/unittest/text/StringTokenizerTest.class.php
+++ ports/classes/net/xp_framework/unittest/text/StringTokenizerTest.class.php
@@ -5,80 +5,28 @@
  */
 
   uses(
-    'unittest.TestCase',
+    'net.xp_framework.unittest.text.TokenizerTest',
     'text.StringTokenizer'
   );
 
   /**
    * Test string tokenizing.
    *
-   * @see      xp://lang.types.StringTokenizer
+   * @see      xp://text.StringTokenizer
    * @purpose  TestCase
    */
-  class StringTokenizerTest extends TestCase {
+  class StringTokenizerTest extends TokenizerTest {
   
     /**
-     * Test string tokenizing
+     * Retrieve a tokenizer instance
      *
+     * @param   string source
+     * @param   string delimiters default ' '
+     * @param   bool returnDelims default FALSE
+     * @return  text.Tokenizer
      */
-    #[@test]
-    public function testSimpleString() {
-      $t= new StringTokenizer("Hello World!\nThis is an example", " \n");
-      $this->assertEquals('Hello', $t->nextToken());
-      $this->assertEquals('World!', $t->nextToken());
-      $this->assertEquals('This', $t->nextToken());
-      $this->assertEquals('is', $t->nextToken());
-      $this->assertEquals('an', $t->nextToken());
-      $this->assertEquals('example', $t->nextToken());
-      $this->assertFalse($t->hasMoreTokens());
-    }
-
-    /**
-     * Test string tokenizing
-     *
-     */
-    #[@test]
-    public function testSimpleStringWithDelims() {
-      $t= new StringTokenizer("Hello World!\nThis is an example", " \n", TRUE);
-      $this->assertEquals('Hello', $t->nextToken());
-      $this->assertEquals(' ', $t->nextToken());
-      $this->assertEquals('World!', $t->nextToken());
-      $this->assertEquals("\n", $t->nextToken());
-      $this->assertEquals('This', $t->nextToken());
-      $this->assertEquals(' ', $t->nextToken());
-      $this->assertEquals('is', $t->nextToken());
-      $this->assertEquals(' ', $t->nextToken());
-      $this->assertEquals('an', $t->nextToken());
-      $this->assertEquals(' ', $t->nextToken());
-      $this->assertEquals('example', $t->nextToken());
-      $this->assertFalse($t->hasMoreTokens());
-    }
-    
-    /**
-     * Test string tokenizing
-     *
-     */
-    #[@test]
-    public function repetetiveDelimiters() {
-      $t= new StringTokenizer("Hello \nWorld!", " \n");
-      $this->assertEquals('Hello', $t->nextToken());
-      $this->assertEquals('', $t->nextToken());
-      $this->assertEquals('World!', $t->nextToken());
-      $this->assertFalse($t->hasMoreTokens());
-    }
-
-    /**
-     * Test string tokenizing
-     *
-     */
-    #[@test]
-    public function repetetiveDelimitersWithDelims() {
-      $t= new StringTokenizer("Hello \nWorld!", " \n", TRUE);
-      $this->assertEquals('Hello', $t->nextToken());
-      $this->assertEquals(' ', $t->nextToken());
-      $this->assertEquals("\n", $t->nextToken());
-      $this->assertEquals('World!', $t->nextToken());
-      $this->assertFalse($t->hasMoreTokens());
+    protected function tokenizerInstance($source, $delimiters= ' ', $returnDelims= FALSE) {
+      return new StringTokenizer($source, $delimiters, $returnDelims);
     }
   }
 ?>
