2891eb0fa027afddd95f3c1dcdbad08926857eb9
thekid <timm.friebe@gmail.com>
2007-05-10 11:52:39 +0000

    - Fix nextToken() method to work with any number of bytes read from   input stream
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@10321 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/text/StreamTokenizer.class.php
+++ skeleton/text/StreamTokenizer.class.php
@@ -32,7 +32,7 @@
      * @return  bool more tokens
      */
     public function hasMoreTokens() {
-      return (!empty($this->_stack) || strlen($this->_buf) > 0);
+      return !empty($this->_stack) || '' != $this->_buf;
     }
     
     /**
@@ -42,21 +42,23 @@
      * @return  string next token
      */
     public function nextToken($delimiters= NULL) {
-      if (empty($this->_stack) || strlen($this->_buf) > 0) {
+      if (empty($this->_stack) || '' != $this->_buf) {
       
         // Read until we have either find a delimiter or until we have 
         // consumed the entire content.
-        $offset= strlen($this->_buf);
-        while (($offset === strlen($this->_buf)) && $this->source->available()) {
-          $this->_buf.= $this->source->read(4);   // XXX FIXME XXX
+        do {
+          $this->_buf.= $this->source->read();
           $offset= strcspn($this->_buf, $delimiters ? $delimiters : $this->delimiters);
-        }
+          if ($offset != strlen($this->_buf)) break;
+        } while ($this->source->available());
+
         if (!$this->returnDelims || $offset > 0) $this->_stack[]= substr($this->_buf, 0, $offset);
         if ($this->returnDelims && $offset < strlen($this->_buf)) {
           $this->_stack[]= $this->_buf{$offset};
         }
         $this->_buf= substr($this->_buf, $offset+ 1);
       }
+      
       return array_shift($this->_stack);
     }
   }
