1a5832c4b1c44fe53b8c2e052a89c0118f07788d
thekid <timm.friebe@gmail.com>
2007-05-15 15:13:04 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@10370 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/people/friebe/shutdown/ok.php
@@ -0,0 +1,20 @@
+<?php
+  require('lang.base.php');
+  xp::sapi('cli');
+  
+  class WithDestructor extends Object {
+  
+    public function __destruct() {
+      Console::writeLine('DESTRUCT');
+    }
+  }
+  
+  function atexit() {
+    Console::writeLine('SHUTDOWN-FUNC');
+  }
+  
+  register_shutdown_function('atexit');
+  
+  $a= array(new WithDestructor());
+  Console::writeLine('OK');
+?>
