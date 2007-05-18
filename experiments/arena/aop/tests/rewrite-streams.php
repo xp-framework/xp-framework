d842135714ef2a8227466d9dbade6f74980f1f1c
thekid <timm.friebe@gmail.com>
2007-05-18 10:44:11 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@10382 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/aop/tests/rewrite-streams.php
+++ experiments/arena/aop/tests/rewrite-streams.php
@@ -2,93 +2,12 @@
   require('lang.base.php');
   xp::sapi('cli');
   uses(
-    'text.StreamTokenizer', 
-    'io.streams.FileInputStream', 
-    'io.File',
+    'util.invoke.aop.Weaver',
     'util.profiling.Timer'
   );
   
   class Aop extends Object {
     public static $pointcuts= array();
-  }
-  
-  class IOWrapper extends Object {
-    private $f, $class= NULL;
-    
-    // {{{ function bool stream_open(string path, string mode, int options, string opened_path)
-    //     Open the given stream and check if file exists
-    function stream_open($path, $mode, $options, $opened_path) {
-      stream_wrapper_restore('file');
-      $this->f= new StreamTokenizer(new FileInputStream(new File($path)), " \r\n\t", TRUE);
-      return TRUE;
-    }
-    // }}}
-    
-    // {{{ string stream_read(int count)
-    //     Read $count bytes up-to-length of file
-    function stream_read($count) {
-      $t= $this->f->nextToken();
-
-      if ('class' === $t) {         // FIXME: Check strings or comments
-        $ws= $this->f->nextToken();
-        $this->class= $this->f->nextToken(' {');
-        return $t.$ws.$this->class;
-      }
-      
-      if ($this->class && 'function' === $t) {
-        $ws= $this->f->nextToken();
-        $name= $this->f->nextToken('(');
-        $this->f->nextToken('(');
-        // DEBUG fputs(STDERR, "NAME = # $this->class::$name #\n");
-        if (!isset(Aop::$pointcuts[$this->class.'::'.$name])) {
-          return $t.$ws.$name.'(';
-        }
-        
-        $args= '('.$this->f->nextToken('{');
-        $this->f->nextToken('{');
-        $t= 'function '.$name.$args.'{ ';
-        
-        // @before
-        $t.= 'call_user_func_array(Aop::$pointcuts[\''.$this->class.'::'.$name.'\'][\'before\'], array'.$args.');';
-        
-        // @except
-        $t.= 'try { $r= $this->·'.$name.$args.'; } catch (Exception $e) { call_user_func(Aop::$pointcuts[\''.$this->class.'::'.$name.'\'][\'except\'], $e); throw $e; } ';
-        
-        // @after
-        $t.= 'call_user_func(Aop::$pointcuts[\''.$this->class.'::'.$name.'\'][\'after\'], $r); return $r;';
-        
-        $t.= '} function ·'.$name.$args.' {';
-        
-        // DEBUG fputs(STDERR, $t."\n");
-      }
-
-      return $t;
-    }
-    // }}}
-    
-    // {{{ bool stream_eof()
-    //     Returns whether stream is at end of file
-    function stream_eof() {
-      $eof= !$this->f->hasMoreTokens();
-      if ($eof) {
-        stream_wrapper_unregister('file');
-        stream_wrapper_register('file', __CLASS__);
-      }
-      return $eof;
-    }
-    // }}}
-    
-    // {{{ <string,int> url_stat(string path)
-    //     Retrieve status of url
-    function url_stat($path) {
-      stream_wrapper_restore('file');
-      $r= @stat($path);
-      stream_wrapper_unregister('file');
-      stream_wrapper_register('file', __CLASS__);
-      return $r;
-    }
-    // }}}
-  
   }
   
   #[@aspect]
@@ -115,25 +34,32 @@
   // Install stream wrapper
   $p= new ParamString();
   if (!$p->exists('disable')) {
-    stream_wrapper_unregister('file');
-    stream_wrapper_register('file', 'IOWrapper');
+    ClassLoader::$transform= 'weave://';
   }
     
   // Register pointcuts
   $pa= new PowerAspect();
+  Aop::$pointcuts['Binford']= TRUE;
   Aop::$pointcuts['Binford::setPoweredBy']= array(
     'before' => array($pa, 'checkPower'),
     'after'  => array($pa, 'logPower'),
   );
   
+  ClassLoader::registerLoader(new ArchiveClassLoader(new ArchiveReader(
+    dirname(__FILE__).DIRECTORY_SEPARATOR.'fqcns.xar'
+  )));
+
   // Load binford class
   $t= new Timer();
   $t->start();
   XPClass::forName('util.Binford');
+  XPClass::forName('info.binford6100.Date');
   $t->stop();
   
   // Create an instance
-  $bf= create(new Binford($p->value(1, NULL, 6100)));
+  $bf= new Binford($p->value(1, NULL, 6100));
+  $bd= new info·binford6100·Date();  
   
-  Console::writeLinef('%s - took %.3f seconds', $bf->toString(), $t->elapsedTime());
+  // Profiling
+  Console::writeLinef('%s && %s - took %.3f seconds', $bf->toString(), $bd->toString(), $t->elapsedTime());
 ?>
---
+++ experiments/arena/aop/util/invoke/aop/Weaver.class.php
@@ -0,0 +1,97 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses(
+    'text.StreamTokenizer', 
+    'io.streams.FileInputStream', 
+    'io.File'
+  );
+
+  /**
+   * Weave interceptors into sourcecode
+   *
+   * @purpose  AOP
+   */
+  class Weaver extends Object {
+    private 
+      $f     = NULL,
+      $class = NULL;
+    
+    
+    static function __static() {
+      stream_wrapper_register('weave', __CLASS__);
+    }
+    
+    /**
+     * Stream wrapper open() implementation
+     *
+     * @param   string path
+     * @param   string mode
+     * @param   int options
+     * @param   string opened_path
+     * @return  bool
+     */
+    function stream_open($path, $mode, $options, $opened_path) {
+      fputs(STDERR, "$path\n");
+      $this->f= new StreamTokenizer(new FileInputStream(new File(substr($path, 8))), " \r\n\t", TRUE);
+      return TRUE;
+    }
+    
+    /**
+     * Stream wrapper read() implementation
+     *
+     * @param   int count
+     * @return  string
+     */
+    function stream_read($count) {
+      $t= $this->f->nextToken();
+
+      if ('class' === $t) {         // FIXME: Check strings or comments
+        $ws= $this->f->nextToken();
+        $this->class= $this->f->nextToken(' {');
+        return $t.$ws.$this->class;
+      }
+      
+      if ($this->class && 'function' === $t) {
+        $ws= $this->f->nextToken();
+        $name= $this->f->nextToken('(');
+        $this->f->nextToken('(');
+        // DEBUG fputs(STDERR, "NAME = # $this->class::$name #\n");
+        if (!isset(Aop::$pointcuts[$this->class.'::'.$name])) {
+          return $t.$ws.$name.'(';
+        }
+        
+        $args= '('.$this->f->nextToken('{');
+        $this->f->nextToken('{');
+        $t= 'function '.$name.$args.'{ ';
+        
+        // @before
+        $t.= 'call_user_func_array(Aop::$pointcuts[\''.$this->class.'::'.$name.'\'][\'before\'], array'.$args.');';
+        
+        // @except
+        $t.= 'try { $r= $this->·'.$name.$args.'; } catch (Exception $e) { call_user_func(Aop::$pointcuts[\''.$this->class.'::'.$name.'\'][\'except\'], $e); throw $e; } ';
+        
+        // @after
+        $t.= 'call_user_func(Aop::$pointcuts[\''.$this->class.'::'.$name.'\'][\'after\'], $r); return $r;';
+        
+        $t.= '} function ·'.$name.$args.' {';
+        
+        // DEBUG fputs(STDERR, $t."\n");
+      }
+
+      return $t;
+    }
+    
+    /**
+     * Stream wrapper eof() implementation
+     *
+     * @return  bool
+     */
+    function stream_eof() {
+      return $this->f->hasMoreTokens();
+    }
+  }
+?>
