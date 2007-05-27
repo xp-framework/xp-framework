c380b9b8fe3dc3d203be40dc68262ffd3ab724e2
kiesel <alex@kiesel.name>
2007-05-27 09:48:28 +0000

    - effort to make work seamlessly on Windows
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@10426 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/technologies/env/bin/findxpclass.sh
+++ ports/technologies/env/bin/findxpclass.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 
-BASE=$(dirname $0)
+SELF=$(realpath $0)
+BASE=$(dirname $SELF)
 
 . $BASE/common.inc.sh
 
 for i in "$@"; do
-  IFS=':'
-  for DIR in $PHP_INCLUDE_PATH; do
+  IFS=$_XP_PATH_SEPARATOR;
+  for DIR in $_XP_INCLUDE_PATH; do
     FILENAME=`echo $i | tr . /`.class.php
-    if [ -f $DIR/$FILENAME ]; then
-      echo $DIR/$FILENAME;
+    if [ -f "$DIR/$FILENAME" ]; then
+      echo $(unixpath "$DIR/$FILENAME");
       exit 0;
     fi
     
     SAPINAME=`echo $i | tr . /`.sapi.php
-    if [ -f $DIR/sapi/$SAPINAME ]; then
-      echo $DIR/sapi/$SAPINAME;
+    if [ -f "$DIR/sapi/$SAPINAME" ]; then
+      echo $(unixpath "$DIR/sapi/$SAPINAME");
       exit 0;
     fi
   done
+    
+  echo "Could not find '$i'"
 done
 
 exit 1;
