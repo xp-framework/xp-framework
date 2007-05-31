3cf831b510ff5976561bfd55632a5cbf1df51df2
thekid <timm.friebe@gmail.com>
2007-05-31 11:24:15 +0000

    - Initial release # Motivated by bug #8
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@10467 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/classes/net/xp_framework/unittest/peer/HttpRequestTest.class.php
@@ -0,0 +1,111 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses(
+    'unittest.TestCase',
+    'peer.http.HttpRequest'
+  );
+
+  /**
+   * TestCase for HTTP request construction
+   *
+   * @see      xp://peer.http.HttpRequest
+   * @purpose  Unittest
+   */
+  class HttpRequestTest extends TestCase {
+  
+    /**
+     * Test HTTP GET
+     *
+     */
+    #[@test]
+    public function getSimpleUrl() {
+      $r= new HttpRequest(new URL('http://example.com'));
+      $r->setMethod(HTTP_GET);
+      $this->assertEquals(
+        "GET / HTTP/1.1\r\nConnection: close\r\nHost: example.com:80\r\n\r\n",
+        $r->getRequestString()
+      );
+    }
+
+    /**
+     * Test HTTP GET - parameters via URL
+     *
+     */
+    #[@test]
+    public function getUrlWithUrlParams() {
+      $r= new HttpRequest(new URL('http://example.com/?a=b'));
+      $r->setMethod(HTTP_GET);
+      $this->assertEquals(
+        "GET /?a=b HTTP/1.1\r\nConnection: close\r\nHost: example.com:80\r\n\r\n",
+        $r->getRequestString()
+      );
+    }
+
+    /**
+     * Test HTTP GET - parameters via setParameters(array<string, string>)
+     *
+     */
+    #[@test]
+    public function getUrlWithArrayParams() {
+      $r= new HttpRequest(new URL('http://example.com/'));
+      $r->setMethod(HTTP_GET);
+      $r->setParameters(array('a' => 'b'));
+      $this->assertEquals(
+        "GET /?a=b HTTP/1.1\r\nConnection: close\r\nHost: example.com:80\r\n\r\n",
+        $r->getRequestString()
+      );
+    }
+
+    /**
+     * Test HTTP GET - parameters via setParameters(string)
+     *
+     */
+    #[@test]
+    public function getUrlWithStringParams() {
+      $r= new HttpRequest(new URL('http://example.com/'));
+      $r->setMethod(HTTP_GET);
+      $r->setParameters('a=b');
+      $this->assertEquals(
+        "GET /?a=b HTTP/1.1\r\nConnection: close\r\nHost: example.com:80\r\n\r\n",
+        $r->getRequestString()
+      );
+    }
+
+    /**
+     * Test HTTP GET - parameters via setParameters(array<string, string>)
+     * in combination with parameters passed in the constructor.
+     *
+     */
+    #[@test]
+    public function getUrlWithArrayAndUrlParams() {
+      $r= new HttpRequest(new URL('http://example.com/?a=b'));
+      $r->setMethod(HTTP_GET);
+      $r->setParameters(array('a' => 'b'));
+      $this->assertEquals(
+        "GET /?a=b HTTP/1.1\r\nConnection: close\r\nHost: example.com:80\r\n\r\n",
+        $r->getRequestString()
+      );
+    }
+
+
+    /**
+     * Test HTTP GET - parameters via setParameters(string)
+     * in combination with parameters passed in the constructor.
+     *
+     */
+    #[@test]
+    public function getUrlWithStringAndUrlParams() {
+      $r= new HttpRequest(new URL('http://example.com/?a=b'));
+      $r->setMethod(HTTP_GET);
+      $r->setParameters('a=b');
+      $this->assertEquals(
+        "GET /?a=b HTTP/1.1\r\nConnection: close\r\nHost: example.com:80\r\n\r\n",
+        $r->getRequestString()
+      );
+    }
+  }
+?>
