a0ff05d8ec1200d904d4b28565e738ce6132a3c2
thekid <timm.friebe@gmail.com>
2007-06-07 12:50:23 +0000

    - Add tests:   . variableInstanceOfMember()   . variableInstanceOfMemberChain()   . variableInstanceOfArray()   . variableInstanceOfHashLookup()
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@10525 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/people/friebe/jay/net/xp_framework/tools/vm/unittest/parser/InstanceOfParserTest.class.php
+++ experiments/people/friebe/jay/net/xp_framework/tools/vm/unittest/parser/InstanceOfParserTest.class.php
@@ -57,5 +57,81 @@
       $this->assertNode('Variable', $nodes[1]->type);
       $this->assertEquals('$name', $nodes[1]->type->name);
     }
+
+    /**
+     * Tests $a instanceof $string->type
+     *
+      */
+    #[@test]
+    public function variableInstanceOfMember() {
+      $nodes= $this->parse('class String { } $a instanceof $string->type;');
+      $this->assertEquals(2, sizeof($nodes));
+      $this->assertNode('InstanceOf', $nodes[1]);
+      $this->assertNode('Variable', $nodes[1]->object);
+      $this->assertEquals('$a', $nodes[1]->object->name);
+      $this->assertNode('ObjectReference', $nodes[1]->type);
+      $this->assertNode('Variable', $nodes[1]->type->class);
+      $this->assertEquals('$string', $nodes[1]->type->class->name);
+      $this->assertNode('Member', $nodes[1]->type->member);
+      $this->assertEquals('type', $nodes[1]->type->member->name);
+    }
+
+    /**
+     * Tests $a instanceof $string->type->name
+     *
+      */
+    #[@test]
+    public function variableInstanceOfMemberChain() {
+      $nodes= $this->parse('class String { } $a instanceof $string->type->name;');
+      $this->assertEquals(2, sizeof($nodes));
+      $this->assertNode('InstanceOf', $nodes[1]);
+      $this->assertNode('Variable', $nodes[1]->object);
+      $this->assertEquals('$a', $nodes[1]->object->name);
+      $this->assertNode('ObjectReference', $nodes[1]->type);
+      $this->assertNode('Variable', $nodes[1]->type->class);
+      $this->assertEquals('$string', $nodes[1]->type->class->name);
+      $this->assertNode('Member', $nodes[1]->type->member);
+      $this->assertEquals('type', $nodes[1]->type->member->name);
+      $this->assertEquals(1, sizeof($nodes[1]->type->chain));
+      $this->assertNode('ObjectReference', $nodes[1]->type->chain[0]);
+      $this->assertNull($nodes[1]->type->chain[0]->class);
+      $this->assertNode('Member', $nodes[1]->type->chain[0]->member);
+      $this->assertEquals('name', $nodes[1]->type->chain[0]->member->name);
+    }
+
+    /**
+     * Tests $a instanceof $types[0]
+     *
+      */
+    #[@test]
+    public function variableInstanceOfArray() {
+      $nodes= $this->parse('class String { } $a instanceof $types[0];');
+      $this->assertEquals(2, sizeof($nodes));
+      $this->assertNode('InstanceOf', $nodes[1]);
+      $this->assertNode('Variable', $nodes[1]->object);
+      $this->assertEquals('$a', $nodes[1]->object->name);
+      $this->assertNode('ArrayAccess', $nodes[1]->type);
+      $this->assertNode('Variable', $nodes[1]->type->expression);
+      $this->assertEquals('$types', $nodes[1]->type->expression->name);
+      $this->assertNode('LongNumber', $nodes[1]->type->offset);
+      $this->assertEquals(0, (int)$nodes[1]->type->offset->value);
+    }
+
+    /**
+     * Tests $a instanceof $types["name"]
+     *
+      */
+    #[@test]
+    public function variableInstanceOfHashLookup() {
+      $nodes= $this->parse('class String { } $a instanceof $types["name"];');
+      $this->assertEquals(2, sizeof($nodes));
+      $this->assertNode('InstanceOf', $nodes[1]);
+      $this->assertNode('Variable', $nodes[1]->object);
+      $this->assertEquals('$a', $nodes[1]->object->name);
+      $this->assertNode('ArrayAccess', $nodes[1]->type);
+      $this->assertNode('Variable', $nodes[1]->type->expression);
+      $this->assertEquals('$types', $nodes[1]->type->expression->name);
+      $this->assertEquals('"name"', $nodes[1]->type->offset);
+    }
   }
 ?>
