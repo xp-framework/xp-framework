debd7ae50f43068b46c736e9a98a08ec733c477d
Christian Gellweiler <gelli@developer.xp-framework.net>
2007-06-15 08:03:56 +0000

    - Extract SOAPAction header from environment
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@10621 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/sapi/cgi.sapi.php
+++ skeleton/sapi/cgi.sapi.php
@@ -25,6 +25,7 @@
       'Host'             => @$_ENV['HTTP_HOST'],
       'Referer'          => @$_ENV['HTTP_REFERER'],
       'User-Agent'       => @$_ENV['HTTP_USER_AGENT'],
+      'SOAPAction'       => @$_ENV['HTTP_SOAPACTION'],
     );
     
     // Provide them in standard naming (remove HTTP_ at the beginning)
