df256fbbdb9ca8368fa0c43f325945160a6decc3
thekid <timm.friebe@gmail.com>
2007-06-29 11:22:15 +0000

    - Simplify date comparison - SQLFunctions are now supported as LHS for SimpleExpressions
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@10682 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/or-mapper/net/xp_forge/examples/SelectAccount.class.php
+++ experiments/arena/or-mapper/net/xp_forge/examples/SelectAccount.class.php
@@ -29,24 +29,10 @@
       } else if (is_numeric($criteria)) {
         $this->criteria->add(Account::column('account_id')->equal($criteria));
       } else if ('@' == $criteria{0}) {
-        $func= SQLFunctions::datediff(
-          'DAY', 
-          Account::column('lastchange'), 
-          Date::fromString(substr($criteria, 1))
-        );
-
-        $this->criteria->add(newinstance('rdbms.criterion.Criterion', array($func, '%d', 0, EQUAL), '{
-          public function __construct(SQLFunction $func, $type, $value, $op) {
-            $this->func= $func;
-            $this->value= $value;
-            $this->op= $op;
-            $this->type= $type;
-          }
-
-          public function asSql(DBConnection $conn, Peer $peer) {
-            return $this->func->asSql($conn)." ".$conn->prepare(str_replace("?", $this->type, $this->op), $this->value);
-          }
-        }'));
+        $this->criteria->add(Restrictions::equal(
+          SQLFunctions::datediff('day', Account::column('lastchange'), Date::fromString(substr($criteria, 1))),
+          0
+        ));
       } else if (is_string($criteria)) {
         $this->criteria->add(Account::column('username')->like($criteria));
       }
@@ -57,14 +43,7 @@
      *
      */
     public function run() {
-      $this->out->writeLinef(
-        '===> Selecting by <%s>',
-        $this->criteria->toSql(
-          ConnectionManager::getInstance()->getByHost('test-ds', 0), 
-          Account::getPeer()
-        )
-      );
-      
+      $this->out->writeLine('===> Selecting by ', $this->criteria);
       $rows= Account::getPeer()->doSelect($this->criteria);
       $this->out->writeLine($rows);
     }
