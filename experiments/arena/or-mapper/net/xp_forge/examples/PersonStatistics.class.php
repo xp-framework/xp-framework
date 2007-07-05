c23c31758144671e3eedd038c12d74cf347c4876
thekid <timm.friebe@gmail.com>
2007-07-05 20:19:43 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@10730 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/arena/or-mapper/net/xp_forge/examples/PersonStatistics.class.php
@@ -0,0 +1,42 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('net.xp_forge.examples.AbstractExampleCommand');
+
+  /**
+   * Projections demo
+   *
+   * @purpose  Example
+   */
+  class PersonStatistics extends net·xp_forge·examples·AbstractExampleCommand {
+
+    /**
+     * Set criteria
+     *
+     * @param   string criteria default 'bz_id'
+     */
+    #[@arg(position= 0)]
+    public function setField($field= 'bz_id') {
+      $this->criteria= create(new Criteria())
+        ->setProjection(Projections::projectionList(array(
+          Projections::property(Person::column($field)), Projections::count())
+        ))
+        ->addGroupBy($field)
+      ;
+    }
+
+
+    /**
+     * Runs this command
+     *
+     */
+    public function run() {
+      for ($i= Person::getPeer()->iteratorFor($this->criteria); $i->hasNext(); ) {
+        $this->out->writeLine('- ', $i->next());
+      }
+    }
+  }
+?>
