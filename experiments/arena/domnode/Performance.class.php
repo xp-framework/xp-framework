e9783f7047e1c113c07e69c57209194d3aa731b0
thekid <timm.friebe@gmail.com>
2007-07-27 23:29:50 +0000

    - Calculate total elapsed time - Show XML generated from tree implementation
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@10873 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/domnode/Performance.class.php
+++ experiments/arena/domnode/Performance.class.php
@@ -55,14 +55,15 @@
     /**
      * (Insert method's description here)
      *
+     * @param   int nodes
      * @return  xml.Tree tree
      */
-    public function xpTree() {
+    public function xpTree($nodes) {
       $tree= new Tree();
-      $nodes= $tree->addChild(new Node('nodes'));
-      for ($i= 0; $i < 10000; $i++) {
-        $nodes->addChild(new Node('node', NULL, array(
-          'id' => $i
+      $n= $tree->addChild(new Node('nodes'));
+      for ($i= 0; $i < $nodes; $i++) {
+        $n->addChild(new Node('node', '<&">', array(
+          'id' => '&'.$i
         )));
       }
       return $tree;
@@ -71,14 +72,15 @@
     /**
      * (Insert method's description here)
      *
+     * @param   int nodes
      * @return  xml.Tree tree
      */
-    public function domTree() {
+    public function domTree($nodes) {
       $tree= new DomBackedTree();
-      $nodes= $tree->addChild(new DomBackedNode('nodes'));
-      for ($i= 0; $i < 10000; $i++) {
-        $nodes->addChild(new DomBackedNode('node', NULL, array(
-          'id' => $i
+      $n= $tree->addChild(new DomBackedNode('nodes'));
+      for ($i= 0; $i < $nodes; $i++) {
+        $n->addChild(new DomBackedNode('node', '<&">', array(
+          'id' => '&'.$i
         )));
       }
       return $tree;
@@ -109,7 +111,8 @@
      *
      */
     public function run() {
-      $this->out->writeLine('---> ', $this->method, ' via ', $this->tree);
+      $this->out->writeLine('---> ', $this->method);
+      $this->out->writeLine('---> ', $this->tree, ' : ', $this->tree->invoke($this, array(1))->getSource(INDENT_NONE));
       $t= new Timer();
 
       $processor= new DomXSLProcessor();
@@ -117,26 +120,30 @@
       
       // Compose tree
       with ($t->start()); {
-        $tree= $this->tree->invoke($this);
+        $tree= $this->tree->invoke($this, array(10000));
         $t->stop();
-        $this->out->writeLinef('    >> Create tree: %.3f seconds', $t->elapsedTime());
+        $this->out->writeLinef('    >> Create tree: %.3f seconds', $create= $t->elapsedTime());
       }
       
       // Set XML
       with ($t->start()); {
         $this->method->invoke($this, array($processor, $tree));
         $t->stop();
-        $this->out->writeLinef('    >> Pass to XSL processor: %.3f seconds', $t->elapsedTime());
+        $this->out->writeLinef('    >> Pass to XSL processor: %.3f seconds', $proc= $t->elapsedTime());
       }
       
       // Run processor
       with ($t->start()); {
         $processor->run();
         $t->stop();
-        $this->out->writeLinef('    >> Run processor: %.3f seconds', $t->elapsedTime());
+        $this->out->writeLinef('    >> Run processor: %.3f seconds', $run= $t->elapsedTime());
       }
       
       $this->out->writeLine('---> Done, output= ', $processor->output());
+      $this->out->writeLinef(
+        '===> Elapsed time: %.3f seconds',
+        $create+ $proc+ $run
+      );
       $this->out->writeLinef(
         '===> Memory usage: %.3f / peak: %.3f', 
         memory_get_usage() / 1024,
