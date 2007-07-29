c460bcf6630ad5e0f1a88cf4d0d8bb65dc4369d4
thekid <timm.friebe@gmail.com>
2007-07-29 12:17:17 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@10877 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/people/friebe/performance/lang/Enum.class.php
@@ -0,0 +1,106 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Enumeration
+   *
+   * @purpose  Abstract base class   
+   */
+  abstract class Enum extends Object {
+    public
+      $name     = '';
+    
+    protected
+      $ordinal  = 0;
+  
+    /**
+     * Constructor
+     *
+     * @param   int ordinal default 0
+     * @param   string name default ''
+     */
+    public function __construct($ordinal= 0, $name= '') {
+      $this->ordinal= $ordinal;
+      $this->name= $name;
+    }
+  
+    /**
+     * Returns the enumeration member uniquely identified by 
+     *
+     * @param   lang.XPClass class class object
+     * @param   string name enumeration member
+     * @return  lang.Enum
+     * @throws  lang.IllegalArgumentException in case the enum member does not exist or when the given class is not an enum
+     */
+    public static function valueOf(XPClass $class, $name) {
+      if (!$class->isSubclassOf('lang.Enum')) {
+        throw new IllegalArgumentException('Argument class must be lang.XPClass<? extends lang.Enum>');
+      }
+      try {
+        return $class->_reflect->getStaticPropertyValue($name);
+      } catch (ReflectionException $e) {
+        throw new IllegalArgumentException($e->getMessage());
+      }
+    }
+
+    /**
+     * Returns the enumeration member uniquely identified by 
+     *
+     * @param   lang.XPClass class class object
+     * @return  lang.Enum[]
+     * @throws  lang.IllegalArgumentException in case the given class is not an enum
+     */
+    public static function valuesOf(XPClass $class) {
+      if (!$class->isSubclassOf('lang.Enum')) {
+        throw new IllegalArgumentException('Argument class must be lang.XPClass<? extends lang.Enum>');
+      }
+      try {
+        return array_values($class->_reflect->getStaticProperties());
+      } catch (ReflectionException $e) {
+        throw new IllegalArgumentException($e->getMessage());
+      }
+    }
+    
+    /**
+     * Clone interceptor - ensures enums cannot be cloned
+     *
+     * @throws  lang.CloneNotSupportedException'  
+     */
+    public final function __clone() {
+      raise('lang.CloneNotSupportedException', 'Enums cannot be cloned');
+    }
+
+    /**
+     * Returns the name of this enum constant, exactly as declared in its 
+     * enum declaration.
+     *
+     * @return  string
+     */
+    public function name() {
+      return $this->name;
+    }
+    
+    /**
+     * Create a string representation of this enum
+     *
+     * @return  string
+     */
+    public function toString() {
+      return $this->name;
+    }
+    
+    /**
+     * Returns all members for a given enum.
+     *
+     * @param   string class
+     * @return  lang.Enum[]
+     */
+    protected static function membersOf($class) {
+      $c= new ReflectionClass($class);
+      return array_values($c->getStaticProperties());
+    }
+  }
+?>
