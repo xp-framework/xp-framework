d9a9a093c20424cbc94fd6d92e3a38107578b266
kiesel <alex@kiesel.name>
2007-08-23 12:28:55 +0000

    - initial revision
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@10928 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ experiments/people/kiesel/rfc0115/net/xp_framework/unittest/util/DateUtilTest.class.php
@@ -0,0 +1,262 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+ 
+  uses(
+    'unittest.TestCase',
+    'util.DateUtil',
+    'util.TimeZone'
+  );
+
+  /**
+   * Test Date utility class
+   *
+   * @see      xp://util.DateUtil
+   * @purpose  Unit Test
+   */
+  class DateUtilTest extends TestCase {
+    protected
+      $fixture= NULL;
+
+    /**
+     * Sets up this test
+     *
+     */
+    public function setUp() {
+      $this->fixture= Date::create(2000, 1, 1, 12, 15, 11, 'Europe/Berlin');
+    }
+
+    /**
+     * Test simple add operations on a usual date.
+     *
+     */
+    #[@test]
+    public function addSeconds() {
+      $this->assertEquals(
+        new Date(Date::mktime(12, 15, 30, 1, 1, 2000)),
+        DateUtil::addSeconds($this->fixture, 19)
+      );
+    }
+      
+    /**
+     * Test simple add operations on a usual date.
+     *
+     */
+    #[@test]
+    public function addMinutes() {
+      $this->assertEquals(
+        new Date(Date::mktime(12, 44, 11, 1, 1, 2000)),
+        DateUtil::addMinutes($this->fixture, 29)
+      );
+    }
+      
+    /**
+     * Test simple add operations on a usual date.
+     *
+     */
+    #[@test]
+    public function addHours() {
+      $this->assertEquals(
+        new Date(Date::mktime(13, 15, 11, 1, 1, 2000)),
+        DateUtil::addHours($this->fixture, 1)
+      );
+    }
+      
+    /**
+     * Test simple add operations on a usual date.
+     *
+     */
+    #[@test]
+    public function addDays() {
+      $this->assertEquals(
+        new Date(Date::mktime(12, 15, 11, 1, 2, 2000)),
+        DateUtil::addDays($this->fixture, 1)
+      );
+    }
+      
+    /**
+     * Test simple add operations on a usual date.
+     *
+     */
+    #[@test]
+    public function addMonths() {
+      $this->assertEquals(
+        new Date(Date::mktime(12, 15, 11, 2, 1, 2000)),
+        DateUtil::addMonths($this->fixture, 1)
+      );
+    }
+    
+    /**
+     * Test basic add operations with negative values.
+     *
+     */
+    #[@test]
+    public function addNegativeSeconds() {
+      $this->assertEquals(
+        new Date(Date::mktime(12, 14, 52, 1, 1, 2000)),
+        DateUtil::addSeconds($this->fixture, -19)
+      );
+    }
+      
+    /**
+     * Test basic add operations with negative values.
+     *
+     */
+    #[@test]
+    public function addNegativeMinutes() {
+      $this->assertEquals(
+        new Date(Date::mktime(11, 46, 11, 1, 1, 2000)),
+        DateUtil::addMinutes($this->fixture, -29)
+      );
+    }
+      
+    /**
+     * Test basic add operations with negative values.
+     *
+     */
+    #[@test]
+    public function addNegativeHours() {
+      $this->assertEquals(
+        new Date(Date::mktime(11, 15, 11, 1, 1, 2000)),
+        DateUtil::addHours($this->fixture, -1)
+      );
+    }
+      
+    /**
+     * Test basic add operations with negative values.
+     *
+     */
+    #[@test]
+    public function addNegativeDays() {
+      $this->assertEquals(
+        new Date(Date::mktime(12, 15, 11, 12, 31, 1999)),
+        DateUtil::addDays($this->fixture, -1)
+      );
+    }
+      
+    /**
+     * Test basic add operations with negative values.
+     *
+     */
+    #[@test]
+    public function addNegativeMonths() {
+      $this->assertEquals(
+        new Date(Date::mktime(12, 15, 11, 12, 1, 1999)),
+        DateUtil::addMonths($this->fixture, -1)
+      );
+    }
+    
+    /**
+     * Check leap year handling
+     *
+     */
+    #[@test]
+    public function testLeapYear() {
+      $date= new Date(Date::mktime(0, 0, 0, 2, 1, 2000));
+      
+      $this->assertEquals(
+        new Date(Date::mktime(0, 0, 0, 3, 1, 2000)),
+        DateUtil::addMonths($date, 1)
+      );
+      
+      $this->assertEquals(
+        new Date(Date::mktime(0, 0, 0, 3, 2, 2000)),
+        DateUtil::addDays($date, 30)
+      );
+    }
+    
+    /**
+     * Check non-leap year handling
+     *
+     */
+    #[@test]
+    public function testNonLeapYear() {
+      $date= new Date(Date::mktime(0, 0, 0, 2, 1, 1999));
+      
+      $this->assertEquals(
+        new Date(Date::mktime(0, 0, 0, 3, 1, 1999)),
+        DateUtil::addMonths($date, 1)
+      );
+      
+      $this->assertEquals(
+        new Date(Date::mktime(0, 0, 0, 3, 3, 1999)),
+        DateUtil::addDays($date, 30)
+      );
+    }
+    
+    /**
+     * Tests the compare method
+     *
+     */
+    #[@test]
+    public function comparison() {
+      $this->assertTrue(DateUtil::compare(new Date('1977-12-14'), new Date('1980-05-28')) < 0, 'a < b') &&
+      $this->assertTrue(DateUtil::compare(new Date('1980-05-28'), new Date('1977-12-14')) > 0, 'a > b') &&
+      $this->assertTrue(DateUtil::compare(new Date('1980-05-28'), new Date('1980-05-28')) == 0, 'a == b');
+    }
+
+    /**
+     * Tests using the compare method as a usort() callback
+     *
+     * @see     php://usort
+     */
+    #[@test]
+    public function sorting() {
+      $list= array(
+        new Date('1977-12-14'),
+        new Date('2002-02-21'),
+        new Date('1980-05-28'),
+      );
+      
+      usort($list, array('DateUtil', 'compare'));
+      $this->assertEquals(new Date('1977-12-14'), $list[0], 'offset 0') &&
+      $this->assertEquals(new Date('1980-05-28'), $list[1], 'offset 1') &&
+      $this->assertEquals(new Date('2002-02-21'), $list[2], 'offset 2');
+    }
+    
+    /**
+     * Tests the getBeginningOfWeek and
+     * getEndOfWeek methods.
+     *
+     */
+    #[@test]
+    public function testBeginAndEndOfWeek() {
+      $this->assertEquals(
+        new Date(Date::mktime(0, 0, 0, 1, 14, 2007)),
+        DateUtil::getBeginningOfWeek(new Date('2007-1-18'))
+      );
+      $this->assertEquals(
+        new Date(Date::mktime(23, 59, 59, 1, 20, 2007)),
+        DateUtil::getEndOfWeek(new Date('2007-1-18'))
+      );
+    }
+    
+    /**
+     * Test
+     *
+     */
+    #[@test]
+    public function testMoveToTimezone() {
+      $copy= clone $this->fixture;
+      $tz= new TimeZone('Australia/Sydney');
+      
+      $this->assertEquals($this->fixture, DateUtil::moveToTimeZone($copy, $tz));
+    }
+    
+    /**
+     * Test
+     *
+     */
+    #[@test]
+    public function testSetTimezone() {
+      $this->assertEquals(
+        Date::create(2000, 1, 1, 17, 15, 11),
+        DateUtil::setTimeZone($this->fixture, new TimeZone('America/New_York'))
+      );
+    }
+    
+    
+  }
+?>
---
+++ experiments/people/kiesel/rfc0115/net/xp_framework/unittest/util/TimeZoneTest.class.php
@@ -0,0 +1,132 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses(
+    'unittest.TestCase',
+    'util.TimeZone'
+  );
+
+  /**
+   * TestCase
+   *
+   * @see      reference
+   * @purpose  purpose
+   */
+  class TimeZoneTest extends TestCase {
+  
+    /**
+     * Setup fixture
+     *
+     */
+    public function setUp() {
+      $this->fixture= new TimeZone('Europe/Berlin');
+    }    
+  
+    /**
+     * Test
+     *
+     */
+    #[@test]
+    public function name() {
+      $this->assertEquals('Europe/Berlin', $this->fixture->getName());
+    }
+    
+    /**
+     * Test
+     *
+     */
+    #[@test]
+    public function offsetDST() {
+      $this->assertEquals('+0200', $this->fixture->getOffset(new Date('2007-08-21')));
+    }
+    
+    /**
+     * Test
+     *
+     */
+    #[@test]
+    public function offsetNoDST() {
+      $this->assertEquals('+0100', $this->fixture->getOffset(new Date('2007-01-21')));
+    }
+    
+    /**
+     * Test
+     *
+     */
+    #[@test]
+    public function offsetWithHalfHourDST() {
+      // Australia/Adelaide is +10:30 in DST
+      $this->fixture= new TimeZone('Australia/Adelaide');
+      $this->assertEquals('+1030', $this->fixture->getOffset(new Date('2007-01-21')));
+    }
+    
+    /**
+     * Test
+     *
+     */
+    #[@test]
+    public function offsetWithHalfHourNoDST() {
+      // Australia/Adelaide is +09:30 in non-DST
+      $this->fixture= new TimeZone('Australia/Adelaide');
+      $this->assertEquals('+0930', $this->fixture->getOffset(new Date('2007-08-21')));
+    }
+    
+    /**
+     * Test
+     *
+     */
+    #[@test]
+    public function offsetInSecondsDST() {
+      $this->assertEquals(7200, $this->fixture->getOffsetInSeconds(new Date('2007-08-21')));
+    }
+    
+    /**
+     * Test
+     *
+     */
+    #[@test]
+    public function offsetInSecondsNoDST() {
+      $this->assertEquals(3600, $this->fixture->getOffsetInSeconds(new Date('2007-01-21')));
+    }
+    
+    /**
+     * Test
+     *
+     */
+    #[@test]
+    public function convert() {
+      $date= new Date('2007-01-01 00:00 Australia/Sydney');
+      $this->assertEquals(
+        new Date('2006-12-31 14:00:00 Europe/Berlin'),
+        $this->fixture->convertDate($date)
+      );
+    }
+    
+    /**
+     * Test
+     *
+     */
+    #[@test]
+    public function previousTransition() {
+      $transition= $this->fixture->previousTransition(new Date('2007-08-23'));
+      $this->assertEquals(TRUE, $transition->isDst());
+      $this->assertEquals('CEST', $transition->abbr());
+      $this->assertEquals(new Date('2007-03-25 02:00:00 Europe/Berlin'), $transition->getDate());
+    }
+    
+    /**
+     * Test
+     *
+     */
+    #[@test]
+    public function nextTransition() {
+      $transition= $this->fixture->nextTransition(new Date('2007-08-23'));
+      $this->assertEquals(FALSE, $transition->isDst());
+      $this->assertEquals('CET', $transition->abbr());
+      $this->assertEquals(new Date('2007-10-28 02:00:00 Europe/Berlin'), $transition->getDate());
+    }
+  }
+?>
