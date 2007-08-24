f302c14f919bc7c8d391d930b9b2a5338d061d44
kiesel <alex@kiesel.name>
2007-08-24 16:21:24 +0000

    - add more methods
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@10951 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/people/kiesel/rfc0135/xml/xslt/XSLStringCallback.class.php
+++ experiments/people/kiesel/rfc0135/xml/xslt/XSLStringCallback.class.php
@@ -53,6 +53,30 @@
     #[@xslmethod]
     public function strtoupper($string) {
       return strtoupper($string);
-    }    
+    }
+    
+    /**
+     * Substitute one string through another in a given string
+     *
+     * @param   string str
+     * @param   string search
+     * @param   string replace
+     * @return  string
+     */
+    #[@xslmethod]
+    public function replace($str, $search, $replace) {
+      return str_replace($search, $replace, $str);
+    }
+    
+    /**
+     * Convert newlines to <br/>
+     *
+     * @param   string string
+     * @return  string
+     */
+    #[@xslmethod]
+    public function nl2br($string) {
+      return nl2br($string);
+    }
   }
 ?>
