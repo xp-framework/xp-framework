a624f77bd82b08d7e7ce41cc5aca82cf315ab475
thekid <timm.friebe@gmail.com>
2007-09-11 21:18:04 +0000

    - Add tests:   . stringRepresentation()   . unequality()   . equality()   . nonExistantGroup()   . nonUnicodePattern()   . unicodePattern()   . stringObjectInput()   . stringPrimitiveInput()
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@11054 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/regex/tests/PatternTest.class.php
+++ experiments/arena/regex/tests/PatternTest.class.php
@@ -6,7 +6,8 @@
 
   uses(
     'unittest.TestCase',
-    'text.regex.Pattern'
+    'text.regex.Pattern',
+    'lang.types.String'
   );
 
   /**
@@ -15,15 +16,63 @@
    * @purpose  Unit test
    */
   class PatternTest extends TestCase {
-  
+
     /**
-     * Tests 
+     * Tests the MatchResult::length() method
      *
      */
     #[@test]
-    public function pattern() {
+    public function length() {
+      $this->assertEquals(
+        0, 
+        Pattern::compile('ABC')->matches('123')->length()
+      );
+    }
+
+    /**
+     * Tests "." as a pattern with string primitive
+     *
+     */
+    #[@test]
+    public function stringPrimitiveInput() {
+      $this->assertEquals(0, Pattern::compile('.')->matches('')->length());
       $this->assertEquals(1, Pattern::compile('.')->matches('a')->length());
       $this->assertEquals(2, Pattern::compile('.')->matches('ab')->length());
+    }
+
+    /**
+     * Tests "." as a pattern with lang.types.String instances
+     *
+     */
+    #[@test]
+    public function stringObjectInput() {
+      $this->assertEquals(0, Pattern::compile('.')->matches(new String(''))->length());
+      $this->assertEquals(1, Pattern::compile('.')->matches(new String('a'))->length());
+      $this->assertEquals(2, Pattern::compile('.')->matches(new String('ab'))->length());
+    }
+
+    /**
+     * Tests unicode pattern
+     *
+     */
+    #[@test]
+    public function unicodePattern() {
+      $this->assertEquals(
+        array('GÃ¼n'), 
+        Pattern::compile('.Ã¼.', Pattern::UTF8)->matches(new String('Günter'))->group(0)
+      );
+    }
+
+    /**
+     * Tests non-unicode pattern
+     *
+     */
+    #[@test]
+    public function nonUnicodePattern() {
+      $this->assertEquals(
+        array('Gün'), 
+        Pattern::compile('.ü.')->matches(new String('Günter'))->group(0)
+      );
     }
 
     /**
@@ -32,7 +81,10 @@
      */
     #[@test]
     public function caseInsensitive() {
-      $this->assertEquals(1, Pattern::compile('a', Pattern::CASE_INSENSITIVE)->matches('A')->length());
+      $this->assertEquals(
+        1, 
+        Pattern::compile('a', Pattern::CASE_INSENSITIVE)->matches('A')->length()
+      );
     }
 
     /**
@@ -56,6 +108,51 @@
       $this->assertEquals(
         array('Hello'), 
         Pattern::compile('H[ea]llo')->matches('Hello')->group(0)
+      );
+    }
+
+    /**
+     * Tests the MatchResult::group() method
+     *
+     */
+    #[@test, @expect('lang.IndexOutOfBoundsException')]
+    public function nonExistantGroup() {
+      Pattern::compile('H[ea]llo')->matches('Hello')->group(1);
+    }
+
+    /**
+     * Tests the Pattern::equals() method
+     *
+     */
+    #[@test]
+    public function equality() {
+      $this->assertEquals(
+        Pattern::compile('[a-z]+'),
+        Pattern::compile('[a-z]+')
+      );
+    }
+
+    /**
+     * Tests the Pattern::equals() method
+     *
+     */
+    #[@test]
+    public function unequality() {
+      $this->assertNotEquals(
+        Pattern::compile('[a-z]+', Pattern::CASE_INSENSITIVE),
+        Pattern::compile('[a-z]+')
+      );
+    }
+
+    /**
+     * Tests the Pattern::toString() method
+     *
+     */
+    #[@test]
+    public function stringRepresentation() {
+      $this->assertEquals(
+        'text.regex.Pattern</[a-z]+/i>',
+        Pattern::compile('[a-z]+', Pattern::CASE_INSENSITIVE)->toString()
       );
     }
 
