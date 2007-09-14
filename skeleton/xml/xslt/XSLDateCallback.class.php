e39e396cb00d8ab339f26e624076ff74f2f4c258
kiesel <alex@kiesel.name>
2007-09-14 05:57:40 +0000

    - fully implement RFC #0135 # was depending on RFC #0115
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@11061 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/xml/xslt/XSLDateCallback.class.php
+++ skeleton/xml/xslt/XSLDateCallback.class.php
@@ -4,13 +4,14 @@
  * $Id$ 
  */
 
-  uses('util.Date');
+  uses('util.Date', 'util.DateMath', 'util.DateInterval');
 
   /**
    * XSL callbacks for Date operations
    *
    * @ext       ext/date
    * @see       xp://util.Date
+   * @see       xp://util.DateMath
    * @purpose   XSL callback
    */
   class XSLDateCallback extends Object {
@@ -25,7 +26,24 @@
      */
     #[@xslmethod]
     public function format($date, $format, $timezone= NULL) {
-      return create(new Date($date))->toString($format);
+      return create(new Date($date))->toString($format, new TimeZone($timezone));
+    }
+    
+    /**
+     * Diff two dates with the given interval
+     *
+     * @param   string type
+     * @param   string strdate1
+     * @param   string strdate2
+     * @return  int
+     */
+    #[@xslmethod]
+    public function diff($type, $strdate1, $strdate2) {
+      return DateMath::diff(
+        Enum::valueOf(XPClass::forName('util.DateInterval'), $type),
+        new Date($strdate1),
+        new Date($strdate2)
+      );
     }
   }
 ?>
