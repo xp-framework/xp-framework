086b302fcf380bdb491f8d0fa6b7fe0ca7924751
thekid <timm.friebe@gmail.com>
2007-10-14 16:34:04 +0000

    - Implement uploadFrom() / downloadTo() methods # Use inline io.streams -> PHP streams wrapper, might be a good idea to add # this cass to io.streams API...
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@11149 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- experiments/arena/rfc0140/peer/ftp/FtpFile.class.php
+++ experiments/arena/rfc0140/peer/ftp/FtpFile.class.php
@@ -4,7 +4,7 @@
  * $Id$
  */
 
-  uses('peer.ftp.FtpEntry');
+  uses('peer.ftp.FtpEntry', 'io.streams.InputStream', 'io.streams.OutputStream');
 
   /**
    * FTP file
@@ -13,14 +13,107 @@
    * @purpose  FtpEntry implementation
    */
   class FtpFile extends FtpEntry {
+    private static 
+      $sw= NULL;
+    
+    static function __static() {
+      self::$sw= newinstance('lang.Object', array(), '{
+        private static $streams= array();
+        private $id= NULL;
+        
+        public function wrap($s) { 
+          self::$streams[$s->hashCode()]= $s;
+          return "iostr://".$s->hashCode(); 
+        }
+        
+        function stream_open($path, $mode, $options, $opened_path) {
+          sscanf($path, "iostr://%[^$]", $this->id);
+          if (!isset(self::$streams[$this->id])) {
+            throw new FileNotFoundException("No such iostr ".$this->id);
+          }
+          return TRUE;
+        }
+        
+        function stream_read($count) {
+          return self::$streams[$this->id]->read($count);
+        }
+
+        function stream_eof() {
+          return 0 === self::$streams[$this->id]->available();
+        }
+        
+        function stream_write($data) {
+          self::$streams[$this->id]->write($data);
+          return strlen($data);
+        }
+
+        function stream_close() {
+          unset(self::$streams[$this->id]);
+        }
+
+        public function stream_stat() {
+          return array("size" => self::$streams[$this->id]->available());
+        }
+      }');
+      stream_wrapper_register('iostr', get_class(self::$sw));
+    }
 
     /**
      * Delete this entry
      *
-     * @throws  peer.SocketException in case of an I/O error
+     * @throws  io.IOException in case of an I/O error
      */
     public function delete() {
-      return ftp_delete($this->connection->handle, $this->name);
+      if (FALSE === ftp_delete($this->connection->handle, $this->name)) {
+        throw new IOException('Could not delete file "'.$this->name.'"');
+      }
+    }
+
+    /**
+     * Upload to this file from an input stream
+     *
+     * @param   io.streams.InputStream in
+     * @param   int mode default FTP_ASCII
+     * @throws  peer.SocketException in case of an I/O error
+     */
+    public function uploadFrom(InputStream $in, $mode= FTP_ASCII) {
+      $r= ftp_fput(
+        $this->connection->handle, 
+        $this->name, 
+        $sw= fopen(self::$sw->wrap($in), 'rb'), 
+        $mode
+      );
+      fclose($sw);
+      if (TRUE === $r) return;
+
+      throw new SocketException(sprintf(
+        'Could not put %s to %s using mode %s',
+        $in->toString(), $remote, $mode
+      ));
+    }
+
+    /**
+     * Upload to this file from an input stream
+     *
+     * @param   io.streams.OutputStream out
+     * @param   int mode default FTP_ASCII
+     * @return  io.streams.OutputStream the output stream passed
+     * @throws  peer.SocketException in case of an I/O error
+     */
+    public function downloadTo(OutputStream $out, $mode= FTP_ASCII) {
+      $r= ftp_fget(
+        $this->connection->handle, 
+        $sw= fopen(self::$sw->wrap($out), 'wb'),
+        $this->name, 
+        $mode
+      );
+      fclose($sw);
+      if (TRUE === $r) return $out;
+
+      throw new SocketException(sprintf(
+        'Could not get %s to %s using mode %s',
+        $remote, $out->toString, $mode
+      ));
     }
   }
 ?>
