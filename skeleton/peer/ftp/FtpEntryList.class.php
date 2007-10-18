bd8a1e1d2831441c57b9d2137146b9e878e4d156
thekid <timm.friebe@gmail.com>
2007-10-18 07:31:01 +0000

    - Initial release # Part of RFC #0140
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@11173 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/peer/ftp/FtpEntryList.class.php
@@ -0,0 +1,87 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses('peer.ftp.FtpListIterator');
+
+  /**
+   * List of entries on an FTP server
+   *
+   * @see      xp://peer.ftp.FtpDir#entries
+   * @purpose  List object
+   */
+  class FtpEntryList extends Object implements IteratorAggregate {
+    protected
+      $connection   = NULL,
+      $list         = array(),
+      $base         = '';
+
+    /**
+     * Constructor
+     *
+     * @param   string[] list
+     * @param   peer.ftp.FtpConnection connection
+     * @param   string base default "/"
+     */
+    public function __construct(array $list, FtpConnection $connection, $base= '/') {
+      $this->list= $list;
+      $this->connection= $connection;
+      $this->base= $base;
+    }
+    
+    /**
+     * Returns an iterator for use in foreach()
+     *
+     * @see     php://language.oop5.iterations
+     * @return  php.Iterator
+     */
+    public function getIterator() {
+      return new FtpListIterator($this->list, $this->connection, $this->base);
+    }
+
+    /**
+     * Returns the number of elements in this list.
+     *
+     * @return  int
+     */
+    public function size() {
+      return sizeof($this->list) - 2;     // XXX what happens if "." and ".." are not returned by the FTP server?
+    }
+
+    /**
+     * Tests if this list has no elements.
+     *
+     * @return  bool
+     */
+    public function isEmpty() {
+      return 0 === $this->size();
+    }
+  
+    /**
+     * Returns all elements in this list as an array.
+     *
+     * @return  peer.ftp.FtpEntry[] an array of all entries
+     * @throws  lang.FormatException in case an entry cannot be parsed
+     */
+    public function asArray() {
+      $dotdirs= array($this->base.'./', $this->base.'../');
+
+      for ($i= 0, $r= array(), $s= sizeof($this->list); $i < $s; $i++) {
+        $e= $this->connection->parser->entryFrom($this->list[$i], $this->connection, $this->base);
+        in_array($e->getName(), $dotdirs) || $r[]= $e;
+      }
+      return $r;
+    }
+
+    /**
+     * Creates a string representation of this list
+     *
+     * @return  string
+     */
+    public function toString() {
+      return $this->getClassName().'('.$this->size().' entries)@'.xp::stringOf($this->list);
+    }
+  }
+?>
