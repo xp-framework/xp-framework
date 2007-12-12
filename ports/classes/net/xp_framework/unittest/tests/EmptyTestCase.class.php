dff7654cf57be86ff75bc2b386623bb9a74c195c
thekid <timm.friebe@gmail.com>
2007-12-12 14:37:41 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@11456 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/classes/net/xp_framework/unittest/tests/EmptyTestCase.class.php
@@ -0,0 +1,18 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+ 
+  uses('unittest.TestCase');
+
+  /**
+   * This class is used in the SuiteTest class' emptyTest method
+   *
+   * @see      xp://net.xp_framework.unittest.tests.SuiteTest#addingEmptyTest
+   * @purpose  Unit Test
+   */
+  class EmptyTestCase extends TestCase {
+
+  }
+?>
