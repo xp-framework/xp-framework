8f29f0f310013c57ebdd5d13f5d3840081aa2ce7
thekid <timm.friebe@gmail.com>
2007-12-25 17:05:20 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@11639 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/classes/net/xp_framework/unittest/rdbms/DriverManagerTest.class.php
@@ -0,0 +1,91 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses(
+    'unittest.TestCase',
+    'rdbms.DriverManager'
+  );
+
+  /**
+   * TestCase
+   *
+   * @see      xp://rdbms.DriverManager
+   * @purpose  purpose
+   */
+  class DriverManagerTest extends TestCase {
+  
+    /**
+     * Test Sybase support
+     *
+     */
+    #[@test]
+    public function sybase() {
+      $this->assertClass(
+        DriverManager::getConnection('sybase://localhost'),
+        'rdbms.sybase.SybaseConnection'
+      );
+    }
+
+    /**
+     * Test MySQL support
+     *
+     */
+    #[@test]
+    public function mysql() {
+      $this->assertClass(
+        DriverManager::getConnection('mysql://localhost'),
+        'rdbms.mysql.MySQLConnection'
+      );
+    }
+
+    /**
+     * Test PostgreSQL support
+     *
+     */
+    #[@test]
+    public function pgsql() {
+      $this->assertClass(
+        DriverManager::getConnection('pgsql://localhost'),
+        'rdbms.pgsql.PostgreSQLConnection'
+      );
+    }
+
+    /**
+     * Test sqlite support
+     *
+     */
+    #[@test]
+    public function sqlite() {
+      $this->assertClass(
+        DriverManager::getConnection('sqlite://localhost'),
+        'rdbms.sqlite.SQLiteConnection'
+      );
+    }
+
+    /**
+     * Test getConnection() throws an exception in case an unsupported
+     * driver is encountered
+     *
+     */
+    #[@test, @expect('rdbms.DriverNotSupportedException')]
+    public function unsupported() {
+      DriverManager::getConnection('unsupported://localhost');
+    }
+
+    /**
+     * Test registering a connection
+     *
+     */
+    #[@test]
+    public function register() {
+      DriverManager::register('mock', XPClass::forName('net.xp_framework.unittest.rdbms.mock.MockConnection'));
+      $this->assertClass(
+        DriverManager::getConnection('mock://localhost'),
+        'net.xp_framework.unittest.rdbms.mock.MockConnection'
+      );
+    }
+  }
+?>
