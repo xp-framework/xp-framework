e1df2a37c5fe032dd5c6c583f5b851032a4d25ff
thekid <timm.friebe@gmail.com>
2007-12-25 13:29:34 +0000

    - Add tests for non-existant values - Add tests for situations when short values are given but queried with long - QA: Remove test-prefix for existing tests, we use annotations now # Untested code-paths found by coverage analysis
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@11617 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/classes/net/xp_framework/unittest/util/cmd/ParamStringTest.class.php
+++ ports/classes/net/xp_framework/unittest/util/cmd/ParamStringTest.class.php
@@ -22,7 +22,7 @@
      *
      */
     #[@test]
-    public function testShortFlag() {
+    public function shortFlag() {
       $p= new ParamString(array('-k'));
 
       $this->assertTrue($p->exists('k'));
@@ -34,7 +34,7 @@
      *
      */
     #[@test]
-    public function testShortValue() {
+    public function shortValue() {
       $p= new ParamString(array('-d', 'sql'));
 
       $this->assertTrue($p->exists('d'));
@@ -46,7 +46,7 @@
      *
      */
     #[@test]
-    public function testLongFlag() {
+    public function longFlag() {
       $p= new ParamString(array('--verbose'));
 
       $this->assertTrue($p->exists('verbose'));
@@ -58,11 +58,102 @@
      *
      */
     #[@test]
-    public function testLongValue() {
+    public function longValue() {
       $p= new ParamString(array('--level=3'));
 
       $this->assertTrue($p->exists('level'));
       $this->assertEquals('3', $p->value('level'));
+    }
+
+    /**
+     * Test Long option with value
+     *
+     */
+    #[@test]
+    public function longValueShortGivenDefault() {
+      $p= new ParamString(array('-l', '3'));
+
+      $this->assertTrue($p->exists('level'));
+      $this->assertEquals('3', $p->value('level'));
+    }
+
+    /**
+     * Test Long option with value
+     *
+     */
+    #[@test]
+    public function longValueShortGiven() {
+      $p= new ParamString(array('-L', '3', '-l', 'FAIL'));
+
+      $this->assertTrue($p->exists('level', 'L'));
+      $this->assertEquals('3', $p->value('level', 'L'));
+    }
+
+    /**
+     * Test positional query
+     *
+     */
+    #[@test]
+    public function positional() {
+      $p= new ParamString(array('That is a realm'));
+      
+      $this->assertTrue($p->exists(0));
+      $this->assertEquals('That is a realm', $p->value(0));
+    }
+
+    /**
+     * Test exists() method
+     *
+     */
+    #[@test]
+    public function existance() {
+      $p= new ParamString(array('a', 'b'));
+      
+      $this->assertTrue($p->exists(0));
+      $this->assertTrue($p->exists(1));
+      $this->assertFalse($p->exists(2));
+    }
+
+    /**
+     * Test positional query
+     *
+     */
+    #[@test, @expect('lang.IllegalArgumentException')]
+    public function nonExistantPositional() {
+      create(new ParamString(array('a')))->value(1);
+    }
+
+    /**
+     * Test positional query
+     *
+     */
+    #[@test]
+    public function nonExistantPositionalWithDefault() {
+      $this->assertEquals(
+        'Default', 
+        create(new ParamString(array('--verbose')))->value(1, NULL, 'Default')
+      );
+    }
+
+    /**
+     * Test named query
+     *
+     */
+    #[@test, @expect('lang.IllegalArgumentException')]
+    public function nonExistantNamed() {
+      create(new ParamString(array('--verbose')))->value('name');
+    }
+
+    /**
+     * Test named query
+     *
+     */
+    #[@test]
+    public function nonExistantNamedWithDefault() {
+      $this->assertEquals(
+        'Default', 
+        create(new ParamString(array('--verbose')))->value('name', 'n', 'Default')
+      );
     }
     
     /**
