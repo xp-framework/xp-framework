10ee02bc18ef61ddb454dddd6ee83ad625e684d9
thekid <timm.friebe@gmail.com>
2007-12-29 19:53:01 +0000

    - Initial release # Part of RFC #0010
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@11655 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/lang/ClassCastException.class.php
@@ -0,0 +1,15 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Indicates a class cast failed
+   *
+   * @purpose  Exception
+   */
+  class ClassCastException extends XPException {
+    
+  }
+?>
