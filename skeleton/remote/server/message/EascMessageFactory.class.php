2ff90fe816ad3a939ece9f8fa87347927be98697
thekid <timm.friebe@gmail.com>
2008-01-11 18:24:25 +0000

    - Make handlers changeable via setHandler()
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@11699 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/remote/server/message/EascMessageFactory.class.php
+++ skeleton/remote/server/message/EascMessageFactory.class.php
@@ -18,24 +18,40 @@
    *
    * @purpose  Create EASC message
    */
-  class EascMessageFactory extends Object {
+  abstract class EascMessageFactory extends Object {
+    protected static 
+      $handlers= array();
+    
+    static function __static() {
+      self::$handlers[REMOTE_MSG_INIT]= XPClass::forName('remote.server.message.EascInitMessage');
+      self::$handlers[REMOTE_MSG_LOOKUP]= XPClass::forName('remote.server.message.EascLookupMessage');
+      self::$handlers[REMOTE_MSG_CALL]= XPClass::forName('remote.server.message.EascCallMessage');
+      self::$handlers[REMOTE_MSG_VALUE]= XPClass::forName('remote.server.message.EascValueMessage');
+      self::$handlers[REMOTE_MSG_EXCEPTION]= XPClass::forName('remote.server.message.EascExceptionMessage');
+    }
+  
+    /**
+     * Set handler for a given type to 
+     *
+     * @param   int type
+     * @param   lang.XPClass class
+     */
+    public static function setHandler($type, XPClass $class) {
+      self::$handlers[$type]= $class;
+    }
   
     /**
      * Factory method
      *
      * @param   int type
-     * @return  lang.XPClass
+     * @return  remote.server.message.EascMessage
+     * @throws  lang.IllegalArgumentException if no message exists for this type.
      */
     public static function forType($type) {
-      $handlers= array(
-        REMOTE_MSG_INIT       => 'Init',
-        REMOTE_MSG_LOOKUP     => 'Lookup',
-        REMOTE_MSG_CALL       => 'Call',
-        REMOTE_MSG_VALUE      => 'Value',
-        REMOTE_MSG_EXCEPTION  => 'Exception'
-      );
-      
-      return XPClass::forName(sprintf('remote.server.message.Easc%sMessage', $handlers[$type]))->newInstance();
+      if (!isset(self::$handlers[$type])) {
+        throw new IllegalArgumentException('Unknown message type '.$type);
+      }
+      return self::$handlers[$type]->newInstance();
     }
   }
 ?>
