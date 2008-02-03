7c7dba18e0f982c7784a2f595ed80eb5038eaece
thekid <timm.friebe@gmail.com>
2008-02-03 22:17:53 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@11744 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/classes/net/xp_framework/unittest/core/ProcessTest.class.php
@@ -0,0 +1,141 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses(
+    'unittest.TestCase',
+    'lang.Runtime',
+    'lang.System',
+    'lang.Process',
+    'io.streams.Streams',
+    'io.streams.MemoryOutputStream'
+  );
+
+  /**
+   * TestCase
+   *
+   * @see      xp://lang.Process
+   * @purpose  Unittest
+   */
+  class ProcessTest extends TestCase {
+  
+    /**
+     * Return executable name
+     *
+     * @return  string
+     */
+    protected function executable() {
+      return Runtime::getInstance()->getExecutable()->getFilename();
+    }
+  
+    /**
+     * Test process status information methods
+     *
+     * @see      xp://lang.Process#getProcessId
+     * @see      xp://lang.Process#getFilename
+     * @see      xp://lang.Process#getCommandLine
+     * @see      xp://lang.Process#exitValue
+     */
+    #[@test]
+    public function information() {
+      $p= new Process($this->executable(), array('-v'));
+      try {
+        $this->assertEquals(-1, $p->exitValue(), 'Process should not have exited yet');
+        $this->assertNotEquals(0, $p->getProcessId());
+        $this->assertNotEquals('', $p->getFilename());
+        $this->assertTrue(create(new String($p->getCommandLine()))->contains('-v'));
+        $p->close();
+      } catch (AssertionFailedError $e) {
+        $p->close();    // Ensure process is closed
+        throw $e;
+      }
+    }
+
+    /**
+     * Test exit value
+     *
+     */
+    #[@test]
+    public function exitValue() {
+      $p= new Process($this->executable(), array('-r', escapeshellarg('exit(0);')));
+      $this->assertEquals(0, $p->close());
+    }
+
+    /**
+     * Test non-zero exit value
+     *
+     */
+    #[@test]
+    public function nonZeroExitValue() {
+      $p= new Process($this->executable(), array('-r', escapeshellarg('exit(2);')));
+      $this->assertEquals(2, $p->close());
+    }
+
+    /**
+     * Test standard input
+     *
+     */
+    #[@test]
+    public function stdIn() {
+      $p= new Process($this->executable(), array('-r', escapeshellarg('fprintf(STDOUT, fread(STDIN, 0xFF));')));
+      try {
+        $p->in->write('IN');
+        $p->in->close();
+        $this->assertEquals('IN', $p->out->read());
+      } catch (AssertionFailedError $e) {
+        $p->close();    // Ensure process is closed
+        throw $e;
+      }
+    }
+
+    /**
+     * Test standard output
+     *
+     */
+    #[@test]
+    public function stdOut() {
+      $p= new Process($this->executable(), array('-r', escapeshellarg('fprintf(STDOUT, "OUT");')));
+      try {
+        $this->assertEquals('OUT', $p->out->read());
+      } catch (AssertionFailedError $e) {
+        $p->close();    // Ensure process is closed
+        throw $e;
+      }
+    }
+
+    /**
+     * Test standard error
+     *
+     */
+    #[@test]
+    public function stdErr() {
+      $p= new Process($this->executable(), array('-r', escapeshellarg('fprintf(STDERR, "ERR");')));
+      try {
+        $this->assertEquals('ERR', $p->err->read());
+      } catch (AssertionFailedError $e) {
+        $p->close();    // Ensure process is closed
+        throw $e;
+      }
+    }
+
+    /**
+     * Test running a non-existant file
+     *
+     */
+    #[@test, @expect('io.IOException')]
+    public function runningNonExistantFile() {
+      new Process(':FILE_DOES_NOT_EXIST:');
+    }
+
+    /**
+     * Test running a directory (System::tempDir() used as argument)
+     *
+     */
+    #[@test, @expect('io.IOException')]
+    public function runningDirectory() {
+      new Process(System::tempDir());
+    }
+  }
+?>
