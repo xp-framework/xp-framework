a033019ebd1ed42fa39c65090272c4f1b385e447
thekid <timm.friebe@gmail.com>
2008-03-30 13:18:49 +0000

    - QA: Rewrite apidoc
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@11987 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/lang/ClassNotFoundException.class.php
+++ skeleton/lang/ClassNotFoundException.class.php
@@ -5,9 +5,11 @@
  */
  
   /**
-   * Encapsulates ClassNotFoundException
+   * Indicates a class specified by a name cannot be found - that is,
+   * no classloader provides such a class.
    *
-   * @see Exception
+   * @see   xp://lang.IClassLoader#loadClass
+   * @see   xp://lang.XPClass#forName
    */
   class ClassNotFoundException extends XPException {
   
