52f4e71f5a1cd069c99434cd978af441b37f96bf
thekid <timm.friebe@gmail.com>
2008-04-13 11:15:46 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@12036 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/classes/net/xp_framework/website/doc/scriptlet/state/AbstractDocState.class.php
@@ -0,0 +1,67 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses(
+    'scriptlet.xml.workflow.AbstractState', 
+    'text.doclet.markup.MarkupBuilder',
+    'text.doclet.markup.DelegatingProcessor',
+    'io.File',
+    'io.FileUtil',
+    'util.PropertyManager'
+  );
+
+  /**
+   * Abstract base class for all classes reading user documentation
+   * from the file system.
+   *
+   * @purpose  State
+   */
+  abstract class AbstractDocState extends AbstractState {
+  
+    /**
+     * Returns which entry to display
+     *
+     * @param   scriptlet.xml.workflow.WorkflowScriptletRequest request
+     * @return  string entry name
+     */
+    protected function entryFor($request) {
+      sscanf($request->getQueryString(), '%[a-zA-Z_]/%[a-zA-Z_]', $base, $entry);
+      return $base.DIRECTORY_SEPARATOR.$entry;
+    }
+
+    /**
+     * Process this state.
+     *
+     * @param   scriptlet.xml.workflow.WorkflowScriptletRequest request
+     * @param   scriptlet.xml.XMLScriptletResponse response
+     */
+    public function process($request, $response) {
+      $entry= $this->entryFor($request);
+
+      // Read from storage (XXX: Make exchangeable)
+      try {
+        $text= FileUtil::getContents(new File(
+          PropertyManager::getInstance()->getProperties('storage')->readString('text', 'base'),
+          $entry.'.txt'
+        ));
+      } catch (FileNotFoundException $e) {
+        throw new HttpScriptletException('Entry "'.$entry.'" not found', HTTP_NOT_FOUND, $e);
+      }
+      
+      $builder= new MarkupBuilder();
+      
+      // Add <summary>...</summary>
+      $builder->registerProcessor('summary', newinstance('text.doclet.markup.DelegatingProcessor', array($builder->processors['default']), '{
+        public function tag() { return "summary"; }
+      }'));
+      
+      // Insert markup
+      $response->addFormresult(new Node('documentation', new PCData(
+        '<p>'.$builder->markupFor($text).'</p>'
+      )));
+    }
+  }
+?>
---
+++ ports/classes/net/xp_framework/website/doc/scriptlet/state/DocState.class.php
@@ -0,0 +1,17 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('net.xp_framework.website.doc.scriptlet.state.AbstractDocState');
+
+  /**
+   * Handles /xml/doc
+   *
+   * @purpose  State
+   */
+  class DocState extends AbstractDocState {
+
+  }
+?>
