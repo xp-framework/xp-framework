989bf57e5e417000e7932834833253545f928fc6
thekid <timm.friebe@gmail.com>
2008-05-01 09:39:39 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@12132 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/classes/net/xp_framework/website/dev/scriptlet/state/AboutRfcState.class.php
@@ -0,0 +1,66 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses(
+    'scriptlet.xml.workflow.AbstractState', 
+    'text.doclet.markup.MarkupBuilder',
+    'text.doclet.markup.DelegatingProcessor',
+    'io.File',
+    'io.FileUtil',
+    'util.PropertyManager'
+  );
+
+  /**
+   * About RFCs
+   *
+   * @purpose  State
+   */
+  class AboutRfcState extends AbstractState {
+  
+    /**
+     * Returns which entry to display
+     *
+     * @param   scriptlet.xml.workflow.WorkflowScriptletRequest request
+     * @return  string entry name
+     */
+    protected function entryFor($request) {
+      sscanf($request->getQueryString(), '%[a-zA-Z_]', $entry);
+      return $entry;
+    }
+
+    /**
+     * Process this state.
+     *
+     * @param   scriptlet.xml.workflow.WorkflowScriptletRequest request
+     * @param   scriptlet.xml.XMLScriptletResponse response
+     */
+    public function process($request, $response) {
+      $entry= $this->entryFor($request);
+
+      // Read from storage (XXX: Make exchangeable)
+      try {
+        $text= FileUtil::getContents(new File(
+          PropertyManager::getInstance()->getProperties('storage')->readString('text', 'base'),
+          $entry.'.txt'
+        ));
+      } catch (FileNotFoundException $e) {
+        throw new HttpScriptletException('Entry "'.$entry.'" not found', HTTP_NOT_FOUND, $e);
+      }
+      
+      $builder= new MarkupBuilder();
+      
+      // Add <summary>...</summary>
+      $builder->registerProcessor('summary', newinstance('text.doclet.markup.DelegatingProcessor', array($builder->processors['default']), '{
+        public function tag() { return "summary"; }
+      }'));
+      
+      // Insert markup
+      $response->addFormresult(new Node('documentation', new PCData(
+        '<p>'.$builder->markupFor($text).'</p>'
+      )));
+    }
+  }
+?>
