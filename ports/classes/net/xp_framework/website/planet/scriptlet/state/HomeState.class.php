fa33278253560694aea56bdd65ec894ce09982c1
thekid <timm.friebe@gmail.com>
2008-05-02 09:45:41 +0000

    - Select newest blog articles
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@12146 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/classes/net/xp_framework/website/planet/scriptlet/state/HomeState.class.php
+++ ports/classes/net/xp_framework/website/planet/scriptlet/state/HomeState.class.php
@@ -1,18 +1,61 @@
 <?php
 /* This class is part of the XP framework
  *
- * $Id: ViewNewsState.class.php 8971 2006-12-27 15:27:10 +0000 (Mi, 27 Dez 2006) friebe $ 
+ * $Id$
  */
 
-  uses(
-    'scriptlet.xml.workflow.AbstractState'
-  );
+  uses('net.xp_framework.website.news.scriptlet.AbstractNewsState');
 
   /**
-   * Handles /xml/news/view
+   * Handles /xml/home
    *
    * @purpose  State
    */
-  class HomeState extends AbstractState {
+  class HomeState extends AbstractNewsState {
+
+    /**
+     * Process this state.
+     *
+     * @param   scriptlet.xml.workflow.WorkflowScriptletRequest request
+     * @param   scriptlet.xml.XMLScriptletResponse response
+     */
+    public function process($request, $response) {
+
+      // Blog entries
+      $conn= ConnectionManager::getInstance()->getByHost('news', 0);
+      $n= $response->addFormResult(new Node('blog'));
+      $q= $conn->query('
+        select distinct 
+          entry.id as id,
+          entry.title as title,
+          entry.timestamp as date
+        from
+          serendipity_entries entry,
+          serendipity_entrycat matrix,
+          serendipity_category category
+        where
+          (category.parentid = %1$d or category.categoryid = %1$d)
+          and entry.id = matrix.entryid
+          and matrix.categoryid = category.categoryid
+          and entry.isdraft = "false"
+        order by
+          timestamp desc
+        limit 7',
+        self::MASTER_CATEGORY
+      );
+
+      while ($record= $q->next()) {
+        $e= $n->addChild(new Node('entry', NULL, array('id' => $record['id'], 'link' => $this->sanitizeHref($record['title']))));
+        $e->addChild(new Node('title', $record['title']));
+        $e->addChild(Node::fromObject(new Date($record['timestamp']), 'date'));
+        
+        foreach ($this->categoriesOfEntry($record['id']) as $row) {
+          $e->addChild(new Node('category', $row['category_name'], array(
+            'link'  => $this->sanitizeHref($row['category_name']),
+            'id'    => $row['categoryid']
+          )));
+        }
+      }
+    }
   }
 ?>
