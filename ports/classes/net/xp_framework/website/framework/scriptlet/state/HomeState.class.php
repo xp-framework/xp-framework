f96d4db5abccd42912648111e209210e0e38d7e5
thekid <timm.friebe@gmail.com>
2008-06-29 17:58:15 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@12357 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/classes/net/xp_framework/website/framework/scriptlet/state/HomeState.class.php
@@ -0,0 +1,17 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('scriptlet.xml.workflow.AbstractState');
+
+  /**
+   * Handles /xml/home
+   *
+   * @purpose  State
+   */
+  class HomeState extends AbstractState {
+
+  }
+?>
