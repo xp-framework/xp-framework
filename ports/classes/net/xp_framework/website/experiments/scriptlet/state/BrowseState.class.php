7bdaffac13bd9ee97d791dd1c5d09664aa75f4db
thekid <timm.friebe@gmail.com>
2008-06-29 10:28:39 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@12331 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/classes/net/xp_framework/website/experiments/scriptlet/state/BrowseState.class.php
@@ -0,0 +1,61 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses(
+    'scriptlet.xml.workflow.AbstractState', 
+    'util.PropertyManager',
+    'io.collections.FileCollection',
+    'io.collections.iterate.FilteredIOCollectionIterator',
+    'io.collections.iterate.NegationOfFilter',
+    'io.collections.iterate.AnyOfFilter',
+    'io.collections.iterate.NameMatchesFilter',
+    'util.MimeType'
+  );
+
+  /**
+   * Handles /xml/browse
+   *
+   * @purpose  State
+   */
+  class BrowseState extends AbstractState {
+
+    /**
+     * Process this state.
+     *
+     * @param   scriptlet.xml.workflow.WorkflowScriptletRequest request
+     * @param   scriptlet.xml.XMLScriptletResponse response
+     */
+    public function process($request, $response) {
+      $path= $request->getQueryString();
+
+      $prop= PropertyManager::getInstance()->getProperties('storage');
+      $base= new FileCollection($prop->readString('storage', 'base').DIRECTORY_SEPARATOR.strtr($path, array(
+        ','   => DIRECTORY_SEPARATOR, 
+        '..'  => ''
+      )));
+      $excludes= array();
+      foreach ($prop->readArray('storage', 'excludes') as $pattern) {
+        $excludes[]= new NameMatchesFilter('/'.$pattern.'/i');
+      }
+      
+      $i= new FilteredIOCollectionIterator($base, new NegationOfFilter(new AnyOfFilter($excludes)));
+      $n= $response->addFormResult(new Node('list', NULL, array(
+        'path' => $path
+      )));
+      foreach ($i as $element) {
+        if ($element instanceof IOCollection) {
+          $e= $n->addChild(new Node('collection'));
+        } else {
+          $e= $n->addChild(new Node('element'));
+          $e->addChild(new Node('mime', MimeType::getByFilename($element->getUri())));
+        }
+
+        $e->addChild(new Node('name', basename($element->getUri())));
+        $e->addChild(Node::fromObject($element->lastModified(), 'modified'));
+      }
+    }
+  }
+?>
