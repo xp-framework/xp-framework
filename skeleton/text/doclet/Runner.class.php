b27454363f12da0c5f08fcdc3c4ae7a890e49b70
thekid <timm.friebe@gmail.com>
2008-09-20 09:00:22 +0000

    - Initial release # Used by doclet runner
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@12469 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/text/doclet/Runner.class.php
@@ -0,0 +1,37 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  $package= 'text.doclet';
+
+  uses('text.doclet.RootDoc', 'util.cmd.ParamString');
+
+  /**
+   * Command line doclet runner
+   *
+   * @purpose  RootDoc CLI adaptor
+   */
+  class text·doclet·Runner extends Object {
+  
+    /**
+     * Runner method
+     *
+     */
+    public static function main(array $args) {
+      if (sizeof($args) < 1) {
+        Console::$err->writeLine('*** No doclet classname given');
+        return -1;
+      }
+
+      $class= XPClass::forName($args[0]);
+      if (!$class->isSubclassOf('text.doclet.Doclet')) {
+        Console::$err->writeLine('*** ', $class, ' is not a doclet');
+        return -2;
+      }
+
+      RootDoc::start($class->newInstance(), new ParamString($args));
+    }
+  }
+?>
