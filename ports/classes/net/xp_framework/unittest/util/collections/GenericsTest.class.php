67e13a1d7289f29f714f444a33097957ce31b661
thekid <timm.friebe@gmail.com>
2008-09-28 10:40:24 +0000

    - Use fully qualified class names in create() to be forward-compatible
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@12535 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/classes/net/xp_framework/unittest/util/collections/GenericsTest.class.php
+++ ports/classes/net/xp_framework/unittest/util/collections/GenericsTest.class.php
@@ -32,319 +32,319 @@
      */
     #[@test, @expect('lang.IllegalArgumentException')]
     public function nonGenericPassedToCreate() {
-      create('new Object<String>');
+      create('new lang.Object<lang.types.String>');
     }
   
     /**
-     * Tests HashTable<String, String>
+     * Tests HashTable<lang.types.String, lang.types.String>
      *
      */
     #[@test]
     public function stringStringHash() {
-      create('new HashTable<String, String>')->put('hello', new String('World'));
+      create('new util.collections.HashTable<lang.types.String, lang.types.String>')->put('hello', new String('World'));
     }
 
     /**
-     * Tests HashTable<String, String>
+     * Tests HashTable<lang.types.String, lang.types.String>
      *
      */
     #[@test]
     public function getFromStringStringHash() {
-      with ($h= create('new HashTable<String, String>')); {
+      with ($h= create('new util.collections.HashTable<lang.types.String, lang.types.String>')); {
         $h->put('hello', new String('World'));
         $this->assertEquals(new String('World'), $h->get('hello'));
       }
     }
 
     /**
-     * Tests HashTable<String, String>
+     * Tests HashTable<lang.types.String, lang.types.String>
      *
      */
     #[@test]
     public function removeFromStringStringHash() {
-      with ($h= create('new HashTable<String, String>')); {
+      with ($h= create('new util.collections.HashTable<lang.types.String, lang.types.String>')); {
         $h->put('hello', new String('World'));
         $this->assertEquals(new String('World'), $h->remove('hello'));
       }
     }
 
     /**
-     * Tests HashTable<String, String>
+     * Tests HashTable<lang.types.String, lang.types.String>
      *
      */
     #[@test]
     public function testStringStringHash() {
-      with ($h= create('new HashTable<String, String>')); {
+      with ($h= create('new util.collections.HashTable<lang.types.String, lang.types.String>')); {
         $h->put('hello', new String('World'));
         $this->assertTrue($h->containsKey('hello'));
       }
     }
 
     /**
-     * Tests HashTable<String, String>
+     * Tests HashTable<lang.types.String, lang.types.String>
      *
      */
     #[@test, @expect('lang.IllegalArgumentException')]
     public function stringStringHashPutIllegalValue() {
-      create('new HashTable<String, String>')->put('hello', new Integer(1));
+      create('new util.collections.HashTable<lang.types.String, lang.types.String>')->put('hello', new Integer(1));
     }
 
     /**
-     * Tests HashTable<String, String>
+     * Tests HashTable<lang.types.String, lang.types.String>
      *
      */
     #[@test, @expect('lang.IllegalArgumentException')]
     public function stringStringHashGetIllegalValue() {
-      create('new HashTable<String, String>')->get(new Integer(1));
+      create('new util.collections.HashTable<lang.types.String, lang.types.String>')->get(new Integer(1));
     }
 
     /**
-     * Tests HashTable<String, String>
+     * Tests HashTable<lang.types.String, lang.types.String>
      *
      */
     #[@test, @expect('lang.IllegalArgumentException')]
     public function stringStringHashRemoveIllegalValue() {
-      create('new HashTable<String, String>')->remove(new Integer(1));
+      create('new util.collections.HashTable<lang.types.String, lang.types.String>')->remove(new Integer(1));
     }
 
     /**
-     * Tests HashTable<String, String>
+     * Tests HashTable<lang.types.String, lang.types.String>
      *
      */
     #[@test, @expect('lang.IllegalArgumentException')]
     public function stringStringHashContainsKeyIllegalValue() {
-      create('new HashTable<String, String>')->containsKey(new Integer(1));
+      create('new util.collections.HashTable<lang.types.String, lang.types.String>')->containsKey(new Integer(1));
     }
 
     /**
-     * Tests HashTable<String, String>
+     * Tests HashTable<lang.types.String, lang.types.String>
      *
      */
     #[@test, @expect('lang.IllegalArgumentException')]
     public function stringStringHashContainsValueIllegalValue() {
-      create('new HashTable<String, String>')->containsValue(new Integer(1));
+      create('new util.collections.HashTable<lang.types.String, lang.types.String>')->containsValue(new Integer(1));
     }
 
     /**
-     * Tests HashTable<String, String>
+     * Tests HashTable<lang.types.String, lang.types.String>
      *
      */
     #[@test, @expect('lang.IllegalArgumentException')]
     public function stringStringHashIllegalKey() {
-      create('new HashTable<String, String>')->put(1, new String('World'));
+      create('new util.collections.HashTable<lang.types.String, lang.types.String>')->put(1, new String('World'));
     }
 
     /**
-     * Tests Vector<String>
+     * Tests Vector<lang.types.String>
      *
      */
     #[@test]
     public function stringVector() {
-      create('new Vector<String>')->add(new String('Hi'));
+      create('new util.collections.Vector<lang.types.String>')->add(new String('Hi'));
     }
 
     /**
-     * Tests Vector<String>
+     * Tests Vector<lang.types.String>
      *
      */
     #[@test]
     public function createStringVector() {
       $this->assertEquals(
         new String('one'), 
-        create('new Vector<String>', array(new String('one')))->get(0)
+        create('new util.collections.Vector<lang.types.String>', array(new String('one')))->get(0)
       );
     }
 
     /**
-     * Tests Vector<String>
+     * Tests Vector<lang.types.String>
      *
      */
     #[@test, @expect('lang.IllegalArgumentException')]
     public function stringVectorAddIllegalValue() {
-      create('new Vector<String>')->add(new Integer(1));
+      create('new util.collections.Vector<lang.types.String>')->add(new Integer(1));
     }
 
     /**
-     * Tests Vector<String>
+     * Tests Vector<lang.types.String>
      *
      */
     #[@test, @expect('lang.IllegalArgumentException')]
     public function stringVectorSetIllegalValue() {
-      create('new Vector<String>', array(new String('')))->set(0, new Integer(1));
+      create('new util.collections.Vector<lang.types.String>', array(new String('')))->set(0, new Integer(1));
     }
 
     /**
-     * Tests Vector<String>
+     * Tests Vector<lang.types.String>
      *
      */
     #[@test, @expect('lang.IllegalArgumentException')]
     public function stringVectorContainsIllegalValue() {
-      create('new Vector<String>')->contains(new Integer(1));
+      create('new util.collections.Vector<lang.types.String>')->contains(new Integer(1));
     }
 
     /**
-     * Tests Vector<String>
+     * Tests Vector<lang.types.String>
      *
      */
     #[@test, @expect('lang.IllegalArgumentException')]
     public function stringVectorRemoveIllegalValue() {
-      create('new Vector<String>')->remove(new Integer(1));
+      create('new util.collections.Vector<lang.types.String>')->remove(new Integer(1));
     }
 
     /**
-     * Tests Vector<String>
+     * Tests Vector<lang.types.String>
      *
      */
     #[@test, @expect('lang.IllegalArgumentException')]
     public function createStringVectorWithIllegalValue() {
-      create('new Vector<String>', array(new Integer(1)));
+      create('new util.collections.Vector<lang.types.String>', array(new Integer(1)));
     }
 
     /**
-     * Tests Stack<String>
+     * Tests Stack<lang.types.String>
      *
      */
     #[@test]
     public function stringStack() {
-      create('new util.collections.Stack<String>')->push(new String('One'));
+      create('new util.collections.Stack<lang.types.String>')->push(new String('One'));
     }
 
     /**
-     * Tests Stack<String>
+     * Tests Stack<lang.types.String>
      *
      */
     #[@test, @expect('lang.IllegalArgumentException')]
     public function stringStackPushIllegalValue() {
-      create('new util.collections.Stack<String>')->push(new Integer(1));
+      create('new util.collections.Stack<lang.types.String>')->push(new Integer(1));
     }
 
     /**
-     * Tests Stack<String>
+     * Tests Stack<lang.types.String>
      *
      */
     #[@test, @expect('lang.IllegalArgumentException')]
     public function stringStackSearchIllegalValue() {
-      create('new util.collections.Stack<String>')->search(new Integer(1));
+      create('new util.collections.Stack<lang.types.String>')->search(new Integer(1));
     }
 
     /**
-     * Tests Queue<String>
+     * Tests Queue<lang.types.String>
      *
      */
     #[@test]
     public function stringQueue() {
-      create('new util.collections.Queue<String>')->put(new String('One'));
+      create('new util.collections.Queue<lang.types.String>')->put(new String('One'));
     }
 
     /**
-     * Tests Queue<String>
+     * Tests Queue<lang.types.String>
      *
      */
     #[@test, @expect('lang.IllegalArgumentException')]
     public function stringQueuePutIllegalValue() {
-      create('new util.collections.Queue<String>')->put(new Integer(1));
+      create('new util.collections.Queue<lang.types.String>')->put(new Integer(1));
     }
 
     /**
-     * Tests Queue<String>
+     * Tests Queue<lang.types.String>
      *
      */
     #[@test, @expect('lang.IllegalArgumentException')]
     public function stringQueueSearchIllegalValue() {
-      create('new util.collections.Queue<String>')->search(new Integer(1));
+      create('new util.collections.Queue<lang.types.String>')->search(new Integer(1));
     }
 
     /**
-     * Tests Queue<String>
+     * Tests Queue<lang.types.String>
      *
      */
     #[@test, @expect('lang.IllegalArgumentException')]
     public function stringQueueRemoveIllegalValue() {
-      create('new util.collections.Queue<String>')->remove(new Integer(1));
+      create('new util.collections.Queue<lang.types.String>')->remove(new Integer(1));
     }
 
     /**
-     * Tests LRUBuffer<String>
+     * Tests LRUBuffer<lang.types.String>
      *
      */
     #[@test]
     public function stringLRUBuffer() {
-      create('new util.collections.LRUBuffer<String>', 1)->add(new String('One'));
+      create('new util.collections.LRUBuffer<lang.types.String>', 1)->add(new String('One'));
     }
 
     /**
-     * Tests LRUBuffer<String>
+     * Tests LRUBuffer<lang.types.String>
      *
      */
     #[@test, @expect('lang.IllegalArgumentException')]
     public function stringLRUBufferAddIllegalValue() {
-      create('new util.collections.LRUBuffer<String>', 1)->add(new Integer(1));
+      create('new util.collections.LRUBuffer<lang.types.String>', 1)->add(new Integer(1));
     }
 
     /**
-     * Tests LRUBuffer<String>
+     * Tests LRUBuffer<lang.types.String>
      *
      */
     #[@test, @expect('lang.IllegalArgumentException')]
     public function stringLRUBufferUpdateIllegalValue() {
-      create('new util.collections.LRUBuffer<String>', 1)->update(new Integer(1));
+      create('new util.collections.LRUBuffer<lang.types.String>', 1)->update(new Integer(1));
     }
 
     /**
-     * Tests HashSet<String>
+     * Tests HashSet<lang.types.String>
      *
      */
     #[@test]
     public function stringHashSet() {
-      create('new util.collections.HashSet<String>')->add(new String('One'));
+      create('new util.collections.HashSet<lang.types.String>')->add(new String('One'));
     }
 
     /**
-     * Tests HashSet<String>
+     * Tests HashSet<lang.types.String>
      *
      */
     #[@test, @expect('lang.IllegalArgumentException')]
     public function stringHashSetAddIllegalValue() {
-      create('new util.collections.HashSet<String>')->add(new Integer(1));
+      create('new util.collections.HashSet<lang.types.String>')->add(new Integer(1));
     }
 
     /**
-     * Tests HashSet<String>
+     * Tests HashSet<lang.types.String>
      *
      */
     #[@test, @expect('lang.IllegalArgumentException')]
     public function stringHashSetContainsIllegalValue() {
-      create('new util.collections.HashSet<String>')->contains(new Integer(1));
+      create('new util.collections.HashSet<lang.types.String>')->contains(new Integer(1));
     }
 
     /**
-     * Tests HashSet<String>
+     * Tests HashSet<lang.types.String>
      *
      */
     #[@test, @expect('lang.IllegalArgumentException')]
     public function stringHashSetRemoveIllegalValue() {
-      create('new util.collections.HashSet<String>')->remove(new Integer(1));
+      create('new util.collections.HashSet<lang.types.String>')->remove(new Integer(1));
     }
 
     /**
-     * Tests HashSet<String>
+     * Tests HashSet<lang.types.String>
      *
      */
     #[@test, @expect('lang.IllegalArgumentException')]
     public function stringHashSetAddAllIllegalValue() {
-      create('new util.collections.HashSet<String>')->addAll(array(
+      create('new util.collections.HashSet<lang.types.String>')->addAll(array(
         new String('HELLO'),    // Still OK
         new Integer(2),         // Blam
       ));
     }
 
     /**
-     * Tests HashSet<String>
+     * Tests HashSet<lang.types.String>
      *
      */
     #[@test]
     public function stringHashSetUnchangedAferAddAllIllegalValue() {
-      $h= create('new util.collections.HashSet<String>');
+      $h= create('new util.collections.HashSet<lang.types.String>');
       try {
         $h->addAll(array(new String('HELLO'), new Integer(2)));
       } catch (IllegalArgumentException $expected) {
