ec99dc5cc9f0a04029ab53f5eae3fd782a87ac35
thekid <timm.friebe@gmail.com>
2008-10-08 08:31:57 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@12581 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/peer/SocketOutputStream.class.php
@@ -0,0 +1,63 @@
+<?php
+/* This file is part of the XP framework's experiments
+ *
+ * $Id$
+ */
+
+  uses('io.streams.OutputStream', 'peer.Socket');
+
+  /**
+   * OutputStream that reads from a socket
+   *
+   * @purpose  OutputStream implementation
+   */
+  class SocketOutputStream extends Object implements OutputStream {
+    protected
+      $socket= NULL;
+    
+    /**
+     * Constructor
+     *
+     * @param   peer.Socket socket
+     */
+    public function __construct(Socket $socket) {
+      $this->socket= $socket;
+      $this->socket->isConnected() || $this->socket->connect();
+    }
+
+
+    /**
+     * Write a string
+     *
+     * @param   mixed arg
+     */
+    public function write($arg) {
+      $this->socket->write($arg);
+    }
+
+    /**
+     * Flush this buffer
+     *
+     */
+    public function flush() {
+      // NOOP, sockets cannot be flushed
+    }
+
+    /**
+     * Close this buffer
+     *
+     */
+    public function close() {
+      $this->socket->close();
+    }
+
+    /**
+     * Creates a string representation of this output strean
+     *
+     * @return  string
+     */
+    public function toString() {
+      return $this->getClassName().'<'.$this->socket->toString().'>';
+    }
+  }
+?>
