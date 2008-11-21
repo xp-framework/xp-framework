99acec09b55c80a1b57f14df37d36f294cb1dc01
thekid <timm.friebe@gmail.com>
2008-11-21 16:47:31 +0000

    - Initial release # Empty class, fixes "fatal error: cannot find webservices.wddx.WddxFaultException"
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@12654 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/webservices/wddx/WddxFaultException.class.php
@@ -0,0 +1,15 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * WDDX Fault
+   *
+   * @purpose  Exception
+   */
+  class WddxFaultException extends XPException {
+    
+  }
+?>
