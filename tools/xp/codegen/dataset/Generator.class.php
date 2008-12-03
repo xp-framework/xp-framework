de4c3a671a70864ad49a9c1abae4086420a98f33
Christian Gellweiler <gelli@developer.xp-framework.net>
2008-12-03 13:58:00 +0000

    - Make prefixes work again
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@12668 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- tools/xp/codegen/dataset/Generator.class.php
+++ tools/xp/codegen/dataset/Generator.class.php
@@ -1,17 +1,17 @@
 <?php
 /* This class is part of the XP framework
  *
- * $Id$ 
+ * $Id$
  */
 
   $package= 'xp.codegen.dataset';
-  
+
   uses(
     'xp.codegen.AbstractGenerator',
-    'rdbms.DSN', 
+    'rdbms.DSN',
     'rdbms.DBTable',
     'rdbms.DriverManager',
-    'rdbms.util.DBConstraintXmlGenerator', 
+    'rdbms.util.DBConstraintXmlGenerator',
     'rdbms.util.DBXMLNamingContext',
     'rdbms.util.DBXmlGenerator',
     'xml.DomXSLProcessor'
@@ -45,21 +45,21 @@
     const
       CONSTRAINT_FILE_NAME= '__Constraints';
 
-    protected static 
+    protected static
       $adapters = array();
-    
+
     protected
       $adapter  = NULL,
       $processor= NULL,
       $package  = '';
-    
+
     static function __static() {
       self::$adapters['mysql']= XPClass::forName('rdbms.mysql.MySQLDBAdapter');
       self::$adapters['sqlite']= XPClass::forName('rdbms.sqlite.SQLiteDBAdapter');
       self::$adapters['pgsql']= XPClass::forName('rdbms.pgsql.PostgreSQLDBAdapter');
       self::$adapters['sybase']= XPClass::forName('rdbms.sybase.SybaseDBAdapter');
     }
-    
+
     /**
      * Constructor
      *
@@ -74,12 +74,21 @@
       $this->package= $args->value('package', 'p', 'db');
       $this->host= $args->value('host', 'h', $dsn->getHost());
 
+      $this->prefix= $args->value('prefix', 'pf', NULL);
+      $this->ptargets= explode('|', $args->value('ptargets', 'pi', ''));
+      $this->pexclude= explode('|', $args->value('pexclude', 'pe', ''));
+
       // Setup generator
       $this->processor= new DomXSLProcessor();
       $this->processor->setXSLBuf($this->getClass()->getPackage()->getResource($args->value('lang', 'l', 'xp5.php').'.xsl'));
       $this->processor->setParam('package', $this->package);
+
+      if ($this->prefix) {
+        $this->processor->setParam('prefix', $this->prefix);
+        $this->processor->setParam($this->pexclude ? 'exprefix' : 'incprefix', $this->ptargets);
+      }
     }
-    
+
     /**
      * Connect the database
      *
@@ -96,7 +105,7 @@
     #[@target(depends= 'connect')]
     public function fetchTables() {
       return DBTable::getByDatabase(
-        $this->adapter, 
+        $this->adapter,
         $this->adapter->conn->dsn->getDatabase()
       );
     }
@@ -108,7 +117,7 @@
     #[@target(depends= 'connect')]
     public function fetchConstraints() {
       return DBConstraintXmlGenerator::createFromDatabase(
-        $this->adapter, 
+        $this->adapter,
         $this->adapter->conn->dsn->getDatabase()
       )->getTree();
     }
@@ -121,22 +130,32 @@
     public function generateTableXml($tables, $constraints, $storage) {
       $xml= array();
       foreach ($tables as $table) {
-        $gen= DBXmlGenerator::createFromTable(
-          $table, 
-          $this->host,          
-          $this->adapter->conn->dsn->getDatabase()
-        )->getTree();
-        
+
         // Calculate classname
         $className= ucfirst($table->name);
-        
+        if (isset($this->prefix)) {
+          switch (1) {
+            case (FALSE == $this->ptargets):
+            case (in_array($table->name, $this->ptargets) && FALSE == $this->pexclude):
+            case (!in_array($table->name, $this->ptargets) && TRUE == $this->pexclude):
+              $className= $this->prefix.$className;
+              break;
+          }
+        }
+
+        $gen= DBXmlGenerator::createFromTable(
+          $table,
+          $this->host,
+          $this->adapter->conn->dsn->getDatabase()
+        )->getTree();
+
         // Add extra information
         with ($node= $gen->root->children[0]); {
           $node->setAttribute('dbtype', $this->adapter->conn->dsn->getDriver());
           $node->setAttribute('class', $className);
           $node->setAttribute('package', $this->package);
         }
-        
+
         $xml[]= $storage->write($className, $gen->getSource(INDENT_DEFAULT));
       }
       $storage->write(self::CONSTRAINT_FILE_NAME, $constraints->getSource(INDENT_DEFAULT));
@@ -156,7 +175,7 @@
       foreach ($tables as $stored) {
         $this->processor->setXMLBuf($stored->data());
         $this->processor->run();
-        
+
         $output->append($dir.$stored->name().xp::CLASS_FILE_EXT, $this->processor->output());
       }
     }
