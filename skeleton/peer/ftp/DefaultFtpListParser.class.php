e169a4b52070fd880efa94048a14b5afe06f87ab
thekid <timm.friebe@gmail.com>
2009-01-22 13:20:41 +0000

    - Add a reference date to entryFrom() - of omitted, defaults to Date::now() - Fix date calculation for compact dates ("Apr 4 20:16" instead of "Apr 4 2009"). # Compact format is used when the file time is within six months from the current date # in either direction. # Verified by net.xp_framework.unittest.peer.ftp.DefaultFtpListParserTest::compactDate
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@12733 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/peer/ftp/DefaultFtpListParser.class.php
+++ skeleton/peer/ftp/DefaultFtpListParser.class.php
@@ -21,9 +21,10 @@
      * @param   string raw a single line
      * @param   peer.ftp.FtpConnection connection
      * @param   string base default "/"
+     * @param   util.Date ref default NULL
      * @return  peer.ftp.FtpEntry
      */
-    public function entryFrom($raw, FtpConnection $conn= NULL, $base= '/') {
+    public function entryFrom($raw, FtpConnection $conn= NULL, $base= '/', Date $ref= NULL) {
       sscanf(
         $raw, 
         '%s %d %s %s %d %s %d %[^ ] %[^$]',
@@ -49,15 +50,23 @@
       } else {
         $e= new FtpFile($filename, $conn);
       }
-
-      $d= new Date($month.' '.$day.' '.(strstr($date, ':') ? date('Y').' '.$date : $date));
-
-      // Check for "recent" file which are specified "HH:MM" instead
-      // of year for the last 6 month (as specified in coreutils/src/ls.c)
+      
+      // If the entry contains a timestamp, the year is omitted, "Apr 4 20:16" 
+      // instead of "Apr 4 2009". This compact format is used when the file 
+      // time is within six months* from the current date, in either direction!
+      //
+      // *] #define SIXMONTHS       ((365 / 2) * 86400) := 15724800
+      //    See http://svn.freebsd.org/base/projects/releng_7_xen/bin/ls/print.c
       if (strstr($date, ':')) {
-        $now= Date::now();
-        if ($d->getMonth() > $now->getMonth()) $d= DateUtil::addMonths($d, -12);
+        $ref || $ref= Date::now();
+        $d= new Date($month.' '.$day.' '.$ref->getYear().' '.$date);
+        if ($d->getTime() - $ref->getTime() > 15724800) {
+          $d= DateUtil::addMonths($d, -12);
+        }
+      } else {
+        $d= new Date($month.' '.$day.' '.$date);
       }
+
         
       try {
         $e->setPermissions(substr($permissions, 1));
