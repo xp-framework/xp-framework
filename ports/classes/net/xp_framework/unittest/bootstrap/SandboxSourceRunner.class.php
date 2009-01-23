d8cc2111d1efc63f878e63208c6a3d6248ba1419
thekid <timm.friebe@gmail.com>
2009-01-23 02:49:31 +0000

    - Quote include path
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@12755 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/classes/net/xp_framework/unittest/bootstrap/SandboxSourceRunner.class.php
+++ ports/classes/net/xp_framework/unittest/bootstrap/SandboxSourceRunner.class.php
@@ -28,7 +28,7 @@
      */
     public function __construct($executable) {
       $this->setExecutable(preg_replace('#^/cygdrive/(\w)/#', '$1:/', $executable));
-      $this->setSetting('include_path', ini_get('include_path'));
+      $this->setSetting('include_path', '"'.ini_get('include_path').'"');
     }
 
     /**
