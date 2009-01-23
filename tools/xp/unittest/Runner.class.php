dafa11dc85ab16b7c016854a64f34b654d6d0d13
thekid <timm.friebe@gmail.com>
2009-01-23 00:05:40 +0000

    - Generate usage from apidoc instead of hardcoding it - Warn about missing argument to -cp - Add -? option to show usage (unittest without args also shows it) - Add -e option to evaluate assertions directly from the command line - Pass arguments if available (e.g. in .ini-file) - Only run testsuite if there are tests inside - Warn about not finding tests in one of the source classes but continue anyways - QA: Update usage
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@12751 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- tools/xp/unittest/Runner.class.php
+++ tools/xp/unittest/Runner.class.php
@@ -12,6 +12,7 @@
     'xp.unittest.sources.PropertySource',
     'xp.unittest.sources.ClassSource',
     'xp.unittest.sources.ClassFileSource',
+    'xp.unittest.sources.EvaluationSource',
     'io.File',
     'unittest.TestSuite',
     'util.Properties',
@@ -21,18 +22,68 @@
 
   /**
    * Unittest command
+   * ~~~~~~~~~~~~~~~~
+   *
+   * Usage:
+   * <pre>
+   *   unittest [options] test [test [test...]]
+   * </pre>
+   *
+   * Options is one of:
+   * <ul>
+   *   <li>-v : Be verbose</li>
+   *   <li>-cp: Add classpath elements</li>
+   * </ul>
+   * Tests can one or more of:
+   * <ul>
+   *   <li>{tests}.ini: A configuration file</li>
+   *   <li>{Test}.class.php: A class file</li>
+   *   <li>{test.class.Name}: A fully qualified class name</li>
+   *   <li>-e {test method sourcecode}: Evaluate source</li>
+   * </ul>
    *
    * @purpose  Tool
    */
   class xp·unittest·Runner extends Object {
 
     /**
-     * Displays usage and exists
+     * Converts api-doc "markup" to plain text w/ ASCII "art"
+     *
+     * @param   string markup
+     * @return  string text
+     */
+    protected static function textOf($markup) {
+      $line= str_repeat('=', 72);
+      return strip_tags(preg_replace(array(
+        '#<pre>#', '#</pre>#', '#<li>#',
+      ), array(
+        $line, $line, '* ',
+      ), trim($markup)));
+    }
+
+    /**
+     * Displays usage and exits
      *
      */
     protected static function usage() {
-      Console::$err->writeLine('*** Usage: unittest [-v] Test.class.php [tests.ini [test.class.Name]]');
+      Console::$err->writeLine(self::textOf(XPClass::forName(xp::nameOf(__CLASS__))->getComment()));
       exit(1);
+    }
+
+    /**
+     * Gets an argument
+     *
+     * @param   string[] args
+     * @param   int offset
+     * @param   string option
+     * @return  string
+     * @throws  lang.IllegalArgumentException if no argument exists by this offset
+     */
+    protected static function arg($args, $offset, $option) {
+      if (!isset($args[$offset])) {
+        throw new IllegalArgumentException('Option -'.$option.' requires an argument');
+      }
+      return $args[$offset];
     }
 
     /**
@@ -43,24 +94,37 @@
     public static function main(array $args) {
       if (!$args) self::usage();
 
-      $sources= new Vector();
-
       // Parse arguments
+      $sources= new Vector();
       $verbose= FALSE;
-      for ($i= 0, $s= sizeof($args); $i < $s; $i++) {
-        if ('-v' === $args[$i]) {
-          $verbose= TRUE;
-        } else if ('-cp' === $args[$i]) {
-          foreach (explode(PATH_SEPARATOR, $args[++$i]) as $path) {
-            ClassLoader::getDefault()->registerPath($path);
+      try {
+        for ($i= 0, $s= sizeof($args); $i < $s; $i++) {
+          if ('-v' === $args[$i]) {
+            $verbose= TRUE;
+          } else if ('-cp' === $args[$i]) {
+            foreach (explode(PATH_SEPARATOR, self::arg($args, ++$i, 'cp')) as $path) {
+              ClassLoader::getDefault()->registerPath($path);
+            }
+          } else if ('-e' === $args[$i]) {
+            $sources->add(new EvaluationSource(self::arg($args, ++$i, 'e')));
+          } else if ('-?' === $args[$i]) {
+            self::usage();
+          } else if (strstr($args[$i], '.ini')) {
+            $sources->add(new PropertySource(new Properties($args[$i])));
+          } else if (strstr($args[$i], xp::CLASS_FILE_EXT)) {
+            $sources->add(new ClassFileSource(new File($args[$i])));
+          } else {
+            $sources->add(new ClassSource(XPClass::forName($args[$i])));
           }
-        } else if (strstr($args[$i], '.ini')) {
-          $sources->add(new PropertySource(new Properties($args[$i])));
-        } else if (strstr($args[$i], '.class.php')) {
-          $sources->add(new ClassFileSource(new File($args[$i])));
-        } else {
-          $sources->add(new ClassSource(XPClass::forName($args[$i])));
         }
+      } catch (IllegalArgumentException $e) {
+        Console::$err->writeLine('*** ', $e->getMessage());
+        exit(1);
+      }
+      
+      if ($sources->isEmpty()) {
+        Console::$err->writeLine('*** No tests specified');
+        exit(1);
       }
       
       // Setup suite
@@ -76,19 +140,19 @@
         $classes= $source->testClasses();
         foreach ($classes->keys() as $class) {
           try {
-            $suite->addTestClass($class);
+            $suite->addTestClass($class, $classes[$class]->values);
           } catch (NoSuchElementException $e) {
             Console::writeLine('*** Warning: ', $e->getMessage());
-            return;
+            continue;
           } catch (IllegalArgumentException $e) {
             Console::writeLine('*** Error: ', $e->getMessage());
             return;
           }
         }
       }
-
+      
       // Run it!
-      $suite->run();
+      $suite->numTests() && $suite->run();
     }    
   }
 ?>
