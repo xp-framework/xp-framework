ba132f566eeb73468c2a578b2bdb0939d13a66d3
thekid <timm.friebe@gmail.com>
2009-01-25 20:00:33 +0000

    - Add content from WEB-INF
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@12781 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/classes/net/xp_framework/website/projects/scriptlet/state/ProjectState.class.php
+++ ports/classes/net/xp_framework/website/projects/scriptlet/state/ProjectState.class.php
@@ -8,7 +8,10 @@
     'scriptlet.xml.workflow.AbstractState', 
     'io.Folder', 
     'io.File',
+    'io.FileUtil',
     'xml.Tree',
+    'text.doclet.markup.MarkupBuilder',
+    'text.doclet.markup.DelegatingProcessor',
     'util.PropertyManager'
   );
 
@@ -31,16 +34,39 @@
 
       // Read from storage (XXX: Make exchangeable)
       $base= PropertyManager::getInstance()->getProperties('storage')->readString('projects', 'base');
-      $metaInf= new Folder($base.DIRECTORY_SEPARATOR.$category.DIRECTORY_SEPARATOR.$project.DIRECTORY_SEPARATOR.'META-INF');
-      if (!$metaInf->exists()) {
+      $projectDir= new Folder($base.DIRECTORY_SEPARATOR.$category.DIRECTORY_SEPARATOR.$project.DIRECTORY_SEPARATOR);
+      if (!$projectDir->exists()) {
         throw new HttpScriptletException('Project "'.$projectId.'" not found', HTTP_NOT_FOUND);
       }
-      
+
+      // Project information      
       with ($project= $response->addFormResult(new Node('project', NULL, array('id' => $projectId)))); {
-        foreach (Tree::fromFile(new File($metaInf, 'project.xml'))->root->children as $child) {
+        foreach (Tree::fromFile(new File($projectDir, 'META-INF/project.xml'))->root->children as $child) {
           $project->addChild($child);
         }
       }
+      
+      // Site
+      try {
+        $text= FileUtil::getContents(new File($projectDir, 'WEB-INF/overview.txt'));
+      } catch (FileNotFoundException $e) {
+        throw new HttpScriptletException('Entry "'.$entry.'" not found', HTTP_NOT_FOUND, $e);
+      }
+
+      
+      $builder= new MarkupBuilder();
+
+      // Add <summary>...</summary>
+      $builder->registerProcessor('summary', newinstance('text.doclet.markup.DelegatingProcessor', array($builder->processors['default']), '{
+        public function tag() { return "summary"; }
+      }'));
+      $builder->registerProcessor('item', newinstance('text.doclet.markup.DelegatingProcessor', array($builder->processors['default']), '{
+        public function tag() { return "item"; }
+      }'));
+      
+      $response->addFormResult(new Node('content', new PCData(
+        '<p>'.$builder->markupFor($text).'</p>'
+      )));
     }
   }
 ?>
