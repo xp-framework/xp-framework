c668620baec2e493deb1dd5aafcf1d3272be0430
thekid <timm.friebe@gmail.com>
2009-02-05 20:50:08 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@12825 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/classes/net/xp_framework/unittest/io/streams/FileOutputStreamTest.class.php
@@ -0,0 +1,119 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses(
+    'unittest.TestCase',
+    'io.streams.FileOutputStream',
+    'io.FileUtil',
+    'io.TempFile'
+  );
+
+  /**
+   * TestCase
+   *
+   * @see      xp://io.streams.FileOutputStream
+   */
+  class FileOutputStreamTest extends TestCase {
+    protected 
+      $file = NULL;
+  
+    /**
+     * Sets up test case - creates temporary file
+     *
+     */
+    public function setUp() {
+      try {
+        $this->file= new TempFile();
+        FileUtil::setContents($this->file, 'Created by FileOutputStreamTest');
+      } catch (IOException $e) {
+        throw new PrerequisitesNotMetError('Cannot write temporary file', $e, array($this->file));
+      }
+    }
+    
+    /**
+     * Tear down this test case - removes temporary file
+     *
+     */
+    public function tearDown() {
+      try {
+        $this->file->isOpen() && $this->file->close();
+        $this->file->unlink();
+      } catch (IOException $ignored) {
+        // Can't really do anything about it...
+      }
+    }
+    
+    /**
+     * Test write() method
+     *
+     */
+    #[@test]
+    public function writing() {
+      with ($stream= new FileOutputStream($this->file), $buffer= 'Created by '.$this->name); {
+        $stream->write($buffer);
+        $this->assertEquals($buffer, FileUtil::getContents($this->file));
+      }
+    }
+
+    /**
+     * Test write() method
+     *
+     */
+    #[@test]
+    public function appending() {
+      with ($stream= new FileOutputStream($this->file, TRUE)); {
+        $stream->write('!');
+        $this->assertEquals('Created by FileOutputStreamTest!', FileUtil::getContents($this->file));
+      }
+    }
+
+    /**
+     * Test file remains open when FileOutputStream instance is deleted
+     *
+     */
+    #[@test]
+    public function delete() {
+      with ($stream= new FileOutputStream($this->file)); {
+        $this->assertTrue($this->file->isOpen());
+        delete($stream);
+        $this->assertTrue($this->file->isOpen());
+      }
+    }
+
+    /**
+     * Test opening a file output stream with a non-existant file name
+     *
+     */
+    #[@test, @expect('io.IOException')]
+    public function nonExistantFile() {
+      new FileOutputStream('::NON-EXISTANT::');
+    }
+
+    /**
+     * Test writing after stream has been closed
+     *
+     */
+    #[@test, @expect('io.IOException')]
+    public function writingAfterClose() {
+      with ($stream= new FileOutputStream($this->file)); {
+        $stream->close();
+        $stream->write('');
+      }
+    }
+
+    /**
+     * Test closig an already closed stream
+     *
+     */
+    #[@test, @expect('io.IOException')]
+    public function doubleClose() {
+      with ($stream= new FileOutputStream($this->file)); {
+        $stream->close();
+        $stream->close();
+      }
+    }
+  }
+?>
