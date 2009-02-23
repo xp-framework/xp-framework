b85cba6cc6f706be87a5efb25ab751fea047be83
ohinckel <juniorchicken@gmx.de>
2009-02-23 08:59:32 +0000

    - added StreamAppender class
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@12902 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/util/log/StreamAppender.class.php
@@ -0,0 +1,41 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('util.log.LogAppender');
+
+  /**
+   * StreamAppender which appends data to a stream
+   *
+   * @see      xp://util.log.LogAppender
+   * @purpose  Appender
+   */  
+  class StreamAppender extends LogAppender {
+    public 
+      $stream = NULL;
+    
+    /**
+     * Constructor
+     *
+     * @param   io.streams.OutputStream stream
+     */
+    public function __construct($stream) {
+      $this->stream= $stream;
+    }
+    
+    /**
+     * Appends log data to the file
+     *
+     * @param   mixed* args variables
+     */
+    public function append() {
+      with ($args= func_get_args()); {
+        foreach ($args as $idx => $arg) {
+          $this->stream->write($fd, $this->varSource($arg). ($idx < sizeof($args)-1 ? ' ' : ''));
+        }
+      }
+    }
+  }
+?>
