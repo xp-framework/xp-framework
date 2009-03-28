4aa0ba5f9465e2bb675017c6461381da6e058f4a
thekid <timm.friebe@gmail.com>
2009-03-28 12:23:28 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@12950 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/classes/net/xp_framework/website/framework/scriptlet/state/ReleasesState.class.php
@@ -0,0 +1,17 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('scriptlet.xml.workflow.AbstractState');
+
+  /**
+   * Handles /xml/releases
+   *
+   * @purpose  State
+   */
+  class ReleasesState extends AbstractState {
+
+  }
+?>
