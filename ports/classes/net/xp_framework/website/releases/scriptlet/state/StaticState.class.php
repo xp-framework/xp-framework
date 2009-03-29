9fa42fa42fb2a148a5c69115a5de7edb30c954c2
thekid <timm.friebe@gmail.com>
2009-03-29 13:39:37 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@12959 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/classes/net/xp_framework/website/releases/scriptlet/state/StaticState.class.php
@@ -0,0 +1,58 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses(
+    'scriptlet.xml.workflow.AbstractState', 
+    'text.doclet.markup.MarkupBuilder',
+    'text.doclet.markup.DelegatingProcessor',
+    'io.File',
+    'io.FileUtil',
+    'util.PropertyManager'
+  );
+
+  /**
+   * Static state
+   *
+   * @purpose  State
+   */
+  class StaticState extends AbstractState {
+  
+    /**
+     * Process this state.
+     *
+     * @param   scriptlet.xml.workflow.WorkflowScriptletRequest request
+     * @param   scriptlet.xml.XMLScriptletResponse response
+     */
+    public function process($request, $response) {
+      sscanf($request->getQueryString(), '%[a-zA-Z_]/%[a-zA-Z_]', $base, $topic);
+      $entry= $topic ? $base.DIRECTORY_SEPARATOR.$topic : $base;
+
+      // Read from storage (XXX: Make exchangeable)
+      try {
+        $text= FileUtil::getContents(new File(
+          PropertyManager::getInstance()->getProperties('storage')->readString('text', 'base'),
+          $entry.'.txt'
+        ));
+      } catch (FileNotFoundException $e) {
+        throw new HttpScriptletException('Entry "'.$entry.'" not found', HTTP_NOT_FOUND, $e);
+      }
+      
+      $builder= new MarkupBuilder();
+      
+      // Add <summary>...</summary>
+      $builder->registerProcessor('summary', newinstance('text.doclet.markup.DelegatingProcessor', array($builder->processors['default']), '{
+        public function tag() { return "summary"; }
+      }'));
+      
+      // Insert markup
+      $n= $response->addFormresult(new Node('content', new PCData(
+        '<p>'.$builder->markupFor($text).'</p>'
+      )));
+      $n->setAttribute('base', $base);
+      $topic && $n->setAttribute('topic', $topic);
+    }
+  }
+?>
