d12dea1cbb8edd1398e2aa4830afc683fef616b5
thekid <timm.friebe@gmail.com>
2009-04-11 10:02:10 +0000

    - Throw an IllegalArgumentException from the constructor if   length argument is less than or equal to zero
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@12985 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/text/util/RandomCodeGenerator.class.php
+++ skeleton/text/util/RandomCodeGenerator.class.php
@@ -21,10 +21,13 @@
      * Constructor
      *
      * @param   int length default 16
+     * @throws  lang.IllegalArgumentException if length is not greater than zero
      */
     public function __construct($length= 16) {
+      if ($length <= 0) {
+        throw new IllegalArgumentException('Length must be greater than zero');
+      }
       $this->length= $length;
-      
     }
     
     /**
