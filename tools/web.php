973681f51930d85f21bd933d7e210a428e015e5a
kiesel <alex@kiesel.name>
2009-04-22 13:12:33 +0000

    - refactor web.php (refactorings motivated by real world exposure)   this is not yet production ready - add xp.scriptlet.Runner class in tools
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@12997 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- tools/web.php
+++ tools/web.php
@@ -31,47 +31,15 @@
   $webroot= getenv('DOCUMENT_ROOT').'/..';
   set_include_path(
     scanpath(explode(PATH_SEPARATOR, ini_get('user_dir')), $webroot).
-    scanpath($webroot, $webroot).
-    get_include_path()
+    scanpath(array($webroot), $webroot).
+    scanpath(explode(PATH_SEPARATOR, get_include_path()), $webroot)
   );
-
+  
   // Bootstrap 
   if (!include('lang.base.php')) {
     trigger_error('[bootstrap] Cannot determine boot class path', E_USER_ERROR);
     exit(0x3d);
   }
-  uses('sapi.scriptlet.ScriptletRunner', 'util.PropertyManager', 'rdbms.ConnectionManager');
-  
-  $pm= PropertyManager::getInstance();
-  $pm->configure($webroot.'/etc');
-  $pr= $pm->getProperties('web');
-
-  $url= getenv('SCRIPT_URL');
-  $specific= 'app@'.getenv('SERVER_NAME');
-  $mappings= $pr->readHash($specific, 'mappings', $pr->readHash('app', 'mappings'));
-  foreach ($mappings->keys() as $pattern) {
-    if (!preg_match('°'.$pattern.'°', $url)) continue;
-    
-    // Run first scriptlet that matches
-    $scriptlet= $mappings->get($pattern);
-    $class= XPClass::forName($pr->readString($scriptlet, 'class'));
-    $args= array();
-    foreach ($pr->readArray($scriptlet, 'init-params') as $value) {
-      $args[]= strtr($value, array('{WEBROOT}' => $webroot));
-    }
-    
-    // HACK #1: Always make connection manager available - should be done inside scriptlet init
-    ConnectionManager::getInstance()->configure($pm->getProperties('database'));
-    
-    // HACK #2: Parse URL - should be done inside XMLScriptlet
-    preg_match('°^/([^/]+)°', $url, $patterns);
-    putenv('STATE='.(isset($patterns[1]) ? $patterns[1] : 'home'));
-    putenv('PRODUCT=planet');
-    putenv('LANGUAGE=en_US');
-    
-    $r= new ScriptletRunner($pr->readInteger($specific, 'flags', $pr->readInteger('app', 'flags', 0)));
-    exit($r->run($class->getConstructor()->newInstance($args)));
-  }
-  
-  trigger_error('[scriptlet] No scriptlet at url "'.htmlspecialchars($url).'"', E_USER_ERROR);
+  uses('xp.scriptlet.Runner');
+  exit(xp·scriptlet·Runner::main(array($webroot)));
 ?>
---
+++ tools/xp/scriptlet/Runner.class.php
@@ -0,0 +1,122 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  $package= 'xp.scriptlet';
+
+  uses(
+    'util.PropertyManager',
+    'rdbms.ConnectionManager'
+  );
+  
+  /**
+   * Scriptlet runner
+   *
+   * @purpose  Tool
+   */
+  class xp·scriptlet·Runner extends Object {
+    const
+      SHOW_XML        = 0x0001,
+      SHOW_ERRORS     = 0x0002,
+      SHOW_STACKTRACE = 0x0004;
+      
+    protected
+      $flags  = 0x0000;
+
+    public static function main(array $args) {
+      $webroot= $args[0];
+      
+      $pm= PropertyManager::getInstance();
+      $pm->configure($webroot.'/etc');
+      $pr= $pm->getProperties('web');
+
+      $url= getenv('SCRIPT_URL');
+      $specific= 'app@'.getenv('SERVER_NAME');
+
+      // TBD: How do we find 'specifics' - environment, server name, ...?      
+      $mappings= $pr->readHash($specific, 'mappings', $pr->readHash('app', 'mappings'));
+      foreach ($mappings->keys() as $pattern) {
+        if (!preg_match('°'.$pattern.'°', $url)) continue;
+
+        // Run first scriptlet that matches
+        $scriptlet= $mappings->get($pattern);
+        $class= XPClass::forName($pr->readString($scriptlet, 'class'));
+        $args= array();
+        foreach ($pr->readArray($scriptlet, 'init-params') as $value) {
+          // TBI: Inject properties?
+          $args[]= strtr($value, array('{WEBROOT}' => $webroot));
+        }
+
+        // HACK #1: Always make connection manager available - should be done inside scriptlet init
+        ConnectionManager::getInstance()->configure($pm->getProperties('database'));
+        
+        $self= new self();
+        
+        // Determine debug level
+        foreach ($pr->readArray($scriptlet, 'debug', array()) as $lvl) {
+          $self->flags|= $self->getClass()->getConstant($lvl);
+        }
+        
+        $self->run($class->getConstructor()->newInstance($args));
+        return;
+      }
+      
+      throw new IllegalArgumentException('Could not find app responsible for request to '.$url);
+    }
+    
+    /**
+     * Run scriptlet instance
+     *
+     * @param   scriptlet.HttpScriptlet scriptlet
+     */
+    protected function run(HttpScriptlet $scriptlet) {
+      try {
+        $scriptlet->init();
+        $response= $scriptlet->process();
+      } catch (HttpScriptletException $e) {
+        $response= $e->getResponse();
+        $this->except($response, $e);
+      }
+
+      // Send output
+      
+      // XXX HACK: Do not send headers when they've been sent before
+      headers_sent() || $response->sendHeaders();
+      $response->sendContent();
+      flush();
+
+      // Call scriptlet's finalizer
+      $scriptlet->finalize();
+      
+      if (
+        ($this->flags & self::SHOW_XML) &&
+        ($response && isset($response->document))
+      ) {
+        echo '<xmp>', $response->document->getDeclaration()."\n".$response->document->getSource(0), '</xmp>';
+      }
+      
+      if (($this->flags & self::SHOW_ERRORS)) {
+        echo '<xmp>', var_export(xp::registry('errors'), 1), '</xmp>';
+      }
+    }
+    
+    /**
+     * Handle exception from scriptlet
+     *
+     * @param   scriptlet.HttpScriptletResponse response
+     * @param   lang.Throwable e
+     */
+    protected function except(HttpScriptletResponse $response, Throwable $e) {
+      $response->setContent(str_replace(
+        '<xp:value-of select="reason"/>',
+        (($this->flags & self::SHOW_STACKTRACE)
+          ? $e->toString()
+          : $e->getMessage()
+        ),
+        $this->getClass()->getPackage()->getResource('error'.$response->statusCode.'.html')
+      ));
+    }
+  }
+?>
