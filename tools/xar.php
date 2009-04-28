b93b7abadfed50c30ffb977c00f52eea062d2c15
thekid <timm.friebe@gmail.com>
2009-04-28 05:35:47 +0000

    - Inline sapi/cli and thus deprecate it - Make these work with CGI-FCGI SAPI # The latter requires that the runners are also updated
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@13014 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- tools/xar.php
+++ tools/xar.php
@@ -1,4 +1,6 @@
 <?php 
+  define('EPREPEND_IDENTIFIER', "\6100");
+
   // {{{ string scanpath(string[] path, string home)
   //     Scans a path file 
   function scanpath($paths, $home) {
@@ -28,6 +30,36 @@
   }
   // }}}
 
+  // {{{ internal string __output(string buf)
+  //     Output handler. Checks for fatal errors
+  function __output($buf) {
+    if (FALSE !== ($p= strpos($buf, EPREPEND_IDENTIFIER))) {
+      $e= new Error(str_replace(EPREPEND_IDENTIFIER, '', substr($buf, $p)));
+      fputs(STDERR, $e->toString());
+    }
+
+    return $buf;
+  }
+  // }}}
+
+  // {{{ internal void __except(Exception e)
+  //     Exception handler
+  function __except($e) {
+    fputs(STDERR, 'Uncaught exception: '.xp::stringOf($e));
+  }    
+  // }}}
+
+  // Verify SAPI
+  if ('cgi' === PHP_SAPI || 'cgi-fcgi' === PHP_SAPI) {
+    ini_set('html_errors', 0);
+    define('STDIN', fopen('php://stdin', 'rb'));
+    define('STDOUT', fopen('php://stdout', 'wb'));
+    define('STDERR', fopen('php://stderr', 'wb'));
+  } else if ('cli' !== PHP_SAPI) {
+    trigger_error('[bootstrap] Cannot be run under '.PHP_SAPI.' SAPI', E_USER_ERROR);
+    exit(0x3d);
+  }
+
   $home= getenv('HOME');
   list($use, $include)= explode(PATH_SEPARATOR.PATH_SEPARATOR, get_include_path());
   set_include_path(
@@ -39,10 +71,15 @@
     trigger_error('[bootstrap] Cannot determine boot class path', E_USER_ERROR);
     exit(0x3d);
   }
-  xp::sapi('cli');
-  uses('util.Properties');
+  uses('util.cmd.ParamString', 'util.cmd.Console', 'util.Properties');
+
+  ini_set('error_prepend_string', EPREPEND_IDENTIFIER);
+  set_exception_handler('__except');
+  ob_start('__output');
 
   $cl= ClassLoader::registerLoader(new ArchiveClassLoader($argv[1]));
   $pr= Properties::fromString($cl->getResource('META-INF/manifest.ini'));
+
+  array_shift($_SERVER['argv']);
   exit(XPClass::forName($pr->readString('archive', 'main-class'), $cl)->getMethod('main')->invoke(NULL, array(array_slice($argv, 2)))); 
 ?>
