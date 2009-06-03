643c8d31f02bffd18abfa5fc8ea4c2317d154bb1
thekid <timm.friebe@gmail.com>
2009-06-03 08:50:12 +0000

    - Add usage
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@13077 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- tools/xp/doclet/Runner.class.php
+++ tools/xp/doclet/Runner.class.php
@@ -10,20 +10,53 @@
 
   /**
    * Command line doclet runner
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~
    *
-   * @purpose  RootDoc CLI adaptor
+   * Usage:
+   * <pre>
+   *   doclet class [options] name [name [name...]]
+   * </pre>
+   * Names can be one or more of:
+   * <ul>
+   *   <li>{package.name}.*: All classes inside a given package</li>
+   *   <li>{package.name}.**: All classes inside a given package and all it subpackages</li>
+   *   <li>{qualified.class.Name}: A fully qualified class name</li>
+   * </ul> 
+   *
+   * @purpose  Tool
    */
   class xp·doclet·Runner extends Object {
+
+    /**
+     * Converts api-doc "markup" to plain text w/ ASCII "art"
+     *
+     * @param   string markup
+     * @return  string text
+     */
+    protected static function textOf($markup) {
+      $line= str_repeat('=', 72);
+      return strip_tags(preg_replace(array(
+        '#<pre>#', '#</pre>#', '#<li>#',
+      ), array(
+        $line, $line, '* ',
+      ), trim($markup)));
+    }
+
+    /**
+     * Displays usage and exits
+     *
+     */
+    protected static function usage() {
+      Console::$err->writeLine(self::textOf(XPClass::forName(xp::nameOf(__CLASS__))->getComment()));
+      exit(1);
+    }
   
     /**
      * Runner method
      *
      */
     public static function main(array $args) {
-      if (sizeof($args) < 1) {
-        Console::$err->writeLine('*** No doclet classname given');
-        return -1;
-      }
+      if (!$args) self::usage();
 
       $class= XPClass::forName($args[0]);
       if (!$class->isSubclassOf('text.doclet.Doclet')) {
