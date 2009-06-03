5e2535f72d89f2dd571908e9a80255058bc5e62e
thekid <timm.friebe@gmail.com>
2009-06-03 09:33:08 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@13081 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/classes/net/xp_framework/unittest/text/doclet/classes/B.class.php
@@ -0,0 +1,17 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  $package= 'net.xp_framework.unittest.text.doclet.classes';
+
+  /**
+   * Class "B"
+   *
+   * @see      xp://net.xp_framework.unittest.text.doclet.ClassIteratorTest
+   */
+  class net·xp_framework·unittest·text·doclet·classes·B extends Object {
+    
+  }
+?>
---
+++ ports/classes/net/xp_framework/unittest/text/doclet/classes/sub/D.class.php
@@ -0,0 +1,17 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  $package= 'net.xp_framework.unittest.text.doclet.classes.sub';
+
+  /**
+   * Class "D"
+   *
+   * @see      xp://net.xp_framework.unittest.text.doclet.ClassIteratorTest
+   */
+  class net·xp_framework·unittest·text·doclet·classes·sub·D extends Object {
+    
+  }
+?>
