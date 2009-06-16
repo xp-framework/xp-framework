9a37668013cf4be99211b1dc71d1645dc120ad26
thekid <timm.friebe@gmail.com>
2009-06-16 11:32:20 +0000

    - Initial release # Part of RFC #0159
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@13150 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/rdbms/SQLDeadlockException.class.php
@@ -0,0 +1,53 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('rdbms.SQLException');
+
+  /**
+   * Indicates a deadlock occured
+   * 
+   * @purpose  SQL-Exception
+   */
+  class SQLDeadlockException extends SQLException {
+    protected $sql= '';
+
+    /**
+     * Constructor
+     *
+     * @param   string message
+     * @param   string sql default NULL the SQL query string sent
+     */
+    public function __construct($message, $sql= NULL) {
+      parent::__construct($message);
+      $this->sql= $sql;
+    }
+
+    /**
+     * Get SQL leading to this deadlock
+     *
+     * @return  string
+     */
+    public function getSql() {
+      return $this->sql;
+    }
+
+    /**
+     * Return compound message of this exception.
+     *
+     * @return  string
+     */
+    public function compoundMessage() {
+      return sprintf(
+        "Exception %s (deadlock: %s) {\n".
+        "  %s\n".
+        "}\n",
+        $this->getClassName(),
+        $this->message,
+        $this->sql
+      );
+    }
+  }
+?>
