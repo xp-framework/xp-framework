cdc0dbc860e503c00e952e0b70d08e714f74b7cf
thekid <timm.friebe@gmail.com>
2009-07-21 09:10:45 +0000

    - QA: Fix typo in apidoc # Thanks to Rene Patzer @ GF
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@13257 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/lang/archive/ArchiveClassLoader.class.php
+++ skeleton/lang/archive/ArchiveClassLoader.class.php
@@ -85,7 +85,7 @@
      * @param   string class fully qualified class name
      * @return  string class name of class loaded
      * @throws  lang.ClassNotFoundException in case the class can not be found
-     * @throws  lang.ClassFormatException in case the class format is invalud
+     * @throws  lang.ClassFormatException in case the class format is invalid
      */
     public function loadClass0($class) {
       if (isset(xp::$registry['classloader.'.$class])) return xp::reflect($class);
