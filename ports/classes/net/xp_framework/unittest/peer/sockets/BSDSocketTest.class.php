ee1068af8e4cac99fe199e0b5764dae1856ed79a
thekid <timm.friebe@gmail.com>
2009-08-11 08:30:24 +0000

    - Skip all tests if ext/sockets is not available
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@13329 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/classes/net/xp_framework/unittest/peer/sockets/BSDSocketTest.class.php
+++ ports/classes/net/xp_framework/unittest/peer/sockets/BSDSocketTest.class.php
@@ -12,9 +12,21 @@
   /**
    * TestCase
    *
+   * @ext      sockets
    * @see      xp://peer.BSDSocket
    */
   class BSDSocketTest extends AbstractSocketTest {
+
+    /**
+     * Setup this test case
+     *
+     */
+    public function setUp() {
+      if (!Runtime::getInstance()->extensionAvailable('sockets')) {
+        throw new PrerequisitesNotMetError('Sockets extension not available', NULL, array('ext/sockets'));
+      }
+      parent::setUp();
+    }
     
     /**
      * Creates a new client socket
