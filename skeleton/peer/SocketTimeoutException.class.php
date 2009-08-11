b4323878152145886fb1ff0282f0149dba72dfe7
thekid <timm.friebe@gmail.com>
2009-08-11 09:37:31 +0000

    - Add timeout parameter to constructor - Add getTimeout() accessor - Include timeout in compound message
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@13334 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/peer/SocketTimeoutException.class.php
+++ skeleton/peer/SocketTimeoutException.class.php
@@ -13,6 +13,40 @@
    * @see      xp://peer.SocketException
    */
   class SocketTimeoutException extends SocketException {
+    protected $timeout= 0.0;
+    
+    /**
+     * Constructor
+     *
+     * @param   string message
+     * @param   float timeout
+     */
+    public function __construct($message, $timeout) {
+      parent::__construct($message);
+      $this->timeout= $timeout;
+    }
 
+    /**
+     * Get timeout
+     *
+     * @return  float
+     */
+    public function getTimeout() {
+      return $this->_timeout;
+    }
+    
+    /**
+     * Return compound message of this exception.
+     *
+     * @return  string
+     */
+    public function compoundMessage() {
+      return sprintf(
+        'Exception %s (%s after %.3f seconds)',
+        $this->getClassName(),
+        $this->message,
+        $this->timeout
+      );
+    }
   }
 ?>
