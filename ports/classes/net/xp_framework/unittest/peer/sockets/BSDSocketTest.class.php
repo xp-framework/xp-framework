aeb08b12082a0461b24276e64b7d73b92be6aa66
thekid <timm.friebe@gmail.com>
2009-08-11 08:34:04 +0000

    - Add tests for domain accessors - Add tests for type accessors - Add tests for protocol accessors
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@13330 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/classes/net/xp_framework/unittest/peer/sockets/BSDSocketTest.class.php
+++ ports/classes/net/xp_framework/unittest/peer/sockets/BSDSocketTest.class.php
@@ -38,5 +38,95 @@
     protected function newSocket($addr, $port) {
       return new BSDSocket($addr, $port);
     }
+    
+    /**
+     * Test setDomain() and getDomain() with AF_INET
+     *
+     */
+    #[@test]
+    public function inetDomain() {
+      $this->fixture->setDomain(AF_INET);
+      $this->assertEquals(AF_INET, $this->fixture->getDomain());
+    }
+
+    /**
+     * Test setDomain() and getDomain() with AF_UNIX
+     *
+     */
+    #[@test]
+    public function unixDomain() {
+      $this->fixture->setDomain(AF_UNIX);
+      $this->assertEquals(AF_UNIX, $this->fixture->getDomain());
+    }
+
+    /**
+     * Test setDomain()
+     *
+     */
+    #[@test, @expect('lang.IllegalStateException')]
+    public function setDomainOnConnected() {
+      $this->fixture->connect();
+      $this->fixture->setDomain(AF_UNIX);
+    }
+
+    /**
+     * Test setType() and getType() with SOCK_STREAM
+     *
+     */
+    #[@test]
+    public function streamType() {
+      $this->fixture->setType(SOCK_STREAM);
+      $this->assertEquals(SOCK_STREAM, $this->fixture->getType());
+    }
+
+    /**
+     * Test setType() and getType() with SOCK_DGRAM
+     *
+     */
+    #[@test]
+    public function dgramType() {
+      $this->fixture->setType(SOCK_DGRAM);
+      $this->assertEquals(SOCK_DGRAM, $this->fixture->getType());
+    }
+
+    /**
+     * Test setType()
+     *
+     */
+    #[@test, @expect('lang.IllegalStateException')]
+    public function setTypeOnConnected() {
+      $this->fixture->connect();
+      $this->fixture->setType(SOCK_STREAM);
+    }
+
+    /**
+     * Test setProtocol() and getProtocol() with SOL_TCP
+     *
+     */
+    #[@test]
+    public function tcpProtocol() {
+      $this->fixture->setProtocol(SOL_TCP);
+      $this->assertEquals(SOL_TCP, $this->fixture->getProtocol());
+    }
+
+    /**
+     * Test setProtocol() and getProtocol() with SOL_UDP
+     *
+     */
+    #[@test]
+    public function udpProtocol() {
+      $this->fixture->setProtocol(SOL_UDP);
+      $this->assertEquals(SOL_UDP, $this->fixture->getProtocol());
+    }
+
+    /**
+     * Test setProtocol()
+     *
+     */
+    #[@test, @expect('lang.IllegalStateException')]
+    public function setProtocolOnConnected() {
+      $this->fixture->connect();
+      $this->fixture->setProtocol(SOL_TCP);
+    }
   }
 ?>
