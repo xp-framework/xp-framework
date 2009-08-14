cd311e92b65ee0d539536b6b9483e102f9b5694b
kiesel <alex@kiesel.name>
2009-08-14 14:37:19 +0000

    - initial revision
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@13388 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/classes/net/xp_framework/unittest/rdbms/integration/MsSQLIntegrationTest.class.php
@@ -0,0 +1,46 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses('net.xp_framework.unittest.rdbms.integration.RdbmsIntegrationTest');
+
+  /**
+   * Sybase integration test
+   *
+   * @ext       mssql
+   */
+  class MsSQLIntegrationTest extends RdbmsIntegrationTest {
+    
+    /**
+     * Before class method: set minimun server severity;
+     * otherwise server messages end up on the error stack
+     * and will let the test fail (no error policy).
+     *
+     */
+    #[@beforeClass]
+    public static function messageLevel() {
+      mssql_min_message_severity(12);
+    }
+
+    /**
+     * Retrieve dsn
+     *
+     * @return  string
+     */
+    public function _dsn() {
+      return 'mssql';
+    }
+    
+    /**
+     * Create autoincrement table
+     *
+     * @param   string name
+     */
+    protected function createAutoIncrementTable($name) {
+      $this->removeTable($name);
+      $this->db()->query('create table %c (pk int identity, username varchar(30))', $name);
+    }
+  }
+?>
