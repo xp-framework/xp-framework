947ce3dcd5013b98b4321ae8d9d8bb3630da65e5
thekid <timm.friebe@gmail.com>
2009-08-25 08:05:21 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/branches/xp5_8@13443 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/classes/net/xp_framework/unittest/annotations/UnterminatedStringLiteral.class.php
@@ -0,0 +1,17 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  $package= 'net.xp_framework.unittest.annotations';
+
+  /**
+   * Uses by BrokenAnnotationTest
+   *
+   */
+  #[@attribute(key= 'value)]
+  class net·xp_framework·unittest·annotations·UnterminatedStringLiteral extends Object {
+    
+  }
+?>
