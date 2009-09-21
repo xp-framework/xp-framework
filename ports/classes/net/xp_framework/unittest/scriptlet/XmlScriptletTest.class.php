cc70c7dca966f23d0c7c8869e21e376bf91ee3c5
thekid <timm.friebe@gmail.com>
2009-09-21 12:19:12 +0000

    - Add tests for special "__" parameters passed to processor:   . __state   . __page   . __lang   . __product   . __sess   . __query
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@13557 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/classes/net/xp_framework/unittest/scriptlet/XmlScriptletTest.class.php
+++ ports/classes/net/xp_framework/unittest/scriptlet/XmlScriptletTest.class.php
@@ -50,6 +50,12 @@
     protected function newResponse(Stylesheet $stylesheet) {
       $res= new XMLScriptletResponse();
       $res->setProcessor(new DOMXSLProcessor());
+      $stylesheet->addParam('__state');
+      $stylesheet->addParam('__page');
+      $stylesheet->addParam('__lang');
+      $stylesheet->addParam('__product');
+      $stylesheet->addParam('__sess');
+      $stylesheet->addParam('__query');
       $res->setStylesheet($stylesheet, XSLT_TREE);
       return $res;
     }
@@ -114,6 +120,146 @@
       $this->assertEquals(
         '<?xml version="1.0" encoding="iso-8859-1"?>'."\n".
         "<html>\n  <body>POST</body>\n</html>\n",
+        $res->getContent()
+      );
+    }
+    
+    /**
+     * Test parameters passed to XSL
+     *
+     */
+    #[@test]
+    public function defaultParameters() {
+      $req= $this->newRequest('GET', new URL('http://localhost/'));
+      $res= $this->newResponse(create(new Stylesheet())
+        ->withOutputMethod('xml')
+        ->withTemplate(create(new XslTemplate())->matching('/')
+          ->withChild(create(new Node('html'))
+            ->withChild(create(new Node('body'))
+              ->withChild(new Node('xsl:value-of', NULL, array('select' => 'concat(
+                "state=",   $__state, ", ",
+                "page=",    $__page, ", ",
+                "lang=",    $__lang, ", ",
+                "product=", $__product, ", ",
+                "sess=",    $__sess, ", ",
+                "query=",   $__query
+              )')))
+            )
+          )
+        )
+      );
+      
+      $s= new XMLScriptlet();
+      $s->service($req, $res);
+      $this->assertEquals(HttpConstants::STATUS_OK, $res->statusCode);
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?>'."\n".
+        "<html>\n  <body>state=static, page=home, lang=en_US, product=, sess=, query=</body>\n</html>\n",
+        $res->getContent()
+      );
+    }
+
+    /**
+     * Test parameters passed to XSL
+     *
+     */
+    #[@test]
+    public function homeState() {
+      $req= $this->newRequest('GET', new URL('http://localhost/xml/home'));
+      $res= $this->newResponse(create(new Stylesheet())
+        ->withOutputMethod('xml')
+        ->withTemplate(create(new XslTemplate())->matching('/')
+          ->withChild(create(new Node('html'))
+            ->withChild(create(new Node('body'))
+              ->withChild(new Node('xsl:value-of', NULL, array('select' => 'concat(
+                "state=",   $__state, ", ",
+                "page=",    $__page, ", ",
+                "lang=",    $__lang, ", ",
+                "product=", $__product, ", ",
+                "sess=",    $__sess, ", ",
+                "query=",   $__query
+              )')))
+            )
+          )
+        )
+      );
+      
+      $s= new XMLScriptlet();
+      $s->service($req, $res);
+      $this->assertEquals(HttpConstants::STATUS_OK, $res->statusCode);
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?>'."\n".
+        "<html>\n  <body>state=home, page=home, lang=en_US, product=, sess=, query=</body>\n</html>\n",
+        $res->getContent()
+      );
+    }
+
+    /**
+     * Test parameters passed to XSL
+     *
+     */
+    #[@test]
+    public function productAndLanguage() {
+      $req= $this->newRequest('GET', new URL('http://localhost/xml/public.de_DE/home'));
+      $res= $this->newResponse(create(new Stylesheet())
+        ->withOutputMethod('xml')
+        ->withTemplate(create(new XslTemplate())->matching('/')
+          ->withChild(create(new Node('html'))
+            ->withChild(create(new Node('body'))
+              ->withChild(new Node('xsl:value-of', NULL, array('select' => 'concat(
+                "state=",   $__state, ", ",
+                "page=",    $__page, ", ",
+                "lang=",    $__lang, ", ",
+                "product=", $__product, ", ",
+                "sess=",    $__sess, ", ",
+                "query=",   $__query
+              )')))
+            )
+          )
+        )
+      );
+      
+      $s= new XMLScriptlet();
+      $s->service($req, $res);
+      $this->assertEquals(HttpConstants::STATUS_OK, $res->statusCode);
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?>'."\n".
+        "<html>\n  <body>state=home, page=home, lang=de_DE, product=public, sess=, query=</body>\n</html>\n",
+        $res->getContent()
+      );
+    }
+
+    /**
+     * Test parameters passed to XSL
+     *
+     */
+    #[@test]
+    public function query() {
+      $req= $this->newRequest('GET', new URL('http://localhost/?a=b'));
+      $res= $this->newResponse(create(new Stylesheet())
+        ->withOutputMethod('xml')
+        ->withTemplate(create(new XslTemplate())->matching('/')
+          ->withChild(create(new Node('html'))
+            ->withChild(create(new Node('body'))
+              ->withChild(new Node('xsl:value-of', NULL, array('select' => 'concat(
+                "state=",   $__state, ", ",
+                "page=",    $__page, ", ",
+                "lang=",    $__lang, ", ",
+                "product=", $__product, ", ",
+                "sess=",    $__sess, ", ",
+                "query=",   $__query
+              )')))
+            )
+          )
+        )
+      );
+      
+      $s= new XMLScriptlet();
+      $s->service($req, $res);
+      $this->assertEquals(HttpConstants::STATUS_OK, $res->statusCode);
+      $this->assertEquals(
+        '<?xml version="1.0" encoding="iso-8859-1"?>'."\n".
+        "<html>\n  <body>state=static, page=home, lang=en_US, product=, sess=, query=a=b</body>\n</html>\n",
         $res->getContent()
       );
     }
