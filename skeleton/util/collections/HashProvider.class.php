9b6d6409e1282620b8a6cf6bff4835863ff19ecf
thekid <timm.friebe@gmail.com>
2009-09-29 16:36:49 +0000

    - Fix "Hex number is too big: 0x20c49ba5e35423" # Only occurs in broken PHP versions!
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@13615 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/util/collections/HashProvider.class.php
+++ skeleton/util/collections/HashProvider.class.php
@@ -39,7 +39,7 @@
       // class apidoc for an explanation. Earlier versions returned LONG_MAX for
       // hex numbers larger than LONG_MAX. Use 2^64 + 1 as hex literal and see if
       // it's "truncated", using the slower hexdec(md5()) implementation then.
-      if (LONG_MAX === 0x20c49ba5e35423) {
+      if (LONG_MAX === @0x20c49ba5e35423) {
         $impl= XPClass::forName('util.collections.MD5HexHashImplementation')->newInstance();
       } else {
         $impl= new MD5HashImplementation();
