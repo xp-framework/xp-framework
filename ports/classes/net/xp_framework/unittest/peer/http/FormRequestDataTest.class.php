55726828c0da4c926aaef1c302764517b9a9e423
kiesel <alex@kiesel.name>
2009-10-06 09:57:20 +0000

    - initial revision
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@13657 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/classes/net/xp_framework/unittest/peer/http/FormRequestDataTest.class.php
@@ -0,0 +1,95 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses(
+    'unittest.TestCase',
+    'peer.http.FormRequestData'
+  );
+
+  /**
+   * TestCase
+   *
+   * @see       xp://peer.http.FormRequestData
+   * @see       xp://peer.http.FormData
+   * @purpose   Testcase
+   */
+  class FormRequestDataTest extends TestCase {
+    protected
+      $fixture  = NULL;
+
+    /**
+     * Setup test case
+     *
+     */
+    public function setUp() {
+      $this->fixture= new FormRequestData();
+    }
+
+    /**
+     * Test adding new parameters
+     *
+     */
+    #[@test]
+    public function add() {
+      $data= new FormData('key', 'value');
+      $this->assertEquals($data, $this->fixture->addPart($data));
+    }
+
+    /**
+     * Test representation of value w/ default content-type and charset
+     *
+     */
+    #[@test]
+    public function simpleMimeRepresentation() {
+      $this->fixture->addPart(new FormData('key', 'value'));
+
+      $this->assertEquals(
+        "\r\n--".$this->fixture->getBoundary()."\r\n".
+        "Content-Disposition: form-data; name=\"key\"\r\n\r\n".
+        "value\r\n--".$this->fixture->getBoundary()."--\r\n",
+
+        $this->fixture->getData()
+      );
+    }
+
+    /**
+     * Test representation of value w/ non-default mime-type
+     *
+     */
+    #[@test]
+    public function noDefaultTypeMimeRepresentation() {
+      $this->fixture->addPart(new FormData('key', 'value', 'text/html'));
+
+      $this->assertEquals(
+        "\r\n--".$this->fixture->getBoundary()."\r\n".
+        "Content-Disposition: form-data; name=\"key\"\r\n".
+        "Content-Type: text/html\r\n\r\n".
+        "value\r\n--".$this->fixture->getBoundary()."--\r\n",
+
+        $this->fixture->getData()
+      );
+    }
+
+    /**
+     * Test representation of value w/ default content-type but
+     * non-default charset
+     *
+     */
+    #[@test]
+    public function noDefaultCharsetMimeRepresentation() {
+      $this->fixture->addPart(new FormData('key', 'value', 'text/plain', 'UTF-8'));
+
+      $this->assertEquals(
+        "\r\n--".$this->fixture->getBoundary()."\r\n".
+        "Content-Disposition: form-data; name=\"key\"\r\n".
+        "Content-Type: text/plain; charset=\"UTF-8\"\r\n\r\n".
+        "value\r\n--".$this->fixture->getBoundary()."--\r\n",
+
+        $this->fixture->getData()
+      );
+    }
+  }
+?>
