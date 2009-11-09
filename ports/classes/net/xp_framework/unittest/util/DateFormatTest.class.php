80354c79a0d349f2438116e8c18264337df1d723
thekid <timm.friebe@gmail.com>
2009-11-09 19:43:46 +0000

    - Add tests for AM | PM - Add tests for 12 hour format - Add tests for formatting - Separate date and date *and* time parsing tests
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@13795 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/classes/net/xp_framework/unittest/util/DateFormatTest.class.php
+++ ports/classes/net/xp_framework/unittest/util/DateFormatTest.class.php
@@ -21,10 +21,94 @@
      *
      */
     #[@test]
-    public function usFormat() {
+    public function parseUsFormat() {
+      $this->assertEquals(
+        new Date('2009-01-01'),
+        create(new DateFormat('%Y-%m-%d'))->parse('2009-01-01')
+      );
+    }
+
+    /**
+     * Test US-format (YYYY-MM-DD)
+     *
+     */
+    #[@test]
+    public function formatUsFormat() {
+      $this->assertEquals(
+        '2009-12-14',
+        create(new DateFormat('%Y-%m-%d'))->format(new Date('2009-12-14'))
+      );
+    }
+
+    /**
+     * Test US-format (YYYY-MM-DD HH:MM:SS (AM|PM))
+     *
+     */
+    #[@test]
+    public function parseUsFormatWithTime() {
+      $this->assertEquals(
+        new Date('2009-12-14 14:24:36'),
+        create(new DateFormat('%Y-%m-%d %I:%M:%S %p'))->parse('2009-12-14 02:24:36 PM')
+      );
+    }
+
+    /**
+     * Test US-format (YYYY-MM-DD HH:MM:SS (AM|PM))
+     *
+     */
+    #[@test]
+    public function formatUsFormatWithTime() {
+      $this->assertEquals(
+        '2009-12-14 02:24:36 PM',
+        create(new DateFormat('%Y-%m-%d %I:%M:%S %p'))->format(new Date('2009-12-14 14:24:36'))
+      );
+    }
+
+    /**
+     * Test US-format (YYYY-MM-DD HH:MM:SS (AM|PM))
+     *
+     */
+    #[@test]
+    public function parseUsFormatWith24HourTime() {
+      $this->assertEquals(
+        new Date('2009-12-14 14:00:01'),
+        create(new DateFormat('%Y-%m-%d %H:%M:%S'))->parse('2009-12-14 14:00:01')
+      );
+    }
+
+    /**
+     * Test US-format (YYYY-MM-DD HH:MM:SS)
+     *
+     */
+    #[@test]
+    public function formatUsFormatWith24HourTime() {
+      $this->assertEquals(
+        '2009-12-14 14:24:36',
+        create(new DateFormat('%Y-%m-%d %H:%M:%S'))->format(new Date('2009-12-14 14:24:36'))
+      );
+    }
+
+    /**
+     * Test EU-format (DD.MM.YYYY)
+     *
+     */
+    #[@test]
+    public function parseEuFormat() {
       $this->assertEquals(
         new Date('2009-12-14'),
-        create(new DateFormat('%Y-%m-%d'))->parse('2009-12-14')
+        create(new DateFormat('%d.%m.%Y'))->parse('14.12.2009')
+      );
+    }
+
+    /**
+     * Test EU-format (DD.MM.YYYY)
+     *
+     */
+    #[@test]
+    public function formatEuFormat() {
+      $this->assertEquals(
+        '14.12.2009',
+        create(new DateFormat('%d.%m.%Y'))->format(new Date('2009-12-14'))
       );
     }
 
@@ -33,10 +117,22 @@
      *
      */
     #[@test]
-    public function euFormat() {
+    public function parseEuFormatWithTime() {
       $this->assertEquals(
         new Date('2009-12-14 11:45:00'),
         create(new DateFormat('%d.%m.%Y %H:%M:%S'))->parse('14.12.2009 11:45:00')
+      );
+    }
+
+    /**
+     * Test EU-format (DD.MM.YYYY HH:II:SS)
+     *
+     */
+    #[@test]
+    public function formatEuFormatWithTime() {
+      $this->assertEquals(
+        '14.12.2009 11:45:00',
+        create(new DateFormat('%d.%m.%Y %H:%M:%S'))->format(new Date('2009-12-14 11:45:00'))
       );
     }
  
