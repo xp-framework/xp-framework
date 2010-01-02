9710d8d43db8f17b3debe811b79fd760a5a45080
thekid <timm.friebe@gmail.com>
2010-01-02 18:34:42 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@13982 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/io/streams/DeflatingOutputStream.class.php
@@ -0,0 +1,60 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('io.streams.OutputStream', 'io.streams.Streams');
+
+  /**
+   * OuputStream that deflates 
+   *
+   * @ext      zlib
+   * @test     xp://net.xp_framework.unittest.io.streams.DeflatingOutputStreamTest
+   * @purpose  OuputStream implementation
+   */
+  class DeflatingOutputStream extends Object implements OutputStream {
+    protected 
+      $out   = NULL,
+      $level = 0;
+    
+    /**
+     * Constructor
+     *
+     * @param   io.streams.OutputStream out
+     * @param   int level default 6
+     */
+    public function __construct(OutputStream $out, $level= 6) {
+      $this->out= Streams::writeableFd($out);
+      if (!stream_filter_append($this->out, 'zlib.deflate', STREAM_FILTER_WRITE)) {
+        throw new IOException('Could not append stream filter');
+      }
+    }
+    
+    /**
+     * Write a string
+     *
+     * @param   mixed arg
+     */
+    public function write($arg) {
+      fwrite($this->out, $arg);
+    }
+
+    /**
+     * Flush this buffer
+     *
+     */
+    public function flush() {
+      fflush($this->out);
+    }
+
+    /**
+     * Close this buffer. Flushes this buffer and then calls the close()
+     * method on the underlying OuputStream.
+     *
+     */
+    public function close() {
+      fclose($this->out);
+    }
+  }
+?>
