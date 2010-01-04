e63bd63f199faa72c21123e115366c4bac6174c0
thekid <timm.friebe@gmail.com>
2010-01-04 14:05:26 +0000

    - Add workaround for crc32b endianess bug in certain setups # See http://bugs.php.net/bug.php?id=45028
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@14003 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/security/checksum/CRC32Digest.class.php
+++ skeleton/security/checksum/CRC32Digest.class.php
@@ -13,6 +13,15 @@
    * @see      xp://security.checksum.MessageDigest
    */
   class CRC32Digest extends MessageDigest {
+    protected static $invert= FALSE;
+
+    static function __static() {
+
+      // Workaround for http://bugs.php.net/bug.php?id=45028
+      // crc32b algorithm's endianess inverted - fixed 2008-08-18
+      // and thus with PHP 5.2.7
+      self::$invert= '0a1cb779' === hash('crc32b', 'AAAAAAAA');
+    }
 
     /**
      * Returns algorithm
@@ -30,7 +39,15 @@
      * @return  security.checksum.Checksum
      */
     protected function instance($final) {
-      return new CRC32((string)hexdec($final));
+      $crc= hexdec($final);
+      if (!self::$invert) {
+        return new CRC32($crc);
+      } else {
+        return new CRC32(sprintf(
+          '%u', 
+          (($crc & 0xFF) << 24) + (($crc & 0xFF00) << 8) + (($crc & 0xFF0000) >> 8) + (($crc >> 24) & 0xFF)
+        ));
+      }
     }
   }
 ?>
