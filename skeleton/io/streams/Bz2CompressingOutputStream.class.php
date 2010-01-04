5b067caf888bfe8240003a819b35a053cd85be74
thekid <timm.friebe@gmail.com>
2010-01-04 09:19:48 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@13984 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/io/streams/Bz2CompressingOutputStream.class.php
@@ -0,0 +1,58 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('io.streams.OutputStream', 'io.streams.Streams');
+
+  /**
+   * OuputStream that compresses content using bzip2
+   *
+   * @ext      bz2
+   * @test     xp://net.xp_framework.unittest.io.streams.Bz2CompressingOutputStreamTest
+   * @purpose  OuputStream implementation
+   */
+  class Bz2CompressingOutputStream extends Object implements OutputStream {
+    protected $out= NULL;
+    
+    /**
+     * Constructor
+     *
+     * @param   io.streams.OutputStream out
+     * @param   int level default 6
+     */
+    public function __construct(OutputStream $out, $level= 6) {
+      $this->out= Streams::writeableFd($out);
+      if (!stream_filter_append($this->out, 'bzip2.compress', STREAM_FILTER_WRITE, array('blocks' => $level))) {
+        throw new IOException('Could not append stream filter');
+      }
+    }
+    
+    /**
+     * Write a string
+     *
+     * @param   mixed arg
+     */
+    public function write($arg) {
+      fwrite($this->out, $arg);
+    }
+
+    /**
+     * Flush this buffer
+     *
+     */
+    public function flush() {
+      fflush($this->out);
+    }
+
+    /**
+     * Close this buffer. Flushes this buffer and then calls the close()
+     * method on the underlying OuputStream.
+     *
+     */
+    public function close() {
+      fclose($this->out);
+    }
+  }
+?>
