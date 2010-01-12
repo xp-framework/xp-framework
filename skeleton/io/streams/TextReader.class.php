361c4c47482d05c12296e643b0afb28ea3bb34d3
thekid <timm.friebe@gmail.com>
2010-01-12 19:59:23 +0000

    - Use convert.iconv stream filter instead of handcrafted solution
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@14082 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/io/streams/TextReader.class.php
+++ skeleton/io/streams/TextReader.class.php
@@ -4,7 +4,7 @@
  * $Id$ 
  */
 
-  uses('io.streams.Reader');
+  uses('io.streams.Reader', 'io.streams.Streams');
 
   /**
    * Reads text from an underlying input stream, converting it from the
@@ -14,9 +14,8 @@
    * @ext     iconv
    */
   class TextReader extends Reader {
-    protected $charset= '';
-    protected $buf= '';
-    protected $bl= 0;
+    protected $in = NULL;
+    protected $buf = '';
   
     /**
      * Constructor. Creates a new TextReader on an underlying input
@@ -27,7 +26,10 @@
      */
     public function __construct(InputStream $stream, $charset= 'iso-8859-1') {
       parent::__construct($stream);
-      $this->charset= $charset;
+      $this->in= Streams::readableFd($stream);
+      if (!stream_filter_append($this->in, 'convert.iconv.'.$charset.'/iso-8859-1', STREAM_FILTER_READ)) {
+        throw new IOException('Could not append stream filter');
+      }
     }
   
     /**
@@ -37,54 +39,21 @@
      * @return  string NULL when end of data is reached
      */
     public function read($size= 8192) {
-      if (0 === $size) return '';       // Short-circuit this
-      
-      // See if we have enough characters in the buffer. If we
-      // don't, fill up the buffer until we do.
-      $buffered= $this->bl;
-      while ($buffered < $size && $this->stream->available()) {
-      
-        // Read a chunk (the difference in buffered length and requested 
-        // size, but a minimum of 128 bytes) and see if we catch 
-        // a situation like this
-        //
-        //   "He is the Übercoder" (UTF-8)
-        //    ^^^^^^^^^^^
-        //    chunk
-        //
-        // In this case, we need to read more bytes until we have enough 
-        // bytes to form a complete character. If we reach the end of the
-        // underlying stream we can be sure the input is malformed.
-        $read= $this->stream->read(max($size - $this->bl, 0x80));
-        while (FALSE === ($len= @iconv_strlen($read, $this->charset))) {
-          if (!$this->stream->available()) {
-            throw new FormatException('Broken input string "'.$read.'"');
-          }
-          $read.= $this->stream->read(1);
+      if (0 === $size) return '';
+
+      $c= fread($this->in, $size);
+      if ('' === $c) {
+        if (xp::errorAt(__FILE__, __LINE__ - 2)) {
+          $message= key(xp::$registry['errors'][__FILE__][__LINE__ - 3]);
+          xp::gc(__FILE__);
+          throw new FormatException($message); 
         }
-        
-        // The read variable now contains valid characters in the relevant
-        // character set, so we can concatenate it to the buffer
-        $buffered+= $len;
-        $this->buf.= $read;
-      }
-      
-      // Finally: We might have exceeded the size given, so only return 
-      // exactly the amount of requested characters.
-      if (0 === $buffered) {
         return NULL;
-      } else if (1 === $buffered) {
-        $chunk= $this->buf;
-        $this->buf= '';
-        $this->bl= 0;
-      } else {
-        $chunk= iconv_substr($this->buf, 0, $size, $this->charset);
-        $this->buf= substr($this->buf, strlen($chunk));
-        $this->bl= iconv_strlen($this->buf, $this->charset);
       }
       
-      // Ignore characters not convertible to iso-8859-1
-      return @iconv($this->charset, 'iso-8859-1//IGNORE', $chunk);
+      $chunk= $this->buf.$c;
+      $this->buf= '';
+      return $chunk;
     }
     
     /**
@@ -106,6 +75,26 @@
         $line.= $c;
       } while (NULL !== ($c= $this->read(1)));
       return $line;
+    }
+
+    /**
+     * Close this buffer.
+     *
+     */
+    public function close() {
+      fclose($this->in);
+      $this->in= NULL;
+      // No call to parent::close() as fclose() will already close underlying stream
+    }
+
+    /**
+     * Destructor. Ensures output stream is closed.
+     *
+     */
+    public function __destruct() {
+      if (!$this->in) return;
+      fclose($this->in);
+      $this->in= NULL;
     }
   }
 ?>
