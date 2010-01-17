8fe3d72e9587a276a0a04f3f638c5293e1e8d840
thekid <timm.friebe@gmail.com>
2010-01-17 18:49:53 +0000

    - Add usableInSaveHTMLFile() test - Use Runtime::extensionAvailable() instead of extension_loaded()
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@14103 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/classes/net/xp_framework/unittest/io/DomApiStreamsTest.class.php
+++ ports/classes/net/xp_framework/unittest/io/DomApiStreamsTest.class.php
@@ -8,12 +8,14 @@
     'unittest.TestCase',
     'io.streams.MemoryInputStream',
     'io.streams.MemoryOutputStream',
-    'io.streams.Streams'
+    'io.streams.Streams',
+    'lang.Runtime'
   );
 
   /**
    * TestCase
    *
+   * @see      php://DOMDocument
    * @see      xp://io.streams.Streams
    * @purpose  Unittest
    */
@@ -25,8 +27,8 @@
      * @throws  unittest.PrerequisitesNotMetError
      */
     public function setUp() {
-      if (!extension_loaded('dom')) {
-        throw new PrerequisitesNotMetError('DOM extension not loaded');
+      if (!Runtime::getInstance()->extensionAvailable('dom')) {
+        throw new PrerequisitesNotMetError('DOM extension not loaded', NULL, array('ext/dom'));
       }
     }
  
@@ -35,7 +37,7 @@
      *
      */
     #[@test]
-    public function usableInHTMLFile() {
+    public function usableInLoadHTMLFile() {
       $dom= new DOMDocument();
       $this->assertTrue($dom->loadHTMLFile(Streams::readableUri(new MemoryInputStream(trim('
         <?xml version="1.0" encoding="utf-8"?>
@@ -48,6 +50,30 @@
           </body>
         </html>
       ')))));
+      $this->assertEquals('übercoder', $dom->getElementsByTagName('title')->item(0)->nodeValue);
+    }
+
+    /**
+     * Test DOMDocument::saveHTMLFile()
+     *
+     */
+    #[@test]
+    public function usableInSaveHTMLFile() {
+      $out= new MemoryOutputStream();
+
+      // Create DOM and save it to stream
+      $dom= new DOMDocument();
+      $dom->appendChild($dom->createElement('html'))
+        ->appendChild($dom->createElement('head'))
+        ->appendChild($dom->createElement('title', 'übercoder'))
+      ;
+      $dom->saveHTMLFile(Streams::writeableUri($out));
+      
+      // Check file contents
+      $this->assertEquals(
+        '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>&uuml;bercoder</title></head></html>', 
+        trim($out->getBytes())
+      );
     }
   }
 ?>
