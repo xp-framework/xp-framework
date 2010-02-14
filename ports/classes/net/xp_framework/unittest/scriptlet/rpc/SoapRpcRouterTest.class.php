22647f0012c505306cf790f16cee72a261a33092
thekid <timm.friebe@gmail.com>
2010-02-14 19:42:56 +0000

    - Refrain from using deprecated HTTP_* constants
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@14227 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/classes/net/xp_framework/unittest/scriptlet/rpc/SoapRpcRouterTest.class.php
+++ ports/classes/net/xp_framework/unittest/scriptlet/rpc/SoapRpcRouterTest.class.php
@@ -26,7 +26,7 @@
      */
     public function setUp() {
       $this->router= new SoapRpcRouterMock('net.xp_framework.unittest.scriptlet.rpc.impl');
-      $this->router->setMockMethod(HTTP_POST);
+      $this->router->setMockMethod(HttpConstants::POST);
       $this->router->setMockHeaders(array(
         'SOAPAction'    => 'DummyRpcImplementation#getImplementationName',
         'Content-Type'  => 'text/xml; charset=iso-8859-1'
@@ -66,7 +66,7 @@
      */
     #[@test, @expect('scriptlet.HttpScriptletException')]
     public function basicGetRequest() {
-      $this->router->setMockMethod(HTTP_GET);
+      $this->router->setMockMethod(HttpConstants::GET);
       $this->router->init();
       $response= $this->router->process();
     }
