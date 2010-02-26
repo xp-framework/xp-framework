33b662c16794b4624490f901e1b5acc11071806b
thekid <timm.friebe@gmail.com>
2010-02-26 21:54:48 +0000

    - Simplify sourcecode in assertInstanceOf()
    git-svn-id: svn://svn.xp-framework.net/xp/branches/xp5_8@14299 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/unittest/TestCase.class.php
+++ skeleton/unittest/TestCase.class.php
@@ -232,16 +232,7 @@
         $type= Type::forName($type);
       }
       
-      if ($type instanceof XPClass) {
-        $verify= $type->isInstance($var);
-      } else if ($type instanceof Primitive) {
-        $verify= $type->equals(Type::forName(xp::typeOf($var)));
-      } else if (Type::$VAR->equals($type)) {
-        $verify= TRUE;
-      } else if (Type::$VOID->equals($type)) {
-        $verify= FALSE;
-      }
-      $verify || $this->fail($error, $type->getName(), xp::typeOf($var));
+      $type->isInstance($var) || $this->fail($error, $type->getName(), xp::typeOf($var));
     }
     
     /**
