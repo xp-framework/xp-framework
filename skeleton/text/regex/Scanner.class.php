e1955947aaf6a3c04e1da29ca39d69d960cd5b86
thekid <timm.friebe@gmail.com>
2010-02-28 17:35:23 +0000

    - Make text.regex.Scanner behave like text.regex.Pattern
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@14332 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/text/regex/Scanner.class.php
+++ skeleton/text/regex/Scanner.class.php
@@ -27,17 +27,17 @@
         if ('%' === $pattern{$i}) {
           $i++;
           switch ($pattern{$i}) {
-            case '%': $this->pattern[]= '2%'; break; 
-            case 'd': $this->pattern[]= '1+-0123456789'; break;
-            case 'x': $this->pattern[]= '1x0123456789abcdefABCDEF'; break;
-            case 'f': $this->pattern[]= '1+-0123456789.'; break;
-            case 's': $this->pattern[]= "0\1\2\3\4\5\6\7\10\11\12\13\14\15\16\17\20\21\22\23\24\25\26\27\30\31\32\33\34\35\36\37\40"; break;
+            case '%': $this->pattern[]= '20%'; break; 
+            case 'd': $this->pattern[]= '11+-0123456789'; break;
+            case 'x': $this->pattern[]= '11x0123456789abcdefABCDEF'; break;
+            case 'f': $this->pattern[]= '11+-0123456789.'; break;
+            case 's': $this->pattern[]= "01\1\2\3\4\5\6\7\10\11\12\13\14\15\16\17\20\21\22\23\24\25\26\27\30\31\32\33\34\35\36\37\40"; break;
             case '[': {   // [^a-z]: everything except a-z, [a-z]: only a-z, []01]: only "[", "0" and "1"
               if ('^' === $pattern{$i+ 1}) {
-                $match= '0';
+                $match= '01';
                 $i++;
               } else {
-                $match= '1';
+                $match= '11';
               }
               if (FALSE === ($p= strpos($pattern, ']', $i + (']' === $pattern{$i+ 1} ? 2 : 0)))) {
                 throw new FormatException('Unmatched "]" in format string');
@@ -61,7 +61,7 @@
           }
         } else {
           $p= strcspn($pattern, '%', $i);
-          $this->pattern[].= '2'.substr($pattern, $i, $p);
+          $this->pattern[].= '20'.substr($pattern, $i, $p);
           $i+= $p- 1;
         }
       }
@@ -78,9 +78,9 @@
       $matches= 0;
       foreach ($this->pattern as $match) {
         switch ($match[0]) {
-          case '0': $l= strcspn($input, substr($match, 1), $o); break;
-          case '1': $l= strspn($input, substr($match, 1), $o); break;
-          case '2': $s= strlen($match)- 1; $l= substr($match, 1) === substr($input, $o, $s) ? $s : 0; break;
+          case '0': $l= strcspn($input, substr($match, 2), $o); break;
+          case '1': $l= strspn($input, substr($match, 2), $o); break;
+          case '2': $s= strlen($match)- 2; $l= substr($match, 2) === substr($input, $o, $s) ? $s : 0; break;
         }
         if (0 === $l) break;
         $matches++;
@@ -96,19 +96,23 @@
      * @return  text.regex.MatchResult
      */
     public function match($input) {
-      $matches= array();
+      $matches= array(0 => '');
       $o= 0;
       foreach ($this->pattern as $match) {
         switch ($match[0]) {
-          case '0': $l= strcspn($input, substr($match, 1), $o); break;
-          case '1': $l= strspn($input, substr($match, 1), $o); break;
-          case '2': $s= strlen($match)- 1; $l= substr($match, 1) === substr($input, $o, $s) ? $s : 0; break;
+          case '0': $l= strcspn($input, substr($match, 2), $o); break;
+          case '1': $l= strspn($input, substr($match, 2), $o); break;
+          case '2': $s= strlen($match)- 2; $l= substr($match, 2) === substr($input, $o, $s) ? $s : 0; break;
         }
         if (0 === $l) break;
-        $matches[]= substr($input, $o, $l);
+        $matched= substr($input, $o, $l);
+        $matches[0].= $matched;
+        $match[1] && $matches[]= $matched;
         $o+= $l;
       }
-      return new MatchResult(sizeof($matches), $matches ? array($matches) : array());
+      
+      if ('' === $matches[0]) return MatchResult::$EMPTY;
+      return new MatchResult(sizeof($matches)- 1, array($matches));
     }
   }
 ?>
