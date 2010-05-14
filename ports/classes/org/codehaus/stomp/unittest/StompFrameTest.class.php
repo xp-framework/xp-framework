2a7b3919b24b79a8d83734e5e5319e119b375faa
thekid <timm.friebe@gmail.com>
2010-05-14 09:31:29 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@14562 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/classes/org/codehaus/stomp/unittest/StompFrameTest.class.php
@@ -0,0 +1,81 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses(
+    'unittest.TestCase',
+    'org.codehaus.stomp.frame.Frame'
+  );
+
+  /**
+   * Tests STOMP frame class
+   *
+   * @see   xp://org.codehaus.stomp.frame.Frame
+   */
+  class StompFrameTest extends TestCase {
+    protected $fixture= NULL;
+
+    /**
+     * Sets up unittest and creates fixture
+     *
+     */
+    public function setUp() {
+      $this->fixture= newinstance('org.codehaus.stomp.frame.Frame', array(), '{
+        public function command() { 
+          return "test"; 
+        }
+      }');
+    }
+
+    /**
+     * Tests getHeaders()
+     *
+     */
+    #[@test]
+    public function getHeadersInitiallyEmpty() {
+      $this->assertEquals(array(), $this->fixture->getHeaders());
+    }
+
+    /**
+     * Tests addHeader() and hasHeader()
+     *
+     */
+    #[@test]
+    public function hasHeader() {
+      $this->fixture->addHeader('content-length', 200);
+      $this->assertTrue($this->fixture->hasHeader('content-length'));
+    }
+
+    /**
+     * Tests addHeader() and getHeader()
+     *
+     */
+    #[@test]
+    public function getHeader() {
+      $this->fixture->addHeader('content-length', 200);
+      $this->assertEquals(200, $this->fixture->getHeader('content-length'));
+    }
+
+    /**
+     * Tests addHeader() and getHeaders()
+     *
+     */
+    #[@test]
+    public function getHeaders() {
+      $this->fixture->addHeader('content-length', 200);
+      $this->assertEquals(array('content-length' => 200), $this->fixture->getHeaders());
+    }
+ 
+     /**
+     * Tests requiresImmediateResponse()
+     *
+     */
+    #[@test]
+    public function receiptHeader() {
+      $this->fixture->addHeader('receipt', 'message-12345');
+      $this->assertTrue($this->fixture->requiresImmediateResponse());
+    }
+ }
+?>
