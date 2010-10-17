831eae315e8f7bc252276905a01457a17af7e496
thekid <timm.friebe@gmail.com>
2010-10-17 14:01:34 +0000

    - Change lambda syntax to #{ (arg_opt) -> (stmt_or_block) } # Inspired by http://cr.openjdk.java.net/~briangoetz/lambda/lambda-state-3.html
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@14902 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/classes/net/xp_lang/tests/execution/source/LambdaTest.class.php
+++ ports/classes/net/xp_lang/tests/execution/source/LambdaTest.class.php
@@ -21,7 +21,7 @@
     #[@test]
     public function apply() {
       $this->assertEquals(array(2, 4, 6), $this->run(
-        'return apply([1, 2, 3], { $a => $a * 2 });',
+        'return apply([1, 2, 3], #{ $a -> $a * 2 });',
         array('import static net.xp_lang.tests.execution.source.Functions.apply;')
       ));
     }
@@ -33,7 +33,7 @@
     #[@test]
     public function applyWithCapturing() {
       $this->assertEquals(array(3, 6, 9), $this->run(
-        '$mul= 3; return apply([1, 2, 3], { $a => $a * $mul });',
+        '$mul= 3; return apply([1, 2, 3], #{ $a -> $a * $mul });',
         array('import static net.xp_lang.tests.execution.source.Functions.apply;')
       ));
     }
