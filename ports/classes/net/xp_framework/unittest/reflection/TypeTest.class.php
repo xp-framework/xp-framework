b0f9fab408c1ca3115b30dc8b163b1f7ffa8fc47
thekid <timm.friebe@gmail.com>
2010-10-21 16:31:15 +0000

    - Refactor Type::forName() to be more concise
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@14910 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/classes/net/xp_framework/unittest/reflection/TypeTest.class.php
+++ ports/classes/net/xp_framework/unittest/reflection/TypeTest.class.php
@@ -6,7 +6,9 @@
 
   uses(
     'unittest.TestCase',
-    'lang.Type'
+    'lang.Type',
+    'util.collections.Vector',
+    'util.collections.HashTable'
   );
 
   /**
@@ -44,12 +46,12 @@
     }
 
     /**
-     * Test bool type
+     * Test boolean type
      *
      */
     #[@test]
     public function boolType() {
-      $this->assertEquals(Primitive::$BOOLEAN, Type::forName('bool'));
+      $this->assertEquals(Primitive::$BOOLEAN, Type::forName('boolean'));
     }
 
     /**
@@ -80,84 +82,12 @@
     }
 
     /**
-     * Test "array<string, string>" (deprecated syntax)
-     *
-     */
-    #[@test]
-    public function mapOfStringToStringDeprecatedSyntax() {
-      $this->assertEquals(MapType::forName('[:string]'), Type::forName('array<string, string>'));
-    }
-
-    /**
      * Test "[:string]"
      *
      */
     #[@test]
-    public function mapOfStringToString() {
+    public function mapOfString() {
       $this->assertEquals(MapType::forName('[:string]'), Type::forName('[:string]'));
-    }
-
-    /**
-     * Test "array"
-     *
-     */
-    #[@test]
-    public function arrayKeyword() {
-      $this->assertEquals(ArrayType::forName('var[]'), Type::forName('array'));
-    }
-
-    /**
-     * Test string type mis-spelled as "char"
-     *
-     */
-    #[@test]
-    public function stringTypeVariant() {
-      $this->assertEquals(Primitive::$STRING, Type::forName('char'));
-    }
-
-    /**
-     * Test int type mis-spelled as "integer"
-     *
-     */
-    #[@test]
-    public function intTypeVariant() {
-      $this->assertEquals(Primitive::$INT, Type::forName('integer'));
-    }
-
-    /**
-     * Test double type mis-spelled as "float"
-     *
-     */
-    #[@test]
-    public function doubleTypeVariant() {
-      $this->assertEquals(Primitive::$DOUBLE, Type::forName('float'));
-    }
-
-    /**
-     * Test bool type mis-spelled as "boolean"
-     *
-     */
-    #[@test]
-    public function boolTypeVariant() {
-      $this->assertEquals(Primitive::$BOOLEAN, Type::forName('boolean'));
-    }
-
-    /**
-     * Test var type mis-spelled as "mixed"
-     *
-     */
-    #[@test]
-    public function varTypeMixedVariant() {
-      $this->assertEquals(Type::$ANY, Type::forName('mixed'));
-    }
-
-    /**
-     * Test var type mis-spelled as "*"
-     *
-     */
-    #[@test]
-    public function varTypeStarVariant() {
-      $this->assertEquals(Type::$ANY, Type::forName('*'));
     }
 
     /**
@@ -170,6 +100,65 @@
     }
 
     /**
+     * Test Object
+     *
+     */
+    #[@test]
+    public function objectTypeShortClass() {
+      $this->assertEquals(XPClass::forName('lang.Object'), Type::forName('Object'));
+    }
+
+    /**
+     * Test "Vector<string>"
+     *
+     */
+    #[@test]
+    public function generic() {
+      $this->assertEquals(
+        XPClass::forName('util.collections.Vector')->newGenericType(array(Primitive::$STRING)), 
+        Type::forName('util.collections.Vector<string>')
+      );
+    }
+
+    /**
+     * Test "Vector<string>"
+     *
+     */
+    #[@test]
+    public function genericShortClass() {
+      $this->assertEquals(
+        XPClass::forName('util.collections.Vector')->newGenericType(array(Primitive::$STRING)), 
+        Type::forName('Vector<string>')
+      );
+    }
+
+    /**
+     * Test "Vector<Vector<int>>"
+     *
+     */
+    #[@test]
+    public function genericOfGeneneric() {
+      $vectorClass= XPClass::forName('util.collections.Vector');
+      $this->assertEquals(
+        $vectorClass->newGenericType(array($vectorClass->newGenericType(array(Primitive::$INT)))), 
+        Type::forName('util.collections.Vector<util.collections.Vector<int>>')
+      );
+    }
+
+    /**
+     * Test "Vector<Vector<int>>"
+     *
+     */
+    #[@test]
+    public function genericOfGenenericShortClass() {
+      $vectorClass= XPClass::forName('util.collections.Vector');
+      $this->assertEquals(
+        $vectorClass->newGenericType(array($vectorClass->newGenericType(array(Primitive::$INT)))), 
+        Type::forName('Vector<Vector<int>>')
+      );
+    }
+
+    /**
      * Test util.collections.HashTable<String, Object>
      *
      */
@@ -179,7 +168,110 @@
         $this->assertInstanceOf('lang.XPClass', $t);
         $this->assertTrue($t->isGeneric());
         $this->assertEquals(XPClass::forName('util.collections.HashTable'), $t->genericDefinition());
+        $this->assertEquals(
+          array(XPClass::forName('lang.types.String'), XPClass::forName('lang.Object')), 
+          $t->genericArguments()
+        );
       }
+    }
+
+    /**
+     * Test "array"
+     *
+     * @deprecated
+     */
+    #[@test]
+    public function arrayKeyword() {
+      $this->assertEquals(ArrayType::forName('var[]'), Type::forName('array'));
+    }
+
+    /**
+     * Test string type mis-spelled as "char"
+     *
+     * @deprecated
+     */
+    #[@test]
+    public function stringTypeVariant() {
+      $this->assertEquals(Primitive::$STRING, Type::forName('char'));
+    }
+
+    /**
+     * Test int type mis-spelled as "integer"
+     *
+     * @deprecated
+     */
+    #[@test]
+    public function intTypeVariant() {
+      $this->assertEquals(Primitive::$INT, Type::forName('integer'));
+    }
+
+    /**
+     * Test "array<string, string>" (deprecated syntax)
+     *
+     * @deprecated
+     */
+    #[@test]
+    public function mapOfStringDeprecatedSyntax() {
+      $this->assertEquals(MapType::forName('[:string]'), Type::forName('array<string, string>'));
+    }
+
+    /**
+     * Test "array<string>" (deprecated syntax)
+     *
+     * @deprecated
+     */
+    #[@test]
+    public function stringArrayDeprecatedSyntax() {
+      $this->assertEquals(ArrayType::forName('string[]'), Type::forName('array<string>'));
+    }
+
+    /**
+     * Test double type mis-spelled as "float"
+     *
+     * @deprecated
+     */
+    #[@test]
+    public function doubleTypeVariant() {
+      $this->assertEquals(Primitive::$DOUBLE, Type::forName('float'));
+    }
+
+    /**
+     * Test bool type mis-spelled as "boolean"
+     *
+     * @deprecated
+     */
+    #[@test]
+    public function booleanTypeVariant() {
+      $this->assertEquals(Primitive::$BOOLEAN, Type::forName('bool'));
+    }
+
+    /**
+     * Test var type mis-spelled as "mixed"
+     *
+     * @deprecated
+     */
+    #[@test]
+    public function varTypeMixedVariant() {
+      $this->assertEquals(Type::$VAR, Type::forName('mixed'));
+    }
+
+    /**
+     * Test var type mis-spelled as "*"
+     *
+     * @deprecated
+     */
+    #[@test]
+    public function varTypeStarVariant() {
+      $this->assertEquals(Type::$VAR, Type::forName('*'));
+    }
+
+    /**
+     * Test "resource" as type name
+     *
+     */
+    #[@test]
+    public function resourceType() {
+      $this->assertEquals(Type::$VAR, Type::forName('resource'));
     }
 
     /**
