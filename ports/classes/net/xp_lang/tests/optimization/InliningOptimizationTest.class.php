07b71fcc91955d366c5924c417338ec067872ac3
thekid <timm.friebe@gmail.com>
2010-11-21 15:20:10 +0000

    - Add tests for parameter rewriting
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@15016 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/classes/net/xp_lang/tests/optimization/InliningOptimizationTest.class.php
+++ ports/classes/net/xp_lang/tests/optimization/InliningOptimizationTest.class.php
@@ -12,8 +12,10 @@
     'xp.compiler.ast.MethodCallNode',
     'xp.compiler.ast.StaticMethodCallNode',
     'xp.compiler.ast.MethodNode',
+    'xp.compiler.ast.MemberAccessNode',
     'xp.compiler.ast.VariableNode',
     'xp.compiler.ast.UnaryOpNode',
+    'xp.compiler.ast.BinaryOpNode',
     'xp.compiler.ast.ReturnNode',
     'xp.compiler.ast.ParseTree',
     'xp.compiler.types.TypeName',
@@ -144,6 +146,54 @@
           'body'        => array(
             new ReturnNode(
               new UnaryOpNode(array('op' => '++', 'postfix' => FALSE, 'expression' => new VariableNode('in')))
+            )
+          )
+        )))
+      );
+    }
+
+    /**
+     * Test parameter rewriting with the following declaration:
+     * <code>
+     *   inline T add(T $x, T $y) { return $x + $y; }
+     * </code>
+     */
+    #[@test]
+    public function parameterRewritingWithTwoParameters() {
+      $call= new MethodCallNode(new VariableNode('this'), 'add', array(new VariableNode('a'), new VariableNode('b')));
+      $this->assertEquals(
+        new BinaryOpNode(array('lhs' => new VariableNode('a'), 'rhs' => new VariableNode('b'), 'op' => '+')), 
+        $this->optimize($call, new MethodNode(array(
+          'modifiers'   => MODIFIER_INLINE,
+          'name'        => 'add',
+          'parameters'  => array(array('name' => 'x'), array('name' => 'y')),
+          'body'        => array(
+            new ReturnNode(
+              new BinaryOpNode(array('lhs' => new VariableNode('x'), 'rhs' => new VariableNode('y'), 'op' => '+'))
+            )
+          )
+        )))
+      );
+    }
+
+    /**
+     * Test parameter rewriting with the following declaration:
+     * <code>
+     *   inline T inc(T $x) { return $x + $this.step; }
+     * </code>
+     */
+    #[@test]
+    public function parameterRewritingWithOneParameterAndOneMember() {
+      $call= new MethodCallNode(new VariableNode('this'), 'inc', array(new VariableNode('a')));
+      $this->assertEquals(
+        new BinaryOpNode(array('lhs' => new VariableNode('a'), 'rhs' => new MemberAccessNode(new VariableNode('this'), 'step'), 'op' => '+')), 
+        $this->optimize($call, new MethodNode(array(
+          'modifiers'   => MODIFIER_INLINE,
+          'name'        => 'inc',
+          'parameters'  => array(array('name' => 'x')),
+          'body'        => array(
+            new ReturnNode(
+              new BinaryOpNode(array('lhs' => new VariableNode('x'), 'rhs' => new MemberAccessNode(new VariableNode('this'), 'step'), 'op' => '+'))
             )
           )
         )))
