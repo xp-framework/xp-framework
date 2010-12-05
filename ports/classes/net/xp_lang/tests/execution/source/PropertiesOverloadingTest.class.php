aac076f33791af9d76a1ef4a1fa704951d2c4870
thekid <timm.friebe@gmail.com>
2010-12-05 19:56:50 +0000

    - Add tests for:   . Completely overridden behaviour (minutes property)   . Not overridden behaviour (seconds property)
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@15060 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/classes/net/xp_lang/tests/execution/source/PropertiesOverloadingTest.class.php
+++ ports/classes/net/xp_lang/tests/execution/source/PropertiesOverloadingTest.class.php
@@ -40,10 +40,25 @@
             get { return $this.seconds / 3600; }
             set { $this.seconds= 3600 * $value; }
           }
+
+          public double minutes {
+            get { return $this.seconds / 60; }
+            set { $this.seconds= $value * 60; }
+          }
+
+          public int seconds {
+            get { return $this.seconds; }
+            set { $this.seconds= $value; }
+          }
         }');
         self::$child= $this->define('class', 'WholeTimePeriod', self::$base, '{
           public int hours {
             get { return floor($this.seconds / 3600) as int; }
+          }
+
+          public int minutes {
+            get { return floor($this.seconds / 60) as int; }
+            set { $this.seconds= $value * 60; }
           }
         }', array('import native standard.floor;'));
       } catch (Throwable $e) {
@@ -92,6 +107,68 @@
       $period= self::$child->newInstance();
       $period->hours= 3700 / 3600;
       $this->assertEquals(1, $period->hours);
+    }
+
+    /**
+     * Test base class
+     *
+     */
+    #[@test]
+    public function readMinutesBase() {
+      $period= self::$base->newInstance(3700);
+      $this->assertEquals(3700 / 60, $period->minutes);
+    }
+
+    /**
+     * Test base class
+     *
+     */
+    #[@test]
+    public function writeMinutesBase() {
+      $period= self::$base->newInstance();
+      $period->minutes= 3700 / 60;
+      $this->assertEquals(3700 / 60, $period->minutes);
+    }
+
+    /**
+     * Test child class
+     *
+     */
+    #[@test]
+    public function readMinutesChild() {
+      $period= self::$child->newInstance(3700);
+      $this->assertEquals(61, $period->minutes);
+    }
+
+    /**
+     * Test child class
+     *
+     */
+    #[@test]
+    public function writeMinutesChild() {
+      $period= self::$child->newInstance();
+      $period->minutes= 3700 / 60;
+      $this->assertEquals(61, $period->minutes);
+    }
+
+    /**
+     * Test base class
+     *
+     */
+    #[@test]
+    public function readSecondsBase() {
+      $period= self::$base->newInstance(3700);
+      $this->assertEquals(3700, $period->seconds);
+    }
+
+    /**
+     * Test child class
+     *
+     */
+    #[@test]
+    public function readSecondsChild() {
+      $period= self::$child->newInstance(3700);
+      $this->assertEquals(3700, $period->seconds);
     }
   }
 ?>
