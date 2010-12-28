3452791933caf7cbbb4fa491f43f0db6580b27ee
thekid <timm.friebe@gmail.com>
2010-12-28 18:31:08 +0000

    - Test more permutations   . Missing section   . Empty section   . Multiple sources   . Multiple sources with duplicate entries
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@15112 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/classes/net/xp_lang/tests/CompilationProfileReaderTest.class.php
+++ ports/classes/net/xp_lang/tests/CompilationProfileReaderTest.class.php
@@ -37,6 +37,31 @@
     protected function newProperties($source) {
       return new Properties(Streams::readableUri(new MemoryInputStream($source)));
     }
+
+    /**
+     * Test warnings section parsing
+     *
+     */
+    #[@test]
+    public function noWarningsMissingSection() {
+      $this->fixture->addSource($this->newProperties('
+      '));
+
+      $this->assertEquals(array(), array_keys($this->fixture->getProfile()->warnings));
+    }
+
+    /**
+     * Test warnings section parsing
+     *
+     */
+    #[@test]
+    public function noWarningsEmptySection() {
+      $this->fixture->addSource($this->newProperties('
+        [warnings]
+      '));
+
+      $this->assertEquals(array(), array_keys($this->fixture->getProfile()->warnings));
+    }
     
     /**
      * Test warnings section parsing
@@ -71,6 +96,73 @@
         array('xp.compiler.checks.TypeHasDocumentation', 'xp.compiler.checks.TypeMemberHasDocumentation'),
         array_keys($this->fixture->getProfile()->warnings)
       );
+    }
+
+    /**
+     * Test warnings section parsing
+     *
+     */
+    #[@test]
+    public function twoWarningsViaTwoSources() {
+      $this->fixture->addSource($this->newProperties('
+        [warnings]
+        class[]=xp.compiler.checks.TypeHasDocumentation
+      '));
+      $this->fixture->addSource($this->newProperties('
+        [warnings]
+        class[]=xp.compiler.checks.TypeMemberHasDocumentation
+      '));
+
+      $this->assertEquals(
+        array('xp.compiler.checks.TypeHasDocumentation', 'xp.compiler.checks.TypeMemberHasDocumentation'),
+        array_keys($this->fixture->getProfile()->warnings)
+      );
+    }
+
+    /**
+     * Test warnings section parsing
+     *
+     */
+    #[@test]
+    public function sameWarningViaTwoSources() {
+      $this->fixture->addSource($this->newProperties('
+        [warnings]
+        class[]=xp.compiler.checks.TypeHasDocumentation
+      '));
+      $this->fixture->addSource($this->newProperties('
+        [warnings]
+        class[]=xp.compiler.checks.TypeHasDocumentation
+      '));
+
+      $this->assertEquals(
+        array('xp.compiler.checks.TypeHasDocumentation'),
+        array_keys($this->fixture->getProfile()->warnings)
+      );
+    }
+
+    /**
+     * Test errors section parsing
+     *
+     */
+    #[@test]
+    public function noErrorsMissingSection() {
+      $this->fixture->addSource($this->newProperties('
+      '));
+
+      $this->assertEquals(array(), array_keys($this->fixture->getProfile()->errors));
+    }
+
+    /**
+     * Test errors section parsing
+     *
+     */
+    #[@test]
+    public function noErrorsEmptySection() {
+      $this->fixture->addSource($this->newProperties('
+        [errors]
+      '));
+
+      $this->assertEquals(array(), array_keys($this->fixture->getProfile()->errors));
     }
 
     /**
@@ -109,6 +201,73 @@
     }
 
     /**
+     * Test errors section parsing
+     *
+     */
+    #[@test]
+    public function twoErrorsViaTwoSources() {
+      $this->fixture->addSource($this->newProperties('
+        [errors]
+        class[]=xp.compiler.checks.TypeHasDocumentation
+      '));
+      $this->fixture->addSource($this->newProperties('
+        [errors]
+        class[]=xp.compiler.checks.TypeMemberHasDocumentation
+      '));
+
+      $this->assertEquals(
+        array('xp.compiler.checks.TypeHasDocumentation', 'xp.compiler.checks.TypeMemberHasDocumentation'),
+        array_keys($this->fixture->getProfile()->errors)
+      );
+    }
+
+    /**
+     * Test errors section parsing
+     *
+     */
+    #[@test]
+    public function sameErrorViaTwoSources() {
+      $this->fixture->addSource($this->newProperties('
+        [errors]
+        class[]=xp.compiler.checks.TypeHasDocumentation
+      '));
+      $this->fixture->addSource($this->newProperties('
+        [errors]
+        class[]=xp.compiler.checks.TypeHasDocumentation
+      '));
+
+      $this->assertEquals(
+        array('xp.compiler.checks.TypeHasDocumentation'),
+        array_keys($this->fixture->getProfile()->errors)
+      );
+    }
+
+    /**
+     * Test optimizations section parsing
+     *
+     */
+    #[@test]
+    public function noOptimizationsMissingSection() {
+      $this->fixture->addSource($this->newProperties('
+      '));
+
+      $this->assertEquals(array(), array_keys($this->fixture->getProfile()->optimizations));
+    }
+
+    /**
+     * Test optimizations section parsing
+     *
+     */
+    #[@test]
+    public function noOptimizationsEmptySection() {
+      $this->fixture->addSource($this->newProperties('
+        [optimizations]
+      '));
+
+      $this->assertEquals(array(), array_keys($this->fixture->getProfile()->optimizations));
+    }
+
+    /**
      * Test optimizations section parsing
      *
      */
@@ -139,6 +298,48 @@
 
       $this->assertEquals(
         array('xp.compiler.optimize.BinaryOptimization', 'xp.compiler.optimize.DeadCodeElimination'),
+        array_keys($this->fixture->getProfile()->optimizations)
+      );
+    }
+
+    /**
+     * Test optimizations section parsing
+     *
+     */
+    #[@test]
+    public function twoOptimizationsViaTwoSources() {
+      $this->fixture->addSource($this->newProperties('
+        [optimizations]
+        class[]=xp.compiler.optimize.BinaryOptimization
+      '));
+      $this->fixture->addSource($this->newProperties('
+        [optimizations]
+        class[]=xp.compiler.optimize.DeadCodeElimination
+      '));
+
+      $this->assertEquals(
+        array('xp.compiler.optimize.BinaryOptimization', 'xp.compiler.optimize.DeadCodeElimination'),
+        array_keys($this->fixture->getProfile()->optimizations)
+      );
+    }
+
+    /**
+     * Test optimizations section parsing
+     *
+     */
+    #[@test]
+    public function sameOptimizationViaTwoSources() {
+      $this->fixture->addSource($this->newProperties('
+        [optimizations]
+        class[]=xp.compiler.optimize.BinaryOptimization
+      '));
+      $this->fixture->addSource($this->newProperties('
+        [optimizations]
+        class[]=xp.compiler.optimize.BinaryOptimization
+      '));
+
+      $this->assertEquals(
+        array('xp.compiler.optimize.BinaryOptimization'),
         array_keys($this->fixture->getProfile()->optimizations)
       );
     }
