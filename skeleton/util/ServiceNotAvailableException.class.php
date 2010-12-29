1bae169ed9db3e0307f5c577f7278342d9878403
thekid <timm.friebe@gmail.com>
2010-12-29 10:42:03 +0000

    - Initial release
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@15120 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ skeleton/util/ServiceNotAvailableException.class.php
@@ -0,0 +1,17 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses('util.ServiceException');
+
+  /**
+   * Indicates a service is not available
+   *
+   * @see   xp://util.ServiceException
+   */
+  class ServiceNotAvailableException extends ServiceException {
+    
+  }
+?>
