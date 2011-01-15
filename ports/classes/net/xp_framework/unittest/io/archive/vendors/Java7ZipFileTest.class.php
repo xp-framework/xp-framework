7340922dce012c5cc3bf2a50aa8efac0e2eb8647
thekid <timm.friebe@gmail.com>
2011-01-15 15:39:29 +0000

    - Initial release # See http://blogs.sun.com/xuemingshen/entry/non_utf_8_encoding_in
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@15190 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ ports/classes/net/xp_framework/unittest/io/archive/vendors/Java7ZipFileTest.class.php
@@ -0,0 +1,38 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('net.xp_framework.unittest.io.archive.vendors.ZipFileVendorTest');
+
+  /**
+   * Tests ZIP file implementation with ZIP files created by
+   * Java 7's java.util.zip API
+   *
+   * @see   http://blogs.sun.com/xuemingshen/entry/non_utf_8_encoding_in
+   * @see   http://cr.openjdk.java.net/~sherman/4244499/webrev/
+   * @see   http://cr.openjdk.java.net/~sherman/4244499/webrev/test/java/util/zip/zip.java.patch
+   * @see   http://java.sun.com/javase/6/docs/api/java/util/zip/package-summary.html
+   */
+  class Java7ZipFileTest extends ZipFileVendorTest {
+    
+    /**
+     * Returns vendor name
+     *
+     * @return  string
+     */
+    protected function vendorName() {
+      return 'java7';
+    }
+
+    /**
+     * Tests reading an empty zipfile
+     *
+     */
+    #[@test, @ignore('Cannot create empty zipfiles with java.util.zip')]
+    public function emptyZipFile() {
+      parent::emptyZipFile();
+    }
+  }
+?>
