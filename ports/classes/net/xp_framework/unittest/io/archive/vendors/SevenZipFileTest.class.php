8350be25706ae9c1d6d0e80b636566ce6fa35a94
thekid <timm.friebe@gmail.com>
2011-01-15 17:35:46 +0000

    - Add tests for different compression algorithms # Currently not supported are Deflate64, LZMA and PPMD
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@15196 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- ports/classes/net/xp_framework/unittest/io/archive/vendors/SevenZipFileTest.class.php
+++ ports/classes/net/xp_framework/unittest/io/archive/vendors/SevenZipFileTest.class.php
@@ -4,7 +4,7 @@
  * $Id$
  */
 
-  uses('net.xp_framework.unittest.io.archive.vendors.ZipFileVendorTest');
+  uses('net.xp_framework.unittest.io.archive.vendors.ZipFileVendorTest', 'io.streams.Streams');
 
   /**
    * Tests 7-ZIP archives
@@ -20,6 +20,69 @@
      */
     protected function vendorName() {
       return '7zip';
+    }
+    
+    /**
+     * Assertion helper
+     *
+     * @param   io.archive.zip.ZipArchiveReader reader
+     * @throws  unittest.AssertionFailedError
+     */
+    protected function assertCompressedEntryIn($reader) {
+      $entry= $reader->iterator()->next();
+      $this->assertEquals('compression.txt', $entry->getName());
+      $this->assertEquals(1660, $entry->getSize());
+      
+      with ($is= $entry->getInputStream()); {
+        $this->assertEquals('This file is to be compressed', $is->read(29));
+        $is->read(1630);
+        $this->assertEquals('.', $is->read(1));
+      }
+    }
+
+    /**
+     * Tests deflate algorithm
+     *
+     */
+    #[@test]
+    public function deflate() {
+      $this->assertCompressedEntryIn($this->archiveReaderFor($this->vendor, 'deflate'));
+    }
+
+    /**
+     * Tests bzip2 algorithm
+     *
+     */
+    #[@test]
+    public function bzip2() {
+      $this->assertCompressedEntryIn($this->archiveReaderFor($this->vendor, 'bzip2'));
+    }
+
+    /**
+     * Tests deflate64 algorithm
+     *
+     */
+    #[@test, @ignore('Not yet supported')]
+    public function deflate64() {
+      $this->assertCompressedEntryIn($this->archiveReaderFor($this->vendor, 'deflate64'));
+    }
+
+    /**
+     * Tests lzma algorithm
+     *
+     */
+    #[@test, @ignore('Not yet supported')]
+    public function lzma() {
+      $this->assertCompressedEntryIn($this->archiveReaderFor($this->vendor, 'lzma'));
+    }
+
+    /**
+     * Tests ppmd algorithm
+     *
+     */
+    #[@test, @ignore('Not yet supported')]
+    public function ppmd() {
+      $this->assertCompressedEntryIn($this->archiveReaderFor($this->vendor, 'ppmd'));
     }
   }
 ?>
