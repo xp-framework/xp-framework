16a707c2b286a7e8fab61d6016fc11291e9e1e29
thekid <timm.friebe@gmail.com>
2011-01-17 01:33:15 +0000

    - Add floor(), ceil() and round()
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@15224 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/math/BigFloat.class.php
+++ skeleton/math/BigFloat.class.php
@@ -7,11 +7,68 @@
   uses('math.BigNum');
 
   /**
-   * (Insert class' description here)
+   * A big float
    *
-   * @ext      xp://math.BigNum
+   * @see   xp://math.BigNum
    */
   class BigFloat extends BigNum {
-    
+
+    /**
+     * Creates a new bigfloat instance
+     *
+     * @param   string in
+     */
+    public function __construct($in) {
+      parent::__construct(FALSE !== strpos($in, '.') ? rtrim(rtrim($in, '0'), '.') : $in);
+    }
+  
+    /**
+     * Returns the next lowest "integer" value by rounding down value if necessary. 
+     *
+     * @return  math.BigFloat
+     */
+    public function ceil() {
+      return new self(FALSE === strpos($this->num, '.') 
+        ? $this->num 
+        : ('-' === $this->num{0} ? bcsub($this->num, 0, 0) : bcadd($this->num, 1, 0))
+      );
+    }
+
+    /**
+     * Returns the next highest "integer" value by rounding up value if necessary
+     *
+     * @return  math.BigFloat
+     */
+    public function floor() {
+      return new self(FALSE === strpos($this->num, '.') 
+        ? $this->num 
+        : ('-' === $this->num{0} ? bcsub($this->num, 1, 0) : bcadd($this->num, 0, 0))
+      );
+    }
+
+    /**
+     * Returns the next highest "integer" value by rounding up value if necessary
+     *
+     * @param   int precision
+     * @return  math.BigFloat
+     */
+    public function round($precision= 0) {
+      if (FALSE === strpos($this->num, '.')) return new self($this->num);
+      
+      $a= '0.'.str_repeat('0', $precision).'5';
+      return new self(FALSE === strpos($this->num, '.') 
+        ? $this->num 
+        : ('-' === $this->num{0} ? bcsub($this->num, $a, $precision) : bcadd($this->num, $a, $precision))
+      );
+    }
+
+    /**
+     * String cast overloading
+     *
+     * @return  string
+     */
+    public function __toString() {
+      return $this->num;
+    }
   }
 ?>
