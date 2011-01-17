45ff4f8c69b11031dc8d6353f3d976a1b7789477
thekid <timm.friebe@gmail.com>
2011-01-17 09:37:03 +0000

    - Use {op}0 methods where we can be sure to work with ints only - Use divide(float) instead of divide(int) to get correct results
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@15240 d2cacbed-c0f6-0310-851a-9dad52fc623a

--- skeleton/rdbms/mysqlx/MySqlPassword.class.php
+++ skeleton/rdbms/mysqlx/MySqlPassword.class.php
@@ -30,10 +30,10 @@
           for ($i= 0, $s= strlen($in); $i < $s; $i++) {
             $ord= ord($in{$i});
             if (0x20 === $ord || 0x09 === $ord) continue;
-            $value= $nr->bitwiseAnd(63)->add($add)->multiply($ord)->add($nr->multiply(0x100));
+            $value= $nr->bitwiseAnd(63)->add0($add)->multiply0($ord)->add0($nr->multiply0(0x100));
             $nr= $nr->bitwiseXor($value);
-            $nr2= $nr2->multiply(0x100)->bitwiseXor($nr)->add($nr2);
-            $add= $add->add($ord);
+            $nr2= $nr2->multiply0(0x100)->bitwiseXor($nr)->add0($nr2);
+            $add= $add->add0($ord);
           }
           return array($nr->bitwiseAnd(0x7FFFFFFF), $nr2->bitwiseAnd(0x7FFFFFFF));
         }
@@ -49,16 +49,14 @@
           $seed2= $hp[1]->bitwiseXor($hm[1])->modulo($SEED_MAX);
           $to= "";
           for ($i= 0, $s= strlen($message); $i < $s; $i++) {
-            $seed1= $seed1->multiply(3)->add($seed2)->modulo($SEED_MAX);
-            $seed2= $seed1->add($seed2)->add(33)->modulo($SEED_MAX);
-            $rnd= $seed1->divide($SEED_MAX)->multiply(31);
-            $to.= chr($rnd->intValue() + 64);
+            $seed1= $seed1->multiply0(3)->add0($seed2)->modulo($SEED_MAX);
+            $seed2= $seed1->add0($seed2)->add0(33)->modulo($SEED_MAX);
+            $to.= chr($seed1->divide(new BigFloat($SEED_MAX))->multiply(31)->intValue() + 64);
           }
-          $seed1= $seed1->multiply(3)->add($seed2)->modulo($SEED_MAX);
-          $seed2= $seed1->add($seed2)->add(33)->modulo($SEED_MAX);
-          $rnd= $seed1->divide($SEED_MAX)->multiply(31);
+          $seed1= $seed1->multiply0(3)->add0($seed2)->modulo($SEED_MAX);
+          $seed2= $seed1->add0($seed2)->add0(33)->modulo($SEED_MAX);
 
-          return $to ^ str_repeat(chr($rnd->intValue()), strlen($message));
+          return $to ^ str_repeat(chr($seed1->divide(new BigFloat($SEED_MAX))->multiply(31)->intValue()), strlen($message));
         }
       }');
       self::$PROTOCOL_41= newinstance(__CLASS__, array(1, 'PROTOCOL_41'), '{
