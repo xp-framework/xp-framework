e7cfa01e79f42e38b572d0aa0adc55a72bba5d1d
Timm Friebe <thekid@thekid.de>
2011-08-03 11:27:07 +0200

    - Initial release # Part of implementation for issue #24

---
+++ tools/src/main/php/xp/unittest/QuietListener.class.php
@@ -0,0 +1,101 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses('unittest.TestListener');
+
+  /**
+   * Quiet listener - no output at all
+   *
+   * @purpose  TestListener
+   */
+  class QuietListener extends Object implements TestListener {
+
+
+    /**
+     * Called when a test case starts.
+     *
+     * @param   unittest.TestCase failure
+     */
+    public function testStarted(TestCase $case) {
+      // NOOP
+    }
+
+    /**
+     * Called when a test fails.
+     *
+     * @param   unittest.TestFailure failure
+     */
+    public function testFailed(TestFailure $failure) {
+      // NOOP
+    }
+
+    /**
+     * Called when a test errors.
+     *
+     * @param   unittest.TestError error
+     */
+    public function testError(TestError $error) {
+      // NOOP
+    }
+
+    /**
+     * Called when a test raises warnings.
+     *
+     * @param   unittest.TestWarning warning
+     */
+    public function testWarning(TestWarning $warning) {
+      // NOOP
+    }
+    
+    /**
+     * Called when a test finished successfully.
+     *
+     * @param   unittest.TestSuccess success
+     */
+    public function testSucceeded(TestSuccess $success) {
+      // NOOP
+    }
+    
+    /**
+     * Called when a test is not run because it is skipped due to a 
+     * failed prerequisite.
+     *
+     * @param   unittest.TestSkipped skipped
+     */
+    public function testSkipped(TestSkipped $skipped) {
+      // NOOP
+    }
+
+    /**
+     * Called when a test is not run because it has been ignored by using
+     * the @ignore annotation.
+     *
+     * @param   unittest.TestSkipped ignore
+     */
+    public function testNotRun(TestSkipped $ignore) {
+      // NOOP
+    }
+
+    /**
+     * Called when a test run starts.
+     *
+     * @param   unittest.TestSuite suite
+     */
+    public function testRunStarted(TestSuite $suite) {
+      // NOOP
+    }
+    
+    /**
+     * Called when a test run finishes.
+     *
+     * @param   unittest.TestSuite suite
+     * @param   unittest.TestResult result
+     */
+    public function testRunFinished(TestSuite $suite, TestResult $result) {
+      // NOOP
+    }
+  }
+?>
