5f5108802119d3af4b036956f0a7b48702222f80
Stormwind <stormwind@stormwinds-page.de>
2011-10-19 13:36:16 +0200

    - add generic ElementAlreadyExistsException

---
+++ core/src/main/php/util/ElementAlreadyExistsException.class.php
@@ -0,0 +1,14 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  /**
+   * Represents a ElementAlreadyExistsException
+   *
+   * @purpose  Exception
+   */
+  class ElementAlreadyExistsException extends XPException {
+  }
+?>
