c70f8f1db325cf6c9fdda4097500e2ad252ba315
Timm Friebe <thekid@thekid.de>
2011-10-29 13:59:09 +0200

    - Refactor magic quotes related test fatalsForMagicQuotesGPC()   into a separate class and add test methods for PHP 5.2, 5.3 and 5.4   which all expose distinct behaviour # See issue #76

---
+++ core/src/test/php/net/xp_framework/unittest/core/MagicQuotesTest.class.php
@@ -0,0 +1,102 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses(
+    'unittest.TestCase',
+    'lang.Runtime'
+  );
+
+  /**
+   * Testcase ensuring XP refuses to start 
+   *
+   */
+  class MagicQuotesTest extends TestCase {
+  
+    /**
+     * Setup test. Verifies PHP version constraint
+     *
+     */
+    public function setUp() {
+      static $ops= array(
+        'l' => array('[' =>  'ge', ']' => 'gt'),
+        'u' => array('[' =>  'lt', ']' => 'le'),
+      );
+
+      $constraint= $this->getClass()->getMethod($this->name)->getAnnotation('runtime');
+      $lim= explode(',', $constraint, 2);
+      $cmp= substr(PHP_VERSION, 0, 5);
+      $result= (
+        ($lim[0] ? version_compare($cmp, substr($lim[0], 1), $ops['l'][$lim[0]{0}]) : TRUE) &&
+        ($lim[1] ? version_compare($cmp, substr($lim[1], 0, -1), $ops['u'][$lim[1]{strlen($lim[1]) - 1}]) : TRUE)
+      );
+
+      if (!$result) {
+        throw new PrerequisitesNotMetError('PHP version '.$compare.' not compatible', NULL, $limits);
+      }
+    }
+  
+    /**
+     * Create a new runtime
+     *
+     * @return  var[] an array with three elements: exitcode, stdout and stderr contents
+     */
+    protected function run() {
+      $runtime= Runtime::getInstance();
+      $options= $runtime->startupOptions()->withSetting('magic_quotes_gpc', 1)->withSetting('error_reporting', 'E_ALL');
+      $out= $err= '';
+
+      with ($p= $runtime->newInstance($options, NULL)); {
+        $p->in->write('<?php require("lang.base.php"); ?>');
+        $p->in->close();
+
+        // Read output
+        while ($b= $p->out->read()) { $out.= $b; }
+        while ($b= $p->err->read()) { $err.= $b; }
+
+        // Close child process
+        $exitv= $p->close();
+      }
+      return array($exitv, $out, $err);
+    }
+
+    /**
+     * Before PHP 5.3, the XP Framework is the only one that has a problem
+     * with magic quotes.
+     *
+     */
+    #[@test, @runtime(',5.3.0[')]
+    public function xpFrameworkRefusesToStart() {
+      $r= $this->run();
+      $this->assertEquals(255, $r[0], 'exitcode');
+      $this->assertTrue((bool)strstr($r[1], "[xp::core] magic_quotes_gpc enabled"), $r[1]);
+    }
+
+    /**
+     * As of PHP 5.3+, magic_quotes_gpc = On raises a deprecation warning 
+     * to standard error. 
+     *
+     */
+    #[@test, @runtime('[5.3.0,5.4.0[')]
+    public function xpFrameworkRefusesToStartAndDeprecationWarning() {
+      $r= $this->run();
+      $this->assertEquals(255, $r[0], 'exitcode');
+      $this->assertTrue((bool)strstr($r[1], "[xp::core] magic_quotes_gpc enabled"), $r[1]);
+      $this->assertTrue((bool)strstr($r[2], "Directive 'magic_quotes_gpc' is deprecated in PHP 5.3 and greater"), $r[2]);
+    }
+    
+    /**
+     * As of PHP 5.4, magic quotes have been removed and enabling them will
+     * cause PHP to issue a fatal error.
+     *
+     */
+    #[@test, @runtime('[5.4.0,')]
+    public function phpRefusesToStart() {
+      $r= $this->run();
+      $this->assertEquals(1, $r[0], 'exitcode');
+      $this->assertTrue((bool)strstr($r[1], "Directive 'magic_quotes_gpc' is no longer available in PHP"), $r[1]);
+    }
+  }
+?>
