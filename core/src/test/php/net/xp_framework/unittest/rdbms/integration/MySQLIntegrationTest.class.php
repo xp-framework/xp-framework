0bbff53ac615d8723000fbdc8fc95fc42e63a4fc
Timm Friebe <thekid@thekid.de>
2011-11-24 12:55:36 +0100

    - Remove non-portable check for field value in setUp()

--- core/src/test/php/net/xp_framework/unittest/rdbms/integration/MySQLIntegrationTest.class.php
+++ core/src/test/php/net/xp_framework/unittest/rdbms/integration/MySQLIntegrationTest.class.php
@@ -14,17 +14,6 @@
   class MySQLIntegrationTest extends RdbmsIntegrationTest {
 
     /**
-     * Set up testcase
-     *
-     */
-    public function setUp() {
-      parent::setUp();
-      if ('real' !== ($q= $this->db()->query('select 0.5 as value')->fields['value'])) {
-        throw new PrerequisitesNotMetError('Broken mysql library - select 0.5 yields '.$q.' as type');
-      }
-    }
-    
-    /**
      * Retrieve dsn
      *
      * @return  string
