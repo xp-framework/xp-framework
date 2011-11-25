e4eaf20ac8da25d5a5ee7f7f2906ee2587213aa8
Timm Friebe <thekid@thekid.de>
2011-11-25 22:52:43 +0100

    - First working implementation of row reading # Only for int and varchars, but working!

--- core/src/main/php/rdbms/tds/TdsV7Protocol.class.php
+++ core/src/main/php/rdbms/tds/TdsV7Protocol.class.php
@@ -211,7 +211,7 @@
       $length= strlen($arg)+ 8;
       $packet= pack('CCnnCc', $type, self::EOM, $length, 0x0000, $this->pkt, 0).$arg;
 
-      Console::writeLine('W-> ', array(
+      Console::$err->writeLine('W-> ', array(
         'type'    => $type,
         'status'  => self::EOM,
         'length'  => $length,
@@ -219,7 +219,7 @@
         'packet'  => $this->pkt,
         'window'  => 0
       ));
-      Console::writeLine(self::dump($packet));
+      Console::$err->writeLine(self::dump($packet));
  
       $this->sock->write($packet);
       $this->pkt= $this->pkt+ 1 & 0xFF;
@@ -261,6 +261,73 @@
       return $chunk;
     }
     
+    const SYBCHAR               = 47; // 0x2F
+    const SYBVARCHAR            = 39; // 0x27
+    const SYBINTN               = 38; // 0x26
+    const SYBINT1               = 48; // 0x30
+    const SYBDATE               = 49; // 0x31 Sybase 12
+    const SYBTIME               = 51; // 0x33 Sybase 12
+    const SYBINT2               = 52; // 0x34
+    const SYBINT4               = 56; // 0x38
+    const SYBINT8               = 127;// 0x7F
+    const SYBFLT8               = 62; // 0x3E
+    const SYBDATETIME           = 61; // 0x3D
+    const SYBBIT                = 50; // 0x32
+    const SYBTEXT               = 35; // 0x23
+    const SYBNTEXT              = 99; // 0x63
+    const SYBIMAGE              = 34; // 0x22
+    const SYBMONEY4             = 122;// 0x7A
+    const SYBMONEY              = 60; // 0x3C
+    const SYBDATETIME4          = 58; // 0x3A
+    const SYBREAL               = 59; // 0x3B
+    const SYBBINARY             = 45; // 0x2D
+    const SYBVOID               = 31; // 0x1F
+    const SYBVARBINARY          = 37; // 0x25
+    const SYBNVARCHAR           = 103;// 0x67
+    const SYBBITN               = 104;// 0x68
+    const SYBNUMERIC            = 108;// 0x6C
+    const SYBDECIMAL            = 106;// 0x6A
+    const SYBFLTN               = 109;// 0x6D
+    const SYBMONEYN             = 110;// 0x6E
+    const SYBDATETIMN           = 111;// 0x6F
+    const SYBDATEN              = 123;// 0x7B SYBASE 12
+    const SYBTIMEN              = 147;// 0x93 SYBASE 12
+    const XSYBCHAR              = 175;// 0xAF
+    const XSYBVARCHAR           = 167;// 0xA7
+    const XSYBNVARCHAR          = 231;// 0xE7
+    const XSYBNCHAR             = 239;// 0xEF
+    const XSYBVARBINARY         = 165;// 0xA5
+    const XSYBBINARY            = 173;// 0xAD
+    const SYBUNITEXT            = 174;// 0xAE SYBASE 15
+    const SYBLONGBINARY         = 225;// 0xE1 SYBASE 12
+    const SYBSINT1              = 64; // 0x40
+    const SYBUINT2              = 65; // 0x41 SYBASE 15
+    const SYBUINT4              = 66; // 0x42 SYBASE 15
+    const SYBUINT8              = 67; // 0x43 SYBASE 15
+    const SYBUINTN              = 68; // 0x44 SYBASE 15
+    const SYBUNIQUE             = 36; // 0x24
+    const SYBVARIANT            = 98; // 0x62
+    const SYBSINT8              = 191;// 0xBF SYBASE 15
+
+    protected static $fixed= array(
+      self::SYBINT1   => 1,
+      self::SYBINT2   => 2,
+      self::SYBINT4   => 4,
+      self::SYBINT8   => 8,
+      self::SYBFLT8   => 8,
+      self::SYBBIT    => 1,
+      self::SYBMONEY4 => 4,
+      self::SYBMONEY  => 8,
+      self::SYBREAL   => 4,
+      self::SYBDATE   => 4,
+      self::SYBTIME   => 4,
+      self::SYBSINT1  => 1,
+      self::SYBUINT2  => 2,
+      self::SYBUINT4  => 3,
+      self::SYBUINT8  => 8,
+      self::SYBSINT8  => 8,
+    );
+    
     /**
      * Protocol read
      *
@@ -273,29 +340,85 @@
 
       $data= $this->readFully($header['length'] - 8);
       Console::$err->writeLine(self::dump($data));
-      
-      // An error response
-      if ("\xAA" === $data{0}) {
-        $meta= unpack('vlength/Vnumber/Cstate/Cclass', substr($data, 1, 8));
-        $consumed= 9;
-        $meta['message']= $this->lstr($data, 'v', $consumed);
-        $meta['server']= $this->lstr($data, 'C', $consumed);
-        $meta['proc']= $this->lstr($data, 'C', $consumed);
-        $meta['line']= current(unpack('v', substr($data, $consumed, 2)));
 
-        throw new TdsProtocolException(
-          $meta['message'],
-          $meta['number'], 
-          $meta['state'], 
-          $meta['class'],
-          $meta['server'],
-          $meta['proc'],
-          $meta['line']
-        );
-      } else {
-        Console::$err->writeLine('OK');
+      // See 2.2.5.7 Data Buffer Stream Tokens
+      switch ($data{0}) {
+        case "\xAA":    // ERROR
+          $meta= unpack('vlength/Vnumber/Cstate/Cclass', substr($data, 1, 8));
+          $consumed= 9;
+          $meta['message']= $this->lstr($data, 'v', $consumed);
+          $meta['server']= $this->lstr($data, 'C', $consumed);
+          $meta['proc']= $this->lstr($data, 'C', $consumed);
+          $meta['line']= current(unpack('v', substr($data, $consumed, 2)));
+
+          throw new TdsProtocolException(
+            $meta['message'],
+            $meta['number'], 
+            $meta['state'], 
+            $meta['class'],
+            $meta['server'],
+            $meta['proc'],
+            $meta['line']
+          );
+        
+        case "\x81":    // COLMETADATA
+          $nfields= current(unpack('v', substr($data, 1, 2)));
+          $consumed= 3;
+          $fields= array();
+          for ($i= 0; $i < $nfields; $i++) {
+            $field= unpack('Cx1/Cx2/Cflags/Cx3/Ctype', substr($data, $consumed, 5));
+            $consumed+= 5;
+
+            // Handle column. TODO: blob types - read table name
+            if ($field['type'] > 128) {
+              $field['size']= current(unpack('v', substr($data, $consumed, 2)));
+              $consumed+= 2;
+              $consumed+= 5;  // Collation?
+            } else if (isset(self::$fixed[$field['type']])) {
+              $field['size']= self::$fixed[$field['type']];
+            } else {
+              $field['size']= current(unpack('C', substr($data, $consumed, 1)));
+              $consumed+= 1;
+            }
+
+            $field['name']= $this->lstr($data, 'C', $consumed);
+            $fields[]= $field;
+          }
+          Console::$err->writeLine($nfields, ' FIELDS ', $fields);
+
+          $rows= array();
+          while ("\xD1" === $data{$consumed}) {
+            $consumed++;
+            $row= array();
+            foreach ($fields as $field) {
+              switch ($field['type']) {
+                case self::XSYBVARCHAR:    // XX Collation?
+                  $len= current(unpack('v', substr($data, $consumed, 2)));
+                  $row[$field['name']]= substr($data, $consumed+ 2, $len);
+                  $consumed+= $len+ 2;
+                  break;
+
+                case self::SYBINTN:
+                  $len= current(unpack('C', substr($data, $consumed, 1)));
+                  switch ($len) {
+                    case 4: $row[$field['name']]= current(unpack('V', substr($data, $consumed+ 1, 4)));
+                  }
+                  $consumed+= $len+ 1;
+                  break;
+              }
+            }
+            $rows[]= $row;
+          }
+
+          return $rows;
+
+        case "\xFD":    // DONE
+          $meta= unpack('vstatus/vcmd/Vrowcount', substr($data, 1, 8));
+          return $meta;
+        
+        default:
+          Console::$err->writeLinef('Unhandled packet %02X', ord($data{0}));
       }
-      
       return $data;
     }
 
@@ -307,7 +430,7 @@
      */
     public function query($sql) {
       $this->write(self::MSG_QUERY, iconv('iso-8859-1', 'ucs-2le', $sql));
-      $this->read();
+      return $this->read();
     }
 
     /**
@@ -319,7 +442,6 @@
 
       try {
         $this->write(self::MSG_LOGOFF, "\0");
-        $this->read();
       } catch (IOException $ignored) {
         // Can't do much here
       } 
