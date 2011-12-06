4f889f7dcb16b28c3840388b89846fc2b0ab54db
Timm Friebe <thekid@thekid.de>
2011-12-06 15:38:07 +0100

    - Locate freetds.conf in various known locations

--- core/src/main/php/rdbms/tds/FreeTdsLookup.class.php
+++ core/src/main/php/rdbms/tds/FreeTdsLookup.class.php
@@ -10,21 +10,28 @@
    * Lookup host name and port to connect to by freetds.conf file
    *
    * @test    xp://net.xp_framework.unittest.rdbms.tds.FreeTdsLookupTest
+   * @test    xp://net.xp_framework.unittest.rdbms.tds.FreeTdsConfigLocationTest
    */
   class FreeTdsLookup extends Object implements rdbms·tds·ConnectionLookup {
     protected $conf= NULL;
     
     /**
      * Creates a new sql.conf lookup instance with a given file. If
-     * the file is omitted, /etc/freetds/freetds.conf is used.
+     * the file is omitted, the file will be checked for in the following
+     * locations:
+     *
+     * <ul>
+     *   <li>ENV{FREETDSCONF}</li>
+     *   <li>ENV{HOME}/.freetds.conf</li>
+     *   <li>/etc/freetds.conf</li>
+     *   <li>/etc/freetds/freetds.conf</li>
+     *   <li>/usr/local/etc/freetds.conf</li>
+     * </ul>
      *
      * @param   io.File conf
      */
     public function __construct($conf= NULL) {
-      $this->conf= NULL === $conf
-        ? new File('/etc/freetds/freetds.conf')
-        : $conf
-      ;
+      $this->conf= $conf;
     }
     
     /**
@@ -53,12 +60,35 @@
     }
 
     /**
+     * Locate config file
+     *
+     * @return  io.File
+     */
+    protected function locateConf() {
+      foreach (array(
+        getenv('FREETDSCONF'),
+        getenv('HOME').'/.freetds.conf',
+        '/etc/freetds/freetds.conf',
+        '/etc/freetds.conf',
+        '/usr/local/etc/freetds.conf'
+      ) as $location) {
+        $f= new File($location);
+        if ($f->exists()) return $f;
+      }
+      return NULL;
+    }
+
+    /**
      * Look up DSN. Reparses config file every time its called.
      *
      * @param   rdbms.DSN dsn
      */
     public function lookup($dsn) {
-      if (!$this->conf->exists()) return;
+      if (NULL === $this->conf) {
+        if (NULL === ($this->conf= $this->locateConf())) return;
+      } else {
+        if (!$this->conf->exists()) return;
+      }
 
       $host= strtolower($dsn->getHost());
       $sections= $this->parse();
