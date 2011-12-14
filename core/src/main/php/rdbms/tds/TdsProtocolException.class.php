4bfcdb07f9fbeb3287e568c9af86a4bbdf86eb18
Timm Friebe <thekid@thekid.de>
2011-12-14 10:59:22 +0100

    - Add compoundMessage() implementation - Make all constructor parameters after message optional

--- core/src/main/php/rdbms/tds/TdsProtocolException.class.php
+++ core/src/main/php/rdbms/tds/TdsProtocolException.class.php
@@ -30,7 +30,7 @@
      * @param   string proc
      * @param   int line
      */
-    public function __construct($message, $number, $state, $class, $server, $proc, $line) {
+    public function __construct($message, $number= 0, $state= 0, $class= 0, $server= NULL, $proc= NULL, $line= 0) {
       parent::__construct($message);
       $this->number= $number;
       $this->state= $state;
@@ -38,6 +38,24 @@
       $this->server= $server;
       $this->proc= $proc;
       $this->line= $line;
+    }
+
+    /**
+     * Return compound message of this exception.
+     *
+     * @return  string
+     */
+    public function compoundMessage() {
+      $addr= array_filter(array($this->server, $this->proc, $this->line));
+      return sprintf(
+        'Exception %s (#%d, state %d, class %d: %s%s)',
+        $this->getClassName(),
+        $this->number,
+        $this->state,
+        $this->class,
+        $this->message,
+        $addr ? ' @ '.implode(':', $addr) : ''
+      );
     }
   }
 ?>
