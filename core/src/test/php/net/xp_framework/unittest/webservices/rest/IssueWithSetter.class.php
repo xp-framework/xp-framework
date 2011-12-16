24fe930c8aba16bf4e4b18e421095f395e968863
Alex Kiesel <alex.kiesel@1und1.de>
2011-12-16 10:48:37 +0100

    - initial import from thekid/xp-experiments

---
+++ core/src/test/php/net/xp_framework/unittest/webservices/rest/IssueWithSetter.class.php
@@ -0,0 +1,56 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $issueId$ 
+ */
+
+  $package= 'net.xp_framework.unittest.webservices.rest';
+
+  /**
+   * Issue
+   *
+   */
+  class net·xp_framework·unittest·webservices·rest·IssueWithSetter extends Object {
+    protected $issueId= 0;
+    protected $title= NULL;
+    
+    /**
+     * Constructor
+     *
+     * @param   int issueId
+     * @param   string title
+     */
+    public function __construct($issueId= 0, $title= NULL) {
+      $this->issueId= $issueId;
+      $this->title= $title;
+    }
+
+    /**
+     * Set title
+     *
+     * @param   string title
+     */
+    public function setTitle($title) {
+      $this->title= $title;
+    }
+
+    /**
+     * Set issueId
+     *
+     * @param   int issueId
+     */
+    public function setissueId($issueId) {
+      $this->issueId= $issueId;
+    }
+    
+    /**
+     * Checks whether another object is equal to this issue
+     *
+     * @param   var cmp
+     * @return  bool
+     */
+    public function equals($cmp) {
+      return $cmp instanceof self && $cmp->issueId === $this->issueId && $cmp->title === $this->title;
+    }
+  }
+?>
---
+++ core/src/test/php/net/xp_framework/unittest/webservices/rest/IssueWithUnderscoreField.class.php
@@ -0,0 +1,38 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $issue_id$ 
+ */
+
+  $package= 'net.xp_framework.unittest.webservices.rest';
+
+  /**
+   * Issue
+   *
+   */
+  class net·xp_framework·unittest·webservices·rest·IssueWithUnderscoreField extends Object {
+    public $issue_id= 0;
+    public $title= NULL;
+    
+    /**
+     * Constructor
+     *
+     * @param   int issue_id
+     * @param   string title
+     */
+    public function __construct($issue_id= 0, $title= NULL) {
+      $this->issue_id= $issue_id;
+      $this->title= $title;
+    }
+
+    /**
+     * Checks whether another object is equal to this issue
+     *
+     * @param   var cmp
+     * @return  bool
+     */
+    public function equals($cmp) {
+      return $cmp instanceof self && $cmp->issue_id === $this->issue_id && $cmp->title === $this->title;
+    }
+  }
+?>
