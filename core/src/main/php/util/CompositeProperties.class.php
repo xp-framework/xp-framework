80b97ae47fba613dc80c481b8fd6b8f9cf356feb
Alex Kiesel <alex@kiesel.name>
2011-12-21 09:13:53 +0100

    - QA: add apidoc

--- core/src/main/php/util/CompositeProperties.class.php
+++ core/src/main/php/util/CompositeProperties.class.php
@@ -10,19 +10,31 @@
   );
   
   /**
-   * Description of CompositeProperties
+   * Composite class for util.Properties; can be used to group together
+   * arbitrary many Properties objects
    *
-   * @purpose
+   * @test    xp://net.xp_framework.unittest.util.CompositePropertiesTest
    */
   class CompositeProperties extends Object implements PropertyAccess {
     protected $props  = array();
     private $sections = NULL;
 
+    /**
+     * Constructor
+     *
+     * @param   util.Properties p
+     * @param   util.Properties[] s default array()
+     */
     public function __construct(Properties $p, array $s= array()) {
       $this->props[]= $p;
       if (sizeof($s)) $this->props= array_merge($this->props, $s);
     }
 
+    /**
+     * Add another Properties
+     *
+     * @param   util.Properties a
+     */
     public function add(Properties $a) {
       foreach ($this->props as $p) {
         if ($p === $a) return;
@@ -33,10 +45,24 @@
       $this->sections= NULL;
     }
 
+    /**
+     * Retrieve number of grouped properties
+     *
+     * @return  int
+     */
     public function length() {
       return sizeof($this->props);
     }
 
+    /**
+     * Helper method to delegate call
+     *
+     * @param   string method
+     * @param   string section
+     * @param   string key
+     * @param   mixed default
+     * @return  mixed
+     */
     private function _read($method, $section, $key, $default) {
       foreach ($this->props as $p) {
         $res= call_user_func_array(array($p, $method), array($section, $key, xp::null()));
@@ -48,34 +74,97 @@
       return $default;
     }
 
+    /**
+     * Read string value
+     *
+     * @param   string section
+     * @param   string key
+     * @param   mixed default default NULL
+     * @return  string
+     */
     public function readString($section, $key, $default= NULL) {
       return $this->_read(__FUNCTION__, $section, $key, $default);
     }
 
+    /**
+     * Read bool value
+     *
+     * @param   string section
+     * @param   string key
+     * @param   bool default default FALSE
+     * @return  bool
+     */
     public function readBool($section, $key, $default= FALSE) {
       return $this->_read(__FUNCTION__, $section, $key, $default);
     }
 
+    /**
+     * Read arrayvalue
+     *
+     * @param   string section
+     * @param   string key
+     * @param   mixed default default array()
+     * @return  string[]
+     */
     public function readArray($section, $key, $default= array()) {
       return $this->_read(__FUNCTION__, $section, $key, $default);
     }
 
+    /**
+     * Read hash value
+     *
+     * @param   string section
+     * @param   string key
+     * @param   mixed default default NULL
+     * @return  [string:string]
+     */
     public function readHash($section, $key, $default= NULL) {
       return $this->_read(__FUNCTION__, $section, $key, $default);
     }
 
+    /**
+     * Read integer value
+     *
+     * @param   string section
+     * @param   string key
+     * @param   mixed default default 0
+     * @return  int
+     */
     public function readInteger($section, $key, $default= 0) {
       return $this->_read(__FUNCTION__, $section, $key, $default);
     }
 
+    /**
+     * Read float value
+     *
+     * @param   string section
+     * @param   string key
+     * @param   mixed default default 0.0
+     * @return  double
+     */
     public function readFloat($section, $key, $default= 0.0) {
       return $this->_read(__FUNCTION__, $section, $key, $default);
     }
 
+    /**
+     * Read range value
+     *
+     * @param   string section
+     * @param   string key
+     * @param   mixed default default 0.0
+     * @return  int[]
+     */
     public function readRange($section, $key, $default= 0.0) {
       return $this->_read(__FUNCTION__, $section, $key, $default);
     }
 
+    /**
+     * Read section
+     *
+     * @param   string section
+     * @param   mixed default default array()
+     * @return  [string:string]
+     */
     public function readSection($section, $default= array()) {
       $result= array(); $sectionFound= FALSE;
       foreach (array_reverse($this->props) as $p) {
@@ -88,6 +177,12 @@
       return $result;
     }
 
+    /**
+     * Test whether a given section exists
+     *
+     * @param   string section
+     * @return  bool
+     */
     public function hasSection($section) {
       foreach ($this->props as $p) {
         if (TRUE === $p->hasSection($section)) return TRUE;
@@ -96,6 +191,11 @@
       return FALSE;
     }
 
+    /**
+     * Retrieve first section name, set internal pointer
+     *
+     * @return  string
+     */
     public function getFirstSection() {
 
       // Lazy initialize - for subsequent loops
@@ -114,6 +214,11 @@
       return key($this->sections);
     }
 
+    /**
+     * Retrieve next section name, NULL if no more sections exist
+     *
+     * @return  string
+     */
     public function getNextSection() {
       if (!is_array($this->sections)) return NULL;
       next($this->sections);
