5cc0d1c3fa80f6c74898c4ed92073570fd959e19
Timm Friebe <thekid@thekid.de>
2012-01-21 13:55:15 +0100

    - Initial release # Covers static methods in lang.reflect.Module

---
+++ core/src/test/php/net/xp_framework/unittest/core/modules/ModuleTest.class.php
@@ -0,0 +1,36 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses(
+    'unittest.TestCase',
+    'lang.reflect.Module'
+  );
+
+  /**
+   * TestCase
+   *
+   */
+  class ModuleTest extends TestCase {
+
+    /**
+     * Test forName()
+     *
+     */
+    #[@test, @expect(class= 'lang.ElementNotFoundException', withMessage= 'No such module non-existant')]
+    public function forname_throws_exception_for_non_existant_module() {
+      Module::forName('non-existant');
+    }
+
+    /**
+     * Test getModules()
+     *
+     */
+    #[@test]
+    public function getmodules_returns_list_of_modules() {
+      $this->assertInstanceOf('lang.reflect.Module[]', Module::getModules());
+    }
+  }
+?>
