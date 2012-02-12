bb524c6e8f99c8d363c86161954e8d09053983fe
Timm Friebe <thekid@thekid.de>
2012-02-12 17:11:23 +0100

    - Initial release # Implementation for xp-framework/rfc#237

---
+++ tools/src/main/php/xp/runtime/Dump.class.php
@@ -0,0 +1,52 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+ 
+  $package= 'xp.runtime';
+ 
+  uses('util.cmd.Console');
+
+  /**
+   * Evaluates code and dumps its output.
+   *
+   */
+  class xp·runtime·Dump extends Object {
+    
+    /**
+     * Main
+     *
+     * @param   string[] args
+     */
+    public static function main(array $args) {
+      $way= array_shift($args);
+      $src= trim($args[0]).';';
+      
+      // Extract uses() and load classes
+      if (0 === strncmp($src, 'uses', 4)) {
+        $p= strpos($src, ');');
+        $uses= substr($src, 5, $p- 5);    // "uses("
+        $src= substr($src, $p+ 2);        // ");"
+        $cl= ClassLoader::getDefault();
+        foreach (explode(',', $uses) as $class) {
+          $cl->loadClass0(trim($class, '" '));
+        }
+      }
+      
+      // Allow missing return
+      strstr($src, 'return ') || $src= 'return '.$src;
+
+      // Rewrite argc, argv
+      $argv= array(xp::nameOf(__CLASS__)) + $args;
+      $argc= sizeof($argv);
+
+      // Perform
+      $return= eval($src);
+      switch ($way) {
+        case 'w': Console::writeLine($return); break;
+        case 'd': var_dump($return); break;
+      }
+    }
+  }
+?>
