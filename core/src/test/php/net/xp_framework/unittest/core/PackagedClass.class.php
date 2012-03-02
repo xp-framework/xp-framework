ad0762ec57f9ccb895a72a006dbc8df108c18217
Timm Friebe <thekid@thekid.de>
2012-03-02 10:17:50 +0100

    - Add tests for newinstance() & packages

---
+++ core/src/test/php/net/xp_framework/unittest/core/PackagedClass.class.php
@@ -0,0 +1,17 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  $package= 'net.xp_framework.unittest.core';
+
+  /**
+   * Fixture for newinstance tests
+   *
+   * @see   xp://net.xp_framework.unittest.core.NewInstanceTest
+   */
+  class net·xp_framework·unittest·core·PackagedClass extends Object {
+    
+  }
+?>
