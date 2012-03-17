74f588698188edc524b885d7c329797fd69ce3a1
Timm Friebe <thekid@thekid.de>
2012-03-17 19:02:21 +0100

    - Initial release

---
+++ core/src/main/php/webservices/rest/RestJsonSerializer.class.php
@@ -0,0 +1,35 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses('webservices.rest.RestSerializer');
+
+  /**
+   * A serializer
+   *
+   * @see   xp://webservices.rest.RestRequest#setPayload
+   */
+  class RestJsonSerializer extends Object implements RestSerializer {
+
+    /**
+     * Return the Content-Type header's value
+     *
+     * @return  string
+     */
+    public function contentType() {
+      return 'application/json; charset=utf-u8';
+    }
+    
+    /**
+     * Serialize
+     *
+     * @param   var value
+     * @return  string
+     */
+    public function serialize($payload) {
+      return JsonFactory::create()->encode($payload);
+    }
+  }
+?>
