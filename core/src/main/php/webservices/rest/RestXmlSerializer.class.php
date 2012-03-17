534eea82eeb6ad050f863c33e2d3c9c66b763dd4
Timm Friebe <thekid@thekid.de>
2012-03-17 19:06:26 +0100

    - Initial release

---
+++ core/src/main/php/webservices/rest/RestXmlSerializer.class.php
@@ -0,0 +1,48 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses('webservices.rest.RestSerializer', 'xml.Tree');
+
+  /**
+   * A serializer
+   *
+   * @see   xp://webservices.rest.RestRequest#setPayload
+   */
+  class RestXmlSerializer extends Object implements RestSerializer {
+    protected $root;
+  
+    /**
+     * Creates a new XML serializer instance
+     *
+     * @param   string root default 'root' root node's name
+     */
+    public function __construct($root= 'root') {
+      $this->root= $root;
+    }
+
+    /**
+     * Return the Content-Type header's value
+     *
+     * @return  string
+     */
+    public function contentType() {
+      return 'text/xml; charset=utf-u8';
+    }
+    
+    /**
+     * Serialize
+     *
+     * @param   var value
+     * @return  string
+     */
+    public function serialize($payload) {
+      $t= new Tree();
+      $t->setEncoding('utf-8');
+      $t->root= Node::fromArray($payload, $this->root);
+      return $t->getDeclaration()."\n".$t->getSource(INDENT_NONE));
+    }
+  }
+?>
