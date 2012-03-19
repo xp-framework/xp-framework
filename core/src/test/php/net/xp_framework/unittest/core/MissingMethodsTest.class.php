b3e8f9596b504aebdfa0d20439e068ed9cdbca8f
Timm Friebe <thekid@thekid.de>
2012-03-19 10:23:27 +0100

    - Initial release: Tests for missing methods # See xp-framework/xp-framework#133

---
+++ core/src/test/php/net/xp_framework/unittest/core/MissingMethodsTest.class.php
@@ -0,0 +1,40 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('unittest.TestCase');
+
+  /**
+   * Verifies lang.Object's <tt>__call()</tt> implementation
+   *
+   */
+  class MissingMethodsTest extends TestCase {
+
+    /**
+     * Tests missing methods
+     *
+     */
+    #[@test, @expect(class= 'lang.Error', withMessage= '/Call to undefined method lang.Object::run()/')]
+    public function missingMethodInvocation() {
+      $o= new Object();
+      $o->run();
+    }
+
+    /**
+     * Tests missing methods
+     *
+     * @see   https://github.com/xp-framework/xp-framework/issues/133
+     */
+    #[@test, @expect(class= 'lang.Error', withMessage= '/Call to undefined method lang.Object::run()/')]
+    public function missingParentMethodInvocation() {
+      $o= newinstance('lang.Object', array(), '{
+        public function run() {
+          parent::run();
+        }
+      }');
+      $o->run();
+    }
+  }
+?>
