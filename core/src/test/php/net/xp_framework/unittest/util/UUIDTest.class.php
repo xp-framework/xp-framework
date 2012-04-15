0587435a51d135284e2d6a789fd14b31c4c53aff
Timm Friebe <thekid@thekid.de>
2012-04-15 12:45:13 +0200

    - Add tests for UUID class

---
+++ core/src/test/php/net/xp_framework/unittest/util/UUIDTest.class.php
@@ -0,0 +1,52 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$ 
+ */
+
+  uses('unittest.TestCase', 'util.UUID');
+
+  /**,
+   * TestCase
+   *
+   * @see   xp://util.UUID
+   */
+  class UUIDTest extends TestCase {
+    protected $fixture= NULL;
+  
+    /**
+     * Creates fixture
+     *
+     */
+    public function setUp() {
+      $this->fixture= UUID::fromString('00000000-0000-0000-C000-000000000046');
+    }
+  
+    /**
+     * Test node
+     *
+     */
+    #[@test]
+    public function node() {
+      $this->assertEquals(array(0, 0, 0, 0, 0, 70), $this->fixture->node);
+    }
+
+    /**
+     * Test creating a UUID
+     *
+     */
+    #[@test]
+    public function createdUUIDNotEqualToFixture() {
+      $this->assertNotEquals($this->fixture, UUID::create());
+    }
+
+    /**
+     * Test creating a UUID
+     *
+     */
+    #[@test]
+    public function twoCreatedUUIDsNotEqual() {
+      $this->assertNotEquals(UUID::create(), UUID::create());
+    }
+  }
+?>
