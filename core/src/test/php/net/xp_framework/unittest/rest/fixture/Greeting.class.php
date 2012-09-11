8ab10186ccb159577d61a3fb53cfad5871b12894
Timm Friebe <thekid@thekid.de>
2012-09-11 22:59:14 +0200

    - Add value object

---
+++ core/src/test/php/net/xp_framework/unittest/rest/fixture/Greeting.class.php
@@ -0,0 +1,27 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  /**
+   * Fixture for default router
+   *
+   * @see  xp://net.xp_framework.unittest.rest.server.RestDefaultRouterTest
+   */
+  class Greeting extends Object {
+    public $word;
+    public $name;
+
+    /**
+     * Greet someone
+     * 
+     * @param   string name
+     * @param   string greeting
+     */
+    public function __construct($greeting, $name) {
+      $this->greeting= $greeting;
+      $this->name= $name;
+    }
+  }
+?>
---
+++ core/src/test/php/net/xp_framework/unittest/rest/fixture/GreetingHandler.class.php
@@ -0,0 +1,64 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('net.xp_framework.unittest.rest.fixture.Greeting');
+
+  /**
+   * Fixture for default router
+   *
+   * @see  xp://net.xp_framework.unittest.rest.server.RestDefaultRouterTest
+   */
+  #[@webservice]
+  class GreetingHandler extends TestCase {
+    protected $fixture= NULL;
+
+    /**
+     * Greet someone
+     * 
+     * @param   string name
+     * @param   string greeting
+     * @return  string
+     */
+    #[@webmethod(verb= 'GET', path= '/greet/{name}'), @$name: path, @$greeting: param]
+    public function greet($name, $greeting= 'Hello') {
+      return $greeting.' '.$name;
+    }
+
+    /**
+     * Say "hello" someone
+     * 
+     * @param   string name
+     * @return  net.xp_framework.unittest.rest.fixture.Greeting
+     */
+    #[@webmethod(verb= 'GET', path= '/hello/{name}', returns= 'application/vnd.example.v2+json'), @$name: path]
+    public function hello($name) {
+      return new Greeting('Hello', $name);
+    }
+
+    /**
+     * Say "hello" to someone
+     * 
+     * @param   string payload
+     * @return  string
+     */
+    #[@webmethod(verb= 'POST', path= '/greet')]
+    public function greet_posted($payload) {
+      sscanf($payload, '%s %s', $greeting, $name);
+      return $this->greet($name, $greeting);
+    }
+
+    /**
+     * Say "hello" to someone
+     * 
+     * @param   net.xp_framework.unittest.rest.fixture.Greeting
+     * @return  bool
+     */
+    #[@webmethod(verb= 'POST', path= '/hello', accepts= 'application/vnd.example.v2+json')]
+    public function hello_posted(Greeting $payload) {
+      return $this->greet($payload->name, $payload->word);
+    }
+  }
+?>
