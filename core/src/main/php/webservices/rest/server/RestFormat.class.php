cfc92de7e727624e11126c53b15f0b0b2ae235a9
Timm Friebe <thekid@thekid.de>
2012-09-11 16:10:03 +0200

    - QA: Apidoc

--- core/src/main/php/webservices/rest/server/RestFormat.class.php
+++ core/src/main/php/webservices/rest/server/RestFormat.class.php
@@ -31,20 +31,46 @@
       self::$XML= new self(2, 'XML', new RestXmlSerializer(), new RestXmlDeserializer());
     }
 
+    /**
+     * Constructor
+     *
+     * @param  int ordinal
+     * @param  string name
+     * @param  webservices.rest.RestSerializer serializer
+     * @param  webservices.rest.RestDeserializer deserializer
+     */
     public function __construct($ordinal, $name, $serializer, $deserializer) {
       parent::__construct($ordinal, $name);
       $this->serializer= $serializer;
       $this->deserializer= $deserializer;
     }
 
+    /**
+     * Read the payload from the request
+     *
+     * @param  scriptlet.Request request
+     * @param  lang.Type type
+     * @return var
+     */
     public function read($request, $type) {
       return $this->deserializer->deserialize(new MemoryInputStream($request->getData()), $type);
     }
 
+    /**
+     * Read the payload from the request
+     *
+     * @param  scriptlet.Response request
+     * @param  var value
+     */
     public function write($response, $value) {
       $response->write($this->serializer->serialize($value));
     }
 
+    /**
+     * Values method
+     *
+     * @return lang.Enum[]
+     */
     public static function values() {
       return parent::membersOf(__CLASS__);
     }
