684761ff817b8caedd985a4a5ce3ff7c69379a37
Timm Friebe <thekid@thekid.de>
2012-11-01 16:09:08 +0100

    - QA: Test and document webservices.rest.srv.Response class

--- core/src/main/php/webservices/rest/srv/Response.class.php
+++ core/src/main/php/webservices/rest/srv/Response.class.php
@@ -7,31 +7,65 @@
   /**
    * REST response
    *
+   * @test  xp://net.xp_framework.unittest.rest.srv.ResponseTest
    */
   class Response extends Object {
     public $status;
     public $headers= array();
     public $payload;
 
+    /**
+     * Creates a new response instance
+     *
+     * @param  int status
+     */
+    public function __construct($status= NULL) {
+      $this->status= $status;
+    }
+
+    /**
+     * Creates a new response instance with the status code set to 200 (OK)
+     *
+     * @return  self
+     */
     public static function ok() {
       $self= new self();
       $self->status= 200;
       return $self;
     }
 
-    public static function created($location) {
+    /**
+     * Creates a new response instance with the status code set to 201 (Created)
+     * and an optional location.
+     *
+     * @param   string location
+     * @return  self
+     */
+    public static function created($location= NULL) {
       $self= new self();
       $self->status= 201;
-      $self->headers['Location']= $location;
+      if (NULL !== $location) $self->headers['Location']= $location;
       return $self;
     }
 
+    /**
+     * Creates a new response instance with the status code set to 204 (No content)
+     *
+     * @return  self
+     */
     public static function noContent() {
       $self= new self();
       $self->status= 204;
       return $self;
     }
 
+    /**
+     * Creates a new response instance with the status code set to 302 (See other)
+     * and a specified location.
+     *
+     * @param   string location
+     * @return  self
+     */
     public static function see($location) {
       $self= new self();
       $self->status= 302;
@@ -39,41 +73,70 @@
       return $self;
     }
 
+    /**
+     * Creates a new response instance with the status code set to 304 (Not modified)
+     *
+     * @return  self
+     */
     public static function notModified() {
       $self= new self();
       $self->status= 304;
       return $self;
     }
 
+    /**
+     * Creates a new response instance with the status code set to 404 (Not found)
+     *
+     * @return  self
+     */
     public static function notFound() {
       $self= new self();
       $self->status= 404;
       return $self;
     }
 
+    /**
+     * Creates a new response instance with the status code set to 406 (Not acceptable)
+     *
+     * @return  self
+     */
     public static function notAcceptable() {
       $self= new self();
       $self->status= 406;
       return $self;
     }
 
-    public static function error() {
+    /**
+     * Creates a new response instance with the status code set to a given error
+     * code (defaulting to 500 - Internal Server Error).
+     *
+     * @param   int code
+     * @return  self
+     */
+    public static function error($code= 500) {
       $self= new self();
-      $self->status= 500;
+      $self->status= $code;
       return $self;
     }
 
-    public static function status($status) {
-      $self= new self();
-      $self->status= $status;
-      return $self;
-    }
-
+    /**
+     * Adds a header and returns this instance
+     * 
+     * @param   string name
+     * @param   string value
+     * @return  self
+     */
     public function withHeader($name, $value) {
       $this->headers[$name]= $value;
       return $this;
     }
 
+    /**
+     * Sets payload and returns this instance
+     * 
+     * @param   var data
+     * @return  self
+     */
     public function withPayload($data) {
       $this->payload= $data;
       return $this;
---
+++ core/src/test/php/net/xp_framework/unittest/rest/srv/ResponseTest.class.php
@@ -0,0 +1,182 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  $package= 'net.xp_framework.unittest.rest.srv';
+
+  uses(
+    'unittest.TestCase',
+    'webservices.rest.srv.Response'
+  );
+  
+  /**
+   * Test response class
+   *
+   * @see  xp://webservices.rest.srv.Response
+   */
+  class net·xp_framework·unittest·rest·srv·ResponseTest extends TestCase {
+
+    /**
+     * Test constructor
+     * 
+     */
+    #[@test]
+    public function create() {
+      $this->assertEquals(NULL, create(new Response())->status);
+    }
+
+    /**
+     * Test constructor
+     * 
+     */
+    #[@test]
+    public function create_with_status() {
+      $this->assertEquals(200, create(new Response(200))->status);
+    }
+
+    /**
+     * Test payload is initially null
+     * 
+     */
+    #[@test]
+    public function payload_initially_null() {
+      $this->assertNull(create(new Response())->payload);
+    }
+
+    /**
+     * Test headers are initially empty
+     * 
+     */
+    #[@test]
+    public function headers_initially_empty() {
+      $this->assertEquals(array(), create(new Response())->headers);
+    }
+
+    /**
+     * Test ok() method
+     * 
+     */
+    #[@test]
+    public function ok() {
+      $r= Response::ok();
+      $this->assertEquals(200, $r->status);
+    }
+
+    /**
+     * Test created() method
+     * 
+     */
+    #[@test]
+    public function created() {
+      $r= Response::created();
+      $this->assertEquals(201, $r->status);
+      $this->assertEquals(array(), $r->headers);
+    }
+
+    /**
+     * Test created() method
+     * 
+     */
+    #[@test]
+    public function created_with_location() {
+      $location= 'http://example.com/resource/4711';
+      $r= Response::created($location);
+      $this->assertEquals(201, $r->status);
+      $this->assertEquals(array('Location' => $location), $r->headers);
+    }
+
+    /**
+     * Test noContent() method
+     * 
+     */
+    #[@test]
+    public function no_content() {
+      $r= Response::noContent();
+      $this->assertEquals(204, $r->status);
+    }
+
+    /**
+     * Test see() method
+     * 
+     */
+    #[@test]
+    public function see() {
+      $location= 'http://example.com/resource/4711';
+      $r= Response::see($location);
+      $this->assertEquals(302, $r->status);
+    }
+
+    /**
+     * Test notModified() method
+     * 
+     */
+    #[@test]
+    public function not_modified() {
+      $r= Response::notModified();
+      $this->assertEquals(304, $r->status);
+    }
+
+    /**
+     * Test notFound() method
+     * 
+     */
+    #[@test]
+    public function not_found() {
+      $r= Response::notFound();
+      $this->assertEquals(404, $r->status);
+    }
+
+    /**
+     * Test notAcceptable() method
+     * 
+     */
+    #[@test]
+    public function not_acceptable() {
+      $r= Response::notAcceptable();
+      $this->assertEquals(406, $r->status);
+    }
+
+    /**
+     * Test error() method
+     * 
+     */
+    #[@test]
+    public function error() {
+      $r= Response::error();
+      $this->assertEquals(500, $r->status);
+    }
+
+    /**
+     * Test error() method
+     * 
+     */
+    #[@test]
+    public function error_503() {
+      $r= Response::error(503);
+      $this->assertEquals(503, $r->status);
+    }
+
+    /**
+     * Test withHeader() method
+     * 
+     */
+    #[@test]
+    public function with_extra_header() {
+      $r= create(new Response())->withHeader('X-Exception', 'SQL');
+      $this->assertEquals(array('X-Exception' => 'SQL'), $r->headers);
+    }
+
+    /**
+     * Test withPayload() method
+     * 
+     */
+    #[@test]
+    public function with_payload() {
+      $data= array('name' => 'example');
+      $r= create(new Response())->withPayload($data);
+      $this->assertEquals($data, $r->payload);
+    }
+  }
+?>
