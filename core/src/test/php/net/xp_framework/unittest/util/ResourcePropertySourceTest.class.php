d1775de58f1007dd17d449b1b43af3f0fd65d258
Marius Rosoiu <marius.rosoiu@1and1.ro>
2012-12-28 18:00:52 +0200

    Add util.ResourcePropertySource

---
+++ core/src/test/php/net/xp_framework/unittest/util/ResourcePropertySourceTest.class.php
@@ -0,0 +1,77 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses(
+    'unittest.TestCase',
+    'util.Properties',
+    'util.ResourcePropertySource'
+  );
+
+  /**
+   * Test for ResourcePropertySource
+   *
+   * @see      xp://util.ResourcePropertySource
+   */
+  class ResourcePropertySourceTest extends TestCase {
+    protected $fixture= NULL;
+
+    public function setUp() {
+      $this->fixture= new ResourcePropertySource('/net/xp_framework/unittest/util');
+    }
+
+    /**
+     * Test
+     *
+     */
+    #[@test]
+    public function doesNotHaveProperty() {
+      $this->assertFalse($this->fixture->provides('non-existent'));
+    }
+
+    /**
+     * Test
+     *
+     */
+    #[@test]
+    public function hasProperty() {
+      $this->assertTrue($this->fixture->provides('example'));
+    }
+
+    /**
+     * Test
+     *
+     */
+    #[@test]
+    public function returnsProperties() {
+      $p= Properties::fromFile($this->getClass()->getPackage()->getResourceAsStream('example.ini'));
+      $this->assertEquals($p, $this->fixture->fetch('example'));
+    }
+
+    /**
+     * Test
+     *
+     */
+    #[@test]
+    public function sourcesAreEqual() {
+      $p1= new ResourcePropertySource('net/xp_framework/unittest/util');
+      $p2= new ResourcePropertySource('net/xp_framework/unittest/util');
+
+      $this->assertEquals($p1, $p2);
+    }
+
+    /**
+     * Test
+     *
+     */
+    #[@test]
+    public function propertiesFromSameResourceAreEqual() {
+      $p1= new ResourcePropertySource('net/xp_framework/unittest/util');
+      $p2= new ResourcePropertySource('net/xp_framework/unittest/util');
+
+      $this->assertEquals($p1->fetch('example'), $p2->fetch('example'));
+    }
+  }
+?>
