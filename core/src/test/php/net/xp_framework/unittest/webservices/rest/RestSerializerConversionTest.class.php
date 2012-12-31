f707d44350588d52320507b66eb75613a0e1a443
Timm Friebe <thekid@thekid.de>
2012-12-31 18:00:23 +0100

    - Add tests for integers, doubles, booleans, arrays and maps

--- core/src/test/php/net/xp_framework/unittest/webservices/rest/RestSerializerConversionTest.class.php
+++ core/src/test/php/net/xp_framework/unittest/webservices/rest/RestSerializerConversionTest.class.php
@@ -41,5 +41,62 @@
     public function null() {
       $this->assertEquals(NULL, $this->fixture->convert(NULL));
     }
+
+    /**
+     * Test a string
+     *
+     */
+    #[@test]
+    public function string() {
+      $this->assertEquals('Hello', $this->fixture->convert('Hello'));
+    }
+
+    /**
+     * Test an integer
+     *
+     */
+    #[@test]
+    public function int() {
+      $this->assertEquals(6100, $this->fixture->convert(6100));
+    }
+
+    /**
+     * Test a double
+     *
+     */
+    #[@test]
+    public function double() {
+      $this->assertEquals(1.5, $this->fixture->convert(1.5));
+    }
+
+    /**
+     * Test a boolean
+     *
+     */
+    #[@test]
+    public function bool() {
+      $this->assertEquals(TRUE, $this->fixture->convert(TRUE));
+    }
+
+    /**
+     * Test an array of strings
+     *
+     */
+    #[@test]
+    public function string_array() {
+      $this->assertEquals(array('Hello', 'World'), $this->fixture->convert(array('Hello', 'World')));
+    }
+
+    /**
+     * Test an array of strings
+     *
+     */
+    #[@test]
+    public function string_map() {
+      $this->assertEquals(
+        array('greeting' => 'Hello', 'name' => 'World'),
+        $this->fixture->convert(array('greeting' => 'Hello', 'name' => 'World'))
+      );
+    }
   }
 ?>
