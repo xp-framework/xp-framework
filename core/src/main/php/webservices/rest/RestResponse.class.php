4ab40fb72afd6665438fbbda40514ff944a44f7e
Timm Friebe <thekid@thekid.de>
2013-01-02 11:50:40 +0100

    - Add RestResponse::headers() and RestResponse::header(string $name)

--- core/src/main/php/webservices/rest/RestResponse.class.php
+++ core/src/main/php/webservices/rest/RestResponse.class.php
@@ -61,6 +61,30 @@
     public function content() {
       return Streams::readAll($this->input);
     }
+
+    /**
+     * Get headers
+     *
+     * @return  [:var]
+     */
+    public function headers() {
+      $r= array();
+      foreach ($this->response->headers() as $key => $values) {
+        $r[$key]= sizeof($values) > 1 ? $values : $values[0];
+      }
+      return $r;
+    }
+
+    /**
+     * Get header by a specified name
+     *
+     * @param   string name
+     * @return  var
+     */
+    public function header($name) {
+      if (NULL === ($values= $this->response->header($name))) return NULL;  // Not found
+      return sizeof($values) > 1 ? $values : $values[0];
+   }
     
     /**
      * Copy data
