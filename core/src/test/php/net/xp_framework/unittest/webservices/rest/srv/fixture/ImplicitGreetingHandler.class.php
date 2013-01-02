6f35ad264bcc74175b6d78704f98506fcb9b9f79
Timm Friebe <thekid@thekid.de>
2013-01-02 18:14:12 +0100

    - QA: Fix base class

--- core/src/test/php/net/xp_framework/unittest/webservices/rest/srv/fixture/ImplicitGreetingHandler.class.php
+++ core/src/test/php/net/xp_framework/unittest/webservices/rest/srv/fixture/ImplicitGreetingHandler.class.php
@@ -12,7 +12,7 @@
    * @see  xp://net.xp_framework.unittest.webservices.rest.srv.RestDefaultRouterTest
    */
   #[@webservice(path= '/implicit/')]
-  class ImplicitGreetingHandler extends TestCase {
+  class ImplicitGreetingHandler extends Object {
     protected $fixture= NULL;
 
     /**
