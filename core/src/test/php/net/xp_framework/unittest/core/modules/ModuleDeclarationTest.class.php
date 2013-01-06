042671794861471b67bdd90f9c04ef04592ece8a
Timm Friebe <thekid@thekid.de>
2013-01-06 12:58:44 +0100

    - Test various different valid module definitions

--- core/src/test/php/net/xp_framework/unittest/core/modules/ModuleDeclarationTest.class.php
+++ core/src/test/php/net/xp_framework/unittest/core/modules/ModuleDeclarationTest.class.php
@@ -35,7 +35,15 @@
         public function getResourceAsStream($name) { /* Not reached */ }
         public function toString() { return $this->getClassName()."(`".$this->moduleInfo."`)"; }
       }'));
-      $this->assertEquals($version, Module::forName($name)->getVersion());
+
+      try {
+        $this->assertEquals($version, Module::forName($name)->getVersion());
+      } catch (XPException $e) {
+        //
+      } ensure($e); {
+        unset(xp::$registry['modules'][$name]);
+        if ($e) throw($e);
+      }
     }
 
     /**
@@ -43,9 +51,53 @@
      *
      */
     #[@test]
-    public function with_number_inside() {
+    public function with_number_inside_name() {
       $this->assertModule('mp3', '2.1.0', '<?php module mp3(2.1.0) { } ?>');
     }
 
+    /**
+     * Test a module with "-" in its name
+     *
+     */
+    #[@test]
+    public function with_dash_inside_name() {
+      $this->assertModule('jenkins-api', '2.1.0', '<?php module jenkins-api(2.1.0) { } ?>');
+    }
+
+    /**
+     * Test a module with "_" in its name
+     *
+     */
+    #[@test]
+    public function with_underscore_inside_name() {
+      $this->assertModule('com_dotnet', '2.1.0', '<?php module com_dotnet(2.1.0) { } ?>');
+    }
+
+    /**
+     * Test a module with a "-" in its version
+     *
+     */
+    #[@test]
+    public function alpha_version() {
+      $this->assertModule('test', '0.1.0-alpha7', '<?php module test(0.1.0-alpha7) { } ?>');
+    }
+
+    /**
+     * Test a module with letters in its version
+     *
+     */
+    #[@test]
+    public function release_candidate() {
+      $this->assertModule('test', '5.8.3RC4', '<?php module test(5.8.3RC4) { } ?>');
+    }
+
+    /**
+     * Test a module with letters in its version
+     *
+     */
+    #[@test]
+    public function release() {
+      $this->assertModule('test', '5.9.1', '<?php module test(5.9.1) { } ?>');
+    }
   }
 ?>
