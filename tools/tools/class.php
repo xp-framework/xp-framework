d9282aa638c81a9f976d0c052129a1e671366b90
Timm Friebe <thekid@thekid.de>
2013-01-13 12:03:20 +0100

    - Fix code detecting whether a line ends with ".php" # It might contain \r, \r\n, or \n, or nothing, at its end, so work # on trim()ed string

--- tools/tools/class.php
+++ tools/tools/class.php
@@ -29,7 +29,7 @@
           }
 
           $qn= $base.strtr(trim($line), '/', DIRECTORY_SEPARATOR).PATH_SEPARATOR;
-          if ('.php' === substr($line, -4, 4)) {
+          if ('.php' === substr($qn, -5, 4)) {
             require(substr($qn, 0, -1));
           } else {
             $pre ? $inc= $qn.$inc : $inc.= $qn;
