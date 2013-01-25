6c2cecf600d4e879ec731820bccd6db411afc55a
Timm Friebe <thekid@thekid.de>
2013-01-25 20:15:16 +0100

    - Initial release: Simplemost enum declaration

---
+++ core/src/test/php/net/xp_framework/unittest/core/Weekday.class.php
@@ -0,0 +1,16 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('lang.Enum');
+  
+  /**
+   * Weekday enumeration
+   * 
+   */
+  class Weekday extends Enum {
+    public static $MON= 1, $TUE, $WED, $THU, $FRI, $SAT, $SUN;
+  }
+?>
