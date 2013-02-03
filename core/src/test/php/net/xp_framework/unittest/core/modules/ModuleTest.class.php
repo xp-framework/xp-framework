e48f0753528513e9479f3c50c5f21f196ee77158
Timm Friebe <thekid@thekid.de>
2013-02-03 16:11:08 +0100

    - Remove tests relying on "core" module being present

--- core/src/test/php/net/xp_framework/unittest/core/modules/ModuleTest.class.php
+++ core/src/test/php/net/xp_framework/unittest/core/modules/ModuleTest.class.php
@@ -30,23 +30,5 @@
     public function getmodules_returns_list_of_modules() {
       $this->assertInstanceOf('lang.Module[]', Module::getModules());
     }
-
-    /**
-     * Test getModules()
-     *
-     */
-    #[@test]
-    public function getmodules_contains_core_module_at_first_position() {
-      $this->assertEquals(Module::forName('core'), this(Module::getModules(), 0));
-    }
-
-    /**
-     * Test equals()
-     *
-     */
-    #[@test]
-    public function same_module_equals() {
-      $this->assertEquals(Module::forName('core'), Module::forName('core'));
-    }
   }
 ?>
