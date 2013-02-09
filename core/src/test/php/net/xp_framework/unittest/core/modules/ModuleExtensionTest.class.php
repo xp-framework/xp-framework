ea618482a9d306710d47299f4e64405d8ec5b4fb
Timm Friebe <thekid@thekid.de>
2013-02-09 15:02:30 +0100

    - Fix expected return value

--- core/src/test/php/net/xp_framework/unittest/core/modules/ModuleExtensionTest.class.php
+++ core/src/test/php/net/xp_framework/unittest/core/modules/ModuleExtensionTest.class.php
@@ -48,7 +48,7 @@
     public function initializer_has_run() {
       $class= $this->parent->loadClass('rdbms2.Drivers');
       $this->assertEquals(
-        'SybaseCtConnection<sybase+ct://user:password@host, 2.7.12RC2>',
+        'SybaseCtConnection<sybase+ct://user:password@host, 2.7.11RC2>',
         $class->getMethod('newConnection')->invoke(NULL, array('sybase+ct://user:password@host'))
       );
     }
