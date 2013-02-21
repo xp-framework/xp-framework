d2dc662889fac229f04fd5f8ac167945bd753f74
Timm Friebe <thekid@thekid.de>
2013-02-21 16:54:30 +0100

    - Make this easily invokable via "xp xp.scriptlet.Inspect /path/to/doc_root"

--- core/src/main/php/xp/scriptlet/Inspect.class.php
+++ core/src/main/php/xp/scriptlet/Inspect.class.php
@@ -19,18 +19,25 @@
   class xp·scriptlet·Inspect extends Object {
 
     /**
-     * Entry point method
+     * Entry point method. Gets passed the following arguments from "xpws -i":
+     * <ul>
+     *   <li>#0: The document root - defaults to $CWD/static</li>
+     *   <li>#1: The server profile - default to "dev"</li>
+     *   <li>#2: The server address - default to "localhost:8080"</li>
+     * </ul>
      *
      * @param   string[] args
      */
     public static function main(array $args) {
-      $docroot= realpath($args[3] ?: getcwd().'/static');
+      $docroot= realpath(isset($args[0]) ? $args[0] : getcwd().'/static');
       $webroot= dirname($docroot);
-      Console::writeLine('xpws-', $args[0], ' @ ', $args[1], ':', $args[2], ', ', $docroot, ' {');
+      $profile= isset($args[1]) ? $args[1] : 'dev';
+      $address= isset($args[2]) ? $args[2] : 'localhost:8080';
+      Console::writeLine('xpws-', $profile, ' @ ', $address, ', ', $docroot, ' {');
 
       // Dump configured applications
       $conf= new xp·scriptlet·WebConfiguration(new Properties($webroot.'/etc/web.ini'));
-      foreach ($conf->mappedApplications($args[0]) as $url => $app) {
+      foreach ($conf->mappedApplications($profile) as $url => $app) {
         Console::writeLine('  Route<', $url, '*> => ', str_replace("\n", "\n  ", $app->toString()));
       }
 
