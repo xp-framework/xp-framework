125e0bf4b79caa480c321f9f05017fc88ab482ad
Timm Friebe <thekid@thekid.de>
2013-03-09 09:59:11 +0100

    - Add UriMatchesFilter as replacement for RegexFilter # TODO: There should be a rule on matching directory separators # Suggestion: Always use forward-slashes!

--- core/src/test/php/net/xp_framework/unittest/io/collections/IOCollectionIteratorTest.class.php
+++ core/src/test/php/net/xp_framework/unittest/io/collections/IOCollectionIteratorTest.class.php
@@ -19,6 +19,7 @@
     'io.collections.iterate.NameMatchesFilter',
     'io.collections.iterate.NameEqualsFilter',
     'io.collections.iterate.ExtensionEqualsFilter',
+    'io.collections.iterate.UriMatchesFilter',
     'io.collections.iterate.SizeBiggerThanFilter',
     'io.collections.iterate.SizeEqualsFilter',
     'io.collections.iterate.SizeSmallerThanFilter',
@@ -204,6 +205,32 @@
       $this->assertEquals(
         array('./sub/sec/lang.base.php', './sub/sec/__xp__.php'), 
         $this->filterFixtureWith(new ExtensionEqualsFilter('.php'), TRUE)
+      );
+    }
+
+    /**
+     * Test UriMatchesFilter
+     *
+     * @see     xp://io.collections.iterate.UriMatchesFilter
+     */
+    #[@test]
+    public function uriMatches() {
+      $this->assertEquals(
+        array('./first.txt', './second.txt'),
+        $this->filterFixtureWith(new UriMatchesFilter('/\.txt$/'), FALSE)
+      );
+    }
+
+    /**
+     * Test UriMatchesFilter
+     *
+     * @see     xp://io.collections.iterate.UriMatchesFilter
+     */
+    #[@test]
+    public function uriMatchesRecursive() {
+      $this->assertEquals(
+        array('./sub/', './sub/IMG_6100.jpg', './sub/IMG_6100.txt', './sub/sec/', './sub/sec/lang.base.php', './sub/sec/__xp__.php'),
+        $this->filterFixtureWith(new UriMatchesFilter('/sub/'), TRUE)
       );
     }
     
