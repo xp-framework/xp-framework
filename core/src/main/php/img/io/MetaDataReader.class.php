d7f42e3da94e743e18eae2958bd1a388fa27195d
Alex Kiesel <alex@kiesel.name>
2013-03-24 13:26:35 +0100

    - QA: fix apidoc reference to img.ImagingException

--- core/src/main/php/img/io/MetaDataReader.class.php
+++ core/src/main/php/img/io/MetaDataReader.class.php
@@ -85,7 +85,7 @@
      * @param  io.streams.InputStream $in The input stream to read from
      * @param  string $name The input stream's name
      * @return img.io.ImageMetaData
-     * @throws lang.ImagingException if the input stream cannot be parsed
+     * @throws img.ImagingException if the input stream cannot be parsed
      */
     public function read(InputStream $in, $name= 'input stream') {
       if ("\xff\xd8\xff" !== $in->read(3)) {
