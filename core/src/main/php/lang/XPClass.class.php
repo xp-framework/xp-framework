f44ac61b7157cc04d4029c83c8c5449e07953e61
Timm Friebe <thekid@thekid.de>
2013-03-27 12:10:55 +0100

    - If not documented, use "var" type

--- core/src/main/php/lang/XPClass.class.php
+++ core/src/main/php/lang/XPClass.class.php
@@ -861,7 +861,7 @@
             $m= $tokens[$i][1];
             $details[1][$m]= array(
               DETAIL_ARGUMENTS    => array(),
-              DETAIL_RETURNS      => 'void',
+              DETAIL_RETURNS      => NULL === $comment ? NULL : 'void',
               DETAIL_THROWS       => array(),
               DETAIL_COMMENT      => trim(preg_replace('/\n\s+\* ?/', "\n", "\n".substr(
                 $comment, 
