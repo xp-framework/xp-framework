ccd8a16a351a9037ff106f9c40108e9515f2b7eb
Timm Friebe <thekid@thekid.de>
2013-04-11 09:03:50 +0200

    - Fix connect() method for mysql+i driver

--- core/src/main/php/rdbms/mysqli/MySQLiConnection.class.php
+++ core/src/main/php/rdbms/mysqli/MySQLiConnection.class.php
@@ -67,20 +67,21 @@
       // does not work with named pipes"). For mysqlnd, we default to mysqlx
       // anyways, so this works transparently.
       $host= $this->dsn->getHost();
+      $sock= NULL;
       if ('.' === $host) {
         $sock= $this->dsn->getProperty('socket', NULL);
         if (0 === strncasecmp(PHP_OS, 'Win', 3)) {
-          $connect= '.';
+          $host= '.';
           if (NULL !== $sock) $sock= substr($sock, 9);   // 9 = strlen("\\\\.\\pipe\\")
         } else {
-          $connect= 'localhost';
+          $host= 'localhost';
         }
       } else if ('localhost' === $host) {
-        $connect= '127.0.0.1';   // Force TCP/IP
+        $host= '127.0.0.1';   // Force TCP/IP
       }
 
       $this->handle= mysqli_connect(
-        ($this->flags & DB_PERSISTENT ? 'p:' : '').$connect,
+        ($this->flags & DB_PERSISTENT ? 'p:' : '').$host,
         $this->dsn->getUser(), 
         $this->dsn->getPassword(),
         $this->dsn->getDatabase(),
