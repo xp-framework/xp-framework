717d757636edce40aeff8596c5fafcac5b8b502a
Timm Friebe <thekid@thekid.de>
2013-04-14 16:09:30 +0200

    - Show whether a module is selected or not

--- core/src/main/php/xp/install/ListAction.class.php
+++ core/src/main/php/xp/install/ListAction.class.php
@@ -22,13 +22,21 @@
      * List all modules of a given vendor
      *
      * @param  io.collections.IOElement $vendor
+     * @param  io.collections.IOCollection $cwd
      * @return int
      */
-    protected function listModulesOf($vendor) {
+    protected function listModulesOf($vendor, $cwd) {
       $i= 0;
       $meta= self::$json->decodeFrom($vendor->getInputStream());
       foreach (new FilteredIOCollectionIterator($vendor->getOrigin(), new CollectionFilter()) as $dir) {
         sscanf(basename($dir->getURI()), '%[^@]@%s', $name, $version);
+
+        if ($cwd->findElement('.'.$meta['name'].'.'.$name.($version ? '@'.$version : '').'.pth')) {
+          Console::write('A ');
+        } else {
+          Console::write('- ');
+        }
+
         Console::writeLine(new Module($meta['name'], $name), ' @', $version);
         $i++;
       }
@@ -44,20 +52,22 @@
     public function perform($args) {
       $cwd= new FileCollection('.');
 
-      Console::writeLine($cwd->getURI());
+      Console::writeLine('@', $cwd->getURI());
       if (isset($args[0])) {
         if ($vendor= $cwd->getCollection($args[0])->findElement('vendor.json')) {
-          $total= $this->listModulesOf($vendor);
-          Console::writeLine($total, ' modules from ', $args[0], ' installed');
+          $total= $this->listModulesOf($vendor, $cwd);
+          Console::writeLine();
+          Console::writeLine($total, ' module(s) from ', $args[0], ' installed');
         }
       } else {
         $total= 0;
         foreach (new FilteredIOCollectionIterator($cwd, new CollectionFilter()) as $dir) {
           if ($vendor= $dir->findElement('vendor.json')) {
-            $total+= $this->listModulesOf($vendor);
+            $total+= $this->listModulesOf($vendor, $cwd);
           }
         }
-        Console::writeLine($total, ' modules installed');
+        Console::writeLine();
+        Console::writeLine($total, ' module(s) installed');
       }
 
       return 0;
