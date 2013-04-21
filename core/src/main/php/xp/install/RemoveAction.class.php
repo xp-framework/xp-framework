d5545983d05516fd3db1a576144cbd4833609b8e
Timm Friebe <thekid@thekid.de>
2013-04-21 12:41:41 +0200

    - Clean up correctly after removing a module

--- core/src/main/php/xp/install/RemoveAction.class.php
+++ core/src/main/php/xp/install/RemoveAction.class.php
@@ -3,6 +3,11 @@
 
   use \io\Folder;
   use \io\File;
+  use \io\collections\FileCollection;
+  use \io\collections\iterate\FilteredIOCollectionIterator;
+  use \io\collections\iterate\ExtensionEqualsFilter;
+  use \io\collections\iterate\NameMatchesFilter;
+  use \io\streams\StringReader;
   use \util\cmd\Console;
 
   /**
@@ -35,13 +40,45 @@
       }
 
       // Unlink
-      $target->unlink();
       $pth= new File('.'.$module->vendor.'.'.strtr($target->dirname, DIRECTORY_SEPARATOR, '.').'.pth');
       if ($pth->exists()) {
         Console::writeLine('Removing active ', $target);
         $pth->unlink();
+        $active= TRUE;
       } else {
         Console::writeLine('Removing ', $target);
+        $active= FALSE;
+      }
+      $target->unlink();
+
+      // If there is no other version available, remove the module reference completely. 
+      // If there is another version installed, and the deinstalled module was active,
+      // then select this other version.
+      $base= new FileCollection(new Folder($cwd, $module->vendor));
+      $it= new FilteredIOCollectionIterator($base, new NameMatchesFilter('#^'.$module->name.'@.+#'));
+      if ($it->hasNext()) {
+        if ($active) {
+          $next= $it->next();
+          $pth= new File('.'.$module->vendor.'.'.basename($next->getURI()).'.pth');
+          $out= $pth->getOutputStream();
+          $base= strtr(substr($next->getURI(), strlen($cwd->getURI())), DIRECTORY_SEPARATOR, '/');
+          Console::writeLine('Select ', $pth);
+          foreach (new FilteredIOCollectionIterator($next, new ExtensionEqualsFilter('.pth')) as $found) {
+            $r= new StringReader($found->getInputStream());
+            while (NULL !== ($line= $r->readLine())) {
+              if ('' === $line || '#' === $line{0}) {
+                continue;
+              } else if ('!' === $line{0}) {
+                $out->write('!'.$base.substr($line, 1)."\n");
+              } else {
+                $out->write($base.$line."\n");
+              }
+            }
+          }
+        }
+      } else {
+        Console::writeLine('Removing module reference');
+        $base->removeElement($module->name.'.json');
       }
 
       Console::writeLine('Done');
