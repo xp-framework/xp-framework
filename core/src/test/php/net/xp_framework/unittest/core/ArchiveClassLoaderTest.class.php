68c9f016e5a15e4aaa7f941df97c8da9408dc95a
Timm Friebe <thekid@thekid.de>
2013-05-04 16:57:30 +0200

    - Rewrite ArchiveClassLoaderTest to not rely on writeable filesystem

--- core/src/test/php/net/xp_framework/unittest/core/ArchiveClassLoaderTest.class.php
+++ core/src/test/php/net/xp_framework/unittest/core/ArchiveClassLoaderTest.class.php
@@ -6,120 +6,89 @@
 
   uses(
     'unittest.TestCase',
-    'util.collections.HashSet',
     'lang.archive.ArchiveClassLoader',
-    'lang.archive.Archive',
-    'io.TempFile'
+    'lang.archive.Archive'
   );
 
   /**
-   * TestCase
+   * TestCase for archive class loading
    *
-   * @see      xp://lang.archive.ArchiveClassLoader
-   * @purpose  purpose
+   * Relies on an archive.xar file existing in the resources directory
+   * with the following contents:
+   * <pre>
+   *  $ xar tvf archive.xar
+   *     92 test/ClassLoadedFromArchive.class.php
+   *    104 test/package-info.xp
+   * </pre>
+   * 
+   * @see   xp://lang.archive.ArchiveClassLoader
    */
   class ArchiveClassLoaderTest extends TestCase {
-    public
-      $classloader     = NULL,
-      $classname       = '',
-      $interfacename   = '';
-
-    protected
-      $tempfile       = NULL;
-
-    /**
-     * Adds sourcecode to a given XAR archive
-     *
-     * @param   lang.archive.Archive a
-     * @param   string name
-     * @param   string bytes sourcecode
-     */
-    protected function add(Archive $a, $name, $bytes) {
-      $a->addFileBytes($name.'.class.php', $path= '', $name.'.class.php', '<?php '.$bytes.' ?>');
-    }
+    protected $fixture= NULL;
     
     /**
-     * Creates a unique class name for the running test case
-     *
-     * @param   string prefix default ''
-     * @return  string
-     * @throws  lang.IllegalStateException in case the generated class name already exists!
-     */
-    protected function testClassName($prefix= '') {
-      $classname= $prefix.'ClassUsedForArchiveClassLoader'.ucfirst($this->name).'Test';
-      if (class_exists($classname, FALSE)) {
-        throw new IllegalStateException('Class '.$this->classname.' may not exist!');
-      }
-      return $classname;
-    }
-
-    /**
-     * Sets up test case
-     *
+     * Sets fixture to point to archive.xar from src/test/resources/
      */
     public function setUp() {
-      try {
-        $this->classname= $this->testClassName();
-        $this->interfacename= $this->testClassName('I');
-      } catch (IllegalStateException $e) {
-        throw new PrerequisitesNotMetError($e->getMessage());
-      }
-
-      // Create an archive
-      $this->tempfile= new TempFile($this->name);
-      $archive= new Archive($this->tempfile);
-      $archive->open(ARCHIVE_CREATE);
-
-      $this->add($archive, $this->classname, '
-        uses("util.Comparator", "'.$this->interfacename.'");
-        class '.$this->classname.' extends Object implements '.$this->interfacename.', Comparator { 
-          public function compare($a, $b) {
-            return strcmp($a, $b);
-          }
-        }
-      ');
-      $this->add($archive, $this->interfacename, 'interface '.$this->interfacename.' { } ');
-      $archive->create();
-      
-      // Setup classloader
-      $this->classloader= new ArchiveClassLoader($archive);
-      ClassLoader::getDefault()->registerLoader($this->classloader, TRUE);
+      $this->fixture= new ArchiveClassLoader(new Archive(
+        $this->getClass()->getPackage()->getResourceAsStream('archive.xar')
+      ));
     }
-    
+
+    /**
+     * Test providesClass() method
+     */
+    #[@test]
+    public function provides_class_in_archive() {
+      $this->assertTrue($this->fixture->providesClass('test.ClassLoadedFromArchive'));
+    }
+
+    /**
+     * Test providesClass() method
+     */
+    #[@test]
+    public function does_not_provide_non_existant_class() {
+      $this->assertFalse($this->fixture->providesClass('non.existant.Class'));
+    }
+
+    /**
+     * Test providesPackage() method
+     */
+    #[@test]
+    public function provides_package_in_archive() {
+      $this->assertTrue($this->fixture->providesPackage('test'));
+    }
+
+    /**
+     * Test providesPackage() method
+     */
+    #[@test]
+    public function does_not_provide_non_existant_package() {
+      $this->assertFalse($this->fixture->providesPackage('non.existant'));
+    }
+
+    /**
+     * Test providesResource() method
+     */
+    #[@test]
+    public function provides_resource_in_archive() {
+      $this->assertTrue($this->fixture->providesResource('test/package-info.xp'));
+    }
+
+    /**
+     * Test providesResource() method
+     */
+    #[@test]
+    public function does_not_provide_non_existant_resource() {
+      $this->assertFalse($this->fixture->providesResource('non/existant/resource.file'));
+    }
+
     /**
      * Test loadClass() method
-     *
      */
     #[@test]
-    public function loadClass() {
-      $this->assertEquals($this->classloader->loadClass($this->classname)->getName(), $this->classname);
-    }
-
-    /**
-     * Test class implements the interface from the archive
-     *
-     */
-    #[@test]
-    public function classImplementsArchivedInterface() {
-      $class= $this->classloader->loadClass($this->classname);
-      $interface= $this->classloader->loadClass($this->interfacename);
-
-      $interfaces= new HashSet();
-      $interfaces->addAll($class->getInterfaces());
-      $this->assertTrue($interfaces->contains($interface));
-    }
-
-    /**
-     * Test class implements the interface from the archive
-     *
-     */
-    #[@test]
-    public function classImplementsComparatorInterface() {
-      $class= $this->classloader->loadClass($this->classname);
-      $interface= XPClass::forName('util.Comparator');
-      $interfaces= new HashSet();
-      $interfaces->addAll($class->getInterfaces());
-      $this->assertTrue($interfaces->contains($interface));
+    public function load_existing_class_from_package() {
+      $this->assertInstanceOf('lang.XPClass', $this->fixture->loadClass('test.ClassLoadedFromArchive'));
     }
   }
 ?>
