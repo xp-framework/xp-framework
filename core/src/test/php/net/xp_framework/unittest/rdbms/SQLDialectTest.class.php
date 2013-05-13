137f80d8880dbd1c0ff8143183fb58caea86ad87
Timm Friebe <thekid@thekid.de>
2013-05-13 21:25:47 +0200

    - Remove assertion helper with longish verification logic   and replace them with `@values` annotation and provider method

--- core/src/test/php/net/xp_framework/unittest/rdbms/SQLDialectTest.class.php
+++ core/src/test/php/net/xp_framework/unittest/rdbms/SQLDialectTest.class.php
@@ -45,76 +45,57 @@
      *
      * @return  var[]
      */
-    public function connections() {
+    public function dialects() {
       $r= array();
       foreach ($this->conn as $name => $conn) {
-        $r[]= array($conn->getFormatter()->dialect);
+        $r[]= array($conn->getFormatter()->dialect, $name);
       }
       return $r;
     }
 
-    #[@test, @values('connections')]
+    #[@test, @values('dialects')]
     public function dialect_member($dialect) {
       $this->assertInstanceOf('rdbms.SQLDialect', $dialect);
     }
 
-    #[@test, @values('connections')]
+    #[@test, @values('dialects')]
     public function pi_function($dialect) {
       $this->assertEquals('pi()', $dialect->formatFunction(new SQLFunction('pi', '%s')));
     }
 
-    #[@test, @values('connections'), @expect('lang.IllegalArgumentException')]
+    #[@test, @values('dialects'), @expect('lang.IllegalArgumentException')]
     public function unknown_function($dialect) {
       $dialect->formatFunction(new SQLFunction('foo', 1, 2, 3, 4, 5));
     }
 
-    #[@test, @values('connections')]
+    #[@test, @values('dialects')]
     public function month_datepart($dialect) {
       $this->assertEquals('month', $dialect->datepart('month'));
     }
 
-    #[@test, @values('connections'), @expect('lang.IllegalArgumentException')]
+    #[@test, @values('dialects'), @expect('lang.IllegalArgumentException')]
     public function unknown_datepart($dialect) {
       $dialect->datepart('month_foo_bar_buz');
     }
 
-    #[@test, @values('connections')]
+    #[@test, @values('dialects')]
     public function int_datatype($dialect) {
       $this->assertEquals('int', $dialect->datatype('int'));
     }
 
-    #[@test, @values('connections'), @expect('lang.IllegalArgumentException')]
+    #[@test, @values('dialects'), @expect('lang.IllegalArgumentException')]
     public function unknown_datatype($dialect) {
       $dialect->datatype('int_foo_bar_buz');
     }
 
-    #[@test, @values('connections'), @expect('lang.IllegalArgumentException')]
+    #[@test, @values('dialects'), @expect('lang.IllegalArgumentException')]
     public function join_by_empty($dialect) {
       $dialect->makeJoinBy(array());
     }
 
-    /**
-     * helper function to test input to makeJoinBy
-     *
-     * @param  var[] conditions
-     * @param  var[] asserts
-     * @throws unittest.AssertionFailedError
-     */
-    private function assertJoin(array $conditions, array $asserts) {
-      foreach (array_keys($this->conn) as $connName) {
-        $dialect= $this->conn[$connName]->getFormatter()->dialect;
-        if (!array_key_exists($connName, $asserts)) throw new AssertionFailedError('test for '.$connName.' does not exist in test');
-        $this->assertEquals($asserts[$connName], $dialect->makeJoinBy($conditions));
-      }
-    }
-
-    /**
-     * test join formatter
-     *
-     */
-    #[@test]
-    public function joinTwoTablesTest() {
-      $asserts= array(
+    #[@test, @values('dialects')]
+    public function join_two_tables($dialect, $name) {
+      static $asserts= array(
         self::MYSQL  => 'table0 as t0 LEFT OUTER JOIN table1 as t1 on (t0.id1_1 = t0.id1_1 and t0.id1_2 = t0.id1_2) where ',
         self::SYBASE => 'table0 as t0, table1 as t1 where t0.id1_1 *= t0.id1_1 and t0.id1_2 *= t0.id1_2 and ',
       );
@@ -122,22 +103,14 @@
       $t0= new JoinTable('table0', 't0');
       $t1= new JoinTable('table1', 't1');
 
-      $conditions= array(
-        create(new JoinRelation($t0, $t1, array('t0.id1_1 = t0.id1_1', 't0.id1_2 = t0.id1_2')))
-      );
-      
-      $this->assertJoin(
-        $conditions,
-        $asserts
-      );
+      $this->assertEquals($asserts[$name], $dialect->makeJoinBy(array(
+        new JoinRelation($t0, $t1, array('t0.id1_1 = t0.id1_1', 't0.id1_2 = t0.id1_2'))
+      )));
     }
 
-    /**
-     * test join formatter
-     */
-    #[@test]
-    public function joinThreeTablesTest() {
-      $asserts= array(
+    #[@test, @values('dialects')]
+    public function join_three_tables($dialect, $name) {
+      static $asserts= array(
         self::MYSQL  => 'table0 as t0 LEFT OUTER JOIN table1 as t1 on (t0.id1_1 = t0.id1_1 and t0.id1_2 = t0.id1_2) LEFT JOIN table2 as t2 on (t1.id2_1 = t2.id2_1) where ',
         self::SYBASE => 'table0 as t0, table1 as t1, table2 as t2 where t0.id1_1 *= t0.id1_1 and t0.id1_2 *= t0.id1_2 and t1.id2_1 *= t2.id2_1 and ',
       );
@@ -146,15 +119,10 @@
       $t1= new JoinTable('table1', 't1');
       $t2= new JoinTable('table2', 't2');
 
-      $conditions= array(
-        create(new JoinRelation($t0, $t1, array('t0.id1_1 = t0.id1_1', 't0.id1_2 = t0.id1_2'))),
-        create(new JoinRelation($t1, $t2, array('t1.id2_1 = t2.id2_1'))),
-      );
-      
-      $this->assertJoin(
-        $conditions,
-        $asserts
-      );
+      $this->assertEquals($asserts[$name], $dialect->makeJoinBy(array(
+        new JoinRelation($t0, $t1, array('t0.id1_1 = t0.id1_1', 't0.id1_2 = t0.id1_2')),
+        new JoinRelation($t1, $t2, array('t1.id2_1 = t2.id2_1'))
+      )));
     }
   }
 ?>
