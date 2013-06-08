77e71562718d028186a88d6ffc967df6e0505817
Timm Friebe <thekid@thekid.de>
2013-06-08 13:49:09 +0200

    - Remove value object tests from serializer

--- core/src/test/php/net/xp_framework/unittest/webservices/rest/RestJsonSerializerTest.class.php
+++ core/src/test/php/net/xp_framework/unittest/webservices/rest/RestJsonSerializerTest.class.php
@@ -7,9 +7,6 @@
   uses(
     'unittest.TestCase',
     'webservices.rest.RestJsonSerializer',
-    'net.xp_framework.unittest.webservices.rest.IssueWithField',
-    'net.xp_framework.unittest.webservices.rest.IssueWithGetter',
-    'net.xp_framework.unittest.webservices.rest.IssueWithUnderscoreFieldsAndGetter',
     'util.Date',
     'util.TimeZone'
   );
@@ -46,54 +43,6 @@
     #[@test]
     public function intArray() {
       $this->assertEquals('[ 1 , 2 , 3 ]', $this->fixture->serialize(array(1, 2, 3)));
-    }
-
-    /**
-     * Test
-     *
-     */
-    #[@test]
-    public function issueWithField() {
-      $this->assertEquals(
-        '{ "issueId" : 1 , "title" : "New issue" }', 
-        $this->fixture->serialize(new net·xp_framework·unittest·webservices·rest·IssueWithField(1, 'New issue'))
-      );
-    }
-
-    /**
-     * Test
-     *
-     */
-    #[@test]
-    public function issueWithGetter() {
-      $this->assertEquals(
-        '{ "issueId" : 1 , "title" : "New issue" , "createdAt" : null }', 
-        $this->fixture->serialize(new net·xp_framework·unittest·webservices·rest·IssueWithGetter(1, 'New issue'))
-      );
-    }
-
-    /**
-     * Test
-     *
-     */
-    #[@test]
-    public function issueWithGetterAndDate() {
-      $this->assertEquals(
-        '{ "issueId" : 1 , "title" : "New issue" , "createdAt" : "2012-03-19T08:37:00+00:00" }', 
-        $this->fixture->serialize(new net·xp_framework·unittest·webservices·rest·IssueWithGetter(1, 'New issue', new Date('2012-03-19 08:37:00', TimeZone::getByName('GMT'))))
-      );
-    }
-
-    /**
-     * Test
-     *
-     */
-    #[@test]
-    public function issueWithUnderscoreFieldsAndGetter() {
-      $this->assertEquals(
-        '{ "issue_id" : 1 , "title" : "New issue" , "created_at" : null }', 
-        $this->fixture->serialize(new net·xp_framework·unittest·webservices·rest·IssueWithUnderscoreFieldsAndGetter(1, 'New issue'))
-      );
     }
   }
 ?>
--- core/src/test/php/net/xp_framework/unittest/webservices/rest/RestXmlSerializerTest.class.php
+++ core/src/test/php/net/xp_framework/unittest/webservices/rest/RestXmlSerializerTest.class.php
@@ -6,12 +6,7 @@
 
   uses(
     'unittest.TestCase',
-    'webservices.rest.RestXmlSerializer',
-    'net.xp_framework.unittest.webservices.rest.IssueWithField',
-    'net.xp_framework.unittest.webservices.rest.IssueWithGetter',
-    'net.xp_framework.unittest.webservices.rest.IssueWithUnderscoreFieldsAndGetter',
-    'util.Date',
-    'util.TimeZone'
+    'webservices.rest.RestXmlSerializer'
   );
 
   /**
@@ -62,54 +57,6 @@
       $this->assertXmlEquals(
         '<root><root>1</root><root>2</root><root>3</root></root>', 
         $this->fixture->serialize(array(1, 2, 3))
-      );
-    }
-
-    /**
-     * Test
-     *
-     */
-    #[@test]
-    public function issueWithField() {
-      $this->assertXmlEquals(
-        '<root><issueId>1</issueId><title>New issue</title></root>', 
-        $this->fixture->serialize(new net·xp_framework·unittest·webservices·rest·IssueWithField(1, 'New issue'))
-      );
-    }
-
-    /**
-     * Test
-     *
-     */
-    #[@test]
-    public function issueWithGetter() {
-      $this->assertXmlEquals(
-        '<root><issueId>1</issueId><title>New issue</title><createdAt></createdAt></root>', 
-        $this->fixture->serialize(new net·xp_framework·unittest·webservices·rest·IssueWithGetter(1, 'New issue'))
-      );
-    }
-
-    /**
-     * Test
-     *
-     */
-    #[@test]
-    public function issueWithGetterAndDate() {
-      $this->assertXmlEquals(
-        '<root><issueId>1</issueId><title>New issue</title><createdAt>2012-03-19T08:37:00+00:00</createdAt></root>', 
-        $this->fixture->serialize(new net·xp_framework·unittest·webservices·rest·IssueWithGetter(1, 'New issue', new Date('2012-03-19 08:37:00', TimeZone::getByName('GMT'))))
-      );
-    }
-
-    /**
-     * Test
-     *
-     */
-    #[@test]
-    public function issueWithUnderscoreFieldsAndGetter() {
-      $this->assertXmlEquals(
-        '<root><issue_id>1</issue_id><title>New issue</title><created_at></created_at></root>', 
-        $this->fixture->serialize(new net·xp_framework·unittest·webservices·rest·IssueWithUnderscoreFieldsAndGetter(1, 'New issue'))
       );
     }
   }
