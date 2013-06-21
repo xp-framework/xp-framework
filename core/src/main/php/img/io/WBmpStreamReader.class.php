4e482ce19b63b1fae277c5df6842e02e96c406c1
Timm Friebe <thekid@thekid.de>
2013-06-21 10:22:08 +0200

    - Refactor image reading implementations, cope w/ PHP bug 65060

--- core/src/main/php/img/io/WBmpStreamReader.class.php
+++ core/src/main/php/img/io/WBmpStreamReader.class.php
@@ -4,53 +4,31 @@
  * $Id$ 
  */
 
-  uses('img.io.StreamReader');
+  uses('img.io.StreamReader', 'img.io.UriReader');
 
   /**
    * Reads WBMP from an image
    *
-   * @ext      gd
-   * @see      php://imagecreatefromwbmp
-   * @see      xp://img.io.StreamReader
-   * @purpose  Reader
+   * @ext  gd
+   * @see  php://imagecreatefromwbmp
+   * @see  xp://img.io.StreamReader
    */
-  class WBmpStreamReader extends StreamReader {
+  class WBmpStreamReader extends StreamReader implements img·io·UriReader {
 
     /**
-     * Read image via imagecreatefromwbmp()
+     * Read image
      *
      * @param   string uri
      * @return  resource
      * @throws  img.ImagingException
      */
-    protected function readImage0($uri) {
+    public function readImageFromUri($uri) {
       if (FALSE === ($r= imagecreatefromwbmp($uri))) {
-        $e= new ImagingException('Cannot read image');
+        $e= new ImagingException('Cannot read image from "'.$uri.'"');
         xp::gc(__FILE__);
         throw $e;
       }
       return $r;
-    }
-
-    /**
-     * Read an image
-     *
-     * @deprecated
-     * @return  resource
-     * @throws  img.ImagingException
-     */    
-    public function readFromStream() {
-      return $this->readImage0($this->stream->getURI());
-    }
-    
-    /**
-     * Read an image
-     *
-     * @return  resource
-     * @throws  img.ImagingException
-     */    
-    public function readImage() {
-      return $this->readImage0(Streams::readableUri($this->stream));
     }
   }
 ?>
