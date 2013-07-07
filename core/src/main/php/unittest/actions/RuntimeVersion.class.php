5cd7c6d1f7eeb1a186e1ecffe49bd78cf497f79a
Timm Friebe <thekid@thekid.de>
2013-07-07 11:43:58 +0200

    - Initial release: Runtime version

---
+++ core/src/main/php/unittest/actions/RuntimeVersion.class.php
@@ -0,0 +1,92 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('unittest.TestAction');
+
+  /**
+   * Only runs this testcase on a given runtime version, e.g. PHP 5.3.0
+   *
+   * @see  http://getcomposer.org/doc/01-basic-usage.md#package-versions
+   */
+  class RuntimeVersion extends Object implements TestAction {
+    protected $compare= '';
+
+    /**
+     * Create a new RuntimeVersion match
+     *
+     * @param string pattern A pattern to match against PHP_VERSION
+     */
+    public function __construct($pattern) {
+      $cmp= array();
+      foreach (explode(',', $pattern) as $specifier) {
+        if ('*' === $specifier{strlen($specifier)- 1}) {
+          $cmp[]= function($compare) use($specifier) { return 0 === strncmp($compare, $specifier, strlen($specifier)- 1); };
+        } else if ('<' === $specifier{0}) {
+          if ('=' === $specifier{1}) {
+            $op= 'le';
+            $specifier= substr($specifier, 2);
+          } else {
+            $op= 'lt';
+            $specifier= substr($specifier, 1);
+          }
+          $cmp[]= function($compare) use($specifier, $op) { return version_compare($compare, $specifier, $op); };
+        } else if ('>' === $specifier{0}) {
+          if ('=' === $specifier{1}) {
+            $op= 'ge';
+            $specifier= substr($specifier, 2);
+          } else {
+            $op= 'gt';
+            $specifier= substr($specifier, 1);
+          }
+          $cmp[]= function($compare) use($specifier, $op) { return version_compare($compare, $specifier, $op); };
+        } else {
+          $cmp[]= function($compare) use($specifier) { return $compare === $specifier; };
+        }
+      }
+
+      $this->compare= function($compare) use($cmp) {
+        foreach ($cmp as $f) {
+          if (!$f($compare)) return FALSE;
+        }
+        return TRUE;
+      };
+    }
+
+    /**
+     * Verify a given OS matches this platform
+     *
+     * @param  string os The OS' name - omit to use current OS
+     * @return bool
+     */
+    public function verify($version= NULL) {
+      $version ?: $version= PHP_VERSION;
+      return call_user_func($this->compare, $version);
+    }
+
+    /**
+     * This method gets invoked before a test method is invoked, and before
+     * the setUp() method is called.
+     *
+     * @param  unittest.TestCase $t
+     * @throws unittest.PrerequisitesNotMetError
+     */
+    public function beforeTest(TestCase $t) { 
+      if (!$this->verify()) {
+        throw new PrerequisitesNotMetError('Test not intended for this platform ('.self::$os.')', NULL, array($this->platform));
+      }
+    }
+
+    /**
+     * This method gets invoked after the test method is invoked and regard-
+     * less of its outcome, after the tearDown() call has run.
+     *
+     * @param  unittest.TestCase $t
+     */
+    public function afterTest(TestCase $t) {
+      // Empty
+    }
+  }
+?>
---
+++ core/src/test/php/net/xp_framework/unittest/tests/RuntimeVersionTest.class.php
@@ -0,0 +1,69 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('unittest.actions.RuntimeVersion');
+
+  /**
+   * Test test action "Runtime Version"
+   */
+  class RuntimeVersionTest extends TestCase {
+
+    #[@test]
+    public function can_create() {
+      new RuntimeVersion('5.3.0');
+    }
+
+    #[@test]
+    public function exact_version_match() {
+      $this->assertTrue(create(new RuntimeVersion('5.3.0'))->verify('5.3.0'));
+    }
+
+    #[@test, @values(array('5.3.0', '5.3.6', '5.3.26'))]
+    public function wildcard_match($value) {
+      $this->assertTrue(create(new RuntimeVersion('5.3.*'))->verify($value));
+    }
+
+    #[@test, @values(array('4.3.0', '5.2.0', '5.2.99', '5.3.0RC1', '5.3.0alpha', '5.3.0beta'))]
+    public function smaller_than($value) {
+      $this->assertTrue(create(new RuntimeVersion('<5.3.0'))->verify($value));
+    }
+
+    #[@test, @values(array('4.3.0', '5.2.0', '5.2.99', '5.3.0RC1', '5.3.0alpha', '5.3.0beta', '5.3.0'))]
+    public function smaller_than_or_equal_to($value) {
+      $this->assertTrue(create(new RuntimeVersion('<=5.3.0'))->verify($value));
+    }
+
+    #[@test, @values(array('5.3.0', '5.3.1', '6.3.0'))]
+    public function not_smaller_than($value) {
+      $this->assertFalse(create(new RuntimeVersion('<5.3.0'))->verify($value));
+    }
+
+    #[@test, @values(array('5.3.1', '6.3.0'))]
+    public function not_smaller_than_or_equal_to($value) {
+      $this->assertFalse(create(new RuntimeVersion('<=5.3.0'))->verify($value));
+    }
+
+    #[@test, @values(array('5.3.1', '5.3.99', '6.3.0'))]
+    public function larger_than($value) {
+      $this->assertTrue(create(new RuntimeVersion('>5.3.0'))->verify($value));
+    }
+
+    #[@test, @values(array('5.3.0', '5.3.1', '5.3.99', '6.3.0'))]
+    public function larger_than_or_equal_to($value) {
+      $this->assertTrue(create(new RuntimeVersion('>=5.3.0'))->verify($value));
+    }
+
+    #[@test, @values(array('5.3.0', '5.3.0RC1', '5.3.0alpha', '5.3.0beta', '4.3.0'))]
+    public function not_larger_than($value) {
+      $this->assertFalse(create(new RuntimeVersion('>5.3.0'))->verify($value));
+    }
+
+    #[@test, @values(array('5.3.0RC1', '5.3.0alpha', '5.3.0beta', '4.3.0'))]
+    public function not_larger_than_or_equal_to($value) {
+      $this->assertFalse(create(new RuntimeVersion('>=5.3.0'))->verify($value));
+    }
+  }
+?>
