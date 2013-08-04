3e9723a09384ad32cc573b5c64d0f2d572156d49
Timm Friebe <thekid@thekid.de>
2013-08-04 12:48:21 +0200

    - QA: Refactor to use @values annotation

--- core/src/test/php/net/xp_framework/unittest/io/streams/TextReaderTest.class.php
+++ core/src/test/php/net/xp_framework/unittest/io/streams/TextReaderTest.class.php
@@ -189,33 +189,9 @@
      * Test reading lines separated by "\n"
      *
      */
-    #[@test]
-    public function readLinesSeparatedByLineFeed() {
-      $r= $this->newReader("Hello\nWorld");
-      $this->assertEquals('Hello', $r->readLine());
-      $this->assertEquals('World', $r->readLine());
-      $this->assertNull($r->readLine());
-    }
-        
-    /**
-     * Test reading lines separated by "\r"
-     *
-     */
-    #[@test]
-    public function readLinesSeparatedByCarriageReturn() {
-      $r= $this->newReader("Hello\rWorld");
-      $this->assertEquals('Hello', $r->readLine());
-      $this->assertEquals('World', $r->readLine());
-      $this->assertNull($r->readLine());
-    }
-        
-    /**
-     * Test reading lines separated by "\r\n"
-     *
-     */
-    #[@test]
-    public function readLinesSeparatedByCRLF() {
-      $r= $this->newReader("Hello\r\nWorld");
+    #[@test, @values(array("Hello\nWorld", "Hello\rWorld", "Hello\r\nWorld"))]
+    public function readLines($value) {
+      $r= $this->newReader($value);
       $this->assertEquals('Hello', $r->readLine());
       $this->assertEquals('World', $r->readLine());
       $this->assertNull($r->readLine());
