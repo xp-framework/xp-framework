7a34e325213093616d8852d573a444eb83707409
Timm Friebe <thekid@thekid.de>
2013-09-07 14:10:12 +0200

    - Convert package net.xp_framework.unittest.core.extensions to namespaces

--- core/src/test/php/net/xp_framework/unittest/core/extensions/ExtensionInvocationTest.class.php
+++ core/src/test/php/net/xp_framework/unittest/core/extensions/ExtensionInvocationTest.class.php
@@ -1,67 +1,45 @@
-<?php
-/* This class is part of the XP framework
+<?php namespace net\xp_framework\unittest\core\extensions;
+
+use unittest\TestCase;
+use lang\types\ArrayList;
+new \import('net.xp_framework.unittest.core.extensions.ArrayListExtensions');
+new \import('net.xp_framework.unittest.core.extensions.ThrowableExtensions');
+
+/**
+ * TestCase
  *
- * $Id$ 
+ * @see   xp://net.xp_framework.unittest.core.extensions.ArrayListExtensions
+ * @see   xp://net.xp_framework.unittest.core.extensions.ThrowableExtensions
+ * @see   https://github.com/xp-framework/xp-framework/issues/137
  */
+class ExtensionInvocationTest extends TestCase {
 
-  uses(
-    'unittest.TestCase', 
-    'lang.types.ArrayList',
-    'net.xp_framework.unittest.core.extensions.ArrayListExtensions',
-    'net.xp_framework.unittest.core.extensions.ThrowableExtensions'
-  );
-
-  /**
-   * TestCase
-   *
-   * @see   xp://net.xp_framework.unittest.core.extensions.ArrayListExtensions
-   */
-  class ExtensionInvocationTest extends TestCase {
-  
-    /**
-     * Test map() extension method
-     *
-     */
-    #[@test]
-    public function mapMethod() {
-      $this->assertEquals(
-        new ArrayList(2, 4, 6),
-        create(new ArrayList(1, 2, 3))->map(create_function('$e', 'return $e * 2;'))
-      );
-    }
-
-    /**
-     * Test sorted() extension method
-     *
-     */
-    #[@test]
-    public function sortedMethod() {
-      $this->assertEquals(
-        new ArrayList(-1, 0, 1, 7, 10),
-        create(new ArrayList(7, 0, 10, 1, -1))->sorted(SORT_NUMERIC)
-      );
-    }
-
-    /**
-     * Test invoking a non-existant extension method
-     *
-     */
-    #[@test, @expect('lang.Error')]
-    public function nonExistantExtensionMethod() {
-      create(new ArrayList(1, 2, 3))->nonExistant();
-    }
-
-    /**
-     * Test extension methods on lang.Throwable
-     *
-     * @see   https://github.com/xp-framework/xp-framework/issues/137
-     */
-    #[@test]
-    public function throwabeExtensions() {
-      $t= new Throwable('Test');
-      $this->assertNotEquals(array(), $t->getStackTrace());
-      $t->clearStackTrace();
-      $this->assertEquals(array(), $t->getStackTrace());
-    }
+  #[@test]
+  public function mapMethod() {
+    $this->assertEquals(
+      new ArrayList(2, 4, 6),
+      create(new ArrayList(1, 2, 3))->map(create_function('$e', 'return $e * 2;'))
+    );
   }
-?>
+
+  #[@test]
+  public function sortedMethod() {
+    $this->assertEquals(
+      new ArrayList(-1, 0, 1, 7, 10),
+      create(new ArrayList(7, 0, 10, 1, -1))->sorted(SORT_NUMERIC)
+    );
+  }
+
+  #[@test, @expect('lang.Error')]
+  public function nonExistantExtensionMethod() {
+    create(new ArrayList(1, 2, 3))->nonExistant();
+  }
+
+  #[@test]
+  public function throwabeExtensions() {
+    $t= new \lang\Throwable('Test');
+    $this->assertNotEquals(array(), $t->getStackTrace());
+    $t->clearStackTrace();
+    $this->assertEquals(array(), $t->getStackTrace());
+  }
+}
