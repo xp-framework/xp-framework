8ed204ba908cd09fed72e6a4f3c75e8fa080d553
Timm Friebe <thekid@thekid.de>
2013-09-12 09:32:04 +0200

    - Convert package net.xp_framework.unittest.logging to namespaces

--- core/src/test/php/net/xp_framework/unittest/logging/ConsoleAppenderTest.class.php
+++ core/src/test/php/net/xp_framework/unittest/logging/ConsoleAppenderTest.class.php
@@ -1,62 +1,56 @@
-<?php
-/* This class is part of the XP framework
- *
- * $Id$ 
- */
+<?php namespace net\xp_framework\unittest\logging;
 
-  uses(
-    'unittest.TestCase',
-    'util.cmd.Console',
-    'util.log.ConsoleAppender',
-    'util.log.LogCategory',
-    'util.log.Layout',
-    'io.streams.MemoryOutputStream'
-  );
+use unittest\TestCase;
+use util\cmd\Console;
+use util\log\ConsoleAppender;
+use util\log\LogCategory;
+use util\log\Layout;
+use io\streams\MemoryOutputStream;
+
+
+/**
+ * TestCase
+ *
+ * @see   xp://util.cmd.Console
+ * @see   xp://util.log.ConsoleAppender
+ */
+class ConsoleAppenderTest extends TestCase {
+  protected $cat= null;
+  protected $stream= null;
 
   /**
-   * TestCase
+   * Sets up test case and backups Console::$err stream.
    *
-   * @see   xp://util.cmd.Console
-   * @see   xp://util.log.ConsoleAppender
    */
-  class ConsoleAppenderTest extends TestCase {
-    protected $cat= NULL;
-    protected $stream= NULL;
-  
-    /**
-     * Sets up test case and backups Console::$err stream.
-     *
-     */
-    public function setUp() {
-      $this->cat= create(new LogCategory('default'))->withAppender(
-        create(new ConsoleAppender())->withLayout(newinstance('util.log.Layout', array(), '{
-          public function format(LoggingEvent $event) {
-            return implode(" ", $event->getArguments());
-          }
-        }'))
-      );
-      $this->stream= Console::$err->getStream();
-    }
+  public function setUp() {
+    $this->cat= create(new LogCategory('default'))->withAppender(
+      create(new ConsoleAppender())->withLayout(newinstance('util.log.Layout', array(), '{
+        public function format(LoggingEvent $event) {
+          return implode(" ", $event->getArguments());
+        }
+      }'))
+    );
+    $this->stream= Console::$err->getStream();
+  }
 
-    /**
-     * Restores Console::$err stream.
-     *
-     */
-    public function tearDown() {
-      Console::$err->setStream($this->stream);
-    }
+  /**
+   * Restores Console::$err stream.
+   *
+   */
+  public function tearDown() {
+    Console::$err->setStream($this->stream);
+  }
 
-    /**
-     * Test
-     *
-     */
-    #[@test]
-    public function appendMessage() {
-      with ($message= 'Test', $stream= new MemoryOutputStream()); {
-        Console::$err->setStream($stream);
-        $this->cat->warn($message);
-        $this->assertEquals($message, $stream->getBytes());
-      }
+  /**
+   * Test
+   *
+   */
+  #[@test]
+  public function appendMessage() {
+    with ($message= 'Test', $stream= new MemoryOutputStream()); {
+      Console::$err->setStream($stream);
+      $this->cat->warn($message);
+      $this->assertEquals($message, $stream->getBytes());
     }
   }
-?>
+}
