6295ce22c135468d45c263e63a711d1b2adab5d7
Timm Friebe <thekid@thekid.de>
2013-09-12 13:09:38 +0200

    - Convert to new coding standards

--- core/src/test/php/net/xp_framework/unittest/text/doclet/Namespaced.class.php
+++ core/src/test/php/net/xp_framework/unittest/text/doclet/Namespaced.class.php
@@ -1,16 +1,8 @@
-<?php
-/* This class is part of the XP framework
+<?php namespace net\xp_framework\unittest\text\doclet;
+
+/**
+ * Class Namespaced (used in ClassDocTest)
  *
- * $Id$
  */
-
-  namespace net\xp_framework\unittest\text\doclet;
-  
-  /**
-   * Class Namespaced (used in ClassDocTest)
-   *
-   */
-  class Namespaced extends \Object {
-  }
-
-?>
+class Namespaced extends \Object {
+}
