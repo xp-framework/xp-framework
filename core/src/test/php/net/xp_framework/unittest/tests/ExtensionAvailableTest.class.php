6fecabe4e8bfd6c527e733058c64fde01935b943
Timm Friebe <thekid@thekid.de>
2013-10-13 12:54:00 +0200

    - Convert to namespaces

--- core/src/test/php/net/xp_framework/unittest/tests/ExtensionAvailableTest.class.php
+++ core/src/test/php/net/xp_framework/unittest/tests/ExtensionAvailableTest.class.php
@@ -1,29 +1,24 @@
-<?php
-/* This class is part of the XP framework
- *
- * $Id$
+<?php namespace net\xp_framework\unittest\tests;
+
+use unittest\actions\ExtensionAvailable;
+
+/**
+ * Test test action "Extension Available"
  */
+class ExtensionAvailableTest extends \unittest\TestCase {
 
-  uses('unittest.actions.ExtensionAvailable');
-
-  /**
-   * Test test action "Extension Available"
-   */
-  class ExtensionAvailableTest extends TestCase {
-
-    #[@test]
-    public function can_create() {
-      new ExtensionAvailable('core');
-    }
-
-    #[@test]
-    public function verify_core_extension() {
-      $this->assertTrue(create(new ExtensionAvailable('core'))->verify());
-    }
-
-    #[@test]
-    public function verify_non_existant_extension() {
-      $this->assertFalse(create(new ExtensionAvailable('@@non-existant@@'))->verify());
-    }
+  #[@test]
+  public function can_create() {
+    new ExtensionAvailable('core');
   }
-?>
+
+  #[@test]
+  public function verify_core_extension() {
+    $this->assertTrue(create(new ExtensionAvailable('core'))->verify());
+  }
+
+  #[@test]
+  public function verify_non_existant_extension() {
+    $this->assertFalse(create(new ExtensionAvailable('@@non-existant@@'))->verify());
+  }
+}
