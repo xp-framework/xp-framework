a878f4de9c18bca3f7780c32a29dea057bf058b6
Timm Friebe <thekid@thekid.de>
2013-10-19 15:52:41 +0200

    - Also test static members

--- core/src/test/php/net/xp_framework/unittest/annotations/ClassMemberParsingTest.class.php
+++ core/src/test/php/net/xp_framework/unittest/annotations/ClassMemberParsingTest.class.php
@@ -1,6 +1,7 @@
 <?php namespace net\xp_framework\unittest\annotations;
 
 use net\xp_framework\unittest\annotations\fixture\Namespaced;
+use lang\types\String;
 
 /**
  * Tests the XP Framework's annotation parsing implementation
@@ -9,10 +10,16 @@ use net\xp_framework\unittest\annotations\fixture\Namespaced;
  */
 class ClassMemberParsingTest extends \unittest\TestCase {
   const CONSTANT = 'local';
+  protected static $value = 'static';
 
   #[@test, @values([self::CONSTANT])]
   public function class_constant_via_self($value) {
     $this->assertEquals('local', $value);
+  }
+
+  #[@test, @values([new String(self::CONSTANT)])]
+  public function class_constant_via_self_inside_new($value) {
+    $this->assertEquals(new String('local'), $value);
   }
 
   #[@test, @values([ClassMemberParsingTest::CONSTANT])]
@@ -33,5 +40,25 @@ class ClassMemberParsingTest extends \unittest\TestCase {
   #[@test, @values([\net\xp_framework\unittest\annotations\fixture\Namespaced::CONSTANT])]
   public function class_constant_via_fully_qualified($value) {
     $this->assertEquals('namespaced', $value);
+  }
+
+  #[@test, @values([self::$value])]
+  public function static_member_via_self($value) {
+    $this->assertEquals('static', $value);
+  }
+
+  #[@test, @values([new String(self::$value)])]
+  public function static_member_via_self_inside_new($value) {
+    $this->assertEquals(new String('static'), $value);
+  }
+
+  #[@test, @values([ClassMemberParsingTest::$value])]
+  public function static_member_via_unqualified_current($value) {
+    $this->assertEquals('static', $value);
+  }
+
+  #[@test, @values([\net\xp_framework\unittest\annotations\ClassMemberParsingTest::$value])]
+  public function static_member_via_fully_qualified_current($value) {
+    $this->assertEquals('static', $value);
   }
 }
