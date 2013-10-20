94cfd653eabb25cc3f31f1e0b0f674532d13e6fd
Timm Friebe <thekid@thekid.de>
2013-10-20 13:19:50 +0200

    - Add tests for various types

--- core/src/test/php/net/xp_framework/unittest/webservices/rest/RestJsonSerializerTest.class.php
+++ core/src/test/php/net/xp_framework/unittest/webservices/rest/RestJsonSerializerTest.class.php
@@ -19,7 +19,37 @@ class RestJsonSerializerTest extends TestCase {
   public function setUp() {
     $this->fixture= new RestJsonSerializer();
   }
-  
+
+  #[@test]
+  public function null() {
+    $this->assertEquals('null', $this->fixture->serialize(null));
+  }
+
+  #[@test, @values(['', 'Test'])]
+  public function strings($str) {
+    $this->assertEquals('"'.$str.'"', $this->fixture->serialize($str));
+  }
+
+  #[@test, @values([-1, 0, 1, 4711])]
+  public function integers($int) {
+    $this->assertEquals(''.$int, $this->fixture->serialize($int));
+  }
+
+  #[@test, @values([-1.0, 0.0, 1.0, 47.11])]
+  public function decimals($decimal) {
+    $this->assertEquals(''.$decimal, $this->fixture->serialize($decimal));
+  }
+
+  #[@test]
+  public function boolean_true() {
+    $this->assertEquals('true', $this->fixture->serialize(true));
+  }
+
+  #[@test]
+  public function boolean_false() {
+    $this->assertEquals('false', $this->fixture->serialize(false));
+  }
+
   #[@test]
   public function empty_array() {
     $this->assertEquals('[ ]', $this->fixture->serialize(array()));
@@ -33,5 +63,13 @@ class RestJsonSerializerTest extends TestCase {
   #[@test]
   public function string_array() {
     $this->assertEquals('[ "a" , "b" , "c" ]', $this->fixture->serialize(array('a', 'b', 'c')));
+  }
+
+  #[@test]
+  public function string_map() {
+    $this->assertEquals(
+      '{ "a" : "One" , "b" : "Two" , "c" : "Three" }',
+      $this->fixture->serialize(array('a' => 'One', 'b' => 'Two', 'c' => 'Three'))
+    );
   }
 }
