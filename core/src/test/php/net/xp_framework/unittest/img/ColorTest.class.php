0e1163a606a345ed6fbd3c253b13704fc755865d
Timm Friebe <thekid@thekid.de>
2013-11-09 13:18:59 +0100

    - Add net.xp_framework.unittest.img.ColorTest

--- core/src/test/php/net/xp_framework/unittest/img/ColorTest.class.php
+++ core/src/test/php/net/xp_framework/unittest/img/ColorTest.class.php
@@ -1,6 +1,5 @@
 <?php namespace net\xp_framework\unittest\img;
 
-use unittest\TestCase;
 use img\Color;
 
 /**
@@ -8,47 +7,31 @@ use img\Color;
  *
  * @see   xp://img.Color
  */
-class ColorTest extends TestCase {
+class ColorTest extends \unittest\TestCase {
 
-  #[@test]
-  public function createFromHexStringWithHash() {
-    $c= new Color('#fe3200');
-    $this->assertEquals(0xFE, $c->red);
-    $this->assertEquals(0x32, $c->green);
-    $this->assertEquals(0x00, $c->blue);
-  }
-
-  #[@test]
-  public function createFromHexString() {
-    $c= new Color('a7ff03');
+  #[@test, @values(['#a7ff03', 'a7ff03', 'A7FF03', '#A7FF03'])]
+  public function create_from_hex($value) {
+    $c= new Color($value);
     $this->assertEquals(0xA7, $c->red);
     $this->assertEquals(0xFF, $c->green);
     $this->assertEquals(0x03, $c->blue);
   }
 
   #[@test]
-  public function createFromHexStringWithCapitalLetters() {
-    $c= new Color('A7FF03');
-    $this->assertEquals(0xA7, $c->red);
-    $this->assertEquals(0xFF, $c->green);
-    $this->assertEquals(0x03, $c->blue);
+  public function create_from_three_rgb_values() {
+    $c= new Color(1, 2, 3);
+    $this->assertEquals(1, $c->red);
+    $this->assertEquals(2, $c->green);
+    $this->assertEquals(3, $c->blue);
   }
 
   #[@test]
-  public function createFromRgb() {
-    $c= new Color(0, 0, 0);
-    $this->assertEquals(0, $c->red);
-    $this->assertEquals(0, $c->green);
-    $this->assertEquals(0, $c->blue);
-  }
-
-  #[@test]
-  public function toHex() {
+  public function toHex_returns_lowercase_hex_with_leading_hash() {
     $this->assertEquals('#efefef', create(new Color('#efefef'))->toHex());
   }
 
   #[@test]
-  public function toString() {
+  public function string_representation() {
     $this->assertEquals('img.Color@(239, 010, 007)', create(new Color('#ef0a07'))->toString());
   }
 }
