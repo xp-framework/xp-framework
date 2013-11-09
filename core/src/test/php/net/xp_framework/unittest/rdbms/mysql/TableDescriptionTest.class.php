7fab0a7a04c7ab1716dcbae088e688f2128885c5
Timm Friebe <thekid@thekid.de>
2013-11-09 15:08:18 +0100

    - Add net.xp_framework.unittest.rdbms.mysql.TableDescriptionTest

--- core/src/test/php/net/xp_framework/unittest/rdbms/mysql/TableDescriptionTest.class.php
+++ core/src/test/php/net/xp_framework/unittest/rdbms/mysql/TableDescriptionTest.class.php
@@ -1,25 +1,16 @@
 <?php namespace net\xp_framework\unittest\rdbms\mysql;
 
-
-
-use unittest\TestCase;
 use rdbms\mysql\MySQLDBAdapter;
-
 
 /**
  * TestCase
  *
- * @see      xp://rdbms.mysql.MySQLDBAdapter
- * @purpose  Unittest
+ * @see    xp://rdbms.mysql.MySQLDBAdapter
  */
-class TableDescriptionTest extends TestCase {
+class TableDescriptionTest extends \unittest\TestCase {
 
-  /**
-   * Test an auto_increment field
-   *
-   */
   #[@test]
-  public function autoIncrement() {
+  public function auto_increment() {
     $this->assertEquals(
       new \rdbms\DBTableAttribute('contract_id', DB_ATTRTYPE_INT, true, false, 8, 0, 0),
       MySQLDBAdapter::tableAttributeFrom(array(
@@ -33,12 +24,8 @@ class TableDescriptionTest extends TestCase {
     );
   }
 
-  /**
-   * Test a field with unsigned
-   *
-   */
   #[@test]
-  public function unsignedInt() {
+  public function unsigned_int() {
     $this->assertEquals(
       new \rdbms\DBTableAttribute('bz_id', DB_ATTRTYPE_INT, false, false, 6, 0, 0),
       MySQLDBAdapter::tableAttributeFrom(array(
