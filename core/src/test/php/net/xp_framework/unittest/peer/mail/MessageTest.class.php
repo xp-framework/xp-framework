109a2d6fd35771be07b27cab11352f5746e57079
Igor Lankin <igor.lankin@1und1.de>
2013-11-24 02:27:45 +0100

    fix tests using ucfirst message headers

--- core/src/test/php/net/xp_framework/unittest/peer/mail/MessageTest.class.php
+++ core/src/test/php/net/xp_framework/unittest/peer/mail/MessageTest.class.php
@@ -18,15 +18,15 @@ class MessageTest extends AbstractMessageTest {
 
   #[@test]
   public function default_headers_returned_by_getHeaderString() {
-    $this->fixture->setHeader('X-Common-Header', 'test');
+    $this->fixture->setHeader('x-common-header', 'test');
     $this->assertEquals(
-      "X-Common-Header: test\n".
-      "Content-Type: text/plain;\n".
+      "x-common-header: test\n".
+      "content-type: text/plain;\n".
       "\tcharset=\"iso-8859-1\"\n".
-      "Mime-Version: 1.0\n".
-      "Content-Transfer-Encoding: 8bit\n".
-      "X-Priority: 3 (Normal)\n".
-      "Date: ".$this->fixture->getDate()->toString('r')."\n",
+      "mime-version: 1.0\n".
+      "content-transfer-encoding: 8bit\n".
+      "x-priority: 3 (Normal)\n".
+      "date: ".$this->fixture->getDate()->toString('r')."\n",
       $this->fixture->getHeaderString()
     );
   }
