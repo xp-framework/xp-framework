9b4dbc868ff865203865d047cf73fc07b2d1ff86
Timm Friebe <thekid@thekid.de>
2014-07-31 21:31:12 +0200

    MFH: Reorganized HashTable tests Backported for PHP 5.3 compatibility Includes test for xp-framework/xp-framework#361

--- core/src/test/php/net/xp_framework/unittest/util/collections/HashTableTest.class.php
+++ core/src/test/php/net/xp_framework/unittest/util/collections/HashTableTest.class.php
@@ -2,274 +2,346 @@
  
 use unittest\TestCase;
 use util\collections\HashTable;
+use util\collections\Pair;
 use lang\types\String;
-
+use lang\types\Integer;
+use lang\types\Double;
 
 /**
  * Test HashTable class
  *
- * @see      xp://util.collections.HashTable
- * @purpose  Unit Test
+ * @see   xp://util.collections.HashTable
  */
 class HashTableTest extends TestCase {
-  public
-    $map= null;
-  
-  /**
-   * Setup method. Creates the map member
-   *
-   */
-  public function setUp() {
-    $this->map= new HashTable();
-  }
-      
-  /**
-   * Tests the map is initially empty
-   *
-   */
-  #[@test]
-  public function initiallyEmpty() {
-    $this->assertTrue($this->map->isEmpty());
-  }
 
-  /**
-   * Tests map equals its clone
-   *
-   */
-  #[@test]
-  public function equalsClone() {
-    $this->map->put(new String('color'), new String('green'));
-    $this->assertTrue($this->map->equals(clone($this->map)));
-  }
- 
-  /**
-   * Tests map equals another map with the same contents
-   *
-   */
-  #[@test]
-  public function equalsOtherMapWithSameContents() {
-    $other= new HashTable();
-    $this->map->put(new String('color'), new String('green'));
-    $other->put(new String('color'), new String('green'));
-    $this->assertTrue($this->map->equals($other));
-  }
-
-  /**
-   * Tests map does not equal map with different contents
-   *
-   */
-  #[@test]
-  public function doesNotEqualMapWithDifferentContents() {
-    $other= new HashTable();
-    $this->map->put(new String('color'), new String('blue'));
-    $other->put(new String('color'), new String('yellow'));
-    $this->assertFalse($this->map->equals($other));
-  }
- 
-  /**
-   * Tests put()
-   *
-   */
-  #[@test]
-  public function put() {
-    $this->map->put(new String('color'), new String('green'));
-    $this->assertFalse($this->map->isEmpty());
-    $this->assertEquals(1, $this->map->size());
-  }
-
-  /**
-   * Tests put() returns previous value
-   *
-   */
-  #[@test]
-  public function putReturnsPreviousValue() {
-    $color= new String('color');
-    $this->assertNull($this->map->put($color, new String('green')));
-    $this->assertEquals(new String('green'), $this->map->put($color, new String('red')));
-    $this->assertEquals(new String('red'), $this->map->get($color));
-  }
-
-  /**
-   * Tests get()
-   *
-   */
-  #[@test]
-  public function get() {
-    $this->map->put(new String('key'), new String('value'));
-    $this->assertEquals(new String('value'), $this->map->get(new String('key')));
-  }
-
-  /**
-   * Tests get() returns NULL if the list is empty
-   *
-   */
-  #[@test]
-  public function getReturnsNullOnEmptyList() {
-    $this->assertTrue($this->map->isEmpty());
-    $this->assertNull($this->map->get(new String('key')));
-  }
-
-  /**
-   * Tests remove()
-   *
-   */
-  #[@test]
-  public function remove() {
-    $this->map->put(new String('key'), new String('value'));
-    $this->map->remove(new String('key'));
-    $this->assertTrue($this->map->isEmpty());
-  }
-
-  /**
-   * Tests remove() returns previous value
-   *
-   */
-  #[@test]
-  public function removeReturnsPreviousValue() {
-    $this->map->put(new String('key'), new String('value'));
-    $this->assertEquals(new String('value'), $this->map->remove(new String('key')));
-  }
-
-  /**
-   * Tests containsKey() method
-   *
-   */
-  #[@test]
-  public function containsKey() {
-    $this->map->put(new String('key'), new String('value'));
-    $this->assertTrue($this->map->containsKey(new String('key')));
-    $this->assertFalse($this->map->containsKey(new String('non-existant-key')));
-  }
-  
-  /**
-   * Tests clear() method
-   *
-   */
-  #[@test]
-  public function clear() {
-    $this->map->put(new String('key'), new String('value'));
-    $this->map->clear();
-    $this->assertTrue($this->map->isEmpty());
-  }
-
-  /**
-   * Tests containsValue() method
-   *
-   */
-  #[@test]
-  public function containsValue() {
-    $this->map->put(new String('key'), new String('value'));
-    $this->assertTrue($this->map->containsValue(new String('value')));
-    $this->assertFalse($this->map->containsValue(new String('non-existant-value')));
-  }
-
-  /**
-   * Tests keys() method
-   *
-   */
-  #[@test]
-  public function keys() {
-    $this->map->put(new String('key'), new String('value'));
-    $this->assertEquals(array(new String('key')), $this->map->keys());
-  }
-
-  /**
-   * Tests values() method
-   *
-   */
-  #[@test]
-  public function values() {
-    $this->map->put(new String('key'), new String('value'));
-    $this->assertEquals(array(new String('value')), $this->map->values());
-  }
-
-  /**
-   * Tests toString() method
-   *
-   */
-  #[@test]
-  public function stringRepresentation() {
-    $this->map->put(new String('color'), new String('purple'));
-    $this->map->put(new String('price'), new String('25 USD'));
-    $this->assertEquals(
-      "util.collections.HashTable[2] {\n  color => purple,\n  price => 25 USD\n}",
-      $this->map->toString()
+  /** @return var[] */
+  protected function variations() {
+    return array(
+      array(new HashTable(), array(
+        new Pair('color', 'pink'),
+        new Pair('price', null)
+      )),
+      array(new HashTable(), array(
+        new Pair(new String('color'), new String('green')),
+        new Pair(new String('price'), new Double(12.99))
+      )),
+      array(create('new util.collections.HashTable<string, lang.types.String>'), array(
+        new Pair('hello', new String('World')),
+        new Pair('hallo', new String('Welt'))
+      )),
+      array(create('new util.collections.HashTable<lang.types.Integer, string[]>'), array(
+        new Pair(new Integer(1), array('one', 'eins')),
+        new Pair(new Integer(2), array('two', 'zwei'))
+      )),
+      array(create('new util.collections.HashTable<int[], var>'), array(
+        new Pair(array(1, 2), 3),
+        new Pair(array(0, -1), 'Test')
+      ))
     );
   }
 
-  /**
-   * Tests toString() method on an empty map
-   *
-   */
-  #[@test]
-  public function stringRepresentationOfEmptyMap() {
-    $this->assertEquals(
-      'util.collections.HashTable[0] { }',
-      $this->map->toString()
-    );
+  #@test, @values('variations')]
+  public function can_create($fixture, $pairs) {
+    // Intentionally empty
   }
 
-  /**
-   * Populates the fixture
-   *
-   * @return  [:lang.types.String[]]
-   */
-  protected function populatedMapFixture() {
-    $keys= array(new String('color'), new String('price'));
-    $values= array(new String('purple'), new String('25 USD'));
-    foreach ($keys as $i => $key) {
-      $this->map->put($key, $values[$i]);
+  #[@test, @values('variations')]
+  public function map_is_initially_empty($fixture, $pairs) {
+    $this->assertTrue($fixture->isEmpty());
+  }
+
+  #[@test, @values('variations')]
+  public function map_size_is_initially_zero($fixture, $pairs) {
+    $this->assertEquals(0, $fixture->size());
+  }
+
+  #[@test, @values('variations')]
+  public function put($fixture, $pairs) {
+    $fixture->put($pairs[0]->key, $pairs[0]->value);
+  }
+
+  #[@test, @values('variations')]
+  public function array_access_for_writing($fixture, $pairs) {
+    $fixture[$pairs[0]->key]= $pairs[0]->value;
+  }
+
+  #[@test, @values('variations')]
+  public function put_returns_previously_value($fixture, $pairs) {
+    $fixture->put($pairs[0]->key, $pairs[0]->value);
+    $this->assertEquals($pairs[0]->value, $fixture->put($pairs[0]->key, $pairs[1]->value));
+  }
+
+  #[@test, @values('variations')]
+  public function map_no_longer_empty_after_put($fixture, $pairs) {
+    $fixture->put($pairs[0]->key, $pairs[0]->value);
+    $this->assertFalse($fixture->isEmpty());
+  }
+
+  #[@test, @values('variations')]
+  public function map_size_no_longer_zero_after_put($fixture, $pairs) {
+    $fixture->put($pairs[0]->key, $pairs[0]->value);
+    $this->assertEquals(1, $fixture->size());
+  }
+
+  #[@test, @expect('lang.IllegalArgumentException')]
+  public function put_illegal_type_in_key() {
+    create('new util.collections.HashTable<string, lang.types.String>')->put(5, new String('hello'));
+  }
+
+  #[@test, @expect('lang.IllegalArgumentException')]
+  public function put_illegal_type_in_value() {
+    create('new util.collections.HashTable<string, lang.types.String>')->put('hello', new Integer(1));
+  }
+
+  #[@test, @expect('lang.IllegalArgumentException')]
+  public function put_raises_when_using_null_for_string_instance() {
+    create('new util.collections.HashTable<string, lang.types.String>')->put('test', null);
+  }
+
+  #[@test, @expect('lang.IllegalArgumentException')]
+  public function put_raises_when_using_null_for_arrays() {
+    create('new util.collections.HashTable<string, var[]>')->put('test', null);
+  }
+
+  #[@test, @values('variations')]
+  public function get_returns_null_when_key_does_not_exist($fixture, $pairs) {
+    $this->assertNull($fixture->get($pairs[0]->key));
+  }
+
+  #[@test, @values('variations')]
+  public function get_returns_previously_put_element($fixture, $pairs) {
+    $fixture->put($pairs[0]->key, $pairs[0]->value);
+    $this->assertEquals($pairs[0]->value, $fixture->get($pairs[0]->key));
+  }
+
+  #[@test, @values('variations')]
+  public function array_access_for_reading_non_existant($fixture, $pairs) {
+    $this->assertNull($fixture[$pairs[0]->key]);
+  }
+
+  #[@test, @values('variations')]
+  public function array_access_for_reading($fixture, $pairs) {
+    $fixture->put($pairs[0]->key, $pairs[0]->value);
+    $this->assertEquals($pairs[0]->value, $fixture[$pairs[0]->key]);
+  }
+
+  #[@test, @expect('lang.IllegalArgumentException')]
+  public function get_illegal_type_in_argument() {
+    create('new util.collections.HashTable<lang.types.String, lang.types.String>')->get(new Integer(1));
+  }
+
+  #[@test, @values('variations')]
+  public function containsKey_returns_false_when_element_does_not_exist($fixture, $pairs) {
+    $this->assertFalse($fixture->containsKey($pairs[0]->key));
+  }
+
+  #[@test, @values('variations')]
+  public function containsKey_returns_true_when_element_exists($fixture, $pairs) {
+    $fixture->put($pairs[0]->key, $pairs[0]->value);
+    $this->assertTrue($fixture->containsKey($pairs[0]->key));
+  }
+
+  #[@test, @values('variations')]
+  public function array_access_for_testing_non_existant($fixture, $pairs) {
+    $this->assertFalse(isset($fixture[$pairs[0]->key]));
+  }
+
+  #[@test, @values('variations')]
+  public function array_access_for_testing($fixture, $pairs) {
+    $fixture->put($pairs[0]->key, $pairs[0]->value);
+    $this->assertTrue(isset($fixture[$pairs[0]->key]));
+  }
+
+  #[@test, @expect('lang.IllegalArgumentException')]
+  public function containsKey_illegal_type_in_argument() {
+    create('new util.collections.HashTable<lang.types.String, lang.types.String>')->containsKey(new Integer(1));
+  }
+
+  #[@test, @values('variations')]
+  public function containsValue_returns_false_when_element_does_not_exist($fixture, $pairs) {
+    $this->assertFalse($fixture->containsValue($pairs[0]->value));
+  }
+
+  #[@test, @values('variations')]
+  public function containsValue_returns_true_when_element_exists($fixture, $pairs) {
+    $fixture->put($pairs[0]->key, $pairs[0]->value);
+    $this->assertTrue($fixture->containsValue($pairs[0]->value));
+  }
+
+  #[@test, @expect('lang.IllegalArgumentException')]
+  public function containsValue_illegal_type_in_argument() {
+    create('new util.collections.HashTable<lang.types.String, lang.types.String>')->containsValue(new Integer(1));
+  }
+
+  #[@test, @values('variations')]
+  public function remove_returns_previously_value($fixture, $pairs) {
+    $fixture->put($pairs[0]->key, $pairs[0]->value);
+    $this->assertEquals($pairs[0]->value, $fixture->remove($pairs[0]->key));
+  }
+
+  #[@test, @values('variations')]
+  public function remove_previously_put_element($fixture, $pairs) {
+    $fixture->put($pairs[0]->key, $pairs[0]->value);
+    $fixture->remove($pairs[0]->key);
+    $this->assertFalse($fixture->containsKey($pairs[0]->key));
+  }
+
+  #[@test, @values('variations')]
+  public function array_access_for_removing($fixture, $pairs) {
+    $fixture->put($pairs[0]->key, $pairs[0]->value);
+    unset($fixture[$pairs[0]->key]);
+    $this->assertFalse($fixture->containsKey($pairs[0]->key));
+  }
+
+  #[@test, @values('variations')]
+  public function remove_non_existant_element($fixture, $pairs) {
+    $fixture->remove($pairs[0]->key);
+    $this->assertFalse($fixture->containsKey($pairs[0]->key));
+  }
+
+  #[@test, @values('variations')]
+  public function array_access_for_removing_non_existant_element($fixture, $pairs) {
+    unset($fixture[$pairs[0]->key]);
+    $this->assertFalse($fixture->containsKey($pairs[0]->key));
+  }
+
+  #[@test, @expect('lang.IllegalArgumentException')]
+  public function remove_illegal_type_in_argument() {
+    create('new util.collections.HashTable<lang.types.String, lang.types.String>')->remove(new Integer(1));
+  }
+
+  #[@test, @values('variations')]
+  public function equals_its_clone($fixture, $pairs) {
+    $fixture->put($pairs[0]->key, $pairs[0]->value);
+    $this->assertEquals($fixture, clone $fixture);
+  }
+
+  #[@test, @values('variations')]
+  public function equals_its_clone_when_empty($fixture, $pairs) {
+    $this->assertEquals($fixture, clone $fixture);
+  }
+
+  #[@test, @values('variations')]
+  public function does_not_equal_empty_map($fixture, $pairs) {
+    $other= clone $fixture;
+    $fixture->put($pairs[0]->key, $pairs[0]->value);
+    $this->assertNotEquals($fixture, $other);
+  }
+
+  #[@test, @values('variations')]
+  public function does_not_equal_map_with_different_elements($fixture, $pairs) {
+    $other= clone $fixture;
+    $fixture->put($pairs[0]->key, $pairs[0]->value);
+    $other->put($pairs[1]->key, $pairs[1]->value);
+    $this->assertNotEquals($fixture, $other);
+  }
+
+  #[@test, @values('variations')]
+  public function clear($fixture, $pairs) {
+    $fixture->put($pairs[0]->key, $pairs[0]->value);
+    $fixture->clear();
+    $this->assertTrue($fixture->isEmpty());
+  }
+
+  #[@test, @values('variations')]
+  public function keys_returns_empty_array_for_empty_map($fixture, $pairs) {
+    $this->assertEquals(array(), $fixture->keys());
+  }
+
+  #[@test, @values('variations')]
+  public function keys_returns_array_of_added_keys($fixture, $pairs) {
+    $fixture->put($pairs[0]->key, $pairs[0]->value);
+    $fixture->put($pairs[1]->key, $pairs[1]->value);
+    $this->assertEquals(array($pairs[0]->key, $pairs[1]->key), $fixture->keys());
+  }
+
+  #[@test, @values('variations')]
+  public function values_returns_empty_array_for_empty_map($fixture, $pairs) {
+    $this->assertEquals(array(), $fixture->values());
+  }
+
+  #[@test, @values('variations')]
+  public function values_returns_array_of_added_values($fixture, $pairs) {
+    $fixture->put($pairs[0]->key, $pairs[0]->value);
+    $fixture->put($pairs[1]->key, $pairs[1]->value);
+    $this->assertEquals(array($pairs[0]->value, $pairs[1]->value), $fixture->values());
+  }
+
+  #[@test, @values('variations')]
+  public function can_be_used_in_foreach($fixture, $pairs) {
+    $fixture->put($pairs[0]->key, $pairs[0]->value);
+    $fixture->put($pairs[1]->key, $pairs[1]->value);
+    $iterated= array();
+    foreach ($fixture as $pair) {
+      $iterated[]= $pair;
     }
-    return array('keys' => $keys, 'values' => $values);
+    $this->assertEquals(array($pairs[0], $pairs[1]), $iterated);
   }
 
-  /**
-   * Iterate on map
-   *
-   * @param  [:lang.types.String[]] expected
-   * @throws unittest.AssertionFailedError
-   */
-  protected function assertMapIteration($expected) {
-    $actual= array('keys' => array(), 'values' => array());
-    foreach ($this->map as $i => $pair) {
-      $actual['keys'][$i]= $pair->key;
-      $actual['values'][$i]= $pair->value;
+  #[@test, @values('variations')]
+  public function can_be_used_in_foreach_with_empty_map($fixture, $pairs) {
+    $iterated= array();
+    foreach ($fixture as $pair) {
+      $iterated[]= $pair;
     }
-    $this->assertEquals($expected, $actual);
+    $this->assertEquals(array(), $iterated);
   }
 
-  /**
-   * Verifies iteration.
-   *
-   */
-  #[@test]
-  public function iteration() {
-    $expected= $this->populatedMapFixture();
-    $this->assertMapIteration($expected);
-  }
-
-  /**
-   * Verifies iteration.
-   *
-   */
-  #[@test]
-  public function doubleIteration() {
-    $expected= $this->populatedMapFixture();
-    foreach ($this->map as $i => $pair) {
+  #[@test, @values('variations')]
+  public function iteration_invoked_twice($fixture, $pairs) {
+    $fixture->put($pairs[0]->key, $pairs[0]->value);
+    $fixture->put($pairs[1]->key, $pairs[1]->value);
+    $iterated= array();
+    foreach ($fixture as $pair) {
+      $iterated[]= $pair;
     }
-    $this->assertMapIteration($expected);
+    foreach ($fixture as $pair) {
+      $iterated[]= $pair;
+    }
+    $this->assertEquals(array($pairs[0], $pairs[1], $pairs[0], $pairs[1]), $iterated);
   }
 
-  /**
-   * Verifies iteration.
-   *
-   */
-  #[@test]
-  public function breakDoubleIteration() {
-    $expected= $this->populatedMapFixture();
-    foreach ($this->map as $i => $pair) {
+  #[@test, @values('variations')]
+  public function second_iteration_with_break_statement($fixture, $pairs) {
+    $fixture->put($pairs[0]->key, $pairs[0]->value);
+    $fixture->put($pairs[1]->key, $pairs[1]->value);
+    $iterated= array();
+    foreach ($fixture as $pair) {
+      $iterated[]= $pair;
+    }
+    foreach ($fixture as $pair) {
       break;
     }
-    $this->assertMapIteration($expected);
+    $this->assertEquals(array($pairs[0], $pairs[1]), $iterated);
+  }
+
+  #[@test]
+  public function string_representation_of_empty_map() {
+    $this->assertEquals(
+      'util.collections.HashTable[0] { }',
+      create(new HashTable())->toString()
+    );
+  }
+
+  #[@test]
+  public function string_representation_of_map_with_contents() {
+    $fixture= new HashTable();
+    $fixture->put('hello', 'World');
+    $fixture->put('hallo', 'Welt');
+    $this->assertEquals(
+      "util.collections.HashTable[2] {\n".
+      "  \"hello\" => \"World\",\n".
+      "  \"hallo\" => \"Welt\"\n".
+      "}",
+      $fixture->toString()
+    );
+  }
+
+  #[@test]
+  public function string_representation_of_generic_map() {
+    $this->assertEquals(
+      'util.collections.HashTable<string,lang.types.String>[0] { }',
+      create('new util.collections.HashTable<string, lang.types.String>')->toString()
+    );
   }
 }
