cfccde9d3447201d642c7c20e49f1aa6cd0c1176
Timm Friebe <thekid@thekid.de>
2014-08-27 15:38:12 +0200

    Support piping code with PHP open tag

--- core/src/main/php/xp/runtime/Evaluate.class.php
+++ core/src/main/php/xp/runtime/Evaluate.class.php
@@ -14,14 +14,22 @@ class Evaluate extends \lang\Object {
    * @param   string[] args
    */
   public static function main(array $args) {
+    $argc= sizeof($args);
 
     // Read sourcecode from STDIN if no further argument is given
-    if (0 === sizeof($args)) {
+    if (0 === $argc) {
+      $src= file_get_contents('php://stdin');
+    } else if ('--' === $args[0]) {
       $src= file_get_contents('php://stdin');
     } else {
       $src= $args[0];
     }
+
+    // Support <?php
     $src= trim($src, ' ;').';';
+    if (0 === strncmp($src, '<?php', 5)) {
+      $src= substr($src, 6);
+    }
 
     // Perform
     $argv= array(\xp::nameOf(__CLASS__)) + $args;
