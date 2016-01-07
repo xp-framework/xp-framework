3162dff8c05c1f28d24a971e833c97d6e50fabaa
Timm Friebe <thekid@thekid.de>
2016-01-07 16:40:01 +0100

    Welcome 2016

--- core/src/main/php/xp/runtime/Version.class.php
+++ core/src/main/php/xp/runtime/Version.class.php
@@ -23,7 +23,7 @@ class Version extends \lang\Object {
       zend_version(),
       php_uname()
     );
-    Console::writeLine('Copyright (c) 2001-2013 the XP group');
+    Console::writeLine('Copyright (c) 2001-2016 the XP group');
     foreach (\lang\ClassLoader::getLoaders() as $delegate) {
       Console::writeLine($delegate->toString());
     }
